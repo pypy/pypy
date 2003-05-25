5d9b8dbc5e40530b0c797ec1d66e152a47f22e4d
Michael Hudson <micahel@gmail.com>
2003-05-25 16:38:52 +0000

    path tweaks

--- pypy/objspace/std/test/testsupport.py
+++ pypy/objspace/std/test/testsupport.py
@@ -4,4 +4,4 @@ testdir   = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(testdir)
 rootdir   = os.path.dirname(os.path.dirname(parentdir))
 
-sys.path.insert(0, rootdir)
+sys.path.insert(0, os.path.dirname(rootdir))
