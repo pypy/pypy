90416f681ebd02e69beea5e5165324027172baca
Michael Hudson <micahel@gmail.com>
2003-05-25 16:05:49 +0000

    refactor run_file between main() & run_string()

--- pypy/interpreter/main.py
+++ pypy/interpreter/main.py
@@ -6,26 +6,31 @@ from pypy.interpreter import pyframe
 import sys
 
 def run_string(source, fname):
-    space = StdObjSpace()
-    code = compile(source, fname, 'exec')
-    ec = executioncontext.ExecutionContext(space)
-
-    w_globals = ec.make_standard_w_globals()
-    frame = pyframe.PyFrame(space, code, w_globals, w_globals)
     try:
-        ec.eval_frame(frame)
+        space = StdObjSpace()
+        code = compile(source, fname, 'exec')
+        ec = executioncontext.ExecutionContext(space)
+
+        w_globals = ec.make_standard_w_globals()
+        frame = pyframe.PyFrame(space, code, w_globals, w_globals)
     except baseobjspace.OperationError, operationerr:
-        # XXX insert exception info into the application-level sys.last_xxx
-        operationerr.print_detailed_traceback(space)
+        raise PyPyError(operationerr)
+    else:
+        ec.eval_frame(frame)
+
+def run_file(fname):
+    ifile = open(fname)
+    run_string(ifile.read(), fname)
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    fname = argv[1]
-
-    ifile = open(fname)
-    run_string(ifile.read(), fname)
+    try:
+        run_file(argv[1])
+    except baseobjspace.OperationError, operationerr:
+        # XXX insert exception info into the application-level sys.last_xxx
+        operationerr.print_detailed_traceback(space)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
