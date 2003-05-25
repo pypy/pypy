02c89d61afb28fddbb1283d4e1d571e330a6585e
Christian Tismer <tismer@stackless.com>
2003-05-25 16:17:01 +0000

    compile(), just a wrapper producing our code object.

--- pypy/module/builtin.py
+++ pypy/module/builtin.py
@@ -1,13 +1,25 @@
-import objectspace
+import baseobjspace
+
+from pypy.interpreter.pycode import PyCode
 
 #######################
 ####  __builtin__  ####
 #######################
 
-
+# XXX what should methodtables be?
 class methodtable:
 
     def chr(space, w_ascii):
         w_character = space.newstring([w_ascii])
         return w_character
 
+# redirecting certain things to the real builtins
+
+_b = __builtins__
+
+def compile(*args, **kwds):
+    c = _b.compile(*args, **kwds)
+    res = PyCode()
+    res._from_code(c)
+    return res
+compile.__doc__ = _b.compile.__doc__
