ace9880037c4573d9e6716f9a1fdb97f1559e0bb
Tomek Meka <tomek@codespeak.net>
2003-05-26 09:23:39 +0000

    One small change

--- pypy/module/builtin.py
+++ pypy/module/builtin.py
@@ -12,18 +12,12 @@ class methodtable:
         w_character = space.newstring([w_ascii])
         return w_character
 
-# redirecting certain things to the real builtins
-_b = __builtins__
+import __builtin__ as _b
 
 def compile(*args, **kwargs):
-    import types
-    if type(_b) == types.ModuleType:
-       fun = _b.compile
-    else:
-       fun = _b['compile']
-    c = fun(*args, **kwargs)
+    c = _b.compile(*args, **kwargs)
     res = PyCode()
     res._from_code(c)
     return res
 
-print compile("def bla(): return None", "?", "exec")
+compile.__doc__ = _b.compile.__doc__
