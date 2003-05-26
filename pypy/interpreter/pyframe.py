7b719a3ff9ba1d3ba44c4494ed07aa92422e09c5
Christian Tismer <tismer@stackless.com>
2003-05-26 12:52:17 +0000

    there is a problem with circular imports if one of them is spelling the package

--- pypy/interpreter/pyframe.py
+++ pypy/interpreter/pyframe.py
@@ -1,7 +1,10 @@
 """ PyFrame class implementation with the interpreter main loop.
 """
-
-from pypy.interpreter import opcode
+import opcode
+# XXX this is probably a Python bug:
+# circular import doesn't work if we spell this
+# from pypy.interpreter import opcode
+# report this to python-dev
 from executioncontext import OperationError, Stack
 import baseobjspace
 from appfile import AppFile
