38eabc2072b12c984a4dbca551c4722ee32c67a9
Tomek Meka <tomek@codespeak.net>
2003-05-26 15:08:45 +0000

    A simple __import__ hook

--- pypy/module/builtin.py
+++ pypy/module/builtin.py
@@ -1,11 +1,15 @@
 from pypy.interpreter.extmodule import *
 from pypy.interpreter.pycode import PyByteCode
+from pypy.interpreter.executioncontext import OperationError
 
 #######################
 ####  __builtin__  ####
 #######################
 
 import __builtin__ as _b
+
+##compile.__doc__ = _b.compile.__doc__
+
 
 class Builtin(BuiltinModule):
     __pythonname__ = '__builtin__'
@@ -19,18 +23,35 @@ class Builtin(BuiltinModule):
         return self.space.len(w_obj)
     len = appmethod(len)
 
-    def str(self, w_obj):
-        return self.space.str(w_obj)
-    str = appmethod(str)
+
+    def __import__(self,w_modulename,w_locals,w_globals,w_fromlist):
+        space = self.space
+        try:
+            w_mod = space.getitem(space.w_modules,w_modulename)
+            return w_mod
+        except OperationError,e:
+            if not e.match(space,space.w_KeyError):
+                raise
+            raise OperationError(space.w_ImportError)
+            
+        
+
+    __import__ = appmethod(__import__)
 
     def compile(self, w_str, w_filename, w_startstr,
-                w_supplied_flags, w_dont_inherit):
-        space = self.space
+                w_supplied_flags=None, w_dont_inherit=None):
         str = space.unwrap(w_str)
         filename = space.unwrap(w_filename)
         startstr = space.unwrap(w_startstr)
-        supplied_flags = space.unwrap(w_supplied_flags)
-        dont_inherit = space.unwrap(w_dont_inherit)
+        if w_supplied_flags is None:
+            supplied_flags = 0
+        else:
+            supplied_flags = space.unwrap(w_supplied_flags)
+        if w_dont_inherit is None:
+            dont_inherit = False
+        else:
+            dont_inherit = space.unwrap(w_dont_inherit)
+
         c = _b.compile(str, filename, startstr, supplied_flags, dont_inherit)
         res = PyByteCode()
         res._from_code(c)
