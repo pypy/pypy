fbc059a0140cc60bb8c0e251e27fc2f815629f41
Michael Hudson <micahel@gmail.com>
2003-05-26 16:30:35 +0000

    actually put a test in test_sysmodule.py

--- pypy/module/test/test_sysmodule.py
+++ pypy/module/test/test_sysmodule.py
@@ -1,3 +1,19 @@
-import unittest
+import testsupport, unittest
+from pypy.interpreter.unittest_w import TestCase_w
+from pypy.objspace.std.objspace import StdObjSpace
 
+class SysTests(TestCase_w):
+    def setUp(self):
+        self.space = StdObjSpace()
+        self.sys_w = self.space.getitem(self.space.w_modules,
+                                        self.space.wrap("sys"))
+    def tearDown(self):
+        pass
+
+    def test_stdout_exists(self):
+        s = self.space
+        self.failUnless_w(s.getattr(self.sys_w, s.wrap("stdout")))
+
+if __name__ == '__main__':
+    unittest.main()
 
