153ad8814f77394d930f5673b71402314b6a1ec5
Alex Martelli <alex@codespeak.net>
2003-05-26 16:41:51 +0000

    copy master testsupport in all test directories that need it

--- pypy/objspace/std/test/testsupport.py
+++ pypy/objspace/std/test/testsupport.py
@@ -1,9 +1,17 @@
+"""
+Master version of testsupport.py: copy into any subdirectory of pypy
+from which scripts need to be run (typically all of the 'test' subdirs)
+so that any test can "import testsupport" to ensure the parent of pypy
+is on the sys.path -- so that "import pypy.etc.etc." always works.
+"""
 import sys, os
-import unittest
 
-testdir   = os.path.dirname(os.path.abspath(__file__))
-parentdir = os.path.dirname(testdir)
-rootdir   = os.path.dirname(os.path.dirname(parentdir))
-
-sys.path.insert(0, os.path.dirname(rootdir))
+head = this_path = os.path.abspath(__file__)
+while 1:
+    head, tail = os.path.split(head)
+    if not tail:
+        raise EnvironmentError, "pypy not among parents of %r!" % this_path
+    elif tail.lower()=='pypy':
+        sys.path.insert(0, head)
+        break
 
