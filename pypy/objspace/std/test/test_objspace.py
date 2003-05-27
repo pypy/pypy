0b426be028bcb3e9553325bad59896176b450b5d
Michael Hudson <micahel@gmail.com>
2003-05-27 09:03:23 +0000

    add test of newstring behaving itself on incorrect input

--- pypy/objspace/std/test/test_objspace.py
+++ pypy/objspace/std/test/test_objspace.py
@@ -19,5 +19,16 @@ class TestStdObjectSpace(unittest_w.TestCase_w):
         chars_w = [w(ord(c)) for c in s]
         self.assertEqual_w(w(s), self.space.newstring(chars_w))
 
+    def test_newstring_fail(self):
+        w = self.space.wrap
+        s = 'abc'
+        not_chars_w = [w(c) for c in s]
+        self.assertRaises_w(self.space.w_TypeError,
+                            self.space.newstring,
+                            not_chars_w)
+        self.assertRaises_w(self.space.w_ValueError,
+                            self.space.newstring,
+                            [w(-1)])
+
 if __name__ == '__main__':
     unittest.main()
