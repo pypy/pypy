45d73cc073ad2a111fa926394c8f1df504d4d54c
Michael Hudson <micahel@gmail.com>
2003-05-27 09:44:43 +0000

    add test for chr

--- pypy/module/test/test_builtin.py
+++ pypy/module/test/test_builtin.py
@@ -1,6 +1,8 @@
 import testsupport
 
-from pypy.interpreter.unittest_w
+import unittest
+
+from pypy.interpreter import unittest_w
 from pypy.objspace.std import StdObjSpace
 
 class TestCompile(unittest_w.TestCase_w):
@@ -10,6 +12,19 @@ class TestCompile(unittest_w.TestCase_w):
 
    def tearDown(self):
       pass
+
+   def get_builtin(self, name):
+      s = self.space
+      w_name = s.wrap(name)
+      w_bltin = s.getitem(s.w_builtins, w_name)
+      return w_bltin
+
+   def test_chr(self):
+      s = self.space      
+      w = s.wrap
+      w_chr = self.get_builtin('chr')
+      self.assertEqual_w(w(chr(65)),
+                         s.call_function(w_chr, w(65)))
       
 if __name__ == '__main__':
     unittest.main()
