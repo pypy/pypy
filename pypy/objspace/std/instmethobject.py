2085e35cf1457ec3ac3631ee5785831a489e82df
Samuele Pedroni <pedronis@lucediurna.net>
2003-05-27 13:17:15 +0000

    instancemethod object beginning

---
+++ pypy/objspace/std/instmethobject.py
@@ -0,0 +1,30 @@
+from __future__ import nested_scopes
+from pypy.objspace.std.objspace import *
+
+
+class W_InstMethObject(object):
+    def __init__(w_self, w_im_self,w_im_func):
+        w_self.w_im_self = w_im_self
+        w_self.w_im_func = w_im_func
+
+
+#def function_unwrap(space, w_function):
+#    # XXX this is probably a temporary hack
+#    def proxy_function(*args, **kw):
+#        w_arguments = space.wrap(args)
+#        w_keywords  = space.wrap(kw)
+#        w_result = func_call(space, w_function, w_arguments, w_keywords)
+#        return space.unwrap(w_result)
+#    # XXX no closure implemented
+#    return proxy_function
+#
+#StdObjSpace.unwrap.register(function_unwrap, W_FuncObject)
+
+
+def instmeth_call(space, w_instmeth, w_arguments, w_keywords):
+    w_ret = space.call(self.w_im_func,
+                       space.wrap((space.add(space.newtuple([self.w_im_self]),space.unwrap(w_arguments)),
+                       w_keywords)
+    return w_ret
+
+StdObjSpace.call.register(instmeth_call, W_InstMeth, W_ANY, W_ANY)
