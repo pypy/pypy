5ab8c6926a40d27e1d5668407ab2776d6310ecc9
Alex Martelli <alex@codespeak.net>
2003-05-27 09:56:17 +0000

    new version of testsupport is richer and in particular eases the use of either trivial or standard object spaces for testing

--- pypy/testsupport.py
+++ pypy/testsupport.py
@@ -3,6 +3,20 @@ Master version of testsupport.py: copy into any subdirectory of pypy
 from which scripts need to be run (typically all of the 'test' subdirs)
 so that any test can "import testsupport" to ensure the parent of pypy
 is on the sys.path -- so that "import pypy.etc.etc." always works.
+
+Also, this module exposes a member 'TestCase' that is unittest.TestCase
+or a subclass thereof supplying extra methods; and a function 'main'
+that is unittest.main or the equivalent.
+
+Furthermore, this module now exposes a member 'objspace' which is
+by default class pypy.objspace.trivial.TrivialObjSpace but can be
+set to use another objectspace instead; this allows tests to run
+under different objectspaces without needing to edit their sources.
+
+For this setting, use environment variable OBJSPACE and set it to
+a value such as 'pypy.objspace.trivial.TrivialObjSpace' (which is
+also the default if the environment variable is not found or empty
+or without any dot in it).
 """
 import sys, os
 
@@ -14,4 +28,22 @@ while 1:
     elif tail.lower()=='pypy':
         sys.path.insert(0, head)
         break
+
+import pypy.interpreter.unittest_w
+TestCase = pypy.interpreter.unittest_w.TestCase_w
+import unittest
+main = unittest.main
+
+objspace_path = os.environ.get('OBJSPACE')
+if not objspace_path or '.' not in objspace_path:
+    import pypy.objspace.trivial
+    objspace = pypy.objspace.trivial.TrivialObjSpace
+else:
+    objspace_pieces = objspace_path.split('.')
+    objspace_path = '.'.join(objspace_pieces[:-1])
+    objspace_module = __import__(objspace_path)
+    for piece in objspace_pieces[1:-1]:
+        objspace_module = getattr(objspace_module, piece)
+    objspace_classname = objspace_pieces[-1]
+    objspace = getattr(objspace_module, objspace_classname)
 
