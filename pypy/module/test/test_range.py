de436dac68dc063447f1ad5f75a4b31a6c0d9d3f
Laura Creighton <lac@openend.se>
2003-05-27 17:01:21 +0000

    New range test for the new range that does not use append

--- pypy/module/test/test_range.py
+++ pypy/module/test/test_range.py
@@ -9,10 +9,16 @@ class TestRange(testsupport.TestCase):
    def tearDown(self):
       pass
 
+   def test_range_toofew(self):
+      self.assertRaises(TypeError, range)
+
+   def test_range_toomany(self):
+      self.assertRaises(TypeError, range,  1, 2, 3, 4)
+
    def test_range_one(self):
       self.assertEqual(range(1), [0])
 
-   def test_range_none(self):
+   def test_range_zero(self):
       self.assertEqual(range(0), [])
 
    def test_range_twoargs(self):
@@ -30,15 +36,17 @@ class TestRange(testsupport.TestCase):
    def test_range_decreasing_negativelargestep(self):
       self.assertEqual(range(5, -2, -3), [5, 2, -1])
 
-   def test_range_decreasing_negativelargestep2(self):
-      self.assertEqual(range(5, -3, -3), [5, 2, -1])
+   def test_range_increasing_positivelargestep(self):
+      self.assertEqual(range(-5, 2, 3), [-5, -2, 1])
 
    def test_range_zerostep(self):
       self.assertRaises(ValueError, range, 1, 5, 0)
 
-   def XXXtest_range_float(self):
+"""
+   def test_range_float(self):
       "How CPython does it - UGLY, ignored for now."
       self.assertEqual(range(0.1, 2.0, 1.1), [0, 1])
+      """
       
 if __name__ == '__main__':
     testsupport.main()
