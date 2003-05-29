975f4668b12b6f45d30797ba88dbc08b8d143c30
Tomek Meka <tomek@codespeak.net>
2003-05-29 12:08:02 +0000

    small changes..

--- pypy/objspace/std/stringobject.py
+++ pypy/objspace/std/stringobject.py
@@ -5,31 +5,51 @@ from listobject import W_ListObject
 from instmethobject import W_InstMethObject
 from pypy.interpreter.extmodule import make_builtin_func
 
+from rarray import CharArrayFromStr
+
 
 applicationfile = StdObjSpace.AppFile(__name__)
 
 class W_StringObject(W_Object):
     def __init__(w_self, space, str):
         W_Object.__init__(w_self, space)
-        w_self.value = str
+        w_self._value = CharArrayFromStr(str)
+
     def __repr__(w_self):
         """ representation for debugging purposes """
-        return "%s(%r)" % (w_self.__class__.__name__, w_self.value)
+        return "%s(%r)" % (w_self.__class__.__name__, w_self._value.value())
+
     def nonzero(w_self):
-        return W_IntObject(w_self.space, w_self.value != 0)
+        return W_IntObject(self.space, w_self._value.len != 0)
+
     def hash(w_self):
-        return W_IntObject(w_self, hash(w_self.value))
+        return W_IntObject(self, self._value.hash())
 
     def join(w_self, w_list):
-        firstelem = 1
-        res = ""
-        for w_item in w_list.wrappeditems:
-            if firstelem:
-                res = w_item.value    
-                firstelem = 0
-            else:
-                res = res + w_self.value + w_item.value
-        return W_StringObject(w_self.space, res)
+        if w_list:
+            firstelem = 1
+            listlen = 0
+            for w_item in w_list.wrappeditems:
+                reslen = reslen + len(w_item)
+                lislen = listlen + 1
+
+            reslen = reslen + (listlen - 1) * w_self._value.len
+            res = CharArraySize(reslen)
+
+            pos = 0
+            for w_item in w_list.wrappeditems:
+                if firstelem:
+                    res.setsubstring(idx, w_item._value)
+                    idx = idx + w_item._value.len 
+                    firstelem = 0
+                else:
+                    res.setsubstring(idx, w_self._value)
+                    idx = idx + w_self._value.len
+                    res.setsubstring(idx, w_item._value)
+                    idx = idx + w_item._value.len
+            return W_StringObject(w_self.space, res.value())
+        else:
+            return W_StringObject(w_self.space, "")
 
     def split(w_self, w_by=None):
         res = []
@@ -63,49 +83,49 @@ StdObjSpace.getattr.register(getattr_str, W_StringObject, W_ANY)
 
 
 def str_unwrap(space, w_str):
-    return w_str.value
+    return w_str._value.value()
 
 StdObjSpace.unwrap.register(str_unwrap, W_StringObject)
 
 def str_str_lt(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i < j )
 StdObjSpace.lt.register(str_str_lt, W_StringObject, W_StringObject)
 
 def str_str_le(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i <= j )
 StdObjSpace.le.register(str_str_le, W_StringObject, W_StringObject)
 
 def str_str_eq(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i == j )
 StdObjSpace.eq.register(str_str_eq, W_StringObject, W_StringObject)
 
 def str_str_ne(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i != j )
 StdObjSpace.ne.register(str_str_ne, W_StringObject, W_StringObject)
 
 def str_str_gt(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i > j )
 StdObjSpace.gt.register(str_str_gt, W_StringObject, W_StringObject)
 
 def str_str_ge(space, w_str1, w_str2):
-    i = w_str1.value
-    j = w_str2.value
+    i = w_str1._value.value()
+    j = w_str2._value.value()
     return space.newbool( i >= j )
 StdObjSpace.ge.register(str_str_ge, W_StringObject, W_StringObject)
 
 
 def getitem_str_int(space, w_str, w_int):
-    return W_StringObject(space, w_str.value[w_int.intval])
+    return W_StringObject(space, w_str._value.value()[w_int.intval])
 
 StdObjSpace.getitem.register(getitem_str_int, 
                              W_StringObject, W_IntObject)
@@ -117,7 +137,7 @@ StdObjSpace.getitem.register(getitem_str_slice,
                                 W_StringObject, W_SliceObject)
 
 def add_str_str(space, w_left, w_right):
-    return W_StringObject(space, w_left.value + w_right.value)
+    return W_StringObject(space, w_left._value.value() + w_right._value.value())
 
 StdObjSpace.add.register(add_str_str, W_StringObject, W_StringObject)
 
@@ -128,7 +148,7 @@ def mod_str_tuple(space, w_format, w_args):
     notImplemented
 
 def len_str(space, w_str):
-    return space.wrap(len(w_str.value))
+    return space.wrap(w_str._value.len)
 
 StdObjSpace.len.register(len_str, W_StringObject)
 
