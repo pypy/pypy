e97ddcd18268c3ce5a45239bacca63b5d6c9b27a
Michael Hudson <micahel@gmail.com>
2003-05-30 11:16:30 +0000

    make a stab at a better sys.path add less irritating sys.displayhook

--- pypy/module/sysmodule.py
+++ pypy/module/sysmodule.py
@@ -1,7 +1,32 @@
+from pypy.interpreter.baseobjspace import OperationError
 from pypy.interpreter.extmodule import *
 import sys
 
 class Sys(BuiltinModule):
     __pythonname__ = 'sys'
+
+    def __init__(self, space):
+        BuiltinModule.__init__(self, space)
+
+        import sys, os
+        import pypy
+
+        opd = os.path.dirname
+        
+        pypydir = opd(opd(os.path.abspath(pypy.__file__)))
+
+        self.path = appdata([p for p in sys.path if p != pypydir])        
+    
     stdout = appdata(sys.stdout)
-    displayhook = appdata(sys.displayhook)
+
+    def displayhook(self, w_x):
+        space = self.space
+        w = space.wrap
+        if w_x != space.w_None:
+            try:
+                print space.unwrap(self.space.str(w_x))
+            except OperationError:
+                print "! could not print", w_x
+            space.setitem(space.w_builtins, w('_'), w_x)
+    displayhook = appmethod(displayhook)
+    
