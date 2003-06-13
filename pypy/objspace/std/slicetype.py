5aee1a8fef1f73eb1e933b050ba229cedf607d88
Michael Hudson <micahel@gmail.com>
2003-06-13 13:38:15 +0000

    slice_new

--- pypy/objspace/std/slicetype.py
+++ pypy/objspace/std/slicetype.py
@@ -5,3 +5,24 @@ from typeobject import W_TypeObject
 class W_SliceType(W_TypeObject):
 
     typename = 'slice'
+
+def slicetype_new(space, w_slicetype, w_args, w_kwds):
+    if space.is_true(w_kwds):
+        raise OperationError(space.w_TypeError,
+                             space.wrap("no keyword arguments expected"))
+    args = space.unpackiterable(w_args)
+    start = space.w_None
+    stop = space.w_None
+    step = space.w_None
+    if len(args) == 1:
+        stop, = args
+    elif len(args) == 2:
+        start, stop = args
+    elif len(args) == 3:
+        start, stop, step = args        
+    else:
+        raise OperationError(space.w_TypeError,
+                             space.wrap("slice() takes at least 1 argument"))
+    return space.newslice(start, stop, step)
+
+StdObjSpace.new.register(slicetype_new, W_SliceType, W_ANY, W_ANY)
