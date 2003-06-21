ca6c416a2d601ca49d2cab1fe2e292f7e0ee5747
Holger Krekel <holger.krekel@gmail.com>
2003-06-21 16:06:12 +0000

    added some docstrings (+ status info)

--- pypy/objspace/std/noneobject.py
+++ pypy/objspace/std/noneobject.py
@@ -1,13 +1,15 @@
+"""
+  None Object implementation
+
+  ok and tested
+""" 
+
 from pypy.objspace.std.objspace import *
 from nonetype import W_NoneType
 
-
 class W_NoneObject(W_Object):
     statictype = W_NoneType
-
-
 registerimplementation(W_NoneObject)
-
 
 def unwrap__None(space, w_none):
     return None
@@ -17,7 +19,6 @@ def is_true__None(space, w_none):
 
 def repr__None(space, w_none):
     return space.wrap('None')
-
 
 register_all(vars())
 
