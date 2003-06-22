3a8aa08f73b970ea93f9146be89d5f2ce958f3a7
Laura Creighton <lac@openend.se>
2003-06-22 16:05:02 +0000

    Cloned the tuple tests for lists so that gt, eq and friends are tested. In the process we have found an interesting bug, in the lt gt are defined in terms of each other.  But hasattr isn't finding them all despite the fact that they  are there.

--- pypy/objspace/std/test/test_listobject.py
+++ pypy/objspace/std/test/test_listobject.py
@@ -30,20 +30,7 @@ class TestW_ListObject(test.TestCase):
         self.assertEqual_w(self.space.len(w_list), w(1))
         w_list = W_ListObject(self.space, [w(5), w(3), w(99)]*111)
         self.assertEqual_w(self.space.len(w_list), w(333))
-
-    def test_mul(self):
-        # only testing right mul at the moment
-        w = self.space.wrap
-        arg = w(2)
-        n = 3
-        w_lis = W_ListObject(self.space, [arg])
-        w_lis3 = W_ListObject(self.space, [arg]*n)
-        w_res = self.space.mul(w_lis, w(n))
-        self.assertEqual_w(w_lis3, w_res)
-        # commute
-        w_res = self.space.mul(w(n), w_lis)
-        self.assertEqual_w(w_lis3, w_res)
-
+ 
     def test_getitem(self):
         w = self.space.wrap
         w_list = W_ListObject(self.space, [w(5), w(3)])
@@ -68,6 +55,39 @@ class TestW_ListObject(test.TestCase):
         self.assertRaises(NoValue, self.space.next, w_iter)
         self.assertRaises(NoValue, self.space.next, w_iter)
 
+    def test_contains(self):
+        w = self.space.wrap
+        w_list = W_ListObject(self.space, [w(5), w(3), w(99)])
+        self.assertEqual_w(self.space.contains(w_list, w(5)),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.contains(w_list, w(99)),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.contains(w_list, w(11)),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.contains(w_list, w_list),
+                           self.space.w_False)
+
+    def test_getslice(self):
+        w = self.space.wrap
+
+        def test1(testlist, start, stop, step, expected):
+            w_slice  = self.space.newslice(w(start), w(stop), w(step))
+            w_list = W_ListObject(self.space, [w(i) for i in testlist])
+            w_result = self.space.getitem(w_list, w_slice)
+            self.assertEqual(self.space.unwrap(w_result), expected)
+        
+        for testlist in [[], [5,3,99], list(range(5,555,10))]:
+            for start in [-2, -1, 0, 1, 10]:
+                for end in [-1, 0, 2, 999]:
+                    test1(testlist, start, end, 1, testlist[start:end])
+
+        test1([5,7,1,4], 3, 1, -2,  [4,])
+        test1([5,7,1,4], 3, 0, -2,  [4, 7])
+        test1([5,7,1,4], 3, -1, -2, [])
+        test1([5,7,1,4], -2, 11, 2, [1,])
+        test1([5,7,1,4], -3, 11, 2, [7, 4])
+        test1([5,7,1,4], -5, 11, 2, [5, 1])
+
     def test_add(self):
         w = self.space.wrap
         w_list0 = W_ListObject(self.space, [])
@@ -81,6 +101,19 @@ class TestW_ListObject(test.TestCase):
                                               [w(-7)] * 111))
         self.assertEqual_w(self.space.add(w_list1, w_list0), w_list1)
         self.assertEqual_w(self.space.add(w_list0, w_list2), w_list2)
+
+    def test_mul(self):
+        # only testing right mul at the moment
+        w = self.space.wrap
+        arg = w(2)
+        n = 3
+        w_lis = W_ListObject(self.space, [arg])
+        w_lis3 = W_ListObject(self.space, [arg]*n)
+        w_res = self.space.mul(w_lis, w(n))
+        self.assertEqual_w(w_lis3, w_res)
+        # commute
+        w_res = self.space.mul(w(n), w_lis)
+        self.assertEqual_w(w_lis3, w_res)
 
     def test_getslice(self):
         # this takes aaagggeeesss!!!
@@ -118,6 +151,129 @@ class TestW_ListObject(test.TestCase):
         self.assertRaises_w(self.space.w_IndexError,
                             self.space.setitem, w_list, w(-3), w(5))
 
+    def test_eq(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+
+        self.assertEqual_w(self.space.eq(w_list0, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.eq(w_list1, w_list0),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.eq(w_list1, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.eq(w_list1, w_list2),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.eq(w_list2, w_list3),
+                           self.space.w_False)
+    def test_ne(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+
+        self.assertEqual_w(self.space.ne(w_list0, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.ne(w_list1, w_list0),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.ne(w_list1, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.ne(w_list1, w_list2),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.ne(w_list2, w_list3),
+                           self.space.w_True)
+    def test_lt(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+        w_list4 = W_ListObject(self.space, [w(5), w(3), w(9), w(-1)])
+
+        self.assertEqual_w(self.space.lt(w_list0, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.lt(w_list1, w_list0),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.lt(w_list1, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.lt(w_list1, w_list2),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.lt(w_list2, w_list3),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.lt(w_list4, w_list3),
+                           self.space.w_True)
+        
+    def test_ge(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+        w_list4 = W_ListObject(self.space, [w(5), w(3), w(9), w(-1)])
+
+        self.assertEqual_w(self.space.ge(w_list0, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.ge(w_list1, w_list0),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.ge(w_list1, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.ge(w_list1, w_list2),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.ge(w_list2, w_list3),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.ge(w_list4, w_list3),
+                           self.space.w_False)
+        
+    def test_gt(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+        w_list4 = W_ListObject(self.space, [w(5), w(3), w(9), w(-1)])
+
+        self.assertEqual_w(self.space.gt(w_list0, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.gt(w_list1, w_list0),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.gt(w_list1, w_list1),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.gt(w_list1, w_list2),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.gt(w_list2, w_list3),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.gt(w_list4, w_list3),
+                           self.space.w_False)
+        
+    def test_le(self):
+        w = self.space.wrap
+        
+        w_list0 = W_ListObject(self.space, [])
+        w_list1 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list2 = W_ListObject(self.space, [w(5), w(3), w(99)])
+        w_list3 = W_ListObject(self.space, [w(5), w(3), w(99), w(-1)])
+        w_list4 = W_ListObject(self.space, [w(5), w(3), w(9), w(-1)])
+
+        self.assertEqual_w(self.space.le(w_list0, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.le(w_list1, w_list0),
+                           self.space.w_False)
+        self.assertEqual_w(self.space.le(w_list1, w_list1),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.le(w_list1, w_list2),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.le(w_list2, w_list3),
+                           self.space.w_True)
+        self.assertEqual_w(self.space.le(w_list4, w_list3),
+                           self.space.w_True)
 
 if __name__ == '__main__':
     test.main()
