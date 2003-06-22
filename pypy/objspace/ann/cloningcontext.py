39bf2d0e83d6389bc7917202f20cd4ad50268173
Guido van Rossum <guido@python.org>
2003-06-22 16:02:50 +0000

    Introduce a FunctionInfo object.

--- pypy/objspace/ann/cloningcontext.py
+++ pypy/objspace/ann/cloningcontext.py
@@ -13,17 +13,47 @@ class IndeterminateCondition(ControlFlowException):
         frame.next_instr = last_instr # Restart failed opcode (!)
         frame.restarting = (self.w_obj, True) # For bytecode_trace() below
 
+class FunctionInfo(object):
+
+    def __init__(self):
+        self.result = None
+        self.clones = [] # List of frames
+        self.knownframes = {} # Mapping from next_instr to list of frames
+
+    def addresult(self, result):
+        self.result = union(self.result, result)
+
+    def addclone(self, frame):
+        self.clones.append(frame)
+
+    def addknown(self, frame):
+        frames = self.knownframes.setdefault(frame.next_instr, [])
+        frames.append(frame)
+        assert len(frames) <= 1 # We think this is true
+
+    def iterknown(self, frame):
+        return iter(self.knownframes.get(frame.next_instr, []))
+
+    def todo(self):
+        return len(self.clones)
+
+    def getframe(self):
+        return self.clones.pop()
+
+    def getresult(self):
+        return self.result
+
 class CloningExecutionContext(ExecutionContext):
 
     def __init__(self, space):
         ExecutionContext.__init__(self, space)
-        self.knownframes = {}
-        # {(bytecode, w_globals): (result, clones, {next_instr: [frame, ...], ...}), ...}
+        self.functioninfos = {}
+        # {(bytecode, w_globals): FunctionInfo(), ...}
 
     def bytecode_trace(self, frame):
         assert isinstance(frame.w_globals, W_Constant)
         key = (frame.bytecode, id(frame.w_globals.value))
-        result, clones, subdict = self.knownframes[key]
+        info = self.functioninfos[key]
 
         if frame.restarting is not None:
             w_obj, flag = frame.restarting
@@ -32,31 +62,28 @@ class CloningExecutionContext(ExecutionContext):
             if flag:
                 f2 = frame.clone()
                 f2.restarting = (w_obj, False)
-                clones.append(f2)
+                info.addclone(f2)
             return
 
-        frames = subdict.setdefault(frame.next_instr, [])
-        assert len(frames) <= 1 # We think this is true
-        for f in frames:
+        for f in info.iterknown(frame):
             if compatible_frames(frame, f):
                 c1, c2 = unite_frames(frame, f)
                 if not c2:
                     # A fixpoint
                     raise ExitFrame(None)
                 return
-        frames.append(frame.clone())
+        info.addknown(frame.clone())
 
     def eval_frame(self, frame):
-        assert not hasattr(frame, "clones")
-        assert self.space == frame.space
+        assert self.space is frame.space
         frame.restarting = None
         key = (frame.bytecode, id(frame.w_globals.value))
-        result, clones, subdict = self.knownframes.setdefault(key, (None, [], {}))
-        clones.append(frame)
-        while clones:
-            f = clones.pop()
+        info = self.functioninfos.get(key)
+        if info is None:
+            self.functioninfos[key] = info = FunctionInfo()
+        info.addclone(frame)
+        while info.todo():
+            f = info.getframe()
             r = ExecutionContext.eval_frame(self, f)
-            result, clones, subdict = self.knownframes[key]
-            result = union(result, r)
-            self.knownframes[key] = result, clones, subdict
-        return result
+            info.addresult(r)
+        return info.getresult()
