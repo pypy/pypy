0182421ff6fc63006d850c1033c21864c6973244
Seo Sanghyeon <sanxiyn@codespeak.net>
2003-10-11 19:07:58 +0000

    Fixed problem that buildcl-ed function couldn't be called multiple times.

--- pypy/translator/test/buildcl.py
+++ pypy/translator/test/buildcl.py
@@ -14,20 +14,17 @@ def make_cl_func(func, cl, path):
     fun = FlowObjSpace().build_flow(func)
     gen = GenCL(fun)
     out = gen.emitcode()
-    fp = path.join("test.lisp")
-    i = 0
+    i = 1
+    fp = path.join("test%d.lisp" % i)
     while fp.exists():
         fp = path.join("test%d.lisp" % i)
         i += 1
-    fp.write(out)
-    fname = fp.path
     def _(*args):
-        fp = file(fname, "a")
+        fp.write(out)
         print >>fp, "(write (", fun.functionname,
         for arg in args:
-            print >>fp, str(arg),
+            print >>fp, gen.conv(arg),
         print >>fp, "))"
-        fp.close()
         output = exec_cmd("%s %s" % (cl, fname))
         return readlisp(output)
     return _
