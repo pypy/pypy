be3d2bc29468ac57ebe0b168a1045c74a659b6fc
Richard Emslie <richardemslie@gmail.com>
2003-12-15 15:19:05 +0000

    Testing.

--- pypy/objspace/trace.py
+++ pypy/objspace/trace.py
@@ -4,7 +4,7 @@ import sys, operator, types
 import pypy
 from pypy.objspace.std import StdObjSpace
 from pypy.interpreter.baseobjspace import ObjSpace
-
+from pypy.interpreter.pycode import PyCode
 debug = 0
 
 # ______________________________________________________________________
@@ -23,7 +23,36 @@ class TraceObjSpace(ObjSpace):
                 setattr(self, key, logger)
 
 
-Space = TraceObjSpace
+    def runx(self, func, *args):
+        globals = {}
+            
+        w_globals = self.wrap(globals) 
+        args_w = [self.wrap(ii) for ii in args]
 
+        ec = self.getexecutioncontext()
+
+        code = func.func_code
+        code = PyCode()._from_code(code)
+
+        frame = code.create_frame(space, w_globals)
+        frame.setfastscope(args_w)
+        
+        return frame.run()
+        
+
+Space = TraceObjSpace
+s = Space()
 # ______________________________________________________________________
 # End of trace.py
+
+
+def runx(space, func, *args):
+    globals = {}
+    w_globals = space.wrap(globals) 
+    args_w = [space.wrap(ii) for ii in args]
+    ec = space.getexecutioncontext()
+    code = func.func_code
+    code = PyCode()._from_code(code)
+    frame = code.create_frame(space, w_globals)
+    frame.setfastscope(args_w)
+    return frame.run()
