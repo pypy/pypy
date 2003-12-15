f972cbdf4c27d402f303f88610afe59d7c025329
Tomek Meka <tomek@codespeak.net>
2003-12-15 16:13:00 +0000

    we still work on it, please ignone this commit

--- pypy/objspace/trace.py
+++ pypy/objspace/trace.py
@@ -1,6 +1,6 @@
 # ______________________________________________________________________
 import autopath
-import sys, operator, types
+import sys, operator, types, new
 import pypy
 from pypy.objspace.std import StdObjSpace
 from pypy.interpreter.baseobjspace import ObjSpace
@@ -13,15 +13,25 @@ class TraceObjSpace(ObjSpace):
     
     def initialize(self):
         space = StdObjSpace()
+        self.space = space
         for key, item in space.__dict__.items():
             if not callable(item):
+                print "key: %s" % key
                 setattr(self, key, item)
             else:
-                def logger(*args, **kwargs):
-                    print "XXX"
+                def logger(self, *args, **kwargs):
+                    print "instance method %s, args: %s" % (key, args)
                     return item(*args, **kwargs)
-                setattr(self, key, logger)
+                setattr(self, key, new.instancemethod(logger, self, TraceObjSpace))
 
+        for key in space.__class__.__dict__.keys():
+            item = getattr(space, key)
+            if callable(item) and not key.startswith('__'):
+                def logger(self, *args, **kwargs):
+                    print "class method %s, args: %s" % (key, args)
+                    return item(*args, **kwargs)
+
+                setattr(self, key, new.instancemethod(logger, self, TraceObjSpace))
 
     def runx(self, func, *args):
         globals = {}
@@ -56,3 +66,9 @@ def runx(space, func, *args):
     frame = code.create_frame(space, w_globals)
     frame.setfastscope(args_w)
     return frame.run()
+
+if __name__ == "__main__":
+    def a(b):
+        return b+1
+
+    print runx(s, a, 1)
