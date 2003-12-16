02204b2eed3d3560f21920a7d9c0f0c0d8d3b164
Alex Martelli <alex@codespeak.net>
2003-12-16 10:04:41 +0000

    Added application-level test case

--- pypy/objspace/std/test/test_boolobject.py
+++ pypy/objspace/std/test/test_boolobject.py
@@ -23,6 +23,18 @@ class TestW_BoolObject(test.TestCase):
     def test_false(self):
         self.failIf_w(self.false)
         
+class AppBoolTest(test.AppTestCase):
+    def setUp(self):
+        self.space = test.objspace('std')
+
+    def test_bool_callable(self):
+        self.assertEquals(True, bool(1))
+        self.assertEquals(False, bool(0))
+        self.assertEquals(False, bool())
+
+    def test_bool_string(self):
+        self.assertEquals("True", str(True))
+        self.assertEquals("False", str(False))
 
 if __name__ == '__main__':
     test.main()
