4d8eee18c0360ebb3aad9b32cbdf30f092dd8591
Jacob Hallen <jacob@openend.se>
2003-12-16 10:19:23 +0000

    Fixed __new__.

--- pypy/objspace/std/booltype.py
+++ pypy/objspace/std/booltype.py
@@ -13,3 +13,22 @@ class W_BoolType(W_TypeObject):
     staticbases = (W_IntType,)
 
 registerimplementation(W_BoolType)
+
+def type_new__BoolType_BoolType(space, w_basetype, w_booltype, w_args, w_kwds):
+    if space.is_true(w_kwds):
+        raise OperationError(space.w_TypeError,
+                             space.wrap("no keyword arguments expected"))
+    args = space.unpackiterable(w_args)
+    if len(args) == 0:
+        return space.w_False, True
+    elif len(args) == 1:
+        arg = args[0]
+        if space.is_true(arg):
+            return space.w_True, True
+        else:
+            return space.w_False, True
+    else:
+        raise OperationError(space.w_TypeError,
+                             space.wrap("bool() takes at most 1 argument"))
+
+register_all(vars())
