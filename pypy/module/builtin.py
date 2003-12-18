e8389b165c6f6f8ef66390f99cc4fb1df9c97321
Jacob Hallen <jacob@openend.se>
2003-12-18 17:44:26 +0000

    Added license, copyright and stub help.

--- pypy/module/builtin.py
+++ pypy/module/builtin.py
@@ -694,8 +694,27 @@ class __builtin__(ExtModule):
         if not isinstance(s, str):
             raise TypeError("intern() argument 1 must be string.")
         return s
+
+    def app_copyright(self):
+        print 'Copyright 2002-2004 Pypy development team.\nAll rights reserved.\nFor further information see http://www.codespaek.net/pypy.\nSome materials may have a different copyright.\nIn these cases, this is explicitly noted in the source code file.'
+
+    def app_license(self):
+        print \
+"""
+Copyright (c) <2002-2004> <Pypy development team>
+        
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+    def app_help(self):
+        print "You must be joking."
     
 # source code for the builtin xrange-class
+
 xrange_appsource = """if 1: 
     class xrange:
         def __init__(self, start, stop=None, step=1):
