92c657d0b440eee9dcecd53eacf4185861e0fb7e
Patrick Maupin <pmaupin@codespeak.net>
2003-12-19 13:37:25 +0000

    Check that module attributes are strings

--- pypy/interpreter/module.py
+++ pypy/interpreter/module.py
@@ -5,6 +5,11 @@ Module objects.
 from pypy.interpreter.baseobjspace import Wrappable
 from pypy.interpreter.error import OperationError
 
+def _checkattrtype(space, w_attr):
+    attr = space.unwrap(w_attr)
+    if not isinstance(attr, str):
+        raise OperationError(space.w_TypeError,
+                  space.wrap('attribute name must be string'))
 
 class Module(Wrappable):
     """A module."""
@@ -16,6 +21,7 @@ class Module(Wrappable):
 
     def pypy_getattr(self, w_attr):
         space = self.space
+        _checkattrtype(space, w_attr)
         if space.is_true(space.eq(w_attr, space.wrap('__dict__'))):
             return self.w_dict
         try:
@@ -27,10 +33,13 @@ class Module(Wrappable):
             raise OperationError(space.w_AttributeError, w_attr)
 
     def pypy_setattr(self, w_attr, w_value):
-        self.space.setitem(self.w_dict, w_attr, w_value)
+        space = self.space
+        _checkattrtype(space, w_attr)
+        space.setitem(self.w_dict, w_attr, w_value)
 
     def pypy_delattr(self, w_attr):
         space = self.space
+        _checkattrtype(space, w_attr)
         try:
             space.delitem(self.w_dict, w_attr)
         except OperationError, e:
