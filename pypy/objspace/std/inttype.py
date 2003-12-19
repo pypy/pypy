5525ed507396ea10225ab90e2edcb0570cc65102
Patrick Maupin <pmaupin@codespeak.net>
2003-12-19 18:09:57 +0000

    Reorganize int() to allow two parameters, and to propagate most exceptions

--- pypy/objspace/std/inttype.py
+++ pypy/objspace/std/inttype.py
@@ -14,21 +14,32 @@ def type_new__IntType_IntType(space, w_basetype, w_inttype, w_args, w_kwds):
         raise OperationError(space.w_TypeError,
                              space.wrap("no keyword arguments expected"))
     args = space.unpackiterable(w_args)
-    if len(args) == 0:
+    arglen = len(args)
+    
+    if arglen == 0:
         return space.newint(0), True
-    elif len(args) == 1:
-        arg = args[0]
-        if space.is_true(space.issubtype(space.type(arg),
-                                         space.w_str)):
-            try:
-                return space.newint(int(space.unwrap(arg))), True
-            except TypeError:
-                raise OperationError(space.w_TypeError,
-                                     space.wrap("invalid literal for int()"))
-        else:
-            return space.int(args[0]), True
-    else:
+    elif arglen > 2:
         raise OperationError(space.w_TypeError,
-                             space.wrap("int() takes at most 1 argument"))
+                 space.wrap("int() takes at most 2 arguments"))
+    elif space.is_true(space.issubtype(space.type(args[0]), space.w_str)):
+        try:
+            if arglen == 1:
+                return space.newint(int(space.unwrap(args[0]))), True
+            else:
+                return space.newint(int(space.unwrap(args[0]),space.unwrap(args[1]))), True
+        except TypeError, e:
+            raise OperationError(space.w_TypeError,
+                         space.wrap(str(e)))
+        except ValueError, e:
+            raise OperationError(space.w_ValueError,
+                         space.wrap(str(e)))
+        except OverflowError, e:
+            raise OperationError(space.w_OverflowError,
+                         space.wrap(str(e)))
+    elif arglen == 2:
+        raise OperationError(space.w_TypeError,
+             space.wrap("int() can't convert non-string with explicit base"))
+    else:
+        return space.int(args[0]), True
 
 register_all(vars())
