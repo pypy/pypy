df0e0b0ec2cd5b67db5da21f48430957a34e1a06
Richard Emslie <richardemslie@gmail.com>
2004-01-19 10:20:43 +0000

    Tidies to a premature checkin. ;-)
    Tab completion works a little better.

--- pypy/tool/traceinteractive.py
+++ pypy/tool/traceinteractive.py
@@ -1,15 +1,12 @@
-# Python imports
+# Standard imports
 import sys
 import code
-import repr
 import keyword
-import linecache
 
 assert sys.version_info >= (2,3), "sorry, can only run with python2.3 and greater"
 
 try:
     import readline
-    import rlcompleter
     have_readline = True
     
 except:
@@ -27,30 +24,44 @@ from pypy.interpreter.baseobjspace import ObjSpace
 
 from pypy.objspace import trace
 
-# Global
-operations = dict([(r[0], r[0]) for r in ObjSpace.MethodTable])
-
 
 #//////////////////////////////////////////////////////////////////////////
 
 if have_readline:
-    class Completer(rlcompleter.Completer):
-        # TODO Tailor this for own means
 
-        def __init__(self, objspace):
-            self.objspace = objspace
-            
+    class Completer:
+        def __init__(self, space):
+            self.space = space
 
-        # Very frustratingly, we have a lot of duplication of rlcompleter here
+        def complete(self, text, state):
+
+            if state == 0:
+                if "." in text:
+                    self.matches = self.attr_matches(text)
+                else:
+                    self.matches = self.global_matches(text)
+            try:
+                return self.matches[state]
+
+            except IndexError:
+                return None
+
+
         def global_matches(self, text):
-            print "GERE"
-            locals = self.objspace.unwrap(self.objspace.w_locals)
+            
+            import __builtin__            
+
+            w_res = self.space.call_method(self.space.w_globals, "keys")
+            namespace_keys = self.space.unwrap(w_res)
+
             matches = []
             n = len(text)
-            for l in [locals.keys(), __builtin__.__dict__.keys(), keyword.kwlist]:
+            
+            for l in [namespace_keys, __builtin__.__dict__.keys(), keyword.kwlist]:
                 for word in l:
                     if word[:n] == text and word != "__builtins__":
                         matches.append(word)
+            
             return matches
 
         def attr_matches(self, text):
@@ -60,43 +71,36 @@ if have_readline:
                 return
 
             expr, attr = m.group(1, 3)
-            object = self.locals[expr]
-            words = dir(object)
-            if hasattr(object, '__class__'):
-                words.append('__class__')
-                words = words + self.get_class_members(object.__class__)
-
+            s = self.space
+            w_obj = s.getitem(s.w_globals, s.wrap(expr))
+            w_func = s.getitem(s.w_builtins, s.wrap("dir"))
+            w_res = s.call_function(w_func, w_obj)
+            words = s.unwrap(w_res)
             matches = []
             n = len(attr)
             for word in words:
 
                 if word[:n] == attr and word != "__builtins__":
                     matches.append("%s.%s" % (expr, word))
-            return matches
 
-        def get_class_members(self, klass):
-            ret = dir(klass)
-            if hasattr(klass, '__bases__'):
-                for base in klass.__bases__:
-                    ret = ret + self.get_class_members(base)
-            return ret
+            return matches
 
 
 #//////////////////////////////////////////////////////////////////////////
 
 class TraceConsole(code.InteractiveConsole):
-    def __init__(self, objspace):
+    def __init__(self, space):
         code.InteractiveConsole.__init__(self)
-        s = self.space = trace.TraceObjSpace(objspace)
+        s = self.space = trace.TraceObjSpace(space)
         s.setitem(s.w_globals, s.wrap("__pytrace__"), s.w_True)
-        self.objspacename = objspace.__class__.__name__
+        self.objspacename = space.__class__.__name__
 
 
     def interact(self, banner=None):
         if banner is None:
             banner = "Python %s in pypy(trace)\n%s / %s - %s" % (
                 sys.version, self.__class__.__name__,
-                self.objspacename,
+                self.space,
                 " [Use  __pytrace__ flag to turn off tracing.]" )
         code.InteractiveConsole.interact(self, banner)
 
@@ -111,61 +115,71 @@ class TraceConsole(code.InteractiveConsole):
         # 'code' is a CPython code object
         from pypy.interpreter.pycode import PyCode
         pycode = PyCode()._from_code(code)
-        try:
-            s = self.space
-            trace_flag = s.unwrap(s.getitem(s.w_globals,
-                                            s.wrap("__pytrace__")))
-            if trace_flag:
-                s.settrace()
 
+        s = self.space
+        trace_flag = s.unwrap(s.getitem(s.w_globals,
+                                        s.wrap("__pytrace__")))
+        if trace_flag:
+            s.settrace()
+
+        try:
             pycode.exec_code(s, s.w_globals, s.w_globals)
             if trace_flag:
                 res = s.getresult()
                 s.settrace()
                 print_result(res)
-        
+                
         except baseobjspace.OperationError, operationerr:
+            if trace_flag:
+                res = s.getresult()
+                s.settrace()
+                print_result(res)
+
             # XXX insert exception info into the application-level sys.last_xxx
+            print
             operationerr.print_detailed_traceback(self.space)
 
         else:
-            try:
-                if sys.stdout.softspace:
-                    print
+            print
 
-            except AttributeError:
-                # Don't crash if user defined stdout doesn't have softspace
-                pass
-
-    def runsource(self, source, ignored_filename="<input>", symbol="single"):
-        hacked_filename = '<inline>\n'+source
+    def runsource(self, source, ignored_filename = "<input>", symbol = "single"):
+        hacked_filename = '<inline>\n' + source
         try:
             code = self.compile(source, hacked_filename, symbol)
+
         except (OverflowError, SyntaxError, ValueError):
             self.showsyntaxerror(self.filename)
-            return 0
+            return False
+
         if code is None:
-            return 1
+            return True
+
         self.runcode(code)
-        return 0
+        return False
 
 
+#//////////////////////////////////////////////////////////////////////////
 
+def trace_interactive(space, banner = None):
+    s = space
 
-def trace_interactive(objspace, banner = None):
-    s = objspace
-
+    # Create an execution context, and set the globals
     ec = s.getexecutioncontext()
     s.w_globals = ec.make_standard_w_globals()
+    
     s.setitem(s.w_globals, s.wrap("__name__"), s.wrap("__main__"))
     console = TraceConsole(s)
+
     if have_readline:
         # Keep here to save windoze tears
+
         readline.set_completer(Completer(s).complete)
         readline.parse_and_bind("tab: complete")
         readline.set_history_length(25000)
+
         try:
             readline.read_history_file()
+
         except IOError:
             pass # guess it doesn't exit 
 
@@ -175,15 +189,15 @@ def trace_interactive(objspace, banner = None):
     console.interact(banner)
 
 
-if __name__ == '__main__':
+#//////////////////////////////////////////////////////////////////////////
 
+if __name__ == '__main__':
     from pypy.tool import option
-    from pypy.tool import testit
     args = option.process_options(option.get_standard_options(),
                                   option.Options)
 
 
     # Create objspace...
-    objspace = option.objspace()
-    trace_interactive(objspace)
+    space = option.objspace()
+    trace_interactive(space)
     
