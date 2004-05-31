d8d6120b82e16ed4a6d60fa312d81a3f25c467ac
Holger Krekel <holger.krekel@gmail.com>
2004-05-31 15:01:35 +0000

    fixeol + fixtabbing of test_import

--- pypy/module/test/test_import.py
+++ pypy/module/test/test_import.py
@@ -3,73 +3,73 @@ from pypy.tool import testit
 
 class TestImport(testit.AppTestCase):
 
-   def setUp(self): # interpreter-level
-      self.space = testit.new_objspace()
+    def setUp(self): # interpreter-level
+        self.space = testit.new_objspace()
 
-   def test_import_sys(self):
-      import sys
+    def test_import_sys(self):
+        import sys
 
-   def test_import_a(self):
-      import sys
-      sys.path.append('impsubdir')
-      import a
-      self.assert_('a' in sys.modules)
+    def test_import_a(self):
+        import sys
+        sys.path.append('impsubdir')
+        import a
+        self.assert_('a' in sys.modules)
 
-   def test_import_bare_dir_fails(self):
-      def imp():
-         import impsubdir
-      self.assertRaises(ImportError,imp)
+    def test_import_bare_dir_fails(self):
+        def imp():
+           import impsubdir
+        self.assertRaises(ImportError,imp)
 
-   def test_import_pkg(self):
-      import sys
-      sys.path.append('impsubdir')
-      import pkg
-      self.assert_('pkg' in sys.modules)
+    def test_import_pkg(self):
+        import sys
+        sys.path.append('impsubdir')
+        import pkg
+        self.assert_('pkg' in sys.modules)
 
-   def test_import_dotted(self):
-      import sys
-      sys.path.append('impsubdir')
-      import pkg.a
-      self.assert_('pkg' in sys.modules)
-      self.assert_('pkg.a' in sys.modules)
+    def test_import_dotted(self):
+        import sys
+        sys.path.append('impsubdir')
+        import pkg.a
+        self.assert_('pkg' in sys.modules)
+        self.assert_('pkg.a' in sys.modules)
 
-   def test_import_dotted2(self):
-      import sys
-      sys.path.append('impsubdir')
-      import pkg.pkg1.a
-      self.assert_('pkg' in sys.modules)
-      self.assert_('pkg.pkg1' in sys.modules)
-      self.assert_('pkg.pkg1.a' in sys.modules)
+    def test_import_dotted2(self):
+        import sys
+        sys.path.append('impsubdir')
+        import pkg.pkg1.a
+        self.assert_('pkg' in sys.modules)
+        self.assert_('pkg.pkg1' in sys.modules)
+        self.assert_('pkg.pkg1.a' in sys.modules)
 
-   def test_import_ambig(self):
-      import sys
-      sys.path.append('impsubdir')
-      import ambig
-      self.assert_('ambig' in sys.modules)
-      self.assert_(hasattr(ambig,'imapackage'))
+    def test_import_ambig(self):
+        import sys
+        sys.path.append('impsubdir')
+        import ambig
+        self.assert_('ambig' in sys.modules)
+        self.assert_(hasattr(ambig,'imapackage'))
 
-   def test_from_a(self):
-      import sys
-      sys.path.append('impsubdir')
-      from a import imamodule
-      self.assert_('a' in sys.modules)
-      self.assertEquals(imamodule, 1)
+    def test_from_a(self):
+        import sys
+        sys.path.append('impsubdir')
+        from a import imamodule
+        self.assert_('a' in sys.modules)
+        self.assertEquals(imamodule, 1)
 
-   def test_from_dotted(self):
-      import sys
-      sys.path.append('impsubdir')
-      from pkg.a import imamodule
-      self.assert_('pkg' in sys.modules)
-      self.assert_('pkg.a' in sys.modules)
-      self.assertEquals(imamodule, 1)
+    def test_from_dotted(self):
+        import sys
+        sys.path.append('impsubdir')
+        from pkg.a import imamodule
+        self.assert_('pkg' in sys.modules)
+        self.assert_('pkg.a' in sys.modules)
+        self.assertEquals(imamodule, 1)
 
-   def test_from_pkg_import_module(self):
-      import sys
-      sys.path.append('impsubdir')
-      from pkg import a
-      self.assert_('pkg' in sys.modules)
-      self.assert_('pkg.a' in sys.modules)
-      self.assert_(hasattr(a, 'imamodule'))
+    def test_from_pkg_import_module(self):
+        import sys
+        sys.path.append('impsubdir')
+        from pkg import a
+        self.assert_('pkg' in sys.modules)
+        self.assert_('pkg.a' in sys.modules)
+        self.assert_(hasattr(a, 'imamodule'))
 
 if __name__ == '__main__':
     testit.main()
