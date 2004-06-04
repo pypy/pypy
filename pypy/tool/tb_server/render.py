4b5241bfaad9613f3f2837bc70cd34f942706b8a
Michael Hudson <micahel@gmail.com>
2004-06-04 14:20:18 +0000

    add 'FileSystemView', add links to files from tracebacks

--- pypy/tool/tb_server/render.py
+++ pypy/tool/tb_server/render.py
@@ -7,44 +7,58 @@ import traceback
 
 views = TBRequestHandler.views
 
+class Renderer:
+    def render(self, args):
+        try:
+            inner = self.render_self(args)
+        except:
+            import sys, traceback
+            lines = traceback.format_exception(*sys.exc_info())
+            inner =  html.pre(
+                xml.escape(''.join(
+                ['Internal Rendering Error, traceback follows\n'] + lines)))
+            
+        tag = html.html(
+            html.head(),
+            html.body(
+                inner
+            )
+        )
+        return tag.to_unicode()
+    
 
-class TracebackView:
+class TracebackView(Renderer):
     def __init__(self, tb):
         self.name = 'traceback%d' % len(views) 
         views[self.name] = self
         self.tb = tb 
-
-    def render(self, args): 
-        tag = html.html(
-            html.head(),
-            html.body(
-                self.render_tb(args) 
-            )
-        )
-        return tag.to_unicode()
-
-    def render_tb(self, args):
-        try:
-            return self.render_tb_really(args)
-        except:
-            import sys, traceback
-            lines = traceback.format_tb(sys.exc_info()[2])
-            return html.pre(
-                xml.escape(''.join(['Internal Rendering Error, traceback follows\n'] + lines)))
         
-    def render_tb_really(self, args):
+    def render_self(self, args):
         lines = html.pre()
         for tb in dyncode.listtb(self.tb):
             filename = tb.tb_frame.f_code.co_filename 
             lineno = tb.tb_lineno
             name = tb.tb_frame.f_code.co_name
             lines.append('  File "%s", line %d, in %s\n'%(
-                html.a(filename, href=filename).to_unicode().encode('utf-8'),
+                html.a(filename, href='/file' + filename + '#' + str(lineno)).to_unicode().encode('utf-8'),
                 lineno, name))
             lines.append(dyncode.getline(filename, lineno))
         return lines
-            
-            
-            
-            
+
+
+def ln(lineno):
+    return html.a(name=str(lineno))
+
+class FileSystemView(Renderer):
+    def render_self(self, args):
+        fname = '/' + '/'.join(args)
+        lines = html.pre()
+        i = 1
+        for line in open(fname):
+            lines.append(ln(i))
+            lines.append(xml.escape(line)[:-1])
+            i += 1
+        return lines
+    
+views['file'] = FileSystemView()
                 
