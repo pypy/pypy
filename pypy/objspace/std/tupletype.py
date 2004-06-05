4f48f4bc6bd3cd44644e7533dbff418652742a6c
Michael Hudson <micahel@gmail.com>
2004-06-05 09:40:44 +0000

    Careful with those default args!

--- pypy/objspace/std/tupletype.py
+++ pypy/objspace/std/tupletype.py
@@ -2,8 +2,11 @@ from pypy.objspace.std.stdtypedef import *
 from pypy.objspace.std.objecttype import object_typedef
 
 
-def descr__new__(space, w_tupletype, w_items=()):
-    tuple_w = space.unpackiterable(w_items)
+def descr__new__(space, w_tupletype, w_items=None):
+    if w_items is None:
+        tuple_w = []
+    else:
+        tuple_w = space.unpackiterable(w_items)
     w_obj = space.newtuple(tuple_w)
     return space.w_tuple.check_user_subclass(w_tupletype, w_obj)
 
