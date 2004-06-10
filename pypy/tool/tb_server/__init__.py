50dc815023dbfaca245fc2dc43eeb5444cc693a2
Armin Rigo <arigo@tunes.org>
2004-06-10 12:46:46 +0000

    Lazily import the tb_server package's content (so don't import HTTP stuff unless it is really needed).

--- pypy/tool/tb_server/__init__.py
+++ pypy/tool/tb_server/__init__.py
@@ -1,1 +1,15 @@
-from server import start, stop, publish_exc, wait_until_interrupt
+# Lazy import
+
+def start():
+    global start, stop, publish_exc, wait_until_interrupt
+    from server import start, stop, publish_exc, wait_until_interrupt
+    return start()
+
+def stop():
+    pass
+
+def wait_until_interrupt():
+    pass
+
+def publish_exc(exc):
+    pass
