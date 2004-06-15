ecf2a9c6cba5a1b36b4c357e8a4e2c023d38a304
Armin Rigo <arigo@tunes.org>
2004-06-15 20:00:46 +0000

    Deleted attic.

--- pypy/objspace/std/attic/moduletype.py
+++
@@ -1,9 +0,0 @@
-from pypy.objspace.std.objspace import *
-from typeobject import W_TypeObject
-
-
-class W_ModuleType(W_TypeObject):
-
-    typename = 'module'
-
-registerimplementation(W_ModuleType)
--- pypy/objspace/std/attic/stringobject_app.py
+++
@@ -1,6 +0,0 @@
-def getitem_string_slice(str, sliceob):
-     r = []
-     for i in range(*sliceob.indices(len(str))):
-         r.append(str[i])
-     return ''.join(r)
-
