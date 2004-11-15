2a5ebede87299bd207a43a85a6aaf7ba95bc6eab
Bob Ippolito <bob@redivi.com>
2004-11-15 14:31:46 +0000

    clean up implementation

--- pypy/tool/uid.py
+++ pypy/tool/uid.py
@@ -1,7 +1,12 @@
 import sys
+HUGEINT = (sys.maxint + 1L) * 2L
 
 def uid(obj):
+    """
+    Return the id of an object as an unsigned number so that its hex
+    representation makes sense
+    """
     rval = id(obj)
     if rval < 0:
-        rval += (long(sys.maxint)+1)*2
+        rval += HUGEINT
     return rval
