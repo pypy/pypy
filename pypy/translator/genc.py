7f5c8559bff6ef0534cdca81c03bc9071adbd001
Marius Gedminas <marius@gedmin.as>
2004-11-23 12:31:38 +0000

    Small fix: SomeImpossibleValue is not imported.

--- pypy/translator/genc.py
+++ pypy/translator/genc.py
@@ -241,8 +241,8 @@ class GenC:
             else:
                 return "probably"   # True
         classdef = ann.getuserclasses().get(pbc.__class__)
-        if (classdef and
-            classdef.about_attribute(attr) not in [None, SomeImpossibleValue()]):
+        if (classdef and classdef.about_attribute(attr) not in [
+                                None, annmodel.SomeImpossibleValue()]):
             return True
         return False
 
