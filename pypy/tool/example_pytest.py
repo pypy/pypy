e9ddfa341287ee578762ae6a88d3354aa6e04c41
Holger Krekel <holger.krekel@gmail.com>
2005-01-14 22:23:45 +0000

    added a few example for failing tests within pypy

--- pypy/tool/example_pytest.py
+++ pypy/tool/example_pytest.py
@@ -12,3 +12,15 @@ class TestInterpTest:
     def test_interp_method(self): 
         assert self.space.is_true(self.space.w_False) 
 
+def app_test_raises_something():
+    int("hallo") 
+
+def app_test_raises_wrong1():
+    raises(SyntaxError, 'int("hello")')
+
+def app_test_raises_wrong2():
+    raises(SyntaxError, int, "hello") 
+
+def app_test_raises_doesnt():
+    raises(ValueError, int, 3) 
+    
