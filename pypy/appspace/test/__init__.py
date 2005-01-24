560cdcae21d7daef749a7473fd1c75c3935680dd
Armin Rigo <arigo@tunes.org>
2005-01-24 10:04:05 +0000

    Confusion.  When doing 'import test' at app-level, we actually find the package 'pypy.appspace.test'...  This hack "merges" that package with CPython's own 'test' package.

--- pypy/appspace/test/__init__.py
+++ pypy/appspace/test/__init__.py
@@ -1,1 +1,8 @@
-# empty
+#
+#  Confusion.  When doing 'import test' at app-level, we actually
+#  find this package...  This hack "merges" the package with
+#  CPython's own 'test' package.
+#
+
+import os
+__path__.append(os.path.join(os.path.dirname(os.__file__), 'test'))
