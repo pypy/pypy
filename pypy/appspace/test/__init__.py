f383dcc6705b03490392bcb264c97bdf66adb944
Seo Sanghyeon <sanxiyn@codespeak.net>
2005-01-29 06:42:10 +0000

    Eradicate appspace

--- pypy/appspace/test/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-#  Confusion.  When doing 'import test' at app-level, we actually
-#  find this package...  This hack "merges" the package with
-#  CPython's own 'test' package.
-#
-
-import os
-__path__.append(os.path.join(os.path.dirname(os.__file__), 'test'))
--- pypy/appspace/test/conftest.py
+++
@@ -1,9 +0,0 @@
-from __future__ import generators 
-
-import py 
-from pypy.conftest import getobjspace, PyPyItem
-from pypy.interpreter.main import run_string
-
-class Directory(py.test.collect.Directory): 
-    def __iter__(self): 
-        return iter([]) 
