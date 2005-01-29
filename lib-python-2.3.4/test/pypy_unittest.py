0845b06e35324d723a0dfd45c327d6ac7998db3a
Anders Chrigstrom <anders@chrigstrom.se>
2005-01-29 13:32:39 +0000

    Make our TestCase look more like unittest.TestCase

--- lib-python-2.3.4/test/pypy_unittest.py
+++ lib-python-2.3.4/test/pypy_unittest.py
@@ -11,17 +11,54 @@ class TestCase:
             assert x == y, msg 
         else: 
             assert x == y 
+
     def assertNotEqual(self, x, y, msg=None): 
         if msg: 
             assert x != y, msg 
         else: 
             assert x != y 
 
+    def failIfEqual(self, x, y, msg=None): 
+        if msg: 
+            assert not x == y, msg 
+        else: 
+            assert not x == y 
+
+    def failUnless(self, expr, msg=None):
+        if msg is None:
+            assert expr
+        else:
+            assert expr, msg
+
+    def failIf(self, expr, msg=None):
+        if msg is None:
+            assert not expr
+        else:
+            assert not expr, msg
+
+    def fail(self, msg):
+        assert False, msg
+        
     def assertRaises(self, exc, call, *args, **kwargs): 
         raises(exc, call, *args, **kwargs) 
 
+    def assertAlmostEqual(self, x, y, places07, msg=None):
+        if msg is None:
+            msg = '%r != %r within %r places' %(x, y, places)
+        assert round(y-x, places) == 0, msg
+
+    def assertNotAlmostEqual(self, x, y, places07, msg=None):
+        if msg is None:
+            msg = '%r == %r within %r places' %(x, y, places)
+        assert round(y-x, places) != 0, msg
+
     assertEquals = assertEqual 
     assertNotEquals = assertNotEqual 
+    failUnlessRaises = assertRaises
+    failUnlessEqual = assertEqual
+    failIfAlmostEqual = assertNotAlmostEqual
+    failUnlessAlmostEqual = assertAlmostEqual
+    
     def assert_(self, expr, msg=None): 
         if msg: 
             assert expr, msg 
