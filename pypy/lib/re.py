745c3ff92dd001c03be6bfde316e48649f74b38b
Seo Sanghyeon <sanxiyn@codespeak.net>
2005-02-04 04:08:50 +0000

    Regular expression flags

--- pypy/lib/re.py
+++ pypy/lib/re.py
@@ -3,6 +3,15 @@ from dumbre import Pattern
 # from sre_adapt import Pattern
 
 
+# Constants, from CPython
+I = IGNORECASE = 2
+L = LOCALE = 4
+M = MULTILINE = 8
+S = DOTALL = 16
+U = UNICODE = 32
+X = VERBOSE = 64
+
+
 # From CPython
 def escape(pattern):
     "Escape all non-alphanumeric characters in pattern."
