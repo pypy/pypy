17389f3f2f0211f208d64b07d87f9fc29d1325b3
Seo Sanghyeon <sanxiyn@codespeak.net>
2005-02-13 17:05:09 +0000

    Now this works without any hacks.

--- goal/compiler-hack.py
+++ goal/compiler-hack.py
@@ -1,13 +1,6 @@
-# Hacks to import compiler package
-# As of revision 3865
-
-import os
-os.error = OSError
-import __builtin__
-__builtin__.reload = lambda x: x
-import ihooks
-ihooks.install()
 import compiler
 c = compiler.compile('a=1', '', 'exec')
 import dis
 dis.dis(c)
+exec c
+print a
