dd1bb42d4d6e275c0f543b1f9fb0505c46be4d71
Holger Krekel <holger.krekel@gmail.com>
2005-03-20 23:48:44 +0000

    be a bit more careful about printing stdout/stderr on non-ttys

--- pypy/documentation/revreport/revreport.py
+++ pypy/documentation/revreport/revreport.py
@@ -4,21 +4,14 @@ import autopath
 import py
 from pypy.documentation.revreport import delta 
 from pypy.tool.pypyrev import pypyrev 
+from py.__impl__.test.tool.outerrcapture import SimpleOutErrCapture 
 
 BASE = py.path.local(delta.__file__).dirpath() 
 DEST = BASE.join('revdata') 
 
 assert DEST.dirpath().check() 
 
-if __name__ == '__main__':
-    rev = pypyrev()  
-    revdir = DEST.ensure(str(rev), dir=1) 
-    BASE.join('delta.css').copy(revdir) 
-    BASE.join('delta.js').copy(revdir) 
-    delta.genreport(revdir) 
-
-    print "generated into", revdir 
-
+def updatecurrent(): 
     l = []
     for x in DEST.listdir(): 
         try: 
@@ -36,3 +29,27 @@ if __name__ == '__main__':
         latest.copy(current) 
     else: 
         current.mksymlinkto(latest) 
+
+if __name__ == '__main__':
+    rev = pypyrev()  
+    revdir = DEST.ensure(str(rev), dir=1) 
+    BASE.join('delta.css').copy(revdir) 
+    BASE.join('delta.js').copy(revdir) 
+
+    if py.std.sys.stdout.isatty(): 
+        delta.genreport(revdir) 
+    else: 
+        capture = SimpleOutErrCapture() 
+        try: 
+            delta.genreport(revdir) 
+        except: 
+            out, err = capture.done()  
+            print "stdout", out 
+            print "stderr", err 
+            raise 
+        else: 
+            out, err = capture.done() 
+            print "stdout"
+            print out 
+    print "generated into", revdir 
+    updatecurrent() 
