afc27574093664bbdb04365262609ef719474199
Alex Martelli <alex@codespeak.net>
2005-03-20 15:37:41 +0000

    fixed tabs

--- pypy/lib/test2/test_obj.py
+++ pypy/lib/test2/test_obj.py
@@ -4,9 +4,10 @@ import sys, cStringIO, pickle
 
 class Picklable(object):
     def __init__(self):
-	self.a = 5
+        self.a = 5
     def __eq__(self, other):
-	return self.a == other.a
+        return self.a == other.a
+    
 
 class ObjectTest(unittest.TestCase):
 
@@ -23,11 +24,11 @@ class ObjectTest(unittest.TestCase):
         self.assertRaises(TypeError, hash, l)
 
     def test_pickle_plain(self):
-	x = Picklable()
-	for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-	    s = pickle.dumps(x, proto)
-	    y = pickle.loads(s)
-	    self.assertEqual(x, y)
+        x = Picklable()
+        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+            s = pickle.dumps(x, proto)
+            y = pickle.loads(s)
+            self.assertEqual(x, y)
 
 def test_main():
     test.test_support.run_unittest(ObjectTest)
