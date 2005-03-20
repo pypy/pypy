7b8592d9981009185070b967f822a5bc8ea4047a
Alex Martelli <alex@codespeak.net>
2005-03-20 15:45:34 +0000

    add test for pickling object w/__reduce__ returning a 2-tuple

--- pypy/lib/test2/test_obj.py
+++ pypy/lib/test2/test_obj.py
@@ -3,11 +3,16 @@ import unittest, test.test_support
 import sys, cStringIO, pickle
 
 class Picklable(object):
-    def __init__(self):
-        self.a = 5
+    def __init__(self, a=5):
+        self.a = a
     def __eq__(self, other):
         return self.a == other.a
-    
+    def __str__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.a)
+
+class PicklableSpecial2(Picklable):
+    def __reduce__(self):
+        return self.__class__, (self.a,)
 
 class ObjectTest(unittest.TestCase):
 
@@ -23,12 +28,17 @@ class ObjectTest(unittest.TestCase):
         l = range(5)
         self.assertRaises(TypeError, hash, l)
 
-    def test_pickle_plain(self):
-        x = Picklable()
+    def _pickle_some(self, x):
         for proto in range(pickle.HIGHEST_PROTOCOL + 1):
             s = pickle.dumps(x, proto)
             y = pickle.loads(s)
             self.assertEqual(x, y)
+
+    def test_pickle_plain(self):
+        self._pickle_some(Picklable())
+
+    def test_pickle_special2(self):
+        self._pickle_some(PicklableSpecial2())
 
 def test_main():
     test.test_support.run_unittest(ObjectTest)
