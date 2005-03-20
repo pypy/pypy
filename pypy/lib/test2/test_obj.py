7f312c14eaa41076a3122cc3026caf9e06364dcc
Alex Martelli <alex@codespeak.net>
2005-03-20 15:34:41 +0000

    test pickle (plain) on user class

--- pypy/lib/test2/test_obj.py
+++ pypy/lib/test2/test_obj.py
@@ -1,6 +1,12 @@
 # -*- coding: iso-8859-1 -*-
 import unittest, test.test_support
-import sys, cStringIO
+import sys, cStringIO, pickle
+
+class Picklable(object):
+    def __init__(self):
+	self.a = 5
+    def __eq__(self, other):
+	return self.a == other.a
 
 class ObjectTest(unittest.TestCase):
 
@@ -15,6 +21,13 @@ class ObjectTest(unittest.TestCase):
     def test_hash_list(self):
         l = range(5)
         self.assertRaises(TypeError, hash, l)
+
+    def test_pickle_plain(self):
+	x = Picklable()
+	for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+	    s = pickle.dumps(x, proto)
+	    y = pickle.loads(s)
+	    self.assertEqual(x, y)
 
 def test_main():
     test.test_support.run_unittest(ObjectTest)
