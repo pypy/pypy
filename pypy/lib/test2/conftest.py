c033bdb50b055ee901504cac357047633473fae9
Holger Krekel <holger.krekel@gmail.com>
2005-03-20 16:53:25 +0000

    we want multiple instances of object spaces at some point

--- pypy/lib/test2/conftest.py
+++ pypy/lib/test2/conftest.py
@@ -1,5 +1,20 @@
+import autopath 
 import py
+import pypy
+from pypy.conftest import gettestobjspace 
 
-class Directory(py.test.collect.Directory): 
-    def __iter__(self): 
-        return iter([])
+lib = py.path.local(pypy.__file__).dirpath()
+lib = lib.dirpath('lib-python-2.3.4', 'test')
+assert lib.check(dir=1) 
+conftest = lib.join('conftest.py').getpymodule() 
+
+class Module(conftest.Module): 
+    pass 
+
+class Directory(conftest.Module): 
+    pass 
+
+#class Directory(py.test.collect.Directory): 
+#    def __iter__(self): 
+#        return iter([])
+
