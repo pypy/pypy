8b16648859808e98be76eea093df9c9cc08f2bd1
Holger Krekel <holger.krekel@gmail.com>
2005-03-21 16:34:51 +0000

    move this "extra" test of some hash functionality to the standard object space instead of our hacked pypy/lib

---
+++ pypy/objspace/std/test_obj.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-1 -*-
+
+class AppTestObject: 
+    def test_hash_builtin(self):
+        o = object()
+        assert hash(o) == id(o) 
+
+    def test_hash_method(self):
+        o = object()
+        assert hash(o) == o.__hash__() 
+
+    def test_hash_list(self):
+        l = range(5)
+        raises(TypeError, hash, l)
