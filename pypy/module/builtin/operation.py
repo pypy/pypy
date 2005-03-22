701de445b9a271c4d031505af877fab9a6314ebc
Holger Krekel <holger.krekel@gmail.com>
2005-03-22 01:55:32 +0000

    hum, iter() is just fishy with respect to errors/type checking

--- pypy/module/builtin/operation.py
+++ pypy/module/builtin/operation.py
@@ -91,18 +91,21 @@ iter_sentinel = app.interphook("iter_sentinel")
 
 def iter(space, w_collection_or_callable, w_sentinel=NoneNotWrapped):
     if w_sentinel is None:
-        w_res = space.iter(w_collection_or_callable)
-        w_typeres = space.type(w_res) 
-        try: 
-            space.getattr(w_typeres, space.wrap('next'))
-        except OperationError, e: 
-            if not e.match(space, space.w_AttributeError): 
-                raise 
-            raise OperationError(space.w_TypeError, 
-                space.wrap("iter() returned non-iterator of type '%s'" % 
-                           w_typeres.name))
-        else: 
-            return w_res 
+        return space.iter(w_collection_or_callable) 
+        # XXX it seems that CPython checks the following 
+        #     for newstyle but doesn't for oldstyle classes :-( 
+        #w_res = space.iter(w_collection_or_callable)
+        #w_typeres = space.type(w_res) 
+        #try: 
+        #    space.getattr(w_typeres, space.wrap('next'))
+        #except OperationError, e: 
+        #    if not e.match(space, space.w_AttributeError): 
+        #        raise 
+        #    raise OperationError(space.w_TypeError, 
+        #        space.wrap("iter() returned non-iterator of type '%s'" % 
+        #                   w_typeres.name))
+        #else: 
+        #    return w_res 
     else:
         return iter_sentinel(space, w_collection_or_callable, w_sentinel)
 
