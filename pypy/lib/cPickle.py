bde1259da3a8a261f9f8f276d04b35c254f6d4ca
Anders Chrigstrom <anders@chrigstrom.se>
2005-03-30 14:01:19 +0000

    Fix missing sattributes of cPickle.

--- pypy/lib/cPickle.py
+++ pypy/lib/cPickle.py
@@ -3,4 +3,16 @@
 #
 
 from pickle import *
-from pickle import __doc__
+from pickle import __doc__, __version__, format_version, compatible_formats
+
+class UnpickleableError(PicklingError):
+    def __init__(self, *args):
+        self.args=args
+
+    def __str__(self):
+        a=self.args
+        a=a and type(a[0]) or '(what)'
+        return 'Cannot pickle %s objects' % a
+
+class BadPickleGet(UnpicklingError):
+    pass
