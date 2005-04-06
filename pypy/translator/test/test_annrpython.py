0d9b913f2f33c3fe32a422a9d4e71cac2b913ac6
Samuele Pedroni <pedronis@lucediurna.net>
2005-04-06 13:26:33 +0000

    annotate implicit exceptions properly

--- pypy/translator/test/test_annrpython.py
+++ pypy/translator/test/test_annrpython.py
@@ -606,6 +606,18 @@ class TestAnnonateTestCase:
         s = a.build_types(f, [object])
         assert s.knowntype is C
 
+    def test_implicit_exc(self):
+        def f(l):
+            try:
+                l[0]
+            except (KeyError, IndexError),e:
+                return e
+            return None
+
+        a = RPythonAnnotator()
+        s = a.build_types(f, [list])
+        assert s.knowntype is LookupError
+
     def test_overrides(self):
         import sys
         excs = []
