ae06f1fb46cc2b9a3a9a43d6e6bf1bf345167bfc
Holger Krekel <holger.krekel@gmail.com>
2005-04-17 17:05:07 +0000

    helper for the web site:  we list redirections of documents here in the 'redirections' file containing a dictionary.

---
+++ pypy/documentation/test_redirections.py
@@ -0,0 +1,15 @@
+
+import py 
+redir = py.magic.autopath().dirpath('redirections') 
+
+def checkexist(path):
+    assert path.new(ext='.txt').check(file=1) 
+   
+def test_eval(): 
+    d = eval(redir.read()) 
+    return d
+
+def test_redirections(): 
+    d = test_eval() 
+    for newname in d.values(): 
+        yield checkexist, redir.dirpath(newname) 
