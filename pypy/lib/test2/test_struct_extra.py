e811821821fb7f872bdd95a3249b2777c5dadc87
Armin Rigo <arigo@tunes.org>
2005-05-01 13:04:46 +0000

    Partial clean up of lib/test2.

---
+++ pypy/lib/test2/test_struct_extra.py
@@ -0,0 +1,9 @@
+import support
+struct = support.libmodule('struct')
+
+
+def test_simple():
+    morezeros = '\x00' * (struct.calcsize('l')-4)
+    assert struct.pack('<l', 16) == '\x10\x00\x00\x00' + morezeros
+    assert struct.pack('4s', 'WAVE') == 'WAVE'
+    #assert struct.pack('<4sl', 'WAVE', 16) == 'WAVE\x10\x00\x00\x00' + morezeros
