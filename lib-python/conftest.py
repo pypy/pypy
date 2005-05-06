67877a2ac734f32da6048fdb81c71bc303d167a9
Samuele Pedroni <pedronis@lucediurna.net>
2005-05-06 17:27:23 +0000

    test_descr needs oldstyle

--- lib-python/conftest.py
+++ lib-python/conftest.py
@@ -424,9 +424,8 @@ testmap = [
     RegrTest('test_curses.py', enabled=False, dumbtest=1),
     RegrTest('test_datetime.py', enabled=True, core=True),
     RegrTest('test_dbm.py', enabled=False, dumbtest=1),
-    RegrTest('test_descr.py', enabled=False, core=True),
+    RegrTest('test_descr.py', enabled=False, core=True, oldstyle=True),
     RegrTest('test_descrtut.py', enabled=False, core=True, oldstyle=True),
-        #rev 10840: 19 of 96 tests fail
 
     RegrTest('test_difflib.py', enabled=True, dumbtest=1, core=True),
     RegrTest('test_dircache.py', enabled=True, core=True),
