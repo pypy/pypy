99baa896528f91c31220249a363f59b6fefb57bf
Holger Krekel <holger.krekel@gmail.com>
2005-05-15 17:48:03 +0000

    issue4 testing
    There is now preliminary doctest support for our documentation files
    executing ">>>" prompts at interpreter level with the doctest module
    behind the scenes.   This makes some code snippets fail but also
    many of them work.  In getting_started.txt you'll see the
    way you can specify scopes that will not be shown in the
    generated html.
    
    .. >>> from os.path import abspath
    
       >>> assert abspath
    
    will pass the doctest and the import-line will be ignored by
    html generation  (at least it should be).
    
    Let's see if this approach is sensible enough.
    
    Applevel doctests (i.e. >>>> prompts) are not implemented yet
    and are probably a post-M0.5 matter. (and running them simply
    at app level would be at least be slow if not somewhat messy).
    
    P.S.: you need to "svn up" at dist-level because most of
          the doctest support code comes with the py lib
          but upgrading at dist-level is always a good idea.

--- pypy/documentation/conftest.py
+++ pypy/documentation/conftest.py
@@ -1,3 +1,26 @@
+import py
+from py.__.documentation.conftest import Directory, DoctestText, ReSTChecker
 
-from py.__.documentation.conftest import * 
+class PyPyDoctestText(DoctestText): 
+    def execute(self, module, docstring): 
+        # XXX execute PyPy prompts as well 
+        l = []
+        for line in docstring.split('\n'): 
+            if line.find('>>>>') != -1: 
+                line = "" 
+            l.append(line) 
+        text = "\n".join(l) 
+        super(PyPyDoctestText, self).execute(module, text) 
 
+        #mod = py.std.types.ModuleType(self.fspath.basename, text) 
+        #self.mergescopes(mod, scopes) 
+        #failed, tot = py.std.doctest.testmod(mod, verbose=1)
+        #if failed:
+        #    py.test.fail("doctest %s: %s failed out of %s" %(
+        #                 self.fspath, failed, tot))
+
+class PyPyReSTChecker(ReSTChecker): 
+    DoctestText = PyPyDoctestText 
+    
+class Directory(Directory): 
+    ReSTChecker = PyPyReSTChecker 
