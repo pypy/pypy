e4b0d46d3c320325a72e3caebd93c7b9ac249494
Armin Rigo <arigo@tunes.org>
2005-05-16 16:16:27 +0000

    Tests for the previous fix in typedef.py.

--- pypy/interpreter/test/test_typedef.py
+++ pypy/interpreter/test/test_typedef.py
@@ -23,3 +23,20 @@ class AppTestTraceBackAttributes:
         assert hasattr(tb, 'tb_lasti')
         assert hasattr(tb, 'tb_lineno')
         assert hasattr(tb, 'tb_next')
+
+    def test_descr_dict(self):
+        def f():
+            pass
+        dictdescr = type(f).__dict__['__dict__']   # only for functions
+        assert dictdescr.__get__(f) is f.__dict__
+        raises(TypeError, dictdescr.__get__, 5)
+        d = {}
+        dictdescr.__set__(f, d)
+        assert f.__dict__ is d
+        raises(TypeError, dictdescr.__set__, f, "not a dict")
+        raises(TypeError, dictdescr.__set__, 5, d)
+        # in PyPy, the following descr applies to any object that has a dict,
+        # but not to objects without a dict, obviously
+        dictdescr = type.__dict__['__dict__']
+        raises(TypeError, dictdescr.__get__, 5)
+        raises(TypeError, dictdescr.__set__, 5, d)
