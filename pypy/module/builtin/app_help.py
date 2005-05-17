7bf74492245e8fc06a24c0b8d7d3ecf442477802
Holger Krekel <holger.krekel@gmail.com>
2005-05-17 20:32:38 +0000

    issue52 testing
    Added a first draft for a top-level LICENSE files, where the copyrights
    got computed by examining all committers (via svn log) on the pypy and
    py directories.   The list is currently ordered by number of commits.
    If desired i could extend the script and let it try to run "svn blame".
    It probably would give a somewhat more appropriate picture.
    
    Additionally i put a pseudo-LICENSE into the Pyrex directory
    (it was the only notice i could find from the Pyrex author).
    
    So the questions are (apart from the minor ordering issue):
    
    - do we want to reduce the list of PyPy copyright holders
      somehow?
    
    - are there any no included sub-projects that miss copyright
      statements or a LICENSE file?

--- pypy/module/builtin/app_help.py
+++ pypy/module/builtin/app_help.py
@@ -17,7 +17,7 @@ def copyright():
 def license():
     print \
 """
-Copyright (c) <2003-2004> <Pypy development team>
+Copyright (c) <2003-2005> <Pypy development team>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
