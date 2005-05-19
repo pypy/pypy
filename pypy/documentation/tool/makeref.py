2a5994147b735b32bd8598d3041a9ce651d038a1
Holger Krekel <holger.krekel@gmail.com>
2005-05-19 08:21:34 +0000

    issue53 testing
    removed the need to manually add SVN/file references
    as long as:
    
    - you specify them relative to 'dist' or to 'pypy'
    
    - linknames contain a slash
    
    To avoid having to generate thousands of links without
    them being used (docutils parsing is slow) the logic
    for generating references is now reversed: it starts
    off from the .txt files and generates all neccessary
    links.  This means that if you add file references that
    previously didn't exist (py.test fails then), you have to run
    
        python documentation/tool/makeref.py
    
    Not sure yet, if this run should be integrated into py.test
    because it really modifies files (_ref.txt) and that is uncommon
    to do for testing.

---
+++ pypy/documentation/tool/makeref.py
@@ -0,0 +1,44 @@
+
+import py
+py.magic.autopath()
+import pypy
+pypydir = py.path.local(pypy.__file__).dirpath()
+distdir = pypydir.dirpath() 
+dist_url = 'http://codespeak.net/svn/pypy/dist/' 
+
+docdir = pypydir.join('documentation') 
+reffile = pypydir.join('documentation', '_ref.txt') 
+
+linkrex = py.std.re.compile('`(\S+)`_')
+
+name2target = {}
+def addlink(linkname, linktarget): 
+    assert linkname and linkname != '/'
+    if linktarget in name2target: 
+        if linkname in name2target[linktarget]: 
+            return
+    name2target.setdefault(linktarget, []).append(linkname)
+
+for textfile in docdir.visit(lambda x: x.ext == '.txt', 
+                             lambda x: x.check(dotfile=0)): 
+    for linkname in linkrex.findall(textfile.read()): 
+        if '/' not in linkname: 
+            continue
+        for startloc in ('', 'pypy'): 
+            cand = distdir.join(startloc, linkname)
+            if cand.check(): 
+                target = dist_url + cand.relto(distdir)
+                addlink(linkname, target) 
+
+items = name2target.items() 
+items.sort() 
+
+lines = []
+for linktarget, linknamelist in items: 
+    for linkname in linknamelist[:-1]: 
+        lines.append(".. _`%s`:" % linkname)
+    lines.append(".. _`%s`: %s" %(linknamelist[-1], linktarget))
+
+reffile.write("\n".join(lines))
+print "last ten lines"
+for x in lines[-10:]: print x
