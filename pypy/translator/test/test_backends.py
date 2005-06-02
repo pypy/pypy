1e73667b7969d61d4f837cd47976a2a73610c69b
Eric van Riet Paap <ericvrp@gmail.com>
2005-06-02 10:08:50 +0000

    These tests get run by all existing backends. Currently it contains just one test (can backends regenerate identical sourcecode).
    XXX We need to think about self-registering the backends, so we don't endup hardcoding their names at various places!

---
+++ pypy/translator/test/test_backends.py
@@ -0,0 +1,32 @@
+'''
+Test conditions that all backends should do correct.
+'''
+
+import autopath, os, sys
+from pypy.translator.translator import Translator
+from pypy.rpython.rtyper import *
+from pypy.rpython.rarithmetic import *
+from pypy.translator.test.snippet import *
+
+
+backends = 'source c cl llvm pyrex'.split()
+functions = 'forty_two'.split() #XXX add more functions here when RPythonTyper can handle them
+
+regenerate_code = '''def test_regenerate_%(function)s_%(backend)s():
+    t = Translator(%(function)s)
+    t.simplify()
+    a = t.annotate([])
+    a.simplify()
+    typer = RPythonTyper(t.annotator)
+    typer.specialize()
+    first  = t.%(backend)s()
+    second = t.%(backend)s()
+    if first != second:
+        print '<FIRST>\\n'  + first  + '\\n</FIRST>\\n'
+        print '<SECOND>\\n' + second + '\\n</SECOND>\\n'
+        #t.view()
+    assert first == second'''
+
+for backend in backends:
+    for function in functions:
+        exec regenerate_code % vars()
