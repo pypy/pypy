80ad311babf11f85ec4ad265d53e0e507b762d81
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-06-08 07:47:18 +0000

    missing from last night's commit

---
+++ pypy/translator/llvm/pointerrepr.py
@@ -0,0 +1,10 @@
+import autopath
+import sets
+
+from pypy.objspace.flow.model import Variable, Constant
+from pypy.objspace.flow.model import last_exception
+
+from pypy.rpython import lltype
+
+from pypy.annotation import model as annmodel
+
