4749424277d479675ebbd7e309943c6dcaaebbb0
Armin Rigo <arigo@tunes.org>
2005-06-12 23:50:24 +0000

    test_backends is difficult to write generically at the moment. (See explanations in the file itself)

--- pypy/translator/test/test_backends.py
+++ pypy/translator/test/test_backends.py
@@ -9,6 +9,16 @@ from pypy.rpython.rarithmetic import *
 from pypy.translator.test.snippet import *
 
 
+py.test.skip("the Translator and back-ends depend on too many conditions "
+             "to test the back-ends generically")
+# e.g. some back-ends require annotation and/or specialization to be done
+#      while some require it *not* to be done.  GenCL for example has no
+#      chance to understand a specialized graph.  (That's why the test still
+#      used to pass: forty_two is basically an empty function, so
+#      specializing it makes no difference.  Now the RTyper produces a few
+#      helper functions in all cases.)
+
+
 backends = 'source c cl llvm pyrex'.split()
 deterministic_backends = 'source cl llvm pyrex'.split()
 functions = 'forty_two'.split() #XXX add more functions here when RPythonTyper can handle them
