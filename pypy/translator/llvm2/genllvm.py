731889f6d5904dfe8ac4f62c84996f8b30780841
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-06-18 21:39:45 +0000

    skip/fail if genllvm() is called with a 'llvm-as' on the path.

--- pypy/translator/llvm2/genllvm.py
+++ pypy/translator/llvm2/genllvm.py
@@ -1,3 +1,4 @@
+import py
 from pypy.translator.llvm2 import build_llvm_module
 from pypy.translator.llvm2.database import Database 
 from pypy.translator.llvm2.pyxwrapper import write_pyx_wrapper 
@@ -5,7 +6,6 @@ from pypy.translator.llvm2.log import log
 from pypy.objspace.flow.model import Constant
 from pypy.rpython.rmodel import inputconst, getfunctionptr
 from pypy.rpython import lltype
-
 from pypy.tool.udir import udir
 from pypy.translator.llvm2.codewriter import CodeWriter
 
@@ -34,9 +34,20 @@ def genllvm(translator):
     content = str(codewriter) 
     llvmsource.write(content) 
     log.source(content)
-    
+  
+    if not llvm_is_on_path(): 
+        py.test.skip("llvm not found")  # XXX not good to call py.test.skip here
+         
     pyxsource = llvmsource.new(basename=llvmsource.purebasename+'_wrapper'+'.pyx')
     write_pyx_wrapper(entrynode, pyxsource)    
+    
     mod = build_llvm_module.make_module_from_llvm(llvmsource, pyxsource)
     return getattr(mod, func.func_name + "_wrapper")
 
+def llvm_is_on_path():
+    try:
+        py.path.local.sysfind("llvm-as")
+    except py.error.ENOENT: 
+        return False 
+    return True
+    
