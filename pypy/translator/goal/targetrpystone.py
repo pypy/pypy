928c7ab8e6dafa96ec7245cec44122ac67ef0b04
Samuele Pedroni <pedronis@lucediurna.net>
2005-06-18 01:19:24 +0000

    slightly rerworked rpystone targets to make them more directly approachable for rtyper
    (avoiding extra string methods and string formatting code for now in rpystone entrypoint&main)

--- pypy/translator/goal/targetrpystone.py
+++ pypy/translator/goal/targetrpystone.py
@@ -8,28 +8,30 @@ LOOPS = 150000
 # rpystone.setslow(False)
 
 def entry_point():
-    rpystone.entrypoint(LOOPS)
+    return rpystone.pystones(LOOPS)
     
 # _____ Define and setup target _____
 def target():
-    global space, mmentrypoints
-    space = StdObjSpace()
-
-    # ------------------------------------------------------------
-
     return entry_point, []
 
 # _____ Run translated _____
 
 def run(c_entry_point):
-    res_w = c_entry_point()
-    print res_w
+    res = c_entry_point()
+    benchtime, stones = res
+    print "translated rpystone.pystones time for %d passes = %g" % \
+        (LOOPS, benchtime)
+    print "This machine benchmarks at %g translated rpystone pystones/second" % stones
     print "CPython:"
-    rpystone.entrypoint(50000)
+    benchtime, stones = rpystone.pystones(50000)
+    print "rpystone.pystones time for %d passes = %g" % \
+        (50000, benchtime)
+    print "This machine benchmarks at %g rpystone pystones/second" % stones
 
-if __name__ == "__main__":
-    # just run it without translation
-    LOOPS = 50000
-    target()
-    run(entry_point)
+
+#if __name__ == "__main__":
+#    # just run it without translation
+#    LOOPS = 50000
+#    target()
+#    run(entry_point)
     
