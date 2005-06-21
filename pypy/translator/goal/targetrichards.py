d314c87aa3e865190579d691ad4822cce8f6cf62
Samuele Pedroni <pedronis@lucediurna.net>
2005-06-21 20:31:11 +0000

    a target based on a (R)Python port of the classic richards benchmark

---
+++ pypy/translator/goal/targetrichards.py
@@ -0,0 +1,17 @@
+import richards
+
+entry_point = richards.entry_point
+
+# _____ Define and setup target ___
+
+def target():
+    return entry_point, []
+
+# _____ Run translated _____
+def run(c_entry_point):
+    print "Translated:"
+    richards.main(c_entry_point)
+    print "CPython:"
+    richards.main()
+
+    
