02cab887de72d2d7b3fc76eeb0d27d5ac89a6bad
Armin Rigo <arigo@tunes.org>
2005-06-23 15:23:04 +0000

    Compare characters unsignedly in C (credits: tismer)

--- pypy/translator/c/char_include.h
+++ pypy/translator/c/char_include.h
@@ -6,10 +6,10 @@
 /***  binary operations ***/
 
 
-#define OP_CHAR_EQ(x,y,r,err)	  r = ((x) == (y));
-#define OP_CHAR_NE(x,y,r,err)	  r = ((x) != (y));
-#define OP_CHAR_LE(x,y,r,err)	  r = ((x) <= (y));
-#define OP_CHAR_GT(x,y,r,err)	  r = ((x) >  (y));
-#define OP_CHAR_LT(x,y,r,err)	  r = ((x) <  (y));
-#define OP_CHAR_GE(x,y,r,err)	  r = ((x) >= (y));
+#define OP_CHAR_EQ(x,y,r,err)	 r = ((x) == (y));
+#define OP_CHAR_NE(x,y,r,err)	 r = ((x) != (y));
+#define OP_CHAR_LE(x,y,r,err)	 r = ((unsigned char)(x) <= (unsigned char)(y));
+#define OP_CHAR_GT(x,y,r,err)	 r = ((unsigned char)(x) >  (unsigned char)(y));
+#define OP_CHAR_LT(x,y,r,err)	 r = ((unsigned char)(x) <  (unsigned char)(y));
+#define OP_CHAR_GE(x,y,r,err)	 r = ((unsigned char)(x) >= (unsigned char)(y));
 
