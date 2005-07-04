98d77cf1aef09187f66c5d480f696aed0614be1e
Armin Rigo <arigo@tunes.org>
2005-07-04 16:16:12 +0000

    Rewrote the collections module, mostly by copying it from the sandbox of CPython and then fixing a few bugs and adding missing methods.
    Pending: do we want to detect changes to the deque while it is being iterated
    over and raise RuntimeError, like CPython does?

--- pypy/lib/collections.py
+++ pypy/lib/collections.py
@@ -1,56 +1,266 @@
-# Naive collections implementation
+"""High performance data structures
+"""
+#
+# Copied and completed from the sandbox of CPython
+#   (nondist/sandbox/collections/pydeque.py rev 1.1, Raymond Hettinger)
+#
 
-class deque(list):
-    def __init__(self, iter=None):
-        # Do not run init on list, as it will empty list; deque test_basic does:
-        #    d = deque(xrange(100))
-        #    d.__init__(xrange(100, 200))
-        if iter is not None:
-            self.extend(iter)
+import operator
+try:
+    from thread import get_ident as _thread_ident
+except ImportError:
+    def _thread_ident():
+        return -1
 
-    def appendleft(self, item):
-        self.insert(0, item)
+
+n = 30
+LFTLNK = n
+RGTLNK = n+1
+BLOCKSIZ = n+2
+
+class deque(object):
+
+    def __new__(cls, iterable=()):
+        self = super(deque, cls).__new__(cls)
+        self.clear()
+        return self
+
+    def __init__(self, iterable=()):
+        add = self.append
+        for elem in iterable:
+            add(elem)
 
     def clear(self):
-        del self[:]
+        self.right = self.left = [None] * BLOCKSIZ
+        self.rightndx = n//2   # points to last written element
+        self.leftndx = n//2+1
 
-    def extendleft(self, other):
-        self[0:0] = [x for x in other][::-1]
+    def append(self, x):
+        self.rightndx += 1
+        if self.rightndx == n:
+            newblock = [None] * BLOCKSIZ
+            self.right[RGTLNK] = newblock
+            newblock[LFTLNK] = self.right
+            self.right = newblock
+            self.rightndx = 0
+        self.right[self.rightndx] = x
 
-    def popleft(self):
-        x = self[0]
-        del self[0]
+    def appendleft(self, x):
+        self.leftndx -= 1
+        if self.leftndx == -1:
+            newblock = [None] * BLOCKSIZ
+            self.left[LFTLNK] = newblock
+            newblock[RGTLNK] = self.left
+            self.left = newblock
+            self.leftndx = n-1
+        self.left[self.leftndx] = x
+
+    def extend(self, iterable):
+        for elem in iterable:
+            self.append(elem)
+
+    def extendleft(self, iterable):
+        for elem in iterable:
+            self.appendleft(elem)
+
+    def pop(self):
+        if self.left is self.right and self.leftndx > self.rightndx:
+            raise IndexError, "pop from an empty deque"
+        x = self.right[self.rightndx]
+        self.right[self.rightndx] = None
+        self.rightndx -= 1
+        if self.rightndx == -1:
+            prevblock = self.right[LFTLNK]
+            if prevblock is None:
+                # the deque has become empty; recenter instead of freeing block
+                self.rightndx = n//2
+                self.leftndx = n//2+1
+            else:
+                prevblock[RGTLNK] = None
+                self.right[LFTLNK] = None
+                self.right = prevblock
+                self.rightndx = n-1
         return x
 
-    def rotate(self, step=1):
-        if len(self) == 0:
+    def popleft(self):
+        if self.left is self.right and self.leftndx > self.rightndx:
+            raise IndexError, "pop from an empty deque"
+        x = self.left[self.leftndx]
+        self.left[self.leftndx] = None
+        self.leftndx += 1
+        if self.leftndx == n:
+            prevblock = self.left[RGTLNK]
+            if prevblock is None:
+                # the deque has become empty; recenter instead of freeing block
+                self.rightndx = n//2
+                self.leftndx = n//2+1
+            else:
+                prevblock[LFTLNK] = None
+                self.left[RGTLNK] = None
+                self.left = prevblock
+                self.leftndx = 0
+        return x
+
+    def remove(self, value):
+        del self[operator.indexOf(self, value)]
+
+    def rotate(self, n=1):
+        length = len(self)
+        if length == 0:
             return
-        step %= len(self)
-        for i in range(step):
+        halflen = (length+1) >> 1
+        if n > halflen or n < -halflen:
+            n %= length
+            if n > halflen:
+                n -= length
+            elif n < -halflen:
+                n += length
+        while n > 0:
             self.appendleft(self.pop())
+            n -= 1
+        while n < 0:
+            self.append(self.popleft())
+            n += 1
 
     def __repr__(self):
-        return "%s(%s)" % (type(self).__name__, list.__repr__(self))
+        threadlocalattr = '__repr' + str(_thread_ident())
+        if threadlocalattr in self.__dict__:
+            return 'deque([...])'
+        else:
+            self.__dict__[threadlocalattr] = True
+            try:
+                return 'deque(%r)' % (list(self),)
+            finally:
+                del self.__dict__[threadlocalattr]
+
+    def __iter__(self):
+        block = self.left
+        while block:
+            l, r = 0, n
+            if block is self.left:
+                l = self.leftndx
+            if block is self.right:
+                r = self.rightndx + 1
+            for elem in block[l:r]:
+                yield elem
+            block = block[RGTLNK]
+
+    def __reversed__(self):
+        block = self.right
+        while block:
+            l, r = 0, n
+            if block is self.left:
+                l = self.leftndx
+            if block is self.right:
+                r = self.rightndx + 1
+            for elem in reversed(block[l:r]):
+                yield elem
+            block = block[LFTLNK]
+
+    def __len__(self):
+        sum = 0
+        block = self.left
+        while block:
+            sum += n
+            block = block[RGTLNK]
+        return sum + self.rightndx - self.leftndx + 1 - n
+
+    def __getref(self, index):
+        if index >= 0:
+            block = self.left
+            while block:
+                l, r = 0, n
+                if block is self.left:
+                    l = self.leftndx
+                if block is self.right:
+                    r = self.rightndx + 1
+                span = r-l
+                if index < span:
+                    return block, l+index
+                index -= span
+                block = block[RGTLNK]
+        else:
+            block = self.right
+            while block:
+                l, r = 0, n
+                if block is self.left:
+                    l = self.leftndx
+                if block is self.right:
+                    r = self.rightndx + 1
+                negative_span = l-r
+                if index >= negative_span:
+                    return block, r+index
+                index -= negative_span
+                block = block[LFTLNK]
+        raise IndexError("deque index out of range")
+
+    def __getitem__(self, index):
+        block, index = self.__getref(index)
+        return block[index]
+
+    def __setitem__(self, index, value):
+        block, index = self.__getref(index)
+        block[index] = value
+
+    def __delitem__(self, index):
+        length = len(self)
+        if index >= 0:
+            if index >= length:
+                raise IndexError("deque index out of range")
+            self.rotate(-index)
+            self.popleft()
+            self.rotate(index)
+        else:
+            index = ~index
+            if index >= length:
+                raise IndexError("deque index out of range")
+            self.rotate(index)
+            self.pop()
+            self.rotate(-index)
 
     def __reduce_ex__(self, proto):
         return type(self), (), self.__dict__, iter(self), None
 
-    # We want to hide the fact that this deque is a subclass of list:
-    # <type deque> should not be equal to <type list>
+    def __hash__(self):
+        raise TypeError, "deque objects are unhashable"
+
+    def __copy__(self):
+        return self.__class__(self)
+
+    # XXX make comparison more efficient
     def __eq__(self, other):
-        return isinstance(other, deque) and list.__eq__(self, other)
-    
+        if isinstance(other, deque):
+            return list(self) == list(other)
+        else:
+            return NotImplemented
+
     def __ne__(self, other):
-        return not isinstance(other, deque) or list.__ne__(self, other)
-    
+        if isinstance(other, deque):
+            return list(self) != list(other)
+        else:
+            return NotImplemented
+
     def __lt__(self, other):
-        return isinstance(other, deque) and list.__lt__(self, other)
-    
+        if isinstance(other, deque):
+            return list(self) < list(other)
+        else:
+            return NotImplemented
+
     def __le__(self, other):
-        return isinstance(other, deque) and list.__le__(self, other)
-    
-    def __ge__(self, other):
-        return isinstance(other, deque) and list.__ge__(self, other)
-    
+        if isinstance(other, deque):
+            return list(self) <= list(other)
+        else:
+            return NotImplemented
+
     def __gt__(self, other):
-        return isinstance(other, deque) and list.__gt__(self, other)
+        if isinstance(other, deque):
+            return list(self) > list(other)
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, deque):
+            return list(self) >= list(other)
+        else:
+            return NotImplemented
+
