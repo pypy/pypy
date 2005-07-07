1640430c5d13ab3d78a108cdb9dd08acd96ba39b
Armin Rigo <arigo@tunes.org>
2005-07-07 18:18:47 +0000

    Cosmetics of file and macro names.

--- pypy/translator/c/extfunc_include.h
+++ pypy/translator/c/extfunc_include.h
@@ -9,19 +9,19 @@
 /* The functions below are mapped to functions from pypy.rpython.extfunctable
    by the pypy.translator.c.fixedname.EXTERNALS dictionary. */
 
-#define RPyString_GET_SIZE(rps)		((rps)->rs_chars.length)
-#define RPyString_AS_STRING(rps)	((rps)->rs_chars.items)
+#define RPyString_Size(rps)		((rps)->rs_chars.length)
+#define RPyString_AsString(rps)		((rps)->rs_chars.items)
 
 
 int LL_os_open(RPyString *filename, int flag, int mode)
 {
 	char buf[PATH_MAX];
-	int fd, error, namelen = RPyString_GET_SIZE(filename);
+	int fd, error, namelen = RPyString_Size(filename);
 	if (namelen >= PATH_MAX) {
 		error = ENAMETOOLONG;
 	}
 	else {
-		memcpy(buf, RPyString_AS_STRING(filename), namelen);
+		memcpy(buf, RPyString_AsString(filename), namelen);
 		buf[namelen] = 0;
 		fd = open(buf, flag, mode);
 		if (fd != -1)
