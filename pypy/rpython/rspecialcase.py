e5ab86c7d6f6d7d99dd9ee74e4250447d763430a
Samuele Pedroni <pedronis@lucediurna.net>
2005-07-18 14:39:08 +0000

    override:ignore support for methods too

--- pypy/rpython/rspecialcase.py
+++ pypy/rpython/rspecialcase.py
@@ -6,10 +6,10 @@ from pypy.rpython.rmodel import TyperError, inputconst
 
 
 def rtype_call_specialcase(hop):
-    v_function = hop.args_v[0]
-    if not isinstance(v_function, Constant):
-        raise TyperError("call_specialcase on a variable function")
-    func = v_function.value
+    s_function = hop.args_s[0]
+    if len(s_function.prebuiltinstances) != 1:
+        raise TyperError("not monomorphic call_specialcase")
+    func, clsdef = s_function.prebuiltinstances.items()[0]
     tag = func._annspecialcase_
     if not tag.startswith("override:"):
         raise TyperError("call_specialcase only supports 'override:' functions")
@@ -20,11 +20,9 @@ def rtype_call_specialcase(hop):
         raise TyperError("call_specialcase: unknown tag override:" + tag)
     hop2 = hop.copy()
     hop2.r_s_popfirstarg()
-    return rtype_override_fn(hop2)
+    return rtype_override_fn(hop2, clsdef)
 
 
-# def rtype_override_XXX to be added later
 
-
-def rtype_override_ignore(hop):
+def rtype_override_ignore(hop, clsdef): # ignore works for methods too
     return inputconst(hop.r_result, None)
--- pypy/rpython/test/test_rspecialcase.py
+++ pypy/rpython/test/test_rspecialcase.py
@@ -20,3 +20,19 @@ def test_override_ignore():
     assert ''.join(res.chars) == "ab"
     
         
+def test_meth_override_ignore():
+    class X:
+        def f(self):
+            pass
+        f._annspecialcase_ = "override:ignore"
+    def g(i):
+        x = X()
+        if i == 1:
+            return "ab"
+        else:
+            return x.f()
+
+    res = interpret(g, [0])
+    assert not res
+    res = interpret(g, [1])
+    assert ''.join(res.chars) == "ab"
