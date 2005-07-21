e2e5ed27c41940647460e48b4678f15517891ff2
Samuele Pedroni <pedronis@lucediurna.net>
2005-07-21 21:08:34 +0000

    - default error fallback for hardwired_*call*
    - don't loop for setting rtype_contains default

--- pypy/rpython/rmodel.py
+++ pypy/rpython/rmodel.py
@@ -168,12 +168,18 @@ def setattr_default(obj, attr, value):
 
 for opname in annmodel.UNARY_OPERATIONS:
     setattr_default(Repr, 'rtype_' + opname, missing_rtype_operation)
+
+# hardwired_*call*
+setattr_default(Repr, 'rtype_hardwired_simple_call', missing_rtype_operation)
+setattr_default(Repr, 'rtype_hardwired_call_args'  , missing_rtype_operation)
+
 for opname in annmodel.BINARY_OPERATIONS:
     setattr_default(pairtype(Repr, Repr),
                     'rtype_' + opname, missing_rtype_operation)
+
 # not in BINARY_OPERATIONS
-    setattr_default(pairtype(Repr, Repr),
-                    'rtype_contains', missing_rtype_operation)
+setattr_default(pairtype(Repr, Repr),
+                'rtype_contains', missing_rtype_operation)
 
 class __extend__(pairtype(Repr, Repr)):
     def convert_from_to((r_from, r_to), v, llops):
