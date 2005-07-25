0ab0541020804e1b327e8050f3bdf824050df049
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-07-25 22:02:53 +0000

    (pedronis, cfbolz) trying to fix os.path.exists. still failing tests

--- pypy/rpython/module/ll_os_path.py
+++ pypy/rpython/module/ll_os_path.py
@@ -7,15 +7,25 @@ Dummy low-level implementations for the external functions of the 'os.path' modu
 import os
 from pypy.rpython.rstr import STR
 from pypy.rpython.module.support import to_rstr, from_rstr, ll_strcpy
-
+from pypy.rpython.module.ll_os import ll_os_stat
 
 # Does a path exist?
 # This is false for dangling symbolic links.
 
+## This version produces nonsense, keeping it for reference
+## def ll_os_path_exists(path):
+##     """Test whether a path exists"""
+##     try:
+##         st = os.stat(from_rstr(path))
+##     except OSError:
+##         return False
+##     return True
+
 def ll_os_path_exists(path):
     """Test whether a path exists"""
     try:
-        st = os.stat(from_rstr(path))
+        st = ll_os_stat(path)
     except OSError:
         return False
     return True
+
