a12a7a0c4aeea9ea9486f4e765e899941aefa728
Christian Tismer <tismer@stackless.com>
2005-07-25 11:43:16 +0000

    removed divmod

--- pypy/module/unicodedata/generate_unicodedb.py
+++ pypy/module/unicodedata/generate_unicodedb.py
@@ -353,8 +353,12 @@ def name(code):
         0x20000 <= code <= 0x2A6D6):
         return "CJK UNIFIED IDEOGRAPH-" + hex(code)[2:].upper()
     if 0xAC00 <= code <= 0xD7A3:
-        vl_code, t_code = divmod(code - 0xAC00, len(_hangul_T))
-        l_code, v_code = divmod(vl_code,  len(_hangul_V))
+        # vl_code, t_code = divmod(code - 0xAC00, len(_hangul_T))
+        vl_code = (code - 0xAC00) // len(_hangul_T)
+        t_code = (code - 0xAC00) % len(_hangul_T)
+        # l_code, v_code = divmod(vl_code,  len(_hangul_V))
+        l_code = vl_code // len(_hangul_V)
+        v_code = vl_code % len(_hangul_V)
         return ("HANGUL SYLLABLE " + _hangul_L[l_code] +
                 _hangul_V[v_code] + _hangul_T[t_code])
     
