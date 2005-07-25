975e5391577f6e63932d96b2e3c606a56aa6f6e6
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-07-25 11:33:03 +0000

    (pedronis, cfbolz) added tests for ll_math

---
+++ pypy/rpython/module/test/test_ll_math.py
@@ -0,0 +1,19 @@
+import math
+from pypy.rpython.module.ll_math import *
+
+def test_ll_math_atan2():
+    for x in [0, 1, math.pi, 0.123]:
+        for y in [0, 1, math.pi, 0.123]:
+            assert math.atan2(x, y) == ll_math_atan2(x, y)
+
+def test_ll_modf():
+    result = ll_math_modf(10.1)
+    assert result.item0 == math.modf(10.1)[0]
+    assert result.item1 == math.modf(10.1)[1]
+
+def test_ll_frexp():
+    result = ll_math_frexp(10.1)
+    assert result.item0 == math.frexp(10.1)[0]
+    assert result.item1 == math.frexp(10.1)[1]
+
+
