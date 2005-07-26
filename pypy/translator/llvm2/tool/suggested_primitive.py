3abe9e4c08d39215590c0c9c5bb5201ba91a94c8
Eric van Riet Paap <ericvrp@gmail.com>
2005-07-26 21:43:45 +0000

    - show missing primitives only - split-up per module/ll_* file - (minor) using Set instead of dicts

--- pypy/translator/llvm2/tool/suggested_primitive.py
+++ pypy/translator/llvm2/tool/suggested_primitive.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python
 
 import autopath
-import os
+from sets import Set
 
 from pypy.rpython.module import ll_os, ll_os_path, ll_time, ll_math #XXX keep this list up-to-date
 from pypy.translator.llvm2.module.extfunction import extfunctions
 
 def main():
-    suggested_primitives = []
     for module in (ll_os, ll_os_path, ll_time, ll_math):    #XXX keep this list up-to-date too
-        suggested_primitives += [func for func in dir(module) if getattr(module.__dict__[func], 'suggested_primitive', False)]
-
-    implemented_primitives = [f[1:] for f in extfunctions.keys()]
-
-    missing_primitives = [func for func in suggested_primitives if func not in implemented_primitives]
-
-    print 'rpython suggested primitives:'
-    print suggested_primitives
-    print
-    print 'llvm implemented primitives:'
-    print implemented_primitives
-    print
-    print 'llvm missing primitives:'
-    print missing_primitives
+        suggested_primitives   = Set( [func for func in dir(module) if getattr(module.__dict__[func], 'suggested_primitive', False)] )
+        implemented_primitives = Set( [f[1:] for f in extfunctions.keys()] )
+        missing_primitives     = suggested_primitives - implemented_primitives
+        print 'Missing llvm primitives for %s:' % module.__name__
+        for m in missing_primitives:
+            print '   %s' % m
 
 if __name__ == '__main__':
     main()
