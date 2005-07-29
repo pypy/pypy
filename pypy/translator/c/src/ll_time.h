52e24db32f7c998e99ae1989f457eef5008a5118
Samuele Pedroni <pedronis@lucediurna.net>
2005-07-29 16:30:51 +0000

    more precise time

--- pypy/translator/c/src/ll_time.h
+++ pypy/translator/c/src/ll_time.h
@@ -99,12 +99,47 @@ void LL_time_sleep(double secs)
 }
 
 
+#ifdef HAVE_FTIME
+#include <sys/timeb.h>
+#if !defined(MS_WINDOWS) && !defined(PYOS_OS2)
+extern int ftime(struct timeb *);
+#endif /* MS_WINDOWS */
+#endif /* HAVE_FTIME */
+
 static double
 ll_floattime(void)
 {
-	time_t secs;
-	time(&secs);
-	return (double)secs;
+	/* There are three ways to get the time:
+	  (1) gettimeofday() -- resolution in microseconds
+	  (2) ftime() -- resolution in milliseconds
+	  (3) time() -- resolution in seconds
+	  In all cases the return value is a float in seconds.
+	  Since on some systems (e.g. SCO ODT 3.0) gettimeofday() may
+	  fail, so we fall back on ftime() or time().
+	  Note: clock resolution does not imply clock accuracy! */
+#ifdef HAVE_GETTIMEOFDAY
+	{
+		struct timeval t;
+#ifdef GETTIMEOFDAY_NO_TZ
+		if (gettimeofday(&t) == 0)
+			return (double)t.tv_sec + t.tv_usec*0.000001;
+#else /* !GETTIMEOFDAY_NO_TZ */
+		if (gettimeofday(&t, (struct timezone *)NULL) == 0)
+			return (double)t.tv_sec + t.tv_usec*0.000001;
+#endif /* !GETTIMEOFDAY_NO_TZ */
+	}
+#endif /* !HAVE_GETTIMEOFDAY */
+	{
+#if defined(HAVE_FTIME)
+		struct timeb t;
+		ftime(&t);
+		return (double)t.time + (double)t.millitm * (double)0.001;
+#else /* !HAVE_FTIME */
+		time_t secs;
+		time(&secs);
+		return (double)secs;
+#endif /* !HAVE_FTIME */
+	}
 }
 
 double LL_time_time(void) /* xxx had support for better resolutions */
