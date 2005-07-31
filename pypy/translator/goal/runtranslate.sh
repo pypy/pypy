ea21f238a3d59212b7afa6afa3c2cd74182e8ceb
Holger Krekel <holger.krekel@gmail.com>
2005-07-31 12:25:33 +0000

    simplify main invocation of translation target

--- pypy/translator/goal/runtranslate.sh
+++ pypy/translator/goal/runtranslate.sh
@@ -2,4 +2,4 @@ export RTYPERORDER=order,module-list.pedronis
 # stopping on the first error
 #python translate_pypy.py -no-c -no-o -text  -no-snapshot -fork
 # running it all 
-python translate_pypy.py -no-c -no-o -text -t-insist -no-snapshot -fork
+python translate_pypy.py -no-o -text -t-insist -no-snapshot 
