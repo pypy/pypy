323c050ced4cf74b759f344afeb359cef97437b5
Armin Rigo <arigo@tunes.org>
2005-08-04 22:41:05 +0000

    This version translates and builds a stand-alone executable.

--- pypy/translator/goal/runtranslate.sh
+++ pypy/translator/goal/runtranslate.sh
@@ -2,7 +2,7 @@ export RTYPERORDER=order,module-list.pedronis
 # stopping on the first error
 #python translate_pypy.py -no-c -no-o -text  -no-snapshot -fork
 # running it all 
-python translate_pypy.py -no-o -text -t-insist -no-snapshot $*
+python translate_pypy.py targetpypystandalone -text -no-snapshot $*
 
 
 # How to work in parallel:
