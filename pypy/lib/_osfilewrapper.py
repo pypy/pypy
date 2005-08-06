ee56f719ba25343503cd98c20b6dd434bb36e4fe
Christian Tismer <tismer@stackless.com>
2005-08-06 10:48:25 +0000

    enabling marshal:
    _osfilewrapper had problems with os.write. I had to add
    an assertion that this is alsway non-negative. Maybe
    we want to add this info directly to os.write.

--- pypy/lib/_osfilewrapper.py
+++ pypy/lib/_osfilewrapper.py
@@ -25,6 +25,8 @@ class OsFileWrapper(object):
         towrite = len(buf)
         while writecount < towrite:
             # os.write will raise an error itself
+            assert writecount >= 0 # annotator hint, don't remove.
+            # XXX should be tellable in extfunctable
             writecount += os.write(self.fd, buf[writecount:])
 
     def close(self):
