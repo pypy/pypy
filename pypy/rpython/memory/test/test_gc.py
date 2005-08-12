54eedcbd3c1e59c6163972ad3866a04cf766f9ae
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-08-12 19:59:26 +0000

    test for last checkin.
    The checkin message of the last checkin should have been:
    
    changed convertllype so that the gc can also handle programs where
    globals contain root pointers. Global constants now have two words for
    the GC reserved as well.

--- pypy/rpython/memory/test/test_gc.py
+++ pypy/rpython/memory/test/test_gc.py
@@ -131,3 +131,17 @@ class TestMarkSweepGC(object):
         res = interpret(malloc_a_lot, [])
         assert simulator.current_size - curr < 16000
         print "size before: %s, size after %s" % (curr, simulator.current_size)
+
+    def test_global_list(self):
+        curr = simulator.current_size
+        lst = []
+        def append_to_list(i, j):
+            lst.append([i] * 500)
+            return lst[j][0]
+        res = interpret(append_to_list, [0, 0])
+        assert res == 0
+        for i in range(1, 15):
+            res = interpret(append_to_list, [i, i - 1])
+            assert res == i - 1# crashes if constants are not considered roots
+            
+            
