f01eb15fe0de639991e280200f8f1cd648b13d14
Armin Rigo <arigo@tunes.org>
2005-08-16 20:26:15 +0000

    tls.py can provide a complete thread-local object even on top of Python 2.3, because there is a pure Python version of it in 2.4.  We can hack and import it from lib-python/2.4.1 and it appears to work nicely on 2.3 as well.
    Thanks xorAxAx for pointing this out.

--- pypy/tool/tls.py
+++ pypy/tool/tls.py
@@ -7,5 +7,10 @@ except ImportError:   # Python < 2.4
     # XXX needs a real object whose attributes are visible only in
     #     the thread that reads/writes them.
 
-    class tlsobject(object):
-        pass
+    import autopath, os
+    filename = os.path.join(os.path.dirname(autopath.pypydir),
+                            'lib-python', '2.4.1', '_threading_local.py')
+    glob = {'__name__': '_threading_local'}
+    execfile(filename, glob)
+    tlsobject = glob['local']
+    del glob, filename
