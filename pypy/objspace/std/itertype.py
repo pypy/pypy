77e4c7c45581c59d5d256c579014e26f1f77245e
Laura Creighton <lac@openend.se>
2005-08-24 11:12:21 +0000

    These poor functions were too unpopulated for the tool to know where to insert the docstring and get the indentation correct.

--- pypy/objspace/std/itertype.py
+++ pypy/objspace/std/itertype.py
@@ -2,6 +2,12 @@ from pypy.objspace.std.stdtypedef import *
 
 # ____________________________________________________________
 iter_typedef = StdTypeDef("sequenceiterator",
+    __doc__ = '''iter(collection) -> iterator
+iter(callable, sentinel) -> iterator
+
+Get an iterator from an object.  In the first form, the argument must
+supply its own iterator, or be a sequence.
+In the second form, the callable is called until it returns the sentinel.'''
     )
 
 reverse_iter_typedef = StdTypeDef("reversesequenceiterator",
