da437f9e046cc1bacad3e5497c196cbf31f94828
Ludovic Aubry <ludal@codespeak.net>
2005-08-25 09:06:33 +0000

    added SyntaxError class because we raise it and it needs to be annotatable

--- pypy/interpreter/pyparser/error.py
+++ pypy/interpreter/pyparser/error.py
@@ -22,3 +22,28 @@ class ParseError(Exception):
                                              self.lineno,
                                              self.offset,
                                              self.text)
+
+
+class SyntaxError(Exception):
+    """Base class for exceptions raised by the parser."""
+
+    def __init__(self, msg, lineno=0, offset=0, text=0):
+        self.msg = msg
+        self.lineno = lineno
+        self.offset = offset
+        self.text = text
+        self.filename = ""
+        self.print_file_and_line = False
+
+    def wrap_info(self, space, filename):
+        return space.newtuple([space.wrap(self.msg),
+                               space.newtuple([space.wrap(filename),
+                                               space.wrap(self.lineno),
+                                               space.wrap(self.offset),
+                                               space.wrap(self.text)])])
+
+    def __str__(self):
+        return "%s at pos (%d, %d) in %r" % (self.__class__.__name__,
+                                             self.lineno,
+                                             self.offset,
+                                             self.text)
