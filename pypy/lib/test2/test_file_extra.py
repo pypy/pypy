7e48e78889b917d00c661062cee1cd8410f370be
Anders Chrigstrom <anders@chrigstrom.se>
2005-08-26 12:23:37 +0000

    Apply patch from Ben Young.

--- pypy/lib/test2/test_file_extra.py
+++ pypy/lib/test2/test_file_extra.py
@@ -27,7 +27,7 @@ class TestFile:
     def test_repr(self):
         r = repr(self.file)
         assert r.find('open file') >= 0
-        assert r.find(self.file.name) >= 0
+        assert r.find(repr(self.file.name)) >= 0
         assert r.find(self.file.mode) >= 0
 
 class TestFdFile(TestFile):
