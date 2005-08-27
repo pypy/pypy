d125df26de58c64bd418b008d45b0a34ba233d5b
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-08-27 09:18:26 +0000

    test for os.fdopen that uses libfile

---
+++ pypy/module/posix/test/test_posix_libfile.py
@@ -0,0 +1,25 @@
+from pypy.objspace.std import StdObjSpace 
+from pypy.tool.udir import udir
+import os
+
+def setup_module(mod): 
+    mod.space = StdObjSpace(usemodules=['posix'], uselibfile=True)
+    mod.path = udir.join('posixtestfile.txt') 
+    mod.path.write("this is a test")
+
+class AppTestPosix: 
+    def setup_class(cls): 
+        cls.space = space 
+        cls.w_posix = space.appexec([], "(): import %s as m ; return m" % os.name)
+        cls.w_path = space.wrap(str(path))
+    
+    def test_posix_is_pypy_s(self): 
+        assert self.posix.__file__ 
+
+    def test_fdopen(self):
+        path = self.path 
+        posix = self.posix 
+        fd = posix.open(path, posix.O_RDONLY, 0777)
+        f = posix.fdopen(fd, "r")
+        result = f.read()
+        assert result == "this is a test"
