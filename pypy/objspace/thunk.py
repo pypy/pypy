9fc2980ab73e401463e5cf0cbc746d233c2a12c6
Armin Rigo <arigo@tunes.org>
2005-08-29 16:52:04 +0000

    Small clean-up of the thunk object space. This should also make it translatable :-)

--- pypy/objspace/thunk.py
+++ pypy/objspace/thunk.py
@@ -19,60 +19,116 @@
 
 from proxy import create_proxy_space
 from pypy.interpreter import gateway, baseobjspace, argument
+from pypy.interpreter.error import OperationError
 
 # __________________________________________________________________________
 
+# 'w_obj.w_thunkalias' points to another object that 'w_obj' has turned into
+baseobjspace.W_Root.w_thunkalias = None
+
 class W_Thunk(baseobjspace.W_Root, object):
-    def __init__(w_self, space, w_callable, args=argument.Arguments([])):
-        w_self.space = space
+    def __init__(w_self, w_callable, args=argument.Arguments([])):
         w_self.w_callable = w_callable
         w_self.args = args
-        w_self.w_value = None
 
-def force(w_self):
-    while isinstance(w_self, W_Thunk):
-        if w_self.w_value is None:
-            w_self.w_value = w_self.space.call_args(w_self.w_callable,
-                                                    w_self.args)
+# special marker to say that w_self has not been computed yet
+w_NOT_COMPUTED_THUNK = W_Thunk(None, None)
+W_Thunk.w_thunkalias = w_NOT_COMPUTED_THUNK
+
+
+def force(space, w_self):
+    w_alias = w_self.w_thunkalias
+    while w_alias is not None:
+        if w_alias is w_NOT_COMPUTED_THUNK:
+            w_callable = w_self.w_callable
+            args       = w_self.args
+            if w_callable is None:
+                raise OperationError(space.w_RuntimeError,
+                                     "thunk is already being computed")
             w_self.w_callable = None
-            w_self.args = None
-        w_self = w_self.w_value
+            w_self.args       = None
+            w_alias = space.call_args(w_callable, args)
+            w_self.w_thunkalias = w_alias
+        w_self = w_alias
+        w_alias = w_self.w_thunkalias
     return w_self
 
-def thunk(space, w_callable, __args__):
-    return W_Thunk(space, w_callable, __args__)
-app_thunk = gateway.interp2app(thunk, unwrap_spec=[baseobjspace.ObjSpace,
-                                                   baseobjspace.W_Root,
+def thunk(w_callable, __args__):
+    return W_Thunk(w_callable, __args__)
+app_thunk = gateway.interp2app(thunk, unwrap_spec=[baseobjspace.W_Root,
                                                    argument.Arguments])
 
 def is_thunk(space, w_obj):
-    return space.newbool(isinstance(w_obj, W_Thunk))
+    return space.newbool(w_obj.w_thunkalias is w_NOT_COMPUTED_THUNK)
 app_is_thunk = gateway.interp2app(is_thunk)
 
 def become(space, w_target, w_source):
-    while isinstance(w_target, W_Thunk) and w_target.w_value is not None:
-        w_target = w_target.w_value
-    w_target.__class__ = W_Thunk
-    w_target.__dict__ = {'w_value': w_source}
+    w_target = force(space, w_target)
+    w_target.w_thunkalias = w_source
     return space.w_None
 app_become = gateway.interp2app(become)
 
 # __________________________________________________________________________
 
-operation_args_that_dont_force = {
-    ('setattr', 2): True,
-    ('setitem', 2): True,
-    }
+nb_forcing_args = {}
+
+def setup():
+    nb_forcing_args.update({
+        'setattr': 2,   # instead of 3
+        'setitem': 2,   # instead of 3
+        'get': 2,       # instead of 3
+        # ---- irregular operations ----
+        'wrap': 0,
+        'str_w': 1,
+        'int_w': 1,
+        'float_w': 1,
+        'uint_w': 1,
+        'interpclass_w': 1,
+        'unwrap': 1,
+        'is_true': 1,
+        'is_w': 2,
+        'newtuple': 0,
+        'newlist': 0,
+        'newstring': 0,
+        'newunicode': 0,
+        'newdict': 0,
+        'newslice': 0,
+        'call_args': 1,
+        'marshal_w': 1,
+        'log': 1,
+        })
+    for opname, _, arity, _ in baseobjspace.ObjSpace.MethodTable:
+        nb_forcing_args.setdefault(opname, arity)
+    for opname in baseobjspace.ObjSpace.IrregularOpTable:
+        assert opname in nb_forcing_args, "missing %r" % opname
+
+setup()
+del setup
+
+# __________________________________________________________________________
 
 def proxymaker(space, opname, parentfn):
-    def proxy(*args):
-        newargs = []
-        for i in range(len(args)):
-            a = args[i]
-            if (opname, i) not in operation_args_that_dont_force:
-                a = force(a)
-            newargs.append(a)
-        return parentfn(*newargs)
+    nb_args = nb_forcing_args[opname]
+    if nb_args == 0:
+        return parentfn
+    if nb_args == 1:
+        def proxy(w1, *extra):
+            w1 = force(space, w1)
+            return parentfn(w1, *extra)
+    elif nb_args == 2:
+        def proxy(w1, w2, *extra):
+            w1 = force(space, w1)
+            w2 = force(space, w2)
+            return parentfn(w1, w2, *extra)
+    elif nb_args == 3:
+        def proxy(w1, w2, w3, *extra):
+            w1 = force(space, w1)
+            w2 = force(space, w2)
+            w3 = force(space, w3)
+            return parentfn(w1, w2, w3, *extra)
+    else:
+        raise NotImplementedError("operation %r has arity %d" %
+                                  (opname, nb_args))
     return proxy
 
 def Space(space=None):
