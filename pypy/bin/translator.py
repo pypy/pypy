27c9f94349635129a4fbfc7d8028057af06a5634
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2005-09-06 22:44:46 +0000

    Removed duplicate readline code.

--- pypy/bin/translator.py
+++ pypy/bin/translator.py
@@ -236,10 +236,7 @@ if __name__ == '__main__':
     from pypy.translator.test import snippet as test
     #from pypy.translator.llvm.test import llvmsnippet as test2
     from pypy.rpython.rtyper import RPythonTyper
-    try:
-        setup_readline()
-    except ImportError, err:
-        print "Disabling readline support (%s)" % err
+
     if (os.getcwd() not in sys.path and
         os.path.curdir not in sys.path):
         sys.path.insert(0, os.getcwd())
