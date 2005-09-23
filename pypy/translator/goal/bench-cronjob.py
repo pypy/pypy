03951cec1315bf9b150c03aa889b6b666dbd9dea
Eric van Riet Paap <ericvrp@gmail.com>
2005-09-23 15:16:04 +0000

    work in progress

--- pypy/translator/goal/bench-cronjob.py
+++ pypy/translator/goal/bench-cronjob.py
@@ -13,20 +13,18 @@ def update_llvm():
     os.system('cvs -q up 2>&1')
     os.system('make tools-only 2>&1')
 
-def get_names():
+def compile(backend):
+    os.chdir(homedir + '/projects/pypy-dist/pypy/translator/goal')
+    os.system('python translate_pypy_new.py targetpypystandalone --backend=%(backend)s --pygame --batch -r 2>&1' % locals())
+
     os.chdir(homedir + '/projects/pypy-dist')
     try:
         revision = os.popen('svn info 2>&1').readlines()[3].split()[1]
     except:
         revision = 'unknown'
     basename = homedir + '/projects/pypy-dist/pypy/translator/goal/' + 'pypy-' + backend
-    realname = basename+'-'+revision()
-    return basename, realname
-    
-def compile(backend):
-    os.chdir(homedir + '/projects/pypy-dist/pypy/translator/goal')
-    os.system('python translate_pypy_new.py targetpypystandalone --backend=%(backend)s --pygame --batch -r 2>&1' % locals())
-    basename, realname = get_names()
+    realname = basename + '-' + revision
+
     os.open(realname, 'wb').write( open(basename).read() )
     os.chmod(realname, stat.S_IRWXU)
     os.unlink(basename)
