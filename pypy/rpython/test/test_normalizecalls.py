78ec0faa0b581d8a9a3e32e72c84a6e72691bdd8
Armin Rigo <arigo@tunes.org>
2005-10-01 14:42:56 +0000

    Added some tests here, in an attempt to find a bug that was elsewhere after all.

--- pypy/rpython/test/test_normalizecalls.py
+++ pypy/rpython/test/test_normalizecalls.py
@@ -1,6 +1,8 @@
 from pypy.annotation import model as annmodel
 from pypy.translator.translator import Translator
 from pypy.rpython.rtyper import RPythonTyper
+from pypy.rpython.test.test_llinterp import interpret
+from pypy.rpython import lltype
 
 
 def rtype(fn, argtypes=[]):
@@ -55,3 +57,67 @@ def test_normalize_keyword_call():
     assert len(f1graph.getargs()) == 2
     assert len(f2graph.getargs()) == 2   # normalized to the common call pattern
     #translator.view()
+
+def test_normalize_returnvar():
+    def add_one(n):
+        return n+1
+    def add_half(n):
+        return n+0.5
+    def dummyfn(n, i):
+        if i == 1:
+            adder = add_one
+        else:
+            adder = add_half
+        return adder(n)
+
+    res = interpret(dummyfn, [52, 1])
+    assert type(res) is float and res == 53.0
+    res = interpret(dummyfn, [7, 2])
+    assert type(res) is float and res == 7.5
+
+def test_normalize_missing_return():
+    def add_one(n):
+        return n+1
+    def oups(n):
+        raise ValueError
+    def dummyfn(n, i):
+        if i == 1:
+            adder = add_one
+        else:
+            adder = oups
+        try:
+            return adder(n)
+        except ValueError:
+            return -1
+
+    translator = rtype(dummyfn, [int, int])
+    add_one_graph = translator.getflowgraph(add_one)
+    oups_graph    = translator.getflowgraph(oups)
+    assert add_one_graph.getreturnvar().concretetype == lltype.Signed
+    assert oups_graph   .getreturnvar().concretetype == lltype.Signed
+    #translator.view()
+
+def test_normalize_abstract_method():
+    class Base:
+        def fn(self):
+            raise NotImplementedError
+    class Sub1(Base):
+        def fn(self):
+            return 1
+    class Sub2(Base):
+        def fn(self):
+            return 2
+    def dummyfn(n):
+        if n == 1:
+            x = Sub1()
+        else:
+            x = Sub2()
+        return x.fn()
+
+    translator = rtype(dummyfn, [int])
+    base_graph = translator.getflowgraph(Base.fn.im_func)
+    sub1_graph = translator.getflowgraph(Sub1.fn.im_func)
+    sub2_graph = translator.getflowgraph(Sub2.fn.im_func)
+    assert base_graph.getreturnvar().concretetype == lltype.Signed
+    assert sub1_graph.getreturnvar().concretetype == lltype.Signed
+    assert sub2_graph.getreturnvar().concretetype == lltype.Signed
