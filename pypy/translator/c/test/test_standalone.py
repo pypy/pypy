e38e8acf75d0eb81536a086c5e24f993c10e3429
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2005-10-11 15:13:35 +0000

    (valentino, afa): stackless: more tests

--- pypy/translator/c/test/test_standalone.py
+++ pypy/translator/c/test/test_standalone.py
@@ -64,6 +64,39 @@ def test_stack_withptr():
     data = wrap_stackless_function(fn)
     assert data.strip() == '10'
 
+def test_stackless_manytimes():
+    def f(n):
+        if n > 0:
+            stack_frames_depth()
+            res = f(n-1)
+        else:
+            res = stack_frames_depth(), 1
+        return res
+
+    def fn():
+        count0, _ = f(0)
+        count10, _ = f(100)
+        return count10 - count0
+
+    data = wrap_stackless_function(fn)
+    assert data.strip() == '100'
+
+def test_stackless_arguments():
+    def f(n, d, t):
+        if n > 0:
+            res = f(n-1, d, t)
+        else:
+            res = stack_frames_depth(), d, t
+        return res
+
+    def fn():
+        count0, d, t = f(0, 5.5, (1, 2))
+        count10, d, t = f(10, 5.5, (1, 2))
+        return "[" + str(count10 - count0) + ", " + str(d) + ", " + str(t[0]) + ", " + str(t[1]) + "]"
+
+    data = wrap_stackless_function(fn)
+    assert eval(data) == [10, 5.500000, 1, 2]
+
 
 def test_stack_too_big():
     def f(n):
