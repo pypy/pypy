2bb20f5a50586d21c6cbcff85e5ea0f65158eaf7
Christian Tismer <tismer@stackless.com>
2005-10-12 10:24:25 +0000

    (anders, chris) added some commentary about creating stand-alone targets

--- pypy/translator/goal/targetrpystonedalone.py
+++ pypy/translator/goal/targetrpystonedalone.py
@@ -12,3 +12,11 @@ def entry_point(argv):
 def target(*args):
     return entry_point, None
 
+"""
+Why is this a stand-alone target?
+
+The above target specifies None as the argument types list.
+This is a case treated specially in the driver.py . If the list
+of input types is empty, it is meant to be a list of strings,
+actually implementing argv of the executable.
+"""
