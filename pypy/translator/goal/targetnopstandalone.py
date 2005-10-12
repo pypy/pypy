589c8670d136f96665db11e572977957797e5eb4
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2005-10-12 17:01:25 +0000

    Adding a comment telling how to build a standalone target

--- pypy/translator/goal/targetnopstandalone.py
+++ pypy/translator/goal/targetnopstandalone.py
@@ -1,8 +1,16 @@
+"""
+A simple standalone target.
+
+The target below specifies None as the argument types list.
+This is a case treated specially in driver.py . If the list
+of input types is empty, it is meant to be a list of strings,
+actually implementing argv of the executable.
+"""
+
 import os, sys
 
 def debug(msg): 
     os.write(2, "debug: " + msg + '\n')
-   
 
 # __________  Entry point  __________
 
@@ -14,4 +22,3 @@ def entry_point(argv):
 
 def target(*args):
     return entry_point, None
-
