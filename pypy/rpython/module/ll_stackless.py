6ef81dcb32abc9bf5388684c7236fa0f55fa19b7
Boris Feigin <boria>
2005-10-14 15:55:09 +0000

    * Removing rpython/ from trunk.

--- pypy/rpython/module/ll_stackless.py
+++
@@ -1,5 +0,0 @@
-from pypy.rpython import objectmodel
-
-def ll_stackless_stack_frames_depth():
-    return objectmodel.stack_frames_depth()
-ll_stackless_stack_frames_depth.suggested_primitive = True
