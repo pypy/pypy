e2562cb8dfd0b07e8b1298dd9bee15cf463e9b15
Michael Hudson <micahel@gmail.com>
2005-10-15 16:04:04 +0000

    M-x untabify

--- pypy/rpython/ootypesystem/rootype.py
+++ pypy/rpython/ootypesystem/rootype.py
@@ -21,33 +21,33 @@ class OOInstanceRepr(Repr):
 
     def rtype_getattr(self, hop):
         attr = hop.args_s[1].const
-	s_inst = hop.args_s[0]
-	meth = self.lowleveltype._lookup(attr)
-	if meth is not None:
-	    # just return instance - will be handled by simple_call
-	    return hop.inputarg(hop.r_result, arg=0)
-	self.lowleveltype._check_field(attr)
-	vlist = hop.inputargs(self, Void)
-	return hop.genop("oogetfield", vlist,
-			 resulttype = hop.r_result.lowleveltype)
+        s_inst = hop.args_s[0]
+        meth = self.lowleveltype._lookup(attr)
+        if meth is not None:
+            # just return instance - will be handled by simple_call
+            return hop.inputarg(hop.r_result, arg=0)
+        self.lowleveltype._check_field(attr)
+        vlist = hop.inputargs(self, Void)
+        return hop.genop("oogetfield", vlist,
+                         resulttype = hop.r_result.lowleveltype)
 
     def rtype_setattr(self, hop):
         attr = hop.args_s[1].const
-	self.lowleveltype._check_field(attr)
+        self.lowleveltype._check_field(attr)
         vlist = hop.inputargs(self, Void, hop.args_r[2])
         return hop.genop('oosetfield', vlist)
 
 class OOBoundMethRepr(Repr):
     def __init__(self, ootype, name):
         self.lowleveltype = ootype
-	self.name = name
+        self.name = name
 
     def rtype_simple_call(self, hop):
-	vlist = hop.inputargs(self, *hop.args_r[1:])
+        vlist = hop.inputargs(self, *hop.args_r[1:])
         cname = hop.inputconst(Void, self.name)
-	return hop.genop("oosend", [cname]+vlist,
-			 resulttype = hop.r_result.lowleveltype)
-	
+        return hop.genop("oosend", [cname]+vlist,
+                         resulttype = hop.r_result.lowleveltype)
+        
 
 class __extend__(pairtype(OOInstanceRepr, OOBoundMethRepr)):
 
