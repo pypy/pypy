ee63c122239579be9dde202e707208ec08ea9f21
Holger Krekel <holger.krekel@gmail.com>
2005-10-15 11:07:21 +0000

    (hpk,cfbolz)
    refactor and heading towards passing inputargs

--- pypy/rpython/l3interp/test/test_l3interp.py
+++ pypy/rpython/l3interp/test/test_l3interp.py
@@ -4,6 +4,15 @@ from pypy.translator.c.test.test_genc import compile
 from pypy.translator.translator import Translator
 from pypy.annotation import policy
 
+def translate(func, inputargs):
+    t = Translator(func)
+    pol = policy.AnnotatorPolicy()
+    pol.allow_someobjects = False
+    t.annotate(inputargs, policy=pol)
+    t.specialize()
+    return t.ccompile() 
+
+#----------------------------------------------------------------------
 def eval_seven():
     op = model.Operation(l3interp.LLFrame.op_int_add, 0, [-1, -2])
     returnlink = model.ReturnLink(None, [])
@@ -23,11 +32,21 @@ def test_very_simple():
     assert result == 7
 
 def test_very_simple_translated():
+    fn = translate(eval_seven, []) 
+    assert fn() == 7
+
+#----------------------------------------------------------------------
+
+def Xtest_simple():
+    result = eval_seven()
+    assert result == 7
+
+def Xtest_very_simple_translated():
     t = Translator(eval_seven)
     pol = policy.AnnotatorPolicy()
     pol.allow_someobjects = False
     t.annotate([], policy=pol)
     t.specialize()
+    t.view()
     fn = t.ccompile()
-    assert fn() == 7
 
