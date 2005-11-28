7c18b74d97ab647a762b1d5173a63acaaf33faba
Carl Friedrich Bolz <cfbolz@gmx.de>
2005-11-28 10:44:27 +0000

    slight fixes to statistic generation

--- pypy/tool/statistic_over_time.py
+++ pypy/tool/statistic_over_time.py
@@ -12,7 +12,7 @@ URL = "http://codespeak.net/svn/pypy/dist"
 
 tempdir = py.path.svnwc(py.test.ensuretemp("pypy-dist"))
 print "checking out"
-tempdir.checkout(URL, 8500)
+tempdir.checkout(URL)
 print "done"
 pypy = tempdir.join('pypy')
 
@@ -21,7 +21,7 @@ statistic = []
 curr_rev = tempdir.info().rev
 
 try:
-    while curr_rev > 1:
+    while curr_rev > 7024: #afterwards the behaviour becomes strange :-(
         num_revs = 0
         num_files = 0
         num_testfiles = 0
@@ -37,17 +37,17 @@ try:
                 tempdir.update(rev=curr_rev - 1)
             except KeyboardInterrupt:
                 raise
-            except:
+            except Exception, e:
+                print e
                 tempdir.localpath.remove(1)
                 tempdir.localpath.mkdir()
                 while 1:
-		    if curr_rev < 8359:
-                        URL = "http://codespeak.net/svn/pypy/trunk"
                     try:
-                        tempdir.checkout(URL, rev=curr_rev - 1)
+                        tempdir._svn("co -r %r" % (curr_rev - 1), URL)
                     except KeyboardInterrupt:
                         raise
                     except Exception, e:
+                        print e, curr_rev
                         curr_rev -= 1
                     else:
                         break
@@ -55,8 +55,12 @@ try:
             curr_rev = info.rev
             date = datetime.date(*time.gmtime(info.mtime)[:3])
         counter, num_files, num_lines, num_testfiles, num_testlines = get_loccount([pypy.localpath])
+        print
         print date, num_revs, num_files, num_testfiles, num_lines, num_testlines
         statistic.append([date, num_revs, num_files, num_testfiles, num_lines, num_testlines])
+        f = file("intermediate.txt", "a")
+        print >> f, date, num_revs, num_files, num_testfiles, num_lines, num_testlines
+        f.close()
 finally:
     import pickle
     f = file("out.txt", "w")
