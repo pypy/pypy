bbd5ac483d981ad97ddc4f7dfcbfa8f425d7c9f0
Seo Sanghyeon <sanxiyn@codespeak.net>
2005-12-02 15:31:50 +0000

    Insert some blank lines

--- pypy/translator/gencl.py
+++ pypy/translator/gencl.py
@@ -16,6 +16,7 @@ if not isinstance(bool, type):
 
 
 class Op:
+
     def __init__(self, gen, op):
         self.gen = gen
         self.str = gen.str
@@ -23,6 +24,7 @@ class Op:
         self.opname = op.opname
         self.args = op.args
         self.result = op.result
+
     def __call__(self):
         if DEBUG:
             self.op_default()
@@ -33,9 +35,11 @@ class Op:
             default = self.op_default
             meth = getattr(self, "op_" + self.opname, default)
             meth()
+
     def op_default(self):
         print ";;", self.op
         print ";; Op", self.opname, "is missing"
+
     binary_ops = {
         #"add": "+",
         "sub": "-",
@@ -49,15 +53,18 @@ class Op:
         "and_": "logand",
         "getitem": "elt",
     }
+
     def op_binary(self, op):
         s = self.str
         result, (arg1, arg2) = self.result, self.args
         cl_op = self.binary_ops[op]
         print "(setq", s(result), "(", cl_op, s(arg1), s(arg2), "))"
+
     def op_contains(self):
         s = self.str
         result, (arg1, arg2) = self.result, self.args
         print "(setq", s(result), "(not (not (find", s(arg2), s(arg1), "))))"
+
     def op_add(self):
         s = self.str
         result, (arg1, arg2) = self.result, self.args
@@ -72,6 +79,7 @@ class Op:
         }
         self.gen.emit_typecase(table, arg1, arg2)
         print ")"
+
     def op_not_(self):
         s = self.str
         result, (arg1,) = self.result, self.args
@@ -84,6 +92,7 @@ class Op:
         }
         self.gen.emit_typecase(table, arg1)
         print "))"
+
     def op_is_true(self):
         s = self.str
         result, (arg1,) = self.result, self.args
@@ -96,41 +105,49 @@ class Op:
         }
         self.gen.emit_typecase(table, arg1)
         print ")"
+
     def op_newtuple(self):
         s = self.str
         print "(setq", s(self.result), "(list",
         for arg in self.args:
             print s(arg),
         print "))"
+
     def op_newlist(self):
         s = self.str
         print "(setq", s(self.result), "(vector",
         for arg in self.args:
             print s(arg),
         print "))"
+
     def op_alloc_and_set(self):
         s = self.str
         result, (size, init) = self.result, self.args
         print "(setq", s(result), "(make-array", s(size), "))"
         print "(fill", s(result), s(init), ")"
+
     def op_setitem(self):
         s = self.str
         (seq, index, element) = self.args
         print "(setf (elt", s(seq), s(index), ")", s(element), ")"
+
     def op_iter(self):
         s = self.str
         result, (seq,) = self.result, self.args
         print "(setq", s(result), "(make-iterator", s(seq), "))"
+
     def op_next(self):
         s = self.str
         result, (iterator,) = self.result, self.args
         print "(let ((result (funcall", s(iterator), ")))"
         print "  (setq", s(result), "(car result))"
         print "  (setq last-exc (cdr result)))"
+
     builtin_map = {
         pow: "expt",
         range: "python-range",
     }
+
     def op_simple_call(self):
         func = self.args[0]
         if not isinstance(func, Constant):
@@ -146,10 +163,12 @@ class Op:
         for arg in args:
             print s(arg),
         print "))"
+
     def op_getslice(self):
         s = self.str
         result, (seq, start, end) = self.result, self.args
         print "(setq", s(result), "(python-slice", s(seq), s(start), s(end), "))"
+
     def op_pow(self):
         s = self.str
         result, (x,y,z) = self.result, self.args
@@ -160,7 +179,9 @@ class Op:
         self.gen.emit_typecase(table, x, y, z)
         print ")"
 
+
 class GenCL:
+
     def __init__(self, fun, input_arg_types=[]):
         # NB. 'fun' is a graph!
         simplify_graph(fun)
@@ -169,15 +190,19 @@ class GenCL:
         self.annotate(input_arg_types)
         transform_graph(self.ann, extra_passes=default_extra_passes
                         +[transform_slice])
+
     def annotate(self, input_arg_types):
         ann = RPythonAnnotator()
         inputcells = [ann.typeannotation(t) for t in input_arg_types]
         ann.build_graph_types(self.fun, inputcells)
         self.setannotator(ann)
+
     def setannotator(self, annotator):
         self.ann = annotator
+
     def get_type(self, var):
         return self.ann.gettype(var)
+
     def str(self, obj):
         if isinstance(obj, Variable):
             return obj.name
@@ -185,6 +210,7 @@ class GenCL:
             return self.conv(obj.value)
         else:
             return "#<%r>" % (obj,)
+
     def conv(self, val):
         if isinstance(val, tuple):
             val = map(self.conv, val)
@@ -211,6 +237,7 @@ class GenCL:
             return "'last-exc-value"
         else:
             return "#<%r>" % (val,)
+
     def emitcode(self, public=True):
         import sys
         from cStringIO import StringIO
@@ -220,8 +247,10 @@ class GenCL:
         self.emit()
         sys.stdout = oldstdout
         return out.getvalue()
+
     def emit(self):
         self.emit_defun(self.fun)
+
     def emit_defun(self, fun):
         print ";;;; Main"
         print "(defun", fun.name
@@ -259,6 +288,7 @@ class GenCL:
             self.emit_block(block)
         print ")"
         print ")"
+
     def emit_block(self, block):
         self.cur_block = block
         tag = self.blockref[block]
@@ -301,9 +331,11 @@ class GenCL:
         else:
             retval = self.str(block.inputargs[0])
             print "(return", retval, ")"
+
     def emit_jump(self, block):
         tag = self.blockref[block]
         print "(go", "tag" + str(tag), ")"
+
     def emit_link(self, link):
         source = link.args
         target = link.target.inputargs
@@ -313,6 +345,7 @@ class GenCL:
                 print self.str(t), self.str(s),
         print ")"
         self.emit_jump(link.target)
+
     typemap = {
         bool: "boolean",
         int: "fixnum",
@@ -320,6 +353,7 @@ class GenCL:
         type(''): "string", # hack, 'str' is in the namespace!
         list: "vector",
     }
+
     def emit_typecase(self, table, *args):
         argreprs = tuple(map(self.str, args))
         argtypes = tuple(map(self.get_type, args))
@@ -341,6 +375,7 @@ class GenCL:
                 print trans % argreprs,
                 print ")"
             print ")"
+
     def globaldeclarations(self):
         return prelude
 
