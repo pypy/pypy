90faba83608d9345aac540cf6432c0ebf8ecdbba
Eric van Riet Paap <ericvrp@gmail.com>
2005-12-15 14:26:50 +0000

    (ericvrp, cfbolz): adding a switch so that the if merging can be done for all graphs

--- pypy/translator/backendopt/all.py
+++ pypy/translator/backendopt/all.py
@@ -3,12 +3,14 @@ from pypy.translator.backendopt.inline import auto_inlining
 from pypy.translator.backendopt.malloc import remove_simple_mallocs
 from pypy.translator.backendopt.ssa import SSI_to_SSA
 from pypy.translator.backendopt.propagate import propagate_all
+from pypy.translator.backendopt.merge_if_blocks import merge_if_blocks
 from pypy.translator import simplify
 
 
 def backend_optimizations(translator, inline_threshold=1,
                                       mallocs=True,
                                       ssa_form=True,
+                                      merge_if_blocks_to_switch=False,
                                       propagate=False):
     # remove obvious no-ops
     for graph in translator.graphs:
@@ -34,6 +36,10 @@ def backend_optimizations(translator, inline_threshold=1,
                 simplify.transform_dead_op_vars(graph, translator)
     if propagate:
         propagate_all(translator)
+
+    if merge_if_blocks_to_switch:
+        for graph in translator.graphs:
+            merge_if_blocks(graph)
    
     if ssa_form:
         for graph in translator.graphs:
