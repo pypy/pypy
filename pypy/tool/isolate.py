e783a6d7832baeb1e203fc73e84dd6e217c4856e
Samuele Pedroni <pedronis@lucediurna.net>
2005-12-29 23:51:25 +0000

    simple interface to load a module in a separate process and invoke functions in it with simple args and return values. For now using py.execnet, but it should be easy to reimplement or have multiple implementation  with fork/pipes for more speed or popen directly. A isolate instance can be used as a module mod to be invoke functions, mod.f(...). the only difference is that it is better to call the close_isolate cleanup function on it when done.
    To be used for things like test_boehm...

---
+++ pypy/tool/isolate.py
@@ -0,0 +1,88 @@
+import py
+import exceptions
+
+ISOLATE = """
+import sys
+import imp
+
+mod = channel.receive()
+if isinstance(mod, str):
+    mod = __import__(mod, {}, {}, ['__doc__'])
+else:
+    dir, name = mod
+    file, pathname, description = imp.find_module(name, [str(dir)])
+    try:
+        mod = imp.load_module(name, file, pathname, description)
+    finally:
+        if file:
+            file.close()    
+channel.send("loaded")
+while True:
+    func, args = channel.receive()
+    try:
+        res = getattr(mod, func)(*args)
+    except KeyboardInterrupt:
+        raise
+    except:
+        exc_type = sys.exc_info()[0] 
+        channel.send(('exc', (exc_type.__module__, exc_type.__name__)))
+    else:
+        channel.send(('ok', res))
+"""
+
+class IsolateException(Exception):
+    pass
+
+class IsolateInvoker(object):
+    # to have a nice repr
+    
+    def __init__(self, isolate, name):
+        self.isolate = isolate
+        self.name = name
+
+    def __call__(self, *args):
+        return self.isolate._invoke(self.name, args)
+        
+    def __repr__(self):
+        return "<invoker for %r . %r>" % (self.isolate.module, self.name)
+
+class Isolate(object):
+    """
+    Isolate lets load a module in a different process,
+    and support invoking functions from it passing and
+    returning simple values
+
+    module: a dotted module name or a tuple (directory, module-name)
+    """
+
+    def __init__(self, module):
+        self.gw = py.execnet.PopenGateway()
+        chan = self.chan = self.gw.remote_exec(ISOLATE)
+        chan.send(module)
+        assert chan.receive() == "loaded"
+
+    def __getattr__(self, name):
+        return IsolateInvoker(self, name)
+
+    def _invoke(self, func, args):
+        self.chan.send((func, args))
+        status, value =  self.chan.receive()
+        if status == 'ok':
+            return value
+        else:
+            exc_type_module, exc_type_name = value
+            if exc_type_module == 'exceptions':
+                raise getattr(exceptions, exc_type_name)
+            else:
+                raise IsolateException, "%s.%s" % value 
+
+    def _close(self):
+        self.chan.close()
+        self.gw.exit()
+
+    def __del__(self):
+        self.close()
+
+def close_isolate(isolate):
+    assert isinstance(isolate, Isolate)
+    isolate._close()
---
+++ pypy/tool/test/simple.py
@@ -0,0 +1,12 @@
+
+def f(a,b):
+    return a+b
+
+def g():
+    raise ValueError, "booh"
+
+class FancyException(Exception):
+    pass
+
+def h():
+    raise FancyException, "booh"
