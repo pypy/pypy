d59e669e398fb519d1b00cad3a0d5aba54bf0071
Carl Friedrich Bolz <cfbolz@gmx.de>
2006-01-24 11:08:50 +0000

    (arigo, cfbolz):
    another test for the new graph-generating functions that unfortunately didn't
    catch any bugs.

--- pypy/rpython/test/test_rgenop.py
+++ pypy/rpython/test/test_rgenop.py
@@ -3,6 +3,7 @@ from pypy.rpython.lltypesystem.lltype import *
 
 
 def test_square():
+    """def square(v0): return v0*v0"""
     startlink = newgraph("square")
     block = newblock()
     v0 = geninputarg(block, Signed)
@@ -13,3 +14,26 @@ def test_square():
 
     res = runlink(startlink, [17])
     assert res == 289
+
+def test_if():
+    """
+    def f(v0):
+        if v0 < 0:
+            return 0
+        else:
+            return v0
+    """
+    startlink = newgraph("if")
+    block = newblock()
+    v0 = geninputarg(block, Signed)
+    const0 = genconst(block, 0)
+    v1 = genop(block, 'int_lt', [v0, const0], Bool)
+    true_link = newreturnlink(block, const0)
+    false_link = newreturnlink(block, v0)
+    closeblock2(block, v1, false_link, true_link)
+    closelink(startlink, block)
+
+    res = runlink(startlink, [-1])
+    assert res == 0
+    res = runlink(startlink, [42])
+    assert res == 42
