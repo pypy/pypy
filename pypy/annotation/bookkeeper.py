ec65ede341eb21f555ccdae17d639f8dabebcda8
Gerald Klix <gromit@codespeak.net>
2006-01-24 10:02:26 +0000

    ADD: (gromit,stephan) Added a case to Bookkeeper.immutablevalue that annotates rcypte's type descriptor classes.

--- pypy/annotation/bookkeeper.py
+++ pypy/annotation/bookkeeper.py
@@ -354,6 +354,8 @@ class Bookkeeper:
         elif ishashable(x) and x in BUILTIN_ANALYZERS:
 	    _module = getattr(x,"__module__","unknown")
             result = SomeBuiltin(BUILTIN_ANALYZERS[x], methodname="%s.%s" % (_module, x.__name__))
+        elif hasattr(x, "compute_result_annotation"):
+            result = SomeBuiltin(x.compute_result_annotation, "?" )
         elif tp in EXTERNAL_TYPE_ANALYZERS:
             result = SomeExternalObject(tp)
         elif isinstance(x, lltype._ptr):
