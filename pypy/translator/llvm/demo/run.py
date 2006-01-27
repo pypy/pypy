d1071596490fe76066eef23882afa19d6ee9e8f8
Richard Emslie <richardemslie@gmail.com>
2006-01-27 12:52:24 +0000

    Added back 'c' option for comparison fun!

--- pypy/translator/llvm/demo/run.py
+++ pypy/translator/llvm/demo/run.py
@@ -5,12 +5,28 @@ import sys
 from pypy.annotation.model import SomeList, SomeString
 from pypy.annotation.listdef import ListDef
 
+from pypy.translator.translator import TranslationContext
+from pypy.translator.c.genc import CStandaloneBuilder
+    
 from pypy.translator.llvm.genllvm import genllvm_compile
 
 def p():
     print 'Running on top of CPython:'
     entry_point([])
 
+def c(name):
+    s_list_of_strings = SomeList(ListDef(None, SomeString()))
+    s_list_of_strings.listdef.resize()
+    t = TranslationContext()
+    t.buildannotator().build_types(entry_point, [s_list_of_strings])
+    t.buildrtyper().specialize()
+    from pypy.translator.backendopt.all import backend_optimizations
+    backend_optimizations(t)
+    cbuilder = CStandaloneBuilder(t, entry_point)
+    cbuilder.generate_source()
+    cbuilder.compile()
+    os.system("XXX")
+    
 def l(name):
     s_list_of_strings = SomeList(ListDef(None, SomeString()))
     s_list_of_strings.listdef.resize()
@@ -32,7 +48,11 @@ def run(ep, name="go"):
         elif sys.argv[1] == "l":
             l(name)
             run_all = False
+        elif sys.argv[1] == "c":
+            c(name)
+            run_all = False
             
     if run_all:
         l(name)
+        c(name)
         p()
