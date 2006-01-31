50b8fd3666df00823a923a91776f1cf8144d3ffe
Aurelien Campeas <auc@codespeak.net>
2006-01-31 14:49:53 +0000

    last round of fix removing globals (should be)

--- pypy/lib/logic/computation_space/test_variable.py
+++ pypy/lib/logic/computation_space/test_variable.py
@@ -2,9 +2,9 @@ from threading import Thread
 
 from py.test import raises
 
-import computationspace as u
+import computationspace as space
 import variable as v
-from problems import *
+from problems import dummy_problem
 
 class Consumer(Thread):
 
@@ -24,46 +24,48 @@ class NConsumer(Thread):
 
 class TestVariable:
 
-    def setup_method(self, meth):
-        u._cs = u.ComputationSpace(dummy_problem)
-
     def test_no_same_name(self):
-        x = u.var('x')
-        raises(u.AlreadyInStore, u.var, 'x')
+        sp = space.ComputationSpace(dummy_problem)
+        x = sp.var('x')
+        raises(space.AlreadyInStore, sp.var, 'x')
 
     def test_get_by_name(self):
-        x = u.var('x')
-        assert x == u._cs.get_var_by_name('x')
-        raises(u.NotInStore, u._cs.get_var_by_name, 'y')
+        sp = space.ComputationSpace(dummy_problem)
+        x = sp.var('x')
+        assert x == sp.get_var_by_name('x')
+        raises(space.NotInStore, sp.get_var_by_name, 'y')
 
     def test_one_thread_reading_one_var(self):
+        sp = space.ComputationSpace(dummy_problem)
         cons = Consumer()
-        x = u.var('x')
+        x = sp.var('x')
         cons.give_var(x)
         cons.start()
-        u.bind(x, 42)
+        sp.bind(x, 42)
         cons.join()
         assert cons.var.val == 42
 
     def test_many_threads_reading_one_var(self):
+        sp = space.ComputationSpace(dummy_problem)
         conss = [Consumer() for i in range(10)]
-        x = u.var('x')
+        x = sp.var('x')
         for cons in conss:
             cons.give_var(x)
             cons.start()
-        u.bind(x, 42)
+        sp.bind(x, 42)
         for cons in conss:
             cons.join()
         assert cons.var.val == 42
 
     def test_many_thread_reading_many_var(self):
+        sp = space.ComputationSpace(dummy_problem)
         conss = [NConsumer() for i in range(10)]
-        vars_ = [u.var(str(i)) for i in range(10)]
+        vars_ = [sp.var(str(i)) for i in range(10)]
         for cons in conss:
             cons.give_vars(vars_)
             cons.start()
         for var in vars_:
-            u.bind(var, var.name)
+            sp.bind(var, var.name)
         for cons in conss:
             cons.join()
         for i in range(10):
