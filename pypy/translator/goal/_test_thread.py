1c14b436131d34d887384aba4535bea377cf2f98
Christian Tismer <tismer@stackless.com>
2006-02-12 12:47:24 +0000

    better monitoring output. Raises another issue: our output buffering is wrong.

--- pypy/translator/goal/_test_thread.py
+++ pypy/translator/goal/_test_thread.py
@@ -1,14 +1,22 @@
 import thread, time
 
-running = []
+class MonitorList(list):
+    def append(self, obj):
+        list.append(self, obj)
+        print "running grown to %r\n" % self,
+    def remove(self, obj):
+        list.remove(self, obj)
+        print "running shrunk to %r\n" % self,
+
+running = MonitorList()
 
 def f(name, count, modulus):
     running.append(name)
     i = 0
-    print "starting", name, count, modulus
+    print "starting %s %d %d\n" % (name, count, modulus),
     for i in xrange(count):
         if i % modulus == 0:
-            print name, i
+            print "%s %d\n" % (name, i),
     running.remove(name)
 
 thread.start_new_thread(f, ("eins", 10000000, 12345))
@@ -16,8 +24,10 @@ thread.start_new_thread(f, ("zwei", 10000000, 13579))
 thread.start_new_thread(f, ("drei", 10000000, 14680))
 thread.start_new_thread(f, ("vier", 10000000, 15725))
 
-print "waiting for", running, "to finish"
+while not running:
+    pass
+print "waiting for %r to finish\n" % running,
 while running:
     pass
-print "finished waiting."
+print "finished waiting.\n",
 
