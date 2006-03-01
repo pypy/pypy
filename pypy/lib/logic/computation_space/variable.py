ebbe3ffad6097b4dee785522ffb359e68cc18111
Aurelien Campeas <auc@codespeak.net>
2006-03-01 17:17:54 +0000

    simplevar, to be used internally by spaces

--- pypy/lib/logic/computation_space/variable.py
+++ pypy/lib/logic/computation_space/variable.py
@@ -9,6 +9,10 @@ class AlreadyInStore(VariableException):
     def __str__(self):
         return "%s already in store" % self.name
 
+class AlreadyBound(VariableException):
+    def __str__(self):
+        return "%s is already bound" % self.name
+
 class NotAVariable(VariableException):
     def __str__(self):
         return "%s is not a variable" % self.name
@@ -20,8 +24,51 @@ class NoValue: pass
 
 class NoDom: pass
 
-class Var(object):
-    """Single-assignment variable"""
+class SimpleVar(object):
+    def __init__(self, name):
+        self.name = name
+        self._val = NoValue
+        # a condition variable for concurrent access
+        self._value_condition = threading.Condition()
+
+    # value accessors
+    def _set_val(self, val):
+        if val != NoValue:
+            raise AlreadyBound(self.name)
+        
+    def _get_val(self):
+        return self._val
+    val = property(_get_val, _set_val)
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+    def __gt__(self, other):
+        return self.name.__gt__(other.name)
+
+    # public interface
+
+    def is_bound(self):
+        return self.val != NoValue
+
+    def bind(self, val):
+        self.val = val
+
+    def get(self):
+        """Make threads wait on the variable
+           being bound in the top-level space
+        """
+        try:
+            self._value_condition.acquire()
+            while not self._is_bound():
+                self._value_condition.wait()
+            return self.val
+        finally:
+            self._value_condition.release()
+
+
+class Var(SimpleVar):
+    """Dataflow variable linked to a space"""
 
     def __init__(self, name, cs):
         if name in cs.names:
@@ -80,33 +127,8 @@ class Var(object):
         return isinstance(thing, Var) \
                and self.name == thing.name
 
-    def __hash__(self):
-        return self.name.__hash__()
-
-    def __gt__(self, other):
-        return self.name.__gt__(other.name)
-
     def bind(self, val):
         """top-level space bind"""
         self._cs.bind(self, val)
 
     is_bound = _is_bound
-
-
-    #-- Dataflow ops with concurrent semantics ------
-    # should be used by threads that want to block on
-    # unbound variables
-
-    def get(self):
-        """Make threads wait on the variable
-           being bound in the top-level space
-        """
-        try:
-            self._value_condition.acquire()
-            while not self._is_bound():
-                self._value_condition.wait()
-            return self.val
-        finally:
-            self._value_condition.release()
-
-    
