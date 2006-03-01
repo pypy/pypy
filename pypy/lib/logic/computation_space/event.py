90037787bc4b8674830529b9e90902008fc114d3
Aurelien Campeas <auc@codespeak.net>
2006-03-01 14:38:07 +0000

    new event types to trigger propagation

---
+++ pypy/lib/logic/computation_space/event.py
@@ -0,0 +1,14 @@
+class NewSpace: pass
+
+class Clone: pass
+
+class Revise(object):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __eq__(self, other):
+        if not isinstance(other, Distributed): return False
+        return self.var == other.var
+
+    
