7048cbbd422ada9b7db69824aaab6006071a3d0e
Eric van Riet Paap <ericvrp@gmail.com>
2006-03-09 10:00:23 +0000

    skip llvm test for execution context because it does not work in the machines I have tried it on.

--- pypy/translator/llvm/pyllvm/test/test_ee.py
+++ pypy/translator/llvm/pyllvm/test/test_ee.py
@@ -1,7 +1,8 @@
+import py
 from pypy.translator.llvm.buildllvm import llvm_is_on_path
+py.test.skip("'python setup.py build_ext -i' is not quiet working yet")
 if not llvm_is_on_path():
     py.test.skip("llvm not found")
-import py
 from pypy.translator.llvm.pyllvm import pyllvm 
 
 def test_execution_engine():
