59cb3aa38218bebd2ad6a582bff2ecb626ff8a45
Samuele Pedroni <pedronis@lucediurna.net>
2006-03-23 14:32:12 +0000

    another micronbench I had around.

--- pypy/translator/microbench/test_bltn.py
+++ pypy/translator/microbench/test_bltn.py
@@ -10,3 +10,13 @@ def test_call_sin():
     while c < n:
         x = sin(x)
         c += 1
+
+def test_call_fabs():
+    from math import fabs
+    
+    x = -1.0
+    c = 0
+    n = N
+    while c < n:
+        x = fabs(x)
+        c += 1
