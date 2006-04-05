dd361e6e13517563e992eab0b7dd9f70f6e90a56
Michael Hudson <micahel@gmail.com>
2006-04-05 18:17:07 +0000

    very first glimmerings of a stackless transformer. doesn't actually do anything at all much, just computes the lltype.Struct suitable for storing away a bunch of variables.

---
+++ pypy/translator/c/test/test_stacklesstransform.py
@@ -0,0 +1,22 @@
+from pypy.translator.c.stacklesstransform import StacklessTransfomer
+from pypy.rpython.memory.gctransform import varoftype
+from pypy.rpython.lltypesystem import lltype, llmemory
+
+def test_frame_types():
+    st = StacklessTransfomer(None)
+
+    signed = varoftype(lltype.Signed)
+    ptr = varoftype(lltype.Ptr(lltype.GcStruct("S")))
+    addr = varoftype(llmemory.Address)
+    float = varoftype(lltype.Float)
+    longlong = varoftype(lltype.SignedLongLong)
+
+    ft4vars = st.frame_type_for_vars
+    
+    s1 = ft4vars([signed])
+    assert len(s1._flds) == 1
+
+    s2_1 = ft4vars([signed, ptr])
+    s2_2 = ft4vars([ptr, signed])
+
+    assert s2_1 is s2_2
