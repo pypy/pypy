509beed975eeaf2e362c07eb145382f2fdb101ac
Seo Sanghyeon <sanxiyn@codespeak.net>
2006-04-26 15:49:02 +0000

    Add a test for SomeChar representation

--- pypy/translator/cl/test/test_clrepr.py
+++ pypy/translator/cl/test/test_clrepr.py
@@ -5,6 +5,7 @@ def test_const():
     assert repr_const(False) == 'nil'
     assert repr_const(42) == '42'
     assert repr_const(None) == 'nil'
+    assert repr_const('a') == '#\\a'
     assert repr_const('answer') == '"answer"'
     assert repr_const((2, 3)) == "'(2 3)"
     assert repr_const([2, 3]) == "#(2 3)"
