3c5464ef52ad879aee6d00e879bbacd1da8b3dc7
Michael Hudson <micahel@gmail.com>
2006-04-26 13:24:19 +0000

    oops, that wasn't meant to get committed!

--- pypy/translator/translator.py
+++ pypy/translator/translator.py
@@ -30,7 +30,6 @@ class TranslationContext(object):
             raise TypeError("unexpected keyword argument")
         self.annotator = None
         self.rtyper = None
-        print 'NULLING', self
         self.stacklesstransformer = None
         self.graphs = []      # [graph]
         self.callgraph = {}   # {opaque_tag: (caller-graph, callee-graph)}
