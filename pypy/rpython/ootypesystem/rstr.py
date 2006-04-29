d1b25221fbe2037386a706d399372f1828fbe61f
Niklaus Haldimann <nik@codespeak.net>
2006-04-29 09:45:18 +0000

    some comments about the state of ootype strings. unfortunately i didn't really progress as much as i would have liked.

--- pypy/rpython/ootypesystem/rstr.py
+++ pypy/rpython/ootypesystem/rstr.py
@@ -3,6 +3,22 @@ from pypy.rpython.lltypesystem.lltype import Ptr
 from pypy.rpython.ootypesystem.ootype import Signed, Record, String, make_string
 
 class StringRepr(AbstractStringRepr):
+    """
+    Some comments about the state of ootype strings at the end of Tokyo sprint
+
+    What was accomplished:
+    - The rstr module was split in an lltype and ootype version.
+    - There is the beginnings of a String type in ootype.
+    - The runtime representation of Strings is a subclass of the builtin str.
+      The idea is that this saves us from boilerplate code implementing the
+      builtin str methods.
+
+    Nothing more was done because of lack of time and paralysis in the face
+    of too many problems. Among other things, to write any meaningful tests
+    we first need conversion from Chars to Strings (because
+    test_llinterp.interpret won't accept strings as arguments). We will need a
+    new low-level operation (convert_char_to_oostring or some such) for this.
+    """
 
     lowleveltype = String
 
