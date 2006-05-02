955b41c3833cf35237634d8637bb2557e049c75d
Seo Sanghyeon <sanxiyn@codespeak.net>
2006-05-02 05:37:47 +0000

    Rename a test

--- pypy/translator/cl/test/test_call.py
+++ pypy/translator/cl/test/test_call.py
@@ -3,11 +3,11 @@ from pypy.translator.cl.buildcl import make_cl_func
 
 def test_call():
     def add_one(n):
-        n = add_one_really(n)
-        return n
-    def add_one_really(n):
         return n + 1
-    cl_add_one = make_cl_func(add_one, [int])
+    def call_add_one(n):
+        n = add_one(n)
+        return n
+    cl_add_one = make_cl_func(call_add_one, [int])
     assert cl_add_one(1) == 2
 
 def test_indirect_call():
