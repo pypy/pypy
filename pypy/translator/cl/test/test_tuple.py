5db14ad054f2f2d2042817337f2feea338e942b1
Seo Sanghyeon <sanxiyn@codespeak.net>
2006-05-02 06:30:45 +0000

    Tuple tests. Passed without any additional work.

---
+++ pypy/translator/cl/test/test_tuple.py
@@ -0,0 +1,17 @@
+from pypy.translator.cl.buildcl import make_cl_func
+
+def test_tuple_get():
+    def tuple_get(number):
+        tuple = (number,)
+        return tuple[0]
+    cl_tuple_get = make_cl_func(tuple_get, [int])
+    assert cl_tuple_get(1) == 1
+
+def test_tuple_iter():
+    def tuple_double(number):
+        tuple = (number,)
+        for item in tuple:
+            number += item
+        return number
+    cl_double = make_cl_func(tuple_double, [int])
+    assert cl_double(1) == 2
