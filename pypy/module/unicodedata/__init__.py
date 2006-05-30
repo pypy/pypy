d72f03967897940f451bbd32ecc82ebeb243e3bb
Anders Chrigstrom <anders@chrigstrom.se>
2006-05-30 12:30:29 +0000

    Some renaming.

--- pypy/module/unicodedata/__init__.py
+++ pypy/module/unicodedata/__init__.py
@@ -4,14 +4,14 @@ class Module(MixedModule):
     appleveldefs = {
     }
     interpleveldefs = {
-        'unidata_version' : 'space.wrap(function.ucd.version)',
-        'ucd_3_2_0'       : 'space.wrap(function.ucd_3_2_0)',
-        'ucd_4_1_0'       : 'space.wrap(function.ucd_4_1_0)',
-        'ucd'             : 'space.wrap(function.ucd)',
+        'unidata_version' : 'space.wrap(interp_ucd.ucd.version)',
+        'ucd_3_2_0'       : 'space.wrap(interp_ucd.ucd_3_2_0)',
+        'ucd_4_1_0'       : 'space.wrap(interp_ucd.ucd_4_1_0)',
+        'ucd'             : 'space.wrap(interp_ucd.ucd)',
         '__doc__'         : "space.wrap('unicode character database')",
     }
     for name in '''lookup name decimal digit numeric category bidirectional
                    east_asian_width combining mirrored decomposition
                    normalize'''.split():
-        interpleveldefs[name] = '''space.getattr(space.wrap(function.ucd),
+        interpleveldefs[name] = '''space.getattr(space.wrap(interp_ucd.ucd),
                                    space.wrap("%s"))''' % name
