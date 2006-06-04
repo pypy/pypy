e87ad876b52489ec5cd977030e61616aa58ce3e5
Holger Krekel <holger.krekel@gmail.com>
2006-06-04 13:01:13 +0000

    have demo use tool's libc

--- pypy/module/_demo/demo.py
+++ pypy/module/_demo/demo.py
@@ -1,35 +1,15 @@
 from pypy.interpreter.error import OperationError
 from pypy.interpreter.baseobjspace import ObjSpace, W_Root
 from pypy.rpython.rctypes.tool import ctypes_platform
+from pypy.rpython.rctypes.tool.libc import libc
 import sys
 from ctypes import *
 
-# __________ the standard C library __________
-
-# LoadLibrary is deprecated in ctypes, this should be removed at some point
-if "load" in dir(cdll):
-    cdll_load = cdll.load
-else:
-    cdll_load = cdll.LoadLibrary
-
-if sys.platform == 'win32':
-    mylib = cdll_load('msvcrt.dll')
-elif sys.platform == 'linux2':
-    mylib = cdll_load('libc.so.6')
-elif sys.platform == 'darwin':
-    mylib = cdll_load('libc.dylib')
-else:
-    py.test.skip("don't know how to load the c lib for %s" % 
-            sys.platform)
-# ____________________________________________
-
-
 time_t = ctypes_platform.getsimpletype('time_t', '#include <time.h>', c_long)
 
-time = mylib.time
+time = libc.time
 time.argtypes = [POINTER(time_t)]
 time.restype = time_t
-
 
 def get(space, name):
     w_module = space.getbuiltinmodule('_demo')
