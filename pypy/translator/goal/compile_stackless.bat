746c11104a03dfcf74494bd0ba41a82fa0a34bda
Christian Tismer <tismer@stackless.com>
2006-06-07 18:19:06 +0000

    change batch file to use new-stackless

--- pypy/translator/goal/compile_stackless.bat
+++ pypy/translator/goal/compile_stackless.bat
@@ -1,3 +1,3 @@
 python -c "import time; print time.ctime(), 'compile start'" >> compile.log
-translate.py --batch --stackless
+translate.py --batch --new-stackless
 python -c "import time; print time.ctime(), 'compile stop'" >> compile.log
