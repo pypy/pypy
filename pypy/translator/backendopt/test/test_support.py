dd674f42d24f7528707f2585e57d91984e207e1e
Michael Hudson <micahel@gmail.com>
2006-06-09 11:31:15 +0000

    (mwh, pedronis) add another test and some comments about what we're testing.

--- pypy/translator/backendopt/test/test_support.py
+++ pypy/translator/backendopt/test/test_support.py
@@ -14,9 +14,13 @@ def varoftype(concretetype):
     return var
 
 def test_nclc_should_be_true():
+    # this is testing a block like:
+    # +--- inputargs: pointer_to_gc
+    # | v0 <- op_getsubstruct pointer_to_gc 'b'
+    # +--- exitargs: v0 (i.e. pointer to non-gc)
     llops = LowLevelOpList()
     ptr_a = varoftype(lltype.Ptr(GcA))
-    v_res = llops.genop("getfield", [ptr_a, model.Constant('b', lltype.Void)],
+    v_res = llops.genop("getsubstruct", [ptr_a, model.Constant('b', lltype.Void)],
                         resulttype=lltype.Ptr(NonGcB))
     block = model.Block([ptr_a])
     block.operations.extend(llops)
@@ -24,9 +28,12 @@ def test_nclc_should_be_true():
     assert needs_conservative_livevar_calculation(block)
 
 def test_nclc_nongc_not_passed_on():
+    # +--- inputargs: pointer_to_gc
+    # | v0 <- op_getsubstruct pointer_to_gc 'b'
+    # +--- exitargs: pointer_to_gc (i.e. the pointer to non-gc doesn't leave the block)
     llops = LowLevelOpList()
     ptr_a = varoftype(lltype.Ptr(GcA))
-    v_res = llops.genop("getfield", [ptr_a, model.Constant('b', lltype.Void)],
+    v_res = llops.genop("getsubstruct", [ptr_a, model.Constant('b', lltype.Void)],
                         resulttype=lltype.Ptr(NonGcB))
     block = model.Block([ptr_a])
     block.operations.extend(llops)
@@ -34,6 +41,12 @@ def test_nclc_nongc_not_passed_on():
     assert not needs_conservative_livevar_calculation(block)
 
 def test_nclc_ignore_functype():
+    # +--- inputargs: pointer_to_gc
+    # | v0 <- op_getfield pointer_to_gc 'c'
+    # +--- exitargs: v0 (i.e. a pointer to function)
+    # pointers to functions are 'not gc' but functions are also
+    # immortal so you don't need to muck around inserting keepalives
+    # so *they* don't die!
     llops = LowLevelOpList()
     ptr_a = varoftype(lltype.Ptr(GcA))
     v_res = llops.genop("getfield", [ptr_a, model.Constant('c', lltype.Void)],
@@ -44,6 +57,9 @@ def test_nclc_ignore_functype():
     assert not needs_conservative_livevar_calculation(block)
 
 def test_sbwk_should_insert_keepalives():
+    # this is testing something like:
+    # v0 <- op_producing_non_gc
+    # v1 <- op_using_v0        <- split here
     llops = LowLevelOpList()
     ptr_a = varoftype(lltype.Ptr(GcA))
     v_res = llops.genop("getfield", [ptr_a, model.Constant('b', lltype.Void)],
@@ -55,4 +71,24 @@ def test_sbwk_should_insert_keepalives():
     block.closeblock(model.Link([], None))
     link = split_block_with_keepalive(block, 1)
     assert 'keepalive' in [op.opname for op in link.target.operations]
-    
+
+def test_sbwk_should_insert_keepalives_2():
+    # this is testing something like:
+    # v0 <- op_producing_non_gc
+    # v1 <- op_not_using_v0        <- split here
+    # v2 <- op_using_v0
+    llops = LowLevelOpList()
+    ptr_a = varoftype(lltype.Ptr(GcA))
+    v_res = llops.genop("getfield", [ptr_a, model.Constant('b', lltype.Void)],
+                        resulttype=lltype.Ptr(NonGcB))
+    llops.genop("direct_call", [model.Constant(None, lltype.Void)],
+                resulttype=lltype.Void)
+    llops.genop("direct_call", [model.Constant(None, lltype.Void), v_res],
+                resulttype=lltype.Void)
+    block = model.Block([ptr_a])
+    block.operations.extend(llops)
+    block.closeblock(model.Link([], None))
+    link = split_block_with_keepalive(block, 1)
+    assert 'keepalive' in [op.opname for op in link.target.operations]
+
+
