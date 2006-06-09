66982da8b96f1e29eda1c1f63132eb2af8f08d0f
Eric van Riet Paap <ericvrp@gmail.com>
2006-06-09 10:16:30 +0000

    Moved old (non ootypesystem) javascript backend to /svn/user/ericvrp . Before PyPy 0.9 js2 should be renamed to js and (getting_started) docs should be updated.

--- pypy/translator/js/opaquenode.py
+++
@@ -1,8 +0,0 @@
-from pypy.translator.js.node import Node
-
-
-class OpaqueNode(Node):
-    def __init__(self, db, value):
-        self.db    = db
-        self.value = value
-        self.ref   = "null"
