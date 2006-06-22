db8fa35d3c0ddcacae7ab8ea21d2ef942c819f22
Maciej Fijalkowski <fijall@gmail.com>
2006-06-22 17:22:30 +0000

    Added files that seems to be necessary.

---
+++ pypy/translator/js/demo/jsdemo/proxy.py
@@ -0,0 +1,25 @@
+
+""" xmlhttp controllers, usefull for testing
+"""
+
+import turbogears
+import cherrypy
+from pypy.translator.js.demo.jsdemo.controllers import Root
+from pypy.rpython.ootypesystem.bltregistry import BasicExternal, MethodDesc
+
+# Needed double inheritance for both server job
+# and semi-transparent communication proxy
+class ProxyRoot(Root, BasicExternal):
+    """ Class for running communication tests which are not designed to end
+    after single function call
+    """
+    _methods = {
+        'send_result' : MethodDesc((('result', "aa"), ('exc', "aa"), ('callback',(lambda : None))), None)
+    }
+    
+    @turbogears.expose(html="jsdemo.templates.xmlhttp")
+    def index(self):
+        import time
+        return dict(now=time.ctime(), onload=self.jsname, code=self.jssource)
+
+ProxyRootInstance = ProxyRoot()
---
+++ pypy/translator/js/demo/jsdemo/templates/master.kid
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?python import sitetemplate ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:py="http://purl.org/kid/ns#" py:extends="sitetemplate">
+
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type" py:replace="''"/>
+    <title>Your title goes here</title>
+</head>
+
+<body py:match="item.tag=='{http://www.w3.org/1999/xhtml}body'">
+    <div py:if="tg_flash" class="flash" py:content="tg_flash"></div>
+    
+    <div py:replace="item[:]"/>
+</body>
+</html>
