ad1328bcaac41b22db86b67f004d9e5d46e8466a
Eric van Riet Paap <ericvrp@gmail.com>
2006-06-30 07:50:55 +0000

    Refactor because this is now replaced by the code in demo/jsdemo. (moved to /user/ericvrp/js/proxy)

--- pypy/translator/js/proxy/README.txt
+++
@@ -1,4 +0,0 @@
-testme proxy for BnB
-
-This is a TurboGears (http://www.turbogears.org) project. It can be
-started by running the testme-start.py script.
--- pypy/translator/js/proxy/testme/model.py
+++
@@ -1,8 +0,0 @@
-from sqlobject import *
-from turbogears.database import PackageHub
-
-hub = PackageHub("testme")
-__connection__ = hub
-
-# class YourDataClass(SQLObject):
-#     pass
