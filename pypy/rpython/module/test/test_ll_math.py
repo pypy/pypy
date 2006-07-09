dead949a8590d718166a0efd74e0cb7a9160bcc3
Antonio Cuni <anto.cuni@gmail.com>
2006-07-09 11:18:51 +0000

    Fix import.

--- pypy/rpython/module/test/test_ll_math.py
+++ pypy/rpython/module/test/test_ll_math.py
@@ -1,5 +1,6 @@
 import math
 from pypy.rpython.module.ll_math import *
+from pypy.rpython.lltypesystem.module.ll_math import Implementation
 
 def test_ll_math_atan2():
     for x in [0, 1, math.pi, 0.123]:
@@ -7,12 +8,12 @@ def test_ll_math_atan2():
             assert math.atan2(x, y) == ll_math_atan2(x, y)
 
 def test_ll_modf():
-    result = ll_math_modf(10.1)
+    result = Implementation.ll_math_modf(10.1)
     assert result.item0 == math.modf(10.1)[0]
     assert result.item1 == math.modf(10.1)[1]
 
 def test_ll_frexp():
-    result = ll_math_frexp(10.1)
+    result = Implementation.ll_math_frexp(10.1)
     assert result.item0 == math.frexp(10.1)[0]
     assert result.item1 == math.frexp(10.1)[1]
 
