b27597052536368e716a3b6e8ce2bd138e947ade
Michael Hudson <micahel@gmail.com>
2006-08-07 08:30:35 +0000

    remove a non zero (mod 4) indent

--- pypy/interpreter/pyframe.py
+++ pypy/interpreter/pyframe.py
@@ -219,9 +219,9 @@ class PyFrame(eval.EvalFrame):
                     try:
                         try:
                             if we_are_translated():
-                                 # always raising, put the resume point just before!
-                                 rstack.resume_point("eval", self, executioncontext)
-                                 self.dispatch_translated(executioncontext)
+                                # always raising, put the resume point just before!
+                                rstack.resume_point("eval", self, executioncontext)
+                                self.dispatch_translated(executioncontext)
                             else:
                                 self.dispatch(executioncontext)
                         # catch asynchronous exceptions and turn them
