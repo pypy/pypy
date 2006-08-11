01639ed6d5c7fe8a85438874e6117d71c8d830ec
Michael Hudson <micahel@gmail.com>
2006-08-11 09:06:14 +0000

    memory reducing hacks, and some code to put data in a form that R can deal with.

--- pypy/tool/readdictinfo.py
+++ pypy/tool/readdictinfo.py
@@ -1,5 +1,3 @@
-import autopath
-
 # this is for use with a pypy-c build with multidicts and using the
 # MeasuringDictImplementation -- it will create a file called
 # 'dictinfo.txt' in the local directory and this file will turn the
@@ -7,9 +5,23 @@ import autopath
 
 # run with python -i !
 
-from pypy.objspace.std.dictmultiobject import DictInfo
-
 import sys
+
+infile = open(sys.argv[1])
+
+curr = None
+slots = []
+for line in infile:
+    if line == '------------------\n':
+        if curr:
+            break
+        curr = 1
+    else:
+        attr, val = [s.strip() for s in line.split(':')]
+        slots.append(attr)
+
+class DictInfo(object):
+    __slots__ = slots
 
 infile = open(sys.argv[1])
 
@@ -39,11 +51,57 @@ def histogram(infos, keyattr, *attrs):
             d2[v2] = d2.get(v2, 0) + 1
     return sorted(r.items())
 
-import pprint
-try:
-    import readline
-except ImportError:
-    pass
-else:
-    import rlcompleter
-    readline.parse_and_bind('tab: complete')
+def reportDictInfos():
+    d = {}
+    stillAlive = 0
+    totLifetime = 0.0
+    for info in infos:
+        for attr in slots:
+            if attr == 'maxcontents':
+                continue
+            v = getattr(info, attr)
+            if not isinstance(v, int):
+                continue
+            d[attr] = d.get(attr, 0) + v
+        if info.lifetime != -1.0:
+            totLifetime += info.lifetime
+        else:
+            stillAlive += 1
+    print 'read info on', len(infos), 'dictionaries'
+    if stillAlive != len(infos):
+        print 'average lifetime', totLifetime/(len(infos) - stillAlive),
+        print '('+str(stillAlive), 'still alive at exit)'
+    print d
+
+def Rify(fname, *attributes):
+    output = open(fname, 'w')
+    for attr in attributes:
+        print >>output, attr,
+    print >>output
+    for info in infos:
+        for attr in attributes:
+            print >>output, getattr(info, attr),
+        print >>output
+
+if __name__ == '__main__':
+    reportDictInfos()
+
+    # interactive stuff:
+
+    import __builtin__
+
+    def displayhook(v):
+        if v is not None:
+            __builtin__._ = v
+            pprint.pprint(v)
+    sys.displayhook = displayhook
+
+    import pprint
+    try:
+        import readline
+    except ImportError:
+        pass
+    else:
+        import rlcompleter
+        readline.parse_and_bind('tab: complete')
+
