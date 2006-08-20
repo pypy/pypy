61c808cea07afe476a454301ec7b973a14920b5c
Lawrence Oluyede <rhymes@codespeak.net>
2006-08-20 21:53:27 +0000

    refactor initialization of the module

--- pypy/module/posix/__init__.py
+++ pypy/module/posix/__init__.py
@@ -48,67 +48,19 @@ corresponding Unix manual entries for more information on calls."""
     'abort'     : 'interp_posix.abort',
     'access'    : 'interp_posix.access',
     }
-    if hasattr(os, 'ftruncate'):
-        interpleveldefs['ftruncate'] = 'interp_posix.ftruncate'
-    if hasattr(os, 'putenv'):
-        interpleveldefs['putenv'] = 'interp_posix.putenv'
-    if hasattr(posix, 'unsetenv'): # note: emulated in os
-        interpleveldefs['unsetenv'] = 'interp_posix.unsetenv'
-    if hasattr(os, 'getpid'):
-        interpleveldefs['getpid'] = 'interp_posix.getpid'
-    if hasattr(os, 'link'):
-        interpleveldefs['link'] = 'interp_posix.link'
-    if hasattr(os, 'symlink'):
-        interpleveldefs['symlink'] = 'interp_posix.symlink'
-    if hasattr(os, 'readlink'):
-        interpleveldefs['readlink'] = 'interp_posix.readlink'
-    if hasattr(os, 'fork'):
-        interpleveldefs['fork'] = 'interp_posix.fork'
-    if hasattr(os, 'waitpid'):
-        interpleveldefs['waitpid'] = 'interp_posix.waitpid'
-    if hasattr(os, 'chown'):
-        interpleveldefs['chown'] = 'interp_posix.chown'
-    if hasattr(os, 'chroot'):
-        interpleveldefs['chroot'] = 'interp_posix.chroot'
-    if hasattr(os, 'confstr'):
-        interpleveldefs['confstr'] = 'interp_posix.confstr'
-    if hasattr(os, 'ctermid'):
-        interpleveldefs['ctermid'] = 'interp_posix.ctermid'
-    if hasattr(os, 'fchdir'):
-        interpleveldefs['fchdir'] = 'interp_posix.fchdir'
-    if hasattr(os, 'fpathconf'):
-        interpleveldefs['fpathconf'] = 'interp_posix.fpathconf'
-    if hasattr(os, 'getegid'):
-        interpleveldefs['getegid'] = 'interp_posix.getegid'
-    if hasattr(os, 'geteuid'):
-        interpleveldefs['geteuid'] = 'interp_posix.geteuid'
-    if hasattr(os, 'getgid'):
-        interpleveldefs['getgid'] = 'interp_posix.getgid'
-    if hasattr(os, 'getuid'):
-        interpleveldefs['getuid'] = 'interp_posix.getuid'    
-    if hasattr(os, 'getpgid'):
-        interpleveldefs['getpgid'] = 'interp_posix.getpgid'
-    if hasattr(os, 'getpid'):
-        interpleveldefs['getpid'] = 'interp_posix.getpid'
-    if hasattr(os, 'getppid'):
-        interpleveldefs['getppid'] = 'interp_posix.getppid'
-    if hasattr(os, 'getpgrp'):
-        interpleveldefs['getpgrp'] = 'interp_posix.getpgrp'
-    if hasattr(os, 'getsid'):
-        interpleveldefs['getsid'] = 'interp_posix.getsid'
-    if hasattr(os, 'getlogin'):
-        interpleveldefs['getlogin'] = 'interp_posix.getlogin'
-    if hasattr(os, 'getgroups'):
-        interpleveldefs['getgroups'] = 'interp_posix.getgroups'
-    if hasattr(os, 'getloadavg'):
-        interpleveldefs['getloadavg'] = 'interp_posix.getloadavg'
     
-
+    for func_name in ['ftruncate', 'putenv', 'unsetenv', 'getpid', 'link'
+        'symlink', 'readlink', 'fork', 'fork', 'waitpid', 'chown', 'chroot',
+        'confstr', 'ctermid', 'fchdir', 'fpathconf', 'getegid', 'geteuid',
+        'getgid', 'getuid', 'getpgid', 'getpid', 'getppid', 'getpgrp',
+        'getsid', 'getlogin', 'getgroups', 'getloadavg']:
+        if hasattr(os, func_name):
+            interpleveldefs[func_name] = 'interp_posix.%s' % func_name
+    
 for constant in dir(os):
     value = getattr(os, constant)
     if constant.isupper() and type(value) is int:
         Module.interpleveldefs[constant] = "space.wrap(%s)" % value
-if hasattr(os, 'confstr_names'):
-    Module.interpleveldefs['confstr_names'] = "space.wrap(%s)" % os.confstr_names
-if hasattr(os, 'pathconf_names'):
-    Module.interpleveldefs['pathconf_names'] = "space.wrap(%s)" % os.pathconf_names
+for const in ['confstr_names', 'pathconf_names']:
+    if hasattr(os, const):
+        Module.interpleveldefs[const] = "space.wrap(%s)" % getattr(os, const)
