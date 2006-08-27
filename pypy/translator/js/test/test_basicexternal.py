f71e139c071ebba35c07efed7dcead8f5f39c059
Maciej Fijalkowski <fijall@gmail.com>
2006-08-27 17:47:44 +0000

    Added a test (actually failing).

--- pypy/translator/js/test/test_basicexternal.py
+++ pypy/translator/js/test/test_basicexternal.py
@@ -12,9 +12,33 @@ class A(BasicExternal):
 
 a = A()
 
+class B(object):
+    pass
+
 def test_decorator():
     def dec_fun():
         a.some_code("aa")
     
     fun = compile_function(dec_fun, [])
     check_source_contains(fun, "\.some_code")
+
+def test_basicexternal_element():
+    def be_fun():
+        b = B()
+        b.a = a
+        b.a.some_code("aa")
+    
+    fun = compile_function(be_fun, [])
+    check_source_contains(fun, "\.some_code")
+
+def test_basicexternal_raise():
+    def raising_fun():
+        try:
+            b = B()
+        except:
+            pass
+        else:
+            return 3
+
+    fun = compile_function(raising_fun, [])
+    assert fun() == 3
