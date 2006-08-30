1c2d5b81578780b21989bedb368dca66d8529c67
Samuele Pedroni <pedronis@lucediurna.net>
2006-08-30 11:35:20 +0000

    ported vlist tests (skipped as in-progress for now)

---
+++ pypy/jit/codegen/i386/test/test_interp_vlist.py
@@ -0,0 +1,13 @@
+import py
+from pypy.jit.timeshifter.test import test_vlist
+from pypy.jit.codegen.i386.test.test_interp_ts import I386LLInterpTimeshiftingTestMixin
+
+py.test.skip("in-progress")
+
+class TestVList(I386LLInterpTimeshiftingTestMixin,
+                test_vlist.TestVList):
+
+    # for the individual tests see
+    # ====> ../../../timeshifter/test/test_vlist.py
+
+    pass
