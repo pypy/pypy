a50972eba653a255a4279aee6e8e3b8c5f3573b9
Carl Friedrich Bolz <cfbolz@gmx.de>
2006-09-17 18:33:04 +0000

    Implementation of _random using the rpy_random mersenne twister implementation. Needs more tests and is currently disabled, but in theory it is superior than the current applevel implementation

--- pypy/module/_random/__init__.py
+++ pypy/module/_random/__init__.py
@@ -1,6 +1,4 @@
-import py             # FINISHME
-py.test.skip("The _random module is not ready: it misses the post-2.2 parts of "
-             "the interface and it needs to use new Mersenne Twister algorithm.")
+import py
 
 from pypy.interpreter.mixedmodule import MixedModule
 
@@ -8,29 +6,6 @@ class Module(MixedModule):
     appleveldefs = {}
   
     interpleveldefs = {
-        'Random'          : 'interp_random.W_Random',  
-        'seed'            : 'interp_random.get_random_method(space, "seed")',
-        'getstate'        : 'interp_random.get_random_method(space, "getstate")',
-        'setstate'        : 'interp_random.get_random_method(space, "setstate")',
-        'jumpahead'       : 'interp_random.get_random_method(space, "jumpahead")',
-        'randrange'       : 'interp_random.get_random_method(space, "randrange")',
-        'randint'         : 'interp_random.get_random_method(space, "randint")',
-        'choice'          : 'interp_random.get_random_method(space, "choice")',
-        'shuffle'         : 'interp_random.get_random_method(space, "shuffle")',
-        'sample'          : 'interp_random.get_random_method(space, "sample")',
-        'random'          : 'interp_random.get_random_method(space, "random")',
-        'uniform'         : 'interp_random.get_random_method(space, "uniform")',
-        'betavariate'     : 'interp_random.get_random_method(space, "betavariate")',
-        'expovariate'     : 'interp_random.get_random_method(space, "expovariate")',
-        'gammavariate'    : 'interp_random.get_random_method(space, "gammavariate")',
-        'jumpahead'       : 'interp_random.get_random_method(space, "jumpahead")',
-        'gauss'           : 'interp_random.get_random_method(space, "gauss")',
-        'lognormvariate'  : 'interp_random.get_random_method(space, "lognormvariate")',
-        'normalvariate'   : 'interp_random.get_random_method(space, "normalvariate")',
-        'vonmisesvariate' : 'interp_random.get_random_method(space, "vonmisesvariate")',
-        'paretovariate'   : 'interp_random.get_random_method(space, "paretovariate")',
-        'cunifvariate'    :  'interp_random.get_random_method(space, "cunifvariate")',
-        'weibullvariate'  : 'interp_random.get_random_method(space, "weibullvariate")',
-        'whseed'          : 'interp_random.get_random_method(space, "whseed")', # officially obsolete
+        'Random'          : 'interp_random.W_Random',
         }
     
--- pypy/module/_random/test/test_random.py
+++ pypy/module/_random/test/test_random.py
@@ -1,8 +1,6 @@
 import py
 from pypy.conftest import gettestobjspace
 
-py.test.skip("XXX missing _random.sample()")
-
 class AppTestRandom:
     def setup_class(cls):
         cls.space = gettestobjspace(usemodules=['_random'])
@@ -11,6 +9,41 @@ class AppTestRandom:
         import _random
         _random.__dict__  # crashes if entries in __init__.py can't be resolved
 
-    # XXX MISSING TESTS XXX
-    # XXX MISSING TESTs XXX
-    # XXX MISSING TESTS XXX
+    def test_random(self):
+        import _random
+        # XXX quite a bad test
+        rnd = _random.Random(1)
+        lst1 = [rnd.random() for i in range(100)]
+        rnd.seed(1)
+        lst2 = [rnd.random() for i in range(100)]
+        assert lst1 == lst2
+        smaller = 0
+        for elt in lst1:
+            assert 0 <= elt <= 1
+            if elt < 0.5:
+                smaller += 1
+        # quite unlikely to fail, but well
+        assert smaller > 10
+
+    def test_getstate_setstate(self):
+        import _random
+        rnd1 = _random.Random()
+        rnd1.random()
+        rnd2 = _random.Random()
+        assert rnd1.getstate() != rnd2.getstate()
+        rnd1.setstate(rnd2.getstate())
+        assert [rnd1.random() for i in range(100)] == [
+                    rnd2.random() for i in range(100)]
+
+    def test_seed(self):
+        import _random
+        rnd = _random.Random()
+        rnd.seed()
+        obj = "spam and eggs"
+        nums = []
+        for o in [obj, hash(obj), -hash(obj)]:
+            rnd.seed(o)
+            nums.append([rnd.random() for i in range(100)])
+        n1 = nums[0]
+        for n2 in nums[1:]:
+            assert n1 == n2
