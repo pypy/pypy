42f7a7b284c807119ec09969adbfcedc468d4260
Maciej Fijalkowski <fijall@gmail.com>
2006-10-04 14:31:25 +0000

    Fixed options (-o conflicts)

--- pypy/bin/jscompile.py
+++ pypy/bin/jscompile.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """ RPython to javascript compiler
-Usage: jscompiler module_to_compile [list of functions to export]
+Usage: jscompile module_to_compile [list of functions to export]
 
 - or -
-jscompiler --help to show list of options
+jscompile --help to show list of options
 """
 
 import autopath
@@ -17,27 +17,30 @@ make_option = optparse.make_option
 
 def get_options():
     options = []
+    config, parser = option.get_standard_options()
     
     options.append(make_option(
         '--view', action='store_true', dest='view',
         help='View flow graphs'))
     
     options.append(make_option(
-        '-o', '--output', action='store', type='string', dest='output',
+        '--output', action='store', type='string', dest='output',
         help='File to save results (default output.js)'))
     
     options.append(make_option(
         '-d', '--debug', action='store_true', dest='debug_transform',
         help="Use !EXPERIMENTAL! debug transform to produce tracebacks"
     ))
-    
-    return options
+
+    parser.add_options(options)
+    return config, parser
     
 def process_options(argv):
-    return option.process_options(get_options(), Options, argv)
+    config, parser = get_options()
+    return option.process_options(parser, Options, argv[1:])
 
 if __name__ == '__main__':
-    argv = process_options(sys.argv[1:])
+    argv = process_options(sys.argv)
     curdir = os.getcwd()
     if curdir not in sys.path:
         sys.path.insert(0, curdir)
