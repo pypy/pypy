13acd2e22272f85dd456868b1de01fe8e809b5d7
Michael Hudson <micahel@gmail.com>
2006-10-11 12:15:34 +0000

    add method stubs to AbstractRGenOp with pointedly empty docstrings :)

--- pypy/jit/codegen/model.py
+++ pypy/jit/codegen/model.py
@@ -26,7 +26,65 @@ class CodeGenBlock(object):
 
 
 class AbstractRGenOp(object):
-    pass
+
+    def newgraph(self, sigtoken):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.genconst(0)
+    def genconst(llvalue):
+        """ """
+        raise NotImplementedError
+
+    constPrebuiltGlobal = genconst
+
+    def gencallableconst(self, sigtoken, name, entrypointaddr):
+        """ """
+        raise NotImplementedError
+
+    # the "token" methods render non-RPython data structures
+    # (instances of LowLevelType) into RPython data structures.  they
+    # are memo-specialized, so they can be full Python inside, but
+    # each method must always return the same type, so the jit can
+    # store the results in a list, for example (each backend can
+    # decide what this type is independently, though)
+
+    @staticmethod
+    @specialize.memo()
+    def fieldToken(T, name):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.memo()
+    def allocToken(T):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.memo()
+    def varsizeAllocToken(T):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.memo()
+    def arrayToken(A):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.memo()
+    def kindToken(T):
+        """ """
+        raise NotImplementedError
+
+    @staticmethod
+    @specialize.memo()
+    def sigToken(FUNCTYPE):
+        """ """
+        raise NotImplementedError
 
 
 class CodeGenSwitch(object):
