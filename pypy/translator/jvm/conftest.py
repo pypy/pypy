680a3c3bed47dce5b43e1aef816dc8ce787182ef
Armin Rigo <arigo@tunes.org>
2006-10-16 11:47:16 +0000

    Fixes(?) to translator/jvm/conftest.py.  For very indirect reasons this conftest.py is loaded if we type 'py.test' in a sibling directory like translator/cli or translator/c.  At least in a Python 2.4 environment, the previous jvm/conftest.py was causing optparse to crash.

--- pypy/translator/jvm/conftest.py
+++ pypy/translator/jvm/conftest.py
@@ -5,15 +5,15 @@ Option = py.test.Config.Option
 option = py.test.Config.addoptions(
     "pypy-jvm options",
 
-    Option('--javac', action='', dest='javac', default='javac',
+    Option('--javac', action='store', dest='javac', default='javac',
            help='Define the java compiler to use'),
-    Option('--java', action='', dest='java', default='java',
+    Option('--java', action='store', dest='java', default='java',
            help='Define the java executable to use'),
-    Option('--view', action='store_true', dest='view', default=False,
-           help='View the graphs before they are generated'),
+##    Option('--view', action='store_true', dest='view', default=False,
+##           help='View the graphs before they are generated'),
     Option('--wd', action='store_true', dest='wd', default=False,
            help='Output to current directory instead of /tmp'),
-    Option('--package', action='', dest='package', default='pypy',
+    Option('--package', action='store', dest='package', default='pypy',
            help='Package to output generated classes into')
     #Option('--opt', action='XXX', dest='YYY', default=DEF, help='HELP')
     )
