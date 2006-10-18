38f9ee1322ed30d684871245e6303b84cd07cb54
Michael Hudson <micahel@gmail.com>
2006-10-18 14:20:32 +0000

    move files around in jit/codegen/ppc

--- pypy/jit/codegen/ppc/test/test_rgenop.py
+++ pypy/jit/codegen/ppc/test/test_rgenop.py
@@ -1,4 +1,4 @@
-from pypy.jit.codegen.ppc.rppcgenop import RPPCGenOp
+from pypy.jit.codegen.ppc.rgenop import RPPCGenOp
 from pypy.jit.codegen.test.rgenop_tests import AbstractRGenOpTests
 
 class TestRPPCGenop(AbstractRGenOpTests):
