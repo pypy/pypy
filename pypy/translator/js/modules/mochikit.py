463c28f13fde9a3b0a26ec70d869d570f8a32cbf
Maciej Fijalkowski <fijall@gmail.com>
2006-11-02 15:32:55 +0000

    Specialise mochikit function.

--- pypy/translator/js/modules/mochikit.py
+++ pypy/translator/js/modules/mochikit.py
@@ -9,19 +9,25 @@ createLoggingPane.suggested_primitive = True
 def log(data):
     pass
 log.suggested_primitive = True
+log._annspecialcase_ = "specialize:argtype(0)"
 
 def logDebug(data):
     pass
 logDebug.suggested_primitive = True
+logDebug._annspecialcase_ = "specialize:argtype(0)"
 
 def logWarning(data):
     pass
 logWarning.suggested_primitive = True
+logWarning._annspecialcase_ = "specialize:argtype(0)"
+
 
 def logError(data):
     pass
 logError.suggested_primitive = True
+logError._annspecialcase_ = "specialize:argtype(0)"
 
 def logFatal(data):
     pass
 logFatal.suggested_primitive = True
+logFatal._annspecialcase_ = "specialize:argtype(0)"
