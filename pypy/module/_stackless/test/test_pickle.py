5a58f356a269e74712d0c9061355fcb08e8510bd
Armin Rigo <arigo@tunes.org>
2006-11-07 15:52:01 +0000

    A new test that passes out of the box on pypy-c-stackless.  It's the same as the coroutine cloning demo, but done with pickling instead (so it doesn't require a stacklessgc).

--- pypy/module/_stackless/test/test_pickle.py
+++ pypy/module/_stackless/test/test_pickle.py
@@ -224,3 +224,71 @@ assert output == [9]
 ''' in mod.__dict__
         finally:
             del sys.modules['mod']
+
+
+    def test_solver(self):
+        import new, sys
+
+        mod = new.module('mod')
+        sys.modules['mod'] = mod
+        try:
+            exec '''
+import _stackless, pickle
+
+class Fail(Exception):
+    pass
+
+class Success(Exception):
+    pass
+
+def first_solution(func):
+    global next_answer
+    co = _stackless.coroutine()
+    co.bind(func)
+    pending = [(co, None)]
+    while pending:
+        co, next_answer = pending.pop()
+        try:
+            co.switch()
+        except Fail:
+            pass
+        except Success, e:
+            return e.args[0]
+        else:
+            # zero_or_one() called, clone the coroutine
+            # NB. this seems to be quite slow
+            co2 = pickle.loads(pickle.dumps(co))
+            pending.append((co2, 1))
+            pending.append((co, 0))
+    raise Fail("no solution")
+
+pending = []
+main = _stackless.coroutine.getcurrent()
+
+def zero_or_one():
+    main.switch()
+    return next_answer
+
+# ____________________________________________________________
+
+invalid_prefixes = {
+    (0, 0): True,
+    (0, 1, 0): True,
+    (0, 1, 1): True,
+    (1, 0): True,
+    (1, 1, 0, 0): True,
+    }
+
+def example():
+    test = []
+    for n in range(5):
+        test.append(zero_or_one())
+        if tuple(test) in invalid_prefixes:
+            raise Fail
+    raise Success(test)
+
+res = first_solution(example)
+assert res == [1, 1, 0, 1, 0]
+''' in mod.__dict__
+        finally:
+            del sys.modules['mod']
