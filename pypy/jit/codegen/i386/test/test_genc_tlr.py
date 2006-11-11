473e5c49658192c2c16da97f3ef35e7d827bb221
Samuele Pedroni <pedronis@lucediurna.net>
2006-11-11 12:40:45 +0000

    fix test_genc_tlr now that the tlr tests use the portal style.
    kill test_interp_tlr which doesn't make complete sense now.

--- pypy/jit/codegen/i386/test/test_genc_tlr.py
+++ pypy/jit/codegen/i386/test/test_genc_tlr.py
@@ -1,9 +1,9 @@
 import py
 from pypy.jit.timeshifter.test import test_tlr
-from pypy.jit.codegen.i386.test.test_genc_ts import I386TimeshiftingTestMixin
+from pypy.jit.codegen.i386.test.test_genc_portal import I386PortalTestMixin
 
 
-class TestTLR(I386TimeshiftingTestMixin,
+class TestTLR(I386PortalTestMixin,
               test_tlr.TestTLR):
 
     # for the individual tests see
