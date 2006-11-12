668bc2641b9cde1ad9a925496641f4f7f07a5de7
Carl Friedrich Bolz <cfbolz@gmx.de>
2006-11-12 21:32:27 +0000

    add a target for the prolog interpreter

---
+++ pypy/translator/goal/targetprologstandalone.py
@@ -0,0 +1,35 @@
+"""
+A simple standalone target for the prolog interpreter.
+"""
+
+import sys
+from pypy.lang.prolog.interpreter.translatedmain import repl, execute
+
+# __________  Entry point  __________
+
+from pypy.lang.prolog.interpreter.engine import Engine
+from pypy.lang.prolog.interpreter import engine, term
+e = Engine()
+engine.DEBUG = False
+term.DEBUG = False
+
+def entry_point(argv):
+    if len(argv) == 2:
+        execute(e, argv[1])
+    try:
+        repl(e)
+    except SystemExit:
+        return 1
+    return 0
+
+# _____ Define and setup target ___
+
+def handle_config(config):
+    config.translation.stackless = True
+
+def target(driver, args):
+    driver.exe_name = 'pyrolog-%(backend)s'
+    return entry_point, None
+
+if __name__ == '__main__':
+    entry_point(sys.argv)
