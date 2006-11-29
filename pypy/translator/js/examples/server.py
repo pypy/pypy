17c5e1c62a6815419c35c11a74ecfda371a53ffc
Maciej Fijalkowski <fijall@gmail.com>
2006-11-29 11:39:23 +0000

    Added custom server for subclassing

---
+++ pypy/translator/js/examples/server.py
@@ -0,0 +1,103 @@
+
+""" This is example of totally basic server for XMLHttp request
+built on top of BaseHTTPServer.
+
+Construction is like that:
+
+you take your own implementation of Handler and subclass it
+to provide whatever you like. Each request is checked first for
+apropriate method in handler (with dots replaced as _) and this method
+needs to have set attribute exposed
+
+If method is not there, we instead try to search exported_methods (attribute
+of handler) for apropriate JSON call. We write down a JSON which we get as
+a return value (note that right now arguments could be only strings) and
+pass them to caller
+"""
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+import thread, threading
+import re
+import time
+import random
+import os
+import sys
+
+import py
+
+from pypy.translator.js import json
+
+from pypy.rpython.ootypesystem.bltregistry import MethodDesc, BasicExternal,\
+    described
+from pypy.translator.js.main import rpython2javascript, Options
+from pypy.translator.js import commproxy
+
+Options.debug_transform = False
+commproxy.USE_MOCHIKIT = True
+
+class ExportedMethods(BasicExternal):
+    _render_xmlhttp = True
+
+exported_methods = ExportedMethods()
+
+class TestHandler(BaseHTTPRequestHandler):
+    exported_methods = exported_methods
+    
+    def do_GET(self):
+        path = self.path
+        if path.endswith("/"):
+            path = path[:-1]
+        if path.startswith("/"):
+            path = path[1:]
+        m = re.match('^(.*)\?(.*)$', path)
+        if m:
+            path = m.group(1)
+            getargs = m.group(2)
+        else:
+            getargs = ""
+        name_path = path.replace(".", "_")
+        method_to_call = getattr(self, name_path, None)
+        if method_to_call is None or not getattr(method_to_call, 'exposed', None):
+            exec_meth = getattr(self.exported_methods, name_path, None)
+            if exec_meth is None:
+                self.send_error(404, "File %s not found" % path)
+            else:
+                self.serve_data('text/json', json.write(exec_meth(**self.parse_args(getargs))))
+        else:
+            method_to_call()
+    
+    def parse_args(self, getargs):
+        # parse get argument list
+        if getargs == "":
+            return {}
+        
+        args = {}
+        arg_pairs = getargs.split("&")
+        for arg in arg_pairs:
+            key, value = arg.split("=")
+            args[key] = value
+        return args
+    
+    def log_message(self, format, *args):
+        # XXX just discard it
+        pass
+    
+    do_POST = do_GET
+    
+    def serve_data(self, content_type, data):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.send_header("Content-length", len(data))
+        self.end_headers()
+        self.wfile.write(data)
+
+def start_server(server_address = ('', 8000), handler=TestHandler, start_new=True):
+    httpd = HTTPServer(server_address, handler)
+
+    if start_new:
+        thread.start_new_thread(httpd.serve_forever, ())
+        print "Server started, listening on %s" % (server_address,)
+    else:
+        print "Server started, listening on %s" % (server_address,)
+        httpd.serve_forever()
