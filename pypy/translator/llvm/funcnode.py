f5cec3322caccd25ce28510066e591ccee74f486
Michael Hudson <micahel@gmail.com>
2006-12-12 20:23:05 +0000

    support a lot more return types for functions with multiple switches

--- pypy/translator/llvm/funcnode.py
+++ pypy/translator/llvm/funcnode.py
@@ -96,7 +96,13 @@ class FuncNode(ConstantLLVMNode):
             codewriter._indent('call void %abort()')
             rettype = self.graph.getreturnvar().concretetype
             if rettype is lltype.Void:
-                codewriter._indent('ret')
+                codewriter._indent('ret void')
+            elif rettype is lltype.Bool:
+                codewriter._indent('ret bool false')
+            elif rettype is lltype.Float:
+                codewriter._indent('ret double 0.0')
+            elif isinstance(rettype, lltype.Ptr):
+                codewriter._indent('ret %s null'%(self.db.repr_type(rettype)))
             else:
                 codewriter._indent('ret %s 0'%(self.db.repr_type(rettype)))
         codewriter.closefunc()
