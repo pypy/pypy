2e2cdea60cdca7ca5e7121ea996f4d6d8d9d35d3
Antonio Cuni <anto.cuni@gmail.com>
2007-01-08 18:31:05 +0000

    First attempt to write a target for a standalone javascript interpreter.

---
+++ pypy/translator/goal/targetjsstandalone.py
@@ -0,0 +1,34 @@
+"""
+A simple standalone target for the javascript interpreter.
+"""
+
+import sys
+from pypy.rlib.streamio import open_file_as_stream
+from pypy.lang.js.interpreter import Interpreter
+from pypy.lang.js.interpreter import *
+from pypy.lang.js.jsobj import ExecutionReturned
+
+# __________  Entry point  __________
+
+interp = Interpreter()
+
+def entry_point(argv):
+    if len(argv) == 2:
+        f = open_file_as_stream(argv[1])
+        interp.load_bytecode(f.readall())
+        interp.run()
+        return 0
+    elif argv[0] == 'foo':
+        raise ExecutionReturned(None)
+    else:
+        print "Usage: %s bytecodefile" % argv[0]
+        return 1
+
+# _____ Define and setup target ___
+
+def target(driver, args):
+    driver.exe_name = 'js-%(backend)s'
+    return entry_point, None
+
+if __name__ == '__main__':
+    entry_point(sys.argv)
