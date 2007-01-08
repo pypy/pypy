e4707f733e441f075ff07c76c27792acf44105ab
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-01-08 13:51:37 +0000

    ansi_print itself moved to the py-lib

--- pypy/tool/ansi_print.py
+++ pypy/tool/ansi_print.py
@@ -4,21 +4,7 @@ A color print.
 
 import sys
 
-def ansi_print(text, esc, file=None, newline=True, flush=False):
-    if file is None: file = sys.stderr
-    text = text.rstrip()
-    if esc and sys.platform != "win32" and file.isatty():
-        if not isinstance(esc, tuple):
-            esc = (esc,)
-        text = (''.join(['\x1b[%sm' % cod for cod in esc])  +  
-                text +
-                '\x1b[0m')     # ANSI color code "reset"
-    if newline:
-        text += '\n'
-    file.write(text)
-    if flush:
-        file.flush()
-
+from py.__.misc.terminal_helper import ansi_print
 
 class AnsiLog:
 
