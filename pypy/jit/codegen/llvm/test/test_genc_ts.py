2ef06c0646d23b15dd720529c1c620c5a1073d00
Eric van Riet Paap <ericvrp@gmail.com>
2007-01-12 16:57:43 +0000

    mark tests that fail on llvm 2.0

--- pypy/jit/codegen/llvm/test/test_genc_ts.py
+++ pypy/jit/codegen/llvm/test/test_genc_ts.py
@@ -31,6 +31,13 @@ class TestTimeshiftLLVM(LLVMTimeshiftingTestMixin,
         test_two_loops_merging = skip_too_minimal #segfault
         test_green_char_at_merge = skip #segfault
         test_residual_red_call_with_exc = skip
+    else: #needs fixing for >= 2.0
+        test_red_array = skip
+        test_red_struct_array = skip
+        test_red_varsized_struct = skip
+        test_array_of_voids = skip
+        test_merge_structures = skip
+        test_green_char_at_merge = skip
 
     if skip_passing:
         test_very_simple = skip
