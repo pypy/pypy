719f3c9e75736f3556716e10de0673fe6ee3e1ab
Armin Rigo <arigo@tunes.org>
2007-01-20 17:06:35 +0000

    These two tests pass.

--- pypy/jit/codegen/i386/test/test_genc_tlc.py
+++ pypy/jit/codegen/i386/test/test_genc_tlc.py
@@ -1,5 +1,4 @@
 import py
-py.test.skip("in-progress")
 from pypy.jit.timeshifter.test import test_tlc
 from pypy.jit.codegen.i386.test.test_genc_portal import I386PortalTestMixin
 
--- pypy/jit/codegen/i386/test/test_genc_tlr.py
+++ pypy/jit/codegen/i386/test/test_genc_tlr.py
@@ -1,5 +1,4 @@
 import py
-py.test.skip("in-progress")
 from pypy.jit.timeshifter.test import test_tlr
 from pypy.jit.codegen.i386.test.test_genc_portal import I386PortalTestMixin
 
