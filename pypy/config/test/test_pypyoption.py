d702f091e172928744dd027cec4c63e6f30004e9
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-01-23 17:05:25 +0000

    test whether files for all the options are there

--- pypy/config/test/test_pypyoption.py
+++ pypy/config/test/test_pypyoption.py
@@ -1,5 +1,8 @@
 import py
 from pypy.config.pypyoption import get_pypy_config
+from pypy.config.config import Config
+
+thisdir = py.magic.autopath().dirpath()
 
 def test_required():
     conf = get_pypy_config()
@@ -23,3 +26,13 @@ def test_stacklessgc_required():
     conf.translation.gc = "stacklessgc"
     assert conf.translation.stackless
     assert conf.translation.type_system == "lltype"
+
+def test_check_documentation():
+    from pypy.doc.config.confrest import all_optiondescrs
+    configdocdir = thisdir.dirpath().dirpath().join("doc", "config")
+    for descr in all_optiondescrs:
+        prefix = descr._name
+        c = Config(descr)
+        for path in c.getpaths(include_groups=True):
+            fn = prefix + "." + path + ".txt"
+            assert configdocdir.join(fn).check()
