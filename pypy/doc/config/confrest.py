7548342babc24df777eb4b7f133f28686326e68c
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-01-23 16:56:47 +0000

    a huge checkin of mostly empty files, one for every option. the plan is to document what each option does in all these files. The confrest file in here is special and adds nice headers that produce headers for all these files, containing extra info about the option (what depends on what, navigation between the options, option types, cmdline args...).

---
+++ pypy/doc/config/confrest.py
@@ -0,0 +1,48 @@
+from pypy.doc.confrest import *
+import pypy.config.makerestdoc
+from pypy.config.config import Config
+from pypy.config import pypyoption, translationoption
+
+
+all_optiondescrs = [pypyoption.pypy_optiondescription,
+                    translationoption.translation_optiondescription,
+                   ]
+
+start_to_descr = dict([(descr._name, descr) for descr in all_optiondescrs])
+
+class PyPyPage(PyPyPage): 
+    def fill(self):
+        super(PyPyPage, self).fill()
+        self.menubar[:] = html.div(
+            html.a("general documentation", href="../index.html",
+                   class_="menu"), " ",
+            html.a("translation options", href="translation.txt", 
+                   class_="menu"), 
+            html.a("standard interpreter options", href="objspace.txt", 
+                   class_="menu"), 
+            " ", id="menubar")
+
+class Project(Project): 
+    stylesheet = "../style.css"
+    title = "PyPy Configuration"
+    prefix_title = "PyPy Configuration"
+    Page = PyPyPage 
+
+    def get_content(self, txtpath, encoding):
+        fullpath = txtpath.purebasename
+        start = fullpath.split(".")[0]
+        path = fullpath.rsplit(".", 1)[0]
+        basedescr = start_to_descr[start]
+        if fullpath.count(".") == 0:
+            descr = basedescr
+            path = ""
+        else:
+            conf = Config(basedescr)
+            subconf, step = conf._cfgimpl_get_home_by_path(
+                    fullpath.split(".", 1)[1])
+            descr = getattr(subconf._cfgimpl_descr, step)
+        text = unicode(descr.make_rest_doc(path).text())
+        if txtpath.check(file=True):
+            return u"%s\n\n%s" % (text, unicode(txtpath.read(), encoding))
+        return text
+
