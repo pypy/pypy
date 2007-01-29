6e4198d5e43e30b89345d7e5e96fda6899ffacea
Armin Rigo <arigo@tunes.org>
2007-01-29 17:22:28 +0000

    Don't run autorun.py forever, it would exhaust memory. Add a comment about how to run it forever from the shell.

--- pypy/jit/codegen/demo/autorun.py
+++ pypy/jit/codegen/demo/autorun.py
@@ -3,7 +3,13 @@ from pypy.jit.codegen.demo import test_random
 from pypy.jit.codegen.demo import conftest as demo_conftest
 
 
-def test_forever():
-    while True:
+# each iteration of test_many_times leaks memory, so we can't run
+# it forever.  If you want that result, use a bash command like:
+#
+#     while py.test autorun.py --pdb; do echo "again"; done
+
+
+def test_many_times():
+    for i in range(80):
         demo_conftest.option.randomseed = random.randrange(0, 100000)
         test_random.test_random_function()
