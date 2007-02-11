4b3f2b5ec52371edbc54e99e18f9d68ffd5ecd35
Armin Rigo <arigo@tunes.org>
2007-02-11 20:30:47 +0000

    Oups, the normalizeexception() is needed.

--- pypy/translator/geninterplevel.py
+++ pypy/translator/geninterplevel.py
@@ -72,7 +72,7 @@ from pypy.tool.ansi_print import ansi_log
 log = py.log.Producer("geninterp")
 py.log.setconsumer("geninterp", ansi_log)
 
-GI_VERSION = '1.1.20'  # bump this for substantial changes
+GI_VERSION = '1.1.21'  # bump this for substantial changes
 # ____________________________________________________________
 
 try:
@@ -1346,6 +1346,7 @@ else:
                 # which goes to the last err%d_%d label written above.
                 # Since we only have OperationError, we need to select:
                 yield "except %s, e:" % (self.nameof(OperationError),)
+                yield "    e.normalize_exception(space)"
                 q = "if"
                 for link in block.exits[1:]:
                     assert issubclass(link.exitcase, py.builtin.BaseException)
