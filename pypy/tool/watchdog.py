31c6fba06814e7cdd940e0fae2ea07622d3623e0
Samuele Pedroni <pedronis@lucediurna.net>
2007-02-11 11:48:23 +0000

    if the watched process died with a signal, print the signal name and consider this a variation on "timeout"

--- pypy/tool/watchdog.py
+++ pypy/tool/watchdog.py
@@ -1,9 +1,18 @@
 import sys, os, signal
 import threading
 
+def getsignalname(n):
+    for name, value in signal.__dict__.items():
+        if value == n and name.startswith('SIG'):
+            return name
+    return 'signal %d' % (n,)
+
 timeout = float(sys.argv[1])
+timedout = False
 
 def childkill():
+    global timedout
+    timedout = True
     sys.stderr.write("="*26 + "timedout" + "="*26 + "\n")
     try:
         os.kill(pid, signal.SIGTERM)
@@ -27,6 +36,13 @@ else: # parent
     if os.WIFEXITED(status):
         sys.exit(os.WEXITSTATUS(status))
     else:
+        assert os.WIFSIGNALED(status)
+        sign = os.WTERMSIG(status)
+        if timedout and sign == signal.SIGTERM:
+            sys.exit(1)
+        signame = getsignalname(sign)
+        sys.stderr.write("="*26 + "timedout" + "="*26 + "\n")        
+        sys.stderr.write("="*25 + " %-08s " %  signame + "="*25 + "\n")
         sys.exit(1)
 
     
