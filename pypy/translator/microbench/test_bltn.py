d202af91ea2a4a309fbc790fb28bbbbd9f79837b
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2007-02-11 10:40:22 +0000

    Two isinstance checks.

--- pypy/translator/microbench/test_bltn.py
+++ pypy/translator/microbench/test_bltn.py
@@ -20,3 +20,29 @@ def test_call_fabs():
     while c < n:
         x = fabs(x)
         c += 1
+
+class foo:
+    pass
+
+class bar(foo):
+    pass
+
+class baz(bar):
+    pass
+
+def test_isinstance1():
+    f = foo()
+    b1 = bar()
+    b2 = baz()
+    for x in xrange(100000):
+        isinstance(b1, foo)
+        isinstance(b1, baz)
+        isinstance(f, bar)
+        isinstance(b2, foo)
+
+def test_isinstance2():
+    for x in xrange(100000):
+        isinstance(1, float)
+        isinstance(1, int)
+        isinstance("foo", basestring)
+
