3e0e91e721d3f27ed24f7b9ecb2875bc45c59c06
Christian Tismer <tismer@stackless.com>
2007-02-13 05:13:14 +0000

    modified the compile_stackless.bat file so it works both n windows and linux. For some reason, the build does not work any longer...

--- pypy/translator/goal/compile_stackless.bat
+++ pypy/translator/goal/compile_stackless.bat
@@ -1,3 +1,3 @@
 python -c "import time; print time.ctime(), 'compile start'" >> compile.log
-translate.py --batch --new-stackless
+python translate.py --batch --stackless
 python -c "import time; print time.ctime(), 'compile stop'" >> compile.log
