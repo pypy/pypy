98f9ab390c9c4367d1741f135580a6bfe7b31fb7
Armin Rigo <arigo@tunes.org>
2007-02-13 13:11:02 +0000

    Oups, that was not RPython, actually.

--- pypy/rlib/rsre/rsre_core.py
+++ pypy/rlib/rsre/rsre_core.py
@@ -69,7 +69,8 @@ class StateMixin(object):
         self.marks, self.lastindex = self.marks_stack.pop()
 
     def marks_pop_keep(self):
-        self.marks[:], self.lastindex = self.marks_stack[-1]
+        marks, self.lastindex = self.marks_stack[-1]
+        self.marks = marks[:]
 
     def marks_pop_discard(self):
         self.marks_stack.pop()
