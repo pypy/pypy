d83d9fe06b2996cb6e49b0b23a4f30a4f58c8246
Guido Wesdorp <guido@codespeak.net>
2007-02-15 14:21:31 +0000

    Fixed links in subdirs: links are now generated taking the file location into account.

--- pypy/doc/confrest.py
+++ pypy/doc/confrest.py
@@ -4,18 +4,27 @@ from py.__.doc.confrest import *
 class PyPyPage(Page): 
     def fill_menubar(self):
         self.menubar = html.div(
-            html.a("news", href="news.html", class_="menu"), " ",
-            html.a("getting-started", 
-                   href="getting-started.html", class_="menu"), " ",
-            html.a("documentation", href="index.html", class_="menu"), " ",
-            html.a("svn", href="https://codespeak.net/viewvc/pypy/dist/", class_="menu"), " ", 
-            html.a("issues", 
-                   href="https://codespeak.net/issue/pypy-dev/", 
-                   class_="menu"), 
-            html.a("contact", href="contact.html", class_="menu"), " ", 
-            html.a("EU/project", 
+            html.a("news", href=self.get_doclink("news.html"),
+                   class_="menu"), " ",
+            html.a("getting-started",
+                   href=self.get_doclink("getting-started.html"),
+                   class_="menu"), " ",
+            html.a("documentation", href=self.get_doclink("index.html"),
+                   class_="menu"), " ",
+            html.a("svn", href="https://codespeak.net/viewvc/pypy/dist/",
+                   class_="menu"), " ",
+            html.a("issues",
+                   href="https://codespeak.net/issue/pypy-dev/",
+                   class_="menu"),
+            html.a("contact", href=self.get_doclink("contact.html"),
+                   class_="menu"), " ",
+            html.a("EU/project",
                    href="http://pypy.org/", class_="menu"), " ",
             " ", id="menubar")
+
+    def get_doclink(self, target):
+        return relpath(self.targetpath.strpath,
+                       self.project.get_docpath().join(target).strpath)
 
 class Project(Project): 
     mydir = py.magic.autopath().dirpath()
