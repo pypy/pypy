3971e50ac7face2b9a8b116ee25f46f3495520d6
Maciej Fijalkowski <fijall@gmail.com>
2007-02-22 08:29:45 +0000

    Missing import

---
+++ pypy/translator/js/examples/console_client.py
@@ -0,0 +1,62 @@
+
+from pypy.translator.js.modules import dom
+from pypy.translator.js.modules.mochikit import log, createLoggingPane
+from pypy.translator.js.examples.console import exported_methods
+
+class Glob(object):
+    pass
+
+glob = Glob()
+
+def add_text(txt):
+    dom.document.getElementById("data").innerHTML += txt    
+
+def refresh_console(msg):
+    inp_elem = dom.document.getElementById("inp")
+    inp_elem.disabled = False
+    inp_elem.scrollIntoView()
+    log(msg[0])
+    if msg[0] == "refresh":
+        data = msg[1]
+        log(data)
+        if data.endswith(">>> ") or data.endswith("... "):
+            # prove positive slice here
+            l = len(data) - 4
+            assert l >= 0
+            glob.prompt = data[l:]
+            data = data[:l]
+        else:
+            exported_methods.refresh_empty(glob.sess_id, refresh_console)
+        add_text(data)
+    elif msg[0] == 'disconnect':
+        dom.document.getElementById("error").innerHTML = "ERROR! disconnected"
+
+def set_sessid(sessid):
+    glob.sess_id = sessid
+    exported_methods.refresh_empty(sessid, refresh_console)
+
+def empty_callback(msg):
+    inp_elem = dom.document.getElementById("inp")
+    inp_elem.disabled = False
+    inp_elem.scrollIntoView()
+
+def keypressed(key):
+    kc = key.keyCode
+    if kc == ord("\r"):
+        inp_elem = dom.document.getElementById("inp")
+        cmd = inp_elem.value
+        inp_elem.value = ''
+        inp_elem.disabled = True
+        add_text(glob.prompt + cmd + "\n")
+        #if not cmd:
+        #    exported_methods.refresh(glob.sess_id, cmd, empty_callback)
+        #else:
+        exported_methods.refresh(glob.sess_id, cmd, refresh_console)
+
+def console_onload():
+    createLoggingPane(True)
+    inp_elem = dom.document.getElementById("inp")
+    inp_elem.focus()
+    dom.document.onkeypress = keypressed
+    exported_methods.get_console(set_sessid)
+
