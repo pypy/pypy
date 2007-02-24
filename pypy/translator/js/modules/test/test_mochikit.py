640faa351fc14c9c62ee8815e76c71d2847895ab
Maciej Fijalkowski <fijall@gmail.com>
2007-02-24 17:01:49 +0000

    Be more verbose in this test

--- pypy/translator/js/modules/test/test_mochikit.py
+++ pypy/translator/js/modules/test/test_mochikit.py
@@ -6,4 +6,4 @@ class TestRender(object):
     def test_escape_html(self):
         def x():
             escapeHTML("xxx") + "xxx"
-        assert js_source([x], use_pdb=False)
+        assert js_source([x], use_pdb=False).find("escapeHTML (") != -1
