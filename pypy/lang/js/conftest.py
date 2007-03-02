09f403779c18d17c1df9941044dec774f2796166
Leonardo Santagada <santagada@gmail.com>
2007-03-02 14:58:38 +0000

    some py.test distributed stuff... lets see if it works as advertised :)

--- pypy/lang/js/conftest.py
+++ pypy/lang/js/conftest.py
@@ -1,5 +1,13 @@
 import py
 
+dist_hosts = ['localhost:/tmp/jspypy', 'localhost']
+dist_rsync_roots = ['../../../',]
+# dist_remotepython = 'python2.4'
+dist_nicelevel = 10 
+dist_boxed = False
+dist_maxwait = 1000 
+dist_taskspernode = 10
+
 Option = py.test.config.Option
 option = py.test.config.addoptions("ecma compatibility tests",
         Option('', '--ecma',
