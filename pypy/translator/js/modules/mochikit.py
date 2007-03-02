356858aa202adf02ab8a6561cc0c9639d9facfa4
Guido Wesdorp <guido@codespeak.net>
2007-03-02 11:45:22 +0000

    Trying to add some MochiKit.Signal stuff, not working yet.

--- pypy/translator/js/modules/mochikit.py
+++ pypy/translator/js/modules/mochikit.py
@@ -2,11 +2,16 @@
 """ mochikit wrappers
 """
 
-from pypy.rpython.extfunc import register_external
+from pypy.rpython.extfunc import _callable, register_external
+from pypy.translator.js.modules import dom
+
+# MochiKit.LoggingPane
 
 def createLoggingPane(var):
     pass
 register_external(createLoggingPane, args=[bool])
+
+# MochiKit.Logging
 
 def log(data):
     print data
@@ -28,10 +33,26 @@ def logFatal(data):
     print "FATAL:", data
 register_external(logFatal, args=None)
 
+# MochiKit.DOM
+
 def escapeHTML(data):
     return data
 register_external(escapeHTML, args=[str], result=str)
 
+# MochiKit.Base
+
 def serializeJSON(data):
     pass
 register_external(serializeJSON, args=None, result=str)
+
+# MochiKit.Signal
+
+def connect(src, signal, dest):
+    print 'connecting event %s' % (event,)
+register_external(connect, args=[dom.EventTarget, str, _callable([dom.Event])],
+                  result=int)
+
+def disconnect(id):
+    pass
+register_external(disconnect, args=[int])
+
