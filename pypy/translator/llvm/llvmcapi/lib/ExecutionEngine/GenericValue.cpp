972a4b800c4865231ec56d2150165fdfdf4fa488
Michael Hudson <micahel@gmail.com>
2007-03-03 11:38:28 +0000

    (rxe, mwh) remove one of the several attempts at a python extension for the LLVM code generation interface.

--- pypy/translator/llvm/llvmcapi/lib/ExecutionEngine/GenericValue.cpp
+++
@@ -1,7 +0,0 @@
-#include "llvm/ExecutionEngine/GenericValue.h"
-
-using namespace llvm;
-
-void*   GenericValue__init__() {
-    return new GenericValue();
-}
--- pypy/translator/llvm/llvmcapi/lib/VMCore/ModuleProvider.cpp
+++
@@ -1,7 +0,0 @@
-#include "llvm/ModuleProvider.h"
-
-using namespace llvm;
-
-void*   ExistingModuleProvider__init__(void* M) {
-    return new ExistingModuleProvider((Module*)M);
-}
