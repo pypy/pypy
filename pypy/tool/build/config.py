b863212412bf963c12ba5593bdafa78a4a7c0ba6
Guido Wesdorp <guido@codespeak.net>
2007-03-14 12:17:23 +0000

    oops :( (thanks cf!)

--- pypy/tool/build/config.py
+++ pypy/tool/build/config.py
@@ -3,7 +3,7 @@ import py
 packageparent = py.magic.autopath().dirpath().dirpath().dirpath().dirpath()
 
 # general settings, used by both server and client
-server = 'localhost'
+server = 'codespeak.net'
 webserver = ''
 port = 12321
 testport = 32123
