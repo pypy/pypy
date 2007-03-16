ca76adf1dfda2e1742827012cb7ef99a1c953d04
Armin Rigo <arigo@tunes.org>
2007-03-16 12:58:26 +0000

    Unused import.

--- pypy/translator/c/test/test_backendoptimized.py
+++ pypy/translator/c/test/test_backendoptimized.py
@@ -1,7 +1,6 @@
 import py
 from pypy.translator.c.test.test_typed import TestTypedTestCase as _TestTypedTestCase
 from pypy.translator.backendopt.all import backend_optimizations
-from pypy.rlib import objectmodel
 from pypy.rlib.rarithmetic import r_uint, r_longlong, r_ulonglong
 from pypy import conftest
 
