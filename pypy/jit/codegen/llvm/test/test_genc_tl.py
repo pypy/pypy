052ab4c0d762e741430525091a2139d5c5a11bc3
Armin Rigo <arigo@tunes.org>
2007-03-18 18:05:55 +0000

    Fix imports.

--- pypy/jit/codegen/llvm/test/test_genc_tl.py
+++ pypy/jit/codegen/llvm/test/test_genc_tl.py
@@ -1,12 +1,12 @@
 import py
-from pypy.jit.timeshifter.test import test_tl
+from pypy.jit.timeshifter.test import test_1tl
 from pypy.jit.codegen.llvm.test.test_genc_ts import LLVMTimeshiftingTestMixin
 
 
 py.test.skip("WIP")
 
 class TestTL(LLVMTimeshiftingTestMixin,
-              test_tl.TestTL):
+              test_1tl.TestTL):
 
     # for the individual tests see
     # ====> ../../../timeshifter/test/test_tl.py
