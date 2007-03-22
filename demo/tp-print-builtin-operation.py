7b48e6f683d8921aee897fa1529c79e1860b4f97
Holger Krekel <holger.krekel@gmail.com>
2007-03-22 07:44:12 +0000

    add a quick demo that prints operations on builtins

---
+++ demo/tp-print-builtin-operation.py
@@ -0,0 +1,27 @@
+"""
+
+This example transparently intercepts and shows operations on 
+builtin objects.  It requires the "--with-transparent-proxy" option. 
+
+"""
+
+from tputil import make_proxy 
+from __pypy__ import become
+
+def make_show_proxy(instance):
+    def controller(operation):
+        res = operation.delegate()
+        print "proxy sees:", operation, "result=%s" %(res,)
+        return res
+    tproxy = make_proxy(controller, obj=instance)
+    return tproxy
+
+mydict = make_show_proxy({}) 
+
+assert type(mydict) is dict      # this looks exactly like a dict 
+mydict['hello'] = 'world'        # will print __
+mydict[42] = 23 
+assert mydict.pop('hello') == 'world'
+assert mydict.popitem() == (42,23)
+
+
