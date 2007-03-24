5768eee5a639435bc650a9cd32ae033463f00ca5
Maciej Fijalkowski <fijall@gmail.com>
2007-03-24 09:32:36 +0000

    Add the docloader class which loads help messages and code snippets from docdir

---
+++ pypy/translator/js/examples/console/test/test_docloader.py
@@ -0,0 +1,16 @@
+
+import py
+from pypy.tool.udir import udir
+from pypy.translator.js.examples.console.docloader import LoadDoc
+
+def test_load_html():
+    tmpdir = udir.ensure("docloader", dir=1)
+    help = tmpdir.ensure("one.html").write("<a href='dupa'>%s</a>")
+    code = tmpdir.ensure("test_snippets.py").write(str(py.code.Source('''
+    class AppTest_one(object):
+        def test_snippet_1(self):
+            x = 1
+    ''')) + '\n')
+    ld = LoadDoc(docdir=tmpdir, consoles=['one'])
+    assert ld.get_html('one') == "<a href='dupa'>x = 1</a>"
+    assert ld.get_snippet('one', 0) == 'x = 1'
