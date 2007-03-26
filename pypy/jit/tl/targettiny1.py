270af4e7bdec8cf6cc38617cae16065b3f90ec6c
Armin Rigo <arigo@tunes.org>
2007-03-26 09:02:17 +0000

    The small example from draft-jit.txt.

---
+++ pypy/jit/tl/targettiny1.py
@@ -0,0 +1,29 @@
+from pypy.jit.tl import tiny1
+from pypy.jit.codegen.hlinfo import highleveljitinfo
+
+
+def entry_point(args):
+    highleveljitinfo.sys_executable = args[0]
+    if len(args) < 4:
+        print "Usage: %s bytecode x y" % (args[0],)
+        return 2
+    bytecode = args[1]
+    x = int(args[2])
+    y = int(args[3])
+    res = tiny1.ll_plus_minus(bytecode, x, y)
+    print res
+    return 0
+
+def target(driver, args):
+    return entry_point, None
+
+# ____________________________________________________________
+
+from pypy.jit.hintannotator.annotator import HintAnnotatorPolicy
+
+class MyHintAnnotatorPolicy(HintAnnotatorPolicy):
+    novirtualcontainer = True
+    oopspec = True
+
+def portal(driver):
+    return tiny2.interpret, MyHintAnnotatorPolicy()
