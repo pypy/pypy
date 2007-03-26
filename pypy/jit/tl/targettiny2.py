d8b33f422ce40a16554d926c22c57ce421741631
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2007-03-26 11:38:51 +0000

    Do arg checking in targettiny2.py to avoid segfaults.

--- pypy/jit/tl/targettiny2.py
+++ pypy/jit/tl/targettiny2.py
@@ -4,6 +4,10 @@ from pypy.jit.codegen.hlinfo import highleveljitinfo
 
 def entry_point(args):
     highleveljitinfo.sys_executable = args[0]
+    if len(args) < 3:
+        print "Invalid command line arguments."
+        print args[0] + " 'tiny2 program string' arg0 [arg1 [arg2 [...]]]"
+        return 1
     bytecode = [s for s in args[1].split(' ') if s != '']
     args = [tiny2.StrBox(arg) for arg in args[2:]]
     res = tiny2.interpret(bytecode, args)
