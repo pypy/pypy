29fe75a729ae412fe69baedc36580436d8dbe769
Guido Wesdorp <guido@codespeak.net>
2007-03-26 15:37:52 +0000

    Moved to /svn/build/buildtool/bin/startcompile.py.

--- pypy/bin/startcompile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import autopath
-from pypy.tool.build import config
-from pypy.tool.build.compile import main, getrequest
-
-request, foreground = getrequest(config)
-main(config, request, foreground)
