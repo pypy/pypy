cd240b882a1095b4c764a874c95efbacb609838f
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2007-04-15 22:48:52 +0000

    Skip test: readline cannot work on Windows

--- pypy/rpython/rctypes/tool/test/test_compilemodule.py
+++ pypy/rpython/rctypes/tool/test/test_compilemodule.py
@@ -1,4 +1,5 @@
 import py
+import sys
 from pypy.rpython.rctypes.tool.compilemodule import compilemodule
 
 def test_demo():
@@ -8,5 +9,8 @@ def test_demo():
     py.test.raises(mod.DemoError, mod.measuretime, -5, long)
 
 def test_readline():
+    if sys.platform == "win32":
+        py.test.skip("no readline on win32")
+    
     mod = compilemodule('readline')
     assert hasattr(mod, 'readline')    # XXX test more
