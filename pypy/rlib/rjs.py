f6e1c25c89f1b657a329747c93b98cf76fcd48c6
Maciej Fijalkowski <fijall@gmail.com>
2007-05-09 19:18:08 +0000

    Some reminescence of old js backend

--- pypy/rlib/rjs.py
+++
@@ -1,7 +0,0 @@
-"""
-This file defines utilities for calling native javascript code in an
-RPython-compliant way, intended for use by genjs.
-"""
-
-def jseval():
-    raise NotImplementedError("only works in translated versions")
