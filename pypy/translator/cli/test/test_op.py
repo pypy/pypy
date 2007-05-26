ce94e0fe67678c86ef1fd624238f1a4a5074fb36
Maciej Fijalkowski <fijall@gmail.com>
2007-05-26 10:16:06 +0000

    Make -k <tab> happy

--- pypy/translator/cli/test/test_op.py
+++ pypy/translator/cli/test/test_op.py
@@ -1,5 +1,7 @@
 from pypy.translator.cli.test.runtest import CliTest
 from pypy.translator.oosupport.test_template.operations import BaseTestOperations
 
+# ====> ../../oosupport/test_template/operations.py
+
 class TestOperations(CliTest, BaseTestOperations):
     pass
