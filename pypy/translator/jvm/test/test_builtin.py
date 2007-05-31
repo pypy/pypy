e86cc1baa2d176283fdb672f75d35f59c8ee47e9
Paul deGrandis <pdg@codespeak.net>
2007-05-31 16:56:21 +0000

    translator/jvm/test - fixed typos

--- pypy/translator/jvm/test/test_builtin.py
+++ pypy/translator/jvm/test/test_builtin.py
@@ -29,14 +29,14 @@ class TestJavaBuiltin(JvmTest, BaseTestRbuiltin):
         py.test.skip("ll_os_stat is not currently implemented in the Jvm backed")
     
     def test_builtin_math_floor(self):
-        py.test.skip("metavm.py needs to be updated to handle this math op")
+        py.test.skip("metavm.py needs to be updated to handle this math op; graphless extrernal")
         
     def test_builtin_math_fmod(self):
-        py.test.skip("metavm.py needs to be updated to handle this math op")
+        py.test.skip("metavm.py needs to be updated to handle this math op; graphless extrernal")
         
     def test_builtin_math_frexp(self):
-        py.test.skip("metavm.py needs to be updated to handle this math op")
+        py.test.skip("metavm.py needs to be updated to handle this math op; graphless extrernal")
         
     def test_builtin_math_modf(self):
-        py.test.skip("metavm.py needs to be updated to handle this math op")
+        py.test.skip("metavm.py needs to be updated to handle this math op; graphless extrernal")
         
--- pypy/translator/jvm/test/test_pbc.py
+++ pypy/translator/jvm/test/test_pbc.py
@@ -2,5 +2,5 @@ import py
 from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.rpython.test.test_rpbc import BaseTestRPBC
 
-class TestCliPBC(JvmTest, BaseTestRPBC):
+class TestJvmPBC(JvmTest, BaseTestRPBC):
     pass
