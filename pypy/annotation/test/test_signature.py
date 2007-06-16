7baf08508806b6a4e73e8c3186c6be91b295f58c
Samuele Pedroni <pedronis@lucediurna.net>
2007-06-16 17:37:29 +0000

    a skipped test about the broken interaction of genericallable and annotation()!

--- pypy/annotation/test/test_signature.py
+++ pypy/annotation/test/test_signature.py
@@ -1,5 +1,5 @@
-
-from pypy.annotation.signature import _annotation_key
+import py
+from pypy.annotation.signature import _annotation_key, annotation
 
 def test__annotation_key():
     assert _annotation_key([[str]]) == ('list', ('list', str))
@@ -7,3 +7,9 @@ def test__annotation_key():
     for i in ([[str]], [str], (int, int, {str: [str]})):
         assert hash(_annotation_key(i))
 
+def test_genericcallable():
+    py.test.skip("this two annotations should be equal - fix!")
+    from pypy.rpython.extfunc import genericcallable
+    s1 = annotation([genericcallable([str], int)])
+    s2 = annotation([genericcallable([str], int)])
+    assert s1 == s2
