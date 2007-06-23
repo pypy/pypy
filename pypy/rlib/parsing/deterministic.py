116f779ecb11a8b919ec99409feb855b70dd1d94
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-06-23 10:23:59 +0000

    ehem

--- pypy/rlib/parsing/deterministic.py
+++ pypy/rlib/parsing/deterministic.py
@@ -270,7 +270,7 @@ def recognize(runner, i):
         state_to_chars_sorted = state_to_chars.items()
         state_to_chars_sorted.sort()
         above = set()
-        for state, nextstates in state_to_chars:
+        for state, nextstates in state_to_chars_sorted:
             above.add(state)
             result.append("        if state == %s:" % (state, ))
             if state in self.final_states:
