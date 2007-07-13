97fdab75b235ad9a4a6d1857ed3deecb48547a3c
Samuele Pedroni <pedronis@lucediurna.net>
2007-07-13 15:16:19 +0000

    revert

--- lib-python/2.4.1/test/test_threading.py
+++ lib-python/2.4.1/test/test_threading.py
@@ -2,61 +2,54 @@
 
 # Create a bunch of threads, let each do some work, wait until all are done
 
-print 'a'
 from test.test_support import verbose
-print 'a'
 import random
-print 'a'
 import threading
-print 'a'
 import time
-print 'a'
 
-print 'a'
-def test_main():
-    # This takes about n/3 seconds to run (about n/3 clumps of tasks, times
-    # about 1 second per clump).
-    numtasks = 10
+# This takes about n/3 seconds to run (about n/3 clumps of tasks, times
+# about 1 second per clump).
+numtasks = 10
 
-    # no more than 3 of the 10 can run at once
-    sema = threading.BoundedSemaphore(value=3)
-    mutex = threading.RLock()
-    running = 0
+# no more than 3 of the 10 can run at once
+sema = threading.BoundedSemaphore(value=3)
+mutex = threading.RLock()
+running = 0
 
-    class TestThread(threading.Thread):
-        def run(self):
-            global running
-            delay = random.random() * 2
-            if verbose:
-                print 'task', self.getName(), 'will run for', delay, 'sec'
-            sema.acquire()
-            mutex.acquire()
-            running = running + 1
-            if verbose:
-                print running, 'tasks are running'
-            mutex.release()
-            time.sleep(delay)
-            if verbose:
-                print 'task', self.getName(), 'done'
-            mutex.acquire()
-            running = running - 1
-            if verbose:
-                print self.getName(), 'is finished.', running, 'tasks are running'
-            mutex.release()
-            sema.release()
+class TestThread(threading.Thread):
+    def run(self):
+        global running
+        delay = random.random() * 2
+        if verbose:
+            print 'task', self.getName(), 'will run for', delay, 'sec'
+        sema.acquire()
+        mutex.acquire()
+        running = running + 1
+        if verbose:
+            print running, 'tasks are running'
+        mutex.release()
+        time.sleep(delay)
+        if verbose:
+            print 'task', self.getName(), 'done'
+        mutex.acquire()
+        running = running - 1
+        if verbose:
+            print self.getName(), 'is finished.', running, 'tasks are running'
+        mutex.release()
+        sema.release()
 
-    threads = []
-    def starttasks():
-        for i in range(numtasks):
-            t = TestThread(name="<thread %d>"%i)
-            threads.append(t)
-            t.start()
+threads = []
+def starttasks():
+    for i in range(numtasks):
+        t = TestThread(name="<thread %d>"%i)
+        threads.append(t)
+        t.start()
 
-    starttasks()
+starttasks()
 
-    if verbose:
-        print 'waiting for all tasks to complete'
-    for t in threads:
-        t.join()
-    if verbose:
-        print 'all tasks done'
+if verbose:
+    print 'waiting for all tasks to complete'
+for t in threads:
+    t.join()
+if verbose:
+    print 'all tasks done'
