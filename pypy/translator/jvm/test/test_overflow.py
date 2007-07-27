4c022ef6df07e4d9ab01ab63d77138bd70240aa1
Antonio Cuni <anto.cuni@gmail.com>
2007-07-27 13:05:17 +0000

    make genjvm reuse test_overflow from oosupport

--- pypy/translator/jvm/test/test_overflow.py
+++ pypy/translator/jvm/test/test_overflow.py
@@ -1,49 +1,13 @@
-# check does not exist because it involves an extensive overhaul on runtest.  See CLI's runtest for an example.
-#from pypy.translator.jvm.test.runtest import check 
 import py
 from pypy.translator.jvm.test.runtest import JvmTest
-from pypy.rlib.rarithmetic import ovfcheck
+from pypy.translator.oosupport.test_template.overflow import BaseTestOverflow
 
-import sys
+class TestOverflow(BaseTestOverflow, JvmTest):
+    def test_sub(self):
+        py.test.skip('fixme!')
 
-class TestJvmOperation(JvmTest):
-    def test_recursive(self):
-        py.test.skip("JVM runtest lacks support to run these tests")
+    def test_lshift(self):
+        py.test.skip('fixme!')
 
-#def op_add(x, y):
-#    try:
-#        return ovfcheck(x+y)
-#    except OverflowError:
-#        return 42
-#
-#def op_sub(x, y):
-#    try:
-#        return ovfcheck(x-y)
-#    except OverflowError:
-#        return 42
-#
-#def op_mul(x, y):
-#    try:
-#        return ovfcheck(x*y)
-#    except OverflowError:
-#        return 42
-#
-#def op_lshift(x, y):
-#    try:
-#        return ovfcheck(x<<y)
-#    except OverflowError:
-#        return 42
-#
-#def op_neg(x):
-#    try:
-#        return ovfcheck(-x)
-#    except OverflowError:
-#        return 42
-#
-#def test_overflow():
-#    yield check, op_add, [int, int], (sys.maxint, 1)
-#    yield check, op_sub, [int, int], (-sys.maxint, 1)
-#    yield check, op_mul, [int, int], (sys.maxint/2 + 1, 2)
-#    yield check, op_lshift, [int, int], (2, 30)
-#    yield check, op_neg, [int], (-sys.maxint-1,)
+
 
