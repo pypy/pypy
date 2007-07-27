65860165a9ab9f21429a897f3d9a5ebdd2391bb6
Antonio Cuni <anto.cuni@gmail.com>
2007-07-27 12:57:32 +0000

    refactor cli/test/test_overflow to use the "new" 1.5 years old testing framework, so it can be easily reused by genjvm

--- pypy/translator/cli/test/test_overflow.py
+++ pypy/translator/cli/test/test_overflow.py
@@ -1,41 +1,5 @@
-from pypy.translator.cli.test.runtest import check
-from pypy.rlib.rarithmetic import ovfcheck
+from pypy.translator.cli.test.runtest import CliTest
+from pypy.translator.oosupport.test_template.overflow import BaseTestOverflow
 
-import sys
-
-def op_add(x, y):
-    try:
-        return ovfcheck(x+y)
-    except OverflowError:
-        return 42
-
-def op_sub(x, y):
-    try:
-        return ovfcheck(x-y)
-    except OverflowError:
-        return 42
-
-def op_mul(x, y):
-    try:
-        return ovfcheck(x*y)
-    except OverflowError:
-        return 42
-
-def op_lshift(x, y):
-    try:
-        return ovfcheck(x<<y)
-    except OverflowError:
-        return 42
-
-def op_neg(x):
-    try:
-        return ovfcheck(-x)
-    except OverflowError:
-        return 42
-
-def test_overflow():
-    yield check, op_add, [int, int], (sys.maxint, 1)
-    yield check, op_sub, [int, int], (-sys.maxint, 1)
-    yield check, op_mul, [int, int], (sys.maxint/2 + 1, 2)
-    yield check, op_lshift, [int, int], (2, 30)
-    yield check, op_neg, [int], (-sys.maxint-1,)
+class TestOverflow(BaseTestOverflow, CliTest):
+    pass
