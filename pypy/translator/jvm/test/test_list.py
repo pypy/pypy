616db1541a9b1fa7eadfcf86542e5bb958e29819
Paul deGrandis <pdg@codespeak.net>
2007-07-27 19:53:49 +0000

    translator/jvm - Sorry about breaking the build, I forgot to uncomment a skipped test in the test_list file while trying to add recursive list support (pdg)

--- pypy/translator/jvm/test/test_list.py
+++ pypy/translator/jvm/test/test_list.py
@@ -3,8 +3,8 @@ from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.rpython.test.test_rlist import BaseTestRlist
 
 class TestJvmList(JvmTest, BaseTestRlist):
-    #def test_recursive(self):
-    #    py.test.skip("JVM doesn't support recursive lists")
-    #
+    def test_recursive(self):
+        py.test.skip("JVM doesn't support recursive lists")
+    
     def test_getitem_exc(self):
         py.test.skip('fixme!')
