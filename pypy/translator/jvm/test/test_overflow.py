65167a1357541d7d93d63e5472e009b929637b28
Paul deGrandis <pdg@codespeak.net>
2007-07-27 18:04:26 +0000

    translator/jvm - fixed overflow detection for subtraction.  It was just a typo on my part.  Thanks anto for removing the need for check. (pdg)

--- pypy/translator/jvm/test/test_overflow.py
+++ pypy/translator/jvm/test/test_overflow.py
@@ -3,8 +3,8 @@ from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.translator.oosupport.test_template.overflow import BaseTestOverflow
 
 class TestOverflow(BaseTestOverflow, JvmTest):
-    def test_sub(self):
-        py.test.skip('fixme!')
+    #def test_sub(self):
+    #    py.test.skip('fixme!')
 
     def test_lshift(self):
         py.test.skip('fixme!')
