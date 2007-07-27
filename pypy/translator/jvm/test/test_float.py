9cfc159377fea0cde4c7a4dfda787ea967c86f12
Paul deGrandis <pdg@codespeak.net>
2007-07-27 19:44:18 +0000

    translator/jvm - Investigated why JVM has such an issue with uints.  It appears to be an I/O problem.  I wrote a new test to illustrate the point.  It is currently skipped. (pdg)

--- pypy/translator/jvm/test/test_float.py
+++ pypy/translator/jvm/test/test_float.py
@@ -25,11 +25,21 @@ class TestJvmFloat(JvmTest, BaseTestRfloat):
     def test_float_constant_conversions(self):
         py.test.skip("JVM backend lacks appropriate percision; 42.000000614400001 == 42.0")
     
-    #The JVM doesn't even have uints, these numbers are totally off
+    #The JVM doesn't even have uints, these numbers are totally off, I need to see if it's the way they
+    # are being rendered to standard out and parsed. see http://rafb.net/p/hsaV4b55.html for more info.
+    # Also, look below at the test_r_uint.
     def test_from_r_uint(self):
         py.test.skip("JVM backend lacks appropriate percision")
     
     #The jvm doesn't even have uints, these numbers are totally off
     def test_to_r_uint(self):
         py.test.skip("JVM backend lacks appropriate percision")
-        
+    
+    def test_r_uint(self):
+        py.test.skip("This illustrates the issue with parsing and handling of uints")
+        #import sys
+        #from pypy.rlib.rarithmetic import r_uint
+        #def ident(i): return i
+        #res = self.interpret(ident, [r_uint(sys.maxint + 1)])
+        #assert res == sys.maxint+1
+
