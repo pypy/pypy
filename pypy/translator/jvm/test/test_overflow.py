69b7eeb8e371b9539e80489b52e0362c43871d67
Paul deGrandis <pdg@codespeak.net>
2007-07-27 18:08:02 +0000

    translator/jvm - updated the test_overflow with the real reason for skipping lshift instead of fixme (pdg)

--- pypy/translator/jvm/test/test_overflow.py
+++ pypy/translator/jvm/test/test_overflow.py
@@ -3,11 +3,9 @@ from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.translator.oosupport.test_template.overflow import BaseTestOverflow
 
 class TestOverflow(BaseTestOverflow, JvmTest):
-    #def test_sub(self):
-    #    py.test.skip('fixme!')
 
     def test_lshift(self):
-        py.test.skip('fixme!')
+        py.test.skip('Shift is currently not implemented in src/PyPy.java because the C version interacts with Pypy directly, and it\'s not clear how to do that in Java')
 
 
 
