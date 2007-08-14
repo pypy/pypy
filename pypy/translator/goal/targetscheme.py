c44f5fe20ad15be5e91db2837d8d3cd68e350cfa
Jakub Gustak <jlg@usrsrc.net>
2007-08-14 10:38:44 +0000

    parser should return list or raise assertion; no translation warnings

--- pypy/translator/goal/targetscheme.py
+++ pypy/translator/goal/targetscheme.py
@@ -6,7 +6,8 @@ import autopath
 import sys
 from pypy.rlib.streamio import open_file_as_stream
 from pypy.lang.scheme.ssparser import parse
-from pypy.lang.scheme.object import SchemeQuit
+from pypy.rlib.parsing.makepackrat import BacktrackException
+from pypy.lang.scheme.object import SchemeQuit, ContinuationReturn
 from pypy.lang.scheme.execution import ExecutionContext
 
 # __________  Entry point  __________
@@ -14,13 +15,26 @@ from pypy.lang.scheme.execution import ExecutionContext
 
 def entry_point(argv):
     if len(argv) == 2:
-        f = open_file_as_stream(argv[1])
-        t = parse(f.readall())
+        code = open_file_as_stream(argv[1]).readall()
+        try:
+            t = parse(code)
+        except BacktrackException:
+            #(line, col) = e.error.get_line_column(code)
+            #expected = " ".join(e.error.expected)
+            print "parse error"
+            return 1
+
+        #this should not be necessary here
+        assert isinstance(t, list)
         ctx = ExecutionContext()
         try:
             for sexpr in t:
-                w_retval = sexpr.eval(ctx)
-                print w_retval.to_string()
+                try:
+                    w_retval = sexpr.eval(ctx)
+                    print w_retval.to_string()
+                except ContinuationReturn, e:
+                    print e.result.to_string()
+
         except SchemeQuit, e:
             return 0
 
