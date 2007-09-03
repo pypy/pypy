9cd807d5ed89bebb190460d946accd6032c489e3
Antonio Cuni <anto.cuni@gmail.com>
2007-09-03 09:37:04 +0000

    test_uint_to_float passes

--- pypy/translator/jvm/test/test_cast.py
+++ pypy/translator/jvm/test/test_cast.py
@@ -3,7 +3,4 @@ from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.translator.oosupport.test_template.cast import BaseTestCast
 
 class TestCast(BaseTestCast, JvmTest):
-
-    def test_uint_to_float(self):
-        # This is most likely with how we render uints when we print them, and they get parsed.
-        py.test.skip('Same issue seen in other tests with uints... 2147450880.0 == 2147483648.0')
+    pass
