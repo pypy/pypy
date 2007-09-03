15c99f32c95efb838c714b7e5f624dce50ccf0e0
Antonio Cuni <anto.cuni@gmail.com>
2007-09-03 15:38:59 +0000

    move some tests shared by cli and jvm to oosupport. More jvm tests pass.

--- pypy/translator/cli/test/test_class.py
+++ pypy/translator/cli/test/test_class.py
@@ -1,69 +1,9 @@
 import py
 from pypy.translator.cli.test.runtest import CliTest
-from pypy.rpython.test.test_rclass import BaseTestRclass
-from pypy.rpython.test.test_rspecialcase import BaseTestRspecialcase
+from pypy.translator.oosupport.test_template.class_ import BaseTestClass, BaseTestSpecialcase
 
-class TestCliClass(CliTest, BaseTestRclass):    
-    def test_abstract_method(self):
-        class Base:
-            pass
-        class A(Base):
-            def f(self, x):
-                return x+1
-        class B(Base):
-            def f(self, x):
-                return x+2
-        def call(obj, x):
-            return obj.f(x)
-        def fn(x):
-            a = A()
-            b = B()
-            return call(a, x) + call(b, x)
-        assert self.interpret(fn, [0]) == 3
+class TestCliClass(CliTest, BaseTestClass):    
+    pass
 
-    def test_abstract_method2(self):
-        class Root:
-            pass
-        class Class1(Root):
-            pass
-        class Derived(Class1):
-            x = 1
-        class Class2(Root):
-            x = 2
-        def get_x(obj):
-            return obj.x
-        def fn():
-            a = Derived()
-            b = Class2()
-            return get_x(a) + get_x(b)
-        assert self.interpret(fn, []) == 3
-
-    def test_same_name(self):
-        class A:
-            def __init__(self, x):
-                self.x = x
-        B=A
-        class A:
-            def __init__(self, y):
-                self.y = y
-        assert A is not B
-        assert A.__name__ == B.__name__
-        def fn(x, y):
-            obj1 = B(x)
-            obj2 = A(y)
-            return obj1.x + obj2.y
-        assert self.interpret(fn, [1, 2]) == 3
-
-    def test_ctr_location(self):
-        class A:
-            _annspecialcase_ = "specialize:ctr_location"
-            def __init__(self, x):
-                self.x = x
-        def fn(x, y):
-            a = A(x)
-            b = A(y)
-            return a.x + b.x
-        assert self.interpret(fn, [1, 2]) == 3
-
-class TestCliSpecialCase(CliTest, BaseTestRspecialcase):
+class TestCliSpecialCase(CliTest, BaseTestSpecialcase):
     pass
--- pypy/translator/jvm/test/test_class.py
+++ pypy/translator/jvm/test/test_class.py
@@ -1,52 +1,10 @@
 import py
 from pypy.translator.jvm.test.runtest import JvmTest
-from pypy.rpython.test.test_rclass import BaseTestRclass
-from pypy.rpython.test.test_rspecialcase import BaseTestRspecialcase
+from pypy.translator.oosupport.test_template.class_ import BaseTestClass, BaseTestSpecialcase
 
-class TestJvmClass(JvmTest, BaseTestRclass):    
+class TestJvmClass(JvmTest, BaseTestClass):    
     def test_overridden_classattr_as_defaults(self):
         py.test.skip("JVM doesn't support overridden default value yet")
 
-    def test_abstract_method(self):
-        class Base(object):
-            pass
-        class A(Base):
-            def f(self, x):
-                return x+1
-        class B(Base):
-            def f(self, x):
-                return x+2
-        def call(obj, x):
-            return obj.f(x)
-        def fn(x):
-            a = A()
-            b = B()
-            return call(a, x) + call(b, x)
-        assert self.interpret(fn, [0]) == 3
-
-    def test_abstract_method2(self):
-        class Root(object):
-            pass
-        class Class1(Root):
-            pass
-        class Derived(Class1):
-            x = 1
-        class Class2(Root):
-            x = 2
-        def get_x(obj):
-            return obj.x
-        def fn():
-            derived = Derived()
-            cls2 = Class2()
-            return get_x(derived) + get_x(cls2)
-        assert self.interpret(fn, []) == 3
-
-    def test_same_name(self):
-        py.test.skip("JVM doesn't support classes with the same name")
-
-    def test_ctr_location(self):
-        py.test.skip("Ask cuni if this applies to JVM -I don't think so")
-
-
-#class TestCliSpecialCase(CliTest, BaseTestRspecialcase):
-#    pass
+class TestJvmSpecialCase(JvmTest, BaseTestSpecialcase):
+    pass
