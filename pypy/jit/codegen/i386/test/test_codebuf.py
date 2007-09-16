df6b836a540e1bb69d91094230e2b26dd0fbd830
Armin Rigo <arigo@tunes.org>
2007-09-16 09:52:57 +0000

    An extra test for 'free', which is otherwise only called by a __del__ (exceptions there are ignored and don't show up as test failures).

--- pypy/jit/codegen/i386/test/test_codebuf.py
+++ pypy/jit/codegen/i386/test/test_codebuf.py
@@ -1,6 +1,15 @@
 from pypy.jit.codegen.i386.ri386 import *
-from pypy.jit.codegen.i386.codebuf import MachineCodeBlock
+from pypy.jit.codegen.i386.codebuf import MachineCodeBlock, memhandler
 
+
+def test_alloc_free():
+    map_size = 65536
+    data = memhandler.alloc(map_size)
+    for i in range(0, map_size, 171):
+        data[i] = chr(i & 0xff)
+    for i in range(0, map_size, 171):
+        assert data[i] == chr(i & 0xff)
+    memhandler.free(data, map_size)
 
 def test_machinecodeblock():
     mc = MachineCodeBlock(4096)
