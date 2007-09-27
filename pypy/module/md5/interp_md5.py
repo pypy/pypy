3da050715c69df7d241bd93d863af1c96d2ce6cb
Armin Rigo <arigo@tunes.org>
2007-09-27 16:51:39 +0000

    Checking this in, but I'm not getting why annotation fails...

--- pypy/module/md5/interp_md5.py
+++ pypy/module/md5/interp_md5.py
@@ -14,6 +14,9 @@ class W_MD5(Wrappable, rmd5.RMD5):
         self._init()
         self.update(initialdata)
 
+    def update_w(self, string):
+        self.update(string)
+
     def digest_w(self):
         return self.space.wrap(self.digest())
 
@@ -32,16 +35,15 @@ def W_MD5___new__(space, w_subtype, initialdata=''):
     md5 = space.interp_w(W_MD5, w_md5)
     W_MD5.__init__(md5, space, initialdata)
     return w_md5
-W_MD5___new__.unwrap_spec = [ObjSpace, W_Root, str]
 
 
 W_MD5.typedef = TypeDef(
     'MD5Type',
-    __new__   = interp2app(W_MD5___new__),
-    update    = interp2app(W_MD5.update, unwrap_spec=['self', str]),
-    digest    = interp2app(W_MD5.digest_w),
-    hexdigest = interp2app(W_MD5.hexdigest_w),
-    copy      = interp2app(W_MD5.copy_w),
+    __new__   = interp2app(W_MD5___new__, unwrap_spec=[ObjSpace, W_Root, str]),
+    update    = interp2app(W_MD5.update_w, unwrap_spec=['self', str]),
+    digest    = interp2app(W_MD5.digest_w, unwrap_spec=['self']),
+    hexdigest = interp2app(W_MD5.hexdigest_w, unwrap_spec=['self']),
+    copy      = interp2app(W_MD5.copy_w, unwrap_spec=['self']),
     __doc__   = """md5(arg) -> return new md5 object.
 
 If arg is present, the method call update(arg) is made.""")
