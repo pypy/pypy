d81a1c6cc5b532ee17d466de34fe5dcf6cb5f0e5
Maciej Fijalkowski <fijall@gmail.com>
2007-10-01 08:53:54 +0000

    Enable tproxy by default

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -115,7 +115,7 @@ pypy_optiondescription = OptionDescription("objspace", "Object Space Options", [
 
     OptionDescription("std", "Standard Object Space Options", [
         BoolOption("withtproxy", "support transparent proxies",
-                   default=False),
+                   default=True),
 
         BoolOption("withsmallint", "use tagged integers",
                    default=False,
