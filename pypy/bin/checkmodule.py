26d88f0315e7890fd76c375152df888210b662a8
Antonio Cuni <anto.cuni@gmail.com>
2007-10-16 09:32:20 +0000

    add support for -h and --help options

--- pypy/bin/checkmodule.py
+++ pypy/bin/checkmodule.py
@@ -24,6 +24,9 @@ def main(argv):
         if len(argv) == 2:
             backend = 'cli'
             modname = argv[1]
+            if modname in ('-h', '--help'):
+                print >> sys.stderr, __doc__
+                sys.exit(0)
         else:
             _, b, backend, modname = argv
             assert b == '-b'
