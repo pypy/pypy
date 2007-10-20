71c03d794ba3ef00301d716df4b69855a1b4ab3e
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-10-20 17:33:48 +0000

    same test (passing) for transformed GCs

--- pypy/rpython/memory/test/test_transformed_gc.py
+++ pypy/rpython/memory/test/test_transformed_gc.py
@@ -178,6 +178,21 @@ class GenericGCTests(GCTest):
         heap_size = self.heap_usage(statistics)
         assert heap_size < 16000 * INT_SIZE / 4 # xxx
 
+    def test_nongc_static_root(self):
+        from pypy.rpython.lltypesystem import lltype
+        T1 = lltype.GcStruct("C", ('x', lltype.Signed))
+        T2 = lltype.Struct("C", ('p', lltype.Ptr(T1)))
+        static = lltype.malloc(T2, immortal=True)
+        def f():
+            t1 = lltype.malloc(T1)
+            t1.x = 42
+            static.p = t1
+            llop.gc__collect(lltype.Void)
+            return static.p.x
+        run = self.runner(f, nbargs=0)
+        res = run([])
+        assert res == 42
+
     def test_finalizer(self):
         class B(object):
             pass
