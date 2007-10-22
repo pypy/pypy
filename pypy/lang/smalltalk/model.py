deb9ed781060d19b02362a40304b8832432f5561
Armin Rigo <arigo@tunes.org>
2007-10-22 15:52:31 +0000

    (all) More of the basic object model: NamedVarsObject, for objects with a fixed-length list of slots (i.e. most Squeak objects).

--- pypy/lang/smalltalk/model.py
+++ pypy/lang/smalltalk/model.py
@@ -3,11 +3,34 @@ class W_Object:
     def __init__(self, w_class):
         self.w_class = w_class
 
+class W_NamedVarsObject(W_Object):
+    def __init__(self, w_class):
+        W_Object.__init__(self, w_class)
+        self.named_vars = [None] * w_class.namedvarssize
+
+    def getnamedvar(self, index):
+        return self.named_vars[index]
+
+    def setnamedvar(self, index, w_value):
+        self.named_vars[index] = w_value
+
+
+NO_INDEXED   = 0
+OBJ_INDEXED  = 1
+BYTE_INDEXED = 2
+WORD_INDEXED = 3
+
 class W_Class(W_Object):
-    def __init__(self, w_class, w_superclass):
+    def __init__(self, w_class, w_superclass, namedvarssize=0,
+                 indexed=NO_INDEXED):
         W_Object.__init__(self, w_class)
         self.w_superclass = w_superclass
         self.methoddict = {}
+        self.namedvarssize = namedvarssize
+        self.indexed = indexed
 
     def new(self):
-        return W_Object(self)
+        if self.indexed == NO_INDEXED:
+            return W_NamedVarsObject(self)
+        else:
+            raise NotImplementedError(self.indexed)
--- pypy/lang/smalltalk/test/test_model.py
+++ pypy/lang/smalltalk/test/test_model.py
@@ -1,3 +1,4 @@
+import py
 from pypy.lang.smalltalk import model
 
 def test_new():
@@ -5,3 +6,13 @@ def test_new():
     w_myinstance = w_mycls.new()
     assert isinstance(w_myinstance, model.W_Object)
     assert w_myinstance.w_class is w_mycls
+
+def test_new_namedvars():
+    w_mycls = model.W_Class(None, None, 3)
+    w_myinstance = w_mycls.new()
+    assert isinstance(w_myinstance, model.W_NamedVarsObject)
+    assert w_myinstance.w_class is w_mycls
+    assert w_myinstance.getnamedvar(0) is None
+    py.test.raises(IndexError, lambda: w_myinstance.getnamedvar(3))
+    w_myinstance.setnamedvar(1, w_myinstance)
+    assert w_myinstance.getnamedvar(1) is w_myinstance
