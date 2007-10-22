7a8f2233f1ddfddd4d41273a9ff2de20c51811e2
Armin Rigo <arigo@tunes.org>
2007-10-22 16:02:31 +0000

    (all) Implemented objects with indexed variables (i.e. var-sized, with the size choosen at instantiation time).

--- pypy/lang/smalltalk/model.py
+++ pypy/lang/smalltalk/model.py
@@ -14,6 +14,18 @@ class W_NamedVarsObject(W_Object):
     def setnamedvar(self, index, w_value):
         self.named_vars[index] = w_value
 
+class W_IndexedNamedVarsObject(W_NamedVarsObject):
+    def __init__(self, w_class, size):
+        W_NamedVarsObject.__init__(self, w_class)
+        self.indexed_vars = [None] * size
+
+    def getindexedvar(self, index):
+        return self.indexed_vars[index]
+
+    def setindexedvar(self, index, w_value):
+        self.indexed_vars[index] = w_value
+
+# ____________________________________________________________ 
 
 NO_INDEXED   = 0
 OBJ_INDEXED  = 1
@@ -29,8 +41,11 @@ class W_Class(W_Object):
         self.namedvarssize = namedvarssize
         self.indexed = indexed
 
-    def new(self):
+    def new(self, size=0):
         if self.indexed == NO_INDEXED:
+            assert size == 0
             return W_NamedVarsObject(self)
+        elif self.indexed == OBJ_INDEXED:
+            return W_IndexedNamedVarsObject(self, size)
         else:
             raise NotImplementedError(self.indexed)
--- pypy/lang/smalltalk/test/test_model.py
+++ pypy/lang/smalltalk/test/test_model.py
@@ -16,3 +16,17 @@ def test_new_namedvars():
     py.test.raises(IndexError, lambda: w_myinstance.getnamedvar(3))
     w_myinstance.setnamedvar(1, w_myinstance)
     assert w_myinstance.getnamedvar(1) is w_myinstance
+
+def test_new_indexednamedvars():
+    w_mycls = model.W_Class(None, None, 3, model.OBJ_INDEXED)
+    w_myinstance = w_mycls.new(2)
+    assert isinstance(w_myinstance, model.W_IndexedNamedVarsObject)
+    assert w_myinstance.w_class is w_mycls
+    assert w_myinstance.getnamedvar(0) is None
+    py.test.raises(IndexError, lambda: w_myinstance.getnamedvar(3))
+    w_myinstance.setnamedvar(1, w_myinstance)
+    assert w_myinstance.getnamedvar(1) is w_myinstance
+    assert w_myinstance.getindexedvar(1) is None
+    py.test.raises(IndexError, lambda: w_myinstance.getindexedvar(2))
+    w_myinstance.setindexedvar(0, w_myinstance)
+    assert w_myinstance.getindexedvar(0) is w_myinstance
