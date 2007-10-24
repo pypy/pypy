fa9b68f32cc6208bb64d5b565dcd371a21793f81
Toon Verwaest <tverwaes@codespeak.net>
2007-10-24 17:16:06 +0000

    testing methods

--- pypy/lang/smalltalk/tools/analyseimage.py
+++ pypy/lang/smalltalk/tools/analyseimage.py
@@ -2,6 +2,7 @@ import autopath
 import py
 from pypy.lang.smalltalk import squeakimage as sq
 from pypy.lang.smalltalk import model as sqm
+from pypy.lang.smalltalk import interpreter as sqi
 
 mini_image = py.magic.autopath().dirpath().dirpath().join('mini.image')
 
@@ -11,16 +12,44 @@ def test_miniimageexists():
 def get_miniimage():
     return sq.ImageReader(sq.Stream(mini_image.open()))
 
-def printStringsInImage():
+def create_squeakimage():
     example = get_miniimage()
     example.initialize()
     
     image = sq.SqueakImage()
-    image.from_reader(example)    
-    
+    image.from_reader(example)
+    return image
+
+def printStringsInImage():
+    image = create_squeakimage()
     for each in image.objects:
         if isinstance(each,sqm.W_BytesObject):
-          print ''.join(each.bytes)
+          print repr(''.join(each.bytes))
+
+def testCompiledMethods():
+    image = create_squeakimage()
+    amethod = None
+    skip = 0
+
+    for each in image.objects:
+        if isinstance(each,sqm.W_CompiledMethod):
+            if (amethod == None and
+                each.argsize == 0 and
+                each.tempsize == 0 and
+                each.primitive == 1 and skip >= 0):
+                amethod = each
+            else:
+                skip += 1
+            #print "%d %d %d" % (each.argsize, each.tempsize, each.primitive)
+    
+                        # receiver, arguments
+    w_frame = amethod.createFrame("receiver", [])
+    interp = sqi.Interpreter()
+    interp.activeContext = w_frame
+    interp.interpret()
+
+def test_do():
+    testCompiledMethods()
 
 if __name__ == '__main__':
-    printStringsInImage()
+    test_do()
