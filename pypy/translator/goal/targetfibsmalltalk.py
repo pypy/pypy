945c83fbdf8d621ede9f5381c53e952914a2a93d
Armin Rigo <arigo@tunes.org>
2007-10-25 18:42:26 +0000

    Fix the target.

--- pypy/translator/goal/targetfibsmalltalk.py
+++ pypy/translator/goal/targetfibsmalltalk.py
@@ -9,21 +9,33 @@ mockclass = classtable.bootstrap_class
 def new_interpreter(bytes):
     assert isinstance(bytes, str)
     w_method = model.W_CompiledMethod(0, bytes=bytes)
-    w_frame = w_method.createFrame(objtable.w_nil, [])
+    w_frame = w_method.create_frame(objtable.w_nil, [])
     interp = interpreter.Interpreter()
     interp.w_active_context = w_frame
     return interp
 
+def build():
+    bytecode = ''.join(map(chr, [ 16, 119, 178, 154, 118, 164, 11, 112, 16, 118, 177, 224, 112, 16, 119, 177, 224, 176, 124 ]))
+    shadow = mockclass(0).as_class_get_shadow()
+    method = model.W_CompiledMethod(1, bytecode, 1)
+    method.literals = fakeliterals("fib:")
+    shadow.installmethod("fib:", method)
+    w_object = shadow.new()
+    interp = new_interpreter(sendLiteralSelectorBytecode(16) + returnTopFromMethod)
+    interp.w_active_context.w_method().literals = fakeliterals("fib:")
+    return interp, w_object
 
-bytecode = ''.join(map(chr, [ 16, 119, 178, 154, 118, 164, 11, 112, 16, 118, 177, 224, 112, 16, 119, 177, 224, 176, 124 ]))
-shadow = mockclass(0).as_class_get_shadow()
-method = model.W_CompiledMethod(1, bytecode, 1)
-method.literals = fakeliterals("fib:")
-shadow.installmethod("fib:", method)
-w_object = shadow.new()
-interp = new_interpreter(sendLiteralSelectorBytecode(16) + returnTopFromMethod)
-interp.w_active_context.method.literals = fakeliterals("fib:")
+def check_me():
+    interp, w_object = build()
+    interp.w_active_context.push(w_object)
+    interp.w_active_context.push(wrap_int(8))
+    result = interp.interpret()
+    assert primitives.unwrap_int(result) == 34
+    print "check_me() ok"
+check_me()
 
+
+interp, w_object = build()
 
 def entry_point(argv):
     if len(argv) > 1:
