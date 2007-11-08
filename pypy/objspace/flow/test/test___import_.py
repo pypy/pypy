acf9cd9e1ea134bd12e2e241cbc9482cd1313124
Christian Tismer <tismer@stackless.com>
2007-11-08 03:55:24 +0000

    making a trivial test a bit less trivial

--- pypy/objspace/flow/test/test___import_.py
+++ pypy/objspace/flow/test/test___import_.py
@@ -4,9 +4,9 @@ def test___import_():
     import os
     mod = os.name
     raises(TypeError, __import__, ())
-    x = __import__(mod)
-    x = __import__(mod, None)
-    x = __import__(mod, None, None)
-    x = __import__(mod, None, None, None)
+    assert __import__(mod).__name__ == os.name
+    assert __import__(mod, None).__name__ == os.name
+    assert __import__(mod, None, None).__name__ == os.name
+    assert __import__(mod, None, None, None).__name__ == os.name
     raises(TypeError, __import__, (mod, None, None, None, None))
     # XXX this will have to be adjusted for Python 2.5 pretty soon-ish :-)
