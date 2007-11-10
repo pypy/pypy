c31f8252fc299659bf6676892fdf99e1da2e28d9
Richard Emslie <richardemslie@gmail.com>
2007-11-10 21:26:17 +0000

    unskip a test

--- pypy/translator/llvm/test/test_rstr.py
+++ pypy/translator/llvm/test/test_rstr.py
@@ -3,13 +3,12 @@ from pypy.rpython.test.test_rstr import BaseTestRstr
 from pypy.translator.llvm.test.runtest import *
 
 class TestLLVMStr(LLVMTest, BaseTestRstr):
+    EMPTY_STRING_HASH = -1
+
     def test_int(self):
         py.test.skip('XXX special case me')
 
     def test_float(self):
-        py.test.skip('XXX special case me')
-
-    def test_hash(self):
         py.test.skip('XXX special case me')
 
 
