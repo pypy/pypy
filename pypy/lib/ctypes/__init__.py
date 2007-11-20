6c3404dcdc5330e155082ce2f2573ac23089fed1
Maciej Fijalkowski <fijall@gmail.com>
2007-11-20 12:25:52 +0000

    Shuffle stuff around

--- pypy/lib/ctypes/__init__.py
+++ pypy/lib/ctypes/__init__.py
@@ -28,69 +28,8 @@ class _CFuncPtr(object):
         self._handle = self.lib._handle.ptr(self.name, llargs,
                                            self.restype._type_)
 
-class _SimpleCData(object):
-    def __init__(self, value):
-        self.value = value
-
-class c_ushort(_SimpleCData):
-    _type_ = 'H'
-
-class c_double(_SimpleCData):
-    _type_ = 'd'
-
-class c_ubyte(_SimpleCData):
-    _type_ = 'B'
-
-class c_float(_SimpleCData):
-    _type_ = 'f'
-
-class c_ulong(_SimpleCData):
-    _type_ = 'L'
-
-class c_short(_SimpleCData):
-    _type_ = 'h'
-
-class c_ubyte(_SimpleCData):
-    _type_ = 'b'
-
-class c_byte(_SimpleCData):
-    _type_ = 'B'
-
-class c_char(_SimpleCData):
-    _type_ = 'c'
-
-class c_long(_SimpleCData):
-    _type_ = 'l'
-
-class c_ulonglong(_SimpleCData):
-    _type_ = 'Q'
-
-class c_longlong(_SimpleCData):
-    _type_ = 'q'
-
-class c_int(_SimpleCData):
-    _type_ = 'i'
-
-class c_uint(_SimpleCData):
-    _type_ = 'I'
-
-class c_double(_SimpleCData):
-    _type_ = 'd'
-
-class c_float(_SimpleCData):
-    _type_ = 'f'
-
-c_size_t = c_ulong # XXX
-
-class POINTER(object):
-    def __init__(self, cls):
-        self.cls = cls
-
-class c_void_p(_SimpleCData):
-    _type_ = 'P'
-
-class c_char_p(_SimpleCData):
-    _type_ = 's'
+from ctypes._basics import *
+from ctypes._pointers import POINTER
 
 class CDLL(object):
     """An instance of this class represents a loaded dll/shared
