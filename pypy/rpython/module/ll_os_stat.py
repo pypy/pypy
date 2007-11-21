b473cdf78c1f43bfe5d7e450c37ed623a67dbe94
Christian Tismer <tismer@stackless.com>
2007-11-21 12:27:40 +0000

    fixing includes, to make windows build, again

--- pypy/rpython/module/ll_os_stat.py
+++ pypy/rpython/module/ll_os_stat.py
@@ -138,7 +138,7 @@ class MakeStatResultEntry(extregistry.ExtRegistryEntry):
 
 if sys.platform.startswith('win'):
     _name_struct_stat = '_stati64'
-    INCLUDES = []
+    INCLUDES = ['sys/types.h', 'sys/stat.h']
 else:
     _name_struct_stat = 'stat'
     INCLUDES = ['sys/types.h', 'sys/stat.h', 'unistd.h']
