a835dd46d277f1b869b96c48ee636b60e8fdb1a5
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2007-11-26 20:51:37 +0000

    Fix test: minor typo, remove skip.

--- pypy/translator/llvm/test/test_standalone.py
+++ pypy/translator/llvm/test/test_standalone.py
@@ -79,11 +79,10 @@ def test__del__():
     #assert 0 < res <= 84 
 
 def test_strtod():
-    py.test.skip("Explodes")
     def entry_point(args):
         print float(args[1])
         return 0
 
     exe_name = 'test_strtod'
     compile_standalone(entry_point, exe_name=exe_name)
-    data = cmdexed(exe_name, '3.13e1')
+    data = cmdexec(exe_name, '3.13e1')
