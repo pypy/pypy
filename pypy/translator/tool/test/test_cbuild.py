c0b4d8a36f658420b4d2338a3977f57ebd4b93a5
Armin Rigo <arigo@tunes.org>
2007-12-01 10:42:42 +0000

    This doesn't make much sense any more.

--- pypy/translator/tool/test/test_cbuild.py
+++ pypy/translator/tool/test/test_cbuild.py
@@ -20,26 +20,6 @@ def test_simple_executable():
     testexec = build_executable([t], eci)
     out = py.process.cmdexec(testexec)
     assert out.startswith('hello world')
-    
-def test_compile_threads():
-    if sys.platform == 'nt':
-        py.test.skip("Linux-specific test")
-    try:
-        import ctypes
-    except ImportError:
-        py.test.skip("Need ctypes for that test")
-    from pypy.tool.autopath import pypydir
-    pypydir = py.path.local(pypydir)
-    csourcedir = pypydir.join('translator', 'c', 'src')
-    include_dirs = [str(csourcedir.dirpath())]
-    files = [csourcedir.join('thread.c')]
-    eci = ExternalCompilationInfo(
-        include_dirs=include_dirs,
-        libraries=['pthread']
-    )
-    mod = compile_c_module(files, '_thread', eci)
-    cdll = ctypes.CDLL(mod)
-    assert hasattr(cdll, 'RPyThreadLockInit')
 
 class TestEci:
     def setup_class(cls):
