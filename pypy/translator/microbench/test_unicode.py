5fac204cfef175dabe0fc9e58d893acda1412ed8
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-12-03 16:43:14 +0000

    some more find microbenches. python2.4 is marginally faster than pypy here and python2.5 is tons faster. we should steal their algorithm:
    http://effbot.org/zone/stringlib.htm

--- pypy/translator/microbench/test_unicode.py
+++ pypy/translator/microbench/test_unicode.py
@@ -3,3 +3,38 @@ N = (2 ** 19 - 1)
 u1 = (u"not the xyz" * N)
 def test_find_worstcase():
     u1.find(u"not there")
+
+def test_count_worstcase():
+    u1.count(u"not there")
+
+u2 = (u"aaa" * 1000)
+def test_find_pattern16():
+    i = 1
+    while i < N:
+        i += 1
+        u2.find(u"bbbbbbbbbbbbbbbb")
+
+def test_find_pattern8():
+    i = 1
+    while i < N:
+        i += 1
+        u2.find(u"bbbbbbbb")
+
+def test_find_pattern4():
+    i = 1
+    while i < N:
+        i += 1
+        u2.find(u"bbbb")
+
+def test_find_pattern2():
+    i = 1
+    while i < N:
+        i += 1
+        u2.find(u"bb")
+
+
+def test_find_pattern1():
+    i = 1
+    while i < N:
+        i += 1
+        u2.find(u"b")
