036a397d6b1d5775c274e1d8844ac90d9349a1b5
Carl Friedrich Bolz <cfbolz@gmx.de>
2007-12-03 16:55:01 +0000

    a find microbench where pypy is both faster than cpy 2.4 and 2.5. I think I am hitting the case here where our algorithm is O(len(text)) and theirs is O(len(text) * len(pattern))

--- pypy/translator/microbench/test_unicode.py
+++ pypy/translator/microbench/test_unicode.py
@@ -38,3 +38,11 @@ def test_find_pattern1():
     while i < N:
         i += 1
         u2.find(u"b")
+
+u3 = u"baaaaaaaaaaaaaaa" * 1000
+def test_bad_case_python2_5():
+    p = u"a" * 16 + u"b" + u"a" * 16
+    i = 0
+    while i < 10000:
+        i += 1
+        u3.find(p)
