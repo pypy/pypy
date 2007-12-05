67cb53d30986ae233630e2dc3c38b5c37b417855
Maciej Fijalkowski <fijall@gmail.com>
2007-12-05 11:57:04 +0000

    Forgotten test.

---
+++ pypy/rlib/test/test_debug.py
@@ -0,0 +1,29 @@
+
+import py
+from pypy.rlib.debug import check_annotation
+from pypy.rpython.test.test_llinterp import interpret
+
+def test_check_annotation():
+    class Error(Exception):
+        pass
+    
+    def checker(ann, bk):
+        from pypy.annotation.model import SomeList, SomeInteger
+        if not isinstance(ann, SomeList):
+            raise Error()
+        if not isinstance(ann.listdef.listitem.s_value, SomeInteger):
+            raise Error()
+    
+    def f(x):
+        result = [x]
+        check_annotation(result, checker)
+        return result
+
+    interpret(f, [3])
+
+    def g(x):
+        check_annotation(x, checker)
+        return x
+
+    py.test.raises(Error, "interpret(g, [3])")
+
