eabb4bdddeb7e19b558da7a3c404afd85f6d7539
Maciej Fijalkowski <fijall@gmail.com>
2007-12-10 14:07:28 +0000

    typo

--- pypy/rlib/parsing/main.py
+++ pypy/rlib/parsing/main.py
@@ -7,7 +7,7 @@ def make_parser_from_file(filename):
     try:
         t = py.path.local(filename).read(mode='U')
         regexs, rules, ToAST = parse_ebnf(t)
-    except ParserError, e:
+    except ParseError, e:
         print e.nice_error_message(filename=filename, source=t)
         raise
     return make_parse_function(regexs, rules, eof=True)
