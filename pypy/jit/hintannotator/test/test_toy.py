328f3bb5105cea75de0d30901b0157f702e8f461
Antonio Cuni <anto.cuni@gmail.com>
2007-12-22 10:54:14 +0000

    hoorray, all relevant tests pass out of the box. We can consider the hintannotator to be fully ported to ootype now :-)

--- pypy/jit/hintannotator/test/test_toy.py
+++ pypy/jit/hintannotator/test/test_toy.py
@@ -1,3 +1,4 @@
+import py
 from pypy.jit.hintannotator.test.test_annotator import AbstractAnnotatorTest
 from pypy.jit.hintannotator.test.test_annotator import P_OOPSPEC, P_OOPSPEC_NOVIRTUAL
 
@@ -22,6 +23,8 @@ class BaseToyTest(AbstractAnnotatorTest):
 class TestLLType(BaseToyTest):
     type_system = 'lltype'
 
-## XXX: all tests fail :-(
-##class TestOOType(BaseToyTest):
-##    type_system = 'ootype'
+class TestOOType(BaseToyTest):
+    type_system = 'ootype'
+
+    def test_hannotate_tl(self):
+        py.test.skip('fixme? (This policy is not relevant for now)')
