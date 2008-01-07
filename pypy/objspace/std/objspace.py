f197a8ea08cf841547430ce493eb48229a0091f1
Armin Rigo <arigo@tunes.org>
2008-01-07 13:21:21 +0000

    Fix an XXX before I forget about it.

--- pypy/objspace/std/objspace.py
+++ pypy/objspace/std/objspace.py
@@ -622,12 +622,17 @@ class StdObjSpace(ObjSpace, DescrOperation):
             self.setitem(w_obj, w_key, w_value)
 
     def getindex_w(self, w_obj, w_exception, objdescr=None):
-        # performance shortcut for W_IntObject
-        # XXX we should also have one for W_SmallIntObject, I guess
-        if type(w_obj) is W_IntObject:
-            return w_obj.intval
+        # Performance shortcut for the common case of w_obj being an int.
+        # If withsmallint is disabled, we check for W_IntObject.
+        # If withsmallint is enabled, we only check for W_SmallIntObject - it's
+        # probably not useful to have a shortcut for W_IntObject at all then.
+        if self.config.objspace.std.withsmallint:
+            if type(w_obj) is W_SmallIntObject:
+                return w_obj.intval
         else:
-            return ObjSpace.getindex_w(self, w_obj, w_exception, objdescr)
+            if type(w_obj) is W_IntObject:
+                return w_obj.intval
+        return ObjSpace.getindex_w(self, w_obj, w_exception, objdescr)
 
     def call_method(self, w_obj, methname, *arg_w):
         if self.config.objspace.opcodes.CALL_METHOD:
