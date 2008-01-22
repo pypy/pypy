7ef72f9ebf502f0af3d4c5c211f57269bae19260
Maciej Fijalkowski <fijall@gmail.com>
2008-01-22 11:34:33 +0000

    * Remove unnecessary stuff from here * EXPLODE when calling nonimplemented function

--- pypy/lib/_ctypes/dummy.py
+++ pypy/lib/_ctypes/dummy.py
@@ -5,13 +5,8 @@ class Union(object):
     __metaclass__ = UnionType
 
 def dummyfunc(*args, **kwargs):
-    return None
+    EXPLODE
 
-addressof = dummyfunc
-alignment = dummyfunc
 resize = dummyfunc
-_memmove_addr = dummyfunc
 _memset_addr = dummyfunc
-_string_at_addr = dummyfunc
-_cast_addr = dummyfunc
 
