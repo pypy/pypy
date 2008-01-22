4e40bd062a7ce94de61e45a73b89650a115952ea
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-01-22 22:57:11 +0000

    some failing test lines (made unreachable by a return) that show the problem of not being able to fall back sanely from within the reflective space.

--- pypy/objspace/test/test_reflective.py
+++ pypy/objspace/test/test_reflective.py
@@ -128,3 +128,8 @@ class AppTest_Reflective:
         g = f(1, 2)
         assert g(3) == f(1, 2, 3)
         assert f(4)(6)(7) == f(4, 6, 7)
+        return
+        # XXX the following does not work:
+        def g(x):
+            return f(x)
+        assert g(4)(5, 6) == f(4, 5, 6)
