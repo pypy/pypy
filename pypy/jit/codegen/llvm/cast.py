338233cbed46910ddfdaa4ffba244ad107e4bd75
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-01-27 13:15:48 +0000

    kill the JIT dir to restore it to its old state

--- pypy/jit/codegen/llvm/cast.py
+++
@@ -1,8 +0,0 @@
-import py
-from pypy.jit.codegen.llvm.genvarorconst import Var, BoolConst, CharConst,\
-    IntConst, UIntConst, FloatConst, AddrConst
-from pypy.jit.codegen.llvm.compatibility import icmp, scmp, ucmp, fcmp, inttoptr,\
-    trunc, zext, bitcast, inttoptr, shr_prefix, define, i1, i8, i16, i32, f64
-    
-def cast(osrc, dst):
-    print src, '->', dst
