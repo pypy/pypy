0da23f67dc5d21eb0467e6d8edd8aeb577b5e07d
Maciej Fijalkowski <fijall@gmail.com>
2008-02-09 14:42:32 +0000

    fromaddress

--- pypy/rlib/test/test_rbuffer.py
+++ pypy/rlib/test/test_rbuffer.py
@@ -14,3 +14,10 @@ class TestRbuffer:
         buf.setitem(4, '\x01')
         assert buf.getitem(4) == '\x01'
         buf.free()
+
+    def test_fromaddress(self):
+        buf = RBuffer(10)
+        buf.setitem(2, '\x03')
+        buf2 = RBuffer(7, buf.address() + 2)
+        assert buf2.getitem(0) == '\x03'
+        buf.free()
