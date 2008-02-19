5acdbcb97433b38a59b1eaabb14b5e00e17c544c
Armin Rigo <arigo@tunes.org>
2008-02-19 14:02:50 +0000

    Recover a GCFLAG bit that was "lost" by mistake.  (Not really important.)

--- pypy/rpython/memory/gc/generation.py
+++ pypy/rpython/memory/gc/generation.py
@@ -11,11 +11,11 @@ from pypy.rpython.lltypesystem.lloperation import llop
 # in the nursery.  It is initially set on all prebuilt and old objects,
 # and gets cleared by the write_barrier() when we write in them a
 # pointer to a young object.
-GCFLAG_NO_YOUNG_PTRS = SemiSpaceGC.first_unused_gcflag << 1
+GCFLAG_NO_YOUNG_PTRS = SemiSpaceGC.first_unused_gcflag << 0
 
 # The following flag is set for static roots which are not on the list
 # of static roots yet, but will appear with write barrier
-GCFLAG_NO_HEAP_PTRS = SemiSpaceGC.first_unused_gcflag << 2
+GCFLAG_NO_HEAP_PTRS = SemiSpaceGC.first_unused_gcflag << 1
 
 DEBUG_PRINT = False
 
@@ -29,7 +29,7 @@ class GenerationGC(SemiSpaceGC):
     inline_simple_malloc_varsize = True
     needs_write_barrier = True
     prebuilt_gc_objects_are_static_roots = False
-    first_unused_gcflag = SemiSpaceGC.first_unused_gcflag << 3
+    first_unused_gcflag = SemiSpaceGC.first_unused_gcflag << 2
 
     def __init__(self, chunk_size=DEFAULT_CHUNK_SIZE,
                  nursery_size=128,
