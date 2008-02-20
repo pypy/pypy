931e222470119e0a16a0b9b44873a5d003244ab7
Carl Friedrich Bolz <cfbolz@gmx.de>
2008-02-20 13:54:57 +0000

    make this test robust against other tests leaking

--- pypy/module/_rawffi/test/test_tracker.py
+++ pypy/module/_rawffi/test/test_tracker.py
@@ -10,26 +10,27 @@ class AppTestTracker:
 
     def test_array(self):
         import _rawffi
-        assert _rawffi._num_of_allocated_objects() == 0
+        oldnum = _rawffi._num_of_allocated_objects()
         a = _rawffi.Array('c')(3)
-        assert _rawffi._num_of_allocated_objects() == 1
+        assert _rawffi._num_of_allocated_objects() - oldnum == 1
         a.free()
-        assert _rawffi._num_of_allocated_objects() == 0
+        assert _rawffi._num_of_allocated_objects() - oldnum == 0
 
     def test_structure(self):
         import _rawffi
-        assert _rawffi._num_of_allocated_objects() == 0
+        oldnum = _rawffi._num_of_allocated_objects()
         s = _rawffi.Structure([('a', 'i'), ('b', 'i')])()
-        assert _rawffi._num_of_allocated_objects() == 1
+        assert _rawffi._num_of_allocated_objects() - oldnum == 1
         s.free()
-        assert _rawffi._num_of_allocated_objects() == 0
+        assert _rawffi._num_of_allocated_objects() - oldnum == 0
 
     def test_callback(self):
         import _rawffi
+        oldnum = _rawffi._num_of_allocated_objects()
         c = _rawffi.CallbackPtr(lambda : 3, [], 'i')
-        assert _rawffi._num_of_allocated_objects() == 1
+        assert _rawffi._num_of_allocated_objects() - oldnum== 1
         c.free()
-        assert _rawffi._num_of_allocated_objects() == 0
+        assert _rawffi._num_of_allocated_objects() - oldnum== 0
 
     def teardown_class(cls):
         Tracker.DO_TRACING = False
