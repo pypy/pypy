f83559dda90a910a9cbee26db7710845e4323dd8
Jared Grubb <jared.grubb@codespeak.net>
2008-03-02 20:51:37 +0000

    Add the PCRE testoutput1 file, including license in the regtest.

--- pypy/rlib/parsing/test/test_pcre_regtest.py
+++ pypy/rlib/parsing/test/test_pcre_regtest.py
@@ -1,18 +1,87 @@
-# This file can (in progress) read and parse PCRE regression tests to try out
-# on our regular expression library.
-# 
-# To try this out, 'man pcretest' and then grab testinput1 and testoutput1 from 
-# the PCRE source code. (I need to look into whether we could distribute these
-# files with pypy?)
+"""This test can read and parse PCRE regression tests to try out
+on our regular expression library."""
+
+# The PCRE library is distributed under the BSD license. We have borrowed some
+# of the regression tests (the ones that fit under the DFA scope) in order to
+# exercise our regex implementation. Those tests are distributed under PCRE's
+# BSD license. Here is the text:
+
+#        PCRE LICENCE
+#        ------------
+#        
+#        PCRE is a library of functions to support regular expressions whose syntax
+#        and semantics are as close as possible to those of the Perl 5 language.
+#
+#        Release 7 of PCRE is distributed under the terms of the "BSD" licence, as
+#        specified below. The documentation for PCRE, supplied in the "doc"
+#        directory, is distributed under the same terms as the software itself.
+#        
+#        The basic library functions are written in C and are freestanding. Also
+#        included in the distribution is a set of C++ wrapper functions.
+#        
+#        THE BASIC LIBRARY FUNCTIONS
+#        ---------------------------
+#        
+#        Written by:       Philip Hazel
+#        Email local part: ph10
+#        Email domain:     cam.ac.uk
+#        
+#        University of Cambridge Computing Service,
+#        Cambridge, England.
+#        
+#        Copyright (c) 1997-2008 University of Cambridge
+#        All rights reserved.
+#        
+#        THE C++ WRAPPER FUNCTIONS
+#        -------------------------
+#        
+#        Contributed by:   Google Inc.
+#        
+#        Copyright (c) 2007-2008, Google Inc.
+#        All rights reserved.
+#        
+#        THE "BSD" LICENCE
+#        -----------------
+#        
+#        Redistribution and use in source and binary forms, with or without
+#        modification, are permitted provided that the following conditions are met:
+#        
+#            * Redistributions of source code must retain the above copyright notice,
+#              this list of conditions and the following disclaimer.
+#        
+#            * Redistributions in binary form must reproduce the above copyright
+#              notice, this list of conditions and the following disclaimer in the
+#              documentation and/or other materials provided with the distribution.
+#        
+#            * Neither the name of the University of Cambridge nor the name of Google
+#              Inc. nor the names of their contributors may be used to endorse or
+#              promote products derived from this software without specific prior
+#              written permission.
+#        
+#        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#        AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#        IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#        ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+#        LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#        CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#        SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#        POSSIBILITY OF SUCH DAMAGE.
+#        
+#        End
 
 import py
 from pypy.rlib.parsing.regexparse import make_runner, unescape
 import string
 import re
 
-py.test.skip("In Progress...")
+py.test.skip("Still in progress")
 
-def read_file(file):
+def create_pcre_pickle(file, picklefile):
+    import pickle
+
     lines = [line for line in file.readlines()]
     
     # Look for things to skip...
@@ -98,66 +167,74 @@ def read_file(file):
                 print " *** %r ***" % match
                 raise Exception("Lost sync in output.")
             tests.append((test,match))
+    pickle.dump(suite, picklefile)
+
+def get_pcre_pickle(file):
+    import pickle
+    suite = pickle.load(file)
     return suite
 
-def test_file():
-    """Open the PCRE tests and run them."""
-    suite = read_file(open('testoutput1','r'))
+def run_individual_test(regex, tests):
+    regex_to_use = regex
+
+    anchor_left = regex_to_use.startswith('^')
+    anchor_right = regex_to_use.endswith('$') and not regex_to_use.endswith('\\$')
+    if anchor_left:
+        regex_to_use = regex_to_use[1:]   # chop the ^ if it's there
+    if anchor_right:
+        regex_to_use = regex_to_use[:-1]  # chop the $ if it's there
+
+    if not regex_to_use:
+        #print "  SKIPPED (Cant do blank regex)"
+        return
         
-    import pdb
+    runner = make_runner(regex)
+    # Now run the test expressions against the Regex
+    for test, match in tests:
+        # Create possible subsequences that we should test
+        if anchor_left:
+            start_range = [0]
+        else:
+            start_range = range(0, len(test))
+        
+        if anchor_right:
+            subseq_gen = ( (start, len(test)) for start in start_range )
+        else:
+            # Go backwards to simulate greediness
+            subseq_gen = ( (start, end) for start in start_range for end in range(len(test)+1, start, -1) )
+
+        # Search the possibilities for a match...
+        for start, end in subseq_gen:
+            attempt = test[start:end]
+            matched = runner.recognize(attempt)
+            if matched: 
+                break
+        
+        # Did we get what we expected?
+        if match == 'No match':
+            if matched:
+                assert "FALSE MATCH: regex==%r test==%r" % (regex, test)
+            else:
+                pass
+                #print "  pass:        regex==%r test==%r" % (regex, test)
+        elif match.startswith(' 0: '):
+            if not matched:
+                assert "MISSED:      regex==%r test==%r" % (regex, test)
+            elif not attempt==match[4:]:
+                assert "BAD MATCH:   regex==%r test==%r found==%r expect==%r" % (regex, test, attempt, match[4:])
+            else:
+                pass
+                #print "  pass:        regex==%r test==%r" % (regex, test)
+
+def run_pcre_tests(suite):
+    """Run PCRE tests as given in suite."""
     while suite:
         regex, flags, tests = suite.pop(0)
-        print '/%r/%s' % (regex, flags)
-    
-        regex_to_use = regex
-    
-        anchor_left = regex_to_use.startswith('^')
-        anchor_right = regex_to_use.endswith('$') and not regex_to_use.endswith('\\$')
-        if anchor_left:
-            regex_to_use = regex_to_use[1:]   # chop the ^ if it's there
-        if anchor_right:
-            regex_to_use = regex_to_use[:-1]  # chop the $ if it's there
-    
-        if not regex_to_use:
-            print "  SKIPPED (Cant do blank regex)"
-            continue
-            
-        # Finally, we make the pypy regex runner
-        runner = make_runner(regex_to_use)
-        
-        # Now run the test expressions against the Regex
-        for test, match in tests:
-            # Create possible subsequences that we should test
-            if anchor_left:
-                start_range = [0]
-            else:
-                start_range = range(0, len(test))
-            
-            if anchor_right:
-                subseq_gen = ( (start, len(test)) for start in start_range )
-            else:
-                # Go backwards to simulate greediness
-                subseq_gen = ( (start, end) for start in start_range for end in range(len(test)+1, start, -1) )
+        yield run_individual_test, regex, tests
 
-            # Search the possibilities for a match...
-            for start, end in subseq_gen:
-                attempt = test[start:end]
-                matched = runner.recognize(attempt)
-                if matched: 
-                    break
-            
-            # Did we get what we expected?
-            if match == 'No match':
-                if matched:
-                    print "  FALSE MATCH: regex==%r test==%r" % (regex, test)
-                else:
-                    pass
-                    #print "  pass:        regex==%r test==%r" % (regex, test)
-            elif match.startswith(' 0: '):
-                if not matched:
-                    print "  MISSED:      regex==%r test==%r" % (regex, test)
-                elif not attempt==match[4:]:
-                    print "  BAD MATCH:   regex==%r test==%r found==%r expect==%r" % (regex, test, attempt, match[4:])
-                else:
-                    pass
-                    #print "  pass:        regex==%r test==%r" % (regex, test)
+
+def test_output1():
+    suite = get_pcre_pickle(open('testoutput1.pickle','r'))
+    for test in run_pcre_tests(suite):
+        yield test
+        
