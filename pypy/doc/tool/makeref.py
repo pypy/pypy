725b75eded22275b6be92a7549983e27d0340b0b
Armin Rigo <arigo@tunes.org>
2008-03-18 17:59:52 +0000

    Cannot include ../_ref.txt because all relative paths are bogus. Fix this by generating a new _ref.txt in each directory that needs it (currently only doc/ and doc/jit/).

--- pypy/doc/tool/makeref.py
+++ pypy/doc/tool/makeref.py
@@ -7,47 +7,59 @@ distdir = pypydir.dirpath()
 dist_url = 'http://codespeak.net/svn/pypy/dist/' 
 issue_url = 'http://codespeak.net/issue/pypy-dev/' 
 
-docdir = pypydir.join('doc') 
-reffile = pypydir.join('doc', '_ref.txt') 
+def makeref(docdir):
+    reffile = docdir.join('_ref.txt') 
 
-linkrex = py.std.re.compile('`(\S+)`_')
+    linkrex = py.std.re.compile('`(\S+)`_')
 
-name2target = {}
-def addlink(linkname, linktarget): 
-    assert linkname and linkname != '/'
-    if linktarget in name2target: 
-        if linkname in name2target[linktarget]: 
-            return
-    name2target.setdefault(linktarget, []).append(linkname)
+    name2target = {}
+    def addlink(linkname, linktarget): 
+        assert linkname and linkname != '/'
+        if linktarget in name2target: 
+            if linkname in name2target[linktarget]: 
+                return
+        name2target.setdefault(linktarget, []).append(linkname)
 
-for textfile in docdir.visit(lambda x: x.ext == '.txt', 
-                             lambda x: x.check(dotfile=0)): 
-    for linkname in linkrex.findall(textfile.read()): 
-        if '/' in linkname: 
-            for startloc in ('', 'pypy'): 
-                cand = distdir.join(startloc, linkname)
-                if cand.check(): 
-                    rel = cand.relto(distdir)
-                    # we are in pypy/doc/x.txt 
-                    target = '../../' + cand.relto(distdir)
-                    addlink(linkname, target) 
-                    break
-            else: 
-                print "WARNING %s: link %r may be bogus" %(textfile, linkname) 
-        elif linkname.startswith('issue'): 
-            addlink(linkname, issue_url+linkname)
+    for textfile in docdir.listdir():  # for subdirs, see below
+        if textfile.ext != '.txt':
+            continue
+        for linkname in linkrex.findall(textfile.read()): 
+            if '/' in linkname: 
+                for startloc in ('', 'pypy'): 
+                    cand = distdir.join(startloc, linkname)
+                    if cand.check(): 
+                        rel = cand.relto(distdir)
+                        assert docdir.relto(distdir)
+                        dotdots = 0
+                        p = docdir
+                        while p != distdir:
+                            p = p.dirpath()
+                            dotdots += 1
+                        target = '../' * dotdots + cand.relto(distdir)
+                        addlink(linkname, target) 
+                        break
+                else: 
+                    print "WARNING %s: link %r may be bogus" %(textfile, linkname) 
+            elif linkname.startswith('issue'): 
+                addlink(linkname, issue_url+linkname)
 
-items = name2target.items() 
-items.sort() 
+    items = name2target.items() 
+    items.sort() 
 
-lines = []
-for linktarget, linknamelist in items: 
-    linknamelist.sort()
-    for linkname in linknamelist[:-1]: 
-        lines.append(".. _`%s`:" % linkname)
-    lines.append(".. _`%s`: %s" %(linknamelist[-1], linktarget))
+    lines = []
+    for linktarget, linknamelist in items: 
+        linknamelist.sort()
+        for linkname in linknamelist[:-1]: 
+            lines.append(".. _`%s`:" % linkname)
+        lines.append(".. _`%s`: %s" %(linknamelist[-1], linktarget))
 
-reffile.write("\n".join(lines))
-print "wrote %d references to %r" %(len(lines), reffile)
-#print "last ten lines"
-#for x in lines[-10:]: print x
+    reffile.write("\n".join(lines))
+    print "wrote %d references to %r" %(len(lines), reffile)
+    #print "last ten lines"
+    #for x in lines[-10:]: print x
+
+
+# We need to build a new _ref.txt for each directory that uses it, because
+# they differ in the number of "../" that they need in the link targets...
+makeref(pypydir.join('doc'))
+makeref(pypydir.join('doc').join('jit'))
