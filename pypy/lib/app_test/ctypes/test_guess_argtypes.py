f03c5f8b05f109077353b9ec50c629af51edd152
Maciej Fijalkowski <fijall@gmail.com>
2008-04-03 05:16:58 +0000

    Add a test for guessing argtypes.

---
+++ pypy/lib/app_test/ctypes/test_guess_argtypes.py
@@ -0,0 +1,29 @@
+
+""" This test checks whether args wrapping behavior is correct
+"""
+
+import sys
+
+from ctypes import *
+
+def test_wrap_args():
+    if not hasattr(sys, 'pypy_translation_info'):
+        py.test.skip("whitebox test")
+    from _ctypes.function import CFuncPtr
+
+    guess = CFuncPtr._guess_argtypes
+
+    assert guess([13]) == [c_int]
+    assert guess([0]) == [c_int]
+    assert guess(['xca']) == [c_char_p]
+    assert guess([None]) == [c_void_p]
+    assert guess([c_int(3)]) == [c_int]
+
+    class Stuff:
+        pass
+    s = Stuff()
+    s._as_parameter_ = None
+    
+    assert guess([s]) == [c_void_p]
+
+    # not sure what else....
