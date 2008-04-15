9563276c9bbb6204044df96d9f4bc89910486019
Armin Rigo <arigo@tunes.org>
2008-04-15 11:44:33 +0000

    Add cStringIO to allworkingmodules.

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -25,7 +25,7 @@ working_modules = default_modules.copy()
 working_modules.update(dict.fromkeys(
     ["_socket", "unicodedata", "mmap", "fcntl", "rctime", "select",
      "crypt", "signal", "dyngram", "_rawffi", "termios", "zlib",
-     "struct", "md5", "sha", "bz2", "_minimal_curses",
+     "struct", "md5", "sha", "bz2", "_minimal_curses", "cStringIO",
     ]
 ))
 
