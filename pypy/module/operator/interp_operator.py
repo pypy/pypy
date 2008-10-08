44462cdd762c9b1bff3dd7d8299d7ea787a7e003
Samuele Pedroni <pedronis@lucediurna.net>
2008-10-08 11:40:19 +0000

    translation fix

--- pypy/module/operator/interp_operator.py
+++ pypy/module/operator/interp_operator.py
@@ -229,12 +229,14 @@ def irepeat(space, w_obj1, w_obj2):
     'irepeat(a, b) -- Same as a *= b, for a and b sequences.'
     if space.findattr(w_obj1, space.wrap('__getitem__')) is None:
         # first arg has to be a sequence
-        raise OperationError(space.w_TypeError, "non-sequence object can't be repeated")
+        raise OperationError(space.w_TypeError,
+                           space.wrap("non-sequence object can't be repeated"))
 
     if not (space.is_true(space.isinstance(w_obj2, space.w_int)) or \
             space.is_true(space.isinstance(w_obj2, space.w_long))):
         # second arg has to be int/long
-        raise OperationError(space.w_TypeError, 'an integer is required')
+        raise OperationError(space.w_TypeError,
+                             space.wrap('an integer is required'))
 
     return space.inplace_mul(w_obj1, w_obj2)
 
