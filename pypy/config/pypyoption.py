698e1b7a151956386f342091472611301b4158b2
Maciej Fijalkowski <fijall@gmail.com>
2008-10-26 21:22:51 +0000

    Suggest strslice and strjoin for -Omem

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -367,6 +367,8 @@ def set_pypy_opt_level(config, level):
         config.objspace.std.suggest(withrangelist=True)
         config.objspace.std.suggest(withprebuiltchar=True)
         config.objspace.std.suggest(withsharingdict=True)
+        config.objspace.std.suggest(withstrslice=True)
+        config.objspace.std.suggest(withstrjoin=True)
         # xxx other options? ropes maybe?
 
     # completely disable geninterp in a level 0 translation
