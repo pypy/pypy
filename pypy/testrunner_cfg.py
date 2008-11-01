a43609a2f87b6d0380d9233382609001eb9c6d82
Samuele Pedroni <pedronis@lucediurna.net>
2008-11-01 15:13:23 +0000

    explode the slow gc tests too

--- pypy/testrunner_cfg.py
+++ pypy/testrunner_cfg.py
@@ -3,7 +3,8 @@
 def collect_one_testdir(testdirs, reldir, tests):
     if (reldir.startswith('jit/codegen/i386/') or
         reldir.startswith('jit/timeshifter/') or
-        reldir.startswith('translator/c/')):
+        reldir.startswith('translator/c/') or 
+        reldir.startswith('/rpython/memory/'):
         testdirs.extend(tests)
     else:     
         testdirs.append(reldir)
