929eaf3a8156431aef54e8cda87dd1b22e6f7bcf
Armin Rigo <arigo@tunes.org>
2008-11-28 13:43:54 +0000

    List rlib/test/ in the places to test one file at a time instead of one dir at a time.

--- pypy/testrunner_cfg.py
+++ pypy/testrunner_cfg.py
@@ -4,6 +4,7 @@ def collect_one_testdir(testdirs, reldir, tests):
     if (reldir.startswith('jit/codegen/i386/') or
         reldir.startswith('jit/timeshifter/') or
         reldir.startswith('translator/c/') or 
+        reldir.startswith('rlib/test') or
         reldir.startswith('rpython/memory/')):
         testdirs.extend(tests)
     else:     
