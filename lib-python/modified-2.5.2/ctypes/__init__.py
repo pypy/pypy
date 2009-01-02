b15de6ff605e46744d0420f4aa06027a25a43f59
Armin Rigo <arigo@tunes.org>
2009-01-02 16:09:37 +0000

    Make general code instead of PyPy-specific only.

--- lib-python/modified-2.5.2/ctypes/__init__.py
+++ lib-python/modified-2.5.2/ctypes/__init__.py
@@ -433,7 +433,15 @@ class LibraryLoader(object):
 cdll = LibraryLoader(CDLL)
 pydll = LibraryLoader(PyDLL)
 
-pythonapi = None
+if _sys.subversion[0] == 'CPython':
+    if _os.name in ("nt", "ce"):
+        pythonapi = PyDLL("python dll", None, _sys.dllhandle)
+    elif _sys.platform == "cygwin":
+        pythonapi = PyDLL("libpython%d.%d.dll" % _sys.version_info[:2])
+    else:
+        pythonapi = PyDLL(None)
+else:
+    pythonapi = None
 
 
 if _os.name in ("nt", "ce"):
