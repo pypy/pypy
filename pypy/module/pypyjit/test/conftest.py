42bf2bdefc2c085bec1d38ac397a45425a143af1
Armin Rigo <arigo@tunes.org>
2009-03-07 13:29:53 +0000

    Remove this directory.

--- pypy/module/pypyjit/test/conftest.py
+++
@@ -1,9 +0,0 @@
-import py
-
-
-Option = py.test.config.Option
-
-option = py.test.config.addoptions("ppc options",
-        Option('--pypy-c', action="store", default=None,
-               dest="pypy_c",
-               help=""))
