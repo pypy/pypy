3a770887dd9b69801fd25fae8377e86820950826
Armin Rigo <arigo@tunes.org>
2009-03-07 13:45:53 +0000

    Kill old directory.

--- pypy/jit/old-metainterp/test/test_loop_spec.py
+++
@@ -1,8 +0,0 @@
-import py
-from pypy.jit.metainterp.test import test_loop
-
-
-class TestLoopSpec(test_loop.TestLoop):
-    specialize = True
-
-    # ====> test_loop.py
--- pypy/jit/old-metainterp/test/test_zrpy_virtualizable.py
+++
@@ -1,8 +0,0 @@
-import py
-from pypy.jit.metainterp.test import test_virtualizable
-from pypy.jit.metainterp.test.test_zrpy_basic import LLInterpJitMixin
-
-
-class TestLLImplicitVirtualizable(LLInterpJitMixin,
-                       test_virtualizable.ImplicitVirtualizableTests):
-    pass
