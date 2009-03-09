866ab018675325d5bfda84f4850480a975ba0819
Maciej Fijalkowski <fijall@gmail.com>
2009-03-09 11:58:03 +0000

    port test_zrpy_virtualizable to x86 backend

---
+++ pypy/jit/backend/x86/test/test_zrpy_virtualizable.py
@@ -0,0 +1,9 @@
+
+import py
+from pypy.jit.metainterp.test import test_virtualizable
+from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
+
+
+class TestLLImplicitVirtualizable(Jit386Mixin,
+                       test_virtualizable.ImplicitVirtualizableTests):
+    pass
