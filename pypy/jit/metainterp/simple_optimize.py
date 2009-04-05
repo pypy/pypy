cb096c2efee9a2888a6308e8e196a673e4030edb
Maciej Fijalkowski <fijall@gmail.com>
2009-04-05 04:33:22 +0000

    remove guard_nonvirtualize for simple_optimize as they're nonnecessary

--- pypy/jit/metainterp/simple_optimize.py
+++ pypy/jit/metainterp/simple_optimize.py
@@ -1,14 +1,25 @@
 
 """ Simplified optimize.py
 """
+from pypy.jit.metainterp.resoperation import rop
 
 def optimize_loop(options, old_loops, loop, cpu=None):
     if old_loops:
         return old_loops[0]
     else:
+        newoperations = []
+        for op in loop.operations:
+            if op.opnum == rop.GUARD_NONVIRTUALIZED:
+                continue
+            newoperations.append(op)
+        loop.operations = newoperations
         return None
 
 def optimize_bridge(options, old_loops, loop, cpu=None):
     return old_loops[0]
+
+class Optimizer:
+    optimize_loop = staticmethod(optimize_loop)
+    optimize_bridge = staticmethod(optimize_bridge)
 
 
