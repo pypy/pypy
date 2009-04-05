e200791bf101514fab1208b1a48d84ee3dc4c159
Maciej Fijalkowski <fijall@gmail.com>
2009-04-05 04:33:41 +0000

    this has move somewhere else

--- pypy/jit/metainterp/test/test_loop_dummy.py
+++ pypy/jit/metainterp/test/test_loop_dummy.py
@@ -1,11 +1,7 @@
 
 from pypy.jit.metainterp.test import test_loop
 from pypy.jit.metainterp.warmspot import ll_meta_interp
-from pypy.jit.metainterp.simple_optimize import optimize_loop, optimize_bridge
-
-class Optimizer:
-    optimize_loop = staticmethod(optimize_loop)
-    optimize_bridge = staticmethod(optimize_bridge)
+from pypy.jit.metainterp.simple_optimize import Optimizer
 
 class TestLoopDummy(test_loop.TestLoop):
     def meta_interp(self, func, args, **kwds):
