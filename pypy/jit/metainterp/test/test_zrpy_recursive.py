166993464a77a57263de6c6276a7d2cc29be4f90
Armin Rigo <arigo@tunes.org>
2009-04-11 10:06:57 +0000

    Tests need a higher recursion limit...

--- pypy/jit/metainterp/test/test_zrpy_recursive.py
+++ pypy/jit/metainterp/test/test_zrpy_recursive.py
@@ -1,9 +1,15 @@
-import py
+import py, sys
 from pypy.jit.metainterp.test import test_recursive
 from pypy.jit.metainterp.test.test_zrpy_basic import LLInterpJitMixin
 
 
 class TestLLRecursive(test_recursive.RecursiveTests, LLInterpJitMixin):
-    pass
+
+    def setup_class(cls):
+        cls._recursion_limit = sys.getrecursionlimit()
+        sys.setrecursionlimit(5000)
+
+    def teardown_class(cls):
+        sys.setrecursionlimit(cls._recursion_limit)
 
     # ==========> test_recursive.py
