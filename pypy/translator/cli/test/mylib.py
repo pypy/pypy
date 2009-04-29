9342944949123f964d57e4b3a9afc8c009a9f87e
Antonio Cuni <anto.cuni@gmail.com>
2009-04-29 10:09:55 +0000

    (in-progress) remove the cli backend. It will be replaced by the one in oo-jit, then the differences will be merged back

--- pypy/translator/cli/test/mylib.py
+++
@@ -1,5 +0,0 @@
-from pypy.translator.cli.carbonpython import export
-
-@export(int, int)
-def sum(a, b):
-    return a+b
--- pypy/translator/cli/test/test_constant.py
+++
@@ -1,5 +0,0 @@
-from pypy.translator.oosupport.test_template.constant import BaseTestConstant
-from pypy.translator.cli.test.runtest import CliTest
-
-class TestConstant(BaseTestConstant, CliTest):
-    pass
