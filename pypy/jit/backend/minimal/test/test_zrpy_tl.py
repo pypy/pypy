a572955a31f1dcf32efa184d867fe3f552de18af
Armin Rigo <arigo@tunes.org>
2009-04-29 16:36:52 +0000

    fix ImportError.

--- pypy/jit/backend/minimal/test/test_zrpy_tl.py
+++ pypy/jit/backend/minimal/test/test_zrpy_tl.py
@@ -1,8 +1,8 @@
 import py
 from pypy.jit.metainterp.test.test_tl import ToyLanguageTests
-from pypy.jit.backend.minimal.test.test_zrpy_exception import TranslatedJitMixin
+from pypy.jit.backend.minimal.test.test_zrpy_exception import LLTranslatedJitMixin
 
-class TestTL(TranslatedJitMixin, ToyLanguageTests):
+class TestTL(LLTranslatedJitMixin, ToyLanguageTests):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_tl.py
     pass
