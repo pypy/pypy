30577ffab389f3148d3c416acd9e594b6028f6c2
Antonio Cuni <anto.cuni@gmail.com>
2009-04-29 17:14:08 +0000

    cool, the first cli translated test passes

--- pypy/jit/backend/minimal/test/test_zrpy_exception.py
+++ pypy/jit/backend/minimal/test/test_zrpy_exception.py
@@ -22,7 +22,32 @@ class OOTranslatedJitMixin(CliCompiledMixin):
 
 
 class TestOOtype(OOTranslatedJitMixin, test_zrpy_exception.TestLLExceptions):
-    pass
+
+    def skip(self):
+        py.test.skip('in-progress')
+
+    test_bridge_from_guard_exception = skip
+    test_bridge_from_guard_no_exception = skip
+    test_loop = skip
+    test_four_levels_checks = skip
+    test_exception_from_outside = skip
+    test_exception_from_outside_2 = skip
+    test_exception_two_cases = skip
+    test_exception_two_cases_2 = skip
+    test_exception_later = skip
+    test_exception_and_then_no_exception = skip
+    test_int_ovf = skip
+    test_int_mod_ovf_zer = skip
+    test_int_lshift_ovf = skip
+    test_reraise_through_portal = skip
+    test_bridge_from_interpreter_exc = skip
+    test_bridge_from_interpreter_exc_2 = skip
+    test_raise = skip
+    test_raise_through = skip
+    test_raise_through_wrong_exc = skip
+    test_raise_through_wrong_exc_2 = skip
+
+
 
 class TestLLtype(LLTranslatedJitMixin, test_zrpy_exception.TestLLExceptions):
     # for the individual tests see
