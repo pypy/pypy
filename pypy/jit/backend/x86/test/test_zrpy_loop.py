208cf15433115a8c4b16a221fefbedb7d91f23dd
Armin Rigo <arigo@tunes.org>
2009-04-30 14:15:34 +0000

    Skips.

--- pypy/jit/backend/x86/test/test_zrpy_loop.py
+++ pypy/jit/backend/x86/test/test_zrpy_loop.py
@@ -1,8 +1,13 @@
-
+import py
 from pypy.jit.metainterp.test.test_loop import LoopTest
 from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
 
 class TestLoop(Jit386Mixin, LoopTest):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_loop.py
-    pass
+
+    def test_interp_many_paths(self):
+        py.test.skip('not supported: pointer as argument')
+
+    def test_interp_many_paths_2(self):
+        py.test.skip('not supported: pointer as argument')
