b416219486fceb22858c89f8034ad43491dbf24c
Armin Rigo <arigo@tunes.org>
2009-04-30 12:34:13 +0000

    GUARD_CLASS.

--- pypy/jit/backend/test/test_random.py
+++ pypy/jit/backend/test/test_random.py
@@ -1,9 +1,9 @@
 import py, sys, math
 from pypy.rlib.rarithmetic import intmask, LONG_BIT
-from pypy.rpython.lltypesystem import lltype, llmemory
+from pypy.rpython.lltypesystem import lltype, llmemory, rclass
 from pypy.jit.backend.test import conftest as demo_conftest
 from pypy.jit.metainterp.history import TreeLoop, BoxInt, ConstInt
-from pypy.jit.metainterp.history import BoxPtr, ConstPtr
+from pypy.jit.metainterp.history import BoxPtr, ConstPtr, ConstAddr
 from pypy.jit.metainterp.resoperation import ResOperation, rop
 from pypy.jit.metainterp.executor import execute
 
@@ -41,37 +41,61 @@ class OperationBuilder:
             v = self.do(rop.INT_IS_TRUE, [v])
         return v
 
-    def get_structptr_var(self, r):
-        if self.ptrvars and r.random() < 0.8:
-            v, S = r.choice(self.ptrvars)
-        elif self.prebuilt_ptr_consts and r.random() < 0.7:
-            v, S, _ = r.choice(self.prebuilt_ptr_consts)
-        else:
-            p = self.get_random_structure(r)
-            S = lltype.typeOf(p).TO
-            v = ConstPtr(lltype.cast_opaque_ptr(llmemory.GCREF, p))
-            self.prebuilt_ptr_consts.append((v, S, self.field_values(p)))
+    def get_structptr_var(self, r, must_have_vtable=False):
+        while True:
+            if self.ptrvars and r.random() < 0.8:
+                v, S = r.choice(self.ptrvars)
+            elif self.prebuilt_ptr_consts and r.random() < 0.7:
+                v, S, _ = r.choice(self.prebuilt_ptr_consts)
+            else:
+                must_have_vtable = must_have_vtable or r.random() < 0.5
+                p = self.get_random_structure(r, has_vtable=must_have_vtable)
+                S = lltype.typeOf(p).TO
+                v = ConstPtr(lltype.cast_opaque_ptr(llmemory.GCREF, p))
+                self.prebuilt_ptr_consts.append((v, S, self.field_values(p)))
+            if not (must_have_vtable and S._names[0] != 'parent'):
+                break
         return v, S
 
-    def get_random_structure_type(self, r):
+    def get_random_structure_type(self, r, has_vtable=False):
         fields = []
+        if has_vtable:
+            fields.append(('parent', rclass.OBJECT))
         for i in range(r.randrange(1, 5)):
             fields.append(('f%d' % i, lltype.Signed))
         S = lltype.GcStruct('S%d' % self.counter, *fields)
         self.counter += 1
         return S
 
-    def get_random_structure(self, r):
-        S = self.get_random_structure_type(r)
-        p = lltype.malloc(S)
+    def get_random_structure_type_and_vtable(self, r):
+        S = self.get_random_structure_type(r, has_vtable=True)
+        vtable = lltype.malloc(rclass.OBJECT_VTABLE, immortal=True)
+        name = S._name
+        vtable.name = lltype.malloc(lltype.Array(lltype.Char), len(name)+1,
+                                    immortal=True)
+        for i in range(len(name)):
+            vtable.name[i] = name[i]
+        vtable.name[len(name)] = '\x00'
+        return S, vtable
+
+    def get_random_structure(self, r, has_vtable=False):
+        if has_vtable:
+            S, vtable = self.get_random_structure_type_and_vtable(r)
+            p = lltype.malloc(S)
+            p.parent.typeptr = vtable
+        else:
+            S = self.get_random_structure_type(r)
+            p = lltype.malloc(S)
         for fieldname in lltype.typeOf(p).TO._names:
-            setattr(p, fieldname, r.random_integer())
+            if fieldname != 'parent':
+                setattr(p, fieldname, r.random_integer())
         return p
 
     def field_values(self, p):
         dic = {}
         for fieldname in lltype.typeOf(p).TO._names:
-            dic[fieldname] = getattr(p, fieldname)
+            if fieldname != 'parent':
+                dic[fieldname] = getattr(p, fieldname)
         return dic
 
     def print_loop(self):
@@ -99,10 +123,17 @@ class OperationBuilder:
             if S not in written:
                 print >>s, '    %s = lltype.GcStruct(%r,' % (S._name, S._name)
                 for name in S._names:
-                    print >>s, '              (%r, lltype.Signed),' % (name,)
+                    if name == 'parent':
+                        print >>s, "              ('parent', rclass.OBJECT),"
+                    else:
+                        print >>s, '              (%r, lltype.Signed),'%(name,)
                 print >>s, '              )'
+                if S._names[0] == 'parent':
+                    print >>s, '    %s_vtable = lltype.malloc(rclass.OBJECT_VTABLE, immortal=True)' % (S._name,)
                 written[S] = True
             print >>s, '    p = lltype.malloc(%s)' % (S._name,)
+            if S._names[0] == 'parent':
+                print >>s, '    p.parent.typeptr = %s_vtable' % (S._name,)
             for name, value in fields.items():
                 print >>s, '    p.%s = %d' % (name, value)
             writevar(v, 'preb')
@@ -120,6 +151,11 @@ class OperationBuilder:
             for v in op.args:
                 if v in names:
                     args.append(names[v])
+                elif isinstance(v, ConstAddr):
+                    name = ''.join([v.value.ptr.name[i]
+                                    for i in range(len(v.value.ptr.name)-1)])
+                    args.append(
+                        'ConstAddr(llmemory.cast_ptr_to_adr(%s_vtable))'% name)
                 else:
                     args.append('ConstInt(%d)' % v.value)
             if op.descr is None:
@@ -189,10 +225,15 @@ class BinaryOperation(AbstractOperation):
         self.put(builder, [v_first, v_second])
 
 class GuardOperation(AbstractOperation):
-
-    def produce_into(self, builder, r):
+    def gen_guard(self, builder, r):
         v = builder.get_bool_var(r)
         op = ResOperation(self.opnum, [v], None)
+        passing = ((self.opnum == rop.GUARD_TRUE and v.value) or
+                   (self.opnum == rop.GUARD_FALSE and not v.value))
+        return op, passing
+
+    def produce_into(self, builder, r):
+        op, passing = self.gen_guard(builder, r)
         builder.loop.operations.append(op)
         k = r.random()
         subset = []
@@ -201,15 +242,30 @@ class GuardOperation(AbstractOperation):
             subset.append(r.choice(builder.intvars))
         r.shuffle(subset)
         op.suboperations = [ResOperation(rop.FAIL, subset, None)]
-        if ((self.opnum == rop.GUARD_TRUE and not v.value) or
-            (self.opnum == rop.GUARD_FALSE and v.value)):
+        if not passing:
             builder.should_fail_by = op.suboperations[0]
             builder.should_fail_by_num = len(builder.loop.operations) - 1
+
+class GuardClassOperation(GuardOperation):
+    def gen_guard(self, builder, r):
+        v, S = builder.get_structptr_var(r, must_have_vtable=True)
+        if r.random() < 0.3:
+            v2 = v
+        else:
+            v2, S2 = builder.get_structptr_var(r, must_have_vtable=True)
+        vtable = v.getptr(rclass.OBJECTPTR).typeptr
+        vtable2 = v2.getptr(rclass.OBJECTPTR).typeptr
+        c_vtable2 = ConstAddr(llmemory.cast_ptr_to_adr(vtable2), builder.cpu)
+        op = ResOperation(self.opnum, [v, c_vtable2], None)
+        return op, (vtable == vtable2)
 
 class GetFieldOperation(AbstractOperation):
     def field_descr(self, builder, r):
         v, S = builder.get_structptr_var(r)
-        name = r.choice(S._names)
+        names = S._names
+        if names[0] == 'parent':
+            names = names[1:]
+        name = r.choice(names)
         descr = builder.cpu.fielddescrof(S, name)
         descr._random_info = 'cpu.fielddescrof(%s, %r)' % (S._name, name)
         return v, descr
@@ -239,8 +295,13 @@ class NewOperation(AbstractOperation):
         return descr
 
     def produce_into(self, builder, r):
-        S = builder.get_random_structure_type(r)
-        v_ptr = builder.do(self.opnum, [], self.size_descr(builder, S))
+        if self.opnum == rop.NEW_WITH_VTABLE:
+            S, vtable = builder.get_random_structure_type_and_vtable(r)
+            args = [ConstAddr(llmemory.cast_ptr_to_adr(vtable), builder.cpu)]
+        else:
+            S = builder.get_random_structure_type(r)
+            args = []
+        v_ptr = builder.do(self.opnum, args, self.size_descr(builder, S))
         builder.ptrvars.append((v_ptr, S))
 
 # ____________________________________________________________
@@ -275,9 +336,6 @@ OPERATIONS.append(BinaryOperation(rop.INT_RSHIFT, LONG_BIT-1))
 OPERATIONS.append(BinaryOperation(rop.INT_LSHIFT, LONG_BIT-1))
 OPERATIONS.append(BinaryOperation(rop.UINT_RSHIFT, LONG_BIT-1))
 
-OPERATIONS.append(GuardOperation(rop.GUARD_TRUE))
-OPERATIONS.append(GuardOperation(rop.GUARD_FALSE))
-
 for _op in [rop.INT_NEG,
             rop.INT_INVERT,
             rop.INT_ABS,
@@ -287,11 +345,16 @@ for _op in [rop.INT_NEG,
 OPERATIONS.append(UnaryOperation(rop.INT_IS_TRUE, boolres=True))
 OPERATIONS.append(BooleanUnaryOperation(rop.BOOL_NOT, boolres=True))
 
-for i in range(4):      # make more common
+for i in range(3):      # make more common
     OPERATIONS.append(GetFieldOperation(rop.GETFIELD_GC))
     OPERATIONS.append(GetFieldOperation(rop.GETFIELD_GC))
     OPERATIONS.append(SetFieldOperation(rop.SETFIELD_GC))
     OPERATIONS.append(NewOperation(rop.NEW))
+    OPERATIONS.append(NewOperation(rop.NEW_WITH_VTABLE))
+
+    OPERATIONS.append(GuardOperation(rop.GUARD_TRUE))
+    OPERATIONS.append(GuardOperation(rop.GUARD_FALSE))
+    OPERATIONS.append(GuardClassOperation(rop.GUARD_CLASS))
 
 # ____________________________________________________________
 
