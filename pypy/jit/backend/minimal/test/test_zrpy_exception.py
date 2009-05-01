080ffa29d99f9d00c4a3b7a1a7143ff1cb252ed1
Antonio Cuni <anto.cuni@gmail.com>
2009-05-01 08:40:52 +0000

    a lot of tests pass out of the box, due to previous checkins

--- pypy/jit/backend/minimal/test/test_zrpy_exception.py
+++ pypy/jit/backend/minimal/test/test_zrpy_exception.py
@@ -26,15 +26,6 @@ class TestOOtype(OOTranslatedJitMixin, test_zrpy_exception.TestLLExceptions):
     def skip(self):
         py.test.skip('in-progress')
 
-    test_exception_from_outside = skip
-    test_exception_from_outside_2 = skip
-    test_exception_two_cases = skip
-    test_exception_two_cases_2 = skip
-    test_exception_later = skip
-    test_exception_and_then_no_exception = skip
-    test_int_ovf = skip
-    test_int_mod_ovf_zer = skip
-    test_int_lshift_ovf = skip
     test_reraise_through_portal = skip
     test_bridge_from_interpreter_exc = skip
     test_bridge_from_interpreter_exc_2 = skip
