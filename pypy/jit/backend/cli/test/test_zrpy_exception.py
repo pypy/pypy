1dab2edb61b2038d8e469790dc544e02af9f9caa
Antonio Cuni <anto.cuni@gmail.com>
2009-05-18 15:40:47 +0000

    nice, a lot of tests passes out of the box

--- pypy/jit/backend/cli/test/test_zrpy_exception.py
+++ pypy/jit/backend/cli/test/test_zrpy_exception.py
@@ -14,15 +14,11 @@ class TestException(CliTranslatedJitMixin, test_exception.TestOOtype):
         py.test.skip('in-progress')
 
     test_bridge_from_guard_exception = skip_loop
+    test_exception_four_cases = skip_loop
+    test_bridge_from_interpreter_exc = skip_loop
+    test_bridge_from_interpreter_exc_2 = skip_loop
     
-    test_exception_from_outside_2 = skip
-    test_exception_two_cases = skip
-    test_exception_two_cases_2 = skip
-    test_exception_four_cases = skip
-    test_exception_later = skip
-    test_exception_and_then_no_exception = skip
-    test_raise_through_wrong_exc_2 = skip
     test_int_ovf = skip
     test_int_lshift_ovf = skip
-    test_bridge_from_interpreter_exc = skip
-    test_bridge_from_interpreter_exc_2 = skip
+
+
