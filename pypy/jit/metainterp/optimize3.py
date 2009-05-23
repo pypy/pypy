eaea89bfc81c8815120e3ece133b1ed9cd6f186b
Antonio Cuni <anto.cuni@gmail.com>
2009-05-23 15:49:29 +0000

    yet another optimize*.py.  The idea is that you can write a new class for each optimization, and methods of the class will be called for each corresponding operation.

---
+++ pypy/jit/metainterp/optimize3.py
@@ -0,0 +1,194 @@
+from pypy.jit.metainterp.resoperation import rop, ResOperation
+from pypy.jit.metainterp.history import Const, Box
+
+class InstanceNode(object):
+    def __init__(self, source, const=False):
+        self.source = source
+        if const:
+            assert isinstance(source, Const)
+        self.const = const
+        self.cls = None
+        self.cleanfields = {}
+        self.dirtyfields = {}
+
+    def __repr__(self):
+        flags = ''
+        #if self.escaped:           flags += 'e'
+        #if self.startbox:          flags += 's'
+        if self.const:             flags += 'c'
+        #if self.virtual:           flags += 'v'
+        #if self.virtualized:       flags += 'V'
+        return "<InstanceNode %s (%s)>" % (self.source, flags)
+
+class Specializer(object):
+    loop = None
+    nodes = None
+
+    def __init__(self, optlist):
+        self.optlist = optlist
+
+    def getnode(self, box):
+        try:
+            return self.nodes[box]
+        except KeyError:
+            if isinstance(box, Const):
+                node = InstanceNode(box, const=True)
+            else:
+                node = InstanceNode(box)
+            self.nodes[box] = node
+            return node
+
+    def getsource(self, box):
+        if isinstance(box, Const):
+            return box
+        return self.nodes[box].source
+
+    def find_nodes(self):
+        for op in self.loop.operations:
+            if op.is_always_pure():
+                is_pure = True
+                for arg in op.args:
+                    if not self.getnode(arg).const:
+                        is_pure = False
+                if is_pure:
+                    box = op.result
+                    assert box is not None
+                    self.nodes[box] = InstanceNode(box.constbox(), const=True)
+                    continue
+            else:
+                if op.is_guard():
+                    for arg in op.suboperations[0].args:
+                        self.getnode(arg)
+                # default case
+                for box in op.args:
+                    self.getnode(box)
+            box = op.result
+            if box is not None:
+                self.nodes[box] = InstanceNode(box)
+
+    def new_arguments(self, op):
+        newboxes = []
+        for box in op.args:
+            if isinstance(box, Box):
+                instnode = self.nodes[box]
+                box = instnode.source
+            newboxes.append(box)
+        return newboxes
+
+    def optimize_operations(self):
+        newoperations = []
+        for op in self.loop.operations:
+            newop = op
+            for optimization in self.optlist:
+                newop = optimization.handle_op(self, newop)
+                if newop is None:
+                    break
+            if newop is not None:
+                newoperations.append(newop)
+        print "Length of the loop:", len(newoperations)
+        self.loop.operations = newoperations
+
+    def clean_up_caches(self, newoperations):
+        for descr, v in self.field_caches.iteritems():
+            for instnode, fieldnode in v:
+                newoperations.append(ResOperation(rop.SETFIELD_GC,
+                    [instnode.source, fieldnode.source], None, descr))
+                del instnode.cleanfields[descr]
+                del instnode.dirtyfields[descr]
+    
+    def optimize_loop(self, loop):
+        self.nodes = {}
+        self.field_caches = {}
+        self.loop = loop
+        self.find_nodes()
+        self.optimize_operations()
+
+# -------------------------------------------------------------------
+
+class AbstractOptimization(object):
+
+    def __init__(self):
+        'NOT_RPYTHON'
+        operations = [None] * (rop._LAST+1)
+        for key, value in rop.__dict__.items():
+            if key.startswith('_'):
+                continue
+            methname = key.lower()
+            if hasattr(self, methname):
+                func = getattr(self, methname).im_func
+            else:
+                func = getattr(self, 'handle_default_op').im_func
+            operations[value] = func
+        self.operations = operations
+
+    def handle_op(self, spec, op):
+        func = self.operations[op.opnum]
+        return func(self, spec, op)
+    
+    def handle_default_op(self, spec, op):
+        return op
+
+
+class OptimizeGuards(AbstractOptimization):
+
+    def optimize_guard(self, spec, op):
+        assert len(op.suboperations) == 1
+        op_fail = op.suboperations[0]
+        op_fail.args = spec.new_arguments(op_fail)
+        # modification in place. Reason for this is explained in mirror
+        # in optimize.py
+        op.suboperations = [op_fail]
+        return op
+
+    def guard_class(self, spec, op):
+        node = spec.getnode(op.args[0])
+        if node.cls is not None:
+            # assert that they're equal maybe
+            return
+        node.cls = InstanceNode(op.args[1], const=True)
+        return self.optimize_guard(spec, op)
+
+##     def guard_value(self, spec, op):
+##         instnode = spec.nodes[op.args[0]]
+##         assert isinstance(op.args[1], Const)
+##         if instnode.const:
+##             return
+##         self.optimize_guard(spec, op)
+##         instnode.const = True
+##         instnode.source = op.args[0].constbox()
+##         return op
+
+##     def guard_nonvirtualized(self, spec, op):
+##         return
+
+##     def handle_default_op(self, spec, op):
+##         if op.is_guard():
+##             return self.optimize_guard(op)
+##         return op
+
+
+
+# -------------------------------------------------------------------
+
+OPTLIST = [
+    OptimizeGuards(),
+    ]
+specializer = Specializer(OPTLIST)
+
+def optimize_loop(options, old_loops, loop, cpu=None):
+    if old_loops:
+        assert len(old_loops) == 1
+        return old_loops[0]
+    else:
+        specializer.optimize_loop(loop)
+        return None
+
+def optimize_bridge(options, old_loops, loop, cpu=None):
+    optimize_loop(options, [], loop, cpu)
+    return old_loops[0]
+
+class Optimizer:
+    optimize_loop = staticmethod(optimize_loop)
+    optimize_bridge = staticmethod(optimize_bridge)
+
+
---
+++ pypy/jit/metainterp/test/test_optimize3.py
@@ -0,0 +1,148 @@
+import py
+from pypy.rpython.lltypesystem import lltype, llmemory
+from pypy.rpython.ootypesystem import ootype
+from pypy.rpython.lltypesystem.rclass import OBJECT, OBJECT_VTABLE
+
+from pypy.jit.metainterp.resoperation import rop, ResOperation
+from pypy.jit.metainterp.history import ConstAddr, BoxPtr, TreeLoop,\
+     ConstInt, BoxInt, BoxObj, ConstObj
+from pypy.jit.backend.llgraph import runner
+
+from pypy.jit.metainterp.optimize3 import AbstractOptimization
+from pypy.jit.metainterp.optimize3 import optimize_loop
+from pypy.jit.metainterp.test.test_optimize import equaloplists, ANY
+
+def test_AbstractOptimization():
+    
+    class MyOpt(AbstractOptimization):
+        def int_add(self, spec, op):
+            return 'hello world', op
+
+    class MyOpt2(MyOpt):
+        def handle_default_op(self, spec, op):
+            return 'default op', op
+
+    myopt = MyOpt()
+    myopt2 = MyOpt2()
+    op = ResOperation(rop.INT_ADD, [], None)
+    assert myopt.handle_op(None, op) == ('hello world', op)
+    assert myopt2.handle_op(None, op) == ('hello world', op)
+
+    op = ResOperation(rop.INT_SUB, [], None)
+    assert myopt.handle_op(None, op) == op
+    assert myopt2.handle_op(None, op) == ('default op', op)
+
+
+class LLtypeMixin(object):
+
+    node_vtable = lltype.malloc(OBJECT_VTABLE, immortal=True)
+    node_vtable_adr = llmemory.cast_ptr_to_adr(node_vtable)
+    cpu = runner.LLtypeCPU(None)
+    vtable_box = ConstAddr(node_vtable_adr, cpu)
+
+    NODE = lltype.GcForwardReference()
+    NODE.become(lltype.GcStruct('NODE', ('parent', OBJECT),
+                                        ('value', lltype.Signed),
+                                        ('next', lltype.Ptr(NODE))))
+    node = lltype.malloc(NODE)
+    nodebox = BoxPtr(lltype.cast_opaque_ptr(llmemory.GCREF, node))
+    nodedescr = cpu.fielddescrof(NODE, 'value')
+
+class OOtypeMixin(object):
+    cpu = runner.OOtypeCPU(None)
+
+    NODE = ootype.Instance('NODE', ootype.ROOT, {})
+    NODE._add_fields({'value': ootype.Signed,
+                      'next': NODE})
+
+    node_vtable = ootype.runtimeClass(NODE)
+    vtable_box = ConstObj(ootype.cast_to_object(node_vtable))
+
+    node = ootype.new(NODE)
+    nodebox = BoxObj(ootype.cast_to_object(node))
+    nodedescr = cpu.fielddescrof(NODE, 'value')
+
+
+
+class BaseTestOptimize3(object):
+
+    @staticmethod
+    def newloop(inputargs, operations):
+        loop = TreeLoop("test")
+        loop.inputargs = inputargs
+        loop.operations = operations
+        return loop
+
+
+    def test_remove_guard_class(self):
+        ops = [
+            ResOperation(rop.GUARD_CLASS, [self.nodebox, self.vtable_box], None),
+            ResOperation(rop.GUARD_CLASS, [self.nodebox, self.vtable_box], None),
+        ]
+        ops[0].suboperations = [ResOperation(rop.FAIL, [], None)]
+        ops[1].suboperations = [ResOperation(rop.FAIL, [], None)]
+        loop = self.newloop([self.nodebox], ops)
+        optimize_loop(None, [], loop)
+        assert len(loop.operations) == 1
+
+    def test_remove_consecutive_guard_value_constfold(self):
+        py.test.skip('in-progress')
+        n = BoxInt(0)
+        n1 = BoxInt(1)
+        n2 = BoxInt(3)
+        ops = [
+            ResOperation(rop.GUARD_VALUE, [n, ConstInt(0)], None),
+            ResOperation(rop.INT_ADD, [n, ConstInt(1)], n1),
+            ResOperation(rop.GUARD_VALUE, [n1, ConstInt(1)], None),
+            ResOperation(rop.INT_ADD, [n1, ConstInt(2)], n2),
+        ]
+        ops[0].suboperations = [ResOperation(rop.FAIL, [], None)]
+        ops[2].suboperations = [ResOperation(rop.FAIL, [], None)]
+        loop = self.newloop([n], ops)
+        optimize_loop(None, [], loop)
+        equaloplists(loop.operations, [
+            ResOperation(rop.GUARD_VALUE, [n, ConstInt(0)], None),
+            ])
+
+    def test_remove_consecutive_getfields(self):
+        py.test.skip('in-progress')
+        n1 = BoxInt()
+        n2 = BoxInt()
+        n3 = BoxInt()
+        ops = [
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n1, self.nodedescr),
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n2, self.nodedescr),
+            ResOperation(rop.INT_ADD, [n1, n2], n3),
+        ]
+        loop = self.newloop([self.nodebox], ops)
+        optimize_loop(None, [], loop)
+        equaloplists(loop.operations, [
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n1, self.nodedescr),
+            ResOperation(rop.INT_ADD, [n1, n1], n3),
+            ])
+
+    def test_setfield_getfield_clean_cache(self):
+        py.test.skip('in-progress')
+        n1 = BoxInt()
+        n2 = BoxInt()
+        n3 = BoxInt()
+        ops = [
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n1, self.nodedescr),
+            ResOperation(rop.SETFIELD_GC, [self.nodebox, ConstInt(3)], None, self.nodedescr),
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n2, self.nodedescr),
+            ResOperation(rop.CALL, [n2], None),
+        ]
+        loop = self.newloop([self.nodebox], ops)
+        optimize_loop(None, [], loop)
+        equaloplists(loop.operations, [
+            ResOperation(rop.GETFIELD_GC, [self.nodebox], n1, self.nodedescr),
+            ResOperation(rop.SETFIELD_GC, [self.nodebox, ConstInt(3)], None, self.nodedescr),
+            ResOperation(rop.CALL, [ConstInt(3)], None),
+            ])
+
+
+class TestLLtype(LLtypeMixin, BaseTestOptimize3):
+    pass
+
+class TestOOtype(OOtypeMixin, BaseTestOptimize3):
+    pass
