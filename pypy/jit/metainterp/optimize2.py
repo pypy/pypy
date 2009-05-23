2293217537456b0db2577b9eeba96204868b45bb
Maciej Fijalkowski <fijall@gmail.com>
2009-05-23 17:22:00 +0000

    kill all optimizations for now, will try to readd them in a modular manner

--- pypy/jit/metainterp/optimize2.py
+++ pypy/jit/metainterp/optimize2.py
@@ -87,44 +87,8 @@ class Specializer(object):
         newoperations = []
         for op in self.loop.operations:
             if op.is_guard():
-                if op.opnum == rop.GUARD_NONVIRTUALIZED:
-                    continue
-                elif op.opnum == rop.GUARD_CLASS:
-                    node = self.getnode(op.args[0])
-                    if node.cls is not None:
-                        # assert that they're equal maybe
-                        continue
-                    node.cls = InstanceNode(op.args[1], const=True)
-                elif op.opnum == rop.GUARD_VALUE:
-                    instnode = self.nodes[op.args[0]]
-                    assert isinstance(op.args[1], Const)
-                    if instnode.const:
-                        continue
-                    self.optimize_guard(op)
-                    instnode.const = True
-                    instnode.source = op.args[0].constbox()
-                    newoperations.append(op)
-                    continue
                 self.optimize_guard(op)
                 newoperations.append(op)
-                continue
-            elif op.opnum == rop.GETFIELD_GC:
-                instnode = self.getnode(op.args[0])
-                descr = op.descr
-                node = instnode.cleanfields.get(descr, None)
-                if node is not None:
-                    self.nodes[op.result] = node
-                    continue
-                else:
-                    instnode.cleanfields[descr] = self.getnode(op.result)
-            elif op.opnum == rop.SETFIELD_GC:
-                instnode = self.getnode(op.args[0])
-                descr = op.descr
-                node = self.getnode(op.args[1])
-                instnode.dirtyfields[descr] = node
-                instnode.cleanfields[descr] = node
-                l = self.field_caches.setdefault(descr, [])
-                l.append((instnode, node))
                 continue
             # default handler
             op = op.clone()
@@ -140,19 +104,9 @@ class Specializer(object):
                     instnode = InstanceNode(box.constbox(), const=True)
                     self.nodes[box] = instnode
                     continue
-            elif not op.has_no_side_effect():
-                self.clean_up_caches(newoperations)
             newoperations.append(op)
         print "Length of the loop:", len(newoperations)
         self.loop.operations = newoperations
-
-    def clean_up_caches(self, newoperations):
-        for descr, v in self.field_caches.iteritems():
-            for instnode, fieldnode in v:
-                newoperations.append(ResOperation(rop.SETFIELD_GC,
-                    [instnode.source, fieldnode.source], None, descr))
-                del instnode.cleanfields[descr]
-                del instnode.dirtyfields[descr]
     
     def optimize_loop(self, loop):
         self.nodes = {}
