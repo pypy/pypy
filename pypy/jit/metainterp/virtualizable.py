dbcdc1d3db594034e86bdbd1f560fca3249448ca
Maciej Fijalkowski <fijall@gmail.com>
2009-05-24 21:18:51 +0000

    fix mro

--- pypy/jit/metainterp/virtualizable.py
+++ pypy/jit/metainterp/virtualizable.py
@@ -7,7 +7,7 @@ from pypy.rpython.annlowlevel import llhelper, cast_base_ptr_to_instance
 from pypy.annotation.model import lltype_to_annotation
 from pypy.rlib.objectmodel import we_are_translated
 
-class VirtualizableDesc(history.AbstractValue):
+class VirtualizableDesc(history.AbstractDescr):
     hash = 0
 
     def __init__(self, cpu, TOPSTRUCT, STRUCTTYPE):
