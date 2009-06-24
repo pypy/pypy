d94973210dc055dcad2f4fc117b0d90f2a44853e
Maciej Fijalkowski <fijall@gmail.com>
2009-06-24 18:39:50 +0000

    oops, forgotten to check in translated tests

---
+++ pypy/jit/tl/spli/test/test_translated.py
@@ -0,0 +1,24 @@
+
+from pypy.rpython.test.test_llinterp import interpret
+from pypy.jit.tl.spli import interpreter, objects
+from pypy.jit.tl.spli.serializer import serialize, deserialize
+
+class TestSPLITranslated(object):
+
+    def test_one(self):
+        def f(a, b):
+            return a + b
+        data = serialize(f.func_code)
+        space = objects.DumbObjSpace()
+        def run(a, b):
+            co = deserialize(data, space)
+            frame = interpreter.SPLIFrame(co)
+            frame.locals[0] = space.wrap(a)
+            frame.locals[1] = space.wrap(b)
+            w_res = frame.run()
+            assert isinstance(w_res, objects.Int)
+            return w_res.value
+
+        assert run(2, 3) == 5
+        res = interpret(run, [2, 3])
+        assert res == 5
