e3c386b0f975a29f2204d02bb8a610026d7a9228
Armin Rigo <arigo@tunes.org>
2009-06-30 21:12:00 +0000

    This directory is also slow to run.

--- pypy/testrunner_cfg.py
+++ pypy/testrunner_cfg.py
@@ -4,7 +4,8 @@ def collect_one_testdir(testdirs, reldir, tests):
     if (reldir.startswith('translator/c/') or 
         reldir.startswith('rlib/test') or
         reldir.startswith('rpython/memory/') or
-        reldir.startswith('jit/backend/x86')):
+        reldir.startswith('jit/backend/x86/') or
+        reldir.startswith('jit/backend/minimal/')):
         testdirs.extend(tests)
     else:
         testdirs.append(reldir)
