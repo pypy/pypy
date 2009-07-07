8aee6f1016ead41449b935fa3f1d910107982ba0
Antonio Cuni <anto.cuni@gmail.com>
2009-07-07 08:59:33 +0000

    first tla tests

---
+++ pypy/jit/tl/test/test_tla.py
@@ -0,0 +1,24 @@
+import py
+from pypy.jit.tl import tla
+
+def test_stack():
+    f = tla.Frame('')
+    f.push(1)
+    f.push(2)
+    f.push(3)
+    assert f.pop() == 3
+    assert f.pop() == 2
+    assert f.pop() == 1
+    py.test.raises(AssertionError, f.pop)
+
+
+def test_W_IntObject():
+    w_a = tla.W_IntObject(0)
+    w_b = tla.W_IntObject(10)
+    w_c = tla.W_IntObject(32)
+    assert not w_a.is_true()
+    assert w_b.is_true()
+    assert w_c.is_true()
+    assert w_b.add(w_c).intvalue == 42
+
+
