913d2d881047f46b6f818c4bb1cd3e0a10df745c
Armin Rigo <arigo@tunes.org>
2009-07-10 17:19:11 +0000

    Skip a test that is not supposed to be translated.

--- pypy/jit/backend/x86/test/test_zrpy_recursive.py
+++ pypy/jit/backend/x86/test/test_zrpy_recursive.py
@@ -1,7 +1,9 @@
 
-
+import py
 from pypy.jit.metainterp.test.test_recursive import RecursiveTests
 from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
 
 class TestRecursive(Jit386Mixin, RecursiveTests):
-    pass
+
+    def test_inline_faulty_can_inline(self):
+        py.test.skip("this test is not supposed to be translated")
