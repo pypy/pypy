2a2791a4c47a4cd9b27cd8e627b3dfe1b47df640
Maciej Fijalkowski <fijall@gmail.com>
2009-07-20 09:00:47 +0000

    Implement some basics of multi-dim arrays

--- pypy/module/micronumpy/test/test_numpy.py
+++ pypy/module/micronumpy/test/test_numpy.py
@@ -39,3 +39,27 @@ class AppTestNumpy(object):
         assert x[4] == 0
         assert len(x) == 5
         raises(ValueError, minimum, ar, zeros(3, dtype=int))
+
+class AppTestMultiDim(object):
+    def setup_class(cls):
+        cls.space = gettestobjspace(usemodules=('micronumpy',))
+
+    def test_multidim(self):
+        from numpy import zeros
+        ar = zeros((3, 3), dtype=int)
+        assert ar[0, 2] == 0
+        raises(IndexError, ar.__getitem__, (3, 0))
+        assert ar[-2, 1] == 0
+
+    def test_multidim_getset(self):
+        from numpy import zeros
+        ar = zeros((3, 3, 3), dtype=int)
+        ar[1, 2, 1] = 3
+        assert ar[1, 2, 1] == 3
+        assert ar[-2, 2, 1] == 3
+        assert ar[2, 2, 1] == 0
+        assert ar[-2, 2, -2] == 3
+
+    def test_len(self):
+        from numpy import zeros
+        assert len(zeros((3, 2, 1), dtype=int)) == 3
