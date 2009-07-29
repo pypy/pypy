bcb4dd9a886c30f348d609238bc8c12f7195df12
Armin Rigo <arigo@tunes.org>
2009-07-29 16:10:05 +0000

    Write the test.  Still not implemented.

--- pypy/jit/metainterp/test/test_virtualizable.py
+++ pypy/jit/metainterp/test/test_virtualizable.py
@@ -637,10 +637,43 @@ class ImplicitVirtualizableTests:
         res = self.meta_interp(f, [240], policy=StopAtXPolicy(g))
         assert res == f(240)
 
-    def test_external_access_sometimes(self):
+    def test_external_read_sometimes(self):
         py.test.skip("known bug: access the frame in a residual call but"
                      " only sometimes, so that it's not seen during tracing")
+        jitdriver = JitDriver(greens = [], reds = ['frame'],
+                              virtualizables = ['frame'])
+        
+        class Frame(object):
+            _virtualizable2_ = ['x', 'y']
+        class SomewhereElse:
+            pass
+        somewhere_else = SomewhereElse()
 
+        def g():
+            somewhere_else.counter += 1
+            if somewhere_else.counter == 70:
+                result = somewhere_else.top_frame.y     # external read
+                debug_print(lltype.Void, '-+-+-+-+- external read:', result)
+                assert result == 79
+            else:
+                result = 1
+            return result
+
+        def f(n):
+            frame = Frame()
+            frame.x = n
+            frame.y = 10
+            somewhere_else.counter = 0
+            somewhere_else.top_frame = frame
+            while frame.x > 0:
+                jitdriver.can_enter_jit(frame=frame)
+                jitdriver.jit_merge_point(frame=frame)
+                frame.x -= g()
+                frame.y += 1
+            return frame.x
+
+        res = self.meta_interp(f, [123], policy=StopAtXPolicy(g))
+        assert res == f(123)
 
     def test_promote_index_in_virtualizable_list(self):
         jitdriver = JitDriver(greens = [], reds = ['frame', 'n'],
