2c93f905466fd88ac0078384bb5819ed3f675caa
Maciej Fijalkowski <fijall@gmail.com>
2009-08-19 13:32:02 +0000

    (pedronis, fijal) Remove some carefulness code, since this case should not occur any more.

--- pypy/jit/backend/x86/regalloc.py
+++ pypy/jit/backend/x86/regalloc.py
@@ -250,14 +250,11 @@ class RegAlloc(object):
         if (operations[i + 1].opnum != rop.GUARD_TRUE and
             operations[i + 1].opnum != rop.GUARD_FALSE):
             return False
-        if operations[i + 1].args[0] is not op.result:
-            return False
-        if self.longevity[op.result][1] > i + 1:
-            return False
-        if op.result in operations[i + 1].inputargs:
-            # XXX implement optimization that replace var with a const
-            #     not right now
-            return False
+        if (operations[i + 1].args[0] is not op.result or
+            self.longevity[op.result][1] > i + 1 or
+            op.result in operations[i + 1].inputargs):
+            print "boolean flag not optimized away"
+            assert False
         return True
 
     def walk_operations(self, tree):
