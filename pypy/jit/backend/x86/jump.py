b4a263e610b18a0338430b505fab5f8f2e576a8e
Armin Rigo <arigo@tunes.org>
2009-08-23 13:09:54 +0000

    (benjamin, arigo) Deal with: no free regs and eax is a source. Add a code comparison function to the test.

--- pypy/jit/backend/x86/jump.py
+++ pypy/jit/backend/x86/jump.py
@@ -1,24 +1,26 @@
-from pypy.jit.backend.x86.ri386 import MODRM, eax
+from pypy.jit.backend.x86.ri386 import *
 
 
 def remap_stack_layout(assembler, src_locations, dst_locations, free_regs=[]):
-    pending_pops = []
+    pushed_eax = False
     for i in range(len(dst_locations)):
         src = src_locations[i]
         dst = dst_locations[i]
         if src is not dst:
+            if pushed_eax and src is eax:
+                assembler.regalloc_load(mem(esp, 0), src)
             if isinstance(dst, MODRM):
                 if isinstance(src, MODRM):
                     if free_regs:
                         tmp = free_regs[0]
                     else:
                         assembler.regalloc_push(eax)
-                        pending_pops.append(eax)
+                        pushed_eax = True
                         tmp = eax
                     assembler.regalloc_load(src, tmp)
                     src = tmp
                 assembler.regalloc_store(src, dst)
             else:
                 assembler.regalloc_load(src, dst)
-    for reg in pending_pops:
-        assembler.regalloc_pop(reg)
+    if pushed_eax:
+        assembler.regalloc_pop(eax)
--- pypy/jit/backend/x86/test/test_jump.py
+++ pypy/jit/backend/x86/test/test_jump.py
@@ -17,6 +17,22 @@ class MockAssembler:
     def regalloc_pop(self, loc):
         self.ops.append(('pop', loc))
 
+    def got(self, expected):
+        print '------------------------ comparing ---------------------------'
+        for op1, op2 in zip(self.ops, expected):
+            print '%-38s| %-38s' % (op1, op2)
+            if op1 == op2:
+                continue
+            assert len(op1) == len(op2)
+            for x, y in zip(op1, op2):
+                if isinstance(x, MODRM) and isinstance(y, MODRM):
+                    assert x.byte == y.byte
+                    assert x.extradata == y.extradata
+                else:
+                    assert x == y
+        assert len(self.ops) == len(expected)
+        return True
+
 
 def test_trivial():
     assembler = MockAssembler()
@@ -64,3 +80,19 @@ def test_simple_stacklocs_no_free_reg():
                              ('store', ebx, s24),
                              ('load', s12, edi),
                              ('pop', eax)]
+
+def test_simple_stacklocs_no_free_reg_2():
+    assembler = MockAssembler()
+    s8 = mem(ebp, -8)
+    s12 = mem(ebp, -12)
+    s20 = mem(ebp, -20)
+    s24 = mem(ebp, -24)
+    p0 = mem(esp, 0)
+    remap_stack_layout(assembler, [s8, eax, s12], [s20, s24, edi], [])
+    assert assembler.got([('push', eax),
+                          ('load', s8, eax),
+                          ('store', eax, s20),
+                          ('load', p0, eax),
+                          ('store', eax, s24),
+                          ('load', s12, edi),
+                          ('pop', eax)])
