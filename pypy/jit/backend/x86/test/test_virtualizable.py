4f262004e1377f84eb1b455f0658ab4eb9dd8ecb
Maciej Fijalkowski <fijall@gmail.com>
2009-09-01 17:11:02 +0000

    update skips

--- pypy/jit/backend/x86/test/test_virtualizable.py
+++ pypy/jit/backend/x86/test/test_virtualizable.py
@@ -4,11 +4,5 @@ from pypy.jit.metainterp.test.test_virtualizable import ImplicitVirtualizableTes
 from pypy.jit.backend.x86.test.test_basic import Jit386Mixin
 
 class TestVirtualizable(Jit386Mixin, ImplicitVirtualizableTests):
-    def test_pass_always_virtual_to_bridge(self):
-        py.test.skip("Not implemented nonsense in patch_jump")
-
-    def test_virtual_obj_on_always_virtual(self):
-        py.test.skip("Widening to trash error")
-
-    def test_virtual_obj_on_always_virtual_more_bridges(self):
-        py.test.skip("Widening to trash error")
+    def test_blackhole_should_not_reenter(self):
+        py.test.skip("Assertion error & llinterp mess")
