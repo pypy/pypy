6646feb14e45dc4c04137943a2a8a2d9dd41a53f
Benjamin Peterson <benjamin@python.org>
2009-09-12 18:48:54 +0000

    remove very old XXXy comments

--- pypy/module/__builtin__/test/test_minmax.py
+++ pypy/module/__builtin__/test/test_minmax.py
@@ -11,17 +11,11 @@ class AppTestMin:
    def test_min_floats(self):
       assert min(0.1, 2.7, 14.7) == 0.1
 
-   # write fixed point if that becomes a type.
-
    def test_min_chars(self):
       assert min('a', 'b', 'c') == 'a'
 
-   # write a unicode test when unicode works.
-
    def test_min_strings(self):
       assert min('aaa', 'bbb', 'c') == 'aaa'
-
-   # write an imaginary test when we have complex numbers  
 
    def test_min_mixed(self):
       assert min('1', 2, 3, 'aa') == 2
@@ -43,17 +37,11 @@ class AppTestMax:
    def test_max_floats(self):
       assert max(0.1, 2.7, 14.7) == 14.7
 
-   # write fixed point if that becomes a type.
-
    def test_max_chars(self):
       assert max('a', 'b', 'c') == 'c'
 
-   # write a unicode test when unicode works.
-
    def test_max_strings(self):
       assert max('aaa', 'bbb', 'c') == 'c'
-
-   # write an imaginary test when we have complex numbers
 
    def test_max_mixed(self):
       assert max('1', 2, 3, 'aa') == 'aa'
