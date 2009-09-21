858757bb31ae38c68d8f723a9036bb1f2d63a8ca
Armin Rigo <arigo@tunes.org>
2009-09-21 13:51:55 +0000

    These tests are meant to be run on top of pypy-c only. There are issues with different versions of ctypes.

--- pypy/lib/app_test/ctypes_tests/conftest.py
+++ pypy/lib/app_test/ctypes_tests/conftest.py
@@ -1,6 +1,12 @@
 import py
 import sys
 
+class Directory(py.test.collect.Directory):
+    def collect(self):
+        if '__pypy__' not in sys.builtin_module_names:
+            py.test.skip("these tests are meant to be run on top of pypy-c")
+        return super(Directory, self).collect()
+
 def compile_so_file():
     from pypy.translator.platform import platform
     from pypy.translator.tool.cbuild import ExternalCompilationInfo
