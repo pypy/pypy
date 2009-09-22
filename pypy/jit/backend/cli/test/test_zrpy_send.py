4050beb6177efa3f809999566a12894822fddcc3
Antonio Cuni <anto.cuni@gmail.com>
2009-09-22 08:19:08 +0000

    xfail these two tests for now

--- pypy/jit/backend/cli/test/test_zrpy_send.py
+++ pypy/jit/backend/cli/test/test_zrpy_send.py
@@ -10,3 +10,8 @@ class TestSend(CliTranslatedJitMixin, test_send.TestOOtype):
     def test_recursive_call_to_portal_from_blackhole(self):
         py.test.skip('string return values are not supported')
 
+    test_oosend_guard_failure = py.test.mark.xfail(
+        test_send.TestOOtype.test_oosend_guard_failure.im_func)
+    
+    test_oosend_guard_failure_2 = py.test.mark.xfail(
+        test_send.TestOOtype.test_oosend_guard_failure_2.im_func)
