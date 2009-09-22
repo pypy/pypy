6be1c6b19e97fc57fdbe2f7d5da7f4d0c69d76af
Samuele Pedroni <pedronis@lucediurna.net>
2009-09-22 09:11:14 +0000

    (micke, pedronis) this test doesn't loop at all so it is a bit useless as a jit test

--- pypy/jit/metainterp/test/test_tl.py
+++ pypy/jit/metainterp/test/test_tl.py
@@ -6,32 +6,6 @@ from pypy.jit.metainterp.test.test_basic import OOJitMixin, LLJitMixin
 
 class ToyLanguageTests:
 
-    def test_tiny1(self):
-        myjitdriver = JitDriver(greens = ['s', 'pc'],
-                                reds = ['y', 'acc'])
-
-        def ll_plus_minus(s, x, y):
-            acc = x
-            pc = 0
-            while pc < len(s):
-                myjitdriver.can_enter_jit(y=y, pc=pc, acc=acc, s=s)
-                myjitdriver.jit_merge_point(y=y, pc=pc, acc=acc, s=s)
-                op = s[pc]
-                if op == '+':
-                    acc += y
-                elif op == '-':
-                    acc -= y
-                pc += 1
-            return acc
-
-        codes = ["++++++++++", "+-++---+-++-"]
-        def main(n, x, y):
-            code = codes[n]
-            return ll_plus_minus(code, x, y)
-
-        res = self.meta_interp(main, [0, 100, 2])
-        assert res == 120
-
     def test_tlr(self):
         from pypy.jit.tl.tlr import interpret, SQUARE
 
