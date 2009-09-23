e9a473012dc06a1377337b7b6e36cd41ea94228a
Antonio Cuni <anto.cuni@gmail.com>
2009-09-23 13:15:51 +0000

    - kill outdated comment
    - factor out common code between lltype and ootype, thus enabling inlining also for ootype

--- pypy/jit/tl/pypyjit_child.py
+++ pypy/jit/tl/pypyjit_child.py
@@ -4,25 +4,6 @@ from pypy.jit.metainterp import warmspot
 from pypy.module.pypyjit.policy import PyPyJitPolicy
 from pypy.rlib.jit import OPTIMIZER_FULL
 
-# Current output: http://paste.pocoo.org/show/106540/
-#
-# Some optimizations missing:
-#
-#   - remove the useless 'ec' argument (p1 and p115 in the trace)
-#
-#   - the guards have very long 'liveboxes' lists containing mostly
-#     Consts -- make sure that these Consts are not stored, or else
-#     remove them entirely
-
-# Some optimizations that might help under different circumstances:
-#
-#   - figure out who calls W_TypeObject.is_heaptype(), leading to
-#     the "int_and 512" (lines 48, 147, 154)
-#
-#   - improve the optimization: e.g. ooisnull followed by oononnull
-#     on the same variable
-#
-
 
 def run_child(glob, loc):
     import sys, pdb
@@ -34,26 +15,25 @@ def run_child(glob, loc):
         return lltype.nullptr(T)
     interp.heap.malloc_nonmovable = returns_null     # XXX
 
-    print 'warmspot.jittify_and_run() started...'
     from pypy.jit.backend.llgraph.runner import LLtypeCPU
     LLtypeCPU.supports_floats = False    # for now
-    policy = PyPyJitPolicy(interp.typer.annotator.translator)
-    option.view = True
-    warmspot.jittify_and_run(interp, graph, [], policy=policy,
-                             listops=True, CPUClass=LLtypeCPU,
-                             backendopt=True, inline=True,
-                             optimizer=OPTIMIZER_FULL)
+    apply_jit(interp, graph, LLtypeCPU)
 
 
 def run_child_ootype(glob, loc):
     import sys, pdb
     interp = loc['interp']
     graph = loc['graph']
-
-    print 'warmspot.jittify_and_run() started...'
     from pypy.jit.backend.llgraph.runner import OOtypeCPU
+    apply_jit(interp, graph, OOtypeCPU)
+
+
+def apply_jit(interp, graph, CPUClass):
+    print 'warmspot.jittify_and_run() started...'
     policy = PyPyJitPolicy(interp.typer.annotator.translator)
     option.view = True
     warmspot.jittify_and_run(interp, graph, [], policy=policy,
-                             listops=True, CPUClass=OOtypeCPU,
-                             backendopt=True)
+                             listops=True, CPUClass=CPUClass,
+                             backendopt=True, inline=True,
+                             optimizer=OPTIMIZER_FULL)
+
