571906ffe839f1dc55fefab0a8e9c1ca7a075177
Samuele Pedroni <pedronis@lucediurna.net>
2009-10-14 13:28:51 +0000

    (cfbolz, pedronis) kill z tests that should not be needed anymore to reach enough coverage, the new ztranslation tests plus the remaining zrpy tests should be enough hopefully

--- pypy/jit/backend/x86/test/test_zrpy_virtualizable.py
+++
@@ -1,9 +0,0 @@
-
-import py
-from pypy.jit.metainterp.test import test_virtualizable
-from pypy.jit.backend.x86.test.test_zrpy_slist import Jit386Mixin
-
-
-class TestLLImplicitVirtualizable(Jit386Mixin,
-                       test_virtualizable.ImplicitVirtualizableTests):
-    slow = True
