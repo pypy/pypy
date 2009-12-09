d5c16fc702774c86c8b1b32a5d87a19598da0642
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2009-12-09 22:49:00 +0000

    skip the grp module on Windows

--- pypy/lib/test2/test_grp_extra.py
+++ pypy/lib/test2/test_grp_extra.py
@@ -1,5 +1,8 @@
 import py
-from pypy.lib import grp
+try:
+    from pypy.lib import grp
+except ImportError:
+    py.test.skip("No grp module on this platform")
 
 def test_extra():
     py.test.raises(TypeError, grp.getgrnam, False)
