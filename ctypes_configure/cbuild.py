303acf6543ba2bbd30085d2ec1f99072226d6457
Maciej Fijalkowski <fijall@gmail.com>
2010-01-21 17:46:27 +0000

    kill the useless dependency on py.log

--- ctypes_configure/cbuild.py
+++ ctypes_configure/cbuild.py
@@ -5,8 +5,6 @@ import distutils
 
 debug = 0
 
-log = py.log.Producer("cbuild")
-
 configdir = py.path.local.make_numbered_dir(prefix='ctypes_configure')
 
 class ExternalCompilationInfo(object):
@@ -327,7 +325,7 @@ def import_module_from_directory(dir, modname):
 def log_spawned_cmd(spawn):
     def spawn_and_log(cmd, *args, **kwds):
         if debug:
-            log.execute(' '.join(cmd))
+            print ' '.join(cmd)
         return spawn(cmd, *args, **kwds)
     return spawn_and_log
 
