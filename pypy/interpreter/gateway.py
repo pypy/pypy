4101d8177effbc0e1a28e385fea06bf0edb43672
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-02-01 10:49:01 +0000

    ouch-ish: the functions in the Python interpreter written at app-level didn't use a module dict for their globals, which makes the code in them a lot slower when jitted.

--- pypy/interpreter/gateway.py
+++ pypy/interpreter/gateway.py
@@ -917,7 +917,7 @@ class ApplevelCache(SpaceCache):
 def build_applevel_dict(self, space):
     "NOT_RPYTHON"
     from pypy.interpreter.pycode import PyCode
-    w_glob = space.newdict()
+    w_glob = space.newdict(module=True)
     space.setitem(w_glob, space.wrap('__name__'), space.wrap(self.modname))
     space.exec_(self.code, w_glob, w_glob,
                 hidden_applevel=self.hidden_applevel)
