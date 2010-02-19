86ac33faa719414bf4f5743f1cf5fe825cf5cd9a
David Schneider <david.schneider@picle.org>
2010-02-19 14:53:21 +0000

    (cfbolz, david) this is needed too

--- pypy/rlib/rgc.py
+++ pypy/rlib/rgc.py
@@ -274,6 +274,7 @@ def ll_arraycopy(source, dest, source_start, dest_start, length):
                          llmemory.sizeof(TP.OF) * length)
     keepalive_until_here(source)
     keepalive_until_here(dest)
+ll_arraycopy._annenforceargs_ = [None, None, int, int, int]
 ll_arraycopy._annspecialcase_ = 'specialize:ll'
 ll_arraycopy._jit_look_inside_ = False
 
