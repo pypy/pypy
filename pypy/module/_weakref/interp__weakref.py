38fcbde61f39e134fc9b6ae89457e4aa7075b262
Maciej Fijalkowski <fijall@gmail.com>
2010-03-01 04:30:03 +0000

    Revert 71579, it breaks tests

--- pypy/module/_weakref/interp__weakref.py
+++ pypy/module/_weakref/interp__weakref.py
@@ -141,9 +141,12 @@ class W_Weakref(W_WeakrefBase):
         if w_obj is None:
             return self.space.w_None
         return w_obj
-        
 
-def descr__new__weakref(space, w_subtype, w_obj, w_callable=None):
+def descr__new__weakref(space, w_subtype, w_obj, __args__):
+    if __args__.arguments_w:
+        w_callable = __args__.arguments_w[0]
+    else:
+        w_callable = space.w_None
     lifeline = w_obj.getweakref()
     if lifeline is None:
         lifeline = WeakrefLifeline(space)
@@ -184,7 +187,7 @@ W_Weakref.typedef = TypeDef("weakref",
 which is called with the weak reference as an argument when 'obj'
 is about to be finalized.""",
     __new__ = interp2app(descr__new__weakref,
-                         unwrap_spec=[ObjSpace, W_Root, W_Root, W_Root]),
+                         unwrap_spec=[ObjSpace, W_Root, W_Root, Arguments]),
     __eq__ = interp2app(descr__eq__,
                         unwrap_spec=[ObjSpace, W_Weakref, W_Root]),
     __ne__ = interp2app(descr__ne__,
