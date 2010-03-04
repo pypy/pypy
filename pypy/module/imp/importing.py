8ade0b6a57655b7d69196685a506818691563911
Maciej Fijalkowski <fijall@gmail.com>
2010-03-04 22:03:23 +0000

    Avoid string concatenation here. It's a bit of microoptimization, true, but also painless

--- pypy/module/imp/importing.py
+++ pypy/module/imp/importing.py
@@ -82,8 +82,7 @@ def check_sys_modules(space, w_modulename):
 
 def importhook(space, modulename, w_globals=None,
                w_locals=None, w_fromlist=None, level=-1):
-    timername = "importhook " + modulename
-    space.timer.start(timername)
+    space.timer.start_name("importhook", modulename)
     if not modulename and level < 0: 
         raise OperationError(
             space.w_ValueError,
@@ -131,7 +130,7 @@ def importhook(space, modulename, w_globals=None,
                                         baselevel,
                                         w_fromlist, tentative=1)
                 if w_mod is not None:
-                    space.timer.stop(timername)
+                    space.timer.stop_name("importhook", modulename)
                     return w_mod
             else:
                 rel_modulename = None
@@ -141,7 +140,7 @@ def importhook(space, modulename, w_globals=None,
     w_mod = absolute_import(space, modulename, 0, w_fromlist, tentative=0)
     if rel_modulename is not None:
         space.setitem(space.sys.get('modules'), w(rel_modulename),space.w_None)
-    space.timer.stop(timername)
+    space.timer.stop_name("importhook", modulename)
     return w_mod
 #
 importhook.unwrap_spec = [ObjSpace, str, W_Root, W_Root, W_Root, int]
