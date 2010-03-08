6e37bc4883153a5a69b6e601187b41c23bd468c3
Armin Rigo <arigo@tunes.org>
2010-03-08 23:07:39 +0000

    Add a test: we cannot use -16(%ebp) as the location of the "push esi", because that's wrong, due to the rounding done by "and esp, xxx".

---
+++ pypy/translator/c/gcc/test/msvc/track6.s
@@ -0,0 +1,15 @@
+_TEXT	SEGMENT
+_pypy_g_foo PROC					; COMDAT
+
+	push	ebp
+	mov	ebp, esp
+	and	esp, -64
+	sub	esp, 12
+	push	esi
+	call	_pypy_g_something_else
+	;; expected {4(%ebp) | %ebx, 0(%esp), %edi, (%ebp) | }
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+	ret	0
+_pypy_g_foo ENDP
