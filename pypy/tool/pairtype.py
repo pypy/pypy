677c052b8585d31db8e9a636047e4246351367f8
Maciej Fijalkowski <fijall@gmail.com>
2010-03-10 00:28:16 +0000

    Avoid copying __metaclass__ and __doc__ (doc is readonly, metaclass we don't always need)

--- pypy/tool/pairtype.py
+++ pypy/tool/pairtype.py
@@ -34,7 +34,7 @@ class extendabletype(type):
         if name == '__extend__':
             for cls in bases:
                 for key, value in dict.items():
-                    if key == '__module__':
+                    if key == '__module__' or key == '__metaclass__' or key == '__doc__':
                         continue
                     # XXX do we need to provide something more for pickling?
                     setattr(cls, key, value)
