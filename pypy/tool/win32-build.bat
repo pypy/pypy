2dbba967b10e6b5281caaccdc739a837dd30b8d4
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-03-10 15:24:07 +0000

    minor change

--- pypy/tool/win32-build.bat
+++ pypy/tool/win32-build.bat
@@ -9,17 +9,17 @@ set TRANSLATEOPTS=--batch
 set TARGET=pypy/translator/goal/targetpypystandalone
 set TARGETOPTS=
 
-set ZIPFILE=pypy-1.2-win32.zip
-
 %PYTHON% %TRANSLATE%             --output=pypy-c.exe         %TRANSLATEOPTS% %TARGET% %TARGETOPTS%
 %PYTHON% %TRANSLATE% -Ojit       --output=pypy-jit.exe       %TRANSLATEOPTS% %TARGET% %TARGETOPTS%
 %PYTHON% %TRANSLATE% --stackless --output=pypy-stackless.exe %TRANSLATEOPTS% %TARGET% %TARGETOPTS%
 %PYTHON% %TRANSLATE% --sandbox   --output=pypy-sandbox.exe   %TRANSLATEOPTS% %TARGET% %TARGETOPTS%
 
+set ZIP=zip.exe
+set ZIPFILE=pypy-1.2-win32.zip
 copy ..\expat-2.0.1\win32\bin\release\libexpat.dll .
 
 del /s pypy\lib\*.pyc lib-python\*.pyc
 del    %ZIPFILE%
-zip    %ZIPFILE% *.exe *.dll
-zip -r %ZIPFILE% pypy\lib lib-python
-zip -d %ZIPFILE% lib-python\2.5.2\plat-*
+%ZIP%    %ZIPFILE% *.exe *.dll
+%ZIP% -r %ZIPFILE% pypy\lib lib-python
+%ZIP% -d %ZIPFILE% lib-python\2.5.2\plat-*
