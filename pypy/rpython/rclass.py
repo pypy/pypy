7e4056317210980bb674b7591660f4416b353a6b
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-03-18 16:50:08 +0000

    Simplify the identity_dict: no need to store the key in the value!

--- pypy/rpython/rclass.py
+++ pypy/rpython/rclass.py
@@ -223,11 +223,11 @@ class AbstractInstanceRepr(Repr):
 
     def convert_const_exact(self, value):
         try:
-            return self.iprebuiltinstances[value][1]
+            return self.iprebuiltinstances[value]
         except KeyError:
             self.setup()
             result = self.create_instance()
-            self.iprebuiltinstances[value] = value, result
+            self.iprebuiltinstances[value] = result
             self.initialize_prebuilt_instance(value, self.classdef, result)
             return result
 
