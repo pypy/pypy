4142054fcb9953544520fc5b2b113707379468af
Benjamin Peterson <benjamin@python.org>
2010-03-18 01:07:28 +0000

    adding None here isn't required

--- pypy/interpreter/astcompiler/codegen.py
+++ pypy/interpreter/astcompiler/codegen.py
@@ -1273,7 +1273,6 @@ class FunctionCodeGenerator(AbstractFunctionCodeGenerator):
             self.add_const(doc_str.s)
             start = 1
         else:
-            self.add_const(self.space.w_None)
             start = 0
         if func.args.args:
             self._handle_nested_args(func.args.args)
