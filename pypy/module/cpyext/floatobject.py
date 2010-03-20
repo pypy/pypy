8503be060592b6c29d70644c6e2ea94ab17c7718
Jan de Mooij <jandem@codespeak.net>
2010-03-20 13:11:11 +0000

    add floatobject tests and PyFloat_AsDouble

--- pypy/module/cpyext/floatobject.py
+++ pypy/module/cpyext/floatobject.py
@@ -4,3 +4,7 @@ from pypy.module.cpyext.api import cpython_api, PyObject, make_ref
 @cpython_api([lltype.Float], PyObject)
 def PyFloat_FromDouble(space, value):
     return space.wrap(value)
+
+@cpython_api([PyObject], lltype.Float)
+def PyFloat_AsDouble(space, w_float):
+	return 10.0 #XXX: for now
