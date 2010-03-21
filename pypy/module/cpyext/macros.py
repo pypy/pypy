533cb788b8c00589f248da88c6a293531ba58a3e
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-03-21 20:17:32 +0000

    Reverted stupid change, thanks amaury.

--- pypy/module/cpyext/macros.py
+++ pypy/module/cpyext/macros.py
@@ -5,7 +5,7 @@ from pypy.module.cpyext.api import cpython_api, PyObject, make_ref
 from pypy.module.cpyext.state import State
 
 # XXX Optimize these functions and put them into macro definitions
-@cpython_api([PyObject], lltype.Void, borrowed=True)
+@cpython_api([PyObject], lltype.Void)
 def Py_DECREF(space, w_obj):
     state = space.fromcache(State)
     obj = state.py_objects_w2r.get(w_obj)
@@ -20,7 +20,7 @@ def Py_DECREF(space, w_obj):
         assert obj.c_obj_refcnt > 0
     return
 
-@cpython_api([PyObject], lltype.Void, borrowed=True)
+@cpython_api([PyObject], lltype.Void)
 def Py_INCREF(space, w_obj):
     state = space.fromcache(State)
     obj = state.py_objects_w2r.get(w_obj)
