0cd15c2dd7d1606625fc2019a14a01e4ceb5f5be
Alexander Schremmer <alex-bitbucket@alexanderweb.de>
2010-03-21 16:24:43 +0000

    Fix wrapping bug.

--- pypy/module/cpyext/test/test_typeobject.py
+++ pypy/module/cpyext/test/test_typeobject.py
@@ -5,11 +5,12 @@ import sys
 
 class AppTestTypeObject(AppTestCpythonExtensionBase):
     def test_typeobject(self):
-        skip("In progress")
+        #skip("In progress")
         import sys
         module = self.import_module(name='foo')
         assert 'foo' in sys.modules
         assert "copy" in dir(module.fooType)
+        print module.fooType.copy
         obj = module.new()
         print "Obj has type", type(obj)
         assert type(obj) is module.fooType
