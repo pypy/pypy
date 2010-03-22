f9dcd46b84f8ea6eb31bc440a34ad71db8fbf38c
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-03-22 17:17:05 +0000

    rffi has a nice function for PyString_FromStringAndSize(), use it

--- pypy/module/cpyext/stringobject.py
+++ pypy/module/cpyext/stringobject.py
@@ -3,13 +3,8 @@ from pypy.module.cpyext.api import cpython_api, PyObject, make_ref, Py_ssize_t
 
 @cpython_api([rffi.CCHARP, Py_ssize_t], PyObject)
 def PyString_FromStringAndSize(space, char_p, length):
-    l = []
-    i = 0
-    while length > 0:
-        l.append(char_p[i])
-        i += 1
-        length -= 1
-    return space.wrap("".join(l))
+    s = rffi.charpsize2str(char_p, length)
+    return space.wrap(s)
 
 @cpython_api([rffi.CCHARP], PyObject)
 def PyString_FromString(space, char_p):
