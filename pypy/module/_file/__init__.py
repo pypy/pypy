7a9a2b0fd4828bf3434d770d9af4990cd8459e32
Armin Rigo <arigo@tunes.org>
2010-03-27 11:14:48 +0000

    Ignore I/O errors when flush()ing streams when the process shuts down, instead of crashing with an RPython-level OSError.

--- pypy/module/_file/__init__.py
+++ pypy/module/_file/__init__.py
@@ -24,8 +24,8 @@ class Module(MixedModule):
         MixedModule.__init__(self, space, *args)
 
     def shutdown(self, space):
-        # at shutdown, flush all open streams
-        from pypy.module._file.interp_file import getopenstreams
+        # at shutdown, flush all open streams.  Ignore I/O errors.
+        from pypy.module._file.interp_file import getopenstreams, StreamErrors
         openstreams = getopenstreams(space)
         while openstreams:
             for stream in openstreams.keys():
@@ -34,7 +34,10 @@ class Module(MixedModule):
                 except KeyError:
                     pass    # key was removed in the meantime
                 else:
-                    stream.flush()
+                    try:
+                        stream.flush()
+                    except StreamErrors:
+                        pass
 
     def setup_after_space_initialization(self):
         from pypy.module._file.interp_file import W_File
