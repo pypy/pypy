8e0735e6335535521eb479fabdadae3aa81c9c4c
Maciej Fijalkowski <fijall@gmail.com>
2010-04-23 00:59:14 +0000

    fix name error (?)

--- pypy/translator/translator.py
+++ pypy/translator/translator.py
@@ -8,7 +8,7 @@ import autopath, os, sys, types, copy
 
 from pypy.translator import simplify
 from pypy.objspace.flow.objspace import FlowObjSpace
-from pypy.objspace.flow.model import FunctionGraph, checkgraph
+from pypy.objspace.flow.model import FunctionGraph, checkgraph, Block
 from pypy.tool.ansi_print import ansi_log
 from pypy.tool.sourcetools import nice_repr_for_func
 from pypy.config.pypyoption import pypy_optiondescription
