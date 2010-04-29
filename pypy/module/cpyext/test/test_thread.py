0ce76fd85f2a20716a0f100f60d6fffaea14410f
Benjamin Peterson <benjamin@python.org>
2010-04-29 22:59:42 +0000

    xfail tests

--- pypy/module/cpyext/test/test_thread.py
+++ pypy/module/cpyext/test/test_thread.py
@@ -1,5 +1,7 @@
+import py
 
-import thread, threading
+import thread
+import threading
 
 from pypy.module.thread.ll_thread import allocate_ll_lock
 from pypy.module.cpyext.test.test_api import BaseApiTest
@@ -22,15 +24,15 @@ class TestPyThread(BaseApiTest):
 
         assert results[0][0] != results[1][0]
 
+    @py.test.mark.xfail
+    def test_acquire_lock(self, space, api):
+        lock = allocate_ll_lock()
+        assert api.PyThread_acquire_lock(lock, space.w_int(0)) == 1
+        assert api.PyThread_acquire_lock(lock, space.w_int(1)) == 0
 
-    # def test_acquire_lock(self, space, api):
-    #     lock = allocate_ll_lock()
-    #     assert api.PyThread_acquire_lock(lock, space.w_int(0)) == 1
-    #     assert api.PyThread_acquire_lock(lock, space.w_int(1)) == 0
-
-
-    # def test_release_lock(self, space, api):
-    #     lock = allocate_ll_lock()
-    #     api.PyThread_acquire_lock(lock, space.w_int(0))
-    #     api.PyThread_release_lock(lock)
-    #     assert api.PyThread_acquire_lock(lock, space.w_int(0)) == 1
+    @py.test.mark.xfail
+    def test_release_lock(self, space, api):
+        lock = allocate_ll_lock()
+        api.PyThread_acquire_lock(lock, space.w_int(0))
+        api.PyThread_release_lock(lock)
+        assert api.PyThread_acquire_lock(lock, space.w_int(0)) == 1
