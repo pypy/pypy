973cd9dfd6233ff7403421f782eaeac48eca4bda
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-05-04 12:39:34 +0000

    test weakref on a collected object.

--- pypy/module/cpyext/test/test_weakref.py
+++ pypy/module/cpyext/test/test_weakref.py
@@ -13,3 +13,17 @@ class TestWeakReference(BaseApiTest):
         assert api.PyWeakref_NewRef(w_obj, space.w_None) is None
         assert api.PyErr_Occurred() is space.w_TypeError
         api.PyErr_Clear()
+
+    def test_weakref_lockobject(self, space, api):
+        # some new weakrefable object
+        w_obj = space.call_function(space.w_type, space.wrap("newtype"),
+                                    space.newtuple([]), space.newdict())
+        assert w_obj is not None
+
+        w_ref = api.PyWeakref_NewRef(w_obj, space.w_None)
+        assert w_obj is not None
+
+        assert space.is_w(api.PyWeakref_LockObject(w_ref), w_obj)
+        del w_obj
+        import gc; gc.collect()
+        assert space.is_w(api.PyWeakref_LockObject(w_ref), space.w_None)
