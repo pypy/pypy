cf82083240469fddf4820e7bb32d6739fe9c6d96
Armin Rigo <arigo@tunes.org>
2010-05-21 13:08:11 +0000

    WildChild reports that --gcremovetypeptr is incompatible with --stackless.

--- pypy/config/translationoption.py
+++ pypy/config/translationoption.py
@@ -23,7 +23,8 @@ translation_optiondescription = OptionDescription(
         "translation", "Translation Options", [
     BoolOption("stackless", "enable stackless features during compilation",
                default=False, cmdline="--stackless",
-               requires=[("translation.type_system", "lltype")]),
+               requires=[("translation.type_system", "lltype"),
+                         ("translation.gcremovetypeptr", False)]),  # XXX?
     ChoiceOption("type_system", "Type system to use when RTyping",
                  ["lltype", "ootype"], cmdline=None, default="lltype",
                  requires={
