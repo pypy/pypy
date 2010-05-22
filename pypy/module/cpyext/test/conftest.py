3b175204dd274405fccb6ac691fd3f07089a0641
Holger Krekel <holger.krekel@gmail.com>
2010-05-22 16:20:39 +0000

    don't use deprecated "class Directory"

--- pypy/module/cpyext/test/conftest.py
+++ pypy/module/cpyext/test/conftest.py
@@ -1,15 +1,12 @@
 import py
 from pypy.conftest import option, gettestobjspace
 
-class Directory(py.test.collect.Directory):
-    def collect(self):
-        if option.runappdirect:
-            py.test.skip("cannot be run by py.test -A")
+def pytest_collect_directory(parent):
+    if parent.config.option.runappdirect:
+        py.test.skip("cannot be run by py.test -A")
 
-        # ensure additional functions are registered
-        import pypy.module.cpyext.test.test_cpyext
-
-        return super(Directory, self).collect()
+    # ensure additional functions are registered
+    import pypy.module.cpyext.test.test_cpyext
 
 def pytest_funcarg__space(request):
     return gettestobjspace(usemodules=['cpyext', 'thread'])
