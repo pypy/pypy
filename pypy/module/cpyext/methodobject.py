c636cf9fd515195c2abf91115a5dd7df70a8ab22
Maciej Fijalkowski <fijall@gmail.com>
2010-06-01 03:54:52 +0000

    This is the used signature

--- pypy/module/cpyext/methodobject.py
+++ pypy/module/cpyext/methodobject.py
@@ -235,10 +235,10 @@ def PyDescr_NewMethod(space, w_type, method):
     return space.wrap(W_PyCMethodObject(space, method, w_type))
 
 def PyDescr_NewWrapper(space, pto, method_name, wrapper_func, wrapper_func_kwds,
-                       flags, func):
+                       doc, func):
     # not exactly the API sig
     return space.wrap(W_PyCWrapperObject(space, pto, method_name,
-        wrapper_func, wrapper_func_kwds, flags, func))
+        wrapper_func, wrapper_func_kwds, doc, func))
 
 @cpython_api([lltype.Ptr(PyMethodDef), PyObject, CONST_STRING], PyObject)
 def Py_FindMethod(space, table, w_obj, name_ptr):
