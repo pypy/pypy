10dbdd2ad3a5143c20c3d5ce7ab8fd5cc96bcfba
Benjamin Peterson <benjamin@python.org>
2010-06-03 21:39:24 +0000

    configure paths with cpython version

--- pypy/tool/pytest/confpath.py
+++ pypy/tool/pytest/confpath.py
@@ -1,11 +1,12 @@
 import autopath 
 import py
 import pypy
+from pypy.module.sys.version import CPYTHON_VERSION_DIR
 
 pypydir = py.path.local(pypy.__file__).dirpath()
 distdir = pypydir.dirpath()
 testresultdir = distdir.join('testresult') 
 assert pypydir.check(dir=1) 
 libpythondir = distdir.join('lib-python') 
-regrtestdir = libpythondir.join('2.5.2', 'test') 
-modregrtestdir = libpythondir.join('modified-2.5.2', 'test') 
+regrtestdir = libpythondir.join(CPYTHON_VERSION_DIR, 'test') 
+modregrtestdir = libpythondir.join('modified-' + CPYTHON_VERSION_DIR, 'test') 
