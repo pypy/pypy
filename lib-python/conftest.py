50d83369951e4218a73dca4cda9552ea925a5561
Benjamin Peterson <benjamin@python.org>
2010-06-03 23:33:40 +0000

    run without site until sys.prefix can be figured out

--- lib-python/conftest.py
+++ lib-python/conftest.py
@@ -664,7 +664,7 @@ class ReallyRunFileExternal(py.test.collect.Item):
             pypy_options.extend(
                 ['--withmod-%s' % mod for mod in regrtest.usemodules])
             sopt = " ".join(pypy_options) 
-            cmd = "%s %s %d %s %s %s %s %s" %(
+            cmd = "%s %s %d %s -S %s %s %s %s" %(
                 python, alarm_script, TIMEOUT, 
                 pypy_script, sopt, 
                 regrrun, regrrun_verbosity, fspath.purebasename)
