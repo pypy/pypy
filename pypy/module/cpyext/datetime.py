853acdefd6e920f4cf933f7c5830d031ac88cc47
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-06-07 09:35:11 +0000

    Implement PyDate, PyTime, PyDateTime: constructors, check functions, and member access.

--- pypy/module/cpyext/datetime.py
+++ pypy/module/cpyext/datetime.py
@@ -2,26 +2,159 @@ from pypy.rpython.lltypesystem import rffi, lltype
 from pypy.module.cpyext.pyobject import PyObject
 from pypy.module.cpyext.api import cpython_api, CANNOT_FAIL
 from pypy.module.cpyext.import_ import PyImport_Import
+from pypy.interpreter.error import OperationError
+from pypy.tool.sourcetools import func_renamer
 
 @cpython_api([], lltype.Void)
 def PyDateTime_IMPORT(space):
     return
 
-@cpython_api([rffi.INT, rffi.INT, rffi.INT, rffi.INT], PyObject)
+PyDateTime_Date = PyObject
+PyDateTime_Time = PyObject
+PyDateTime_DateTime = PyObject
+
+# Check functions
+
+def make_check_function(func_name, type_name):
+    @cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
+    @func_renamer(func_name)
+    def check(space, w_obj):
+        try:
+            return space.is_true(
+                space.appexec([w_obj], """(obj):
+                    from datetime import %s as datatype
+                    return isinstance(obj, datatype)
+                    """ % (type_name,)))
+        except OperationError:
+            return 0
+
+    @cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
+    @func_renamer(func_name + "Exact")
+    def check_exact(space, w_obj):
+        try:
+            return space.is_true(
+                space.appexec([w_obj], """(obj):
+                    from datetime import %s as datatype
+                    return type(obj) is datatype
+                    """ % (type_name,)))
+        except OperationError:
+            return 0
+
+make_check_function("PyDateTime_Check", "datetime")
+make_check_function("PyDate_Check", "date")
+make_check_function("PyTime_Check", "time")
+make_check_function("PyDelta_Check", "timedelta")
+
+# Constructors
+
+@cpython_api([rffi.INT_real, rffi.INT_real, rffi.INT_real], PyObject)
+def PyDate_FromDate(space, year, month, day):
+    """Return a datetime.date object with the specified year, month and day.
+    """
+    w_datetime = PyImport_Import(space, space.wrap("datetime"))
+    return space.call_method(
+        w_datetime, "date",
+        space.wrap(year), space.wrap(month), space.wrap(day))
+
+@cpython_api([rffi.INT_real, rffi.INT_real, rffi.INT_real, rffi.INT_real], PyObject)
 def PyTime_FromTime(space, hour, minute, second, usecond):
+    """Return a ``datetime.time`` object with the specified hour, minute, second and
+    microsecond."""
     w_datetime = PyImport_Import(space, space.wrap("datetime"))
     return space.call_method(
         w_datetime, "time",
         space.wrap(hour), space.wrap(minute), space.wrap(second),
         space.wrap(usecond))
 
-@cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
-def PyDelta_Check(space, w_obj):
-    """Return true if ob is of type PyDateTime_DeltaType or a subtype of
-    PyDateTime_DeltaType.  ob must not be NULL.
+@cpython_api([rffi.INT_real, rffi.INT_real, rffi.INT_real, rffi.INT_real, rffi.INT_real, rffi.INT_real, rffi.INT_real], PyObject)
+def PyDateTime_FromDateAndTime(space, year, month, day, hour, minute, second, usecond):
+    """Return a datetime.datetime object with the specified year, month, day, hour,
+    minute, second and microsecond.
     """
-    return space.is_true(
-        space.appexec([w_obj], """(obj):
-                                    from datetime import timedelta
-                                    return isinstance(obj, timedelta)
-                      """))
+    w_datetime = PyImport_Import(space, space.wrap("datetime"))
+    return space.call_method(
+        w_datetime, "datetime",
+        space.wrap(year), space.wrap(month), space.wrap(day),
+        space.wrap(hour), space.wrap(minute), space.wrap(second),
+        space.wrap(usecond))
+    raise NotImplementedError
+
+@cpython_api([rffi.INT_real, rffi.INT_real, rffi.INT_real], PyObject)
+def PyDelta_FromDSU(space, days, seconds, useconds):
+    """Return a datetime.timedelta object representing the given number of days,
+    seconds and microseconds.  Normalization is performed so that the resulting
+    number of microseconds and seconds lie in the ranges documented for
+    datetime.timedelta objects.
+    """
+    w_datetime = PyImport_Import(space, space.wrap("datetime"))
+    return space.call_method(
+        w_datetime, "timedelta",
+        space.wrap(days), space.wrap(seconds), space.wrap(useconds))
+
+# Accessors
+
+@cpython_api([PyDateTime_Date], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_GET_YEAR(space, w_obj):
+    """Return the year, as a positive int.
+    """
+    return space.getattr(w_obj, space.wrap("year"))
+
+@cpython_api([PyDateTime_Date], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_GET_MONTH(space, w_obj):
+    """Return the month, as an int from 1 through 12.
+    """
+    return space.getattr(w_obj, space.wrap("month"))
+
+@cpython_api([PyDateTime_Date], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_GET_DAY(space, w_obj):
+    """Return the day, as an int from 1 through 31.
+    """
+    return space.getattr(w_obj, space.wrap("day"))
+
+@cpython_api([PyDateTime_DateTime], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_DATE_GET_HOUR(space, w_obj):
+    """Return the hour, as an int from 0 through 23.
+    """
+    return space.getattr(w_obj, space.wrap("hour"))
+
+@cpython_api([PyDateTime_DateTime], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_DATE_GET_MINUTE(space, w_obj):
+    """Return the minute, as an int from 0 through 59.
+    """
+    return space.getattr(w_obj, space.wrap("minute"))
+
+@cpython_api([PyDateTime_DateTime], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_DATE_GET_SECOND(space, w_obj):
+    """Return the second, as an int from 0 through 59.
+    """
+    return space.getattr(w_obj, space.wrap("second"))
+
+@cpython_api([PyDateTime_DateTime], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_DATE_GET_MICROSECOND(space, w_obj):
+    """Return the microsecond, as an int from 0 through 999999.
+    """
+    return space.getattr(w_obj, space.wrap("microsecond"))
+
+@cpython_api([PyDateTime_Time], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_TIME_GET_HOUR(space, w_obj):
+    """Return the hour, as an int from 0 through 23.
+    """
+    return space.getattr(w_obj, space.wrap("hour"))
+
+@cpython_api([PyDateTime_Time], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_TIME_GET_MINUTE(space, w_obj):
+    """Return the minute, as an int from 0 through 59.
+    """
+    return space.getattr(w_obj, space.wrap("minute"))
+
+@cpython_api([PyDateTime_Time], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_TIME_GET_SECOND(space, w_obj):
+    """Return the second, as an int from 0 through 59.
+    """
+    return space.getattr(w_obj, space.wrap("second"))
+
+@cpython_api([PyDateTime_Time], rffi.INT_real, error=CANNOT_FAIL)
+def PyDateTime_TIME_GET_MICROSECOND(space, w_obj):
+    """Return the microsecond, as an int from 0 through 999999.
+    """
+    return space.getattr(w_obj, space.wrap("microsecond"))
--- pypy/module/cpyext/test/test_datetime.py
+++ pypy/module/cpyext/test/test_datetime.py
@@ -2,18 +2,62 @@ from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
 from pypy.module.cpyext.test.test_api import BaseApiTest
 
 class TestDatetime(BaseApiTest):
+    def test_date(self, space, api):
+        w_date = api.PyDate_FromDate(2010, 06, 03)
+        assert space.unwrap(space.str(w_date)) == '2010-06-03'
+
+        assert api.PyDate_Check(w_date)
+        assert api.PyDate_CheckExact(w_date)
+
+        assert space.unwrap(space.newtuple([
+            api.PyDateTime_GET_YEAR(w_date),
+            api.PyDateTime_GET_MONTH(w_date),
+            api.PyDateTime_GET_DAY(w_date)])) == (
+            2010, 06, 03)
+
     def test_time(self, space, api):
         w_time = api.PyTime_FromTime(23, 15, 40, 123456)
         assert space.unwrap(space.str(w_time)) == '23:15:40.123456'
-    def test_deltacheck(self, space, api):
-        w_delta = space.appexec([space.wrap(3), space.wrap(15)], """(days, seconds):
+
+        assert api.PyTime_Check(w_time)
+        assert api.PyTime_CheckExact(w_time)
+
+        assert space.unwrap(space.newtuple([
+            api.PyDateTime_TIME_GET_HOUR(w_time),
+            api.PyDateTime_TIME_GET_MINUTE(w_time),
+            api.PyDateTime_TIME_GET_SECOND(w_time),
+            api.PyDateTime_TIME_GET_MICROSECOND(w_time)])) == (
+            23, 15, 40, 123456)
+
+    def test_datetime(self, space, api):
+        w_date = api.PyDateTime_FromDateAndTime(
+            2010, 06, 03, 23, 15, 40, 123456)
+        assert space.unwrap(space.str(w_date)) == '2010-06-03 23:15:40.123456'
+
+        assert api.PyDateTime_Check(w_date)
+        assert api.PyDateTime_CheckExact(w_date)
+        assert api.PyDate_Check(w_date)
+        assert not api.PyDate_CheckExact(w_date)
+
+        assert space.unwrap(space.newtuple([
+            api.PyDateTime_GET_YEAR(w_date),
+            api.PyDateTime_GET_MONTH(w_date),
+            api.PyDateTime_GET_DAY(w_date),
+            api.PyDateTime_DATE_GET_HOUR(w_date),
+            api.PyDateTime_DATE_GET_MINUTE(w_date),
+            api.PyDateTime_DATE_GET_SECOND(w_date),
+            api.PyDateTime_DATE_GET_MICROSECOND(w_date)])) == (
+            2010, 06, 03, 23, 15, 40, 123456)
+
+    def test_delta(self, space, api):
+        w_delta = space.appexec(
+            [space.wrap(3), space.wrap(15)], """(days, seconds):
             from datetime import timedelta
             return timedelta(days, seconds)
         """)
         assert api.PyDelta_Check(w_delta)
+        assert api.PyDelta_CheckExact(w_delta)
 
-        w_delta = space.appexec([space.wrap(1), space.wrap(1), space.wrap(1999)], """(day, month, year):
-            from datetime import datetime
-            return datetime.now() - datetime(year, month, day)
-        """)
+        w_delta = api.PyDelta_FromDSU(10, 20, 30)
         assert api.PyDelta_Check(w_delta)
+        assert api.PyDelta_CheckExact(w_delta)
