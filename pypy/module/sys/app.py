7f8ad82759969425fda9198d8774da49614d6ed0
Armin Rigo <arigo@tunes.org>
2010-06-09 16:24:25 +0000

    Try to be really careful in sys.excepthook().

--- pypy/module/sys/app.py
+++ pypy/module/sys/app.py
@@ -7,8 +7,35 @@ import sys
 
 def excepthook(exctype, value, traceback):
     """Handle an exception by displaying it with a traceback on sys.stderr."""
-    from traceback import print_exception
-    print_exception(exctype, value, traceback)
+    try:
+        from traceback import print_exception
+        print_exception(exctype, value, traceback)
+    except:
+        if not excepthook_failsafe(exctype, value):
+            raise
+
+def excepthook_failsafe(exctype, value):
+    # This version carefully tries to handle all bad cases (e.g. an
+    # ImportError looking for traceback.py), but may still raise.
+    # If it does, we get "Error calling sys.excepthook" from app_main.py.
+    try:
+        # first try to print the exception's class name
+        stderr = sys.stderr
+        stderr.write(getattr(exctype, '__name__', exctype))
+        # then attempt to get the str() of the exception
+        try:
+            s = str(value)
+        except:
+            s = '<failure of str() on the exception instance>'
+        # then print it, and don't worry too much about the extra space
+        # between the exception class and the ':'
+        if s:
+            stderr.write(': %s\n' % (s,))
+        else:
+            stderr.write('\n')
+        return True     # successfully printed at least the class and value
+    except:
+        return False    # got an exception again... ignore, report the original
 
 def exit(exitcode=0):
     """Exit the interpreter by raising SystemExit(exitcode).
