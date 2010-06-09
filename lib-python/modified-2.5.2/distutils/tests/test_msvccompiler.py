8dc898d91268671a2e952d7ea9b55e6b462c664d
Armin Rigo <arigo@tunes.org>
2010-06-09 10:16:02 +0000

    Attempt to skip this file on non-Windows platforms.

--- lib-python/modified-2.5.2/distutils/tests/test_msvccompiler.py
+++ lib-python/modified-2.5.2/distutils/tests/test_msvccompiler.py
@@ -1,8 +1,8 @@
-import unittest
-from distutils.msvccompiler import get_manifests
+import unittest, sys
 
 class MsvcCompilerTestCase(unittest.TestCase):
     def test_get_manifests(self):
+        from distutils.msvccompiler import get_manifests
         manifests = get_manifests()
         self.assert_(manifests)
         for manifest in manifests:
@@ -12,7 +12,10 @@ class MsvcCompilerTestCase(unittest.TestCase):
             self.fail("could not find a suitable manifest")
 
 def test_suite():
-    return unittest.makeSuite(MsvcCompilerTestCase)
+    if sys.platform == 'win32':
+        return unittest.makeSuite(MsvcCompilerTestCase)
+    else:
+        return unittest.TestSuite([])
 
 if __name__ == "__main__":
     unittest.main(defaultTest="test_suite")
