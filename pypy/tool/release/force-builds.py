9972faa513d796921d0b41ab80e012673cba7947
Maciej Fijalkowski <fijall@gmail.com>
2010-06-18 19:10:57 +0000

    Disable own-macosx for the release

--- pypy/tool/release/force-builds.py
+++ pypy/tool/release/force-builds.py
@@ -19,7 +19,7 @@ from twisted.web.error import PageRedirect
 BUILDERS = [
     'own-linux-x86-32',
     'own-linux-x86-64',
-    'own-macosx-x86-32',
+#    'own-macosx-x86-32',
     'pypy-c-app-level-linux-x86-32',
     'pypy-c-app-level-linux-64',
     'pypy-c-stackless-app-level-linux-x86-32',
