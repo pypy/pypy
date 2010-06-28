10e5701a21002c011e732b1d3a12c3cfd8d21049
Benjamin Peterson <benjamin@python.org>
2010-06-28 15:46:38 +0000

    deprecate BaseException.message

--- pypy/module/exceptions/interp_exceptions.py
+++ pypy/module/exceptions/interp_exceptions.py
@@ -167,6 +167,8 @@ class W_BaseException(Wrappable):
         if self.w_message is None:
             raise OperationError(space.w_AttributeError,
                                  space.wrap("message was deleted"))
+        space.warn("BaseException.message has been deprecated as of Python 2.6",
+                   space.w_DeprecationWarning)
         return self.w_message
 
     def descr_message_set(space, self, w_new):
