253f8eb453943ea89153376f8a082bbec7e10f40
Armin Rigo <arigo@tunes.org>
2010-08-14 08:46:38 +0000

    On Windows, don't rely on the presence of the 'tar' tool. Add a test (that also runs on linux) checking compression with the tarfile module.

--- pypy/tool/release/test/test_package.py
+++ pypy/tool/release/test/test_package.py
@@ -5,7 +5,7 @@ from pypy.tool.release.package import package
 from pypy.module.sys.version import  CPYTHON_VERSION
 import tarfile, os
 
-def test_dir_structure():
+def test_dir_structure(test='test'):
     # make sure we have sort of pypy-c
     pypy_c = py.path.local(pypydir).join('translator', 'goal', 'pypy-c')
     if not pypy_c.check():
@@ -14,8 +14,8 @@ def test_dir_structure():
     else:
         fake_pypy_c = False
     try:
-        builddir = package(py.path.local(pypydir).dirpath(), 'test')
-        prefix = builddir.join('test')
+        builddir = package(py.path.local(pypydir).dirpath(), test)
+        prefix = builddir.join(test)
         cpyver = '%d.%d.%d' % CPYTHON_VERSION[:3]
         assert prefix.join('lib-python', cpyver, 'test').check()
         assert prefix.join('bin', 'pypy-c').check()
@@ -24,18 +24,27 @@ def test_dir_structure():
         assert not prefix.join('lib_pypy', 'ctypes_configure').check()
         assert prefix.join('LICENSE').check()
         assert prefix.join('README').check()
-        th = tarfile.open(str(builddir.join('test.tar.bz2')))
-        assert th.getmember('test/lib_pypy/syslog.py')
+        th = tarfile.open(str(builddir.join('%s.tar.bz2' % test)))
+        assert th.getmember('%s/lib_pypy/syslog.py' % test)
 
         # the headers file could be not there, because they are copied into
         # trunk/include only during translation
         includedir = py.path.local(pypydir).dirpath().join('include')
         def check_include(name):
             if includedir.join(name).check(file=True):
-                assert th.getmember('test/include/%s' % name)
+                assert th.getmember('%s/include/%s' % (test, name))
         check_include('Python.h')
         check_include('modsupport.inl')
         check_include('pypy_decl.h')
     finally:
         if fake_pypy_c:
             pypy_c.remove()
+
+def test_with_tarfile_module():
+    from pypy.tool.release import package
+    prev = package.USE_TARFILE_MODULE
+    try:
+        package.USE_TARFILE_MODULE = True
+        test_dir_structure(test='testtarfile')
+    finally:
+        package.USE_TARFILE_MODULE = prev
