9a6a048b3fee40d277e8082b99088a766d4dc54d
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-09-02 13:29:04 +0000

    A new x86 instruction I'd never seen before (Convert Word to Extended Doubleword) we may ignore it.

--- pypy/translator/c/gcc/trackgcroot.py
+++ pypy/translator/c/gcc/trackgcroot.py
@@ -375,7 +375,7 @@ class FunctionGcRootTracker(object):
 
     IGNORE_OPS_WITH_PREFIXES = dict.fromkeys([
         'cmp', 'test', 'set', 'sahf', 'lahf', 'cltd', 'cld', 'std',
-        'rep', 'movs', 'lods', 'stos', 'scas', 'cwtl', 'prefetch',
+        'rep', 'movs', 'lods', 'stos', 'scas', 'cwtl', 'cwde', 'prefetch',
         # floating-point operations cannot produce GC pointers
         'f',
         'cvt', 'ucomi', 'comi', 'subs', 'subp' , 'adds', 'addp', 'xorp',
