40de3a35019bd26d00a9b79e456d7c2711908325
Hakan Ardo <hakan@debian.org>
2010-09-16 07:40:50 +0000

    Don't unroll to much

--- pypy/module/__builtin__/functional.py
+++ pypy/module/__builtin__/functional.py
@@ -106,7 +106,8 @@ def min_max(space, args, implementation_of):
         compare = space.lt
 
     args_w = args.arguments_w
-    if len(args_w) > 1 and not args.keywords: # Unrollable case
+    if len(args_w) > 1 and len(args_w) < 10 and not args.keywords:
+        # Unrollable case
         w_max_item = None
         for w_item in args_w:
             if w_max_item is None or \
