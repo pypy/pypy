c3bf73be090152865c12b29fea5af9387a40b310
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-09-24 20:23:35 +0000

    Change to a much simpler code that does not require 'bytearray' (which is not yet available in pypy.)
    Yes, I'm modifying the original copy of urllib.py, for two reasons:
    - It's a cosmetic change that will not be backported to CPython
    - It will be discarded when we import a new revision of the 2.7 stdlib,
      but at this time we will have a working bytearray and we will forget
      all this.

--- lib-python/2.7.0/urllib.py
+++ lib-python/2.7.0/urllib.py
@@ -1189,7 +1189,8 @@ always_safe = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                'abcdefghijklmnopqrstuvwxyz'
                '0123456789' '_.-')
 _safe_map = {}
-for i, c in zip(xrange(256), str(bytearray(xrange(256)))):
+for i in xrange(256):
+    c = chr(i)
     _safe_map[c] = c if (i < 128 and c in always_safe) else '%{:02X}'.format(i)
 _safe_quoters = {}
 
