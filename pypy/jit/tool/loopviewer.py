89d0a1342897ca68763368060b6104d00ce00f8f
Antonio Cuni <anto.cuni@gmail.com>
2010-09-28 16:33:07 +0000

    display the details of operations before and after optimizations

--- pypy/jit/tool/loopviewer.py
+++ pypy/jit/tool/loopviewer.py
@@ -31,11 +31,14 @@ def main(loopfile, options):
         nloops, nsummary = consider_category(log, options, "jit-log-noopt-")
         print
         print 'Summary of optimized-away operations'
+        print
         diff = {}
         keys = set(summary.keys()).union(set(nsummary))
         for key in keys:
-            diff[key] = nsummary.get(key, 0) - summary.get(key, 0)
-        print_summary(diff)
+            before = nsummary.get(key, 0)
+            after = summary.get(key, 0)
+            diff[key] = (before-after, before, after)
+        print_diff(diff)
 
 def consider_category(log, options, category):
     loops = logparser.extract_category(log, category)
@@ -55,7 +58,19 @@ def print_summary(summary):
     ops = [(summary[key], key) for key in summary]
     ops.sort(reverse=True)
     for n, key in ops:
-        print '%4d' % n, key
+        print '%5d' % n, key
+
+def print_diff(diff):
+    ops = [(d, before, after, key) for key, (d, before, after) in diff.iteritems()]
+    ops.sort(reverse=True)
+    tot_before = 0
+    tot_after = 0
+    for d, before, after, key in ops:
+        tot_before += before
+        tot_after += after
+        print '%5d - %5d = %5d     ' % (before, after, d), key
+    print '-' * 50
+    print '%5d - %5d = %5d     ' % (tot_before, tot_after, tot_before-tot_after), 'TOTAL'
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="%prog loopfile [options]")
