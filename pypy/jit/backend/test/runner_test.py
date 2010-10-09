6ddcf5dd48c3a17fcfe7cbd2615ce4515772ce16
David Schneider <david.schneider@picle.org>
2010-10-09 05:00:02 +0100

    Add a test for a basic loop using floats

--- pypy/jit/backend/test/runner_test.py
+++ pypy/jit/backend/test/runner_test.py
@@ -104,7 +104,23 @@ class BaseBackendTest(Runner):
         self.cpu.set_future_value_int(0, 2)
         fail = self.cpu.execute_token(looptoken)
         res = self.cpu.get_latest_value_int(0)
-        assert res == 3        
+        assert res == 3
+        assert fail.identifier == 1
+
+    def test_compile_linear_float_loop(self):
+        i0 = BoxFloat()
+        i1 = BoxFloat()
+        operations = [
+            ResOperation(rop.FLOAT_ADD, [i0, ConstFloat(2.3)], i1),
+            ResOperation(rop.FINISH, [i1], None, descr=BasicFailDescr(1))
+            ]
+        inputargs = [i0]
+        looptoken = LoopToken()
+        self.cpu.compile_loop(inputargs, operations, looptoken)
+        self.cpu.set_future_value_float(0, 2.8)
+        fail = self.cpu.execute_token(looptoken)
+        res = self.cpu.get_latest_value_float(0)
+        assert res == 5.1
         assert fail.identifier == 1
 
     def test_compile_loop(self):
