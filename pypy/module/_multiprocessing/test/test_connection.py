9f6202fd61a9ee31d657daeed75b23de9c3de553
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-10-11 21:39:30 +0000

    Refactor: one test for two implementations.

--- pypy/module/_multiprocessing/test/test_connection.py
+++ pypy/module/_multiprocessing/test/test_connection.py
@@ -7,43 +7,48 @@ class TestImport:
         from pypy.module._multiprocessing import interp_connection
         from pypy.module._multiprocessing import interp_semaphore
 
-class AppTestConnection:
+class BaseConnectionTest(object):
+    def test_connection(self):
+        rhandle, whandle = self.make_pair()
+
+        obj = [1, 2.0, "hello"]
+        whandle.send(obj)
+        obj2 = rhandle.recv()
+        assert obj == obj2
+
+class AppTestWinpipeConnection(BaseConnectionTest):
+    def setup_class(cls):
+        if sys.platform != "win32":
+            py.test.skip("win32 only")
+
+        space = gettestobjspace(usemodules=('_multiprocessing', 'thread'))
+        cls.space = space
+
+        # stubs for some modules,
+        # just for multiprocessing to import correctly on Windows
+        w_modules = space.sys.get('modules')
+        space.setitem(w_modules, space.wrap('msvcrt'), space.sys)
+        space.setitem(w_modules, space.wrap('_subprocess'), space.sys)
+
+        cls.w_make_pair = space.appexec([], """():
+            import multiprocessing
+            def make_pair():
+                rhandle, whandle = multiprocessing.Pipe()
+                return rhandle, whandle
+            return make_pair
+        """)
+
+class AppTestSocketConnection(BaseConnectionTest):
     def setup_class(cls):
         space = gettestobjspace(usemodules=('_multiprocessing', 'thread'))
         cls.space = space
-        if sys.platform == "win32":
-            # stubs for some modules,
-            # just for multiprocessing to import correctly.
-            w_modules = space.sys.get('modules')
-            space.setitem(w_modules, space.wrap('msvcrt'), space.sys)
-            space.setitem(w_modules, space.wrap('_subprocess'), space.sys)
-
-        # import multiprocessing once
-        space.appexec([], """(): import multiprocessing""")
-
-    def test_winpipe_connection(self):
-        import sys
-        if sys.platform != "win32":
-            skip("win32 only")
-
-        import multiprocessing
-        rhandle, whandle = multiprocessing.Pipe()
-
-        obj = [1, 2.0, "hello"]
-        whandle.send(obj)
-        obj2 = rhandle.recv()
-        assert obj == obj2
-
-    def test_ospipe_connection(self):
-        import _multiprocessing
-        import os
-        fd1, fd2 = os.pipe()
-        rhandle = _multiprocessing.Connection(fd1, writable=False)
-        whandle = _multiprocessing.Connection(fd2, readable=False)
-
-        obj = [1, 2.0, "hello"]
-        whandle.send(obj)
-        obj2 = rhandle.recv()
-        assert obj == obj2
-
-
+        cls.w_make_pair = space.appexec([], """():
+            import _multiprocessing
+            import os
+            def make_pair():
+                fd1, fd2 = os.pipe()
+                rhandle = _multiprocessing.Connection(fd1, writable=False)
+                whandle = _multiprocessing.Connection(fd2, readable=False)
+                return rhandle, whandle
+            return make_pair
+        """)
