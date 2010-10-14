cb952a0d623946a05e75d22ed006479cb25a9068
David Schneider <david.schneider@picle.org>
2010-10-14 12:39:09 +0000

    Add ARM regalloc

---
+++ pypy/jit/backend/arm/regalloc.py
@@ -0,0 +1,34 @@
+from pypy.jit.backend.llsupport.regalloc import FrameManager, \
+        RegisterManager, compute_vars_longevity
+from pypy.jit.backend.arm import registers as r
+
+class ARMRegisterManager(RegisterManager):
+    all_regs              = r.all_regs
+    box_types             = None       # or a list of acceptable types
+    no_lower_byte_regs    = r.all_regs
+    save_around_call_regs = all_regs
+
+    def __init__(self, longevity, frame_manager=None, assembler=None):
+        RegisterManager.__init__(self, longevity, frame_manager, assembler)
+
+class ARMFrameManager(FrameManager):
+    @staticmethod
+    def frame_pos(loc, type):
+        pass
+
+#class RegAlloc(object):
+#    def __init__(self, assembler, translate_support_code=False):
+#        self.assembler = assembler
+#        self.translate_support_code = translate_support_code
+#        self.fm = None
+#
+#    def _prepare(self, inputargs, operations):
+#        longevity = compute_vars_longevity(inputargs, operations)
+#        self.rm = ARMRegisterManager(longevity, self.fm)
+#
+#    def prepare_loop(self, inputargs, operations, looptoken):
+#        self._prepare(inputargs, operations)
+#
+#    def force_allocate_reg(self, v, forbidden_vars=[], selected_reg=None,
+#                           need_lower_byte=False):
+#        return self.rm.force_allocate_reg(v, forbidden_vars, selected_reg, need_lower_byte)
