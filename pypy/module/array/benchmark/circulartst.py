e45f12a4371be946a4a965e255eb2835f00c006b
Hakan Ardo <hakan@debian.org>
2010-10-20 14:29:46 +0000

    some benchmark results

--- pypy/module/array/benchmark/circulartst.py
+++ pypy/module/array/benchmark/circulartst.py
@@ -4,14 +4,14 @@ class Circular(array):
         self = array.__new__(cls, 'd', range(65536))
         return self
     def __getitem__(self, i):
-        assert self.__len__() == 65536 
+        assert len(self) == 65536 
         return array.__getitem__(self, i & 65535)
 
 def main():
     buf = Circular()
     i = 10
     sa = 0
-    while i < 20000000:
+    while i < 200000000:
         sa += buf[i-2] + buf[i-1] + buf[i] + buf[i+1] + buf[i+2]
         i += 1
     return sa
