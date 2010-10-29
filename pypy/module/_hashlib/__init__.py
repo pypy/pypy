dc256bb39f724c298b040deea79481cb661cb384
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-10-29 20:30:00 +0000

    Expose the same _hashlib items as CPython

--- pypy/module/_hashlib/__init__.py
+++ pypy/module/_hashlib/__init__.py
@@ -5,14 +5,13 @@ from pypy.module._hashlib.interp_hashlib import algorithms
 class Module(MixedModule):
     interpleveldefs = {
         'new' : 'interp_hashlib.new',
-        'HASH': 'interp_hashlib.W_Hash',
         }
 
     appleveldefs = {
         }
 
     for name in algorithms:
-        interpleveldefs[name] = 'interp_hashlib.new_%s' % (name,)
+        interpleveldefs['openssl_' + name] = 'interp_hashlib.new_' + name
 
     def startup(self, space):
         from pypy.rlib.ropenssl import init_digests
