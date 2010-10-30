01b295f544a3b003c5e9e2bc5de0f21e5f1122e1
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-10-30 16:26:57 +0000

    fix import

--- pypy/jit/metainterp/test/test_resume.py
+++ pypy/jit/metainterp/test/test_resume.py
@@ -1,6 +1,7 @@
 import py
 from pypy.rpython.lltypesystem import lltype, llmemory, rffi
-from pypy.jit.metainterp.optimizeopt.virtualize import VirtualValue, OptValue, VArrayValue
+from pypy.jit.metainterp.optimizeopt.optimizer import OptValue
+from pypy.jit.metainterp.optimizeopt.virtualize import VirtualValue, VArrayValue
 from pypy.jit.metainterp.optimizeopt.virtualize import VStructValue
 from pypy.jit.metainterp.resume import *
 from pypy.jit.metainterp.history import BoxInt, BoxPtr, ConstInt
