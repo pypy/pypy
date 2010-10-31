fa1c83c70c77005b8be288cfea57c0f21d70f3b1
Hakan Ardo <hakan@debian.org>
2010-10-31 13:58:19 +0000

    fixed test

--- pypy/jit/metainterp/test/test_loop.py
+++ pypy/jit/metainterp/test/test_loop.py
@@ -397,8 +397,12 @@ class LoopTest(object):
         res = self.meta_interp(f, [100, 5], policy=StopAtXPolicy(externfn))
         assert res == expected
 
-        self.check_loop_count(2)
-        self.check_tree_loop_count(2)   # 1 loop, 1 bridge from interp
+        if self.optimizer == OPTIMIZER_FULL:
+            self.check_loop_count(2)
+            self.check_tree_loop_count(2)   # 1 loop, 1 bridge from interp
+        else:
+            self.check_loop_count(2)
+            self.check_tree_loop_count(1)   # 1 loop, 1 bridge from interp
 
     def test_example(self):
         myjitdriver = JitDriver(greens = ['i'],
