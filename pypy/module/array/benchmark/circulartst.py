c6546d11a5265db9d7bfa5c1484b1f0eda928f1a
Hakan Ardo <hakan@debian.org>
2010-11-02 18:04:49 +0000

    We currently cant virtualize across bridges

--- pypy/module/array/benchmark/circulartst.py
+++ pypy/module/array/benchmark/circulartst.py
@@ -7,12 +7,15 @@ class Circular(array):
         assert len(self) == 65536 
         return array.__getitem__(self, i & 65535)
 
+import sys
 def main():
     buf = Circular()
     i = 10
     sa = 0
-    while i < 200000000:
+    #         1048576
+    while i < 949999:
         sa += buf[i-2] + buf[i-1] + buf[i] + buf[i+1] + buf[i+2]
+        if i%100 == 0: sys.stderr.write('%d\n'%i)
         i += 1
     return sa
 
