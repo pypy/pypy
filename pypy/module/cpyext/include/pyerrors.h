3e52da35c783b517a44e30d415463cd0dc5bd7a4
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-11-07 22:21:06 +0000

    Add PyExceptionClass_Check

--- pypy/module/cpyext/include/pyerrors.h
+++ pypy/module/cpyext/include/pyerrors.h
@@ -7,6 +7,10 @@
 extern "C" {
 #endif
 
+#define PyExceptionClass_Check(x)                                       \
+    (PyClass_Check((x)) || (PyType_Check((x)) &&                        \
+      PyObject_IsSubclass((x), PyExc_BaseException)))
+
 PyObject *PyErr_NewException(char *name, PyObject *base, PyObject *dict);
 PyObject *PyErr_Format(PyObject *exception, const char *format, ...);
 
