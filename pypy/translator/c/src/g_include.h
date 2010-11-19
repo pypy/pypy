9160b3bac2bc721030eae12af25baa7082f32d8e
Armin Rigo <arigo@tunes.org>
2010-11-19 13:37:52 +0000

    Add the high-res timestamp to x86-64 too.

--- pypy/translator/c/src/g_include.h
+++ pypy/translator/c/src/g_include.h
@@ -43,6 +43,10 @@
 #  include "src/asm_gcc_x86.h"
 #endif
 
+#if defined(__GNUC__) && defined(__amd64__)
+#  include "src/asm_gcc_x86_64.h"
+#endif
+
 #if defined(__GNUC__) && defined(__ppc__)
 #  include "src/asm_ppc.h"
 #endif
