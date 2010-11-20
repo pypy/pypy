67954a558304ea56e0987168c4a4e29e3570a6d5
Maciej Fijalkowski <fijall@gmail.com>
2010-11-20 15:03:50 +0000

    Add x86-64 buildbot to stuff we want to run automatically

--- pypy/tool/release/force-builds.py
+++ pypy/tool/release/force-builds.py
@@ -25,6 +25,7 @@ BUILDERS = [
     'pypy-c-stackless-app-level-linux-x86-32',
     'pypy-c-app-level-win-x86-32',
     'pypy-c-jit-linux-x86-32',
+    'pypy-c-jit-linux-x86-64',
 #    'pypy-c-jit-macosx-x86-32',
     'pypy-c-jit-win-x86-32',
 ]
