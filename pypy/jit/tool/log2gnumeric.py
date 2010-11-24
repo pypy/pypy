f130a72cc32d510dc40062866a12bd79e252bc86
Antonio Cuni <anto.cuni@gmail.com>
2010-11-24 12:54:35 +0000

    - fix for the new log format
    - use clocks relative to the "time0", i.e. the first clock value seen in the file
    
    - write tests :-)

--- pypy/jit/tool/log2gnumeric.py
+++ pypy/jit/tool/log2gnumeric.py
@@ -21,17 +21,23 @@ def main():
     outname = logname + '.gnumeric'
     data = open(logname).read()
     data = data.replace('\n', '')
-    maxclock = find_max_clock(data)
+    minclock, maxclock = get_clock_range(data)
+    time0 = minclock # we want "relative clocks"
+    maxtime = maxclock-time0
     #
     xml = gzip.open('log-template.gnumeric').read()
-    xml = replace_sheet(xml, 'translation-task', tasks_rows(data))
-    xml = replace_sheet(xml, 'gc-collect', gc_collect_rows(data))
-    xml = replace_sheet(xml, 'memusage', memusage_rows(logname + '.memusage', maxclock))
+    xml = replace_sheet(xml, 'translation-task', tasks_rows(time0, data))
+    xml = replace_sheet(xml, 'gc-collect', gc_collect_rows(time0, data))
+    xml = replace_sheet(xml, 'memusage', memusage_rows(logname + '.memusage', maxtime))
     #
     out = gzip.open(outname, 'wb')
     out.write(xml)
     out.close()
 
+
+# ========================================================================
+# functions to manipulate gnumeric files
+# ========================================================================
 
 def replace_sheet(xml, sheet_name, data):
     pattern = '<gnm:Sheet .*?<gnm:Name>%s</gnm:Name>.*?(<gnm:Cells>.*?</gnm:Cells>)'
@@ -68,7 +74,17 @@ def gen_cells(data):
     return '\n'.join(parts)
     
 
-def gc_collect_rows(data):
+# ========================================================================
+# functions to extract various data from the logs
+# ========================================================================
+
+def get_clock_range(data):
+    s = r"\[([0-9a-f]+)\] "
+    r = re.compile(s)
+    clocks = [int(x, 16) for x in r.findall(data)]
+    return min(clocks), max(clocks)
+
+def gc_collect_rows(time0, data):
     s = r"""
 ----------- Full collection ------------------
 \| used before collection:
@@ -84,35 +100,34 @@ def gc_collect_rows(data):
     r = re.compile(s.replace('\n', ''))
     yield 'clock', 'gc-before', 'gc-after'
     for a,b,c,d,e,f in r.findall(data):
-        yield int(f, 16), int(a)+int(b), int(c)+int(d)
+        clock = int(f, 16) - time0
+        yield clock, int(a)+int(b), int(c)+int(d)
 
-def tasks_rows(data):
+def tasks_rows(time0, data):
     s = r"""
-\{translation-task
+\[([0-9a-f]+)\] \{translation-task
 starting ([\w-]+)
-\[([0-9a-f]+)\] translation-task\}"""
+"""
     #
     r = re.compile(s.replace('\n', ''))
     yield 'clock', None, 'task'
     for a,b in r.findall(data):
-        yield int(b, 16), 1, a
+        clock = int(a, 16) - time0
+        yield clock, 1, b
 
-def memusage_rows(filename, maxclock):
+def memusage_rows(filename, maxtime):
     try:
-        lines = open(filename)
+        lines = open(filename).readlines()
     except IOError:
         print 'Warning: cannot find file %s, skipping the memusage sheet'
         lines = []
-    yield 'n', 'computed clock', 'VmRSS'
+    yield 'inferred clock', 'VmRSS'
+    numlines = len(lines)
     for i, line in enumerate(lines):
         mem = int(line)
-        yield i, "=max('gc-collect'!$A$1:$A$65536)*(A2/max($A$1:$A$65536)))", mem
+        clock = maxtime * i // numlines
+        yield clock, mem
 
-def find_max_clock(data):
-    s = r"\[([0-9a-f]+)\] "
-    r = re.compile(s)
-    clocks = [int(x, 16) for x in r.findall(data)]
-    return max(clocks)
 
 if __name__ == '__main__':
     main()
---
+++ pypy/jit/tool/test/test_log2gnumeric.py
@@ -0,0 +1,65 @@
+from pypy.jit.tool import log2gnumeric
+
+log = """
+[1000] ...
+[2000] {gc-collect
+
+.----------- Full collection ------------------
+| used before collection:
+|          in ArenaCollection:      500 bytes
+|          raw_malloced:            100 bytes
+| used after collection:
+|          in ArenaCollection:      300 bytes
+|          raw_malloced:            50 bytes
+| number of major collects:         1
+`----------------------------------------------
+[3000] gc-collect}
+[4000] {gc-collect
+
+.----------- Full collection ------------------
+| used before collection:
+|          in ArenaCollection:      600 bytes
+|          raw_malloced:            200 bytes
+| used after collection:
+|          in ArenaCollection:      400 bytes
+|          raw_malloced:            100 bytes
+| number of major collects:         1
+`----------------------------------------------
+[5000] gc-collect}
+...
+...
+[6000] {translation-task
+starting annotate
+...
+...
+[7000] translation-task}
+[8000] {translation-task
+starting rtype_lltype
+...
+...
+[9000] translation-task}
+...
+[a000] ...
+"""
+
+log = log.replace('\n', '')
+
+def test_get_clock_range():
+    minclock, maxclock = log2gnumeric.get_clock_range(log)
+    assert minclock == 0x1000
+    assert maxclock == 0xa000
+    
+
+def test_gc_collect_rows():
+    rows = list(log2gnumeric.gc_collect_rows(0x1000, log))
+    assert len(rows) == 3
+    assert rows[0] == (      'clock', 'gc-before', 'gc-after')
+    assert rows[1] == (0x3000-0x1000,     500+100,    300+ 50)
+    assert rows[2] == (0x5000-0x1000,     600+200,    400+100)
+    
+def test_tasks_rows():
+    rows = list(log2gnumeric.tasks_rows(0x1000, log))
+    assert len(rows) == 3
+    assert rows[0] == (      'clock', None, 'task')
+    assert rows[1] == (0x6000-0x1000,    1, 'annotate')
+    assert rows[2] == (0x8000-0x1000,    1, 'rtype_lltype')
