263ed96d6b0bdd7cdcea651262ef7fb10fd657b4
Armin Rigo <arigo@tunes.org>
2010-11-30 17:59:08 +0000

    Bah.  Cancel this: it's all bound methods, not functions.

--- lib_pypy/readline.py
+++ lib_pypy/readline.py
@@ -6,10 +6,4 @@ Note that some of the functions present in the CPython module 'readline'
 are only stubs at the moment.
 """
 
-import __pypy__
-
-import pyrepl.readline
-__all__ = pyrepl.readline.__all__
-
-for _name in __all__:
-    globals()[_name] = __pypy__.builtinify(getattr(pyrepl.readline, _name))
+from pyrepl.readline import *
