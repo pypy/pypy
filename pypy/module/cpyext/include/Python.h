56d46ce55494e4b00a1d23bfe5aba9bb2b24c3a5
Armin Rigo <arigo@tunes.org>
2010-12-06 14:11:24 +0000

    Two small fixes to allow PIL to compile out of the box:
    * add some #included headers
    * add Py_FileSystemDefaultEncoding, always NULL for now

--- pypy/module/cpyext/include/Python.h
+++ pypy/module/cpyext/include/Python.h
@@ -8,6 +8,8 @@
 # include <stddef.h>
 # include <limits.h>
 # include <math.h>
+# include <errno.h>
+# include <unistd.h>
 # define Py_DEPRECATED(VERSION_UNUSED) __attribute__((__deprecated__))
 # define PyAPI_FUNC(RTYPE) RTYPE
 # define PyAPI_DATA(RTYPE) extern RTYPE
@@ -103,6 +105,7 @@ typedef long Py_ssize_t;
 #include "sliceobject.h"
 #include "datetime.h"
 #include "pystate.h"
+#include "fileobject.h"
 
 // XXX This shouldn't be included here
 #include "structmember.h"
