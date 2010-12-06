10b127ecd07029a38f193bf214027e87d44ab6f7
Armin Rigo <arigo@tunes.org>
2010-12-06 10:25:23 +0000

    Revert this part of r79830.  It breaks on Linux, probably because of passing "-shared" when building a stand-alone executable.

--- pypy/translator/platform/posix.py
+++ pypy/translator/platform/posix.py
@@ -166,7 +166,7 @@ class BasePosix(Platform):
                    'int main(int argc, char* argv[]) '
                    '{ return $(PYPY_MAIN_FUNCTION)(argc, argv); }" > $@')
             m.rule('$(DEFAULT_TARGET)', ['$(TARGET)', 'main.o'],
-                   '$(CC_LINK) $(LDFLAGS) $(LDFLAGSEXTRA) main.o -L. -l$(SHARED_IMPORT_LIB) -o $@')
+                   '$(CC_LINK) main.o -L. -l$(SHARED_IMPORT_LIB) -o $@')
 
         return m
 
