078ec1f1c63335f74d8fe4e0e65c1d2486644157
Maciej Fijalkowski <fijall@gmail.com>
2010-12-14 21:37:44 +0200

    fix test

--- pypy/tool/test/test_version.py
+++ pypy/tool/test/test_version.py
@@ -1,5 +1,7 @@
 
+import py
 from pypy.tool.version import get_mercurial_info
 
 def test_get_mercurial_info():
-    assert get_mercurial_info('completely broken mercurial')
+    assert get_mercurial_info(py.path.local.sysfind(
+        'completely broken mercurial'))
