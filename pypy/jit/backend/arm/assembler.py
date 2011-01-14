8e89a8e62615499bddd5b8dd1f0d6cdc6ffa6fbb
David Schneider <david.schneider@picle.org>
2011-01-14 11:55:53 +0100

    Do not generate instructions for operations without side effects and an unused result

--- pypy/jit/backend/arm/assembler.py
+++ pypy/jit/backend/arm/assembler.py
@@ -465,7 +465,11 @@ class AssemblerARM(ResOpAssembler):
             regalloc.position = i
             op = operations[i]
             opnum = op.getopnum()
-            if self.can_merge_with_next_guard(op, i, operations):
+            if op.has_no_side_effect() and op.result not in regalloc.longevity:
+                i += 1
+                regalloc.possibly_free_vars_for_op(op)
+                continue
+            elif self.can_merge_with_next_guard(op, i, operations):
                 arglocs = regalloc.operations_with_guard[opnum](regalloc, op,
                                         operations[i+1], fcond)
                 fcond = self.operations_with_guard[opnum](self, op,
