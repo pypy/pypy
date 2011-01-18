69a7c90189d323808349a180f21fce26c2617f1e
Alex Gaynor <alex.gaynor@gmail.com>
2011-01-17 18:32:36 -0600

    Fix these tests for when the compiler is included in the version.

--- pypy/module/sys/test/test_version.py
+++ pypy/module/sys/test/test_version.py
@@ -8,7 +8,7 @@ def test_get_version(space, monkeypatch):
     from pypy.module.sys import version
     monkeypatch.setattr(version, 'PYPY_VERSION', (2,5,0, "final", 1))
     res = space.unwrap(version.get_version(space))
-    assert "[PyPy 2.5.0]" in res
+    assert "[PyPy 2.5.0" in res
     monkeypatch.setattr(version, 'PYPY_VERSION', (2,6,3, "alpha", 5))
     res = space.unwrap(version.get_version(space))
-    assert "[PyPy 2.6.3-alpha5]" in res
+    assert "[PyPy 2.6.3-alpha5" in res
