7af15d612aca5da8f424c059810db0d6fe57cb0b
Maciej Fijalkowski <fijall@gmail.com>
2011-01-18 14:12:41 +0200

    Try to improve the nonsense we get from gcc

--- pypy/translator/c/src/asm_gcc_x86_64.h
+++ pypy/translator/c/src/asm_gcc_x86_64.h
@@ -3,6 +3,6 @@
 
 #define READ_TIMESTAMP(val) do {                        \
     unsigned long _rax, _rdx;                           \
-    asm volatile("rdtsc" : "=rax"(_rax), "=rdx"(_rdx)); \
+    asm volatile("rdtsc" : "=a"(_rax), "=d"(_rdx)); \
     val = (_rdx << 32) | _rax;                          \
 } while (0)
