2391416d8014d6702d19e3faa8a21a915aefb05b
Benjamin Peterson <benjamin@python.org>
2011-01-24 16:21:27 -0600

    formatting fix

--- pypy/rpython/module/ll_os.py
+++ pypy/rpython/module/ll_os.py
@@ -170,10 +170,10 @@ class CConfig:
                            ('tms_stime', rffi.INT),
                            ('tms_cutime', rffi.INT),
                            ('tms_cstime', rffi.INT)])
-        
-        GID_T = platform.SimpleType('gid_t',rffi.INT) 
-        #TODO right now is used only in getgroups, may need to update other functions like setgid
-                           
+
+        GID_T = platform.SimpleType('gid_t',rffi.INT)
+        #TODO right now is used only in getgroups, may need to update other
+        #functions like setgid
 
     SEEK_SET = platform.DefinedConstantInteger('SEEK_SET')
     SEEK_CUR = platform.DefinedConstantInteger('SEEK_CUR')
@@ -677,11 +677,11 @@ class RegisterOs(BaseLazyRegistering):
     @registering_if(os, 'getegid')
     def register_os_getegid(self):
         return self.extdef_for_os_function_returning_int('getegid')
-        
+
     @registering_if(os, 'getgroups')
     def register_os_getgroups(self):
         GP = rffi.CArrayPtr(self.GID_T)
-        c_getgroups = self.llexternal('getgroups',[rffi.INT, GP],rffi.INT)
+        c_getgroups = self.llexternal('getgroups', [rffi.INT, GP], rffi.INT)
 
         def getgroups_llimpl():
             groups = lltype.malloc(GP.TO, 0, flavor='raw')
@@ -690,7 +690,7 @@ class RegisterOs(BaseLazyRegistering):
                 if n >= 0:
                     lltype.free(groups, flavor='raw')
                     groups = lltype.malloc(GP.TO, n, flavor='raw')
-                    n = c_getgroups(n,groups)
+                    n = c_getgroups(n, groups)
                     result = [groups[i] for i in range(n)]
                     if n >= 0:
                         return result
@@ -698,7 +698,8 @@ class RegisterOs(BaseLazyRegistering):
             finally:
                 lltype.free(groups, flavor='raw')
 
-        return extdef([],[self.GID_T],llimpl=getgroups_llimpl, export_name="ll_os.ll_getgroups")
+        return extdef([], [self.GID_T], llimpl=getgroups_llimpl,
+                      export_name="ll_os.ll_getgroups")
 
     @registering_if(os, 'getpgrp')
     def register_os_getpgrp(self):
