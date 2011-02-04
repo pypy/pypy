248ca0cc12b7d22c51aeca5d2898b3afe69031b6
Armin Rigo <arigo@tunes.org>
2011-02-04 09:57:58 +0100

    Fix for tests.

--- pypy/jit/metainterp/optimizeopt/fficall.py
+++ pypy/jit/metainterp/optimizeopt/fficall.py
@@ -73,7 +73,7 @@ class OptFfiCall(Optimization):
 
     def setup(self):
         self.funcinfo = None
-        self.logger = self.optimizer.metainterp_sd.logger_noopt
+        self.logger = self.optimizer.metainterp_sd.logger_ops
 
     def propagate_begin_forward(self):
         debug_start('jit-log-ffiopt')
