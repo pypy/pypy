13c6564aa8fa69848c7aeb4172ea4f2803f70971
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-02-09 10:45:34 +0100

    Every iterator should implement an __iter__ method "This is required to allow both containers and iterators to be used with the for and in statements"

--- lib-python/modified-2.7.0/sqlite3/test/dbapi.py
+++ lib-python/modified-2.7.0/sqlite3/test/dbapi.py
@@ -1,4 +1,4 @@
-#-*- coding: ISO-8859-1 -*-
+#-*- coding: iso-8859-1 -*-
 # pysqlite2/test/dbapi.py: tests for DB-API compliance
 #
 # Copyright (C) 2004-2010 Gerhard Häring <gh@ghaering.de>
@@ -331,6 +331,9 @@ class CursorTests(unittest.TestCase):
         class MyIter:
             def __init__(self):
                 self.value = 5
+
+            def __iter__(self):
+                return self
 
             def next(self):
                 if self.value == 10:
