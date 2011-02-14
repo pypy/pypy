d64bc40c95bd9106792c8c79d42eacc15fca8f82
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-02-14 10:45:46 +0100

    Reindent test code, it will also give better messages on failure

--- pypy/module/_codecs/test/test_codecs.py
+++ pypy/module/_codecs/test/test_codecs.py
@@ -408,18 +408,13 @@ class AppTestPartialEvaluation:
 
     def test_errors(self):
         import codecs
-        assert (
-            codecs.replace_errors(UnicodeEncodeError("ascii", u"\u3042", 0, 1, "ouch"))) == (
-            (u"?", 1)
-        )
-        assert (
-            codecs.replace_errors(UnicodeDecodeError("ascii", "\xff", 0, 1, "ouch"))) == (
-            (u"\ufffd", 1)
-        )
-        assert (
-            codecs.replace_errors(UnicodeTranslateError(u"\u3042", 0, 1, "ouch"))) == (
-            (u"\ufffd", 1)
-        )
+        assert codecs.replace_errors(UnicodeEncodeError(
+            "ascii", u"\u3042", 0, 1, "ouch")) == (u"?", 1)
+        assert codecs.replace_errors(UnicodeDecodeError(
+            "ascii", "\xff", 0, 1, "ouch")) == (u"\ufffd", 1)
+        assert codecs.replace_errors(UnicodeTranslateError(
+            u"\u3042", 0, 1, "ouch")) == (u"\ufffd", 1)
+
         class BadStartUnicodeEncodeError(UnicodeEncodeError):
             def __init__(self):
                 UnicodeEncodeError.__init__(self, "ascii", u"", 0, 1, "bad")
