c94638bc1b7956b1c1131d9769bda8fe6256e0aa
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-02-16 11:15:58 +0100

    This file is probably not tested :-(

--- pypy/module/_demo/demo.py
+++ pypy/module/_demo/demo.py
@@ -1,6 +1,6 @@
 from pypy.interpreter.error import OperationError
-from pypy.interpreter.baseobjspace import ObjSpace, W_Root, Wrappable
-from pypy.interpreter.gateway import interp2app
+from pypy.interpreter.baseobjspace import Wrappable
+from pypy.interpreter.gateway import interp2app, unwrap_spec
 from pypy.interpreter.typedef import TypeDef, GetSetProperty
 from pypy.rpython.lltypesystem import rffi, lltype
 from pypy.rpython.tool import rffi_platform
