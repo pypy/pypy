41e9c36b239dec7d312b8d0e2bed9da2950673a3
Antonio Cuni <anto.cuni@gmail.com>
2011-02-23 15:54:36 +0100

    kill this test, we decided to use a different way of testing

--- pypy/module/pypyjit/test_pypy_c/test_pypy_c_new.py
+++ pypy/module/pypyjit/test_pypy_c/test_pypy_c_new.py
@@ -7,31 +7,6 @@ from pypy.module.pypyjit.test_pypy_c.model import Log
 from pypy.module.pypyjit.test_pypy_c.test_model import BaseTestPyPyC
 
 
-class TestInfrastructure(BaseTestPyPyC):
-
-    def test_full(self):
-        py.test.skip('in-progress')
-        def f():
-            i = 0
-            while i < 1003:
-                # LOOP one
-                i += 1
-
-        trace = self.run(f)
-        loop = trace.get_loops('one')
-        loop.get_bytecode(3, 'LOAD_FAST').match('''
-        int_add
-        guard_true
-        ''')
-        loop.get_bytecode(4, 'LOAD_CONST').match_stats(
-            guard='3', call='1-2', call_may_force='0'
-        )
-        # this would make operations that are "costly" obligatory to pass
-        # like new
-        loo.get_bytecode(5, 'INPLACE_ADD').match_stats(
-            allocs='5-10'
-            )
-
 class TestPyPyCNew(BaseTestPyPyC):
     def test_f1(self):
         def f1(n):
