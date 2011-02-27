72f006e39d2f8d8365edfdb1044971d0bead4498
Alex Gaynor <alex.gaynor@gmail.com>
2011-02-27 17:06:22 -0500

    Implemented kevent and started on kqueue.

--- pypy/module/select/__init__.py
+++ pypy/module/select/__init__.py
@@ -17,18 +17,21 @@ class Module(MixedModule):
 
     # TODO: this doesn't feel right...
     if hasattr(select, "epoll"):
-        interpleveldefs['epoll'] = 'interp_epoll.W_Epoll'
-        symbols = [
+        interpleveldefs["epoll"] = "interp_epoll.W_Epoll"
+        for symbol in [
             "EPOLLIN", "EPOLLOUT", "EPOLLPRI", "EPOLLERR", "EPOLLHUP",
             "EPOLLET", "EPOLLONESHOT", "EPOLLRDNORM", "EPOLLRDBAND",
             "EPOLLWRNORM", "EPOLLWRBAND", "EPOLLMSG"
-        ]
-        for symbol in symbols:
+        ]:
             if hasattr(select, symbol):
                 interpleveldefs[symbol] = "space.wrap(%s)" % getattr(select, symbol)
 
     if hasattr(select, "kqueue"):
         interpleveldefs["kqueue"] = "interp_kqueue.W_Kqueue"
+        interpleveldefs["kevent"] = "interp_kqueue.W_Kevent"
+
+        for symbol in ["KQ_FILTER_READ", "KQ_FILTER_WRITE", "KQ_EV_ADD", "KQ_EV_ONESHOT", "KQ_EV_ENABLE"]:
+            interpleveldefs[symbol] = "space.wrap(interp_kqueue.%s)" % symbol
 
 
     def buildloaders(cls):
