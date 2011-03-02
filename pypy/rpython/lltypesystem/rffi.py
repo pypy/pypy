7a8d3c743585777fe561ebd2417a4e75387bafe2
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-03-02 20:22:22 +0100

    Don't do automatic conversions that change the integer size

--- pypy/rpython/lltypesystem/rffi.py
+++ pypy/rpython/lltypesystem/rffi.py
@@ -219,11 +219,11 @@ def llexternal(name, args, result, _callable=None,
         for i, TARGET in unrolling_arg_tps:
             if to_free[i]:
                 lltype.free(to_free[i], flavor='raw')
-        if rarithmetic.r_int is not r_int:
-            if result is INT:
-                return cast(lltype.Signed, res)
-            elif result is UINT or result is SIZE_T:
-                return cast(lltype.Unsigned, res)
+        # XXX these don't work on win64
+        if result is LONG:
+            return cast(lltype.Signed, res)
+        elif result is ULONG or result is SIZE_T:
+            return cast(lltype.Unsigned, res)
         return res
     wrapper._annspecialcase_ = 'specialize:ll'
     wrapper._always_inline_ = True
