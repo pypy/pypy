bc06b635ea0518aea5f198279ab1279ece403277
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-03-10 23:52:15 +0100

    (kleptog) Support for PyUnicode_AsEncodedObject.
    PyUnicode_AsEncodedObject is almost identical to PyUnicode_AsEncodedString
    except that the former does not check that the result is a string.

--- pypy/module/cpyext/unicodeobject.py
+++ pypy/module/cpyext/unicodeobject.py
@@ -8,6 +8,7 @@ from pypy.module.cpyext.api import (
     CONST_WSTRING)
 from pypy.module.cpyext.pyerrors import PyErr_BadArgument
 from pypy.module.cpyext.pyobject import PyObject, from_ref, make_typedescr
+from pypy.module.cpyext.stringobject import PyString_Check
 from pypy.module.sys.interp_encoding import setdefaultencoding
 from pypy.objspace.std import unicodeobject, unicodetype
 from pypy.rlib import runicode
@@ -189,8 +190,8 @@ def PyUnicode_SetDefaultEncoding(space, encoding):
     return 0
 
 @cpython_api([PyObject, CONST_STRING, CONST_STRING], PyObject)
-def PyUnicode_AsEncodedString(space, w_unicode, llencoding, llerrors):
-    """Encode a Unicode object and return the result as Python string object.
+def PyUnicode_AsEncodedObject(space, w_unicode, llencoding, llerrors):
+    """Encode a Unicode object and return the result as Python object.
     encoding and errors have the same meaning as the parameters of the same name
     in the Unicode encode() method. The codec to be used is looked up using
     the Python codec registry. Return NULL if an exception was raised by the
@@ -204,6 +205,19 @@ def PyUnicode_AsEncodedString(space, w_unicode, llencoding, llerrors):
     if llerrors:
         errors = rffi.charp2str(llerrors)
     return unicodetype.encode_object(space, w_unicode, encoding, errors)
+
+@cpython_api([PyObject, CONST_STRING, CONST_STRING], PyObject)
+def PyUnicode_AsEncodedString(space, w_unicode, llencoding, llerrors):
+    """Encode a Unicode object and return the result as Python string object.
+    encoding and errors have the same meaning as the parameters of the same name
+    in the Unicode encode() method. The codec to be used is looked up using
+    the Python codec registry. Return NULL if an exception was raised by the
+    codec."""
+    w_str = PyUnicode_AsEncodedObject(space, w_unicode, llencoding, llerrors)
+    if not PyString_Check(space, w_str):
+        raise OperationError(space.w_TypeError, space.wrap(
+            "encoder did not return a string object"))
+    return w_str
 
 @cpython_api([PyObject], PyObject)
 def PyUnicode_AsUnicodeEscapeString(space, w_unicode):
