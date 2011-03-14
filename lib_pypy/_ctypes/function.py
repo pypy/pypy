18a0c9cd05404d51272d687e8af975cc3f44196c
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-03-14 13:53:10 +0100

    Implement _ctypes.call_function() and fix on test in test_random_things

--- lib_pypy/_ctypes/function.py
+++ lib_pypy/_ctypes/function.py
@@ -29,6 +29,12 @@ def get_com_error(errcode, riid, pIunk):
     from _ctypes import COMError
     return COMError(errcode, None, None)
 
+def call_function(func, args):
+    "Only for debugging so far: So that we can call CFunction instances"
+    funcptr = CFuncPtr(func)
+    funcptr.restype = int
+    return funcptr(*args)
+
 class CFuncPtrType(_CDataMeta):
     # XXX write down here defaults and such things
 
@@ -160,6 +166,8 @@ class CFuncPtr(_CData):
     errcheck = property(_geterrcheck, _seterrcheck, _delerrcheck)
 
     def _ffishapes(self, args, restype):
+        if args is None:
+            args = []
         argtypes = [arg._ffiargshape for arg in args]
         if restype is not None:
             if not isinstance(restype, SimpleType):
