9d5d8629a57818f1aad076a6be42b9abb55522cc
Daniel Roberts <Ademan555@gmail.com>
2011-03-16 11:41:09 -0700

    Cleaning up a bit.

--- pypy/jit/metainterp/optimizeopt/fold_intadd.py
+++ pypy/jit/metainterp/optimizeopt/fold_intadd.py
@@ -21,7 +21,6 @@ class OptAddition(Optimization):
                 func(self, op)
                 break
         else:
-            #self.optimize_default(op)
             self.emit_operation(op)
 
     def _int_operation(self, variable, constant, result):
@@ -66,31 +65,12 @@ class OptAddition(Optimization):
         if lv.is_constant() and rv.is_constant():
             self.emit_operation(op) # XXX: there's support for optimizing this elsewhere, right?
         elif lv.is_constant():
-            #constant = lv.box.getint()
-            #self._process_add(op.getarg(1), constant, result)
-            # TODO: implement
+            # TODO: implement?
             self.emit_operation(op)
         elif rv.is_constant():
             constant = rv.box.getint()
             self._process_add(op.getarg(0), -constant, result)
         else:
             self.emit_operation(op)
-
-    def optimize_default(self, op):
-        for i in range(op.numargs()):
-            arg = self.getvalue(op.getarg(i))
-            if arg.is_constant():
-                continue
-
-            try:
-                variable = op.getarg(i)
-                root, constant = self.args[variable]
-
-                new_op = self._int_operation(root, constant, variable)
-                self.emit_operation(new_op)
-            except KeyError:
-                pass
-        self.emit_operation(op)
-
 
 optimize_ops = _findall(OptAddition, 'optimize_')
