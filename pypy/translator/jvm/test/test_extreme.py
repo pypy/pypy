b05b332d0eb782839f14fac9b68c23ba05a84b3c
Antonio Cuni <anto.cuni@gmail.com>
2011-04-12 16:29:38 +0000

    skip this test because of a hotspot bug

--- pypy/translator/jvm/test/test_extreme.py
+++ pypy/translator/jvm/test/test_extreme.py
@@ -1,5 +1,8 @@
+import py
 from pypy.translator.jvm.test.runtest import JvmTest
 from pypy.translator.oosupport.test_template.extreme import BaseTestExtreme
 
 class TestExtreme(BaseTestExtreme, JvmTest):
-    pass
+
+    def test_runtimeerror_due_to_stack_overflow(self):
+        py.test.skip('hotspot bug')
