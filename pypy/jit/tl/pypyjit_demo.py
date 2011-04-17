056ec0f9947d3265ca0dca843107c658dbd4a646
Carl Friedrich Bolz <cfbolz@gmx.de>
2011-04-17 09:53:28 +0200

    loop with class mutation

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -1,20 +1,24 @@
 
 try:
-    import pypyjit
-    pypyjit.set_param(threshold=3, inlining=True)
+    try:
+        import pypyjit
+        pypyjit.set_param(threshold=3, inlining=True)
+    except ImportError:
+        pass
+    class A(object):
+        x = 1
+        y = 2
+    def sqrt(y):
+        a = A()
+        for i in range(y):
+            assert a.y == 2
+            assert A.__dict__['x'] == i + 1
+            A.x += 1
+        return a.x
 
-    def sqrt(y, n=10000):
-        x = y / 2
-        while n > 0:
-            #assert y > 0 and x > 0
-            if y > 0 and x > 0: pass
-            n -= 1
-            x = (x + y/x) / 2
-        return x
+    print sqrt(1000000)
 
-    print sqrt(1234, 4)
-    
 except Exception, e:
     print "Exception: ", type(e)
     print e
-    
+
