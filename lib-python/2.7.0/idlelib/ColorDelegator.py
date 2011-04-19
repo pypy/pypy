26056a43097749dc706682a1548318f911c66488
Antonio Cuni <anto.cuni@gmail.com>
2011-04-19 13:09:28 +0200

    revert changes that were commited by mistake in d7b6bd1b8284

--- lib-python/2.7.0/idlelib/ColorDelegator.py
+++ lib-python/2.7.0/idlelib/ColorDelegator.py
@@ -6,7 +6,7 @@ from Tkinter import *
 from idlelib.Delegator import Delegator
 from idlelib.configHandler import idleConf
 
-DEBUG = True
+DEBUG = False
 
 def any(name, alternates):
     "Return a named group pattern matching list of alternates."
@@ -89,7 +89,6 @@ class ColorDelegator(Delegator):
     colorizing = False
 
     def notify_range(self, index1, index2=None):
-        print 'notify_range'
         self.tag_add("TODO", index1, index2)
         if self.after_id:
             if DEBUG: print "colorizing already scheduled"
