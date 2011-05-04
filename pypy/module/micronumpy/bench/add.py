bfca82ca92d1e18d68d99558b46946106ac487b4
Alex Gaynor <alex.gaynor@gmail.com>
2011-05-04 19:20:31 -0400

    Fix for forcing with this benchmark.

--- pypy/module/micronumpy/bench/add.py
+++ pypy/module/micronumpy/bench/add.py
@@ -4,7 +4,7 @@ import numpy
 def f():
     a = numpy.zeros(10000000)
     a = a + a + a + a + a
-    if hasattr(a, 'force'):
-        a.force()
+    # To ensure that the computation isn't totally optimized away.
+    a[0] = 3.0
 
 f()
