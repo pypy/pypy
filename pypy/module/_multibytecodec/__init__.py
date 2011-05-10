37bca83416afbbe4c73d522e6b80e26c3d470b0e
Armin Rigo <arigo@tunes.org>
2011-05-10 16:21:47 +0200

    Write a bit more of the app-level interface.  Untested so far.

--- pypy/module/_multibytecodec/__init__.py
+++ pypy/module/_multibytecodec/__init__.py
@@ -4,7 +4,18 @@ from pypy.interpreter.mixedmodule import MixedModule
 class Module(MixedModule):
 
     interpleveldefs = {
+        # for compatibility this name is obscured, and should be called
+        # via the _codecs_*.py modules written in lib_pypy.
+        '__getcodec': 'interp_multibytecodec.getcodec',
     }
 
     appleveldefs = {
+        'MultibyteIncrementalEncoder':
+            'app_multibytecodec.MultibyteIncrementalEncoder',
+        'MultibyteIncrementalDecoder':
+            'app_multibytecodec.MultibyteIncrementalDecoder',
+        'MultibyteStreamReader':
+            'app_multibytecodec.MultibyteStreamReader',
+        'MultibyteStreamWriter':
+            'app_multibytecodec.MultibyteStreamWriter',
     }
