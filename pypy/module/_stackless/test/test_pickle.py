50005bfbf2aae574eafd58610a7fd2079c77c8f1
Carl Friedrich Bolz <cfbolz@gmx.de>
2011-05-11 18:42:30 +0200

    Move all pickling tests to the same file to be less confusing

--- pypy/module/_stackless/test/test_pickle.py
+++ pypy/module/_stackless/test/test_pickle.py
@@ -23,6 +23,34 @@ class AppTestPickle:
             py.test.skip('pure appdirect test (run with -A)')
         cls.space = gettestobjspace(usemodules=('_stackless',))
 
+    def test_pickle_coroutine_empty(self):
+        # this test is limited to basic pickling.
+        # real stacks can only tested with a stackless pypy build.
+        import _stackless as stackless
+        co = stackless.coroutine()
+        import pickle
+        pckl = pickle.dumps(co)
+        co2 = pickle.loads(pckl)
+        # the empty unpickled coroutine can still be used:
+        result = []
+        co2.bind(result.append, 42)
+        co2.switch()
+        assert result == [42]
+
+    def test_pickle_coroutine_bound(self):
+        import pickle
+        import _stackless
+        lst = [4]
+        co = _stackless.coroutine()
+        co.bind(lst.append, 2)
+        pckl = pickle.dumps((co, lst))
+
+        (co2, lst2) = pickle.loads(pckl)
+        assert lst2 == [4]
+        co2.switch()
+        assert lst2 == [4, 2]
+
+
     def test_simple_ish(self):
 
         import new, sys
