d2e8292589149c3861ce0b26ae045a6dd8c39e3f
Hakan Ardo <hakan@debian.org>
2011-05-21 09:08:54 +0200

    for tests we need to keep the operations even when they are cleared from the history

--- pypy/jit/metainterp/history.py
+++ pypy/jit/metainterp/history.py
@@ -960,7 +960,7 @@ class Stats(object):
     compiled_count = 0
     enter_count = 0
     aborted_count = 0
-    history = None
+    operations = None
 
     def __init__(self):
         self.loops = []
@@ -968,7 +968,7 @@ class Stats(object):
         self.aborted_keys = []
 
     def set_history(self, history):
-        self.history = history
+        self.operations = history.operations
 
     def aborted(self):
         self.aborted_count += 1
@@ -998,7 +998,7 @@ class Stats(object):
 
     def check_history(self, expected=None, **check):
         insns = {}
-        for op in self.history.operations:
+        for op in self.operations:
             opname = op.getopname()
             insns[opname] = insns.get(opname, 0) + 1
         if expected is not None:
