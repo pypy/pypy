f1c6ba23a58293374a2c115f499445191af486f5
Armin Rigo <arigo@tunes.org>
2011-07-02 19:18:50 +0200

    (rguillebert, arigo)
    Add the options "pypy --jit=off" and "pypy --jit=default", turning
    off the JIT or setting the default values.  (The latter is only
    useful if you use pypyjit.set_param("default"), actually.)

--- pypy/module/pypyjit/interp_jit.py
+++ pypy/module/pypyjit/interp_jit.py
@@ -176,6 +176,8 @@ def set_param(space, __args__):
     '''Configure the tunable JIT parameters.
         * set_param(name=value, ...)            # as keyword arguments
         * set_param("name=value,name=value")    # as a user-supplied string
+        * set_param("off")                      # disable the jit
+        * set_param("default")                  # restore all defaults
     '''
     # XXXXXXXXX
     args_w, kwds_w = __args__.unpack()
