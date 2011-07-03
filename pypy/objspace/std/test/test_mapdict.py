385faebd245cfa0e65e7a09f1c4d1369cda5b43e
Armin Rigo <arigo@tunes.org>
2011-07-03 11:55:02 +0200

    Test and fix.

--- pypy/objspace/std/test/test_mapdict.py
+++ pypy/objspace/std/test/test_mapdict.py
@@ -479,6 +479,7 @@ class AppTestWithMapDict(object):
         it2 = a.__dict__.popitem()
         assert it2 == ("x", 5)
         assert a.__dict__ == {}
+        raises(KeyError, a.__dict__.popitem)
 
 
 
