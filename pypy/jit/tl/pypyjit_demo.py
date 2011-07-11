47036779ecdb4584f0143e8e913b4560833d23ff
Carl Friedrich Bolz <cfbolz@gmx.de>
2011-07-11 15:58:31 +0200

    go back to a cppyy demo

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -1,11 +1,14 @@
+import cppyy
 
-try:
-    import numpy
-    a = numpy.array(range(10))
-    b = a + a + a
-    print b[3]
+import time
+import cppyy
+lib = cppyy.load_lib("../../module/cppyy/test/example01Dict.so")
+cls = cppyy._type_byname('example01')
+inst = cls.construct(-17)
 
-except Exception, e:
-    print "Exception: ", type(e)
-    print e
-
+t1 = time.time()
+res = 0
+for i in range(10):
+    res += inst.invoke("addDataToInt", i)
+t2 = time.time()
+print t2 - t1
