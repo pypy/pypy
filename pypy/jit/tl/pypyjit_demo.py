47cecd5a08be12c95d3fd51e119a788aa8dac854
Carl Friedrich Bolz <cfbolz@gmx.de>
2011-07-11 17:54:08 +0200

    the interface changed

--- pypy/jit/tl/pypyjit_demo.py
+++ pypy/jit/tl/pypyjit_demo.py
@@ -5,10 +5,11 @@ import cppyy
 lib = cppyy.load_lib("../../module/cppyy/test/example01Dict.so")
 cls = cppyy._type_byname('example01')
 inst = cls.construct(-17)
+cppol = cls.get_overload("addDataToInt")
 
 t1 = time.time()
 res = 0
-for i in range(10):
-    res += inst.invoke("addDataToInt", i)
+for i in range(1000000):
+    res += inst.invoke(cppol, i)
 t2 = time.time()
 print t2 - t1
