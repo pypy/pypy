ae42bec1208b74ac0a642d1eab49c0115c6fa7be
Antonio Cuni <anto.cuni@gmail.com>
2011-07-27 10:43:53 +0200

    this makes all these tests failing, because pypy does not correctly unwrap the parameters before passing them to the callbacks

--- pypy/module/test_lib_pypy/ctypes_tests/test_callbacks.py
+++ pypy/module/test_lib_pypy/ctypes_tests/test_callbacks.py
@@ -14,13 +14,26 @@ class TestCallbacks(BaseCTypesTestChecker):
         return args[-1]
 
     def check_type(self, typ, arg):
+        unwrapped_types = {
+            c_float: float,
+            c_double: float,
+            c_char: str,
+            c_char_p: str,
+            c_uint: long,
+            c_ulong: long,
+            }
+        
         PROTO = self.functype.im_func(typ, typ)
-        result = PROTO(self.callback)(arg)
+        cfunc = PROTO(self.callback)
+        result = cfunc(arg)
         if typ == c_float:
             assert abs(result - arg) < 0.000001
         else:
             assert self.got_args == (arg,)
             assert result == arg
+
+        result2 = cfunc(typ(arg))
+        assert type(result2) is unwrapped_types.get(typ, int)
 
         PROTO = self.functype.im_func(typ, c_byte, typ)
         result = PROTO(self.callback)(-3, arg)
