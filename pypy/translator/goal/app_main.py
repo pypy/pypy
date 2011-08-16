812bbe77fdbb2ca18e6f0975105929cda618d514
Armin Rigo <arigo@tunes.org>
2011-08-16 14:44:12 +0200

    Support for PYTHONDONTWRITEBYTECODE, setting sys.dont_write_bytecode; and when it is set, don't write .pyc files. (transplanted from 6dff8ba8be761d1313c1fc8d3d29221d35c55c85)

--- pypy/translator/goal/app_main.py
+++ pypy/translator/goal/app_main.py
@@ -424,8 +424,11 @@ def parse_command_line(argv):
     # (relevant in case of "reload(sys)")
     sys.argv[:] = argv
 
-    if (PYTHON26 and not options["ignore_environment"] and os.getenv('PYTHONNOUSERSITE')):
-        options["no_user_site"] = True
+    if PYTHON26 and not options["ignore_environment"]:
+        if os.getenv('PYTHONNOUSERSITE'):
+            options["no_user_site"] = True
+        if os.getenv('PYTHONDONTWRITEBYTECODE'):
+            options["dont_write_bytecode"] = True
 
     if (options["interactive"] or
         (not options["ignore_environment"] and os.getenv('PYTHONINSPECT'))):
@@ -434,7 +437,8 @@ def parse_command_line(argv):
     if PYTHON26 and we_are_translated():
         flags = [options[flag] for flag in sys_flags]
         sys.flags = type(sys.flags)(flags)
-        sys.py3kwarning = sys.flags.py3k_warning
+        sys.py3kwarning = bool(sys.flags.py3k_warning)
+        sys.dont_write_bytecode = bool(sys.flags.dont_write_bytecode)
 
         if sys.py3kwarning:
             print >> sys.stderr, (
