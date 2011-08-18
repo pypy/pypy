6cc5a51d2175cfaadacfcf7bbfde03f29f3d217e
Hakan Ardo <hakan@debian.org>
2011-08-18 14:36:27 +0200

    retrace counter is now only increased when we actually do a retrace, unskip test

--- pypy/jit/metainterp/test/test_ajit.py
+++ pypy/jit/metainterp/test/test_ajit.py
@@ -1788,9 +1788,8 @@ class BasicTests:
             return a1.val + b1.val
         res = self.meta_interp(g, [6, 14])
         assert res == g(6, 14)
-        self.check_loop_count(8)
-        self.check_loops(getarrayitem_gc=7, everywhere=True)
-        py.test.skip("for the following, we need setarrayitem(varindex)")
+        self.check_loop_count(9)
+        self.check_loops(getarrayitem_gc=8, everywhere=True)
 
     def test_multiple_specialied_versions_bridge(self):
         myjitdriver = JitDriver(greens = [], reds = ['y', 'x', 'z', 'res'])
