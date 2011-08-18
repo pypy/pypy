73835a28df5026e2def940462b26308f67d1d1aa
hager <sven.hager@uni-duesseldorf.de>
2011-08-18 18:34:11 +0200

    Added arch.py.

---
+++ pypy/jit/backend/ppc/ppcgen/arch.py
@@ -0,0 +1,12 @@
+# Constants that depend on whether we are on 32-bit or 64-bit
+
+import sys
+if sys.maxint == (2**31 - 1):
+    WORD = 4
+    IS_PPC_32 = True
+    IS_PPC_64 = False
+else:
+    WORD = 8
+    IS_PPC_32 = False
+    IS_PPC_64 = True
+
