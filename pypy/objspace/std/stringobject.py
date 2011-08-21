fad3c64c3f03c7dbf74819d4b7d4835fc4edccee
Armin Rigo <arigo@tunes.org>
2011-08-21 11:09:38 +0200

    Backout 1f8c162174c6.  It fails some tests, in test_stringobject (obvious place).

--- pypy/objspace/std/stringobject.py
+++ pypy/objspace/std/stringobject.py
@@ -924,36 +924,35 @@ def repr__String(space, w_str):
 
     for i in range(len(s)):
         c = s[i]
-        if not '\x20' <= c < '\x7f':
-            use_bs_char = False # character quoted by backspace
+        use_bs_char = False # character quoted by backspace
 
-            if c == '\\' or c == quote:
-                bs_char = c
-                use_bs_char = True
-            elif c == '\t':
-                bs_char = 't'
-                use_bs_char = True
-            elif c == '\r':
-                bs_char = 'r'
-                use_bs_char = True
-            elif c == '\n':
-                bs_char = 'n'
-                use_bs_char = True
-            else:
-                n = ord(c)
-                if i != startslice:
-                    buf.append_slice(s, startslice, i)
-                startslice = i + 1
-                buf.append('\\x')
-                buf.append("0123456789abcdef"[n>>4])
-                buf.append("0123456789abcdef"[n&0xF])
+        if c == '\\' or c == quote:
+            bs_char = c
+            use_bs_char = True
+        elif c == '\t':
+            bs_char = 't'
+            use_bs_char = True
+        elif c == '\r':
+            bs_char = 'r'
+            use_bs_char = True
+        elif c == '\n':
+            bs_char = 'n'
+            use_bs_char = True
+        elif not '\x20' <= c < '\x7f':
+            n = ord(c)
+            if i != startslice:
+                buf.append_slice(s, startslice, i)
+            startslice = i + 1
+            buf.append('\\x')
+            buf.append("0123456789abcdef"[n>>4])
+            buf.append("0123456789abcdef"[n&0xF])
 
-            if use_bs_char:
-                if i != startslice:
-                    buf.append_slice(s, startslice, i)
-                startslice = i + 1
-                buf.append('\\')
-                buf.append(bs_char)
+        if use_bs_char:
+            if i != startslice:
+                buf.append_slice(s, startslice, i)
+            startslice = i + 1
+            buf.append('\\')
+            buf.append(bs_char)
 
     if len(s) != startslice:
         buf.append_slice(s, startslice, len(s))
