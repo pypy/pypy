028dd5cf6780da3e35efea32caac7aa91864fbbd
Hakan Ardo <hakan@debian.org>
2011-08-29 21:15:31 +0200

    translation fix

--- pypy/jit/metainterp/optimizeopt/virtualize.py
+++ pypy/jit/metainterp/optimizeopt/virtualize.py
@@ -58,7 +58,7 @@ class AbstractVirtualValue(optimizer.OptValue):
     def _really_force(self):
         raise NotImplementedError("abstract base")
 
-    def import_from(self, other):
+    def import_from(self, other, optimizer):
         raise NotImplementedError("should not be called at this level")
     
 def get_fielddescrlist_cache(cpu):
