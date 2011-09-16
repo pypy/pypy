d2d07acf5429c1dd7d6173e3fef909f76e854ff9
Carl Friedrich Bolz <cfbolz@gmx.de>
2011-09-16 11:01:17 +0200

    change the trace length limit to half its previous value. this seems to be better now with the better heap caching, will do a benchmark run to confirm.

--- pypy/rlib/jit.py
+++ pypy/rlib/jit.py
@@ -294,7 +294,7 @@ class JitHintError(Exception):
 PARAMETERS = {'threshold': 1032, # just above 1024
               'function_threshold': 1617, # slightly more than one above
               'trace_eagerness': 200,
-              'trace_limit': 12000,
+              'trace_limit': 6000,
               'inlining': 1,
               'loop_longevity': 1000,
               'retrace_limit': 5,
