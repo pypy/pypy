71dd38d7fc4c19d6b7240bcce0a6fab8936b0831
Armin Rigo <arigo@tunes.org>
2011-09-22 12:03:56 +0200

    Implement gr_frame.

--- pypy/module/test_lib_pypy/test_greenlet.py
+++ pypy/module/test_lib_pypy/test_greenlet.py
@@ -258,3 +258,25 @@ class AppTestGreenlet:
             assert sys.exc_info() == (None, None, None)
 
         greenlet(f).switch()
+
+    def test_gr_frame(self):
+        from greenlet import greenlet
+        import sys
+        def f2():
+            assert g.gr_frame is None
+            gmain.switch()
+            assert g.gr_frame is None
+        def f1():
+            assert gmain.gr_frame is gmain_frame
+            assert g.gr_frame is None
+            f2()
+            assert g.gr_frame is None
+        gmain = greenlet.getcurrent()
+        assert gmain.gr_frame is None
+        gmain_frame = sys._getframe()
+        g = greenlet(f1)
+        assert g.gr_frame is None
+        g.switch()
+        assert g.gr_frame.f_code.co_name == 'f2'
+        g.switch()
+        assert g.gr_frame is None
