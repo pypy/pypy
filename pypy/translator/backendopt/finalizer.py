860f49b82e4a0c851d616c8a5a34d78441740d7d
Maciej Fijalkowski <fijall@gmail.com>
2011-10-09 21:54:34 +0200

    track_allocation is just fine

--- pypy/translator/backendopt/finalizer.py
+++ pypy/translator/backendopt/finalizer.py
@@ -12,7 +12,7 @@ class FinalizerAnalyzer(graphanalyze.BoolGraphAnalyzer):
     * anything that can allocate
     """
     ok_operations = ['ptr_nonzero', 'free', 'same_as',
-                     'direct_ptradd', 'force_cast']
+                     'direct_ptradd', 'force_cast', 'track_allocation']
     
     def analyze_simple_operation(self, op, graphinfo):
         if op.opname in self.ok_operations:
