4ec4a00427c828fae0558abd1d8da70cf6430128
Justin Peel <notmuchtotell@gmail.com>
2011-10-17 14:16:41 -0600

    better estimate for size of W_Hash's mallocs

--- pypy/module/_hashlib/interp_hashlib.py
+++ pypy/module/_hashlib/interp_hashlib.py
@@ -12,10 +12,12 @@ from pypy.module.thread.os_lock import Lock
 
 algorithms = ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
 
-# HASH_MALLOC_SIZE is the size of EVP_MD, EVP_MD_CTX plus their points.
-# Used for adding memory pressure.
+# HASH_MALLOC_SIZE is the size of EVP_MD, EVP_MD_CTX plus their points
+# Used for adding memory pressure. Last number is an (under?)estimate of
+# EVP_PKEY_CTX's size.
+# XXX: Make a better estimate here
 HASH_MALLOC_SIZE = ropenssl.EVP_MD_SIZE + ropenssl.EVP_MD_CTX_SIZE \
-        + rffi.sizeof(ropenssl.EVP_MD) * 2
+        + rffi.sizeof(ropenssl.EVP_MD) * 2 + 208
 
 class W_Hash(Wrappable):
     ctx = lltype.nullptr(ropenssl.EVP_MD_CTX.TO)
