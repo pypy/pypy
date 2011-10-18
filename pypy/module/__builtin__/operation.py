1ddc2bddcc68a13707ff3eb0be9f565ed1edbb75
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-10-18 08:16:56 +0200

    Translation fix

--- pypy/module/__builtin__/operation.py
+++ pypy/module/__builtin__/operation.py
@@ -38,7 +38,7 @@ def checkattrname(space, w_name):
     # Note that if w_name is already a string (or a subclass of str),
     # it must be returned unmodified (and not e.g. unwrapped-rewrapped).
     if not space.is_w(space.type(w_name), space.w_text):
-        name = space.text_w(w_name)   # typecheck
+        name = space.str_w(w_name)    # typecheck
         w_name = space.wrap(name)     # rewrap as a real string
     return w_name
 
