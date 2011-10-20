fc2f9da8310138ac16935dd2382314b5ae945af3
Alex Gaynor <alex.gaynor@gmail.com>
2011-10-20 00:04:23 -0400

    convert zip to app-level, it's a tad faster this way.  also, more app-level code makes me happy

--- pypy/module/__builtin__/__init__.py
+++ pypy/module/__builtin__/__init__.py
@@ -23,6 +23,7 @@ class Module(MixedModule):
         'map'           : 'app_functional.map',
         'reduce'        : 'app_functional.reduce',
         'filter'        : 'app_functional.filter',
+        'zip'           : 'app_functional.zip',
         'vars'          : 'app_inspect.vars',
         'dir'           : 'app_inspect.dir',
 
@@ -89,7 +90,6 @@ class Module(MixedModule):
         'enumerate'     : 'functional.W_Enumerate',
         'min'           : 'functional.min',
         'max'           : 'functional.max',
-        'zip'           : 'functional.zip',
         'reversed'      : 'functional.reversed',
         'super'         : 'descriptor.W_Super',
         'staticmethod'  : 'descriptor.StaticMethod',
