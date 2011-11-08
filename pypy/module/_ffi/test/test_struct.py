50837cae658e5bc3b545f68e52745e6a646a76fd
Antonio Cuni <anto.cuni@gmail.com>
2011-11-08 10:46:27 +0100

    move this test at the end, after the ones which directly operate on StructDescr

--- pypy/module/_ffi/test/test_struct.py
+++ pypy/module/_ffi/test/test_struct.py
@@ -27,22 +27,6 @@ class AppTestStruct(BaseAppTestFFI):
         assert descr.ffitype.sizeof() == longsize*2
         assert descr.ffitype.name == 'struct foo'
 
-    def test_compute_shape(self):
-        from _ffi import Structure, Field, types
-        class Point(Structure):
-            _fields_ = [
-                Field('x', types.slong),
-                Field('y', types.slong),
-                ]
-
-        longsize = types.slong.sizeof()
-        assert isinstance(Point.x, Field)
-        assert isinstance(Point.y, Field)
-        assert Point.x.offset == 0
-        assert Point.y.offset == longsize
-        assert Point._struct_.ffitype.sizeof() == longsize*2
-        assert Point._struct_.ffitype.name == 'struct Point'
-
     def test_getfield_setfield(self):
         from _ffi import _StructDescr, Field, types
         longsize = types.slong.sizeof()
@@ -70,3 +54,20 @@ class AppTestStruct(BaseAppTestFFI):
         struct = descr.allocate()
         raises(AttributeError, "struct.getfield('missing')")
         raises(AttributeError, "struct.setfield('missing', 42)")
+
+    def test_compute_shape(self):
+        from _ffi import Structure, Field, types
+        class Point(Structure):
+            _fields_ = [
+                Field('x', types.slong),
+                Field('y', types.slong),
+                ]
+
+        longsize = types.slong.sizeof()
+        assert isinstance(Point.x, Field)
+        assert isinstance(Point.y, Field)
+        assert Point.x.offset == 0
+        assert Point.y.offset == longsize
+        assert Point._struct_.ffitype.sizeof() == longsize*2
+        assert Point._struct_.ffitype.name == 'struct Point'
+
