aafdb5a6fab7d03dc37db7eb6a8acef23c2722e6
Alex Gaynor <alex.gaynor@gmail.com>
2011-11-11 11:22:26 -0500

    Fix for rffi.cast(lltype.SingleFloat, <some r_singlefloat>), if the input type is the same as the restype, just retunr the obj.

--- pypy/rpython/lltypesystem/ll2ctypes.py
+++ pypy/rpython/lltypesystem/ll2ctypes.py
@@ -1179,6 +1179,8 @@ def force_cast(RESTYPE, value):
         cvalue = ord(cvalue)     # character -> integer
     elif hasattr(RESTYPE, "_type") and issubclass(RESTYPE._type, base_int):
         cvalue = int(cvalue)
+    elif RESTYPE is TYPE1:
+        return value
 
     if not isinstance(cvalue, (int, long, float)):
         raise NotImplementedError("casting %r to %r" % (TYPE1, RESTYPE))
