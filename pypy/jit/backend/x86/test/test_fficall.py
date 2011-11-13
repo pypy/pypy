deb17d23f7a63ecaee24124ad7f9ec7010b0022a
Alex Gaynor <alex.gaynor@gmail.com>
2011-11-13 15:43:52 -0500

    forgotten test file

---
+++ pypy/jit/backend/x86/test/test_fficall.py
@@ -0,0 +1,8 @@
+import py
+from pypy.jit.metainterp.test import test_fficall
+from pypy.jit.backend.x86.test.test_basic import Jit386Mixin
+
+class TestFfiCall(Jit386Mixin, test_fficall.FfiCallTests):
+    # for the individual tests see
+    # ====> ../../../metainterp/test/test_fficall.py
+    pass
