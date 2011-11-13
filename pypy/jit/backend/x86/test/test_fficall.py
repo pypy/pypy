497c62a6f2deb0f3ab03a54d20833b805a789bc3
Alex Gaynor <alex.gaynor@gmail.com>
2011-11-13 15:44:45 -0500

    missing attr

--- pypy/jit/backend/x86/test/test_fficall.py
+++ pypy/jit/backend/x86/test/test_fficall.py
@@ -5,4 +5,4 @@ from pypy.jit.backend.x86.test.test_basic import Jit386Mixin
 class TestFfiCall(Jit386Mixin, test_fficall.FfiCallTests):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_fficall.py
-    pass
+    supports_all = True
