79ff70f955938e01c159ab68092478e84dff34e6
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2011-11-15 00:46:25 +0100

    cpyext: turn some parameters into const char*

--- pypy/module/cpyext/include/eval.h
+++ pypy/module/cpyext/include/eval.h
@@ -14,8 +14,8 @@ extern "C" {
 
 PyObject * PyEval_CallFunction(PyObject *obj, const char *format, ...);
 PyObject * PyEval_CallMethod(PyObject *obj, const char *name, const char *format, ...);
-PyObject * PyObject_CallFunction(PyObject *obj, char *format, ...);
-PyObject * PyObject_CallMethod(PyObject *obj, char *name, char *format, ...);
+PyObject * PyObject_CallFunction(PyObject *obj, const char *format, ...);
+PyObject * PyObject_CallMethod(PyObject *obj, const char *name, const char *format, ...);
 PyObject * PyObject_CallFunctionObjArgs(PyObject *callable, ...);
 PyObject * PyObject_CallMethodObjArgs(PyObject *callable, PyObject *name, ...);
 
