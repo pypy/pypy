cbded90344e3e1a2c412bcaab9d7c0984b6b9025
Christian Tismer <tismer@stackless.com>
2011-11-22 19:04:06 +0100

    test_zpy works on win64

--- pypy/__init__.py
+++ pypy/__init__.py
@@ -1,1 +1,9 @@
 # Empty
+# XXX Should be empty again, soon.
+# XXX hack for win64:
+# This patch must stay here until the ENDS OF STAGE 1
+# When all tests work, this branch will be merged
+# and the branch stage 2 is started, where we remove this patch.
+import sys
+if hasattr(sys, "maxsize"):
+    sys.maxint = max(sys.maxint, sys.maxsize)
