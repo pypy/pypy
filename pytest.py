eb043369ecc44503be9d2f76f92e3a9356c14e23
Christian Tismer <tismer@stackless.com>
2011-11-22 03:44:44 +0100

    forgot this patch for sys.maxint, which has bad consequences. Note that this patch must stay until the end of stage 1.

--- pytest.py
+++ pytest.py
@@ -4,6 +4,14 @@ unit and functional testing with Python.
 """
 __all__ = ['main']
 
+# XXX hack for win64:
+# This patch must stay here until the ENDS OF STAGE 1
+# When all tests work, this branch will be merged
+# and the branch stage 2 is started, where we remove this patch.
+import sys
+if hasattr(sys, "maxsize"):
+    sys.maxint = max(sys.maxint, sys.maxsize)
+
 from _pytest.core import main, UsageError, _preloadplugins
 from _pytest import core as cmdline
 from _pytest import __version__
