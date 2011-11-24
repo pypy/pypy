89e2a084f31f87f1bbbd79126aad48c93d55dfa2
Christian Tismer <tismer@stackless.com>
2011-11-24 22:50:10 +0100

    removed a merge glitch

--- pypy/jit/metainterp/history.py
+++ pypy/jit/metainterp/history.py
@@ -269,7 +269,6 @@ class ConstInt(Const):
     def __init__(self, value):
         if not we_are_translated():
             if isinstance(value, (int, long)):
-            if isinstance(value, (int, long)):
                 value = int(value)    # bool -> int
             else:
                 assert isinstance(value, Symbolic)
