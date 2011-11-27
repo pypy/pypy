f756f2aca1b9c40dfcd71fe2fddbf6a5d8681fb6
Armin Rigo <arigo@tunes.org>
2011-11-27 11:34:57 +0100

    Bah.

--- pypy/rlib/test/test_rstacklet.py
+++ pypy/rlib/test/test_rstacklet.py
@@ -224,12 +224,12 @@ class BaseTestStacklet(StandaloneTests):
             config.translation.continuation = True
             config.translation.gcrootfinder = cls.gcrootfinder
             GCROOTFINDER = cls.gcrootfinder
-        if cls.gcrootfinder == "asmgcc" and sys.platform == "win32":
-            py.test.skip("fails with asmgcc on win32")
         cls.config = config
         cls.old_values = Runner.config, Runner.STATUSMAX
         Runner.config = config
         Runner.STATUSMAX = 25000
+        if cls.gcrootfinder == "asmgcc" and sys.platform == "win32":
+            py.test.skip("fails with asmgcc on win32")
 
     def teardown_class(cls):
         Runner.config, Runner.STATUSMAX = cls.old_values
