5227eb6b9accce0bbf82cb7bd480ddb4f09b2f17
Christian Tismer <tismer@stackless.com>
2011-11-30 01:11:29 +0100

    adjusted nullpath according to amaury's suggestion

--- pypy/tool/nullpath.py
+++ pypy/tool/nullpath.py
@@ -1,17 +1,12 @@
 import py, os
 
-if os.name <> 'nt':
-    NULLPATHNAME = '/dev/null'
-else:
-    NULLPATHNAME = 'NUL'
-
 class NullPyPathLocal(py.path.local):
 
     def join(self, *args):
         return self.__class__(py.path.local.join(self, *args))
 
     def open(self, mode):
-        return open(NULLPATHNAME, mode)
+        return open(os.devnull, mode)
 
     def __repr__(self):
         return py.path.local.__repr__(self) + ' [fake]'
