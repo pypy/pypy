60147977ef5c5b283d37a3ad2eb2d6899f360bd7
Hakan Ardo <hakan@debian.org>
2011-12-04 20:09:26 +0100

    fix test

--- pypy/module/micronumpy/test/test_zjit.py
+++ pypy/module/micronumpy/test/test_zjit.py
@@ -325,7 +325,7 @@ class TestNumpyJIt(LLJitMixin):
     def test_setslice(self):
         result = self.run("setslice")
         assert result == 11.0
-        self.check_loop_count(1)
+        self.check_trace_count(1)
         self.check_simple_loop({'getinteriorfield_raw': 2, 'float_add' : 1,
                                 'setinteriorfield_raw': 1, 'int_add': 3,
                                 'int_eq': 1, 'guard_false': 1, 'jump': 1})
