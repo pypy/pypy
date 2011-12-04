4913e0232d4aebbc12896d47219321e2bd8edf67
Hakan Ardo <hakan@debian.org>
2011-12-04 12:47:12 +0100

    this is not a simple loop and ops are somewhat reoordered now

--- pypy/module/pypyjit/test_pypy_c/test_generators.py
+++ pypy/module/pypyjit/test_pypy_c/test_generators.py
@@ -15,12 +15,14 @@ class TestGenerators(BaseTestPyPyC):
             g()
 
         log = self.run(main, [500])
-        loop, = log.loops_by_filename(self.filepath)
+        loop, = log.loops_by_filename(self.filepath, is_entry_bridge='*')
         assert loop.match_by_id("generator", """
+            ...
+            label(..., descr=...)
             i16 = force_token()
             p45 = new_with_vtable(ConstClass(W_IntObject))
-            i47 = arraylen_gc(p8, descr=<GcPtrArrayDescr>) # Should be removed by backend
-            setarrayitem_gc(p8, 0, p45, descr=<GcPtrArrayDescr>)
             setfield_gc(p45, i29, descr=<SignedFieldDescr .*>)
+            setarrayitem_gc(p8, 0, p45, descr=<GcPtrArrayDescr>)
+            i47 = arraylen_gc(p8, descr=<GcPtrArrayDescr>) # Should be removed by backend
             jump(..., descr=...)
             """)
