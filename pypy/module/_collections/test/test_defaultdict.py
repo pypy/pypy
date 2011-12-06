aa0071d92c7bbe7b2ce78ff17da2aab23290b3ba
Armin Rigo <arigo@tunes.org>
2011-12-06 17:44:57 +0100

    Test and fix.

--- pypy/module/_collections/test/test_defaultdict.py
+++ pypy/module/_collections/test/test_defaultdict.py
@@ -38,3 +38,22 @@ class AppTestBasic:
         from _collections import defaultdict
         d = defaultdict(default_factory=5)
         assert d.keys() == ['default_factory']
+
+    def test_copy(self):
+        import _collections
+        def f():
+            return 42
+        d = _collections.defaultdict(f, {2: 3})
+        #
+        d1 = d.copy()
+        assert type(d1) is _collections.defaultdict
+        assert len(d1) == 1
+        assert d1[2] == 3
+        assert d1[3] == 42
+        #
+        import copy
+        d2 = copy.deepcopy(d)
+        assert type(d2) is _collections.defaultdict
+        assert len(d2) == 1
+        assert d2[2] == 3
+        assert d2[3] == 42
