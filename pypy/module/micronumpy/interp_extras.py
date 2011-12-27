d306dfe1fbc0efd08e674f347b6a41a31206b081
Maciej Fijalkowski <fijall@gmail.com>
2011-12-27 15:18:28 +0200

    a neat debugging feature

--- pypy/module/micronumpy/interp_extras.py
+++ pypy/module/micronumpy/interp_extras.py
@@ -5,3 +5,11 @@ from pypy.module.micronumpy.interp_numarray import BaseArray
 @unwrap_spec(array=BaseArray)
 def debug_repr(space, array):
     return space.wrap(array.find_sig().debug_repr())
+
+@unwrap_spec(array=BaseArray)
+def remove_invalidates(space, array):
+    """ Array modification will no longer invalidate any of it's
+    potential children. Use only for performance debugging
+    """
+    del array.invalidates[:]
+    return space.w_None
