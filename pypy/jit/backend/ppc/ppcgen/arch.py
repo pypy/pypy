9228fdb22c6879dc1464589e69e5d6d96cb5e166
edelsohn <unknown>
2011-12-28 14:03:25 -0500

    BACKCHAIN_SIZE is 6

--- pypy/jit/backend/ppc/ppcgen/arch.py
+++ pypy/jit/backend/ppc/ppcgen/arch.py
@@ -12,7 +12,7 @@ if sys.maxint == (2**31 - 1):
 else:
     WORD = 8
     IS_PPC_32 = False
-    BACKCHAIN_SIZE = 4
+    BACKCHAIN_SIZE = 6
 
 DWORD                   = 2 * WORD
 IS_PPC_64               = not IS_PPC_32
