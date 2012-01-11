28d61c68e7ada83ba50d2683caf982565fccb816
Maciej Fijalkowski <fijall@gmail.com>
2012-01-11 19:22:29 +0200

    fix a translation problem

--- pypy/jit/metainterp/compile.py
+++ pypy/jit/metainterp/compile.py
@@ -76,7 +76,7 @@ def record_loop_or_bridge(metainterp_sd, loop):
             if descr is not original_jitcell_token:
                 original_jitcell_token.record_jump_to(descr)
             descr.exported_state = None
-            op._descr = None    # clear reference, mostly for tests
+            op.setdescr(None)
         elif isinstance(descr, TargetToken):
             # for a JUMP: record it as a potential jump.
             # (the following test is not enough to prevent more complicated
