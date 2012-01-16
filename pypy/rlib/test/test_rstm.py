c13a24b558e7a261cf0ed7d21b5a8bf8ea461199
Armin Rigo <arigo@tunes.org>
2012-01-16 18:44:55 +0100

    (antocuni, arigo)
    Test (maybe) for inevitable transactions.

--- pypy/rlib/test/test_rstm.py
+++ pypy/rlib/test/test_rstm.py
@@ -1,3 +1,4 @@
+import os
 from pypy.rlib.debug import debug_print
 from pypy.rlib import rstm
 from pypy.translator.stm.test.support import CompiledSTMTests
@@ -8,12 +9,15 @@ class Arg(object):
 
 def setx(arg):
     debug_print(arg.x)
+    if arg.x == 303:
+        # this will trigger stm_become_inevitable()
+        os.write(1, "hello\n")
     arg.x = 42
 
 
-def test_stm_perform_transaction():
+def test_stm_perform_transaction(initial_x=202):
     arg = Arg()
-    arg.x = 202
+    arg.x = initial_x
     rstm.descriptor_init()
     rstm.perform_transaction(setx, Arg, arg)
     rstm.descriptor_done()
@@ -42,3 +46,12 @@ class TestTransformSingleThread(CompiledSTMTests):
         dataout, dataerr = cbuilder.cmdexec('', err=True)
         assert dataout == ''
         assert '202' in dataerr.splitlines()
+
+    def test_perform_transaction_inevitable(self):
+        def f(argv):
+            test_stm_perform_transaction(303)
+            return 0
+        t, cbuilder = self.compile(f)
+        dataout, dataerr = cbuilder.cmdexec('', err=True)
+        assert 'hello' in dataout.splitlines()
+        assert '303' in dataerr.splitlines()
