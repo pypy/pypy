5e5db7b6c0b7f7c5488db822eee5460382653d81
Maciej Fijalkowski <fijall@gmail.com>
2012-01-17 14:48:15 +0200

    (fijal, cfbolz) improve error reporting

--- pypy/objspace/std/unicodeobject.py
+++ pypy/objspace/std/unicodeobject.py
@@ -218,19 +218,19 @@ def _unicode_join_many_items(space, w_self, list_w, size):
     self = w_self._value
     prealloc_size = len(self) * (size - 1)
     for i in range(size):
-        prealloc_size += len(space.unicode_w(list_w[i]))
-    sb = UnicodeBuilder(prealloc_size)
-    for i in range(size):
-        if self and i != 0:
-            sb.append(self)
-        w_s = list_w[i]
         try:
-            sb.append(space.unicode_w(w_s))
+            prealloc_size += len(space.unicode_w(list_w[i]))
         except OperationError, e:
             if not e.match(space, space.w_TypeError):
                 raise
             raise operationerrfmt(space.w_TypeError,
                         "sequence item %d: expected string or Unicode", i)
+    sb = UnicodeBuilder(prealloc_size)
+    for i in range(size):
+        if self and i != 0:
+            sb.append(self)
+        w_s = list_w[i]
+        sb.append(space.unicode_w(w_s))
     return space.wrap(sb.build())
 
 def hash__Unicode(space, w_uni):
