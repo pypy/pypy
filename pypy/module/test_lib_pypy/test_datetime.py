1d3c7d9ba7a26de117a60d02005f297cf367b7c3
David Ripton <dripton@ripton.net>
2012-01-21 11:13:37 -0500

    Clean up os.environ["TZ"] when we're done, in case other tests follow.

--- pypy/module/test_lib_pypy/test_datetime.py
+++ pypy/module/test_lib_pypy/test_datetime.py
@@ -9,9 +9,16 @@ def test_utcfromtimestamp():
 
     Based on danchr's test script in https://bugs.pypy.org/issue986
     """
-    os.putenv("TZ", "GMT")
-    for unused in xrange(100):
-        now = time.time()
-        delta = (datetime.datetime.utcfromtimestamp(now) -
-                 datetime.datetime.fromtimestamp(now))
-        assert delta.days * 86400 + delta.seconds == 0
+    try:
+        prev_tz = os.environ.get("TZ")
+        os.environ["TZ"] = "GMT"
+        for unused in xrange(100):
+            now = time.time()
+            delta = (datetime.datetime.utcfromtimestamp(now) -
+                     datetime.datetime.fromtimestamp(now))
+            assert delta.days * 86400 + delta.seconds == 0
+    finally:
+        if prev_tz is None:
+            del os.environ["TZ"]
+        else:
+            os.environ["TZ"] = prev_tz
