c10a197322cf1610ff7baf85ddc5ff48854b05e7
David Ripton <dripton@ripton.net>
2012-01-22 18:30:04 -0500

    Add an assert, as requested by Alex.

--- pypy/module/test_lib_pypy/test_sqlite3.py
+++ pypy/module/test_lib_pypy/test_sqlite3.py
@@ -1,9 +1,10 @@
 """Tests for _sqlite3.py"""
 
 def test_list_ddl():
-    """From issue996.  Just looking for lack of exceptions."""
+    """From issue996.  Mostly just looking for lack of exceptions."""
     from sqlite3.dbapi2 import connect
     connection = connect(':memory:')
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE foo (bar INTEGER)')
     result = list(cursor)
+    assert result == []
