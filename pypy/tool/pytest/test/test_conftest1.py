3ef78e251c989970aee2e9691bbb2a5dbb9cb376
Antonio Cuni <anto.cuni@gmail.com>
2012-01-25 16:35:35 +0100

    test docstrings separately from the other applevel tests

--- pypy/tool/pytest/test/test_conftest1.py
+++ pypy/tool/pytest/test/test_conftest1.py
@@ -15,18 +15,17 @@ class TestPyPyTests:
         assert "test_method" in passed[1].nodeid
 
     def test_selection_by_keyword_app(self, testdir): 
-        sorter = testdir.inline_run("-k", "applevel", innertest)
+        sorter = testdir.inline_run("-k", "applevel -docstring", innertest)
         passed, skipped, failed = sorter.listoutcomes()
-        assert len(passed) == 3
-        assert len(failed) == 1
+        assert len(passed) == 2
+        assert failed == []
         assert skipped == []
         assert "app_test_something" in passed[0].nodeid
         assert "test_method_app" in passed[1].nodeid
-        assert "test_code_in_docstring_ignored" in passed[2].nodeid
-        assert "test_code_in_docstring_failing" in failed[0].nodeid
-        
+
     def test_runappdirect(self, testdir):
-        sorter = testdir.inline_run(innertest, '-k', 'applevel', '--runappdirect')
+        sorter = testdir.inline_run(innertest, '-k', 'applevel -docstring',
+                                    '--runappdirect')
         passed, skipped, failed = sorter.listoutcomes()
         assert len(passed) == 3
         print passed
@@ -34,7 +33,7 @@ class TestPyPyTests:
         assert "test_method_app" in passed[1].nodeid
         
     def test_appdirect(self, testdir):
-        sorter = testdir.inline_run(innertest, '-k', 'applevel',
+        sorter = testdir.inline_run(innertest, '-k', 'applevel -docstring',
                                     '--appdirect=%s' % (sys.executable,))
         passed, skipped, failed = sorter.listoutcomes()
         assert len(passed) == 3
@@ -42,7 +41,7 @@ class TestPyPyTests:
         assert "app_test_something" in passed[0].nodeid
         assert "test_method_app" in passed[1].nodeid
         
-    def test_docstrings(self, testdir): 
+    def test_docstring_in_methods(self, testdir): 
         sorter = testdir.inline_run("-k", "test_code_in_docstring", innertest)
         passed, skipped, failed = sorter.listoutcomes()
         assert len(passed) == 1
