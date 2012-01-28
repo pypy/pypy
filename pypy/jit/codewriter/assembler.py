c2c754c6388b76dcc5477afc137cc642a1ccf12d
Armin Rigo <arigo@tunes.org>
2012-01-28 08:56:11 +0000

    Fix: keep the original special-cases.  Needed at least for ComputedIntSymbolics, otherwise they might force computation of their value, which is not what we want.

--- pypy/jit/codewriter/assembler.py
+++ pypy/jit/codewriter/assembler.py
@@ -78,16 +78,18 @@ class Assembler(object):
                 value = heaptracker.adr2int(value)
             if TYPE is lltype.SingleFloat:
                 value = longlong.singlefloat2int(value)
-            value = lltype.cast_primitive(lltype.Signed, value)
-            if allow_short:
-                try:
-                    short_num = -128 <= value <= 127
-                except TypeError:    # "Symbolics cannot be compared!"
-                    short_num = False
-                if short_num:
-                    # emit the constant as a small integer
-                    self.code.append(chr(value & 0xFF))
-                    return True
+            if not isinstance(value, (llmemory.AddressAsInt,
+                                      ComputedIntSymbolic)):
+                value = lltype.cast_primitive(lltype.Signed, value)
+                if allow_short:
+                    try:
+                        short_num = -128 <= value <= 127
+                    except TypeError:    # "Symbolics cannot be compared!"
+                        short_num = False
+                    if short_num:
+                        # emit the constant as a small integer
+                        self.code.append(chr(value & 0xFF))
+                        return True
             constants = self.constants_i
         elif kind == 'ref':
             value = lltype.cast_opaque_ptr(llmemory.GCREF, value)
