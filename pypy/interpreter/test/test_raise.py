0c54c81584ed7e41f610843416485fe3ff864dab
Antonio Cuni <anto.cuni@gmail.com>
2012-02-10 10:50:13 +0100

    adapt the syntax to py3k, and kill some outdated tests about the 'raise Type, args' form which is no longer valid

--- pypy/interpreter/test/test_raise.py
+++ pypy/interpreter/test/test_raise.py
@@ -9,43 +9,23 @@ class AppTestRaise:
     def test_control_flow(self):
         try:
             raise Exception
-            raise AssertionError, "exception failed to raise"
+            raise AssertionError("exception failed to raise")
         except:
             pass
         else:
-            raise AssertionError, "exception executing else clause!"
+            raise AssertionError("exception executing else clause!")
 
-    def test_1arg(self):
+    def test_args(self):
         try:
-            raise SystemError, 1
-        except Exception, e:
-            assert e.args[0] == 1
-
-    def test_2args(self):
-        try:
-            raise SystemError, (1, 2)
-        except Exception, e:
-            assert e.args[0] == 1
-            assert e.args[1] == 2
-
-    def test_instancearg(self):
-        try:
-            raise SystemError, SystemError(1, 2)
-        except Exception, e:
-            assert e.args[0] == 1
-            assert e.args[1] == 2
-
-    def test_more_precise_instancearg(self):
-        try:
-            raise Exception, SystemError(1, 2)
-        except SystemError, e:
+            raise SystemError(1, 2)
+        except Exception as e:
             assert e.args[0] == 1
             assert e.args[1] == 2
 
     def test_builtin_exc(self):
         try:
             [][0]
-        except IndexError, e:
+        except IndexError as e:
             assert isinstance(e, IndexError)
 
     def test_raise_cls(self):
@@ -68,7 +48,7 @@ class AppTestRaise:
         except TypeError:
             pass
         else:
-            raise AssertionError, "shouldn't be able to raise 1"
+            raise AssertionError("shouldn't be able to raise 1")
 
     def test_raise_three_args(self):
         import sys
