1176d97cd455b3ebf8f78a59fa0034d49a375ab7
mattip <unknown>
2012-02-19 01:36:17 +0200

    clean up gratuitous 'print' in tests

--- pypy/module/micronumpy/test/test_numarray.py
+++ pypy/module/micronumpy/test/test_numarray.py
@@ -1375,8 +1375,6 @@ class AppTestMultiDim(BaseNumpyAppTest):
         a = array([[1, 2], [3, 4], [5, 6], [7, 8],
                    [9, 10], [11, 12], [13, 14]])
         b = a[::2]
-        print a
-        print b
         assert (b == [[1, 2], [5, 6], [9, 10], [13, 14]]).all()
         c = b + b
         assert c[1][1] == 12
