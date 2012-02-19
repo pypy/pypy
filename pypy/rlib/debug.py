6f9d7d34c38f2eb8977cacfa44cdbdee29e86cd7
Armin Rigo <arigo@tunes.org>
2012-02-19 20:39:21 +0100

    Kill this specialization.  It's mostly pointless and it gives occasionally headaches because fatalerror() is called from several levels.

--- pypy/rlib/debug.py
+++ pypy/rlib/debug.py
@@ -19,14 +19,19 @@ class Entry(ExtRegistryEntry):
         hop.exception_cannot_occur()
         hop.genop('debug_assert', vlist)
 
-def fatalerror(msg, traceback=False):
+def fatalerror(msg):
     from pypy.rpython.lltypesystem import lltype
     from pypy.rpython.lltypesystem.lloperation import llop
-    if traceback:
-        llop.debug_print_traceback(lltype.Void)
+    llop.debug_print_traceback(lltype.Void)
     llop.debug_fatalerror(lltype.Void, msg)
 fatalerror._dont_inline_ = True
-fatalerror._annspecialcase_ = 'specialize:arg(1)'
+
+def fatalerror_notb(msg):
+    # a variant of fatalerror() that doesn't print the RPython traceback
+    from pypy.rpython.lltypesystem import lltype
+    from pypy.rpython.lltypesystem.lloperation import llop
+    llop.debug_fatalerror(lltype.Void, msg)
+fatalerror_notb._dont_inline_ = True
 
 
 class DebugLog(list):
