39791e393646e6453b77bb7dc7462edce222fac5
Armin Rigo <arigo@tunes.org>
2012-02-19 18:50:39 +0100

    Kill an outdated test.

--- pypy/translator/stm/test/test_ztranslated.py
+++ pypy/translator/stm/test/test_ztranslated.py
@@ -10,15 +10,3 @@ class TestSTMTranslated(CompiledSTMTests):
         data = cbuilder.cmdexec('4 5000')
         assert 'done sleeping.' in data
         assert 'check ok!' in data
-
-
-class TestSTMFramework(CompiledSTMTests):
-    gc = "minimark"
-
-    def test_hello_world(self):
-        py.test.skip("in-progress")
-        t, cbuilder = self.compile(targetdemo.entry_point)
-        data = cbuilder.cmdexec('4 5000 1')
-        # ^^^ should check that it doesn't take 1G of RAM
-        assert 'done sleeping.' in data
-        assert 'check ok!' in data
