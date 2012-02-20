28d88b622d0c2b479b0e5d62d850e8adb553f939
Antonio Cuni <anto.cuni@gmail.com>
2012-02-21 00:03:00 +0100

    kill this test, we no longer have the three-args raise form

--- pypy/interpreter/test/test_pyframe.py
+++ pypy/interpreter/test/test_pyframe.py
@@ -350,34 +350,6 @@ class AppTestPyFrame:
         assert len(l) == 2
         assert issubclass(l[0][0], Exception)
         assert issubclass(l[1][0], Exception)
-
-    def test_trace_raise_three_arg(self):
-        import sys
-        l = []
-        def trace(frame, event, arg):
-            if event == 'exception':
-                l.append(arg)
-            return trace
-
-        def g():
-            try:
-                raise Exception
-            except Exception, e:
-                import sys
-                raise Exception, e, sys.exc_info()[2]
-
-        def f():
-            try:
-                g()
-            except:
-                pass
-
-        sys.settrace(trace)
-        f()
-        sys.settrace(None)
-        assert len(l) == 2
-        assert issubclass(l[0][0], Exception)
-        assert issubclass(l[1][0], Exception)
         
 
     def test_trace_generator_finalisation(self):
