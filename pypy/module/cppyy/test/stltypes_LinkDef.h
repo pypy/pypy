f3cffe720699157dcb5a0843ddf55929b0c52398
Wim Lavrijsen <WLavrijsen@lbl.gov>
2012-02-22 13:40:13 -0800

    required link definitions for CINT backend

---
+++ pypy/module/cppyy/test/stltypes_LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class std::vector<int>;
+#pragma link C++ class std::vector<just_a_class>;
+
+#pragma link C++ class just_a_class;
+#pragma link C++ class stringy_class;
+
+#endif
