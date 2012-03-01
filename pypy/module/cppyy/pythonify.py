5dbec55fd77c63685b9c414f33053487c72a7b60
Wim Lavrijsen <WLavrijsen@lbl.gov>
2012-03-01 00:02:31 -0800

    fix typo

--- pypy/module/cppyy/pythonify.py
+++ pypy/module/cppyy/pythonify.py
@@ -255,7 +255,7 @@ def get_cppitem(name, scope=None):
     if pycppitem:
         return pycppitem
 
-    raise AttributeError("'%s' has no attribute '%s'", (str(scope), name))
+    raise AttributeError("'%s' has no attribute '%s'" % (str(scope), name))
 
 get_cppclass = get_cppitem         # TODO: restrict to classes only (?)
 
