e7fc12da352708098943d8680f39523dcaa86734
Antonio Cuni <anto.cuni@gmail.com>
2012-03-02 10:30:30 +0100

    fix test_bisect: None is no longer comparable to ints, and map no longer returns a list

--- pypy/module/_bisect/test/test_bisect.py
+++ pypy/module/_bisect/test/test_bisect.py
@@ -9,7 +9,6 @@ class AppTestBisect:
     def test_bisect_left(self):
         from _bisect import bisect_left
         a = [0, 5, 6, 6, 6, 7]
-        assert bisect_left(a, None) == 0
         assert bisect_left(a, -3) == 0
         assert bisect_left(a, 0) == 0
         assert bisect_left(a, 3) == 1
@@ -47,7 +46,6 @@ class AppTestBisect:
     def test_bisect_right(self):
         from _bisect import bisect_right
         a = [0, 5, 6, 6, 6, 7]
-        assert bisect_right(a, None) == 0
         assert bisect_right(a, -3) == 0
         assert bisect_right(a, 0) == 1
         assert bisect_right(a, 3) == 1
@@ -85,11 +83,11 @@ class AppTestBisect:
         a = [0, 5, 6, 6, 6, 7]
         insort_left(a, 6.0)
         assert a == [0, 5, 6.0, 6, 6, 6, 7]
-        assert map(type, a) == [int, int, float, int, int, int, int]
+        assert list(map(type, a)) == [int, int, float, int, int, int, int]
 
     def test_insort_right(self):
         from _bisect import insort_right
         a = [0, 5, 6, 6, 6, 7]
         insort_right(a, 6.0)
         assert a == [0, 5, 6, 6, 6, 6.0, 7]
-        assert map(type, a) == [int, int, int, int, int, float, int]
+        assert list(map(type, a)) == [int, int, int, int, int, float, int]
