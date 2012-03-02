477408acd2d57145cd64d4890ad519fa2f2886b7
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-03-02 17:04:52 +0100

    testrunner: only do the monkeypatching in scratchbox if its required

--- testrunner/scratchbox_runner.py
+++ testrunner/scratchbox_runner.py
@@ -14,14 +14,14 @@ def run_scratchbox(args, cwd, out, timeout=None):
 def dry_run_scratchbox(args, cwd, out, timeout=None):
     return dry_run(args_for_scratchbox(cwd, args), cwd, out, timeout)
 
-import runner
-# XXX hack hack hack
-dry_run = runner.dry_run
-run = runner.run
-
-runner.dry_run = dry_run_scratchbox
-runner.run = run_scratchbox
-
 if __name__ == '__main__':
+    import runner
+    # XXX hack hack hack
+    dry_run = runner.dry_run
+    run = runner.run
+
+    runner.dry_run = dry_run_scratchbox
+    runner.run = run_scratchbox
+
     import sys
     runner.main(sys.argv)
