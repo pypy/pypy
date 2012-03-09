035f963b1bd510d7c03e8f8df168998ae02ee918
Armin Rigo <arigo@tunes.org>
2012-03-09 11:52:15 -0800

    Found out again why the source I copied this from pushes and pops %ebx instead of listing it in the clobbered registers: on some machines (OS/X 32 for example) gcc complains that %ebx may not be clobbered.

--- pypy/translator/c/src/asm_gcc_x86.h
+++ pypy/translator/c/src/asm_gcc_x86.h
@@ -124,10 +124,12 @@ void pypy_x86_check_sse2(void)
 {
     //Read the CPU features.
     int features;
-    asm("mov $1, %%eax\n"
+    asm("movl $1, %%eax\n"
+        "pushl %%ebx\n"
         "cpuid\n"
-        "mov %%edx, %0"
-        : "=g"(features) : : "eax", "ebx", "edx", "ecx");
+        "popl %%ebx\n"
+        "movl %%edx, %0"
+        : "=g"(features) : : "eax", "edx", "ecx");
     
     //Check bits 25 and 26, this indicates SSE2 support
     if (((features & (1 << 25)) == 0) || ((features & (1 << 26)) == 0))
