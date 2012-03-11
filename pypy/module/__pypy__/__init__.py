241242e208c253106909c45c9bfab4006329202b
Alex Gaynor <alex.gaynor@gmail.com>
2012-03-11 11:56:45 -0700

    Backport time.clock_gettime from python 3.3 to __pypy__

--- pypy/module/__pypy__/__init__.py
+++ pypy/module/__pypy__/__init__.py
@@ -2,6 +2,7 @@
 # Package initialisation
 from pypy.interpreter.mixedmodule import MixedModule
 from pypy.module.imp.importing import get_pyc_magic
+from pypy.module.__pypy__ import interp_time
 
 
 class BuildersModule(MixedModule):
@@ -11,6 +12,19 @@ class BuildersModule(MixedModule):
         "StringBuilder": "interp_builders.W_StringBuilder",
         "UnicodeBuilder": "interp_builders.W_UnicodeBuilder",
     }
+
+class TimeModule(MixedModule):
+    appleveldefs = {}
+    interpleveldefs = {}
+    if interp_time.HAS_CLOCK_GETTIME:
+        interpleveldefs["clock_gettime"] = "interp_time.clock_gettime"
+        interpleveldefs["clock_getres"] = "interp_time.clock_getres"
+        for name in [
+            "CLOCK_REALTIME", "CLOCK_MONOTONIC", "CLOCK_MONOTONIC_RAW",
+            "CLOCK_PROCESS_CPUTIME_ID", "CLOCK_THREAD_CPUTIME_ID"
+        ]:
+            interpleveldefs[name] = "space.wrap(interp_time.%s)" % name
+
 
 class Module(MixedModule):
     appleveldefs = {
@@ -32,6 +46,7 @@ class Module(MixedModule):
 
     submodules = {
         "builders": BuildersModule,
+        "time": TimeModule,
     }
 
     def setup_after_space_initialization(self):
