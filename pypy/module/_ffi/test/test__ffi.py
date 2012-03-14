2eae47f3b9ce6335a057e228411e047d8c972e4e
Christian Tismer <tismer@stackless.com>
2012-03-13 22:48:48 -0700

    Quite a lot of trivial (hmm, not really) changes merged in from win64. Hopefully I did not break too much. Anyway, it still builds.

--- pypy/module/_ffi/test/test__ffi.py
+++ pypy/module/_ffi/test/test__ffi.py
@@ -4,6 +4,8 @@ from pypy.translator.tool.cbuild import ExternalCompilationInfo
 from pypy.module._rawffi.interp_rawffi import TYPEMAP
 from pypy.module._rawffi.tracker import Tracker
 from pypy.translator.platform import platform
+from pypy.rlib.rarithmetic import is_valid_int
+
 
 import os, sys, py
 
@@ -185,7 +187,7 @@ class AppTestFfi:
                                         types.void)
         assert get_dummy() == 0
         ptr = get_dummy_ptr()
-        assert type(ptr) in (int, long)
+        assert is_valid_int(ptr)
         ptr2 = MyPointerWrapper(ptr)
         set_val_to_ptr(ptr2, 123)
         assert get_dummy() == 123
