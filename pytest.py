e32ef14dc5288bae23e15a36c3ac175af9707f5f
Christian Tismer <tismer@stackless.com>
2012-03-18 02:21:13 +0100

    win64 stage 1 is completely synchronized with default. Starting stage 2 soon.

--- pytest.py
+++ pytest.py
@@ -5,12 +5,18 @@ unit and functional testing with Python.
 __all__ = ['main']
 
 # XXX hack for win64:
-# This patch must stay here until the ENDS OF STAGE 1
+# This patch must stay here until the END OF STAGE 1
 # When all tests work, this branch will be merged
 # and the branch stage 2 is started, where we remove this patch.
 import sys
 if hasattr(sys, "maxsize"):
-    sys.maxint = max(sys.maxint, sys.maxsize)
+    if sys.maxint <> sys.maxsize:
+        sys.maxint = sys.maxsize
+        import warnings
+        warnings.warn("""\n
+---> This win64 port is now in stage 1: sys.maxint was modified.
+---> When pypy/__init__.py becomes empty again, we have reached stage 2.
+""")
 
 from _pytest.core import main, UsageError, _preloadplugins
 from _pytest import core as cmdline
