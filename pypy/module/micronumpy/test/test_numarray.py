19276ad799f6d4159aa6ee6a9fd2cc6c347171a0
Mike Blume <mike@loggly.com>
2012-03-19 23:41:36 -0700

    add failing test of ndmin parameter of numpy.array

--- pypy/module/micronumpy/test/test_numarray.py
+++ pypy/module/micronumpy/test/test_numarray.py
@@ -211,6 +211,12 @@ class AppTestNumArray(BaseNumpyAppTest):
         assert a.shape == (3,)
         assert a.dtype is dtype(int)
 
+    def test_ndmin(self):
+        from _numpypy import array
+
+        arr = array([1], ndmin=3)
+        assert arr.shape == (1, 1, 1)
+
     def test_type(self):
         from _numpypy import array
         ar = array(range(5))
