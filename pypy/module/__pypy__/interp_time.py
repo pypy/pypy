623d6f82ce27e2a9cb8cf8308ff3fca419560950
Matti Picus <matti.picus@gmail.com>
2012-04-06 10:00:59 +0300

    fix for clock_gettime

--- pypy/module/__pypy__/interp_time.py
+++ pypy/module/__pypy__/interp_time.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-import sys
+import os
 
 from pypy.interpreter.error import exception_from_errno
 from pypy.interpreter.gateway import unwrap_spec
@@ -7,11 +7,15 @@ from pypy.rpython.lltypesystem import rffi, lltype
 from pypy.rpython.tool import rffi_platform
 from pypy.translator.tool.cbuild import ExternalCompilationInfo
 
+if os.name == 'nt':
+    libraries = []
+else:
+    libraries = ["rt"]
 
 class CConfig:
     _compilation_info_ = ExternalCompilationInfo(
         includes=["time.h"],
-        libraries=["rt"],
+        libraries=libraries,
     )
 
     HAS_CLOCK_GETTIME = rffi_platform.Has('clock_gettime')
@@ -21,11 +25,6 @@ class CConfig:
     CLOCK_MONOTONIC_RAW = rffi_platform.DefinedConstantInteger("CLOCK_MONOTONIC_RAW")
     CLOCK_PROCESS_CPUTIME_ID = rffi_platform.DefinedConstantInteger("CLOCK_PROCESS_CPUTIME_ID")
     CLOCK_THREAD_CPUTIME_ID = rffi_platform.DefinedConstantInteger("CLOCK_THREAD_CPUTIME_ID")
-
-    TIMESPEC = rffi_platform.Struct("struct timespec", [
-        ("tv_sec", rffi.TIME_T),
-        ("tv_nsec", rffi.LONG),
-    ])
 
 cconfig = rffi_platform.configure(CConfig)
 
@@ -37,29 +36,33 @@ CLOCK_MONOTONIC_RAW = cconfig["CLOCK_MONOTONIC_RAW"]
 CLOCK_PROCESS_CPUTIME_ID = cconfig["CLOCK_PROCESS_CPUTIME_ID"]
 CLOCK_THREAD_CPUTIME_ID = cconfig["CLOCK_THREAD_CPUTIME_ID"]
 
-TIMESPEC = cconfig["TIMESPEC"]
+if HAS_CLOCK_GETTIME:
+    TIMESPEC = lltype.Struct("struct timespec", 
+        ("tv_sec", rffi.TIME_T),
+        ("tv_nsec", rffi.LONG),
+    )
 
-c_clock_gettime = rffi.llexternal("clock_gettime",
-    [lltype.Signed, lltype.Ptr(TIMESPEC)], rffi.INT,
-    compilation_info=CConfig._compilation_info_, threadsafe=False
-)
-c_clock_getres = rffi.llexternal("clock_getres",
-    [lltype.Signed, lltype.Ptr(TIMESPEC)], rffi.INT,
-    compilation_info=CConfig._compilation_info_, threadsafe=False
-)
+    c_clock_gettime = rffi.llexternal("clock_gettime",
+        [lltype.Signed, lltype.Ptr(TIMESPEC)], rffi.INT,
+        compilation_info=CConfig._compilation_info_, threadsafe=False
+    )
+    c_clock_getres = rffi.llexternal("clock_getres",
+        [lltype.Signed, lltype.Ptr(TIMESPEC)], rffi.INT,
+        compilation_info=CConfig._compilation_info_, threadsafe=False
+    )
 
-@unwrap_spec(clk_id="c_int")
-def clock_gettime(space, clk_id):
-    with lltype.scoped_alloc(TIMESPEC) as tp:
-        ret = c_clock_gettime(clk_id, tp)
-        if ret != 0:
-            raise exception_from_errno(space, space.w_IOError)
-        return space.wrap(tp.c_tv_sec + tp.c_tv_nsec * 1e-9)
+    @unwrap_spec(clk_id="c_int")
+    def clock_gettime(space, clk_id):
+        with lltype.scoped_alloc(TIMESPEC) as tp:
+            ret = c_clock_gettime(clk_id, tp)
+            if ret != 0:
+                raise exception_from_errno(space, space.w_IOError)
+            return space.wrap(tp.c_tv_sec + tp.c_tv_nsec * 1e-9)
 
-@unwrap_spec(clk_id="c_int")
-def clock_getres(space, clk_id):
-    with lltype.scoped_alloc(TIMESPEC) as tp:
-        ret = c_clock_getres(clk_id, tp)
-        if ret != 0:
-            raise exception_from_errno(space, space.w_IOError)
-        return space.wrap(tp.c_tv_sec + tp.c_tv_nsec * 1e-9)
+    @unwrap_spec(clk_id="c_int")
+    def clock_getres(space, clk_id):
+        with lltype.scoped_alloc(TIMESPEC) as tp:
+            ret = c_clock_getres(clk_id, tp)
+            if ret != 0:
+                raise exception_from_errno(space, space.w_IOError)
+            return space.wrap(tp.c_tv_sec + tp.c_tv_nsec * 1e-9)
