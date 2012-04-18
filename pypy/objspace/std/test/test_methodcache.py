9e89aadfd5de440e2d61a07a70ccc1fab0c7892f
Antonio Cuni <anto.cuni@gmail.com>
2012-04-18 20:36:10 +0200

    add a comment explaining why this test fails

--- pypy/objspace/std/test/test_methodcache.py
+++ pypy/objspace/std/test/test_methodcache.py
@@ -53,6 +53,13 @@ class AppTestMethodCaching(AppTestTypeObject):
         assert sum(cache_counter) == 20
  
     def test_change_methods(self):
+        # this test fails because of the following line in typeobject.py:427
+        #             if cached_name is name:
+        
+        # in py3k, identifiers are stored in W_UnicodeObject and unwrapped by
+        # calling space.str_w, which .encode('ascii') the string, thus
+        # creating new strings all the time. The problem should be solved when
+        # we implement proper unicode identifiers in py3k
         import __pypy__
         class A(object):
             def f(self):
