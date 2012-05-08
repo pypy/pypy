44bf1f2fae804432b2d21c62e0ac8946397aecc8
Hakan Ardo <hakan@debian.org>
2012-05-08 19:44:14 +0200

    Failing test. With OptRewrite disabled, the resulting trace might contain guard_false(0) which makes the x86 backend crash as there is no TEST_ii (This is starting to get ridiculous: I set out do make a one-line bugfix and 15 commits and 2651 tests later things are still not working properly).

--- pypy/jit/backend/x86/test/test_loop_unroll.py
+++ pypy/jit/backend/x86/test/test_loop_unroll.py
@@ -6,3 +6,10 @@ class TestLoopSpec(Jit386Mixin, test_loop_unroll.LoopUnrollTest):
     # for the individual tests see
     # ====> ../../../metainterp/test/test_loop.py
     pass
+
+class TestLoopNoRewrite(test_loop_unroll.LoopUnrollTest, Jit386Mixin):
+    enable_opts = 'intbounds:virtualize:string:earlyforce:pure:heap:ffi:unroll'
+    def check_resops(self, *args, **kwargs):
+        pass
+    def check_trace_count(self, count):
+        pass
