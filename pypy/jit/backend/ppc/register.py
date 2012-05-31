e3c8381484e5a52368ed98e4ac69ca181e7fcfe0
edelsohn <unknown>
2012-05-31 11:24:18 -0400

    First FP argument in r.f1.

--- pypy/jit/backend/ppc/register.py
+++ pypy/jit/backend/ppc/register.py
@@ -33,7 +33,7 @@ MANAGED_REGS = [r3, r4, r5, r6, r7, r8, r9, r10,
                 r27, r28, r29, r30]
 
 PARAM_REGS = [r3, r4, r5, r6, r7, r8, r9, r10]
-PARAM_FPREGS = [f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13]
+PARAM_FPREGS = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13]
 
 def get_managed_reg_index(reg):
     if reg > r13.value:
