423a14be6199b446176d8678c00968ce6e1b7d61
edelsohn <unknown>
2012-06-01 14:37:06 -0400

    Define MANAGED_FP_REGS.

--- pypy/jit/backend/ppc/register.py
+++ pypy/jit/backend/ppc/register.py
@@ -32,6 +32,8 @@ MANAGED_REGS = [r3, r4, r5, r6, r7, r8, r9, r10,
                 r19, r20, r21, r22, r23, r24, r25, r26,
                 r27, r28, r29, r30]
 
+MANAGED_FP_REGS = VOLATILES_FLOAT + NONVOLATILES_FLOAT
+
 PARAM_REGS = [r3, r4, r5, r6, r7, r8, r9, r10]
 PARAM_FPREGS = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13]
 
