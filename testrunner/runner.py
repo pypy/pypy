d6bf54755408896672f24509ff2d165dc5d9c312
David Schneider <david.schneider@picle.org>
2012-07-05 14:24:53 +0200

    do not consider the file pypy/test_all.py as a test_xxx.py test file when collecting directories

--- testrunner/runner.py
+++ testrunner/runner.py
@@ -302,7 +302,11 @@ class RunParam(object):
 
     def is_test_py_file(self, p):
         name = p.basename
-        return name.startswith('test_') and name.endswith('.py')
+        # XXX avoid picking up pypy/test_all.py as a test test_xxx.py file else
+        # the pypy directory is not traversed and picked up as one test
+        # directory
+        return (self.reltoroot(p) != 'pypy/test_all.py'
+                    and (name.startswith('test_') and name.endswith('.py')))
 
     def reltoroot(self, p):
         rel = p.relto(self.root)
