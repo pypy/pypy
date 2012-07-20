45fcf0c2b244044d9e97a0732cdff9fa3ca463bf
Maciej Fijalkowski <fijall@gmail.com>
2012-07-20 10:39:43 +0200

    backout 3c234f36e008

--- pypy/module/micronumpy/interp_numarray.py
+++ pypy/module/micronumpy/interp_numarray.py
@@ -1271,9 +1271,7 @@ def array(space, w_item_or_iterable, w_dtype=None, w_order=None,
     if w_ndmin is not None and not space.is_w(w_ndmin, space.w_None):
         ndmin = space.int_w(w_ndmin)
         if ndmin > shapelen:
-            lgt = (ndmin - shapelen) + shape
-            assert lgt >= 0
-            shape = [1] * lgt
+            shape = [1] * (ndmin - shapelen) + shape
             shapelen = ndmin
     arr = W_NDimArray(shape[:], dtype=dtype, order=order)
     arr_iter = arr.create_iter()
