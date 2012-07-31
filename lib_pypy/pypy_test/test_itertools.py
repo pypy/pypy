3dda4e0bb5b173e437b35ceba04be4defc2fcf2e
Chirag Jadwani <chirag.jadwani@gmail.com>
2012-08-01 01:43:36 +0530

    Add lib_pypy.itertools tests, fix check_number in interp_itertools

--- lib_pypy/pypy_test/test_itertools.py
+++ lib_pypy/pypy_test/test_itertools.py
@@ -48,3 +48,20 @@ class TestItertools(object):
         l = [1, 2]
         m = ['a']
         raises(TypeError, itertools.product, l, m, repeat=1, foo=2)
+
+    def test_tee_copy_constructor(self):
+        a, b = itertools.tee(range(10))
+        next(a)
+        next(a)
+        c, d = itertools.tee(a)
+        assert list(a) == list(d)
+
+    def test_product_kwargs(self):
+        raises(TypeError, itertools.product, range(10), garbage=1)
+
+    def test_takewhile_stops(self):
+        tw = itertools.takewhile(lambda x: bool(x), [1, 1, 0, 1, 1])
+        next(tw)
+        next(tw)
+        raises(StopIteration, next, tw)
+        raises(StopIteration, next, tw)
