3afc265038079fb29407575d4ec1f9c4f863c5a1
David Schneider <david.schneider@picle.org>
2012-08-30 13:32:25 +0000

    remove longlong special case for ARM hardfloat

--- pypy/jit/codewriter/longlong.py
+++ pypy/jit/codewriter/longlong.py
@@ -9,7 +9,6 @@ converting them back and forth.
 import sys
 from pypy.rpython.lltypesystem import lltype, rffi
 from pypy.rlib import rarithmetic, longlong2float
-from pypy.jit.backend.arm.detect import detect_hardfloat
 from pypy.rlib.objectmodel import compute_hash
 
 
@@ -29,22 +28,6 @@ if sys.maxint > 2147483647:
     is_longlong     = lambda TYPE: False
 
     # -------------------------------------
-elif detect_hardfloat():
-    # ---------- ARM 32-bit platform ----------
-    # the type FloatStorage is float
-
-    is_64_bit = False
-    supports_longlong = False
-    r_float_storage = float
-    FLOATSTORAGE = lltype.Float
-
-    getfloatstorage = lambda x: x
-    getrealfloat    = lambda x: x
-    gethash         = compute_hash
-    is_longlong     = lambda TYPE: False
-
-    # -------------------------------------
-
 else:
     # ---------- 32-bit platform ----------
     # the type FloatStorage is r_longlong, and conversion is needed
