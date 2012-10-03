1d7ff92c758dcf82635a284c38bf425fe936fffa
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2012-10-03 10:33:46 +0200

    Fix a crash in some module/thread test.

--- pypy/translator/c/src/thread.c
+++ pypy/translator/c/src/thread.c
@@ -1,6 +1,12 @@
 /* Thread implementation */
 #include "src/thread.h"
 
+/* The following include is required by the Boehm GC, which apparently
+ * crashes where pthread_create() is not redefined to call a Boehm
+ * wrapper function instead.  Ugly.
+ */
+#include "common_header.h"
+
 #ifdef _WIN32
 #include "src/thread_nt.c"
 #else
