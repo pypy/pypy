998d6d34d65b6541a83c311e2e0e639f36631a8e
Philip Jenvey <pjenvey@underboss.org>
2012-10-07 12:05:02 -0700

    workaround (potentially) for parsing extend_from_iterable's trace

--- pypy/module/pypyjit/test_pypy_c/model.py
+++ pypy/module/pypyjit/test_pypy_c/model.py
@@ -104,6 +104,9 @@ class TraceWithIds(Function):
                     yield subchunk
 
     def compute_ids(self, ids):
+        # XXX: valid?
+        if not self.code:
+            return
         #
         # 1. compute the ids of self, i.e. the outer function
         id2opcodes = find_ids(self.code)
