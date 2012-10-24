33d9ff210cfcd2de1ff388e964c1a461cfbd7008
Philip Jenvey <pjenvey@underboss.org>
2012-10-24 13:56:16 -0700

    kill refs to the exceptions module

--- lib_pypy/pypy_test/test_exception_extra.py
+++ lib_pypy/pypy_test/test_exception_extra.py
@@ -1,15 +1,10 @@
 def app_test_environmenterror_repr():
-    import exceptions as ex 
-    e = ex.EnvironmentError("hello")
+    e = EnvironmentError("hello")
     assert str(e) == "hello"
-    e = ex.EnvironmentError(1, "hello")
+    e = EnvironmentError(1, "hello")
     assert str(e) == "[Errno 1] hello"
-    e = ex.EnvironmentError(1, "hello", "world")
+    e = EnvironmentError(1, "hello", "world")
     assert str(e) == "[Errno 1] hello: 'world'"
-
-def app_test_import():
-    import exceptions
-    assert exceptions.SyntaxError is SyntaxError 
 
 def app_test_baseexception():
     assert issubclass(Exception, BaseException)
