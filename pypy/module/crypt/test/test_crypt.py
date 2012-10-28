4eca2af7245889874defbbfdeee03f535a8fff4f
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2012-10-28 23:17:06 +0100

    progress.

--- pypy/module/crypt/test/test_crypt.py
+++ pypy/module/crypt/test/test_crypt.py
@@ -1,9 +1,7 @@
-from pypy.conftest import gettestobjspace
-
 class AppTestCrypt: 
-    def setup_class(cls):
-        cls.space = gettestobjspace(usemodules=['crypt'])
-    def test_crypt(self):
+    spaceconfig = dict(usemodules=['crypt'])
+ 
+   def test_crypt(self):
         import crypt 
         res = crypt.crypt("pass", "ab")
         assert isinstance(res, str)
