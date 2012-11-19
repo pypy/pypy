97d5d75455f3d6c76237163ba8a77a286492f52a
Armin Rigo <arigo@tunes.org>
2012-11-19 10:07:22 +0100

    Backed out changeset 915fc220d488: it should already be skipped by d4ef7d213222.

--- pypy/rlib/parsing/test/test_tree.py
+++ pypy/rlib/parsing/test/test_tree.py
@@ -90,7 +90,6 @@ class TestTreeTranslatedLLType(BaseTestTreeTranslated):
 class TestTreeTranslatedOOType(BaseTestTreeTranslated):
     
     def compile(self, f):
-        py.test.skip("XXX fix me maybe")
         from pypy.translator.cli.test.runtest import compile_function
         return compile_function(f, [], auto_raise_exc=True, exctrans=True)
 
