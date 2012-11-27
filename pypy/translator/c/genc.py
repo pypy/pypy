24a11a681f7a459590a9677b41c470004455cb60
Manuel Jacob <unknown>
2012-11-27 16:43:46 +0100

    Compile stack.c/threadlocal.c only when needed.

--- pypy/translator/c/genc.py
+++ pypy/translator/c/genc.py
@@ -765,8 +765,6 @@ def add_extra_files(eci):
         srcdir / 'profiling.c',
         srcdir / 'debug_print.c',
         srcdir / 'debug_traceback.c',  # ifdef HAVE_RTYPER
-        srcdir / 'stack.c',
-        srcdir / 'threadlocal.c',
         srcdir / 'asm.c',
         srcdir / 'instrument.c',
         srcdir / 'int.c',
