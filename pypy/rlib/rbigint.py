ecb4d3784e3cc1b296561ff51392b830519e2e64
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2012-12-02 00:31:09 +0100

    Backport frombytes() and tobytes() from the py3k branch.

--- pypy/rlib/rbigint.py
+++ pypy/rlib/rbigint.py
@@ -2,6 +2,7 @@ from pypy.rlib.rarithmetic import LONG_BIT, intmask, longlongmask, r_uint, r_ulo
 from pypy.rlib.rarithmetic import ovfcheck, r_longlong, widen, is_valid_int
 from pypy.rlib.rarithmetic import most_neg_value_of_same_type
 from pypy.rlib.rfloat import isinf, isnan
+from pypy.rlib.rstring import StringBuilder
 from pypy.rlib.debug import make_sure_not_resized, check_regular_int
 from pypy.rlib.objectmodel import we_are_translated, specialize
 from pypy.rlib import jit
@@ -11,6 +12,7 @@ from pypy.rpython import extregistry
 import math, sys
 
 SUPPORT_INT128 = hasattr(rffi, '__INT128_T')
+BYTEORDER = sys.byteorder
 
 # note about digit sizes:
 # In division, the native integer type must be able to hold
@@ -94,6 +96,12 @@ def _check_digits(l):
         assert type(x) is type(NULLDIGIT)
         assert UDIGIT_MASK(x) & MASK == UDIGIT_MASK(x)
             
+class InvalidEndiannessError(Exception):
+    pass
+
+class InvalidSignednessError(Exception):
+    pass
+
 class Entry(extregistry.ExtRegistryEntry):
     _about_ = _check_digits
     def compute_result_annotation(self, s_list):
@@ -260,6 +268,117 @@ class rbigint(object):
         # This function is marked as pure, so you must not call it and
         # then modify the result.
         return _decimalstr_to_bigint(s)
+
+    @staticmethod
+    def frombytes(s, byteorder, signed):
+        if byteorder not in ('big', 'little'):
+            raise InvalidEndiannessError()
+
+        if byteorder != BYTEORDER:
+            msb = ord(s[0])
+            itr = range(len(s)-1, -1, -1)
+        else:
+            msb = ord(s[-1])
+            itr = range(0, len(s))
+
+        sign = -1 if msb >= 0x80 and signed else 1
+        accum = _widen_digit(0)
+        accumbits = 0
+        digits = []
+        carry = 1
+
+        for i in itr:
+            c = _widen_digit(ord(s[i]))
+            if sign == -1:
+                c = (0xFF ^ c) + carry
+                carry = c >> 8
+                c &= 0xFF
+
+            accum |= c << accumbits
+            accumbits += 8
+            if accumbits >= SHIFT:
+                digits.append(_store_digit(intmask(accum & MASK)))
+                accum >>= SHIFT
+                accumbits -= SHIFT
+
+        if accumbits:
+            digits.append(_store_digit(intmask(accum)))
+        result = rbigint(digits[:], sign)
+        result._normalize()
+        return result
+
+    @jit.elidable
+    def tobytes(self, nbytes, byteorder, signed):
+        if byteorder not in ('big', 'little'):
+            raise InvalidEndiannessError()
+        if not signed and self.sign == -1:
+            raise InvalidSignednessError()
+
+        bswap = byteorder != BYTEORDER
+        d = _widen_digit(0)
+        j = 0
+        imax = self.numdigits()
+        accum = _widen_digit(0)
+        accumbits = 0
+        result = StringBuilder(nbytes)
+        carry = 1
+
+        for i in range(0, imax):
+            d = self.widedigit(i)
+            if self.sign == -1:
+                d = (d ^ MASK) + carry
+                carry = d >> SHIFT
+                d &= MASK
+
+            accum |= d << accumbits
+            if i == imax - 1:
+                # Avoid bogus 0's
+                s = d ^ MASK if self.sign == -1 else d
+                while s:
+                    s >>=1
+                    accumbits += 1
+            else:
+                accumbits += SHIFT
+
+            while accumbits >= 8:
+                if j >= nbytes:
+                    raise OverflowError()
+                j += 1
+
+                result.append(chr(accum & 0xFF))
+                accum >>= 8
+                accumbits -= 8
+
+        if accumbits:
+            if j >= nbytes:
+                raise OverflowError()
+            j += 1
+
+            if self.sign == -1:
+                # Add a sign bit
+                accum |= (~_widen_digit(0)) << accumbits;
+
+            result.append(chr(accum & 0xFF))
+
+        if j < nbytes:
+            signbyte = 0xFF if self.sign == -1 else 0
+            result.append_multiple_char(chr(signbyte), nbytes - j)
+
+        digits = result.build()
+
+        if j == nbytes and nbytes > 0 and signed:
+            # If not already set, we cannot contain the sign bit
+            msb = digits[-1]
+            if (self.sign == -1) != (ord(msb) >= 0x80):
+                raise OverflowError()
+
+        if bswap:
+            # Bah, this is very inefficient. At least it's not
+            # quadratic.
+            length = len(digits)
+            if length >= 0:
+                digits = ''.join([digits[i] for i in range(length-1, -1, -1)])
+        return digits
 
     @jit.elidable
     def toint(self):
