fcf5ba701881e9a1adccfafd1023de991484bc2e
Armin Rigo <arigo@tunes.org>
2012-12-02 14:11:24 -0800

    Fix

--- pypy/jit/metainterp/resume.py
+++ pypy/jit/metainterp/resume.py
@@ -706,9 +706,9 @@ class AbstractResumeDataReader(object):
         # allocate() methods must fill in the cache as soon as they
         # have the object, before they fill its fields.
         assert self.virtuals_cache is not None
-        assert self.rd_virtuals is not None
         v = self.virtuals_cache[index]
         if not v:
+            assert self.rd_virtuals is not None
             v = self.rd_virtuals[index].allocate(self, index)
             ll_assert(v == self.virtuals_cache[index], "resume.py: bad cache")
         return v
