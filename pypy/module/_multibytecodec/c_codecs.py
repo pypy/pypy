fbec2e75a1ab3d461c0fb258b3acd060ee505cfd
Philip Jenvey <pjenvey@underboss.org>
2012-12-03 15:26:56 -0800

    really fix translation

--- pypy/module/_multibytecodec/c_codecs.py
+++ pypy/module/_multibytecodec/c_codecs.py
@@ -285,7 +285,7 @@ def multibytecodec_encerror(encodebuf, e, errors,
             # py3k only
             replace = rets
         else:
-            assert encodebuf is not None
+            assert retu is not None
             codec = pypy_cjk_enc_getcodec(encodebuf)
             replace = encode(codec, retu, "strict", errorcb, namecb)
     inbuf = rffi.get_nonmovingbuffer(replace)
