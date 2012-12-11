204770aaf6244121a95223121a2503f42079a019
Armin Rigo <arigo@tunes.org>
2012-12-11 15:53:54 +0100

    Use 'try' here, which is more compatible (but may hide for now the same issue that we had in strtod()).

--- pypy/rpython/lltypesystem/rffi.py
+++ pypy/rpython/lltypesystem/rffi.py
@@ -714,7 +714,7 @@ def make_string_mappings(strtype):
             i -= 1
         return array
     str2charp._annenforceargs_ = [strtype]
-    str2charp._always_inline_ = True
+    str2charp._always_inline_ = 'try'
 
     def free_charp(cp):
         lltype.free(cp, flavor='raw')
