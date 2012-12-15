b8ee493ba442838776a5750a6c21065aaba3d3a9
Stian Andreassen <unknown>
2012-12-15 19:56:41 +0100

    Backout 3b95bf4a907e. No significans

--- pypy/translator/platform/linux.py
+++ pypy/translator/platform/linux.py
@@ -14,7 +14,6 @@ class BaseLinux(BasePosix):
     extra_libs = ('-lrt',)
     cflags = tuple(
              ['-O3', '-pthread', '-fomit-frame-pointer',
-              '-fgcse-sm', '-fgcse-las', '-fmodulo-sched', '-fmodulo-sched-allow-regmoves', '-fmerge-all-constants',
               '-Wall', '-Wno-unused']
              + os.environ.get('CFLAGS', '').split())
     standalone_only = ()
