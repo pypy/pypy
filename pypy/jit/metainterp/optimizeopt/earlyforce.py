f7ca6856575e44ef984b69279bed4101731160ca
Antonio Cuni <anto.cuni@gmail.com>
2012-12-19 19:49:15 +0100

    bah, obviosuly closures are not rpython

--- pypy/jit/metainterp/optimizeopt/earlyforce.py
+++ pypy/jit/metainterp/optimizeopt/earlyforce.py
@@ -3,14 +3,16 @@ from pypy.jit.metainterp.optimizeopt.optimizer import Optimization
 from pypy.jit.metainterp.optimizeopt.vstring import VAbstractStringValue
 from pypy.jit.metainterp.resoperation import rop, ResOperation
 
+def is_raw_free(op, opnum):
+    if opnum != rop.CALL:
+        return False
+    einfo = op.getdescr().get_extra_info()
+    return einfo.oopspecindex == EffectInfo.OS_RAW_FREE
+
+
 class OptEarlyForce(Optimization):
     def propagate_forward(self, op):
         opnum = op.getopnum()
-        def is_raw_free():
-            if opnum != rop.CALL:
-                return False
-            einfo = op.getdescr().get_extra_info()
-            return einfo.oopspecindex == EffectInfo.OS_RAW_FREE
 
         if (opnum != rop.SETFIELD_GC and 
             opnum != rop.SETARRAYITEM_GC and
@@ -18,7 +20,7 @@ class OptEarlyForce(Optimization):
             opnum != rop.QUASIIMMUT_FIELD and
             opnum != rop.SAME_AS and
             opnum != rop.MARK_OPAQUE_PTR and
-            not is_raw_free()):
+            not is_raw_free(op, opnum)):
                
             for arg in op.getarglist():
                 if arg in self.optimizer.values:
