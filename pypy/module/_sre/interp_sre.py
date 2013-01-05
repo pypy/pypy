9900ef7bc985202c31cd76da1a311ed25513daac
Alex Gaynor <alex.gaynor@gmail.com>
2013-01-04 18:55:14 -0800

    Optimize match.group('name') by making it a module dict.

--- pypy/module/_sre/interp_sre.py
+++ pypy/module/_sre/interp_sre.py
@@ -90,7 +90,7 @@ def searchcontext(space, ctx):
 # SRE_Pattern class
 
 class W_SRE_Pattern(Wrappable):
-    _immutable_fields_ = ["code", "flags", "num_groups"]
+    _immutable_fields_ = ["code", "flags", "num_groups", "w_indexgroup"]
 
     def cannot_copy_w(self):
         space = self.space
