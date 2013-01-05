54ccb1000ed2f226a8ba47097039936ad35ab379
Alex Gaynor <alex.gaynor@gmail.com>
2013-01-05 00:24:34 -0800

    promote this

--- pypy/module/_sre/interp_sre.py
+++ pypy/module/_sre/interp_sre.py
@@ -54,7 +54,7 @@ def do_flatten_marks(ctx, num_groups):
     result = [-1] * (2 * num_groups)
     mark = ctx.match_marks
     while mark is not None:
-        index = mark.gid
+        index = jit.promote(mark.gid)
         if result[index] == -1:
             result[index] = mark.position
         mark = mark.prev
