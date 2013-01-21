8bf2505fecf5e8a9804fee3d56feb8c06fc24fb9
Maciej Fijalkowski <fijall@gmail.com>
2013-01-21 22:23:48 +0200

    typo

--- rpython/jit/backend/llsupport/llmodel.py
+++ rpython/jit/backend/llsupport/llmodel.py
@@ -55,9 +55,9 @@ class AbstractLLCPU(AbstractCPU):
         FUNC_TP = lltype.Ptr(lltype.FuncType([llmemory.GCREF],
                                              llmemory.GCREF))
 
-        def realloc_frame(frame, size):
+        def realloc_frame(frame):
             frame = lltype.cast_opaque_ptr(jitframe.JITFRAMEPTR, frame)
-            new_frame = jitframe.JITFRAME.allocate(frame.frame_info)
+            new_frame = jitframe.JITFRAME.allocate(frame.jf_frame_info)
             # XXX now we know, rewrite this
             # we need to do this, because we're not sure what things
             # are GC pointers and which ones are not
