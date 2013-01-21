6bb514163dfe955db94a77857e09a31e4f8ab319
Maciej Fijalkowski <fijall@gmail.com>
2013-01-21 11:09:17 +0200

    more casts

--- rpython/jit/backend/x86/regalloc.py
+++ rpython/jit/backend/x86/regalloc.py
@@ -455,6 +455,9 @@ class RegAlloc(object):
         # the potential argument to FINISH
         descr = op.getdescr()
         fail_descr = cast_instance_to_gcref(descr)
+        # we know it does not move, but well
+        rgc._make_sure_does_not_move(fail_descr)
+        fail_descr = rffi.cast(lltype.Signed, fail_descr)
         if op.numargs() == 1:
             loc = self.make_sure_var_in_reg(op.getarg(0))
             locs = [loc, imm(fail_descr)]
