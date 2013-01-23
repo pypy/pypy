57d85bf38a3f34a25d3f3c3b75bc26fc2b5d7714
mattip <unknown>
2013-01-23 10:24:40 +0200

    handle case where running from current dir

--- pypy/test_all.py
+++ pypy/test_all.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         print >> sys.stderr, __doc__
         sys.exit(2)
     #Add toplevel repository dir to sys.path
-    sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
+    sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     import pytest
     import pytest_cov
     sys.exit(pytest.main(plugins=[pytest_cov]))
