93c98b49e34919a53f89831ae40e0d9b1916ad27
Philip Jenvey <pjenvey@underboss.org>
2013-01-24 16:36:33 -0800

    woops, no longer skips but still needs an entry here

--- lib-python/conftest.py
+++ lib-python/conftest.py
@@ -281,6 +281,7 @@ testmap = [
     RegrTest('test_ioctl.py'),
     RegrTest('test_isinstance.py', core=True),
     RegrTest('test_iter.py', core=True),
+    RegrTest('test_iterlen.py', core=True, usemodules="_collections itertools"),
     RegrTest('test_itertools.py', core=True, usemodules="itertools struct"),
     RegrTest('test_json.py'),
     RegrTest('test_kqueue.py'),
