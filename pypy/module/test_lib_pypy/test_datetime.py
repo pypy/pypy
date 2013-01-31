2faf0b47cae1f8fa9a09de2c0677c31318e54ad9
Brian Kearns <bdkearns@gmail.com>
2013-01-31 15:42:15 -0500

    fix this bogus test in test_datetime

--- pypy/module/test_lib_pypy/test_datetime.py
+++ pypy/module/test_lib_pypy/test_datetime.py
@@ -36,24 +36,24 @@ def test_more_datetime_rounding():
     # this test verified on top of CPython 2.7 (using a plain
     # "import datetime" above)
     expected_results = {
-        -1000.0: 'datetime.datetime(1970, 1, 1, 0, 43, 20)',
-        -999.9999996: 'datetime.datetime(1970, 1, 1, 0, 43, 20)',
-        -999.4: 'datetime.datetime(1970, 1, 1, 0, 43, 20, 600000)',
-        -999.0000004: 'datetime.datetime(1970, 1, 1, 0, 43, 21)',
-        -1.0: 'datetime.datetime(1970, 1, 1, 0, 59, 59)',
-        -0.9999996: 'datetime.datetime(1970, 1, 1, 0, 59, 59)',
-        -0.4: 'datetime.datetime(1970, 1, 1, 0, 59, 59, 600000)',
-        -0.0000004: 'datetime.datetime(1970, 1, 1, 1, 0)',
-        0.0: 'datetime.datetime(1970, 1, 1, 1, 0)',
-        0.0000004: 'datetime.datetime(1970, 1, 1, 1, 0)',
-        0.4: 'datetime.datetime(1970, 1, 1, 1, 0, 0, 400000)',
-        0.9999996: 'datetime.datetime(1970, 1, 1, 1, 0, 1)',
-        1000.0: 'datetime.datetime(1970, 1, 1, 1, 16, 40)',
-        1000.0000004: 'datetime.datetime(1970, 1, 1, 1, 16, 40)',
-        1000.4: 'datetime.datetime(1970, 1, 1, 1, 16, 40, 400000)',
-        1000.9999996: 'datetime.datetime(1970, 1, 1, 1, 16, 41)',
-        1293843661.191: 'datetime.datetime(2011, 1, 1, 2, 1, 1, 191000)',
+        -1000.0: 'datetime.datetime(1969, 12, 31, 23, 43, 20)',
+        -999.9999996: 'datetime.datetime(1969, 12, 31, 23, 43, 20)',
+        -999.4: 'datetime.datetime(1969, 12, 31, 23, 43, 20, 600000)',
+        -999.0000004: 'datetime.datetime(1969, 12, 31, 23, 43, 21)',
+        -1.0: 'datetime.datetime(1969, 12, 31, 23, 59, 59)',
+        -0.9999996: 'datetime.datetime(1969, 12, 31, 23, 59, 59)',
+        -0.4: 'datetime.datetime(1969, 12, 31, 23, 59, 59, 600000)',
+        -0.0000004: 'datetime.datetime(1970, 1, 1, 0, 0)',
+        0.0: 'datetime.datetime(1970, 1, 1, 0, 0)',
+        0.0000004: 'datetime.datetime(1970, 1, 1, 0, 0)',
+        0.4: 'datetime.datetime(1970, 1, 1, 0, 0, 0, 400000)',
+        0.9999996: 'datetime.datetime(1970, 1, 1, 0, 0, 1)',
+        1000.0: 'datetime.datetime(1970, 1, 1, 0, 16, 40)',
+        1000.0000004: 'datetime.datetime(1970, 1, 1, 0, 16, 40)',
+        1000.4: 'datetime.datetime(1970, 1, 1, 0, 16, 40, 400000)',
+        1000.9999996: 'datetime.datetime(1970, 1, 1, 0, 16, 41)',
+        1293843661.191: 'datetime.datetime(2011, 1, 1, 1, 1, 1, 191000)',
         }
     for t in sorted(expected_results):
-        dt = datetime.datetime.fromtimestamp(t)
+        dt = datetime.datetime.utcfromtimestamp(t)
         assert repr(dt) == expected_results[t]
