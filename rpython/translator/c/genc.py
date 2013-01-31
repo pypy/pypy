740a125e63d0c0ac18d4a26dd57da568728b4b4c
Armin Rigo <arigo@tunes.org>
2013-01-31 22:25:38 +0100

    Don't include profiling.c by default.  It should be included by interp_lsprof already.  It would end with the file listed twice in the Makefile...

--- rpython/translator/c/genc.py
+++ rpython/translator/c/genc.py
@@ -761,7 +761,6 @@ def add_extra_files(eci):
         srcdir / 'exception.c',
         srcdir / 'rtyper.c',           # ifdef HAVE_RTYPER
         srcdir / 'support.c',
-        srcdir / 'profiling.c',
         srcdir / 'debug_print.c',
         srcdir / 'debug_traceback.c',  # ifdef HAVE_RTYPER
         srcdir / 'asm.c',
