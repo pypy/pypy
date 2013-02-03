f8b6300ee21373844d412948d461be648fe7ed98
Maciej Fijalkowski <fijall@gmail.com>
2013-02-04 00:13:29 +0200

    2.6 compat

--- rpython/jit/backend/llsupport/regalloc.py
+++ rpython/jit/backend/llsupport/regalloc.py
@@ -2,7 +2,10 @@ import os
 from rpython.jit.metainterp.history import Const, Box, REF, INT
 from rpython.rlib.objectmodel import we_are_translated, specialize
 from rpython.jit.metainterp.resoperation import rop
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    OrderedDict = dict # too bad
 
 class TempBox(Box):
     def __init__(self):
