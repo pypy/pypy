74ea3fe5cc9f9f54ca7172b453638e1ea6d05789
Brian Kearns <bdkearns@gmail.com>
2013-02-06 12:59:07 -0800

    catch proper exception here

--- pypy/module/unicodedata/interp_ucd.py
+++ pypy/module/unicodedata/interp_ucd.py
@@ -40,7 +40,7 @@ if MAXUNICODE > 0xFFFF:
             # Host CPython is narrow build, accept surrogates
             try:
                 return ord_accepts_surrogate(space.unicode_w(w_unichr))
-            except ValueError:
+            except TypeError:
                 raise OperationError(space.w_TypeError, space.wrap(
                     'need a single Unicode character as parameter'))
         else:
