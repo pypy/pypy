671b0389c82ce441528bf91c9b16d578367c912c
Armin Rigo <arigo@tunes.org>
2013-02-15 17:06:12 +0000

    Oups

--- pypy/module/__pypy__/interp_signal.py
+++ pypy/module/__pypy__/interp_signal.py
@@ -2,5 +2,5 @@
 def signals_enter(space):
     space.threadlocals.enable_signals()
 
-def signals_exit(space):
+def signals_exit(space, w_ignored1=None, w_ignored2=None, w_ignored3=None):
     space.threadlocals.disable_signals()
