5ef26987753d4f9bb9e97855f41ba2588a486aef
Philip Jenvey <pjenvey@underboss.org>
2013-02-19 13:17:32 -0800

    stub out a method for emitting warnings on implicit closing by __del__

--- rpython/rlib/rsocket.py
+++ rpython/rlib/rsocket.py
@@ -497,8 +497,15 @@ class RSocket(object):
     def __del__(self):
         fd = self.fd
         if fd != _c.INVALID_SOCKET:
-            self.fd = _c.INVALID_SOCKET
-            _c.socketclose(fd)
+            try:
+                self._dealloc_warn()
+            finally:
+                self.fd = _c.INVALID_SOCKET
+                _c.socketclose(fd)
+
+    def _dealloc_warn(self):
+        """Called when implicitly closed via the deconstructor"""
+        pass
 
     if hasattr(_c, 'fcntl'):
         def _setblocking(self, block):
