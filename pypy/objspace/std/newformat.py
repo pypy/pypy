00ff1875520f67765f880cbd88aebee1a81c89eb
Philip Jenvey <pjenvey@underboss.org>
2013-02-19 10:55:50 -0800

    revert the 'reset to g' from 52765baaf1aa, as we're backed by rpython's rfloat.double_to_string we need 'r'

--- pypy/objspace/std/newformat.py
+++ pypy/objspace/std/newformat.py
@@ -935,9 +935,9 @@ def make_formatting_class():
             tp = self._type
             self._get_locale(tp)
             if tp == "\0":
+                flags |= rfloat.DTSF_ADD_DOT_0
                 tp = "r"
                 default_precision = 0
-                flags |= rfloat.DTSF_ADD_DOT_0
             elif tp == "n":
                 tp = "g"
             value = space.float_w(w_float)
@@ -949,8 +949,6 @@ def make_formatting_class():
                 add_pct = False
             if self._precision == -1:
                 self._precision = default_precision
-            if tp == "r":
-                type = "g"
             result, special = rfloat.double_to_string(value, tp,
                                                       self._precision, flags)
             if add_pct:
