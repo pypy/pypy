023babdffd73a83e8a2f8378157c85c667d02aa6
Manuel Jacob <unknown>
2013-03-04 20:44:14 +0100

    Use _attrs_ instead of __slots__ here.

--- pypy/interpreter/buffer.py
+++ pypy/interpreter/buffer.py
@@ -26,7 +26,7 @@ from rpython.rlib.rstring import StringBuilder
 class Buffer(Wrappable):
     """Abstract base class for memory views."""
 
-    __slots__ = ('format', 'itemsize')
+    _attrs_ = ('format', 'itemsize')
     format = 'B'
     itemsize = 1
 
