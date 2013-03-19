7461d6fac85c3ba0dc1652fa496317a125eda534
Maciej Fijalkowski <fijall@gmail.com>
2013-03-18 22:44:20 -0700

    fix a bunch of overly specific tests

--- pypy/module/pypyjit/test_pypy_c/test_00_model.py
+++ pypy/module/pypyjit/test_pypy_c/test_00_model.py
@@ -496,7 +496,7 @@ class TestRunPyPyC(BaseTestPyPyC):
             i10 = getfield_raw(..., descr=<.* pypysig_long_struct.c_value .*>)
             i14 = int_lt(i10, 0)
             guard_false(i14, descr=...)
-            jump(p0, p1, p2, p3, i8, descr=...)
+            jump(..., descr=...)
         """)
         #
         assert loop.match("""
@@ -504,7 +504,7 @@ class TestRunPyPyC(BaseTestPyPyC):
             guard_true(i6, descr=...)
             i8 = int_add(i4, 1)
             --TICK--
-            jump(p0, p1, p2, p3, i8, descr=...)
+            jump(..., descr=...)
         """)
         #
         py.test.raises(InvalidMatch, loop.match, """
@@ -512,7 +512,7 @@ class TestRunPyPyC(BaseTestPyPyC):
             guard_true(i6)
             i8 = int_add(i5, 1) # variable mismatch
             --TICK--
-            jump(p0, p1, p2, p3, i8, descr=...)
+            jump(..., descr=...)
         """)
 
     def test_match_by_id(self):
