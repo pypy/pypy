9ab634606707a9a74a73b01d93d5c823e4a11719
Armin Rigo <arigo@tunes.org>
2013-04-03 11:37:58 +0200

    backout the merge of the 'vendor-rename' branch in 2ba4a1b917cb.

--- pypy/interpreter/app_main.py
+++ pypy/interpreter/app_main.py
@@ -705,7 +705,7 @@ if __name__ == '__main__':
         from os.path import abspath, join, dirname as dn
         thisfile = abspath(__file__)
         root = dn(dn(dn(thisfile)))
-        return [join(root, 'lib-python', '2'),
+        return [join(root, 'lib-python', '2.7'),
                 join(root, 'lib_pypy')]
 
     def pypy_resolvedirof(s):
