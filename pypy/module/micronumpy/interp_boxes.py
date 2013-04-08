1163a15335eaa5e2784ebce08ff41a783b4a778a
Philip Jenvey <pjenvey@underboss.org>
2013-04-08 15:41:52 -0700

    int_typedef -> long_typedef

--- pypy/module/micronumpy/interp_boxes.py
+++ pypy/module/micronumpy/interp_boxes.py
@@ -5,15 +5,15 @@ from pypy.interpreter.typedef import TypeDef, GetSetProperty
 from pypy.objspace.std.floattype import float_typedef
 from pypy.objspace.std.stringtype import str_typedef
 from pypy.objspace.std.unicodetype import unicode_typedef, unicode_from_object
-from pypy.objspace.std.inttype import int_typedef
+from pypy.objspace.std.longtype import long_typedef
 from pypy.objspace.std.complextype import complex_typedef
 from rpython.rlib.rarithmetic import LONG_BIT
 from rpython.rtyper.lltypesystem import rffi
 from rpython.tool.sourcetools import func_with_new_name
 from pypy.module.micronumpy.arrayimpl.voidbox import VoidBoxStorage
 
-MIXIN_32 = (int_typedef,) if LONG_BIT == 32 else ()
-MIXIN_64 = (int_typedef,) if LONG_BIT == 64 else ()
+MIXIN_32 = (long_typedef,) if LONG_BIT == 32 else ()
+MIXIN_64 = (long_typedef,) if LONG_BIT == 64 else ()
 
 # Is this the proper place for this?
 ENABLED_LONG_DOUBLE = False
