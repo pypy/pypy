c9eec4f961e60356fbce8c8f59252ad1a60ec92a
mattip <matti.picus@gmail.com>
2013-04-12 18:13:42 +0300

    backout changeset 1d1c1b2fe42a, belongs on branch not default

--- rpython/jit/backend/test/test_zll_stress_0.py
+++ rpython/jit/backend/test/test_zll_stress_0.py
@@ -1,7 +1,4 @@
 from rpython.jit.backend.test import zll_stress
-import py
+
 def test_stress_0():
-    import sys
-    if sys.platform.startswith('win'):
-        py.test.skip('crashes test platform, fix crash and reenable test')
     zll_stress.do_test_stress(0)
