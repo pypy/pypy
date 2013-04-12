71adb8f493ef3dcb6dd88b99d58982938af9a039
mattip <matti.picus@gmail.com>
2013-04-12 11:01:59 +0300

    skip test (fow now?) that not only crashes but seems to ruin the rest of the test run on buildbot

--- rpython/jit/backend/test/test_zll_stress_0.py
+++ rpython/jit/backend/test/test_zll_stress_0.py
@@ -1,4 +1,7 @@
 from rpython.jit.backend.test import zll_stress
-
+import py
 def test_stress_0():
+    import sys
+    if sys.platform.startswith('win'):
+        py.test.skip('crashes test platform, fix crash and reenable test')
     zll_stress.do_test_stress(0)
