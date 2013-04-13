8bfeb087d926dac2816d3ee4015965fd853e831c
Brian Kearns <bdkearns@gmail.com>
2013-04-12 22:10:22 -0400

    unhandled exceptions in threads should print tracebacks like cpython, test

--- pypy/module/thread/os_thread.py
+++ pypy/module/thread/os_thread.py
@@ -132,7 +132,7 @@ class Bootstrapper(object):
             if not e.match(space, space.w_SystemExit):
                 ident = rthread.get_ident()
                 where = 'thread %d started by ' % ident
-                e.write_unraisable(space, where, w_callable)
+                e.write_unraisable(space, where, w_callable, with_traceback=True)
             e.clear(space)
         # clean up space.threadlocals to remove the ExecutionContext
         # entry corresponding to the current thread
