10ff484a36dff58286f2b5414ceebc95d7b7d5d4
Maciej Fijalkowski <fijall@gmail.com>
2013-04-16 14:34:04 +0200

    Fix tests now that we're slightly more pedantic about descrs

--- rpython/jit/backend/llsupport/test/test_rewrite.py
+++ rpython/jit/backend/llsupport/test/test_rewrite.py
@@ -109,6 +109,8 @@ class FakeTracker(object):
 class BaseFakeCPU(object):
     JITFRAME_FIXED_SIZE = 0
 
+    can_inline_varsize_malloc = True
+
     def __init__(self):
         self.tracker = FakeTracker()
         self._cache = {}
