a0cbff2b6b1b748e4f80a6b343a244f06863f3b0
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-04-28 17:01:50 +0200

    testrunner: use bytesio instead of a file in the unittest

--- testrunner/test/test_scratchbox_runner.py
+++ testrunner/test/test_scratchbox_runner.py
@@ -1,9 +1,9 @@
 import sys
+import io
 import scratchbox_runner
 
 def test_scratchbox(tmpdir):
-    out = tmpdir.join('out').open('w')
-
+    out = io.BytesIO()
     param = scratchbox_runner.ScratchboxRunParam(tmpdir, out)
     expected = ['/scratchbox/login', '-d', tmpdir.strpath, sys.executable]
     assert param.interp == expected
