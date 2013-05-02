07240a6ae003585ddd0418d9b6de90010bacef05
Philip Jenvey <pjenvey@underboss.org>
2013-05-02 12:22:40 -0700

    2to3

--- pypy/module/__builtin__/test/test_zip.py
+++ pypy/module/__builtin__/test/test_zip.py
@@ -15,9 +15,9 @@ class AppTestZip:
 
     def test_two_lists(self):
         # uses a different code path
-        assert zip([1, 2, 3], [3, 4, 5]) == [(1, 3), (2, 4), (3, 5)]
-        assert zip([1, 2, 3], [3, 4]) == [(1, 3), (2, 4)]
-        assert zip([1, 2], [3, 4, 5]) == [(1, 3), (2, 4)]
+        assert list(zip([1, 2, 3], [3, 4, 5])) == [(1, 3), (2, 4), (3, 5)]
+        assert list(zip([1, 2, 3], [3, 4])) == [(1, 3), (2, 4)]
+        assert list(zip([1, 2], [3, 4, 5])) == [(1, 3), (2, 4)]
 
     def test_three_lists_same_size(self):
         assert list(zip([1, 2, 3], [3, 4, 5], [6, 7, 8])) == (
