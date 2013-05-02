a5e1d1682bc429eafba738a57068ccafcf39f7e4
Armin Rigo <arigo@tunes.org>
2013-05-02 23:24:54 +0200

    I *think* this should skip tests when running from the backend/*/test directory and the tests require some more support that what the real cpu provides.

--- rpython/jit/metainterp/test/support.py
+++ rpython/jit/metainterp/test/support.py
@@ -70,6 +70,12 @@ def _get_jitcodes(testself, CPUClass, func, values, type_system,
     cw = codewriter.CodeWriter(cpu, [FakeJitDriverSD()])
     cw.debug = True
     testself.cw = cw
+    if supports_floats and not cpu.supports_floats:
+        py.test.skip("this test requires supports_floats=True")
+    if supports_longlong and not cpu.supports_longlong:
+        py.test.skip("this test requires supports_longlong=True")
+    if supports_singlefloats and not cpu.supports_singlefloats:
+        py.test.skip("this test requires supports_singlefloats=True")
     policy = JitPolicy()
     policy.set_supports_floats(supports_floats)
     policy.set_supports_longlong(supports_longlong)
