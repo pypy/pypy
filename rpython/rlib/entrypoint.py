fb59e5a5865e3db522992ee8ddd2dcee0cd7edbb
Maciej Fijalkowski <fijall@gmail.com>
2013-05-08 19:15:28 +0200

    automatically export all @entrypoints, not sure exactly if it works

--- rpython/rlib/entrypoint.py
+++ rpython/rlib/entrypoint.py
@@ -7,12 +7,16 @@ def entrypoint(key, argtypes, c_name=None, relax=False):
 
     if key == 'main' than it's included by default
     """
+    from rpython.translator.cbuild import ExternalCompilationInfo
+    
     def deco(func):
         secondary_entrypoints.setdefault(key, []).append((func, argtypes))
         if c_name is not None:
             func.c_name = c_name
         if relax:
             func.relax_sig_check = True
+        func._compilation_info = ExternalCompilationInfo(
+            export_symbols=[c_name or func.func_name])
         return func
     return deco
 
