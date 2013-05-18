63bbe1b455e8a6b271eb7d224eb8859579998d10
Manuel Jacob <unknown>
2013-05-18 11:44:56 +0200

    Move the negate helper into the new module pypy.objspace.std.util.

---
+++ pypy/objspace/std/util.py
@@ -0,0 +1,18 @@
+def negate(f):
+    """Create a function which calls `f` and negates its result.  When the
+    result is ``space.w_NotImplemented``, ``space.w_NotImplemented`` is
+    returned. This is useful for complementing e.g. the __ne__ descriptor if
+    your type already defines a __eq__ descriptor.
+    """
+    def _negator(self, space, w_other):
+        # no need to use space.is_ / space.not_
+        tmp = f(self, space, w_other)
+        if tmp is space.w_NotImplemented:
+            return space.w_NotImplemented
+        elif tmp is space.w_False:
+            return space.w_True
+        else:
+            return space.w_False
+    _negator.func_name = 'negate-%s' % f.func_name
+    return _negator
+
