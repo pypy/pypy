fa2a337241529cee98f3e57fb562bfa3efff8d00
Manuel Jacob <unknown>
2013-05-22 15:50:23 +0200

    Add tuple to _interplevel_classes.

--- pypy/objspace/std/objspace.py
+++ pypy/objspace/std/objspace.py
@@ -664,13 +664,10 @@ class StdObjSpace(ObjSpace, DescrOperation):
                 self._interplevel_classes[w_type] = base
 
         # register other things
-        from pypy.objspace.std.dictmultiobject import W_DictMultiObject
-        from pypy.objspace.std.listobject import W_ListObject
-        from pypy.objspace.std.setobject import W_SetObject
-
         self._interplevel_classes[self.w_dict] = W_DictMultiObject
         self._interplevel_classes[self.w_list] = W_ListObject
         self._interplevel_classes[self.w_set] = W_SetObject
+        self._interplevel_classes[self.w_tuple] = W_AbstractTupleObject
 
     @specialize.memo()
     def _get_interplevel_cls(self, w_type):
