2d9613e9f2a1c50028e1bdccf6eca8cdc640ba75
Philip Jenvey <pjenvey@underboss.org>
2013-05-28 18:14:13 -0700

    PipeConnection is win32 only

--- pypy/module/_multiprocessing/__init__.py
+++ pypy/module/_multiprocessing/__init__.py
@@ -6,7 +6,6 @@ class Module(MixedModule):
 
     interpleveldefs = {
         'Connection'      : 'interp_connection.W_FileConnection',
-        'PipeConnection'  : 'interp_connection.W_PipeConnection',
         'SemLock'         : 'interp_semaphore.W_SemLock',
 
         'address_of_buffer' : 'interp_memory.address_of_buffer',
@@ -16,4 +15,6 @@ class Module(MixedModule):
     }
 
     if sys.platform == 'win32':
+        interpleveldefs['PipeConnection'] = \
+            'interp_connection.W_PipeConnection'
         interpleveldefs['win32'] = 'interp_win32.win32_namespace(space)'
