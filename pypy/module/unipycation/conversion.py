5522c40d14c6cf06c79c51484e1bf6d2aa573f2f
Edd Barrett <vext01@gmail.com>
2013-06-12 16:29:02 +0100

    Convert python floats to prolog floats

--- pypy/module/unipycation/conversion.py
+++ pypy/module/unipycation/conversion.py
@@ -4,3 +4,8 @@ def int_p_of_int_w(space, int_w):
     int_val = space.int_w(int_w)
     int_p = pterm.Number(int_val)
     return int_p
+
+def float_p_of_float_w(space, float_w):
+    float_val = space.float_w(float_w)
+    float_p = pterm.Float(float_val)
+    return float_p
--- pypy/module/unipycation/test/test_type_conversion.py
+++ pypy/module/unipycation/test/test_type_conversion.py
@@ -11,3 +11,22 @@ class TestTypeConversion(object):
         unwrap2 = int_p.num
 
         assert unwrap1 == unwrap2
+
+    def test_float_p_of_float_w(self):
+        float_w = self.space.newfloat(678.666)
+        float_p = conv.float_p_of_float_w(self.space, float_w)
+
+        unwrap1 = self.space.float_w(float_w)
+        unwrap2 = float_p.floatval
+
+        assert unwrap1 == unwrap2
+
+    # corner case: test -0.0 converts properly
+    def test_float_p_of_float_w_2(self):
+        float_w = self.space.newfloat(-0.0)
+        float_p = conv.float_p_of_float_w(self.space, float_w)
+
+        unwrap1 = self.space.float_w(float_w)
+        unwrap2 = float_p.floatval
+
+        assert unwrap1 == unwrap2
