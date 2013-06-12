830a5120d8d4dc2c2a606a0f0ce54489513d3f00
Edd Barrett <vext01@gmail.com>
2013-06-12 14:23:39 +0100

    on second thoughts, pyrolog should be stand alone

--- pypy/module/pyrolog/prolog/prolog_modules/structural_comparison.pl
+++
@@ -1,8 +0,0 @@
-:- module(structural_comparison, ['=@='/2]).
-
-'=@='(A, B) :-
-	copy_term(A, A1),
-	copy_term(B, B1),
-	numbervars(A1, 0, N),
-	numbervars(B1, 0, N),
-	A1 == B1.
--- pypy/module/pyrolog/test/test_type_conversion.py
+++ pypy/module/pyrolog/test/test_type_conversion.py
@@ -1,7 +1,14 @@
+import pypy.module.pyrolog.glue.conversion as conv
+
 class TestTypeConversion(object):
     spaceconfig = dict(usemodules=('pyrolog',))
 
     def test_import(self):
-        from pypy.module.pyrolog.glue.conversion import hello_world
-        ok = hello_world()
+        ok = conv.hello_world()
+        assert(ok)
+
+    def test_int_p_of_int_w(self):
+        int_w = space.int_w(666)
+        int_p = conv.int_p_of_int_w(int_w)
+        ok = (str(int_w) == str(int_p))
         assert(ok)
