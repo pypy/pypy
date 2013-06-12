4bfe533ef1bdd88a3324154bc4a42b6685189404
Edd Barrett <vext01@gmail.com>
2013-06-12 17:19:28 +0100

    convert python strings to atoms

--- pypy/module/unipycation/conversion.py
+++ pypy/module/unipycation/conversion.py
@@ -9,3 +9,19 @@ def float_p_of_float_w(space, float_w):
     float_val = space.float_w(float_w)
     float_p = pterm.Float(float_val)
     return float_p
+
+def float_p_of_float_w(space, float_w):
+    float_val = space.float_w(float_w)
+    float_p = pterm.Float(float_val)
+    return float_p
+
+def bigint_p_of_long_w(space, long_w):
+    bigint_val = space.bigint_w(long_w)
+    bigint_p = pterm.BigInt(bigint_val)
+    return bigint_p
+
+def atom_p_of_str_w(space, str_w):
+    str_val = space.str_w(str_w)
+    atom_p = pterm.Atom(str_val)
+    return atom_p
+
--- pypy/module/unipycation/test/test_type_conversion.py
+++ pypy/module/unipycation/test/test_type_conversion.py
@@ -30,3 +30,21 @@ class TestTypeConversion(object):
         unwrap2 = float_p.floatval
 
         assert unwrap1 == unwrap2
+
+    def test_bigint_p_of_long_w(self):
+        long_w = self.space.newlong_from_rbigint(2**65 + 42)
+        bigint_p = conv.bigint_p_of_long_w(self.space, long_w)
+
+        unwrap1 = self.space.bigint_w(long_w)
+        unwrap2 = bigint_p.value
+
+        assert unwrap1 == unwrap2
+
+    def test_atom_p_of_str_w(self):
+        str_w = self.space.wrap("humppa")
+        atom_p = conv.atom_p_of_str_w(self.space, str_w)
+
+        unwrap1 = self.space.str_w(str_w)
+        unwrap2 = atom_p._signature.name
+
+        assert unwrap1 == unwrap2
