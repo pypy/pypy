804a979b627f09224f8e0377b45994b2c2ed0740
Edd Barrett <vext01@gmail.com>
2013-06-17 13:21:52 +0100

    Add unit tests for scaffolded Prolog engine.

---
+++ pypy/module/unipycation/test/test_engine.py
@@ -0,0 +1,11 @@
+import pypy.module.unipycation.engine as eng
+#from prolog.interpreter.continuation import Engine
+
+class AppTestEngine(object):
+    spaceconfig = dict(usemodules=('unipycation',))
+
+    def test_basic(self):
+        import unipycation
+        e = unipycation.Engine("likes(bob, jazz). likes(jim, funk).")
+        assert True
+        
