624385106ad619dd6af20c43376ce65947618f24
Edd Barrett <vext01@gmail.com>
2013-06-19 10:43:09 +0100

    Add a few more tests. Shows that we need to think more. See XXX

--- pypy/module/unipycation/test/test_engine.py
+++ pypy/module/unipycation/test/test_engine.py
@@ -13,4 +13,37 @@ class AppTestEngine(object):
         res = e.query("likes(X, jazz).")
 
         print(res)
-        assert res["X"] == "mac"
+        assert res["X"] in ["mac", "bob"]
+
+    def test_basic_2(self):
+        import unipycation
+
+        e = unipycation.Engine("f(1, a). f(2, b). f(3, c).")
+        assert isinstance(e, unipycation.Engine)
+
+        res = e.query("f(X, Y).")
+
+        print(res)
+        assert res["X"] in [1, 2, 3]
+
+    def test_anonymous(self):
+        import unipycation
+
+        e = unipycation.Engine("f(1, a). f(2, b). f(3, c).")
+        assert isinstance(e, unipycation.Engine)
+
+        res = e.query("f(_, Y).")
+
+        print(res)
+        assert res["Y"] in ["a", "b", "c"]
+
+    def test_anonymous_tautology(self):
+        import unipycation
+
+        e = unipycation.Engine("f(1).")
+        assert isinstance(e, unipycation.Engine)
+
+        res = e.query("f(_)")
+
+        print(res)
+        assert False # We have no way of detecting trivially true/false XXX
