fc73c295a27567d57d8360081eb878795c4ee8c5
Armin Rigo <arigo@tunes.org>
2013-06-29 22:06:53 +0200

    in-progress

--- rpython/translator/stm/stmgcintf.py
+++ rpython/translator/stm/stmgcintf.py
@@ -5,9 +5,13 @@ from rpython.conftest import cdir as cdir2
 
 cdir = os.path.abspath(os.path.join(cdir2, '..', 'stm'))
 
+separate_source = '''
+#include "src_stm/stmgc.c"
+'''
+
 eci = ExternalCompilationInfo(
     include_dirs = [cdir, cdir2],
     includes = ['src_stm/stmgc.h'],
     pre_include_bits = ['#define RPY_STM 1'],
-    separate_module_sources = ['\n#include "src_stm/stmgc.c"\n'],
+    separate_module_sources = [separate_source],
 )
