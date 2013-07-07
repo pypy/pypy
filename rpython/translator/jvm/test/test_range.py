5d22f7334df4fa42d289900486bac9de17c3931b
Ronan Lamy <ronan.lamy@gmail.com>
2013-07-07 13:46:49 +0200

    Remove rpython.translator.jvm

--- rpython/translator/jvm/test/test_range.py
+++
@@ -1,7 +0,0 @@
-import py
-from rpython.translator.jvm.test.runtest import JvmTest
-from rpython.rtyper.test.test_rrange import BaseTestRrange
-
-class TestJvmRange(JvmTest, BaseTestRrange):
-    def test_rlist_range(self):
-        pass # it doesn't make sense here
