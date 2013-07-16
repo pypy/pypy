0911b5c03109bf73d013d0a501d40fe993ea22d4
Edd Barrett <vext01@gmail.com>
2013-07-16 15:52:50 +0100

    Use a custom class that is callable for prolog predicates.

--- lib_pypy/uni.py
+++ lib_pypy/uni.py
@@ -6,19 +6,17 @@ class Engine2(object):
         self.engine = Engine(db_str)
         self.db = Database(self)
         self.multisol = False
-    
-class Database(object):
-    """ A class that represents the predicates exposed by a prolog engine """
 
-    def __init__(self, engine):
+class Predicate(object):
+    """ Represents a "callable" prolog predicate """
+
+    def __init__(self, engine, name):
         self.engine = engine
+        self.multiple_solutions = False
+        self.name = name
 
-    def __getattr__(self, name):
-        """ Predicates are called by db.name and are resolved dynamically """
-        return lambda *args : self._query(name, *args)
-
-    def _query(self, name, *args):
-        print(72 * "-")
+    def __call__(self, *args):
+        name = self.name
         print("query %s: %s" % (name, args, ))
 
         vs = []
@@ -32,17 +30,26 @@ class Database(object):
 
             term_args.append(e)
 
-        print("XXX: %s" % term_args)
         t = Term(name, term_args)
 
-        if self.engine.multisol:
+        if self.multiple_solutions:
             raise NotImplementedError("XXX")
         else:
             sol = self.engine.engine.query_single(t, vs)
             return tuple([ sol[v] for v in vs ])
+    
+class Database(object):
+    """ A class that represents the predicates exposed by a prolog engine """
+
+    def __init__(self, engine):
+        self.engine = engine
+
+    def __getattr__(self, name):
+        """ Predicates are called by db.name and are resolved dynamically """
+        return Predicate(self.engine, name)
 
 if __name__ == "__main__":
-    e = Engine("f(1, 2, 3).")
+    e = Engine2("f(1, 2, 3).")
     d = Database(e)
     sol = d.f(1, None, None)
     print(sol)
