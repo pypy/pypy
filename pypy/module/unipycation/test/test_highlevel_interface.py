2980cfdce245023603aa2ee4387115820b84cbbf
Carl Friedrich Bolz <cfbolz@gmx.de>
2013-07-16 12:10:19 +0200

    a possible interface

--- pypy/module/unipycation/test/test_highlevel_interface.py
+++ pypy/module/unipycation/test/test_highlevel_interface.py
@@ -7,8 +7,32 @@ class AppTestHighLevelInterface(object):
         import uni
 
         e = uni.Engine("f(666).")
-        X = uni.Var()
-        sol = e.db.f([X])
+        e.db.f.argument_spec[0] = uni.result_converter()
+        assert e.db.f() == 666
 
-        assert sol == { X : 666 }
+    def test_many_solutions(self):
+        e = uni.Engine("""
+            sqrt(X, Y) :-
+                Sol1 is sqrt(X), Sol2 is -Sol1, (Y = Sol1; Y = Sol2).
+        """)
+        e.db.sqrt.argument_spec[1] = uni.result_converter()
+        e.db.sqrt.many_solutions = True
+        for x in e.db.sqrt(4):
+            assert x ** 2 == 4
 
+    def test_append(self):
+        import uni
+
+        e = uni.Engine("app([], X, X). app([H | T1], T2, [H | T3]).")
+        e.db.app.argument_spec[2] = uni.result_converter()
+        assert e.db.app([1, 2, 3, 4], [7, 8, 9]) == [1, 2, 3, 4, 7, 8, 9]
+
+    def test_reverse(self):
+        import uni
+        e = uni.Engine("""
+            rev(X, Y) :- rev_helper(X, [], Y).
+            rev_helper([], Acc, Acc).
+            rev_helper([H | T], Acc, Res) :- rev_helper(T, [H | Acc], Res).
+        """)
+        e.db.rev.argument_spec[1] = uni.result_converter()
+        assert e.db.rev([1, 2, 3, 4]) == [4, 3, 2, 1]
