9238c0d01c0a137303b5c2c6021488f93b824765
Armin Rigo <arigo@tunes.org>
2013-07-17 12:20:43 +0200

    int_sub

--- pypy/module/__pypy__/interp_intop.py
+++ pypy/module/__pypy__/interp_intop.py
@@ -5,3 +5,7 @@ from rpython.rlib.rarithmetic import intmask
 @unwrap_spec(n=int, m=int)
 def int_add(space, n, m):
     return space.wrap(intmask(n + m))
+
+@unwrap_spec(n=int, m=int)
+def int_sub(space, n, m):
+    return space.wrap(intmask(n - m))
--- pypy/module/__pypy__/test/test_intop.py
+++ pypy/module/__pypy__/test/test_intop.py
@@ -9,3 +9,10 @@ class AppTestIntOp:
         assert intop.int_add(40, 2) == 42
         assert intop.int_add(sys.maxint, 1) == -sys.maxint-1
         assert intop.int_add(-2, -sys.maxint) == sys.maxint
+
+    def test_int_sub(self):
+        import sys
+        from __pypy__ import intop
+        assert intop.int_sub(40, -2) == 42
+        assert intop.int_sub(sys.maxint, -1) == -sys.maxint-1
+        assert intop.int_sub(-2, sys.maxint) == sys.maxint
