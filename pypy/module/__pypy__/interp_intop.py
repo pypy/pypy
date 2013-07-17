d3fd6c7aaacc4fe0b5b2cd98f7a3048bca5b1bfa
Armin Rigo <arigo@tunes.org>
2013-07-17 12:35:05 +0200

    Use a consistent style and avoid intmask()

--- pypy/module/__pypy__/interp_intop.py
+++ pypy/module/__pypy__/interp_intop.py
@@ -1,20 +1,19 @@
 from pypy.interpreter.gateway import unwrap_spec
-from rpython.rlib.rarithmetic import intmask
 from rpython.rtyper.lltypesystem import lltype
 from rpython.rtyper.lltypesystem.lloperation import llop
 
 
 @unwrap_spec(n=int, m=int)
 def int_add(space, n, m):
-    return space.wrap(intmask(n + m))
+    return space.wrap(llop.int_add(lltype.Signed, n, m))
 
 @unwrap_spec(n=int, m=int)
 def int_sub(space, n, m):
-    return space.wrap(intmask(n - m))
+    return space.wrap(llop.int_sub(lltype.Signed, n, m))
 
 @unwrap_spec(n=int, m=int)
 def int_mul(space, n, m):
-    return space.wrap(intmask(n * m))
+    return space.wrap(llop.int_mul(lltype.Signed, n, m))
 
 @unwrap_spec(n=int, m=int)
 def int_floordiv(space, n, m):
