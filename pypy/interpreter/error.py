ed5054fe9316b13363220d5f3932fc0770e31329
Philip Jenvey <pjenvey@underboss.org>
2013-07-24 13:56:08 -0700

    some debugging for mysteriousness on the buildbot

--- pypy/interpreter/error.py
+++ pypy/interpreter/error.py
@@ -413,7 +413,11 @@ def get_operrcls2(valuefmt):
                             value = space.type(value)
                         result = value.getname(space)
                     else:
-                        result = unicode(value)
+                        try:
+                            result = unicode(value)
+                        except UnicodeDecodeError:
+                            print('_compute_value failed %r' % value)
+                            raise
                     lst[i + i + 1] = result
                 lst[-1] = self.xstrings[-1]
                 return u''.join(lst)
