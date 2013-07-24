189dc62ee6c9d1478ee74be6f458637b75f1b107
Paweł Piotr Przeradowski <przeradowski@gmail.com>
2013-07-24 21:46:21 +0200

    no None allowed

--- pypy/module/__pypy__/interp_os.py
+++ pypy/module/__pypy__/interp_os.py
@@ -3,7 +3,7 @@ import os
 from pypy.interpreter.gateway import unwrap_spec
 
 
-@unwrap_spec(name=str)
+@unwrap_spec(name='str0')
 def real_getenv(space, name):
     """Get an OS environment value skipping Python cache"""
-    return space.wrap(os.getenv(name))
+    return space.wrap(os.environ.get(name))
