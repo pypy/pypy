b71d99a2faa5b5ffd384e5f3011a302e2ced58e6
Maciej Fijalkowski <fijall@gmail.com>
2013-07-24 21:21:00 +0200

    a crucial optimizations

--- rpython/jit/metainterp/optimizeopt/rewrite.py
+++ rpython/jit/metainterp/optimizeopt/rewrite.py
@@ -250,7 +250,7 @@ class OptRewrite(Optimization):
                 # This is only safe if the class of the guard_value matches the
                 # class of the guard_*_class, otherwise the intermediate ops might
                 # be executed with wrong classes.
-                previous_classbox = value.get_constant_class(self.optimizer.cpu)            
+                previous_classbox = value.get_constant_class(self.optimizer.cpu)
                 expected_classbox = self.optimizer.cpu.ts.cls_of_box(op.getarg(1))
                 assert previous_classbox is not None
                 assert expected_classbox is not None
@@ -342,6 +342,15 @@ class OptRewrite(Optimization):
         self.emit_operation(op)
         resvalue = self.getvalue(op.result)
         self.loop_invariant_results[key] = resvalue
+
+    def optimize_COND_CALL(self, op):
+        arg = op.getarg(0)
+        val = self.getvalue(arg)
+        if val.is_constant():
+            if val.box.same_constant(CONST_0):
+                return
+            op = op.copy_and_change(rop.CALL, args=op.getarglist()[1:])
+        self.emit_operation(op)
 
     def _optimize_nullness(self, op, box, expect_nonnull):
         value = self.getvalue(box)
