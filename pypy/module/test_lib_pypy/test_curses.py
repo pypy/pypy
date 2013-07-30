a21f54da596b8e1e5c12c46b9f64a4c4a141a3c8
Paweł Piotr Przeradowski <przeradowski@gmail.com>
2013-07-30 21:56:29 +0200

    failing test for color_content

---
+++ pypy/module/test_lib_pypy/test_curses.py
@@ -0,0 +1,14 @@
+import _curses
+
+lib = _curses.lib
+
+
+def test_color_content(monkeypatch):
+    def color_content(color, r, g, b):
+        r[0], g[0], b[0] = 42, 43, 44
+        return lib.OK
+
+    monkeypatch.setattr(_curses, '_ensure_initialised_color', lambda: None)
+    monkeypatch.setattr(lib, 'color_content', color_content)
+
+    assert _curses.color_content(None) == (42, 43, 44)
