6f5de5b783e51c18df2e9b31b587cf85bdadc8eb
Stefan H. Muller <shmueller2@gmail.com>
2013-07-31 00:42:23 +0200

    - cpyext/include: Add a few definitions needed by PyCXX to headers (for   matplotlib build). - NEW: cpyext/include/missing.h: Temporary place to put definitions from   missing header files.

---
+++ pypy/module/cpyext/include/missing.h
@@ -0,0 +1,15 @@
+
+/* Definitions from missing header files */
+
+#ifndef Py_MISSING_H
+#define Py_MISSING_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PyAPI_DATA(PyTypeObject) PyMethod_Type, PyRange_Type, PyTraceBack_Type;
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_MISSING_H */
