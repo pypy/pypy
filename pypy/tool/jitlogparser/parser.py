7210100cb677ad2fe0b2a8a3d8aebe549becc750
Armin Rigo <arigo@tunes.org>
2013-08-12 17:35:30 +0200

    Print assembler including the address of the current instruction and the raw bytes; I think there is little point in hiding information here, and it lets us see that some jumps have a bogus destination address (unpatched) (transplanted from 2d0eaeda9275f908f52a75973b1b63c6fa9baa80)

--- pypy/tool/jitlogparser/parser.py
+++ pypy/tool/jitlogparser/parser.py
@@ -87,7 +87,7 @@ class SimpleParser(OpParser):
                     continue
                 e = elem.split("\t")
                 adr = e[0]
-                v = " ".join(e[2:])
+                v = elem   # --- more compactly:  " ".join(e[2:])
                 if not start:
                     start = int(adr.strip(":"), 16)
                 ofs = int(adr.strip(":"), 16) - start
