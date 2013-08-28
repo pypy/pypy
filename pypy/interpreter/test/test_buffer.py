8a5d587c32f7db9ab404ac78d72019df632508ee
Manuel Jacob <unknown>
2013-08-28 16:58:48 +0100

    Fix pypy/interpreter/test/test_buffer.py.

--- pypy/interpreter/test/test_buffer.py
+++ pypy/interpreter/test/test_buffer.py
@@ -1,5 +1,5 @@
 import py
-from pypy.interpreter.buffer import Buffer
+from pypy.module.__builtin__.interp_memoryview import W_Buffer
 from rpython.tool.udir import udir
 
 testdir = udir.ensure('test_buffer', dir=1)
@@ -11,19 +11,17 @@ class TestBuffer:
         space = self.space
         w_hello = space.wrap('hello world')
         buf = space.buffer_w(w_hello)
-        assert isinstance(buf, Buffer)
         assert buf.getlength() == 11
         assert buf.as_str() == 'hello world'
         assert buf.getslice(1, 6, 1, 5) == 'ello '
-        assert space.buffer_w(space.wrap(buf)) is buf
+        assert space.buffer_w(W_Buffer(buf)) is buf
         assert space.bufferstr_w(w_hello) == 'hello world'
-        assert space.bufferstr_w(space.buffer(w_hello)) == 'hello world'
+        assert space.bufferstr_w(W_Buffer(space.buffer_w(w_hello))) == 'hello world'
         space.raises_w(space.w_TypeError, space.buffer_w, space.wrap(5))
-        space.raises_w(space.w_TypeError, space.buffer, space.wrap(5))
 
     def test_file_write(self):
         space = self.space
-        w_buffer = space.buffer(space.wrap('hello world'))
+        w_buffer = W_Buffer(space.buffer_w(space.wrap('hello world')))
         filename = str(testdir.join('test_file_write'))
         space.appexec([w_buffer, space.wrap(filename)], """(buffer, filename):
             f = open(filename, 'wb')
