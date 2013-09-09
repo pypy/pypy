215bf21372bde1e69c98a859f5a261e9293cf422
Armin Rigo <arigo@tunes.org>
2013-09-09 19:13:06 +0200

    Fix: allow other types too, e.g. an AddressAsInt instance.

--- rpython/rtyper/lltypesystem/opimpl.py
+++ rpython/rtyper/lltypesystem/opimpl.py
@@ -663,9 +663,7 @@ def op_debug_fatalerror(ll_msg):
 def op_raw_store(p, ofs, newvalue):
     from rpython.rtyper.lltypesystem import rffi
     TP = lltype.typeOf(p)
-    if TP != llmemory.Address:
-        assert TP == llmemory.GCREF
-        p = rffi.cast(llmemory.Address, p)
+    p = rffi.cast(llmemory.Address, p)
     TVAL = lltype.typeOf(newvalue)
     p = rffi.cast(rffi.CArrayPtr(TVAL), p + ofs)
     p[0] = newvalue
@@ -673,9 +671,7 @@ def op_raw_store(p, ofs, newvalue):
 def op_raw_load(TVAL, p, ofs):
     from rpython.rtyper.lltypesystem import rffi
     TP = lltype.typeOf(p)
-    if TP != llmemory.Address:
-        assert TP == llmemory.GCREF
-        p = rffi.cast(llmemory.Address, p)
+    p = rffi.cast(llmemory.Address, p)
     p = rffi.cast(rffi.CArrayPtr(TVAL), p + ofs)
     return p[0]
 op_raw_load.need_result_type = True
