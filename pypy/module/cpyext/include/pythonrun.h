4bc43393ede30a0a73bb0955bf503713734a3253
Philip Jenvey <pjenvey@underboss.org>
2013-09-27 15:42:02 -0700

    update to 3.2.3's prototypes

--- pypy/module/cpyext/include/pythonrun.h
+++ pypy/module/cpyext/include/pythonrun.h
@@ -8,9 +8,10 @@ extern "C" {
 
 PyAPI_FUNC(void) Py_FatalError(const char *message);
 
-/* taken from Python-2.7.3/Include/pydebug.h */
+/* taken from Python-3.2.3/Include/pydebug.h */
 PyAPI_DATA(int) Py_DebugFlag;
 PyAPI_DATA(int) Py_VerboseFlag;
+PyAPI_DATA(int) Py_QuietFlag;
 PyAPI_DATA(int) Py_InteractiveFlag;
 PyAPI_DATA(int) Py_InspectFlag;
 PyAPI_DATA(int) Py_OptimizeFlag;
@@ -18,18 +19,11 @@ PyAPI_DATA(int) Py_NoSiteFlag;
 PyAPI_DATA(int) Py_BytesWarningFlag;
 PyAPI_DATA(int) Py_UseClassExceptionsFlag;
 PyAPI_DATA(int) Py_FrozenFlag;
-PyAPI_DATA(int) Py_TabcheckFlag;
-PyAPI_DATA(int) Py_UnicodeFlag;
 PyAPI_DATA(int) Py_IgnoreEnvironmentFlag;
 PyAPI_DATA(int) Py_DivisionWarningFlag;
 PyAPI_DATA(int) Py_DontWriteBytecodeFlag;
 PyAPI_DATA(int) Py_NoUserSiteDirectory;
-/* _XXX Py_QnewFlag should go away in 3.0.  It's true iff -Qnew is passed,
- *   on the command line, and is used in 2.2 by ceval.c to make all "/" divisions
- *     true divisions (which they will be in 3.0). */
-PyAPI_DATA(int) _Py_QnewFlag;
-/* Warn about 3.x issues */
-PyAPI_DATA(int) Py_Py3kWarningFlag;
+PyAPI_DATA(int) Py_UnbufferedStdioFlag;
 PyAPI_DATA(int) Py_HashRandomizationFlag;
 
 
