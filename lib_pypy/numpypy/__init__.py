02660cdc06d78054b00cbc94ce6fdaf9bec4f2db
Brian Kearns <bdkearns@gmail.com>
2013-10-15 05:22:31 -0400

    move numpypy get_include to match numpy

--- lib_pypy/numpypy/__init__.py
+++ lib_pypy/numpypy/__init__.py
@@ -9,40 +9,7 @@ from .core import round, abs, max, min
 
 __version__ = '1.7.0'
 
-import os
-def get_include():
-    """
-    Return the directory that contains the NumPy \\*.h header files.
-
-    Extension modules that need to compile against NumPy should use this
-    function to locate the appropriate include directory.
-
-    Notes
-    -----
-    When using ``distutils``, for example in ``setup.py``.
-    ::
-
-        import numpy as np
-        ...
-        Extension('extension_name', ...
-                include_dirs=[np.get_include()])
-        ...
-
-    """
-    try:
-        import numpy
-    except:
-        # running from pypy source directory
-        head, tail = os.path.split(os.path.dirname(os.path.abspath(__file__)))
-        return os.path.join(head, '../include')
-    else:
-        # using installed numpy core headers
-        import numpy.core as core
-        d = os.path.join(os.path.dirname(core.__file__), 'include')
-    return d
-
-
-__all__ = ['__version__', 'get_include']
+__all__ = ['__version__']
 __all__ += core.__all__
 __all__ += lib.__all__
 
--- lib_pypy/numpypy/lib/__init__.py
+++ lib_pypy/numpypy/lib/__init__.py
@@ -5,8 +5,10 @@ import math
 from .function_base import *
 from .shape_base import *
 from .twodim_base import *
+from .utils import *
 
 __all__ = ['math']
 __all__ += function_base.__all__
 __all__ += shape_base.__all__
 __all__ += twodim_base.__all__
+__all__ += utils.__all__
