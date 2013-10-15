d6fb5b1cfa5529de478e3ee2457fb49c4a39f4cb
Brian Kearns <bdkearns@gmail.com>
2013-10-15 05:11:14 -0400

    cleanup more imports

--- lib_pypy/numpypy/__init__.py
+++ lib_pypy/numpypy/__init__.py
@@ -1,10 +1,11 @@
-import core
-from core import *
-import lib
-from lib import *
+from . import core
+from .core import *
+from . import lib
+from .lib import *
 
 from __builtin__ import bool, int, long, float, complex, object, unicode, str
-from core import abs, max, min
+
+from .core import round, abs, max, min
 
 __version__ = '1.7.0'
 
@@ -41,11 +42,9 @@ def get_include():
     return d
 
 
-
 __all__ = ['__version__', 'get_include']
 __all__ += core.__all__
 __all__ += lib.__all__
+
 #import sys
 #sys.modules.setdefault('numpy', sys.modules['numpypy'])
-
-
--- lib_pypy/numpypy/lib/__init__.py
+++ lib_pypy/numpypy/lib/__init__.py
@@ -1,11 +1,12 @@
-import function_base
-from function_base import *
-import shape_base
-from shape_base import *
-import twodim_base
-from twodim_base import *
+from __future__ import division, absolute_import, print_function
 
-__all__ = []
+import math
+
+from .function_base import *
+from .shape_base import *
+from .twodim_base import *
+
+__all__ = ['math']
 __all__ += function_base.__all__
 __all__ += shape_base.__all__
 __all__ += twodim_base.__all__
