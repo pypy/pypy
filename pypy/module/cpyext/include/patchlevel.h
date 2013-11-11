62237672ca1e580fa729df98a6e1480dde66b3dd
Armin Rigo <arigo@tunes.org>
2013-11-11 19:32:55 +0100

    Update the version numbers

--- pypy/module/cpyext/include/patchlevel.h
+++ pypy/module/cpyext/include/patchlevel.h
@@ -29,7 +29,7 @@
 #define PY_VERSION		"2.7.3"
 
 /* PyPy version as a string */
-#define PYPY_VERSION "2.2.0-alpha0"
+#define PYPY_VERSION "2.2.0"
 
 /* Subversion Revision number of this file (not of the repository).
  * Empty since Mercurial migration. */
