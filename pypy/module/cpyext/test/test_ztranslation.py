8218d0558e7f0473de5c0da9f915fe65f0c3ee54
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2013-12-12 23:48:16 +0100

    A slighly better way to monkeypatch pyobject.from_ref()

--- pypy/module/cpyext/test/test_ztranslation.py
+++ pypy/module/cpyext/test/test_ztranslation.py
@@ -1,13 +1,10 @@
 from pypy.objspace.fake.checkmodule import checkmodule
 from pypy.module.cpyext import pyobject
 
-def test_cpyext_translates():
+def test_cpyext_translates(monkeypatch):
     def from_ref(space, ref):
         # XXX: avoid 'assert isinstance(w_type, W_TypeObject)' from the
         # original from_ref, just return w_some_obj
         return space.w_object
-    old, pyobject.from_ref = pyobject.from_ref, from_ref
-    try:
-        checkmodule('cpyext', '_ffi')
-    finally:
-        pyobject.from_ref = old
+    monkeypatch.setattr(pyobject, 'from_ref', from_ref)
+    checkmodule('cpyext', '_ffi')
