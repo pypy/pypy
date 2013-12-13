707bd22f6bbca73028dce94848745ceec41990e8
Brian Kearns <bdkearns@gmail.com>
2013-12-13 17:40:14 -0500

    test bin() of numpy scalar

--- pypy/module/micronumpy/test/test_scalar.py
+++ pypy/module/micronumpy/test/test_scalar.py
@@ -26,6 +26,9 @@ class AppTestScalar(BaseNumpyAppTest):
         assert hex(np.int32(11)) == '0xb'
         assert hex(np.float32(11.6)) == '0xb'
         assert hex(np.complex64(11-12j)) == '0xb'
+        assert bin(np.int32(11)) == '0b1011'
+        exc = raises(TypeError, "bin(np.float32(11.6))")
+        assert exc.value.message.find('object cannot be interpreted as an index') != -1
 
     def test_pickle(self):
         from numpypy import dtype, zeros
