b3f2680b39b287dd1347a0ce26a12b405d863ab4
Edd Barrett <vext01@gmail.com>
2013-12-17 16:10:14 +0000

    Backport a test from swithon.
    This is the test that causes issues with term refs in swithon.
    
    Passing here, but perhaps by luck.

--- pypy/module/unipycation/test/test_coreengine.py
+++ pypy/module/unipycation/test/test_coreengine.py
@@ -231,3 +231,22 @@ class AppTestCoreEngine(object):
         import unipycation
         raises(unipycation.PrologError, 'unipycation.CoreEngine(":- a = b.")')
 
+    def test_collect_solutions(self):
+        import unipycation as u
+        e = u.CoreEngine("f(card(5, d)). f(card(6, c)).")
+
+        # f(X).
+        X = u.Var()
+        t = u.CoreTerm("f", [X])
+
+        sols = [ sol[X] for sol in e.query_iter(t, [X]) ]
+
+        assert sols[0].name == "card"
+        assert len(sols[0]) == 2
+        assert sols[0][0] == 5
+        assert sols[0][1] == "d"
+
+        assert sols[1].name == "card"
+        assert len(sols[1]) == 2
+        assert sols[1][0] == 6
+        assert sols[1][1] == "c"
