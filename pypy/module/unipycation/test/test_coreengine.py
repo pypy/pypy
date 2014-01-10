c3f61872986ee1e95f87aa083b165c448ed2b3df
Edd Barrett <vext01@gmail.com>
2014-01-10 14:15:38 +0000

    Move unipycation specific tempfile creation out of shared repo.

--- pypy/module/unipycation/test/test_coreengine.py
+++ pypy/module/unipycation/test/test_coreengine.py
@@ -2,3 +2,10 @@
 from unipycation_shared.tests.base_test_coreengine import BaseTestCoreEngine
 class AppTestCoreEngine(BaseTestCoreEngine):
     spaceconfig = dict(usemodules=('unipycation',))
+
+    def setup_class(cls):
+        import tempfile
+        space = cls.space
+        (fd, fname) = tempfile.mkstemp(prefix="unipycation-")
+        cls.w_fd = space.wrap(fd)
+        cls.w_fname = space.wrap(fname)
