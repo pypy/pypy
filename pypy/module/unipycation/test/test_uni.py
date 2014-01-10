a5c0a4002ed3a7ef4185296b2e6452c41141f503
Edd Barrett <vext01@gmail.com>
2014-01-10 15:57:47 +0000

    Move in pypy specific setup_class

--- pypy/module/unipycation/test/test_uni.py
+++ pypy/module/unipycation/test/test_uni.py
@@ -2,3 +2,9 @@
 from unipycation_shared.tests.base_test_uni import BaseTestUni
 class AppTestUni(BaseTestUni):
     spaceconfig = dict(usemodules=('unipycation',))
+
+    def setup_class(cls):
+        space = cls.space
+        (fd, fname) = tempfile.mkstemp(prefix="unipycation-")
+        cls.w_fd = space.wrap(fd)
+        cls.w_fname = space.wrap(fname)
