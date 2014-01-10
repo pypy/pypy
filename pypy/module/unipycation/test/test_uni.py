6f66107cde3f5614ea844d4471d1c09cef193ac3
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-01-10 18:51:22 +0100

    run the now split out reverse calling tests

--- pypy/module/unipycation/test/test_uni.py
+++ pypy/module/unipycation/test/test_uni.py
@@ -1,10 +1,16 @@
 # Pulled from the shared test repo
-from unipycation_shared.tests.base_test_uni import BaseTestUni
-class AppTestUni(BaseTestUni):
+from unipycation_shared.tests import base_test_uni
+
+class AppTestUni(base_test_uni.BaseTestUni):
     spaceconfig = dict(usemodules=('unipycation',))
 
     def setup_class(cls):
+        import tempfile
         space = cls.space
         (fd, fname) = tempfile.mkstemp(prefix="unipycation-")
         cls.w_fd = space.wrap(fd)
         cls.w_fname = space.wrap(fname)
+
+class AppTestUniRevCall(base_test_uni.BaseTestUniRevCall):
+    spaceconfig = dict(usemodules=('unipycation',))
+
