8a75ac499f5a78fd57abb2c9f9376614a5415870
Edd Barrett <vext01@gmail.com>
2014-01-20 12:51:26 +0000

    Replace current process to ensure build output is correct.
    sh has magic and buffering which suppresses this.

--- translate_unipycation.py
+++ translate_unipycation.py
@@ -5,21 +5,6 @@ import os, os.path, sys
 SCRIPT_PATH = os.path.abspath(__file__)
 SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
 
-try:
-    import pip
-except ImportError:
-    raise ImportError("Please install pip")
-
-# list of (package_name, import_name)
-REQUIRED = [ ("sh", "sh") ]
-for (pkg, mod) in REQUIRED:
-    try:
-        exec("import %s" % mod)
-    except ImportError:
-        print("Installing %s..." % pkg)
-        pip.main(['install', '--user', pkg])
-        exec("import %s" % mod) # should pass this time
-
 if sys.platform.startswith("openbsd"):
     cc = "egcc" # bugs in base compiler. uses loads of memory
 else:
@@ -29,6 +14,7 @@ env = os.environ.copy()
 env["CC"] = cc
 
 os.chdir(os.path.join(SCRIPT_DIR, "pypy", "goal"))
-sh.Command(sys.executable)(
-    "../../rpython/bin/rpython", "-Ojit", "targetpypystandalone.py",
-    _env=env, _out=sys.stdout, _err=sys.stderr)
+
+os.execve("../../rpython/bin/rpython", [
+    "../../rpython/bin/rpython", "-Ojit", "targetpypystandalone.py"],
+    env)
