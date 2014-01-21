f7ce91602640e8d21698d904e3b1d71253830174
Manuel Jacob <unknown>
2014-01-21 14:49:49 +0100

    Remove trailing whitespace.

--- rpython/translator/llvm/common_header.h
+++ rpython/translator/llvm/common_header.h
@@ -1,7 +1,7 @@
 #ifdef _WIN64
    typedef          __int64 Signed;
    typedef unsigned __int64 Unsigned;
-#  define SIGNED_MIN LLONG_MIN 
+#  define SIGNED_MIN LLONG_MIN
 #else
    typedef          long Signed;
    typedef unsigned long Unsigned;
