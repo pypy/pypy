d26675bd383e8e1b980c44e8975c64e5c000c62c
Armin Rigo <arigo@tunes.org>
2014-01-21 16:18:23 +0100

    More jittest-ability.

--- rpython/jit/tl/jittest.py
+++ rpython/jit/tl/jittest.py
@@ -4,15 +4,20 @@ target --jittest.  Feel free to hack it as needed; it is imported
 only after the '---> Checkpoint' fork.
 """
 
+import os
 from rpython import conftest
 from rpython.rtyper.lltypesystem import lltype
 from rpython.rtyper.llinterp import LLInterpreter
 from rpython.rtyper.annlowlevel import llstr
 from rpython.jit.metainterp import warmspot
+from rpython.tool import runsubprocess
+
+os.environ['PYPY_DONT_RUN_SUBPROCESS'] = '1'
+reload(runsubprocess)
 
 
-ARGS = ["--jit", "trace_eagerness=18,threshold=50", "-S",
-        "/home/arigo/pypysrc/32compiled/z.py"]
+ARGS = ["--jit", "threshold=100000,trace_eagerness=100000",
+        "-S", "/home/arigo/pypysrc/32compiled/z.py"]
 
 
 def jittest(driver):
@@ -36,7 +41,8 @@ def apply_jit(policy, interp, graph, CPUClass):
         class MyOpt:
             pass
         conftest.option = MyOpt()
-    conftest.option.view = True
+    conftest.option.view = False
+    conftest.option.viewloops = True   # XXX doesn't seem to work
     LIST = graph.getargs()[0].concretetype
     lst = LIST.TO.ll_newlist(len(ARGS))
     for i, arg in enumerate(ARGS):
