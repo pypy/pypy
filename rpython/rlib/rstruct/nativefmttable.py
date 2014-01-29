b0cb31a1ee7685a36acf2cf9ac733aa12045a520
Philip Jenvey <pjenvey@underboss.org>
2014-01-29 15:07:05 -0800

    kill unnecessary (as of 23a61cfed4a5) bits

--- rpython/rlib/rstruct/nativefmttable.py
+++ rpython/rlib/rstruct/nativefmttable.py
@@ -142,7 +142,7 @@ def setup():
             pack = std.pack_bool
             unpack = std.unpack_bool
         else:
-            pack = std.make_int_packer(size, signed, True)
+            pack = std.make_int_packer(size, signed)
             unpack = std.make_int_unpacker(size, signed)
 
         native_fmttable[fmtchar] = {'size': size,
