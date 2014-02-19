1e4ac8dc33575539bac4a9762bbbfc747dd8c9ec
Philip Jenvey <pjenvey@underboss.org>
2014-02-19 14:55:08 -0800

    put back a more efficient int_BINARY_ADD, cleanup

--- pypy/objspace/std/frame.py
+++ pypy/objspace/std/frame.py
@@ -1,53 +1,53 @@
 """StdObjSpace custom opcode implementations"""
 
-import operator
+from rpython.rlib.rarithmetic import ovfcheck
 
-from rpython.rlib.unroll import unrolling_iterable
-from pypy.interpreter import pyopcode
 from pypy.interpreter.pyframe import PyFrame
-from pypy.interpreter.error import OperationError
-from pypy.objspace.std import intobject
-from pypy.objspace.std.multimethod import FailedToImplement
+from pypy.interpreter.error import oefmt
+from pypy.objspace.std.intobject import W_IntObject
 from pypy.objspace.std.listobject import W_ListObject
 
 
 class BaseFrame(PyFrame):
     """These opcodes are always overridden."""
 
-    def LIST_APPEND(f, oparg, next_instr):
-        w = f.popvalue()
-        v = f.peekvalue(oparg - 1)
+    def LIST_APPEND(self, oparg, next_instr):
+        w = self.popvalue()
+        v = self.peekvalue(oparg - 1)
         if type(v) is W_ListObject:
             v.append(w)
         else:
             raise AssertionError
 
 
-def int_BINARY_ADD(f, oparg, next_instr):
-    w_2 = f.popvalue()
-    w_1 = f.popvalue()
-    if (type(w_1) is intobject.W_IntObject and
-        type(w_2) is intobject.W_IntObject):
-        w_result = w_1.descr_add(f.space, w_2)
-        if f.space.is_w(w_result, f.space.w_NotImplemented):
-            w_result = f.space.add(w_1, w_2)
+def int_BINARY_ADD(self, oparg, next_instr):
+    space = self.space
+    w_2 = self.popvalue()
+    w_1 = self.popvalue()
+    if type(w_1) is W_IntObject and type(w_2) is W_IntObject:
+        try:
+            z = ovfcheck(w_1.intval + w_2.intval)
+        except OverflowError:
+            w_result = w_1.descr_add(space, w_2)
+        else:
+            w_result = space.newint(z)
     else:
-        w_result = f.space.add(w_1, w_2)
-    f.pushvalue(w_result)
+        w_result = space.add(w_1, w_2)
+    self.pushvalue(w_result)
 
 
-def list_BINARY_SUBSCR(f, oparg, next_instr):
-    w_2 = f.popvalue()
-    w_1 = f.popvalue()
-    if type(w_1) is W_ListObject and type(w_2) is intobject.W_IntObject:
+def list_BINARY_SUBSCR(self, oparg, next_instr):
+    space = self.space
+    w_2 = self.popvalue()
+    w_1 = self.popvalue()
+    if type(w_1) is W_ListObject and type(w_2) is W_IntObject:
         try:
             w_result = w_1.getitem(w_2.intval)
         except IndexError:
-            raise OperationError(f.space.w_IndexError,
-                f.space.wrap("list index out of range"))
+            raise oefmt(space.w_IndexError, "list index out of range")
     else:
-        w_result = f.space.getitem(w_1, w_2)
-    f.pushvalue(w_result)
+        w_result = space.getitem(w_1, w_2)
+    self.pushvalue(w_result)
 
 
 def build_frame(space):
