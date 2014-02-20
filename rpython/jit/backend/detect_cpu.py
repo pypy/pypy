a5a93859ee1bb8b012816dfe0c08c607a83d99b2
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2014-02-20 14:00:20 +0000

    Detect PowerPC using uname -r

--- rpython/jit/backend/detect_cpu.py
+++ rpython/jit/backend/detect_cpu.py
@@ -57,6 +57,7 @@ def detect_model_from_host_platform():
             'i86pc': MODEL_X86,    # Solaris/Intel
             'x86': MODEL_X86,      # Apple
             'Power Macintosh': MODEL_PPC_64,
+            'ppc64': MODEL_PPC_64
             'x86_64': MODEL_X86,
             'amd64': MODEL_X86,    # freebsd
             'AMD64': MODEL_X86,    # win64
