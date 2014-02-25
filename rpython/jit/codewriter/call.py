0403ef5fdf860ce79be0009c764387220eee0469
Armin Rigo <arigo@tunes.org>
2014-02-25 14:02:24 +0100

    Replace the crash with a warning for now

--- rpython/jit/codewriter/call.py
+++ rpython/jit/codewriter/call.py
@@ -243,13 +243,15 @@ class CallControl(object):
         #
         # check that the result is really as expected
         if loopinvariant:
-            assert extraeffect == EffectInfo.EF_LOOPINVARIANT, (
+            if extraeffect != EffectInfo.EF_LOOPINVARIANT:
+                from rpython.jit.codewriter.policy import log; log.WARNING(
                 "in operation %r: this calls a _jit_loop_invariant_ function,"
                 " but this contradicts other sources (e.g. it can have random"
                 " effects)" % (op,))
         if elidable:
-            assert extraeffect in (EffectInfo.EF_ELIDABLE_CANNOT_RAISE,
-                                   EffectInfo.EF_ELIDABLE_CAN_RAISE), (
+            if extraeffect not in (EffectInfo.EF_ELIDABLE_CANNOT_RAISE,
+                                   EffectInfo.EF_ELIDABLE_CAN_RAISE):
+                from rpython.jit.codewriter.policy import log; log.WARNING(
                 "in operation %r: this calls an _elidable_function_,"
                 " but this contradicts other sources (e.g. it can have random"
                 " effects)" % (op,))
