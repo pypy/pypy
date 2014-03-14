bd62fb89c6da3f2ee5411f07ebcbd8936345f0a3
Remi Meier <unknown>
2014-03-14 17:07:42 +0100

    don't add stm_read for immutable operations (still very bad)

--- rpython/translator/stm/readbarrier.py
+++ rpython/translator/stm/readbarrier.py
@@ -1,4 +1,4 @@
-from rpython.flowspace.model import SpaceOperation
+from rpython.flowspace.model import SpaceOperation, Constant, Variable
 from rpython.translator.unsimplify import varoftype
 from rpython.rtyper.lltypesystem import lltype
 
@@ -8,6 +8,31 @@ READ_OPS = set(['getfield', 'getarrayitem', 'getinteriorfield', 'raw_load'])
 
 def is_gc_ptr(T):
     return isinstance(T, lltype.Ptr) and T.TO._gckind == 'gc'
+
+def unwraplist(list_v):
+    for v in list_v:
+        if isinstance(v, Constant):
+            yield v.value
+        elif isinstance(v, Variable):
+            yield None    # unknown
+        else:
+            raise AssertionError(v)
+
+def is_immutable(op):
+    if op.opname in ('getfield', 'setfield'):
+        STRUCT = op.args[0].concretetype.TO
+        return STRUCT._immutable_field(op.args[1].value)
+    if op.opname in ('getarrayitem', 'setarrayitem'):
+        ARRAY = op.args[0].concretetype.TO
+        return ARRAY._immutable_field()
+    if op.opname == 'getinteriorfield':
+        OUTER = op.args[0].concretetype.TO
+        return OUTER._immutable_interiorfield(unwraplist(op.args[1:]))
+    if op.opname == 'setinteriorfield':
+        OUTER = op.args[0].concretetype.TO
+        return OUTER._immutable_interiorfield(unwraplist(op.args[1:-1]))
+    if op.opname in ('raw_load', 'raw_store'):
+        return False
 
 
 def insert_stm_read_barrier(transformer, graph):
@@ -19,13 +44,29 @@ def insert_stm_read_barrier(transformer, graph):
     # XXX this can be optimized a lot, but for now we go with the
     # simplest possible solution...
     #
+    gcremovetypeptr = transformer.translator.config.translation.gcremovetypeptr
+
     for block in graph.iterblocks():
         if not block.operations:
             continue
         newops = []
         stm_ignored = False
         for op in block.operations:
-            if op.opname in READ_OPS and is_gc_ptr(op.args[0].concretetype):
+            is_getter = (op.opname in READ_OPS and
+                         op.result.concretetype is not lltype.Void and
+                         is_gc_ptr(op.args[0].concretetype))
+
+            if (gcremovetypeptr and op.opname in ('getfield', 'setfield') and
+                op.args[1].value == 'typeptr' and
+                op.args[0].concretetype.TO._hints.get('typeptr')):
+                # typeptr is always immutable
+                pass
+            elif ((op.opname in ('getarraysize', 'getinteriorarraysize', 'weakref_deref') and
+                  is_gc_ptr(op.args[0].concretetype)) or
+                  (is_getter and is_immutable(op))):
+                # immutable getters
+                pass
+            elif is_getter:
                 if not stm_ignored:
                     v_none = varoftype(lltype.Void)
                     newops.append(SpaceOperation('stm_read',
