fdb38fb8d4727256b827aeb74beeb9c345a8d95a
Armin Rigo <arigo@tunes.org>
2014-03-23 09:29:00 +0100

    Fix (see existing test)

--- rpython/rlib/atomic_ops.py
+++ rpython/rlib/atomic_ops.py
@@ -2,20 +2,14 @@ import py
 from rpython.rtyper.lltypesystem import lltype, llmemory, rffi
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
 
-
-cdir = py.path.local(__file__).join('..', '..', 'translator', 'stm')
-cdir2 = py.path.local(__file__).join('..', '..', 'translator', 'c')
-
 eci = ExternalCompilationInfo(
-    include_dirs = [cdir, cdir2],
     post_include_bits = ['''
-#include "src_stm/atomic_ops.h"
 #define pypy_bool_cas(ptr, old, _new)                \\
-           bool_cas((volatile unsigned long*)(ptr),  \\
+    __sync_bool_compare_and_swap((volatile unsigned long*)(ptr),  \\
                     (unsigned long)(old),            \\
                     (unsigned long)(_new))
 #define pypy_fetch_and_add(ptr, value)                    \\
-           fetch_and_add((volatile unsigned long*)(ptr),  \\
+    __sync_fetch_and_add((volatile unsigned long*)(ptr),  \\
                          (unsigned long)(value))
 '''],
 )
