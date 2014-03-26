8ebb32ed0ec15890781f32ba08bb64d627c797d2
Brian Kearns <bdkearns@gmail.com>
2014-03-26 19:41:57 -0400

    partial backout of bef3dc99bdd7 (broke linux tests)

--- pypy/module/cpyext/test/test_pyerrors.py
+++ pypy/module/cpyext/test/test_pyerrors.py
@@ -158,14 +158,14 @@ class AppTestFetch(AppTestCpythonExtensionBase):
              PyErr_NormalizeException(&type, &val, &tb);
              if (type != PyExc_TypeError)
                  Py_RETURN_FALSE;
-             if (val->ob_type != (_typeobject *)PyExc_TypeError)
+             if (val->ob_type != PyExc_TypeError)
                  Py_RETURN_FALSE;
 
              /* Normalize again */
              PyErr_NormalizeException(&type, &val, &tb);
              if (type != PyExc_TypeError)
                  Py_RETURN_FALSE;
-             if (val->ob_type != (_typeobject *)PyExc_TypeError)
+             if (val->ob_type != PyExc_TypeError)
                  Py_RETURN_FALSE;
 
              PyErr_Restore(type, val, tb);
