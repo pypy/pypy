e03bfdbad48392e1569002d76d2d299b543fea3a
Matti Picus <matti.picus@gmail.com>
2014-03-30 00:26:12 +0300

    fix test

--- pypy/module/test_lib_pypy/test_testcapi.py
+++ pypy/module/test_lib_pypy/test_testcapi.py
@@ -3,14 +3,12 @@ import py, sys
 if '__pypy__' not in sys.builtin_module_names:
     py.test.skip('pypy only test')
 
-from lib_pypy import _testcapi #this should insure _testcapi is built
+from lib_pypy import _testcapi #make sure _testcapi is built
 
 def test_get_hashed_dir():
     import sys
-    script = '''import _testcapi
-            assert 'get_hashed_dir' in dir(_testcapi)
-            return 0
-            '''
+    # This should not compile _testcapi, so the output is empty
+    script = "import _testcapi; assert 'get_hashed_dir' in dir(_testcapi)"
     output = py.process.cmdexec('''"%s" -c "%s"''' %
                              (sys.executable, script))
     assert output == ''
