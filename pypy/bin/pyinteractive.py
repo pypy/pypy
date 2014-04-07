afe3fd10905c04f34752412526128e272ff8e58c
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2014-04-07 17:47:14 +0200

    Use absolute paths in pyinteractive, in case the app does os.chdir()

--- pypy/bin/pyinteractive.py
+++ pypy/bin/pyinteractive.py
@@ -10,7 +10,8 @@ import os
 import sys
 import time
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..', '..')))
 
 from pypy.tool import option
 from pypy.interpreter import main, interactive, error, gateway
