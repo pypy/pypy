34b3ba206c940f256938cae3c906019b21fae97f
Brian Kearns <bdkearns@gmail.com>
2014-04-22 17:34:47 -0400

    fix test_buffer

--- pypy/interpreter/test/test_buffer.py
+++ pypy/interpreter/test/test_buffer.py
@@ -8,18 +8,18 @@ class TestBuffer:
     def test_buffer_w(self):
         space = self.space
         w_hello = space.wrap('hello world')
-        buf = space.buffer_w(w_hello)
+        buf = space.buffer_w(w_hello, space.BUF_SIMPLE)
         assert buf.getlength() == 11
         assert buf.as_str() == 'hello world'
         assert buf.getslice(1, 6, 1, 5) == 'ello '
-        assert space.buffer_w(space.newbuffer(buf)) is buf
+        assert space.buffer_w(space.newbuffer(buf), space.BUF_SIMPLE) is buf
         assert space.bufferstr_w(w_hello) == 'hello world'
-        assert space.bufferstr_w(space.newbuffer(space.buffer_w(w_hello))) == 'hello world'
-        space.raises_w(space.w_TypeError, space.buffer_w, space.wrap(5))
+        assert space.bufferstr_w(space.newbuffer(space.buffer_w(w_hello, space.BUF_SIMPLE))) == 'hello world'
+        space.raises_w(space.w_TypeError, space.buffer_w, space.wrap(5), space.BUF_SIMPLE)
 
     def test_file_write(self):
         space = self.space
-        w_buffer = space.newbuffer(space.buffer_w(space.wrap('hello world')))
+        w_buffer = space.newbuffer(space.buffer_w(space.wrap('hello world'), space.BUF_SIMPLE))
         filename = str(testdir.join('test_file_write'))
         space.appexec([w_buffer, space.wrap(filename)], """(buffer, filename):
             f = open(filename, 'wb')
