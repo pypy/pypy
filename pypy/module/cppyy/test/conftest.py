9fd864ea71a3ff5900374ce88640e1143203bbb3
Wim Lavrijsen <WLavrijsen@lbl.gov>
2014-04-22 10:24:55 -0700

    better handling of tests with different backends

--- pypy/module/cppyy/test/conftest.py
+++ pypy/module/cppyy/test/conftest.py
@@ -3,18 +3,25 @@ import py
 @py.test.mark.tryfirst
 def pytest_runtest_setup(item):
     if py.path.local.sysfind('genreflex') is None:
-        if not item.location[0] in ['test_helper.py', 'test_cppyy.py'] or \
-           (item.location[0] == 'test_cppyy.py' and not 'TestCPPYYImplementation' in item.location[2]):
-            py.test.skip("genreflex is not installed")
+        import pypy.module.cppyy.capi.loadable_capi as lcapi
+        if 'dummy' in lcapi.reflection_library:
+            # run only tests that are covered by the dummy backend and tests
+            # that do not rely on reflex
+            if not item.location[0] in ['test_helper.py', 'test_cppyy.py'] or \
+                (item.location[0] == 'test_cppyy.py' and not 'TestCPPYYImplementation' in item.location[2]):
+                py.test.skip("genreflex is not installed")
 
+def pytest_configure(config):
+    if py.path.local.sysfind('genreflex') is None:
         import pypy.module.cppyy.capi.loadable_capi as lcapi
         try:
             import ctypes
             ctypes.CDLL(lcapi.reflection_library)
         except Exception, e:
+            # build dummy backend (which has reflex info and calls hard-wired)
             import os
             from rpython.translator.tool.cbuild import ExternalCompilationInfo
-            from rpython.translator import platform
+            from rpython.translator.platform import platform
 
             from rpython.rtyper.lltypesystem import rffi
 
@@ -28,12 +35,9 @@ def pytest_runtest_setup(item):
                 use_cpp_linker=True,
             )
 
-            soname = platform.platform.compile(
+            soname = platform.compile(
                 [], eci,
-                outputfilename='libcppyy_backend',
+                outputfilename='libcppyy_dummy_backend',
                 standalone=False)
 
             lcapi.reflection_library = str(soname)
-
-        lcapi.isdummy = True
-
