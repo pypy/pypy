4e947e8885b2a1f018fd86cf925f20a60e85be91
Armin Rigo <arigo@tunes.org>
2014-04-28 19:59:36 +0200

    Jit Fix

--- rpython/rlib/rstm.py
+++ rpython/rlib/rstm.py
@@ -132,6 +132,7 @@ def update_marker_num(odd_num):
 def pop_marker():
     llop.stm_pop_marker(lltype.Void)
 
+@dont_look_inside
 def longest_abort_info():
     state = llop.stm_longest_marker_state(lltype.Signed)
     time = llop.stm_longest_marker_time(lltype.Float)
@@ -139,6 +140,7 @@ def longest_abort_info():
     cother = llop.stm_longest_marker_other(rffi.CCHARP)
     return (state, time, rffi.charp2str(cself), rffi.charp2str(cother))
 
+@dont_look_inside
 def reset_longest_abort_info():
     llop.stm_reset_longest_marker_state(lltype.Void)
 
