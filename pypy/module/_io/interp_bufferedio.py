dfb148fca440f3cabc9267961b75b38fc991a53d
Philip Jenvey <pjenvey@underboss.org>
2014-05-03 15:01:19 -0700

    refix: disallow unicode

--- pypy/module/_io/interp_bufferedio.py
+++ pypy/module/_io/interp_bufferedio.py
@@ -711,7 +711,7 @@ class BufferedMixin:
     def write_w(self, space, w_data):
         self._check_init(space)
         self._check_closed(space, "write to closed file")
-        data = space.getarg_w('s*', w_data).as_str()
+        data = space.bufferstr_w(w_data)
         size = len(data)
 
         with self.lock:
