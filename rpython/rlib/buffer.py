ad9c76f82d1e5f7e682e4f7b3af8f6a8e26ddaba
Brian Kearns <bdkearns@gmail.com>
2014-05-04 12:48:57 -0400

    remove loop from StringBuffer.getslice

--- rpython/rlib/buffer.py
+++ rpython/rlib/buffer.py
@@ -61,7 +61,7 @@ class StringBuffer(Buffer):
         if step == 1:
             assert 0 <= start <= stop
             return self.value[start:stop]
-        return "".join([self.value[start + i*step] for i in xrange(size)])
+        return Buffer.getslice(self, start, stop, step, size)
 
 
 class SubBuffer(Buffer):
