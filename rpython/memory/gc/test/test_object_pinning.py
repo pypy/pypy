c51b6d2625ccda389fdcf77f69798fefa05638de
Gregor Wegberg <code@gregorwegberg.com>
2014-05-14 16:15:38 +0200

    another test rewrite...

--- rpython/memory/gc/test/test_object_pinning.py
+++ rpython/memory/gc/test/test_object_pinning.py
@@ -1,5 +1,5 @@
 import py
-from rpython.rtyper.lltypesystem import lltype, llmemory
+from rpython.rtyper.lltypesystem import lltype, llmemory, llarena
 from rpython.memory.gc.incminimark import IncrementalMiniMarkGC
 from test_direct import BaseDirectGCTest
 
@@ -9,38 +9,6 @@ S.become(lltype.GcStruct('pinning_test_struct',
                          ('next', lltype.Ptr(S))))
 
 class PinningGCTest(BaseDirectGCTest):
-
-    def test_simple_pin(self):
-        ptr = self.malloc(S)
-        ptr.someInt = 100
-        self.stackroots.append(ptr)
-
-        adr = llmemory.cast_ptr_to_adr(ptr)
-        assert self.gc.pin(adr)
-
-        self.gc.collect()
-        
-        assert self.gc.is_in_nursery(adr)
-        assert ptr.someInt == 100
-
-    # XXX not implemented yet
-    def test_pin_referenced_from_stackroot(self):
-        root_ptr = self.malloc(S)
-        next_ptr = self.malloc(S)
-        self.write(root_ptr, 'next', next_ptr)
-        self.stackroots.append(root_ptr)
-        next_ptr.someInt = 100
-
-        next_adr = llmemory.cast_ptr_to_adr(next_ptr)
-        assert self.gc.pin(next_adr)
-
-        self.gc.collect()
-
-        assert self.gc.is_in_nursery(adr)
-        assert next_ptr.someInt == 100
-        root_ptr = self.stackroots[0]
-        assert root_ptr.next == next_ptr
-        
 
     def test_pin_can_move(self):
         # even a pinned object is considered to be movable. Only the caller
@@ -57,12 +25,64 @@ class PinningGCTest(BaseDirectGCTest):
         assert self.gc.pin(adr)
         assert not self.gc.pin(adr)
 
-    # XXX test with multiple mallocs, and only part of them is pinned
-    # XXX test unpin()
+    def test_unpin_not_pinned(self):
+        # this test checks a requirement of the unpin() interface
+        ptr = self.malloc(S)
+        py.test.raises(Exception,
+            self.gc.unpin, llmemory.cast_ptr_to_adr(ptr))
 
+    # XXX test with multiple mallocs, and only part of them is pinned
 
 class TestIncminimark(PinningGCTest):
     from rpython.memory.gc.incminimark import IncrementalMiniMarkGC as GCClass
+
+    def test_simple_pin(self):
+        ptr = self.malloc(S)
+        ptr.someInt = 100
+        self.stackroots.append(ptr)
+
+        adr = llmemory.cast_ptr_to_adr(ptr)
+        assert self.gc.pin(adr)
+
+        self.gc.collect()
+        
+        assert self.gc.is_in_nursery(adr)
+        assert ptr.someInt == 100
+
+    def test_simple_pin_unpin(self):
+        ptr = self.malloc(S)
+        ptr.someInt = 100
+        self.stackroots.append(ptr)
+        adr = llmemory.cast_ptr_to_adr(ptr)
+        # check if pin worked
+        assert self.gc.pin(adr)
+        self.gc.collect()
+        assert self.gc.is_in_nursery(adr)
+        assert ptr.someInt == 100
+        # unpin and check if object is gone from nursery
+        self.gc.unpin(adr)
+        self.gc.collect()
+        py.test.raises(RuntimeError, 'ptr.someInt')
+        ptr_old = self.stackroots[0]
+        assert ptr_old.someInt == 100
+
+    @py.test.mark.xfail(reason="Not implemented yet", run=False)
+    def test_pin_referenced_from_stackroot(self):
+        root_ptr = self.malloc(S)
+        next_ptr = self.malloc(S)
+        self.write(root_ptr, 'next', next_ptr)
+        self.stackroots.append(root_ptr)
+        next_ptr.someInt = 100
+
+        next_adr = llmemory.cast_ptr_to_adr(next_ptr)
+        assert self.gc.pin(next_adr)
+
+        self.gc.collect()
+
+        assert self.gc.is_in_nursery(adr)
+        assert next_ptr.someInt == 100
+        root_ptr = self.stackroots[0]
+        assert root_ptr.next == next_ptr
 
     def test_pin_old(self):
         ptr = self.malloc(S)
