475a51596d822503a770e6e01357cb506ec3fcb7
mattip <matti.picus@gmail.com>
2014-05-15 22:37:09 +0300

    cleanup os.path.isdir by registering a specialcase for windows (ronan)

--- rpython/translator/c/test/test_extfunc.py
+++ rpython/translator/c/test/test_extfunc.py
@@ -1,5 +1,5 @@
 import py
-import os, time, sys, genericpath
+import os, time, sys
 from rpython.tool.udir import udir
 from rpython.rlib.rarithmetic import r_longlong
 from rpython.annotator import model as annmodel
@@ -243,8 +243,6 @@ def test_os_path_exists():
     assert f() == False
 
 def test_os_path_isdir():
-    if sys.platform != 'win32':
-        py.test.skip('use generic.isdir() instead')
     directory = "./."
     def fn():
         return os.path.isdir(directory)
@@ -253,20 +251,6 @@ def test_os_path_isdir():
     directory = "some/random/name"
     def fn():
         return os.path.isdir(directory)
-    f = compile(fn, [])
-    assert f() == False
-
-def test_generic_isdir():
-    # os.path.isdir is not rpython once pywin is installed (win32 specific)
-    # genericpath.isdir is better.
-    directory = "./."
-    def fn():
-        return genericpath.isdir(directory)
-    f = compile(fn, [])
-    assert f() == True
-    directory = "some/random/name"
-    def fn():
-        return genericpath.isdir(directory)
     f = compile(fn, [])
     assert f() == False
 
