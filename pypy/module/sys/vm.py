2a7169481d3c1770b01b167dd81438d305fc0714
mattip <matti.picus@gmail.com>
2014-06-20 16:55:24 +0300

    directly import RawCDLL instead of relying on "import *"

--- pypy/module/sys/vm.py
+++ pypy/module/sys/vm.py
@@ -244,7 +244,8 @@ def _get_dllhandle(space):
     handle = space.fromcache(State).get_pythonapi_handle()
 
     # Make a dll object with it
-    from pypy.module._rawffi.interp_rawffi import W_CDLL, RawCDLL
+    from pypy.module._rawffi.interp_rawffi import W_CDLL
+    from rpython.rlib.clibffi import RawCDLL
     cdll = RawCDLL(handle)
     return space.wrap(W_CDLL(space, "python api", cdll))
 
