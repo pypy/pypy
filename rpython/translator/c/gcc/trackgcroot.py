7861cd75e8f3c0d5f16e5b8c93668c10205af3d4
Armin Rigo <arigo@tunes.org>
2014-06-25 11:37:03 +0200

    Generalize

--- rpython/translator/c/gcc/trackgcroot.py
+++ rpython/translator/c/gcc/trackgcroot.py
@@ -858,15 +858,16 @@ class FunctionGcRootTracker(object):
         return []
 
     def _visit_xchg(self, line):
-        # ignore the 'rpy_fastgil' atomic exchange
-        if 'rpy_fastgil' in line:
-            return []
         # support the format used in VALGRIND_DISCARD_TRANSLATIONS
         # which is to use a marker no-op "xchgl %ebx, %ebx"
         match = self.r_binaryinsn.match(line)
         source = match.group("source")
         target = match.group("target")
         if source == target:
+            return []
+        # ignore the 'rpy_fastgil' atomic exchange, or any locked
+        # atomic exchange at all (involving memory)
+        if not source.startswith('%'):
             return []
         raise UnrecognizedOperation(line)
 
