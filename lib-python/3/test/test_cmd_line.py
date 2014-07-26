46fb421eae2b7e0dcd50b484a2f95a7dcf067a54
Philip Jenvey <pjenvey@underboss.org>
2014-07-26 12:40:52 -0700

    clarify

--- lib-python/3/test/test_cmd_line.py
+++ lib-python/3/test/test_cmd_line.py
@@ -341,7 +341,7 @@ class CmdLineTest(unittest.TestCase):
     def test_hash_randomization(self):
         # Verify that -R enables hash randomization:
         self.verify_valid_flag('-R')
-        if test.support.check_impl_detail(cpython=False):
+        if test.support.check_impl_detail(pypy=True):
             # PyPy doesn't support hash randomization
             return
         hashes = []
