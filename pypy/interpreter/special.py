9113182a74d9090c6c41610b4502cf71975f7394
Philip Jenvey <pjenvey@underboss.org>
2014-07-26 14:40:13 -0700

    fix function identifier clashing breaking translation

--- pypy/interpreter/special.py
+++ pypy/interpreter/special.py
@@ -2,10 +2,20 @@ from pypy.interpreter.baseobjspace import W_Root
 
 
 class Ellipsis(W_Root):
+
+    @staticmethod
+    def descr_new_ellipsis(space, w_type):
+        return space.w_Ellipsis
+
     def descr__repr__(self, space):
         return space.wrap('Ellipsis')
 
 
 class NotImplemented(W_Root):
+
+    @staticmethod
+    def descr_new_notimplemented(space, w_type):
+        return space.w_NotImplemented
+
     def descr__repr__(self, space):
         return space.wrap('NotImplemented')
