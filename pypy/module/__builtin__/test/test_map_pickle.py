16c1b95511840d125607ac8d6385a41519c09f1b
Vasily Kuznetsov <kvas.it@gmail.com>
2014-07-26 16:05:27 +0200

    Added a test for the situation when one of the iterators given to map has been already advanced (suggested by Armin Rigo).

--- pypy/module/__builtin__/test/test_map_pickle.py
+++ pypy/module/__builtin__/test/test_map_pickle.py
@@ -26,3 +26,19 @@ class AppTestMapPickle:
 
         assert list(m1) == list(m1_)
 
+    def test_map2_adv_pickle(self):
+        """If some iterator was advanced, the pickling preserves it."""
+        import pickle
+
+        def pickle_unpickle(obj):
+            d = pickle.dumps(obj)
+            return pickle.loads(d)
+
+        s1 = iter("abc")
+        s2 = iter("defg")
+        next(s2)
+
+        m1 = map(max, s1, s2)
+        m1_ = pickle_unpickle(m1)
+
+        assert list(m1) == list(m1_)
