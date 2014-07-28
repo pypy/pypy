625da503446019b4d0042881a647cc1d2b50300e
Philip Jenvey <pjenvey@underboss.org>
2014-07-27 18:11:53 -0700

    fix space.sys.debug tweak leaking outside of these tests

--- pypy/module/__builtin__/test/test_compile.py
+++ pypy/module/__builtin__/test/test_compile.py
@@ -84,11 +84,18 @@ class C:
 class TestOptimizeO:
     """Test interaction of -O flag and optimize parameter of compile."""
 
+    def setup_method(self, method):
+        space = self.space
+        self._sys_debug = space.sys.debug
+        # imitate -O
+        space.sys.debug = False
+
+    def teardown_method(self, method):
+        self.space.sys.debug = self._sys_debug
+
     def test_O_optmize_0(self):
         """Test that assert is not ignored if -O flag is set but optimize=0."""
         space = self.space
-        space.sys.debug = False  # imitate -O
-
         w_res = space.appexec([], """():
             assert False  # check that our -O imitation hack works
             try:
@@ -103,8 +110,6 @@ class TestOptimizeO:
     def test_O_optimize__1(self):
         """Test that assert is ignored with -O and optimize=-1."""
         space = self.space
-        space.sys.debug = False  # imitate -O
-
         space.appexec([], """():
             exec(compile('assert False', '', 'exec', optimize=-1))
         """)
