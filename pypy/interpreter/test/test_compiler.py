f557a7aad3d096ea02bff391aa7d9c580c775eb9
Martin Matusiak <numerodix@gmail.com>
2014-07-31 21:40:21 +0200

    use helper function to locate the code object

--- pypy/interpreter/test/test_compiler.py
+++ pypy/interpreter/test/test_compiler.py
@@ -718,15 +718,20 @@ with somtehing as stuff:
         from pypy.interpreter.pycode import cpython_code_signature
         from pypy.interpreter.signature import Signature
 
+        def find_func(code):
+            for w_const in code.co_consts_w:
+                if isinstance(w_const, PyCode):
+                    return w_const
+
         snippet = 'def f(a, b, m=1, n=2, **kwargs): pass'
         containing_co = self.compiler.compile(snippet, '<string>', 'single', 0)
-        co = containing_co.co_consts_w[2]
+        co = find_func(containing_co)
         sig = cpython_code_signature(co)
         assert sig == Signature(['a', 'b', 'm', 'n'], None, 'kwargs', [])
 
         snippet = 'def f(a, b, *, m=1, n=2, **kwargs): pass'
         containing_co = self.compiler.compile(snippet, '<string>', 'single', 0)
-        co = containing_co.co_consts_w[4]
+        co = find_func(containing_co)
         sig = cpython_code_signature(co)
         assert sig == Signature(['a', 'b'], None, 'kwargs', ['m', 'n'])
 
