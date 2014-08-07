a4c72c155aac7972849b099860790e82df86b52e
Armin Rigo <arigo@tunes.org>
2014-08-07 17:41:51 +0200

    Kill this test too

--- rpython/rlib/test/test_rstm.py
+++ rpython/rlib/test/test_rstm.py
@@ -4,20 +4,3 @@ from rpython.rlib import rstm
 def test_symbolics():
     assert rstm.adr_nursery_free == rstm.adr_nursery_free
     assert rstm.adr_nursery_free != rstm.adr_nursery_top
-
-def test_tlref_untranslated():
-    class FooBar(object):
-        pass
-    t = rstm.ThreadLocalReference(FooBar)
-    results = []
-    def subthread():
-        x = FooBar()
-        results.append(t.get() is None)
-        t.set(x)
-        results.append(t.get() is x)
-        time.sleep(0.2)
-        results.append(t.get() is x)
-    for i in range(5):
-        thread.start_new_thread(subthread, ())
-    time.sleep(0.5)
-    assert results == [True] * 15
