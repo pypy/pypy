a2bd5197244099072ceb4f091ffcc4bc0df14aee
Philip Jenvey <pjenvey@underboss.org>
2014-08-15 17:58:26 -0700

    py3 compat

--- pypy/interpreter/test/test_generator.py
+++ pypy/interpreter/test/test_generator.py
@@ -288,9 +288,9 @@ res = f()
                 yield 5
                 raise    # should raise "no active exception to re-raise"
         gen = f()
-        gen.next()  # --> 5
+        next(gen)  # --> 5
         try:
-            gen.next()
+            next(gen)
         except TypeError:
             pass
 
