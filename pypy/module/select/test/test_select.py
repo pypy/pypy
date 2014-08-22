135aebf9e073abeb66ad2b345c52c92fa3054608
Philip Jenvey <pjenvey@underboss.org>
2014-08-22 15:55:59 -0700

    move/integrate these into the existing test_select (grafted from 3b2796835762907581e947de99d51e6528027eed)

--- pypy/module/select/test/test_select.py
+++ pypy/module/select/test/test_select.py
@@ -218,6 +218,8 @@ class _AppTestSelect:
             skip("no select.poll() on this platform")
         pollster = select.poll()
         pollster.register(1)
+        raises(OverflowError, pollster.register, 0, -1)
+        raises(OverflowError, pollster.register, 0, 1 << 64)
         exc = raises(OverflowError, pollster.register, 0, 32768) # SHRT_MAX + 1
         assert str(exc.value) == 'signed short integer is greater than maximum'
         exc = raises(OverflowError, pollster.register, 0, -32768 - 1)
@@ -228,6 +230,9 @@ class _AppTestSelect:
         raises(OverflowError, pollster.poll, 4294967296) # UINT_MAX + 1
         exc = raises(TypeError, pollster.poll, '123')
         assert str(exc.value) == 'timeout must be an integer or None'
+
+        raises(OverflowError, pollster.modify, 1, -1)
+        raises(OverflowError, pollster.modify, 1, 1 << 64)
 
 
 class AppTestSelectWithPipes(_AppTestSelect):
