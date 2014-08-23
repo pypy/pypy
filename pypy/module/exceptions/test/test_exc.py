8ae192d367b46861524339a330c76c4e1b2c8328
Alex Gaynor <alex.gaynor@gmail.com>
2014-08-22 20:01:45 -0700

    Added encoding cookies to these files

--- pypy/module/exceptions/test/test_exc.py
+++ pypy/module/exceptions/test/test_exc.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 
 class AppTestExc(object):
     spaceconfig = dict(usemodules=('exceptions',))
--- pypy/module/operator/test/test_operator.py
+++ pypy/module/operator/test/test_operator.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 class AppTestOperator:
     def test_equality(self):
