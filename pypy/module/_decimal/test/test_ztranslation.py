832a8b06522d7b691c3d3d1f3e90acdecd239bdf
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2014-08-24 19:57:48 +0200

    Hack differently, so that unicode.__new__ is analyzed even if previous tests already have loaded the module.

--- pypy/module/_decimal/test/test_ztranslation.py
+++ pypy/module/_decimal/test/test_ztranslation.py
@@ -1,9 +1,11 @@
 
 from pypy.objspace.fake.checkmodule import checkmodule
+from pypy.interpreter.mixedmodule import getinterpevalloader
 from pypy.module._decimal import Module
 
 def test_checkmodule():
-    Module.interpleveldefs['__hack'] = (
-        'interp_decimal.unicodeobject.W_UnicodeObject(u"")')
+    Module.buildloaders()
+    Module.loaders['__hack'] = getinterpevalloader(
+        'pypy.objspace.std', 'unicodeobject.W_UnicodeObject(u"")')
     checkmodule('_decimal')
 
