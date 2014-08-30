fb29f744d89d012a8953f29135ee1b7d18d5fa3c
Armin Rigo <arigo@tunes.org>
2014-08-30 08:29:39 +0200

    Performance tweaks: may return the unicode object passed in as argument if it needs no encoding at all.

--- pypy/module/_pypyjson/interp_encoder.py
+++ pypy/module/_pypyjson/interp_encoder.py
@@ -1,4 +1,6 @@
 from rpython.rlib.rstring import StringBuilder
+from rpython.rlib.runicode import str_decode_utf_8
+from pypy.interpreter import unicodehelper
 
 
 HEX = '0123456789abcdef'
@@ -17,20 +19,39 @@ ESCAPE_BEFORE_SPACE = [ESCAPE_DICT.get(chr(_i), '\\u%04x' % _i)
 def raw_encode_basestring_ascii(space, w_string):
     if space.isinstance_w(w_string, space.w_str):
         s = space.str_w(w_string)
-        for c in s:
+        for i in range(len(s)):
+            c = s[i]
             if c >= ' ' and c <= '~' and c != '"' and c != '\\':
                 pass
             else:
+                first = i
                 break
         else:
             # the input is a string with only non-special ascii chars
             return w_string
 
-        w_string = space.call_method(w_string, 'decode', space.wrap('utf-8'))
+        eh = unicodehelper.decode_error_handler(space)
+        u = str_decode_utf_8(
+                s, len(s), None, final=True, errorhandler=eh,
+                allow_surrogates=True)[0]
+        sb = StringBuilder(len(u))
+        sb.append_slice(s, 0, first)
+    else:
+        u = space.unicode_w(w_string)
+        for i in range(len(u)):
+            c = u[i]
+            if c >= u' ' and c <= u'~' and c != u'"' and c != u'\\':
+                pass
+            else:
+                break
+        else:
+            # the input is a unicode with only non-special ascii chars
+            return w_string
+        sb = StringBuilder(len(u))
+        first = 0
 
-    u = space.unicode_w(w_string)
-    sb = StringBuilder(len(u))
-    for c in u:
+    for i in range(first, len(u)):
+        c = u[i]
         if c <= u'~':
             if c == u'"' or c == u'\\':
                 sb.append('\\')
