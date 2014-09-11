76d6f33ebda1185203980578b488584bfa95331e
Philip Jenvey <pjenvey@underboss.org>
2014-09-11 15:07:33 -0700

    partially backout 9831468e1882: readapt to py3

--- pypy/module/operator/tscmp.py
+++ pypy/module/operator/tscmp.py
@@ -7,14 +7,14 @@ import py
 from rpython.rtyper.lltypesystem import lltype, rffi
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
 
-from pypy.interpreter.error import oefmt
+from pypy.interpreter.error import OperationError, oefmt
 
 cwd = py.path.local(__file__).dirpath()
 eci = ExternalCompilationInfo(
     includes=[cwd.join('tscmp.h')],
     include_dirs=[str(cwd)],
     separate_module_files=[cwd.join('tscmp.c')],
-    export_symbols=['pypy_tscmp', 'pypy_tscmp_wide'])
+    export_symbols=['pypy_tscmp'])
 
 
 def llexternal(*args, **kwargs):
@@ -26,10 +26,6 @@ def llexternal(*args, **kwargs):
 pypy_tscmp = llexternal(
     'pypy_tscmp',
     [rffi.CCHARP, rffi.CCHARP, rffi.LONG, rffi.LONG],
-    rffi.INT)
-pypy_tscmp_wide = llexternal(
-    'pypy_tscmp_wide',
-    [rffi.CWCHARP, rffi.CWCHARP, rffi.LONG, rffi.LONG],
     rffi.INT)
 
 
@@ -47,26 +43,21 @@ def compare_digest(space, w_a, w_b):
     """
     if (space.isinstance_w(w_a, space.w_unicode) and
         space.isinstance_w(w_b, space.w_unicode)):
-        a = space.unicode_w(w_a)
-        b = space.unicode_w(w_b)
-        with rffi.scoped_nonmoving_unicodebuffer(a) as a_buf:
-            with rffi.scoped_nonmoving_unicodebuffer(b) as b_buf:
-                result = pypy_tscmp_wide(a_buf, b_buf, len(a), len(b))
-        return space.wrap(rffi.cast(lltype.Bool, result))
+        try:
+            w_a = space.call_method(w_a, 'encode', space.wrap('ascii'))
+            w_b = space.call_method(w_b, 'encode', space.wrap('ascii'))
+        except OperationError as e:
+            if not e.match(space, space.w_UnicodeEncodeError):
+                raise
+            raise oefmt(space.w_TypeError,
+                        "comparing strings with non-ASCII characters is not "
+                        "supported")
     return compare_digest_buffer(space, w_a, w_b)
 
 
 def compare_digest_buffer(space, w_a, w_b):
-    try:
-        a_buf = w_a.buffer_w(space, space.BUF_SIMPLE)
-        b_buf = w_b.buffer_w(space, space.BUF_SIMPLE)
-    except TypeError:
-        raise oefmt(space.w_TypeError,
-                    "unsupported operand types(s) or combination of types: "
-                    "'%T' and '%T'", w_a, w_b)
-
-    a = a_buf.as_str()
-    b = b_buf.as_str()
+    a = space.bufferstr_w(w_a)
+    b = space.bufferstr_w(w_b)
     with rffi.scoped_nonmovingbuffer(a) as a_buf:
         with rffi.scoped_nonmovingbuffer(b) as b_buf:
             result = pypy_tscmp(a_buf, b_buf, len(a), len(b))
