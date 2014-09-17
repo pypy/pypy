aaa43b0969a8415411b9c3a33873f38c7819c656
Philip Jenvey <pjenvey@underboss.org>
2014-09-17 12:57:57 -0700

    rearrange

--- rpython/rtyper/module/test/test_ll_win32file.py
+++ rpython/rtyper/module/test/test_ll_win32file.py
@@ -8,15 +8,6 @@ if not ll_os._WIN32:
     py.test.skip("requires Windows")
 
 
-def test__getfinalpathname():
-    path = __file__.decode('mbcs')
-    try:
-        result = ll_os._getfinalpathname(path)
-    except ll_os.LLNotImplemented:
-        py.test.skip("_getfinalpathname not supported on this platform")
-    assert os.path.exists(result)
-
-
 def test__getfileinformation():
     with open(__file__) as fp:
         stat = os.fstat(fp.fileno())
@@ -24,3 +15,12 @@ def test__getfileinformation():
     serial, high, low = info
     assert type(serial) in (int, long)
     assert (high << 32) + low == stat.st_ino
+
+
+def test__getfinalpathname():
+    path = __file__.decode('mbcs')
+    try:
+        result = ll_os._getfinalpathname(path)
+    except ll_os.LLNotImplemented:
+        py.test.skip("_getfinalpathname not supported on this platform")
+    assert os.path.exists(result)
