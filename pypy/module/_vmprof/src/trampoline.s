7d4a23ee61595d6ae3aa65a99263d55d06198e86
Antonio Cuni <anto.cuni@gmail.com>
2014-10-25 14:20:59 +0100

    (arigo, antocuni): 1) we need to put TABs instead of spaces, else trackgcroots gets confused; 2) use a normal call but use @PLT, instead of movabs+call *%rax; tell asmgcc that the frame we push is a GCROOT

--- pypy/module/_vmprof/src/trampoline.s
+++ pypy/module/_vmprof/src/trampoline.s
@@ -1,15 +1,17 @@
+// NOTE: you need to use TABs, not spaces!
+        
 	.text
-    .p2align 4,,-1
+	.p2align 4,,-1
 	.globl	pypy_execute_frame_trampoline
 	.type	pypy_execute_frame_trampoline, @function
 pypy_execute_frame_trampoline:
 	.cfi_startproc
-    pushq   %rdi
+	pushq	%rdi
 	.cfi_def_cfa_offset 16
-    movabs  $pypy_pyframe_execute_frame, %rax
-	callq	*%rax
-    popq    %rdi
+	call pypy_pyframe_execute_frame@PLT
+	/* GCROOT 0(%rsp) */
+	popq	%rdi
 	.cfi_def_cfa_offset 8
-    ret
+	ret
 	.cfi_endproc
 	.size	pypy_execute_frame_trampoline, .-pypy_execute_frame_trampoline
