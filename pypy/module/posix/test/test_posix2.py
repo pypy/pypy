dccdbdd5e12661f1bd0fb4b94587bdeee6373c39
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2014-11-01 12:02:11 +0100

    Add a negative test for win32

--- pypy/module/posix/test/test_posix2.py
+++ pypy/module/posix/test/test_posix2.py
@@ -1258,6 +1258,8 @@ class AppTestFdVariants:
         assert os.stat in os.supports_fd  # fstat() is supported everywhere
         if os.name != 'nt':
             assert os.chdir in os.supports_fd  # fchdir()
+        else:
+            assert os.chdir not in os.supports_fd
         if os.name == 'posix':
             assert os.open in os.supports_dir_fd  # openat()
 
