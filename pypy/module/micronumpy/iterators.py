38426e99e7070629738af20f4161d86396519075
Maciej Fijalkowski <fijall@gmail.com>
2014-12-01 15:46:22 +0200

    kill this hack for now - makes matters worse without a deeper thought

--- pypy/module/micronumpy/iterators.py
+++ pypy/module/micronumpy/iterators.py
@@ -155,7 +155,7 @@ class ArrayIter(object):
         index = state.index
         if self.track_index:
             index += 1
-        indices = state.indices[:]
+        indices = state.indices
         offset = state.offset
         if self.contiguous:
             offset += self.array.dtype.elsize
