861ea57d74bfdbff30f58837d0a921539856faa4
Philip Jenvey <pjenvey@underboss.org>
2014-12-07 20:32:50 -0800

    adapt to py3

--- pypy/interpreter/astcompiler/misc.py
+++ pypy/interpreter/astcompiler/misc.py
@@ -110,12 +110,11 @@ def mangle(name, klass):
 
 def intern_if_common_string(space, w_const):
     # only intern identifier-like strings
-    if not space.is_w(space.type(w_const), space.w_str):
-        return w_const
-    for c in space.str_w(w_const):
-        if not (c.isalnum() or c == '_'):
-            return w_const
-    return space.new_interned_w_str(w_const)
+    from pypy.objspace.std.unicodeobject import _isidentifier
+    if (space.is_w(space.type(w_const), space.w_unicode) and
+        _isidentifier(space.unicode_w(w_const))):
+        return space.new_interned_w_str(w_const)
+    return w_const
 
 
 def new_identifier(space, name):
