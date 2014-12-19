d1b936377dd90f187547a734417be345864efc2a
Brian Kearns <bdkearns@gmail.com>
2014-12-18 23:06:23 -0500

    fix test_readline version check

--- lib-python/2.7/test/test_readline.py
+++ lib-python/2.7/test/test_readline.py
@@ -44,7 +44,8 @@ class TestHistoryManipulation (unittest.TestCase):
 
 class TestReadline(unittest.TestCase):
 
-    @unittest.skipIf(readline._READLINE_VERSION < 0x0600
+    @unittest.skipIf(hasattr(readline, '_READLINE_VERSION')
+                     and readline._READLINE_VERSION < 0x0600
                      and "libedit" not in readline.__doc__,
                      "not supported in this library version")
     def test_init(self):
