fa1e99438185fa3355ae02ebf17c57b031f3c186
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2014-12-30 18:02:24 +0100

    Fix compilation, I think

--- pypy/module/faulthandler/faulthandler.h
+++ pypy/module/faulthandler/faulthandler.h
@@ -4,16 +4,16 @@
 #include <signal.h>
 #include "src/precommondefs.h"
 
-RPY_EXPORTED_FOR_TESTS int pypy_faulthandler_read_null(void);
-RPY_EXPORTED_FOR_TESTS void pypy_faulthandler_sigsegv(void);
-RPY_EXPORTED_FOR_TESTS int pypy_faulthandler_sigfpe(void);
-RPY_EXPORTED_FOR_TESTS void pypy_faulthandler_sigabrt();
+RPY_EXTERN int pypy_faulthandler_read_null(void);
+RPY_EXTERN void pypy_faulthandler_sigsegv(void);
+RPY_EXTERN int pypy_faulthandler_sigfpe(void);
+RPY_EXTERN void pypy_faulthandler_sigabrt();
 #ifdef SIGBUS
-RPY_EXPORTED_FOR_TESTS void pypy_faulthandler_sigbus(void);
+RPY_EXTERN void pypy_faulthandler_sigbus(void);
 #endif
 
 #ifdef SIGILL
-RPY_EXPORTED_FOR_TESTS void pypy_faulthandler_sigill(void);
+RPY_EXTERN void pypy_faulthandler_sigill(void);
 #endif
 
 #endif  /* PYPY_FAULTHANDLER_H */
