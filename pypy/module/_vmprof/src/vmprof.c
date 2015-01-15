ffdc86db2c42939924b9d1f4ea2ddb89a0eec75a
Maciej Fijalkowski <fijall@gmail.com>
2015-01-15 18:19:25 +0200

    fix for C legality

--- pypy/module/_vmprof/src/vmprof.c
+++ pypy/module/_vmprof/src/vmprof.c
@@ -105,7 +105,8 @@ static int vmprof_unw_step(unw_cursor_t *cp) {
         vmprof_hacked_unw_cursor_t *cp2 = (vmprof_hacked_unw_cursor_t*)cp;
         void* bp = (void*)sp + sp_offset;
         cp2->sp = bp;
-        cp2->ip = ((void**)(bp - sizeof(void*))[0];
+		bp -= 1;
+        cp2->ip = ((void**)bp)[0];
         // the ret is on the top of the stack minus WORD
         return 1;
     }
