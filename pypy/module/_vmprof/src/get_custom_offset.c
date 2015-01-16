bce4dfb8993e39d74e3fa5b8223d64f10722614d
Maciej Fijalkowski <fijall@gmail.com>
2015-01-16 15:22:13 +0200

    haha

--- pypy/module/_vmprof/src/get_custom_offset.c
+++ pypy/module/_vmprof/src/get_custom_offset.c
@@ -6,7 +6,7 @@ long pypy_jit_stack_depth_at_loc(long);
 static ptrdiff_t vmprof_unw_get_custom_offset(void* ip) {
 	long ip_l = (long)ip;
 
-	if (ip < pypy_jit_start_addr() or ip > pypy_jit_end_addr()) {
+	if (ip < pypy_jit_start_addr() || ip > pypy_jit_end_addr()) {
 		return -1;
 	}
 	return pypy_jit_stack_depth_at_loc(ip);
