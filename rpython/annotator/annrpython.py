dda03933bef56d99d725e469e9e0c38c63a86776
Armin Rigo <arigo@tunes.org>
2015-01-31 14:43:19 +0100

    Use pypystm.stmset() instead of stmidset (which should probably be removed soon).  Also, "try:" instead of crash.

--- rpython/annotator/annrpython.py
+++ rpython/annotator/annrpython.py
@@ -17,7 +17,10 @@ import py
 log = py.log.Producer("annrpython")
 py.log.setconsumer("annrpython", ansi_log)
 
-from transaction import stmidset
+try:
+    from pypystm import stmset
+except ImportError:
+    stmset = set
 
 
 class RPythonAnnotator(object):
@@ -38,7 +41,7 @@ class RPythonAnnotator(object):
         self.added_blocks = None # see processblock() below
         self.links_followed = {} # set of links that have ever been followed
         self.notify = {}        # {block: {positions-to-reflow-from-when-done}}
-        self.fixed_graphs = stmidset()  # set of graphs not to annotate again
+        self.fixed_graphs = stmset()  # set of graphs not to annotate again
         self.blocked_blocks = {} # set of {blocked_block: (graph, index)}
         # --- the following information is recorded for debugging ---
         self.blocked_graphs = {} # set of graphs that have blocked blocks
