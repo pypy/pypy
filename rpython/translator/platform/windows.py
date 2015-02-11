8de4703940b782617a2b374a4787d2e5e76becbd
Armin Rigo <arigo@tunes.org>
2015-02-11 18:55:02 +0100

    Revert too much changes that were done in b782b42ddc67: we still need to say "-Wl,--export-dynamic" in the Makefile, otherwise no symbol is exported if we're "--no-shared".

--- rpython/translator/platform/windows.py
+++ rpython/translator/platform/windows.py
@@ -271,6 +271,7 @@ class MsvcPlatform(Platform):
         linkflags = list(self.link_flags)
         if shared:
             linkflags = self._args_for_shared(linkflags)
+        linkflags += self._exportsymbols_link_flags()
         # Make sure different functions end up at different addresses!
         # This is required for the JIT.
         linkflags.append('/opt:noicf')
