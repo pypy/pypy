f65805dde470fd08a00b491207756d55e547b86e
mattip <matti.picus@gmail.com>
2015-02-13 09:04:08 +0200

    fix cffi versioning

--- lib_pypy/cffi/__init__.py
+++ lib_pypy/cffi/__init__.py
@@ -4,7 +4,7 @@ __all__ = ['FFI', 'VerificationError', 'VerificationMissing', 'CDefError',
 from .api import FFI, CDefError, FFIError
 from .ffiplatform import VerificationError, VerificationMissing
 
-__version__ = "0.8.6"
+__version__ = "0.8.6+"
 __version_info__ = (0, 8, 6)
 
 # The verifier module file names are based on the CRC32 of a string that
