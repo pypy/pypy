2b0fac8566ded7aa695978660d9c090cbd6ebe46
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2015-03-10 23:03:14 +0100

    Fix test_ztranslation of _io module

--- pypy/module/_io/interp_bufferedio.py
+++ pypy/module/_io/interp_bufferedio.py
@@ -306,23 +306,22 @@ class BufferedMixin:
         with self.lock:
             if self._closed(space):
                 return
-        flush_exception = None
+        w_flush_exception = None
         try:
             space.call_method(self, "flush")
-        except OperationError as flush_exception:
-            pass
+        except OperationError as e:
+            w_flush_exception = e.get_w_value(space)
+            raise
         finally:
             with self.lock:
                 try:
                     space.call_method(self.w_raw, "close")
                 except OperationError as e:
-                    if flush_exception:
+                    if w_flush_exception:
                         space.setattr(e.get_w_value(space),
                                       space.wrap('__context__'),
-                                      flush_exception.get_w_value(space))
+                                      w_flush_exception)
                     raise
-            if flush_exception:
-                raise
 
     def _dealloc_warn_w(self, space, w_source):
         space.call_method(self.w_raw, "_dealloc_warn", w_source)
