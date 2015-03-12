1f4dae86843e8ca31b88c78d1a004ff958424493
Armin Rigo <arigo@tunes.org>
2015-03-12 11:29:21 +0100

    Fixes, untested

--- pypy/module/_vmprof/src/get_custom_offset.c
+++ pypy/module/_vmprof/src/get_custom_offset.c
@@ -1,11 +1,11 @@
 
-long pypy_jit_start_addr();
-long pypy_jit_end_addr();
-long pypy_jit_stack_depth_at_loc(long);
-void *pypy_find_codemap_at_addr(long);
-long pypy_yield_codemap_at_addr(void *, long, long *);
-
 extern volatile int pypy_codemap_currently_invalid;
+
+void *pypy_find_codemap_at_addr(long addr);
+long pypy_yield_codemap_at_addr(void *codemap_raw, long addr,
+                                long *current_pos_addr);
+long pypy_jit_stack_depth_at_loc(long loc);
+
 
 void vmprof_set_tramp_range(void* start, void* end)
 {
@@ -18,11 +18,7 @@ int custom_sanity_check()
 
 static ptrdiff_t vmprof_unw_get_custom_offset(void* ip, unw_cursor_t *cp) {
 	intptr_t ip_l = (intptr_t)ip;
-
-	if (ip_l < pypy_jit_start_addr() || ip_l > pypy_jit_end_addr()) {
-		return -1;
-	}
-	return (void*)pypy_jit_stack_depth_at_loc(ip_l);
+	return pypy_jit_stack_depth_at_loc(ip_l);
 }
 
 static long vmprof_write_header_for_jit_addr(void **result, long n,
