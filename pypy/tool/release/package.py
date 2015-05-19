0347331fc48fcaf74c4db5a33808a90d41212748
Armin Rigo <arigo@tunes.org>
2015-05-19 21:26:22 +0200

    audioop: use an out-of-line module

--- pypy/tool/release/package.py
+++ pypy/tool/release/package.py
@@ -53,7 +53,7 @@ def fix_permissions(dirname):
 def create_cffi_import_libraries(pypy_c, options, basedir):
     shutil.rmtree(str(basedir.join('lib_pypy', '__pycache__')),
                   ignore_errors=True)
-    modules = ['_sqlite3', 'audioop']
+    modules = ['_sqlite3', '_audioop_build.py']
     if not sys.platform == 'win32':
         modules += ['_curses', 'syslog', '_gdbm_build.py']
     if not options.no_tk:
