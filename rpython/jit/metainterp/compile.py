43fd9392af51be735b88b1a98e4b0b00ec053c66
Maciej Fijalkowski <fijall@gmail.com>
2015-06-05 18:57:01 +0200

    an attempt to disable unrolling if we hit a certain threshold

--- rpython/jit/metainterp/compile.py
+++ rpython/jit/metainterp/compile.py
@@ -103,6 +103,35 @@ def record_loop_or_bridge(metainterp_sd, loop):
 
 # ____________________________________________________________
 
+
+def disable_unrolling_if_loop_too_long(loop, metainterp):
+    threshold = metainterp.jitdriver_sd.warmstate.disable_unrolling_threshold
+    if len(loop.operations) < threshold:
+        return False
+    # now we need to patch label, like in simplify.py
+    last_op = loop.operations[-1]
+    descr = last_op.getdescr()
+    assert isinstance(descr, TargetToken)
+    descr = descr.targeting_jitcell_token
+    assert isinstance (descr, JitCellToken)
+    last_op = last_op.copy_and_change(rop.JUMP)
+    if not descr.target_tokens:
+        # fish the first label
+        for op in loop.operations:
+            if op.getopnum() == rop.LABEL:
+                target_token = op.getdescr()
+                break
+        else:
+            assert False, "can't find a label"
+        assert isinstance(target_token, TargetToken)
+        assert target_token.targeting_jitcell_token is descr
+        last_op.setdescr(target_token)
+    else:
+        assert len(descr.target_tokens) == 1
+        last_op.setdescr(descr.target_tokens[0])
+    loop.operations[-1] = last_op
+    return True
+
 def compile_loop(metainterp, greenkey, start,
                  inputargs, jumpargs,
                  full_preamble_needed=True,
@@ -148,27 +177,28 @@ def compile_loop(metainterp, greenkey, start,
     if part.quasi_immutable_deps:
         loop.quasi_immutable_deps.update(part.quasi_immutable_deps)
     if part.operations[-1].getopnum() == rop.LABEL:
-        inliner = Inliner(inputargs, jumpargs)
-        part.quasi_immutable_deps = None
-        part.operations = [part.operations[-1]] + \
-                          [inliner.inline_op(h_ops[i]) for i in range(start, len(h_ops))] + \
-                          [ResOperation(rop.JUMP, [inliner.inline_arg(a) for a in jumpargs],
-                                        None, descr=jitcell_token)]
-        target_token = part.operations[0].getdescr()
-        assert isinstance(target_token, TargetToken)
-        all_target_tokens.append(target_token)
-        inputargs = jumpargs
-        jumpargs = part.operations[-1].getarglist()
+        if not disable_unrolling_if_loop_too_long(part, metainterp):
+            inliner = Inliner(inputargs, jumpargs)
+            part.quasi_immutable_deps = None
+            part.operations = [part.operations[-1]] + \
+                              [inliner.inline_op(h_ops[i]) for i in range(start, len(h_ops))] + \
+                              [ResOperation(rop.JUMP, [inliner.inline_arg(a) for a in jumpargs],
+                                            None, descr=jitcell_token)]
+            target_token = part.operations[0].getdescr()
+            assert isinstance(target_token, TargetToken)
+            all_target_tokens.append(target_token)
+            inputargs = jumpargs
+            jumpargs = part.operations[-1].getarglist()
 
-        try:
-            optimize_trace(metainterp_sd, jitdriver_sd, part, enable_opts,
-                           start_state=start_state, export_state=False)
-        except InvalidLoop:
-            return None
+            try:
+                optimize_trace(metainterp_sd, jitdriver_sd, part, enable_opts,
+                               start_state=start_state, export_state=False)
+            except InvalidLoop:
+                return None
 
-        loop.operations = loop.operations[:-1] + part.operations
-        if part.quasi_immutable_deps:
-            loop.quasi_immutable_deps.update(part.quasi_immutable_deps)
+            loop.operations = loop.operations[:-1] + part.operations
+            if part.quasi_immutable_deps:
+                loop.quasi_immutable_deps.update(part.quasi_immutable_deps)
     assert part.operations[-1].getopnum() != rop.LABEL
 
     if not loop.quasi_immutable_deps:
