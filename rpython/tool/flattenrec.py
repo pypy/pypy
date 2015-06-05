325b978378927f4a9a11ea857871ecb40963a871
Armin Rigo <arigo@tunes.org>
2015-06-05 10:36:11 +0200

    Add and use a helper module that delays recursive calls, to turn them non-recursive.

---
+++ rpython/tool/flattenrec.py
@@ -0,0 +1,25 @@
+"""
+A general way to flatten deeply recursive algorithms by delaying some
+parts until later.
+"""
+
+
+class FlattenRecursion(object):
+
+    def __init__(self):
+        self.later = None
+
+    def __call__(self, func, *args, **kwds):
+        """Call func(*args, **kwds), either now, or, if we're recursing,
+        then the call will be done later by the first level.
+        """
+        if self.later is not None:
+            self.later.append((func, args, kwds))
+        else:
+            self.later = lst = []
+            try:
+                func(*args, **kwds)
+                for func, args, kwds in lst:
+                    func(*args, **kwds)
+            finally:
+                self.later = None
