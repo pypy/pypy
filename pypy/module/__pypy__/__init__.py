36f4885b1d30afb761ad4c226514ca957e64e388
Maciej Fijalkowski <fijall@gmail.com>
2015-06-05 16:57:43 +0200

    write a comment

--- pypy/module/__pypy__/__init__.py
+++ pypy/module/__pypy__/__init__.py
@@ -110,6 +110,10 @@ class Module(MixedModule):
                                      'interp_magic.mapdict_cache_counter')
         PYC_MAGIC = get_pyc_magic(self.space)
         self.extra_interpdef('PYC_MAGIC', 'space.wrap(%d)' % PYC_MAGIC)
+        # XXX
+        # the following code prevents --fork-before=pyjitpl from working,
+        # proper fix would be to use some llop that is only rendered by the
+        # JIT
         #
         #try:
         #    from rpython.jit.backend import detect_cpu
