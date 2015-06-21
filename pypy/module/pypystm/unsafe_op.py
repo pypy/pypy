f563dcac14e30fd0745fafc1faccb7a67d85857b
Armin Rigo <arigo@tunes.org>
2015-06-21 22:27:12 +0100

    translation fix

--- pypy/module/pypystm/unsafe_op.py
+++ pypy/module/pypystm/unsafe_op.py
@@ -1,13 +1,18 @@
 from pypy.interpreter.gateway import unwrap_spec
 from pypy.module._cffi_backend import cdataobj
 from rpython.rlib.rstm import stm_ignored
+from rpython.rlib.jit import dont_look_inside
 from rpython.rtyper.lltypesystem import rffi
 
+
+@dont_look_inside
+def unsafe_write(ptr, value):
+    with stm_ignored:
+        ptr[0] = value
 
 @unwrap_spec(w_cdata=cdataobj.W_CData, index=int, value='c_int')
 def unsafe_write_int32(space, w_cdata, index, value):
     with w_cdata as ptr:
         ptr = rffi.cast(rffi.INTP, rffi.ptradd(ptr, index * 4))
         value = rffi.cast(rffi.INT, value)
-        with stm_ignored:
-            ptr[0] = value
+        unsafe_write(ptr, value)
