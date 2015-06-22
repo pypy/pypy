93af46fd4ae1181fc3df35f965c33c28dc7a936b
Armin Rigo <arigo@tunes.org>
2015-06-22 10:46:16 +0200

    Use 'stm_dont_track_raw_accesses' instead of 'stm_ignore' here, which is better supported by the JIT

--- pypy/module/pypystm/unsafe_op.py
+++ pypy/module/pypystm/unsafe_op.py
@@ -1,18 +1,15 @@
 from pypy.interpreter.gateway import unwrap_spec
 from pypy.module._cffi_backend import cdataobj
-from rpython.rlib.rstm import stm_ignored
-from rpython.rlib.jit import dont_look_inside
-from rpython.rtyper.lltypesystem import rffi
+from rpython.rtyper.lltypesystem import lltype, rffi
 
 
-@dont_look_inside
-def unsafe_write(ptr, value):
-    with stm_ignored:
-        ptr[0] = value
+UNSAFE_INT = lltype.Struct('UNSAFE_INT', ('x', rffi.INT),
+                           hints = {'stm_dont_track_raw_accesses': True})
+UNSAFE_INT_P = lltype.Ptr(UNSAFE_INT)
+
 
 @unwrap_spec(w_cdata=cdataobj.W_CData, index=int, value='c_int')
 def unsafe_write_int32(space, w_cdata, index, value):
     with w_cdata as ptr:
-        ptr = rffi.cast(rffi.INTP, rffi.ptradd(ptr, index * 4))
-        value = rffi.cast(rffi.INT, value)
-        unsafe_write(ptr, value)
+        ptr = rffi.cast(UNSAFE_INT_P, rffi.ptradd(ptr, index * 4))
+        ptr.x = rffi.cast(rffi.INT, value)
