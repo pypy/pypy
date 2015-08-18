9632310f5dd14d1fb450dd874a2ca12413d1a7f9
Richard Plangger <rich@pasra.at>
2015-08-18 13:14:10 +0200

    vec_params in interp_jit.py must be a separate case to wrap it in a w_str

--- pypy/module/pypyjit/interp_jit.py
+++ pypy/module/pypyjit/interp_jit.py
@@ -137,6 +137,8 @@ def set_param(space, __args__):
     for key, w_value in kwds_w.items():
         if key == 'enable_opts':
             jit.set_param(None, 'enable_opts', space.str_w(w_value))
+        elif key == 'vec_params':
+            jit.set_param(None, 'vec_params', space.str_w(w_value))
         else:
             intval = space.int_w(w_value)
             for name, _ in unroll_parameters:
