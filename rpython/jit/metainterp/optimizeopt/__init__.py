c10606a5ec56975cb0b19e881f14c07b8e121b1c
Maciej Fijalkowski <fijall@gmail.com>
2015-09-08 14:56:58 +0200

    one small fix

--- rpython/jit/metainterp/optimizeopt/__init__.py
+++ rpython/jit/metainterp/optimizeopt/__init__.py
@@ -33,7 +33,8 @@ assert ENABLE_ALL_OPTS == ALL_OPTS_NAMES, (
 def build_opt_chain(metainterp_sd, enable_opts):
     optimizations = []
     unroll = 'unroll' in enable_opts    # 'enable_opts' is normally a dict
-    if not metainterp_sd.cpu.supports_guard_gc_type:
+    if (metainterp_sd.cpu is not None and
+        not metainterp_sd.cpu.supports_guard_gc_type):
         unroll = False
     for name, opt in unroll_all_opts:
         if name in enable_opts:
