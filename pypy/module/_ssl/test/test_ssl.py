bdb31d958eb80a27f29fa49e54699fcb97d92f8a
Edd Barrett <vext01@gmail.com>
2015-09-17 17:46:56 +0100

    Fix an SSL test when LibreSSL is used.

--- pypy/module/_ssl/test/test_ssl.py
+++ pypy/module/_ssl/test/test_ssl.py
@@ -36,7 +36,8 @@ class AppTestSSL:
         assert isinstance(_ssl.OPENSSL_VERSION_INFO, tuple)
         assert len(_ssl.OPENSSL_VERSION_INFO) == 5
         assert isinstance(_ssl.OPENSSL_VERSION, str)
-        assert 'openssl' in _ssl.OPENSSL_VERSION.lower()
+        lower_version = _ssl.OPENSSL_VERSION.lower()
+        assert 'openssl' in lower_version or "libressl" in lower_version
 
         assert isinstance(_ssl.ALERT_DESCRIPTION_ACCESS_DENIED, int)
 
