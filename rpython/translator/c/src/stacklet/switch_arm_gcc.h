77f5a7f68fe651b691ce3210a0c380ae580d45f1
Armin Rigo <arigo@tunes.org>
2015-10-19 19:48:46 +0200

    Pff pff pff what a mess

--- rpython/translator/c/src/stacklet/switch_arm_gcc.h
+++ rpython/translator/c/src/stacklet/switch_arm_gcc.h
@@ -57,7 +57,20 @@ static void *slp_switch(void *(*save_state)(void*, void*),
       [save_state]"r"(save_state),
       [extra]"r"(extra)
     : "r0", "r1", "r4", "r5", "r6", "r12", "lr",
-      "memory", "cc", "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7"
+      "memory", "cc"
+#ifndef __SOFTFP__
+      , "d0", "d1", "d2",  "d3",  "d4",  "d5",  "d6",  "d7"
+      , "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15"
+/* messsssssssssssss quite unsure it is the correct way */
+# if defined(__TARGET_FPU_SOFTVFP_VFPV3) || \
+     defined(__TARGET_FPU_SOFTVFP_VFPV3_FP16) || \
+     defined(__TARGET_FPU_VFPV3) || \
+     defined(__TARGET_FPU_VFPV3_FP16) || \
+     defined(__TARGET_FPU_VFPV4)
+      , "d16", "d17", "d18", "d19", "d20", "d21", "d22", "d23"
+      , "d24", "d25", "d26", "d27", "d28", "d29", "d30", "d31"
+# endif
+#endif
   );
   return result;
 }
