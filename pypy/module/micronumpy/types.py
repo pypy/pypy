34ee369d120b31e3e5ba0c01a54b3819f755be8c
mattip <matti.picus@gmail.com>
2015-11-03 19:42:57 +0200

    remove extra quotes in conversion from object dtype to unicode, continuation of cdb5f895892c

--- pypy/module/micronumpy/types.py
+++ pypy/module/micronumpy/types.py
@@ -2221,7 +2221,10 @@ class UnicodeType(FlexibleType):
     def coerce(self, space, dtype, w_item):
         if isinstance(w_item, boxes.W_UnicodeBox):
             return w_item
-        value = space.unicode_w(space.unicode_from_object(w_item))
+        if isinstance(w_item, boxes.W_ObjectBox):
+            value = space.unicode_w(space.unicode_from_object(w_item.w_obj))
+        else:
+            value = space.unicode_w(space.unicode_from_object(w_item))
         return boxes.W_UnicodeBox(value)
 
     def store(self, arr, i, offset, box, native):
