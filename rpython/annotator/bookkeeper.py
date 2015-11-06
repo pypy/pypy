d9798cc6216724778686944d1b03b141a9499e1a
Ronan Lamy <ronan.lamy@gmail.com>
2015-11-06 19:07:23 +0000

    backout 26259e6794f : not useful and caused weird issues in normalizecalls.py

--- rpython/annotator/bookkeeper.py
+++ rpython/annotator/bookkeeper.py
@@ -360,7 +360,7 @@ class Bookkeeper(object):
                 if pyobj.__module__ == '__builtin__': # avoid making classdefs for builtin types
                     result = self.getfrozen(pyobj)
                 else:
-                    return self._new_classdesc(pyobj)
+                    result = ClassDesc(self, pyobj)
             elif isinstance(pyobj, types.MethodType):
                 if pyobj.im_self is None:   # unbound
                     return self.getdesc(pyobj.im_func)
@@ -416,12 +416,6 @@ class Bookkeeper(object):
 
     def valueoftype(self, t):
         return annotationoftype(t, self)
-
-    def _new_classdesc(self, pycls):
-        result = ClassDesc(self, pycls)
-        self.descs[pycls] = result
-        result._init_classdef()
-        return result
 
     def get_classpbc_attr_families(self, attrname):
         """Return the UnionFind for the ClassAttrFamilies corresponding to
