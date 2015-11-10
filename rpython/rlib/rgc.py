9f192fcd5a7ecd08d4c8df4b4fbd6eb07cf6c325
Ronan Lamy <ronan.lamy@gmail.com>
2015-11-10 19:43:06 +0000

    Avoid unnecessary dependency on py.test

--- rpython/rlib/rgc.py
+++ rpython/rlib/rgc.py
@@ -46,7 +46,7 @@ def pin(obj):
     """
     _pinned_objects.append(obj)
     return True
-        
+
 
 class PinEntry(ExtRegistryEntry):
     _about_ = pin
@@ -533,12 +533,8 @@ _ffi_cache = None
 def _fetch_ffi():
     global _ffi_cache
     if _ffi_cache is None:
-        try:
-            import _cffi_backend
-            _ffi_cache = _cffi_backend.FFI()
-        except (ImportError, AttributeError):
-            import py
-            py.test.skip("need CFFI >= 1.0")
+        import _cffi_backend
+        _ffi_cache = _cffi_backend.FFI()
     return _ffi_cache
 
 @jit.dont_look_inside
@@ -816,7 +812,7 @@ def clear_gcflag_extra(fromlist):
             pending.extend(get_rpy_referents(gcref))
 
 all_typeids = {}
-        
+
 def get_typeid(obj):
     raise Exception("does not work untranslated")
 
