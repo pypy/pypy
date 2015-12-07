4be209df7fedd48ee601828a0dc1f32eaf1396bd
Armin Rigo <arigo@tunes.org>
2015-12-07 20:19:59 +0100

    in-progress

--- pypy/module/_cffi_backend/cffi1_module.py
+++ pypy/module/_cffi_backend/cffi1_module.py
@@ -1,6 +1,7 @@
 from rpython.rtyper.lltypesystem import lltype, rffi
+from rpython.rlib.entrypoint import entrypoint
 
-from pypy.interpreter.error import oefmt
+from pypy.interpreter.error import OperationError, oefmt
 from pypy.interpreter.module import Module
 from pypy.module._cffi_backend import parse_c_type
 from pypy.module._cffi_backend.ffi_obj import W_FFIObject
@@ -12,14 +13,14 @@ VERSION_MAX    = 0x26FF
 
 VERSION_EXPORT = 0x0A03
 
-initfunctype = lltype.Ptr(lltype.FuncType([rffi.VOIDPP], lltype.Void))
+INITFUNCPTR = lltype.Ptr(lltype.FuncType([rffi.VOIDPP], lltype.Void))
 
 
 def load_cffi1_module(space, name, path, initptr):
     # This is called from pypy.module.cpyext.api.load_extension_module()
     from pypy.module._cffi_backend.call_python import get_ll_cffi_call_python
 
-    initfunc = rffi.cast(initfunctype, initptr)
+    initfunc = rffi.cast(INITFUNCPTR, initptr)
     with lltype.scoped_alloc(rffi.VOIDPP.TO, 16, zero=True) as p:
         p[0] = rffi.cast(rffi.VOIDP, VERSION_EXPORT)
         p[1] = rffi.cast(rffi.VOIDP, get_ll_cffi_call_python())
@@ -38,9 +39,74 @@ def load_cffi1_module(space, name, path, initptr):
 
     w_name = space.wrap(name)
     module = Module(space, w_name)
-    module.setdictvalue(space, '__file__', space.wrap(path))
+    if path is not None:
+        module.setdictvalue(space, '__file__', space.wrap(path))
     module.setdictvalue(space, 'ffi', space.wrap(ffi))
     module.setdictvalue(space, 'lib', space.wrap(lib))
     w_modules_dict = space.sys.get('modules')
     space.setitem(w_modules_dict, w_name, space.wrap(module))
     space.setitem(w_modules_dict, space.wrap(name + '.lib'), space.wrap(lib))
+    return module
+
+
+# ____________________________________________________________
+
+
+EMBED_VERSION_MIN    = 0xB011
+EMBED_VERSION_MAX    = 0xB0FF
+
+STDERR = 2
+INITSTRUCTPTR = lltype.Ptr(lltype.Struct('CFFI_INIT',
+                                         ('name', rffi.CCHARP),
+                                         ('func', rffi.VOIDP),
+                                         ('code', rffi.CCHARP)))
+
+def load_embedded_cffi_module(space, version, init_struct):
+    name = rffi.charp2str(init_struct.name)
+    if not (VERSION_MIN <= version <= VERSION_MAX):
+        raise oefmt(space.w_ImportError,
+            "cffi *embedded* module '%s' has unknown version %s",
+            name, hex(version))
+    module = load_cffi1_module(space, name, None, init_struct.func)
+    #
+    code = rffi.charp2str(init_struct.code)
+    compiler = space.createcompiler()
+    pycode = compiler.compile(code, "<init code for '%s'>" % name,
+                              'exec', 0)
+    w_globals = module.getdict(space)
+    space.call_method(w_globals, "setdefault", "__builtins__",
+                      self.get_builtin())
+    pycode.exec_code(self, w_globals, w_globals)
+
+
+class Global:
+    pass
+glob = Global()
+
+@entrypoint('main', [rffi.INT, rffi.VOIDP],
+            c_name='_pypy_init_embedded_cffi_module')
+def _pypy_init_embedded_cffi_module(version, init_struct):
+    name = "?"
+    try:
+        init_struct = rffi.cast(INITSTRUCTPTR, init_struct)
+        name = rffi.charp2str(init_struct.name)
+        version = rffi.cast(lltype.Signed, version)
+        try:
+            load_embedded_cffi_module(glob.space, version, init_struct)
+            res = 0
+        except OperationError, operr:
+            operr.write_unraisable(glob.space, "initialization of '%s'" % name,
+                                   with_traceback=True)
+            res = -1
+    except Exception, e:
+        # oups! last-level attempt to recover.
+        try:
+            os.write(STDERR, "From initialization of '")
+            os.write(STDERR, name)
+            os.write(STDERR, "':\n")
+            os.write(STDERR, str(e))
+            os.write(STDERR, "\n")
+        except:
+            pass
+        res = -1
+    return rffi.cast(rffi.INT, res)
