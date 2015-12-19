db03ccbffa7edb599e5270f4c985f84fe67a656a
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-12-19 11:35:22 +0100

    Minimize importing

--- pypy/module/micronumpy/test/test_ndarray.py
+++ pypy/module/micronumpy/test/test_ndarray.py
@@ -3252,8 +3252,8 @@ class AppTestMultiDim(BaseNumpyAppTest):
         assert array(x, copy=True) is not x
 
     def test_issue_1674(self):
-        import numpy as np
-        data = np.arange(15).reshape(3, 5) * 1.0
+        from numpy import arange
+        data = arange(15).reshape(3, 5) * 1.0
         # array([[  0.,   1.,   2.,   3.,   4.],
         #        [  5.,   6.,   7.,   8.,   9.],
         #        [ 10.,  11.,  12.,  13.,  14.]])
