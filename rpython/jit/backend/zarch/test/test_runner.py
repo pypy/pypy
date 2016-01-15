b9b9d23b83b1f7b2fd6bd87b3d1e150fde5ce8fc
Richard Plangger <planrichi@gmail.com>
2016-01-15 09:30:14 +0100

    removed a debug test case from yesterdays bughunt

--- rpython/jit/backend/zarch/test/test_runner.py
+++ rpython/jit/backend/zarch/test/test_runner.py
@@ -28,36 +28,3 @@ class TestZARCH(LLtypeBackendTest):
     # realloc frame takes the most space (from just after larl, to lay)
     bridge_loop_instructions = "larl; lg; cgfi; je; lghi; stg; " \
                                "lay; lgfi;( iihf;)? lgfi;( iihf;)? basr; lay; lg; br;$"
-
-    def test_multiple_arguments(self):
-        from rpython.rtyper.annlowlevel import llhelper
-        from rpython.jit.metainterp.typesystem import deref
-        from rpython.rlib.jit_libffi import types
-        from rpython.jit.codewriter.effectinfo import EffectInfo
-        from rpython.rlib.rarithmetic import intmask
-
-        def func_int(a, b, c, d, e, f):
-            sum = intmask(a) + intmask(b) + intmask(c) + intmask(d) + intmask(e) + intmask(f)
-            return sum
-
-        functions = [
-            (func_int, lltype.Signed, types.sint, 655360, 655360),
-            (func_int, lltype.Signed, types.sint, 655360, -293999429),
-            ]
-
-        cpu = self.cpu
-        for func, TP, ffi_type, num, num1 in functions:
-            #
-            FPTR = self.Ptr(self.FuncType([TP] * 6, TP))
-            func_ptr = llhelper(FPTR, func)
-            FUNC = deref(FPTR)
-            funcbox = self.get_funcbox(cpu, func_ptr)
-            # first, try it with the "normal" calldescr
-            calldescr = cpu.calldescrof(FUNC, FUNC.ARGS, FUNC.RESULT,
-                                        EffectInfo.MOST_GENERAL)
-            iargs = [0x7fffFFFFffffFFFF,1,0,0,0,0]
-            args = [InputArgInt(num) for num in iargs]
-            res = self.execute_operation(rop.CALL_I,
-                                         [funcbox] + args,
-                                         'int', descr=calldescr)
-            assert res == sum(iargs)
