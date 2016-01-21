989115d9e41f34027846ac8301c484cfc78c9b31
Carl Friedrich Bolz <cfbolz@gmx.de>
2016-01-21 21:49:25 +0100

    fix translation by killing dead import

--- pypy/interpreter/pycode.py
+++ pypy/interpreter/pycode.py
@@ -6,7 +6,7 @@ The bytecode interpreter itself is implemented by the PyFrame class.
 
 import dis, imp, struct, types, new, sys, os
 
-from pypy.interpreter import eval, valueprof
+from pypy.interpreter import eval
 from pypy.interpreter.signature import Signature
 from pypy.interpreter.error import OperationError
 from pypy.interpreter.gateway import unwrap_spec
