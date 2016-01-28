e3996dc6808e30d25c2047949a96259c7bd8e570
Richard Plangger <planrichi@gmail.com>
2016-01-28 22:08:25 +0100

    adapted test to match the expected assembly after the changes

--- rpython/jit/backend/zarch/test/test_runner.py
+++ rpython/jit/backend/zarch/test/test_runner.py
@@ -26,5 +26,5 @@ class TestZARCH(LLtypeBackendTest):
 
     add_loop_instructions = "lg; lgr; larl; agr; cgfi; je; j;$"
     # realloc frame takes the most space (from just after larl, to lay)
-    bridge_loop_instructions = "larl; lg; cgfi; je; lghi; stg; " \
-                               "lay; lgfi;( iihf;)? lgfi;( iihf;)? basr; lay; lg; br;$"
+    bridge_loop_instructions = "larl; lg; cgfi; jhe; lghi; " \
+                               "lgfi;( iihf;)? lgfi;( iihf;)? basr; lg; br;$"
