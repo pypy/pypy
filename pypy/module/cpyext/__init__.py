9157d94c0070358955d5c6c7165170c9b0495904
Armin Rigo <arigo@tunes.org>
2016-02-14 12:42:53 +0100

    Kill PyPy_Borrow()

--- pypy/module/cpyext/__init__.py
+++ pypy/module/cpyext/__init__.py
@@ -61,7 +61,6 @@ import pypy.module.cpyext.weakrefobject
 import pypy.module.cpyext.funcobject
 import pypy.module.cpyext.frameobject
 import pypy.module.cpyext.classobject
-import pypy.module.cpyext.pypyintf
 import pypy.module.cpyext.memoryobject
 import pypy.module.cpyext.codecs
 import pypy.module.cpyext.pyfile
