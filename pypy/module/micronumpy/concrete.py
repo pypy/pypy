0ac46135a5a8d8e7257630938077c13c2dfd5b10
mattip <matti.picus@gmail.com>
2016-02-19 16:29:25 +0200

    test, fix indexing ndarray with scalar, single boolean array

--- pypy/module/micronumpy/concrete.py
+++ pypy/module/micronumpy/concrete.py
@@ -12,8 +12,8 @@ from pypy.module.micronumpy.base import convert_to_array, W_NDimArray, \
     ArrayArgumentException, W_NumpyObject
 from pypy.module.micronumpy.iterators import ArrayIter
 from pypy.module.micronumpy.strides import (
-    IntegerChunk, SliceChunk, NewAxisChunk, EllipsisChunk, new_view,
-    calc_strides, calc_new_strides, shape_agreement,
+    IntegerChunk, SliceChunk, NewAxisChunk, EllipsisChunk, BooleanChunk,
+    new_view, calc_strides, calc_new_strides, shape_agreement,
     calculate_broadcast_strides, calc_backstrides, calc_start, is_c_contiguous,
     is_f_contiguous)
 from rpython.rlib.objectmodel import keepalive_until_here
@@ -236,6 +236,8 @@ class BaseConcreteArray(object):
 
     @jit.unroll_safe
     def _prepare_slice_args(self, space, w_idx):
+        print '_prepare_slice_args', w_idx
+        from pypy.module.micronumpy import boxes
         if space.isinstance_w(w_idx, space.w_str):
             raise oefmt(space.w_IndexError, "only integers, slices (`:`), "
                 "ellipsis (`...`), numpy.newaxis (`None`) and integer or "
@@ -258,6 +260,7 @@ class BaseConcreteArray(object):
         result = []
         i = 0
         has_ellipsis = False
+        has_filter = False
         for w_item in space.fixedview(w_idx):
             if space.is_w(w_item, space.w_Ellipsis):
                 if has_ellipsis:
@@ -272,6 +275,16 @@ class BaseConcreteArray(object):
             elif space.isinstance_w(w_item, space.w_slice):
                 result.append(SliceChunk(w_item))
                 i += 1
+            elif isinstance(w_item, W_NDimArray) and w_item.get_dtype().is_bool():
+                if has_filter:
+                    # in CNumPy, the support for this is incomplete
+                    raise oefmt(space.w_ValueError,
+                        "an index can only have a single boolean mask; "
+                        "use np.take or create a sinlge mask array")
+                has_filter = True
+                result.append(BooleanChunk(w_item))
+            elif isinstance(w_item, boxes.W_GenericBox):
+                result.append(IntegerChunk(w_item.descr_int(space)))
             else:
                 result.append(IntegerChunk(w_item))
                 i += 1
@@ -280,11 +293,14 @@ class BaseConcreteArray(object):
         return result
 
     def descr_getitem(self, space, orig_arr, w_index):
+        print 'concrete descr_gettiem %s' % str(w_index)[:35]
         try:
             item = self._single_item_index(space, w_index)
+            print 'concrete descr_gettiem _single_item_index succeeded'
             return self.getitem(item)
         except IndexError:
             # not a single result
+            print 'concrete descr_gettiem _single_item_index failed'
             chunks = self._prepare_slice_args(space, w_index)
             return new_view(space, orig_arr, chunks)
 
