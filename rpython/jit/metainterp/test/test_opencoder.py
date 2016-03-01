605a49eee451d5e3ca716cb56118e99ff1ee8fa6
fijal <unknown>
2016-03-01 10:38:30 +0100

    start writing a test

--- rpython/jit/metainterp/test/test_opencoder.py
+++ rpython/jit/metainterp/test/test_opencoder.py
@@ -28,6 +28,22 @@ class TestOpencoder(object):
         assert l[0].getarg(1) is i1
 
     def test_rd_snapshot(self):
+        class JitCode(object):
+            def __init__(self, index):
+                self.index = index
+
+        class FakeFrame(object):
+            parent_resumedata_frame_info_list = None
+            parent_resumedata_snapshot = None
+
+            def __init__(self, pc, jitcode, boxes):
+                self.pc = pc
+                self.jitcode = jitcode
+                self.boxes = boxes
+
+            def get_list_of_active_boxes(self, flag):
+                return self.boxes
+
         i0, i1 = InputArgInt(), InputArgInt()
         t = Trace([i0, i1])
         add = t.record_op(rop.INT_ADD, [i0, i1])
@@ -35,8 +51,7 @@ class TestOpencoder(object):
         # now we write rd_snapshot and friends
         virtualizable_boxes = []
         virutalref_boxes = []
-        framestack = []
-        framestack.xxx
+        framestack = [FakeFrame(1, JitCode(2), [i0, i1])]
         resume.capture_resumedata(framestack, virtualizable_boxes,
                                   virutalref_boxes, t)
         (i0, i1), l = self.unpack(t)
