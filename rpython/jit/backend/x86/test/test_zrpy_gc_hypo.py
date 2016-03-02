2e1442eabea062bafc27270bc830e0eaf6e4cae2
Richard Plangger <planrichi@gmail.com>
2016-03-02 22:13:44 +0100

    removed test files that where moved to gcstress-hypothesis

--- rpython/jit/backend/x86/test/test_zrpy_gc_hypo.py
+++
@@ -1,6 +0,0 @@
-from rpython.jit.backend.llsupport.tl.test.zrpy_gc_hypo_test import GCHypothesis
-
-class TestGCHypothesis(GCHypothesis):
-    # runs ../../llsupport/tl/test/zrpy_gc_hypo_test.py
-    gcrootfinder = "shadowstack"
-    gc = "incminimark"
