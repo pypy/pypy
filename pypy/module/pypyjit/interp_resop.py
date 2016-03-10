27077558fdb6d18c03ecddd42275c4cb6239aa2c
fijal <unknown>
2016-03-10 16:41:15 +0200

    some rpythonization

--- pypy/module/pypyjit/interp_resop.py
+++ pypy/module/pypyjit/interp_resop.py
@@ -249,7 +249,7 @@ class W_JitLoopInfo(W_Root):
                 ofs = debug_info.asminfo.ops_offset
             else:
                 ofs = {}
-            ops = debug_info.operations
+            _, ops = debug_info.trace.unpack()
             self.w_ops = space.newlist(wrap_oplist(space, logops, ops, ofs))
         else:
             self.w_ops = space.w_None
