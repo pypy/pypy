c9b41e7d2bc30b0b3bf99e0d3d38aa224a59ce3d
fijal <unknown>
2016-03-11 15:44:42 +0200

    add more debug prints

--- rpython/jit/metainterp/opencoder.py
+++ rpython/jit/metainterp/opencoder.py
@@ -215,6 +215,7 @@ class Trace(BaseTrace):
         debug_print(" bigint consts: " + str(self._consts_bigint) + " " + str(len(self._bigints)))
         debug_print(" float consts: " + str(self._consts_float) + " " + str(len(self._floats)))
         debug_print(" ref consts: " + str(self._consts_ptr) + " " + str(len(self._refs)))
+        debug_print(" descrs: " + len(self._descrs))
         debug_stop("jit-trace-done")
         return 0 # completely different than TraceIter.done, but we have to
         # share the base class
