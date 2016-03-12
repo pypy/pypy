21e4c192de3a4d4e345d75187a2b1ca0ed951a37
mattip <matti.picus@gmail.com>
2016-03-12 20:29:41 +0200

    simplify ifdefs, add arm case

--- rpython/rlib/rvmprof/src/vmprof_config.h
+++ rpython/rlib/rvmprof/src/vmprof_config.h
@@ -1,6 +1,15 @@
 #define HAVE_SYS_UCONTEXT_H
-#if defined(__FreeBSD__) || defined(__APPLE__)
+#if defined(__FreeBSD__)
 #define PC_FROM_UCONTEXT uc_mcontext.mc_rip
+#elif defined( __APPLE__)
+  #if ((ULONG_MAX) == (UINT_MAX))
+    #define PC_FROM_UCONTEXT uc_mcontext->__ss.__eip
+  #else
+    #define PC_FROM_UCONTEXT uc_mcontext->__ss.__rip
+  #endif
+#elif defined(__arm__)
+#define PC_FROM_UCONTEXT uc_mcontext.arm_ip
 #else
+/* linux, gnuc */
 #define PC_FROM_UCONTEXT uc_mcontext.gregs[REG_RIP]
 #endif
