09ed48dfce6595dc8d1c8f6f9b2fb1c0523d1183
Alex Gaynor <alex.gaynor@gmail.com>
2016-03-19 23:37:34 -0400

    Fixed a.fromstring(a)

--- pypy/module/array/interp_array.py
+++ pypy/module/array/interp_array.py
@@ -238,6 +238,11 @@ class W_ArrayBase(W_Root):
         Appends items from the string, interpreting it as an array of machine
         values,as if it had been read from a file using the fromfile() method).
         """
+        if self is w_s:
+            raise OperationError(
+                self.space.w_ValueError,
+                self.space.wrap("array.fromstring(x): x cannot be self")
+            )
         s = space.getarg_w('s#', w_s)
         if len(s) % self.itemsize != 0:
             msg = 'string length not a multiple of item size'
