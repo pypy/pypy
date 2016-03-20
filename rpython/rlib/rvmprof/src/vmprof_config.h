20f65decb3798654b03b4c7d52f0b4f16ce1be03
Armin Rigo <arigo@tunes.org>
2016-03-20 09:15:16 +0100

    Patch from issue #2262 for FreeBSD 32-bit

--- rpython/rlib/rvmprof/src/vmprof_config.h
+++ rpython/rlib/rvmprof/src/vmprof_config.h
@@ -1,5 +1,11 @@
 #define HAVE_SYS_UCONTEXT_H
-#if defined(__FreeBSD__) || defined(__APPLE__)
+#if defined(__FreeBSD__)
+#if defined(__i386__)
+#define PC_FROM_UCONTEXT uc_mcontext.mc_eip
+#else
+#define PC_FROM_UCONTEXT uc_mcontext.mc_rip
+#endif
+#elif defined(__APPLE__)
 #define PC_FROM_UCONTEXT uc_mcontext.mc_rip
 #else
 #define PC_FROM_UCONTEXT uc_mcontext.gregs[REG_RIP]
