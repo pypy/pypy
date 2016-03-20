0644ac0260b4c2f4fdb3b4900cdf28a52c934142
Alex Gaynor <alex.gaynor@gmail.com>
2016-03-19 23:43:47 -0400

    Added typecheck in Row.__init__, like CPython does

--- lib_pypy/_sqlite3.py
+++ lib_pypy/_sqlite3.py
@@ -1155,6 +1155,8 @@ class Statement(object):
 
 class Row(object):
     def __init__(self, cursor, values):
+        if not isinstance(cursor, Cursor):
+            raise TypeError("instance of cursor required for first argument")
         self.description = cursor.description
         self.values = values
 
