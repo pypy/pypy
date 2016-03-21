000b66ffc3f318280d158a75833758283bac4a9c
Stefano Rivera <stefano@rivera.za.net>
2016-03-21 14:32:17 -0400

    Support GNU/kFreeBSD Debian ports in vmprof

--- rpython/rlib/rvmprof/src/vmprof_config.h
+++ rpython/rlib/rvmprof/src/vmprof_config.h
@@ -1,5 +1,5 @@
 #define HAVE_SYS_UCONTEXT_H
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
   #ifdef __i386__
     #define PC_FROM_UCONTEXT uc_mcontext.mc_eip
   #else
