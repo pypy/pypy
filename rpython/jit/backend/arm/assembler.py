51e4dc094ee3cdd9dd3d29eb0b69fb2070b5d946
Armin Rigo <arigo@tunes.org>
2016-04-02 00:09:35 +0200

    fix(?)

--- rpython/jit/backend/arm/assembler.py
+++ rpython/jit/backend/arm/assembler.py
@@ -557,7 +557,7 @@ class AssemblerARM(ResOpAssembler):
         debug_stop('jit-backend-ops')
 
     def _call_header(self):
-        assert self.mc.currpos() == 0
+        # there is the gc table before this point
         self.gen_func_prolog()
 
     def _call_header_with_stack_check(self):
