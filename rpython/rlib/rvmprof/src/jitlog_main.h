61616d7a5ebfb0824f982a7dd26fab63abb5b95d
Richard Plangger <planrichi@gmail.com>
2016-04-01 12:50:32 +0200

    try init from env variable rewritten, there is currently no filtering mechanism. filtering does not make sense anymore

--- rpython/rlib/rvmprof/src/jitlog_main.h
+++ rpython/rlib/rvmprof/src/jitlog_main.h
@@ -21,53 +21,22 @@ void jitlog_try_init_using_env(void) {
     char *filename = getenv("JITLOG");
 
     if (filename && filename[0]) {
-        char *newfilename = NULL, *escape;
-        char *colon = strchr(filename, ':');
-        if (filename[0] == '+') {
-            filename += 1;
-            colon = NULL;
+        // mode is 775
+        mode_t mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+        jitlog_fd = open(filename, O_WRONLY | O_CREAT, mode);
+        if (jitlog_fd == -1) {
+            perror("could not open");
+            exit(-1);
         }
-        if (!colon) {
-            /* JITLOG=+filename (or just 'filename') --- profiling version */
-            //pypy_setup_profiling();
-        } else {
-            /* JITLOG=prefix:filename --- conditional logging */
-            int n = colon - filename;
-            jitlog_prefix = malloc(n + 1);
-            memcpy(jitlog_prefix, filename, n);
-            //debug_prefix[n] = '\0';
-            filename = colon + 1;
-        }
-        escape = strstr(filename, "%d");
-        if (escape) {
-            /* a "%d" in the filename is replaced with the pid */
-            newfilename = malloc(strlen(filename) + 32);
-            if (newfilename != NULL) {
-                char *p = newfilename;
-                memcpy(p, filename, escape - filename);
-                p += escape - filename;
-                sprintf(p, "%ld", (long)getpid());
-                strcat(p, escape + 2);
-                filename = newfilename;
-            }
-        }
-        if (strcmp(filename, "-") != 0) {
-            // mode is 775
-            mode_t mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
-            jitlog_fd = open(filename, O_WRONLY | O_CREAT, mode);
-        }
-
-        if (escape) {
-            free(newfilename);   /* if not null */
-            /* the env var is kept and passed to subprocesses */
-      } else {
-#ifndef _WIN32
-          unsetenv("JITLOG");
-#else
-          putenv("JITLOG=");
-#endif
-      }
+    } else {
+        jitlog_ready = 0;
+        return;
     }
+#ifndef _WIN32
+    unsetenv("JITLOG");
+#else
+    putenv("JITLOG=");
+#endif
     jitlog_ready = 1;
 }
 
