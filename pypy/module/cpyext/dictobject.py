b1686b0e85005655cf38bc18b081e34b3545f9c2
mattip <matti.picus@gmail.com>
2016-04-21 00:02:06 +0300

    remove debug cruft

--- pypy/module/cpyext/dictobject.py
+++ pypy/module/cpyext/dictobject.py
@@ -259,12 +259,12 @@ def PyDictProxy_New(space, w_dict):
 @cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
 def PyDictProxy_Check(space, w_obj):
     w_typ = make_frozendict(space)
-    print 'check', w_typ, space.type(w_obj)
+    #print 'check', w_typ, space.type(w_obj)
     return space.isinstance_w(w_obj, w_typ)
 
 @cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
 def PyDictProxy_CheckExact(space, w_obj):
     w_typ = make_frozendict(space)
-    print 'exact', w_typ, w_obj
+    #print 'exact', w_typ, w_obj
     return space.is_w(space.type(w_obj), w_typ)
 
