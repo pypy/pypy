3de1e4bd4c9f046c8a1b7f71a07cb2d940133072
Philip Jenvey <pjenvey@underboss.org>
2016-04-26 21:16:58 -0700

    adjust per PyCode's changes to frozen modules and app_name='__import__'

--- pypy/module/_frozen_importlib/interp_import.py
+++ pypy/module/_frozen_importlib/interp_import.py
@@ -1,12 +1,14 @@
 from pypy.interpreter.gateway import interp2app
 from pypy.interpreter.error import OperationError
 
-@interp2app
 def import_with_frames_removed(space, __args__):
     try:
         return space.call_args(
             space.getbuiltinmodule('_frozen_importlib').getdictvalue(
                 space, '__import__'), __args__)
     except OperationError as e:
-        e.remove_traceback_module_frames('<frozen importlib._bootstrap>')
+        e.remove_traceback_module_frames(
+            '<builtin>/frozen importlib._bootstrap')
         raise
+import_with_frames_removed = interp2app(import_with_frames_removed,
+                                        app_name='__import__')
