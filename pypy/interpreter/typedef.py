b29bf2e84a60200a0444333d84d8c71e498e6b40
Carl Friedrich Bolz <cfbolz@gmx.de>
2016-04-28 23:24:49 +0300

    fix problems with __del__: only make a single RPython subclass if the base class already has a del

--- pypy/interpreter/typedef.py
+++ pypy/interpreter/typedef.py
@@ -113,11 +113,18 @@ def get_unique_interplevel_subclass(space, cls, needsdel=False):
         return _subclass_cache[key]
     except KeyError:
         # XXX can save a class if cls already has a __del__
-        if needsdel:
+        keys = [key]
+        base_has_del = hasattr(cls, '__del__')
+        if base_has_del:
+            # if the base has a __del__, we only need one class
+            keys = [(space, cls, True), (space, cls, False)]
+            needsdel = True
+        elif needsdel:
             cls = get_unique_interplevel_subclass(space, cls, False)
         subcls = _getusercls(space, cls, needsdel)
         assert key not in _subclass_cache
-        _subclass_cache[key] = subcls
+        for key in keys:
+            _subclass_cache[key] = subcls
         return subcls
 get_unique_interplevel_subclass._annspecialcase_ = "specialize:memo"
 _subclass_cache = {}
@@ -133,20 +140,24 @@ def _getusercls(space, cls, wants_del, reallywantdict=False):
     name = cls.__name__ + "User"
 
     mixins_needed = []
-    if cls is W_ObjectObject or cls is W_InstanceObject:
-        mixins_needed.append(_make_storage_mixin_size_n())
-    else:
-        mixins_needed.append(MapdictStorageMixin)
-    copy_methods = [BaseUserClassMapdict]
-    if reallywantdict or not typedef.hasdict:
-        # the type has no dict, mapdict to provide the dict
-        copy_methods.append(MapdictDictSupport)
-        name += "Dict"
-    if not typedef.weakrefable:
-        # the type does not support weakrefs yet, mapdict to provide weakref
-        # support
-        copy_methods.append(MapdictWeakrefSupport)
-        name += "Weakrefable"
+    copy_methods = []
+    mixins_needed = []
+    name = cls.__name__
+    if not cls.user_overridden_class:
+        if cls is W_ObjectObject or cls is W_InstanceObject:
+            mixins_needed.append(_make_storage_mixin_size_n())
+        else:
+            mixins_needed.append(MapdictStorageMixin)
+        copy_methods = [BaseUserClassMapdict]
+        if reallywantdict or not typedef.hasdict:
+            # the type has no dict, mapdict to provide the dict
+            copy_methods.append(MapdictDictSupport)
+            name += "Dict"
+        if not typedef.weakrefable:
+            # the type does not support weakrefs yet, mapdict to provide weakref
+            # support
+            copy_methods.append(MapdictWeakrefSupport)
+            name += "Weakrefable"
     if wants_del:
         name += "Del"
         parent_destructor = getattr(cls, '__del__', None)
