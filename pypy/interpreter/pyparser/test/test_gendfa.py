ad8ea7d6f37e6ce164a8f3eeb6ba470a9fa01a05
Raffael Tfirst <raffael.tfirst@gmail.com>
2016-05-22 22:07:28 +0200

    Fix test_gendfa with correct whitespaces

--- pypy/interpreter/pyparser/test/test_gendfa.py
+++ pypy/interpreter/pyparser/test/test_gendfa.py
@@ -7,11 +7,10 @@ def test_states():
     assert output('test', DFA, d, states) == """\
 accepts = [False, True]
 states = [
-    #0
+    # 0
     {'\\x00': 1},
-    #1
+    # 1
     {'\\x01': 0},
     ]
 test = automata.pypy.interpreter.pyparser.automata.DFA(states, accepts)
-
 """
