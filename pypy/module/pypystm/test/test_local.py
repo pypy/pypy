d81223584ccf873f14917470218b3c3732b7a1db
Remi Meier <remi.meier@gmail.com>
2016-07-11 11:34:32 +0200

    add very direct test for the previous commit

--- pypy/module/pypystm/test/test_local.py
+++ pypy/module/pypystm/test/test_local.py
@@ -11,3 +11,37 @@ class AppTestSTMLocal(test_local.AppTestLocal):
             import pypystm
             return pypystm.local
         """)
+
+
+def test_direct_call_to_become_inevitable():
+    # this test directly checks if we call rstm.become_inevitable() in the
+    # right places (before modifying the real threadlocal). Could possibly be
+    # tested in a better way...
+    from pypy.module.pypystm.threadlocals import STMThreadLocals
+    from rpython.rlib import rstm
+    from pypy.interpreter.executioncontext import ExecutionContext
+
+    class FakeEC(ExecutionContext):
+        def __init__(self): pass
+    class FakeConfig:
+        class translation:
+            rweakref = False
+    class FakeSpace:
+        config = FakeConfig()
+        def createexecutioncontext(self):
+            return FakeEC()
+    call_counter = [0]
+    def fake_become_inevitable():
+        call_counter[0] += 1
+    rstm.become_inevitable = fake_become_inevitable
+
+    space = FakeSpace()
+
+    l = STMThreadLocals(space)
+    assert call_counter[0] == 0
+    l.try_enter_thread(space)
+    assert call_counter[0] == 1
+    l.enter_thread(space)
+    assert call_counter[0] == 2
+    l.leave_thread(space)
+    assert call_counter[0] == 3
