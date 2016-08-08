c7fd33702702a588fb84d0f87a3d928406dbcd66
Raffael Tfirst <raffael.tfirst@gmail.com>
2016-08-08 19:02:16 +0200

    remove setup_class in test_asyncio, make better use of test_gil_issue

--- pypy/module/_asyncio/test/test_asyncio.py
+++ pypy/module/_asyncio/test/test_asyncio.py
@@ -2,16 +2,13 @@ class AppTestAsyncIO(object):
     
     spaceconfig = dict(usemodules=["select","_socket","thread","signal","struct","_multiprocessing","array","_posixsubprocess","fcntl","unicodedata"])
     
-    def setup_class(cls):
-        cls.space.appexec([], """():
+    def test_gil_issue(self):
+        # the problem occured at await asyncio.open_connection after calling run_until_complete
+        """
         import encodings.idna
         import asyncio
         async def f():
             reader, writer = await asyncio.open_connection('example.com', 80)
         
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(f())""")
-    
-    def test_gil_issue(self):
-        #needed to execute setup_call in the first place
-        assert 1==1
+        loop.run_until_complete(f())"""
