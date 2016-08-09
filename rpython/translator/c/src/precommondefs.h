27c65b62a9e3f90fdd0409c38a358efee662c3dc
Mark Young <marky1991@gmail.com>
2016-08-09 16:17:52 +0000

    Reapply the fix from issue 2348. Translation works now.

--- rpython/translator/c/src/precommondefs.h
+++ rpython/translator/c/src/precommondefs.h
@@ -20,7 +20,7 @@
 #define _NETBSD_SOURCE 1
 /* Define to activate features from IEEE Stds 1003.1-2001 */
 #ifndef _POSIX_C_SOURCE
-#  define _POSIX_C_SOURCE 200112L
+#  define _POSIX_C_SOURCE 200809L
 #endif
 /* Define on FreeBSD to activate all library features */
 #define __BSD_VISIBLE 1
