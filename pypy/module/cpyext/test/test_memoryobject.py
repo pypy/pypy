ba4c6f7536856deaacf4d1b68905e92c1cc08239
Matti Picus <matti.picus@gmail.com>
2016-08-16 18:47:23 +0300

    add tests, implement buffer check

--- pypy/module/cpyext/test/test_memoryobject.py
+++ pypy/module/cpyext/test/test_memoryobject.py
@@ -1,5 +1,8 @@
 import pytest
 from pypy.module.cpyext.test.test_api import BaseApiTest
+from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
+
+
 
 class TestMemoryViewObject(BaseApiTest):
     def test_fromobject(self, space, api):
@@ -8,10 +11,16 @@ class TestMemoryViewObject(BaseApiTest):
             py.test.skip("unsupported before Python 2.7")
 
         w_hello = space.newbytes("hello")
+        assert api.PyObject_CheckBuffer(w_hello)
         w_view = api.PyMemoryView_FromObject(w_hello)
         w_bytes = space.call_method(w_view, "tobytes")
         assert space.unwrap(w_bytes) == "hello"
 
-    @pytest.mark.skipif(True, reason='write a test for this')
-    def test_get_base_and_get_buffer(self, space, api):
-        assert False # XXX test PyMemoryView_GET_BASE, PyMemoryView_GET_BUFFER
+
+class AppTestBufferProtocol(AppTestCpythonExtensionBase):
+    def test_buffer_protocol(self):
+        module = self.import_module(name='buffer_test')
+        arr = module.PyMyArray(10)
+        y = memoryview(arr)
+        assert y.format == 'i'
+
