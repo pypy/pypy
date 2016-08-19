37aaeaf1073684f8c3a708149de62c59b90b1290
Armin Rigo <arigo@tunes.org>
2016-08-19 10:54:35 +0200

    Another important speed-up for space initialization: instead of compiling importlib/_bootstrap.py every time, marshal the bytecode and cache it in rpython/_cache/.

--- pypy/module/_frozen_importlib/__init__.py
+++ pypy/module/_frozen_importlib/__init__.py
@@ -20,17 +20,41 @@ class Module(MixedModule):
         # "from importlib/_boostrap.py import *"
         # It's not a plain "import importlib._boostrap", because we
         # don't want to freeze importlib.__init__.
-        ec = space.getexecutioncontext()
         with open(os.path.join(lib_python, 'importlib', '_bootstrap.py')) as fp:
             source = fp.read()
         pathname = "<frozen importlib._bootstrap>"
-        code_w = ec.compiler.compile(source, pathname, 'exec', 0)
+        code_w = self._cached_compile(source, pathname, 'exec', 0)
         space.setitem(self.w_dict, space.wrap('__name__'), self.w_name)
         space.setitem(self.w_dict, space.wrap('__builtins__'),
                       space.wrap(space.builtin))
         code_w.exec_code(space, self.w_dict, self.w_dict)
 
         self.w_import = space.wrap(interp_import.import_with_frames_removed)
+
+    def _cached_compile(self, source, *args):
+        from rpython.config.translationoption import CACHE_DIR
+        from pypy.module.marshal import interp_marshal
+
+        space = self.space
+        cachename = os.path.join(CACHE_DIR, 'frozen_importlib_bootstrap')
+        try:
+            if space.config.translating:
+                raise IOError("don't use the cache when translating pypy")
+            with open(cachename, 'rb') as f:
+                previous = f.read(len(source) + 1)
+                if previous != source + '\x00':
+                    raise IOError("source changed")
+                w_bin = space.newbytes(f.read())
+                code_w = interp_marshal.loads(space, w_bin)
+        except IOError:
+            # must (re)compile the source
+            ec = space.getexecutioncontext()
+            code_w = ec.compiler.compile(source, *args)
+            w_bin = interp_marshal.dumps(space, code_w, space.wrap(2))
+            content = source + '\x00' + space.bytes_w(w_bin)
+            with open(cachename, 'wb') as f:
+                f.write(content)
+        return code_w
 
     def startup(self, space):
         """Copy our __import__ to builtins."""
