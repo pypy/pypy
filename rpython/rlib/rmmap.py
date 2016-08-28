01fee8b4361ec81b7534ad095dd85ed8b8dc2a9a
Edd Barrett <vext01@gmail.com>
2016-08-28 20:33:05 +0100

    Fix assertions in rmmap.set_pages_*

--- rpython/rlib/rmmap.py
+++ rpython/rlib/rmmap.py
@@ -727,7 +727,7 @@ if _POSIX:
         return c_mprotect_safe(addr, size, prot)
 
     def set_pages_executable(addr, size):
-        assert isinstance(addr, lltype._ptr)
+        assert lltype.typeOf(addr) == rffi.CCHARP
         assert isinstance(size, int)
         rv = mprotect(addr, size, PROT_EXEC | PROT_READ)
         if int(rv) < 0:
@@ -735,7 +735,7 @@ if _POSIX:
             debug.fatalerror_notb("set_pages_executable failed")
 
     def set_pages_writable(addr, size):
-        assert isinstance(addr, lltype._ptr)
+        assert lltype.typeOf(addr) == rffi.CCHARP
         assert isinstance(size, int)
         rv = mprotect(addr, size, PROT_WRITE | PROT_READ)
         if int(rv) < 0:
