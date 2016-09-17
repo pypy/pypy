db551de1f68ac6ba962e9d15a00da6f6e36d743d
Armin Rigo <arigo@tunes.org>
2016-09-17 19:04:15 +0200

    A direct test for "async for" (I have still no idea how we're supposed to call a coroutine)

--- pypy/interpreter/test/test_coroutine.py
+++ pypy/interpreter/test/test_coroutine.py
@@ -8,3 +8,26 @@ class AppTestCoroutine:
         raises(TypeError, iter, f(5))
         raises(TypeError, next, f(5))
         """
+
+    def test_async_for(self): """
+        class X:
+            def __aiter__(self):
+                return MyAIter()
+        class MyAIter:
+            async def __anext__(self):
+                return 42
+        async def f(x):
+            sum = 0
+            async for a in x:
+                sum += a
+                if sum > 100:
+                    break
+            return sum
+        cr = f(X())
+        try:
+            next(cr.__await__())
+        except StopIteration as e:
+            assert e.value == 42 * 3
+        else:
+            assert False, "should have raised"
+        """
