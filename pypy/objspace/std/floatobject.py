504ac6f7fc6a5e25bd1866196a746977d610818b
Daniel Patrick <danieljudepatrick@gmail.com>
2016-09-25 20:25:16 +0100

    Remove excess spaces from error message (commit f1684db)

--- pypy/objspace/std/floatobject.py
+++ pypy/objspace/std/floatobject.py
@@ -226,8 +226,8 @@ class W_FloatObject(W_Root):
                 if e.match(space, space.w_TypeError):
                     raise oefmt(
                         space.w_TypeError,
-                        "float() argument must be a string or a number,\
-                                not '%T'", w_value)
+                        ("float() argument must be a string or a number,"
+                                "not '%T'"), w_value)
                 raise
             value = _string_to_float(space, w_value, value)
         w_obj = space.allocate_instance(W_FloatObject, w_floattype)
