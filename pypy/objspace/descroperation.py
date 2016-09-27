a86c024f36a6e3e96b1225c8961403f3f50ff212
Ronan Lamy <ronan.lamy@gmail.com>
2016-09-27 17:42:06 +0100

    Test and fix for allowing __len__ to return any index-like

--- pypy/objspace/descroperation.py
+++ pypy/objspace/descroperation.py
@@ -277,7 +277,7 @@ class DescrOperation(object):
 
     def _check_len_result(space, w_obj):
         # Will complain if result is too big.
-        result = space.int_w(w_obj, allow_conversion=False)
+        result = space.getindex_w(w_obj, space.w_OverflowError)
         if result < 0:
             raise oefmt(space.w_ValueError, "__len__() should return >= 0")
         return result
