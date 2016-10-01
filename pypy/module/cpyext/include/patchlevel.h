79ce697b12f6d625231d0addbcbcf94cff15005e
Philip Jenvey <pjenvey@underboss.org>
2016-10-01 12:32:28 -0700

    bump to 2.7.12

--- pypy/module/cpyext/include/patchlevel.h
+++ pypy/module/cpyext/include/patchlevel.h
@@ -21,12 +21,12 @@
 /* Version parsed out into numeric values */
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	7
-#define PY_MICRO_VERSION	11
+#define PY_MICRO_VERSION	12
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION		"2.7.11"
+#define PY_VERSION		"2.7.12"
 
 /* PyPy version as a string */
 #define PYPY_VERSION "5.5.0-alpha0"
