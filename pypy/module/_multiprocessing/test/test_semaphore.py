a071c86846d25e3ed7c6503a460ab5b8efd62f54
Richard Plangger <planrichi@gmail.com>
2016-10-14 11:59:00 +0200

    modify and pass the test to use the new SemLock api (constructor and rebuild)

--- pypy/module/_multiprocessing/test/test_semaphore.py
+++ pypy/module/_multiprocessing/test/test_semaphore.py
@@ -35,7 +35,7 @@ class AppTestSemaphore:
         maxvalue = 1
         # the following line gets OSError: [Errno 38] Function not implemented
         # if /dev/shm is not mounted on Linux
-        sem = SemLock(kind, value, maxvalue)
+        sem = SemLock(kind, value, maxvalue, "1", False)
         assert sem.kind == kind
         assert sem.maxvalue == maxvalue
         assert isinstance(sem.handle, int)
@@ -68,7 +68,7 @@ class AppTestSemaphore:
         maxvalue = 1
         # the following line gets OSError: [Errno 38] Function not implemented
         # if /dev/shm is not mounted on Linux
-        sem = SemLock(kind, value, maxvalue)
+        sem = SemLock(kind, value, maxvalue, "2", False)
 
         sem.acquire()
         sem.release()
@@ -88,7 +88,7 @@ class AppTestSemaphore:
         kind = self.SEMAPHORE
         value = 1
         maxvalue = 1
-        sem = SemLock(kind, value, maxvalue)
+        sem = SemLock(kind, value, maxvalue, "3", False)
 
         res = sem.acquire()
         assert res == True
@@ -100,9 +100,9 @@ class AppTestSemaphore:
         kind = self.SEMAPHORE
         value = 1
         maxvalue = 1
-        sem = SemLock(kind, value, maxvalue)
+        sem = SemLock(kind, value, maxvalue, "4", False)
 
-        sem2 = SemLock._rebuild(sem.handle, kind, value)
+        sem2 = SemLock._rebuild(sem.handle, kind, value, "10")
         assert sem.handle == sem2.handle
 
     def test_semaphore_contextmanager(self):
@@ -110,7 +110,7 @@ class AppTestSemaphore:
         kind = self.SEMAPHORE
         value = 1
         maxvalue = 1
-        sem = SemLock(kind, value, maxvalue)
+        sem = SemLock(kind, value, maxvalue, "5", False)
 
         with sem:
             assert sem._count() == 1
