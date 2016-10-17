bf55fba193e35114bab8d7d3e65f6424f9118009
Matti Picus <matti.picus@gmail.com>
2016-10-17 22:53:47 +0300

    hack around failure to wrap __buffer__ in cf68a51fde59

--- pypy/module/cpyext/buffer.py
+++ pypy/module/cpyext/buffer.py
@@ -2,6 +2,7 @@ from rpython.rtyper.lltypesystem import rffi
 from pypy.module.cpyext.api import (
     cpython_api, CANNOT_FAIL, Py_TPFLAGS_HAVE_NEWBUFFER)
 from pypy.module.cpyext.pyobject import PyObject
+from pypy.module.cpyext.bytesobject import PyBytesObject
 
 @cpython_api([PyObject], rffi.INT_real, error=CANNOT_FAIL)
 def PyObject_CheckBuffer(space, pyobj):
@@ -9,6 +10,10 @@ def PyObject_CheckBuffer(space, pyobj):
     as_buffer = pyobj.c_ob_type.c_tp_as_buffer
     flags = pyobj.c_ob_type.c_tp_flags
     if (flags & Py_TPFLAGS_HAVE_NEWBUFFER and as_buffer.c_bf_getbuffer):
+        return 1
+    name = rffi.charp2str(pyobj.c_ob_type.c_tp_name)
+    if  name in ('str', 'bytes'):
+        # XXX remove once wrapper of __buffer__ -> bf_getbuffer works
         return 1
     return 0  
 
