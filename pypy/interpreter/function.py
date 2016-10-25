7a0e45b845a9294b1334c0d819e7dad9443c1aea
Ronan Lamy <ronan.lamy@gmail.com>
2016-10-25 18:05:52 +0100

    Compatibility: implement staticmethod.__init__ and classmethod.__init__ (backport of 6961be5cf15d)

--- pypy/interpreter/function.py
+++ pypy/interpreter/function.py
@@ -622,8 +622,11 @@ class StaticMethod(W_Root):
 
     def descr_staticmethod__new__(space, w_subtype, w_function):
         instance = space.allocate_instance(StaticMethod, w_subtype)
-        instance.__init__(w_function)
-        return space.wrap(instance)
+        instance.__init__(space.w_None)
+        return instance
+
+    def descr_init(self, space, w_function):
+        self.w_function = w_function
 
 
 class ClassMethod(W_Root):
@@ -641,8 +644,11 @@ class ClassMethod(W_Root):
 
     def descr_classmethod__new__(space, w_subtype, w_function):
         instance = space.allocate_instance(ClassMethod, w_subtype)
-        instance.__init__(w_function)
-        return space.wrap(instance)
+        instance.__init__(space.w_None)
+        return instance
+
+    def descr_init(self, space, w_function):
+        self.w_function = w_function
 
 class FunctionWithFixedCode(Function):
     can_change_code = False
