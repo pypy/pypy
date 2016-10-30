f36cbb723fb30878a968e2de6741045b015d8467
Armin Rigo <arigo@tunes.org>
2016-10-30 17:47:22 +0100

    slot_tp_new

--- pypy/module/cpyext/userslot.py
+++ pypy/module/cpyext/userslot.py
@@ -10,7 +10,10 @@ track changes to an app-level Python class (addition or removal of
 '__xxx__' special methods) after initalization of the PyTypeObject.
 """
 
+from pypy.interpreter.error import oefmt
+from pypy.interpreter.argument import Arguments
 from pypy.module.cpyext.api import cpython_api, PyObject, Py_ssize_t
+from pypy.module.cpyext.api import PyTypeObjectPtr
 
 
 @cpython_api([PyObject], Py_ssize_t, error=-1, header=None)
@@ -24,3 +27,13 @@ def slot_sq_item(space, w_obj, index):
 @cpython_api([PyObject, PyObject], PyObject, header=None)
 def slot_nb_add(space, w_obj1, w_obj2):
     return space.add(w_obj1, w_obj2)
+
+@cpython_api([PyTypeObjectPtr, PyObject, PyObject], PyObject, header=None)
+def slot_tp_new(space, w_type, w_args, w_kwds):
+    _, w_descr = space.lookup_in_type_where(w_type, '__new__')
+    if w_descr is None:
+        raise oefmt(space.w_RuntimeError, "cpyext: '__new__' not found")
+    # w_descr is typically a StaticMethod
+    args = Arguments(space, [w_type],
+                     w_stararg=w_args, w_starstararg=w_kwds)
+    return space.call_args(space.get(w_descr, w_type), args)
