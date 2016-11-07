c39d9dc06cce38ebdcbd7226bd7795c2d18514fb
Armin Rigo <arigo@tunes.org>
2016-11-07 19:09:09 +0100

    oups

--- pypy/module/pypyjit/interp_jit.py
+++ pypy/module/pypyjit/interp_jit.py
@@ -15,7 +15,7 @@ from pypy.interpreter.error import OperationError, oefmt
 from pypy.interpreter.pycode import CO_GENERATOR, CO_COROUTINE, PyCode
 from pypy.interpreter.gateway import unwrap_spec
 from pypy.interpreter.pyframe import PyFrame
-from pypy.interpreter.pyopcode import Yield
+from pypy.interpreter.pyopcode import Yield, Return
 from pypy.interpreter.baseobjspace import W_Root
 from pypy.interpreter.typedef import TypeDef
 from pypy.interpreter.gateway import interp2app
