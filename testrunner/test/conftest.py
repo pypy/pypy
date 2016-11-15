b1f3ae789db4e960ed3512fed145b30cb30ed78c
Ronan Lamy <ronan.lamy@gmail.com>
2016-11-15 23:53:21 +0000

    update testrunner tests

--- testrunner/test/conftest.py
+++ testrunner/test/conftest.py
@@ -2,7 +2,6 @@ import pytest
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
-    report = yield
+    report = (yield).result
     if 'out' in item.funcargs:
         report.sections.append(('out', item.funcargs['out'].read()))
-    return report
