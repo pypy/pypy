5687711261143185e43fa8d26ffe38a3411eb4ba
Armin Rigo <arigo@tunes.org>
2016-11-30 18:16:32 +0000

    fix test

--- pypy/module/pypyjit/test_pypy_c/test_instance.py
+++ pypy/module/pypyjit/test_pypy_c/test_instance.py
@@ -340,9 +340,16 @@ class TestInstance(BaseTestPyPyC):
             i78 = int_lt(i72, 300)
             guard_true(i78, descr=...)
             guard_not_invalidated(descr=...)
-            i79 = force_token()
-            i80 = force_token()
+            p1 = force_token()
+            p65 = force_token()
+            p3 = force_token()
             i81 = int_add(i72, 1)
-            --TICK--
+
+            # can't use TICK here, because of the extra setfield_gc
+            ticker0 = getfield_raw_i(#, descr=<FieldS pypysig_long_struct.c_value .*>)
+            setfield_gc(p0, p65, descr=<FieldP pypy.interpreter.pyframe.PyFrame.vable_token .>)
+            ticker_cond0 = int_lt(ticker0, 0)
+            guard_false(ticker_cond0, descr=...)
+
             jump(..., descr=...)
         """)
