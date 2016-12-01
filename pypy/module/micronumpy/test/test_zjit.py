06fd09474ace921556637cf89296044fa9561122
Richard Plangger <planrichi@gmail.com>
2016-12-01 17:45:01 +0100

    change tests to match the new behaviour

--- pypy/module/micronumpy/test/test_zjit.py
+++ pypy/module/micronumpy/test/test_zjit.py
@@ -398,7 +398,7 @@ class TestNumpyJit(LLJitMixin):
     def test_sum_multi(self):
         result = self.run("sum_multi")
         assert result == sum(range(30)) + sum(range(60))
-        self.check_vectorized(1, 1)
+        self.check_vectorized(1, 0)
 
     def define_sum_float_to_int16():
         return """
@@ -480,7 +480,7 @@ class TestNumpyJit(LLJitMixin):
         assert retval == sum(range(1,11))
         # check that we got only one loop
         assert len(get_stats().loops) == 1
-        self.check_vectorized(2, 1)
+        self.check_vectorized(2, 0)
 
     def test_reduce_axis_compile_only_once(self):
         self.compile_graph()
@@ -491,7 +491,7 @@ class TestNumpyJit(LLJitMixin):
         retval = self.interp.eval_graph(self.graph, [i])
         # check that we got only one loop
         assert len(get_stats().loops) == 1
-        self.check_vectorized(3, 1)
+        self.check_vectorized(3, 0)
 
     def define_prod():
         return """
