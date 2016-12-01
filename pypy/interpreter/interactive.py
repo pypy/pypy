c8edb4fa71f44963f883b500166a30e269e91d8d
Carl Friedrich Bolz <cfbolz@gmx.de>
2016-12-01 13:46:33 +0100

    make sure that hacked_filename is a str

--- pypy/interpreter/interactive.py
+++ pypy/interpreter/interactive.py
@@ -169,12 +169,12 @@ class PyPyConsole(code.InteractiveConsole):
 
     def runsource(self, source, ignored_filename="<input>", symbol="single"):
         # the following hacked file name is recognized specially by error.py
-        hacked_filename = '<inline>\n' + source
         compiler = self.space.getexecutioncontext().compiler
 
         # CPython 2.6 turns console input into unicode
         if isinstance(source, unicode):
             source = source.encode(sys.stdin.encoding)
+        hacked_filename = '<inline>\n' + source
 
         def doit():
             # compile the provided input
