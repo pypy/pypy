740c48d64c1bae893cc7fc2910e6ce3ad230fc28
Armin Rigo <arigo@tunes.org>
2016-12-17 09:33:59 +0100

    Translation fix

--- pypy/module/cpyext/import_.py
+++ pypy/module/cpyext/import_.py
@@ -131,16 +131,14 @@ def _PyImport_AcquireLock(space):
     in different threads to return with a partially loaded module.
     These calls are serialized by the global interpreter lock."""
     try:
-        w_func = space.getbuiltinmodule('imp').get('acquire_lock')
-        space.call_function(w_func)
+        space.call_method(space.getbuiltinmodule('imp'), 'acquire_lock')
     except OperationError as e:
         e.write_unraisable(space, "_PyImport_AcquireLock")
 
 @cpython_api([], rffi.INT_real, error=CANNOT_FAIL)
 def _PyImport_ReleaseLock(space):
     try:
-        w_func = space.getbuiltinmodule('imp').get('release_lock')
-        space.call_function(w_func)
+        space.call_method(space.getbuiltinmodule('imp'), 'release_lock')
         return 1
     except OperationError as e:
         e.write_unraisable(space, "_PyImport_ReleaseLock")
