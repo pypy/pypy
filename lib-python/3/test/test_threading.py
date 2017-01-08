3f75d4d92006eb8478e64af0007373f7c3ae728f
Armin Rigo <arigo@tunes.org>
2017-01-08 21:24:25 +0100

    Skip tests that are really about subinterpreters

--- lib-python/3/test/test_threading.py
+++ lib-python/3/test/test_threading.py
@@ -836,6 +836,7 @@ class ThreadJoinOnShutdown(BaseTestCase):
 
 class SubinterpThreadingTests(BaseTestCase):
 
+    @cpython_only
     def test_threads_join(self):
         # Non-daemon threads should be joined at subinterpreter shutdown
         # (issue #18808)
@@ -859,6 +860,7 @@ class SubinterpThreadingTests(BaseTestCase):
         # The thread was joined properly.
         self.assertEqual(os.read(r, 1), b"x")
 
+    @cpython_only
     def test_threads_join_2(self):
         # Same as above, but a delay gets introduced after the thread's
         # Python code returned but before the thread state is deleted.
