d55fe16da88fab372e72ad4bf2f7cc3d2013b464
Armin Rigo <arigo@tunes.org>
2017-01-12 19:19:44 +0100

    Update the version numbers to 3.5.2, after 215771f42f81

--- pypy/module/cpyext/include/patchlevel.h
+++ pypy/module/cpyext/include/patchlevel.h
@@ -21,12 +21,12 @@
 /* Version parsed out into numeric values */
 #define PY_MAJOR_VERSION	3
 #define PY_MINOR_VERSION	5
-#define PY_MICRO_VERSION	1
+#define PY_MICRO_VERSION	2
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION		"3.5.1"
+#define PY_VERSION		"3.5.2"
 
 /* PyPy version as a string */
 #define PYPY_VERSION "5.6.0-alpha0"
