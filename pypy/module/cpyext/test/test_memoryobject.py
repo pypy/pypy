778ac15a1318e98bde1cf8e12749dc51f732d78e
Ronan Lamy <ronan.lamy@gmail.com>
2017-01-24 14:02:53 +0000

    fix tests

--- pypy/module/cpyext/test/test_memoryobject.py
+++ pypy/module/cpyext/test/test_memoryobject.py
@@ -13,8 +13,8 @@ class TestMemoryViewObject(BaseApiTest):
         w_hello = space.newbytes("hello")
         #assert api.PyObject_CheckBuffer(w_hello)
         w_view = from_ref(space, api.PyMemoryView_FromObject(w_hello))
-        w_char = space.call_method(w_view, '__getitem__', space.wrap(0))
-        assert space.eq_w(w_char, space.wrap('h'))
+        w_byte = space.call_method(w_view, '__getitem__', space.wrap(0))
+        assert space.eq_w(w_byte, space.wrap(ord('h')))
         w_bytes = space.call_method(w_view, "tobytes")
         assert space.unwrap(w_bytes) == "hello"
 
@@ -72,7 +72,6 @@ class AppTestPyBuffer_FillInfo(AppTestCpythonExtensionBase):
 
 class AppTestBufferProtocol(AppTestCpythonExtensionBase):
     def test_buffer_protocol_app(self):
-        import struct
         module = self.import_module(name='buffer_test')
         arr = module.PyMyArray(10)
         y = memoryview(arr)
@@ -80,8 +79,7 @@ class AppTestBufferProtocol(AppTestCpythonExtensionBase):
         assert y.shape == (10,)
         assert len(y) == 10
         s = y[3]
-        assert len(s) == struct.calcsize('i')
-        assert s == struct.pack('i', 3)
+        assert s == 3
 
     def test_buffer_protocol_capi(self):
         foo = self.import_extension('foo', [
