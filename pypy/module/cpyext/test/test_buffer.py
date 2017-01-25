83c3d87ee252b6dd738b2819d2bac09215075eb2
Ronan Lamy <ronan.lamy@gmail.com>
2017-01-25 14:10:22 +0000

    Add failing mmap test

---
+++ pypy/module/cpyext/test/test_buffer.py
@@ -0,0 +1,26 @@
+from pypy.module.cpyext.test.test_cpyext import AppTestCpythonExtensionBase
+
+class AppTestMmap(AppTestCpythonExtensionBase):
+    spaceconfig = {
+        'usemodules': (AppTestCpythonExtensionBase.spaceconfig['usemodules']
+                       + ['mmap'])}
+    def test_mmap_buffer(self):
+        module = self.import_extension('mmap_buffer', [
+            ('isbuffer', 'METH_O',
+             """
+             Py_buffer view;
+
+             if (PyObject_GetBuffer(args, &view,
+                    PyBUF_ANY_CONTIGUOUS|PyBUF_WRITABLE) != 0) {
+                return NULL;
+             }
+             return PyLong_FromLong(1);
+             """)])
+        import os, mmap
+        tmpname = os.path.join(self.udir, 'test_mmap_buffer')
+        print(tmpname)
+        with open(tmpname, 'w+b') as f:
+            f.write(b'123')
+            f.flush()
+            m = mmap.mmap(f.fileno(), 3)
+            assert module.isbuffer(m) == 1
