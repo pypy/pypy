e83331189be1f25462538a73004d33a6cf1fae94
Armin Rigo <arigo@tunes.org>
2017-02-05 18:00:37 +0100

    PyCoro_CheckExact()

--- pypy/module/cpyext/genobject.py
+++ pypy/module/cpyext/genobject.py
@@ -1,5 +1,7 @@
-from pypy.interpreter.generator import GeneratorIterator
+from pypy.interpreter.generator import GeneratorIterator, Coroutine
 from pypy.module.cpyext.api import build_type_checkers
 
 
 PyGen_Check, PyGen_CheckExact = build_type_checkers("Gen", GeneratorIterator)
+
+_, PyCoro_CheckExact = build_type_checkers("Coro", Coroutine)
