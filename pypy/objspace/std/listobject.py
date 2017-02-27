241671775a532cd1e2950cc2f6dc1977a81d3db9
Armin Rigo <arigo@tunes.org>
2017-02-27 14:40:52 +0100

    cherry-pick e0ed90dd707f

--- pypy/objspace/std/listobject.py
+++ pypy/objspace/std/listobject.py
@@ -1966,7 +1966,7 @@ class IntOrFloatListStrategy(ListStrategy):
 class BytesListStrategy(ListStrategy):
     import_from_mixin(AbstractUnwrappedStrategy)
 
-    _none_value = None
+    _none_value = ""
 
     def wrap(self, stringval):
         return self.space.newbytes(stringval)
@@ -1998,7 +1998,7 @@ class BytesListStrategy(ListStrategy):
 class UnicodeListStrategy(ListStrategy):
     import_from_mixin(AbstractUnwrappedStrategy)
 
-    _none_value = None
+    _none_value = u""
 
     def wrap(self, stringval):
         assert stringval is not None
