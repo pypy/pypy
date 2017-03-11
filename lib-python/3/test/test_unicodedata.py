2088fdc2ae9ce3de0a2259567db41b01b6e5e985
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2017-03-11 19:14:59 +0100

    Skip a test that tries to remove the unicodedata module. PyPy's unicodedata is builtin and always available from RPython code.

--- lib-python/3/test/test_unicodedata.py
+++ lib-python/3/test/test_unicodedata.py
@@ -9,7 +9,7 @@
 import sys
 import unittest
 import hashlib
-from test.support import script_helper
+from test.support import script_helper, impl_detail
 
 encoding = 'utf-8'
 errors = 'surrogatepass'
@@ -224,6 +224,7 @@ class UnicodeFunctionsTest(UnicodeDatabaseTest):
 
 class UnicodeMiscTest(UnicodeDatabaseTest):
 
+    @impl_detail("pypy's unicodedata module is always available", pypy=False)
     def test_failed_import_during_compiling(self):
         # Issue 4367
         # Decoding \N escapes requires the unicodedata module. If it can't be
