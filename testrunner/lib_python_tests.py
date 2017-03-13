718d8d0ebc478adc1f4310e63b8bd4890d0fe3b5
Armin Rigo <arigo@tunes.org>
2017-03-13 08:23:08 +0100

    Check in here test runners for the lib-python and the pypyjit steps of buildbot.  Will then fix buildbot to execute these.  It allows us to customize the steps on py3.5.
    If you are running buildbot on other branches, you need to copy these
    two files too (or merge default).

---
+++ testrunner/lib_python_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+This is what the buildbot runs to execute the lib-python tests
+on top of pypy-c.
+"""
+
+import sys, os
+import subprocess
+
+rootdir = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
+
+popen = subprocess.Popen(
+    [sys.executable, "pypy/test_all.py",
+                     "--pypy=pypy/goal/pypy-c",
+                     "--timeout=3600",
+                     "--resultlog=cpython.log", "lib-python"],
+    cwd=rootdir)
+
+try:
+    ret = popen.wait()
+except KeyboardInterrupt:
+    popen.kill()
+    print "\ninterrupted"
+    ret = 1
+
+sys.exit(ret)
