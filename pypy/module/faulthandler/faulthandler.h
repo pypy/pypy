dc2bd2dd7f7c6423b56476078210861de7391c23
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2017-04-06 16:36:24 +0200

    Fix one failure on windows

--- pypy/module/faulthandler/faulthandler.h
+++ pypy/module/faulthandler/faulthandler.h
@@ -2,7 +2,11 @@
 #define PYPY_FAULTHANDLER_H
 
 #include "src/precommondefs.h"
+#ifdef _MSC_VER
+typedef Signed intptr_t;
+#else
 #include <stdint.h>
+#endif
 
 
 typedef void (*pypy_faulthandler_cb_t)(int fd, intptr_t *array_p,
