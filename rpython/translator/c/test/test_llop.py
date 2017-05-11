b99db0764d551aa563bbc6fd2ae41450481106af
Antonio Cuni <anto.cuni@gmail.com>
2017-05-11 18:50:57 +0200

    test and implement gc_store_indexed in the C backend

--- rpython/translator/c/test/test_llop.py
+++ rpython/translator/c/test/test_llop.py
@@ -1,5 +1,6 @@
 from rpython.rtyper.lltypesystem import lltype, llmemory, rffi
-from rpython.rtyper.test.test_llop import BaseLLOpTest, str_gc_load
+from rpython.rtyper.test.test_llop import (BaseLLOpTest, str_gc_load,
+                                           newlist_and_gc_store)
 from rpython.translator.c.test.test_genc import compile
 
 
@@ -24,3 +25,22 @@ class TestLLOp(BaseLLOpTest):
         fn = self.cache[TYPE]
         x = fn(buf, offset)
         return lltype.cast_primitive(TYPE, x)
+
+    def newlist_and_gc_store(self, TYPE, value):
+        if TYPE not in self.cache:
+            assert isinstance(TYPE, lltype.Primitive)
+            if TYPE in (lltype.Float, lltype.SingleFloat):
+                argtype = float
+            else:
+                argtype = int
+
+            def llf(value):
+                value = lltype.cast_primitive(TYPE, value)
+                lst = newlist_and_gc_store(TYPE, value)
+                return ''.join(lst)
+
+            fn = compile(llf, [argtype])
+            self.cache[TYPE] = fn
+        #
+        fn = self.cache[TYPE]
+        return fn(value)
