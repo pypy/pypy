67156cba777ff1c70b65d67a8c90e68532420edd
Armin Rigo <arigo@tunes.org>
2017-05-31 19:17:32 +0200

    update

--- pypy/module/_cffi_backend/test/_backend_test_c.py
+++ pypy/module/_cffi_backend/test/_backend_test_c.py
@@ -3843,7 +3843,7 @@ def test_cdata_dir():
 
 def test_char_pointer_conversion():
     import warnings
-    assert __version__.startswith(("1.8", "1.9", "1.10")), (
+    assert __version__.startswith(("1.8", "1.9", "1.10", "1.11")), (
         "consider turning the warning into an error")
     BCharP = new_pointer_type(new_primitive_type("char"))
     BIntP = new_pointer_type(new_primitive_type("int"))
