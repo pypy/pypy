389fb93cceb489d8d3dbe319b4f7e9706d01a9d6
Ronan Lamy <ronan.lamy@gmail.com>
2017-06-02 18:00:04 +0100

    Let OrderedDict.__init__ behave like CPython wrt. subclasses overridding __setitem__

--- pypy/module/_collections/test/test_ordereddict.py
+++ pypy/module/_collections/test/test_ordereddict.py
@@ -12,3 +12,13 @@ class AppTestBasic:
         d = OrderedDict()
         d[1] = d
         assert repr(d) == 'OrderedDict([(1, ...)])'
+
+    def test_subclass(self):
+        from _collections import OrderedDict
+        class MyODict(OrderedDict):
+            def __setitem__(self, key, value):
+                super().__setitem__(key, 42)
+        d = MyODict(x=1)
+        assert d['x'] == 42
+        d.update({'y': 2})
+        assert d['y'] == 42
