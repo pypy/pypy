79422408f542d0ab927ed654716aee4181fd5a65
Ronan Lamy <ronan.lamy@gmail.com>
2017-06-23 18:25:28 +0100

    remove unittest cruft

--- extra_tests/test_decimal.py
+++ extra_tests/test_decimal.py
@@ -8,52 +8,49 @@ P = import_fresh_module('decimal', blocked=['_decimal'])
 # import _decimal as C
 # import _pydecimal as P
 
+def test_C():
+    sys.modules["decimal"] = C
+    import decimal
+    d = decimal.Decimal('1')
+    assert isinstance(d, C.Decimal)
+    assert isinstance(d, decimal.Decimal)
+    assert isinstance(d.as_tuple(), C.DecimalTuple)
 
-class TestPythonAPI:
+    assert d == C.Decimal('1')
 
-    def check_equal(self, val, proto):
-        d = C.Decimal(val)
-        p = pickle.dumps(d, proto)
-        assert d == pickle.loads(p)
+def check_round_trip(val, proto):
+    d = C.Decimal(val)
+    p = pickle.dumps(d, proto)
+    assert d == pickle.loads(p)
 
-    def test_C(self):
+def test_pickle():
+    v = '-3.123e81723'
+    for proto in range(pickle.HIGHEST_PROTOCOL + 1):
         sys.modules["decimal"] = C
-        import decimal
-        d = decimal.Decimal('1')
-        assert isinstance(d, C.Decimal)
-        assert isinstance(d, decimal.Decimal)
-        assert isinstance(d.as_tuple(), C.DecimalTuple)
+        check_round_trip('-3.141590000', proto)
+        check_round_trip(v, proto)
 
-        assert d == C.Decimal('1')
+        cd = C.Decimal(v)
+        pd = P.Decimal(v)
+        cdt = cd.as_tuple()
+        pdt = pd.as_tuple()
+        assert cdt.__module__ == pdt.__module__
 
-    def test_pickle(self):
-        v = '-3.123e81723'
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            sys.modules["decimal"] = C
-            self.check_equal('-3.141590000', proto)
-            self.check_equal(v, proto)
+        p = pickle.dumps(cdt, proto)
+        r = pickle.loads(p)
+        assert isinstance(r, C.DecimalTuple)
+        assert cdt == r
 
-            cd = C.Decimal(v)
-            pd = P.Decimal(v)
-            cdt = cd.as_tuple()
-            pdt = pd.as_tuple()
-            assert cdt.__module__ == pdt.__module__
+        sys.modules["decimal"] = C
+        p = pickle.dumps(cd, proto)
+        sys.modules["decimal"] = P
+        r = pickle.loads(p)
+        assert isinstance(r, P.Decimal)
+        assert r == pd
 
-            p = pickle.dumps(cdt, proto)
-            r = pickle.loads(p)
-            assert isinstance(r, C.DecimalTuple)
-            assert cdt == r
-
-            sys.modules["decimal"] = C
-            p = pickle.dumps(cd, proto)
-            sys.modules["decimal"] = P
-            r = pickle.loads(p)
-            assert isinstance(r, P.Decimal)
-            assert r == pd
-
-            sys.modules["decimal"] = C
-            p = pickle.dumps(cdt, proto)
-            sys.modules["decimal"] = P
-            r = pickle.loads(p)
-            assert isinstance(r, P.DecimalTuple)
-            assert r == pdt
+        sys.modules["decimal"] = C
+        p = pickle.dumps(cdt, proto)
+        sys.modules["decimal"] = P
+        r = pickle.loads(p)
+        assert isinstance(r, P.DecimalTuple)
+        assert r == pdt
