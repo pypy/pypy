90ed51b65ed971c9c9faf15046775d498c817962
Richard Plangger <planrichi@gmail.com>
2017-07-23 18:22:41 -0400

    remove write_all_code_objects, this method is not called when it does not exist

--- pypy/module/_vmprof/interp_vmprof.py
+++ pypy/module/_vmprof/interp_vmprof.py
@@ -70,11 +70,6 @@ def enable(space, fileno, period, memory, lines, native, real_time):
     except rvmprof.VMProfError as e:
         raise VMProfError(space, e)
 
-def write_all_code_objects(space):
-    """ Needed on cpython, just empty function here
-    """
-    pass
-
 def disable(space):
     """Disable vmprof.  Remember to close the file descriptor afterwards
     if necessary.
