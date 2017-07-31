e81fb0c8b7ec6d5e14183b64bbce1102c03f8483
Wim Lavrijsen <WLavrijsen@lbl.gov>
2017-07-31 16:48:22 -0700

    remove backend code (lives in http://bitbucket/wlav/cppyy-backend and is shared with CPython) and the builtin capi option (never used in production)

--- pypy/module/_cppyy/capi/__init__.py
+++ pypy/module/_cppyy/capi/__init__.py
@@ -1,16 +1,6 @@
 from rpython.rtyper.lltypesystem import rffi, lltype
 
-# There are two possible ways of accessing the backend through the reflection
-# C-API: built it into pypy-c, or load it dynamically. The latter is preferred
-# (and is the default) for use with Reflex. B/c of some builtin pythonizations,
-# the former is recommended (for now) with CINT.
-
-# Note: if builtin_capi is chosen, then inside builtin_capi.py, there is still
-# the selection of the desired backend (default is Reflex).
-
-# choose C-API access method:
 from pypy.module._cppyy.capi.loadable_capi import *
-#from pypy.module._cppyy.capi.builtin_capi import *
 
 from pypy.module._cppyy.capi.capi_types import C_OBJECT,\
     C_NULL_TYPE, C_NULL_OBJECT
