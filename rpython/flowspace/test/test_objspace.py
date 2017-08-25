c5e9c1304bc0f7ec40e78ee4bd121c91fd1bdea0
Ronan Lamy <ronan.lamy@gmail.com>
2017-08-25 16:18:21 +0200

    Imperative py.test.skip() is evil

--- rpython/flowspace/test/test_objspace.py
+++ rpython/flowspace/test/test_objspace.py
@@ -839,15 +839,15 @@ class TestFlowObjSpace(Base):
             return x[s]
         graph = self.codetest(myfunc)
 
+    @py.test.mark.xfail
     def test_unichr_constfold(self):
-        py.test.skip("not working")
         def myfunc():
             return unichr(1234)
         graph = self.codetest(myfunc)
         assert graph.startblock.exits[0].target is graph.returnblock
 
+    @py.test.mark.xfail
     def test_unicode_constfold(self):
-        py.test.skip("not working for now")
         def myfunc():
             return unicode("1234")
         graph = self.codetest(myfunc)
