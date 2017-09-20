7057407bf692ce13047f0ac38f6c4e522ab4db31
Philip Jenvey <pjenvey@underboss.org>
2017-09-20 10:26:19 -0700

    fix memory leak in _get_crl_dp
    see https://github.com/python/cpython/commit/b2b00e0

--- pypy/module/_ssl/interp_ssl.py
+++ pypy/module/_ssl/interp_ssl.py
@@ -996,9 +996,6 @@ def _get_aia_uri(space, certificate, nid):
         libssl_AUTHORITY_INFO_ACCESS_free(info)
 
 def _get_crl_dp(space, certificate):
-    if OPENSSL_VERSION_NUMBER >= 0x10001000:
-        # Calls x509v3_cache_extensions and sets up crldp
-        libssl_X509_check_ca(certificate)
     dps = rffi.cast(stack_st_DIST_POINT, libssl_X509_get_ext_d2i(
         certificate, NID_crl_distribution_points, None, None))
     if not dps:
@@ -1020,8 +1017,7 @@ def _get_crl_dp(space, certificate):
                 s_uri = rffi.charpsize2str(uri.c_data, length)
                 cdp_w.append(space.newtext(s_uri))
     finally:
-        if OPENSSL_VERSION_NUMBER < 0x10001000:
-            libssl_sk_DIST_POINT_free(dps)
+        libssl_CRL_DIST_POINTS_free(dps)
     return space.newtuple(cdp_w[:])
 
 def checkwait(space, w_sock, writing):
