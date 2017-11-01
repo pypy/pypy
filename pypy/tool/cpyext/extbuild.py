1710314d057fe832ba60525f6c4821213ffffc63
Matti Picus <matti.picus@gmail.com>
2017-11-01 21:29:06 +0200

    patches from issue #2697 fix compilation on FreeBSD

--- pypy/tool/cpyext/extbuild.py
+++ pypy/tool/cpyext/extbuild.py
@@ -244,13 +244,13 @@ def get_sys_info_app(base_dir):
     if sys.platform == 'win32':
         compile_extra = ["/we4013"]
         link_extra = ["/LIBPATH:" + os.path.join(sys.exec_prefix, 'libs')]
-    elif sys.platform == 'darwin':
-        compile_extra = link_extra = None
-        pass
     elif sys.platform.startswith('linux'):
         compile_extra = [
             "-O0", "-g", "-Werror=implicit-function-declaration", "-fPIC"]
         link_extra = None
+    else:
+        compile_extra = link_extra = None
+        pass
     return ExtensionCompiler(
         builddir_base=base_dir,
         include_extra=[get_python_inc()],
