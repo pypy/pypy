44a2e20e1f2492c4ef87d9d96735171c25338a65
Ronan Lamy <ronan.lamy@gmail.com>
2017-11-03 19:13:10 +0000

    Kill fake test

--- extra_tests/test_failing.py
+++
@@ -1,8 +0,0 @@
-from hypothesis import given, strategies
-
-def mean(a, b):
-    return (a + b)/2.
-
-@given(strategies.integers(), strategies.integers())
-def test_mean_failing(a, b):
-    assert mean(a, b) >= min(a, b)
