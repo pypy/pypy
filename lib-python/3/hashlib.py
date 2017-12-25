3ffe59c3c09380f993099a7ea64d4b0b47d714af
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2017-12-25 18:11:41 +0100

    Add sha3 (aka. Keccak) hashes to hashlib.

--- lib-python/3/hashlib.py
+++ lib-python/3/hashlib.py
@@ -57,10 +57,8 @@ More condensed:
 # always available algorithm is added.
 __always_supported = ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512',
                       'blake2b', 'blake2s',
-                      # FIXME the following algorithms were added in
-                      # cpython3.6 but are missing in pypy
-                      # 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
-                      # 'shake_128', 'shake_256'
+                      'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
+                      'shake_128', 'shake_256'
 )
 
 
