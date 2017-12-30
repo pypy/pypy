68f74a9f2b1dcb4ed3f0ccf5927b7ad8a474a633
Matti Picus <matti.picus@gmail.com>
2017-12-30 19:23:11 +0200

    pypy uses bin not Scripts, https://github.com/pypa/virtualenv/issues/789

--- testrunner/get_info.py
+++ testrunner/get_info.py
@@ -8,14 +8,21 @@ import os
 import json
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-TARGET_BASENAME = 'pypy-c'
+if sys.platform.startswith('win'):
+    TARGET_NAME = r'pypy-c.exe'
+    # see https://github.com/pypa/virtualenv/issues/789
+    TARGET_DIR = 'bin'
+else:
+    TARGET_NAME = 'pypy3-c'
+    TARGET_DIR = 'bin'
+VENV_DIR = 'pypy-venv'
 
 def make_info_dict():
-    target = TARGET_BASENAME
-    if sys.platform.startswith('win'):
-        target += '.exe'
-    target_path = os.path.join(BASE_DIR, 'pypy', 'goal', target)
-    return {'target_path': target_path}
+    target_path = os.path.join(BASE_DIR, 'pypy', 'goal', TARGET_NAME)
+    return {'target_path': target_path,
+            'virt_pypy': os.path.join(VENV_DIR, TARGET_DIR, TARGET_NAME),
+            'venv_dir': VENV_DIR,
+           }
 
 def dump_info():
     return json.dumps(make_info_dict())
