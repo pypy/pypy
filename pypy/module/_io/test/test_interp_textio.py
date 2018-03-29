759ce7c930c6e4c19f644fc6e77ba5b5799db5a2
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2018-03-29 11:47:24 +0200

    try to see whether disk I/O is the culprit

--- pypy/module/_io/test/test_interp_textio.py
+++ pypy/module/_io/test/test_interp_textio.py
@@ -29,7 +29,7 @@ def st_readline(draw, st_nlines=st.integers(min_value=0, max_value=10)):
 
 @given(data=st_readline(),
        mode=st.sampled_from(['\r', '\n', '\r\n', '']))
-@settings(deadline=None)
+@settings(deadline=None, database=None)
 def test_readline(space, data, mode):
     txt, limits = data
     w_stream = W_BytesIO(space)
