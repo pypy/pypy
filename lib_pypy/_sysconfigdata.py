9ba00aef0d2b09220a80af03b7d3be1950c09b73
Yusuke Tsutsumi <yusuket@tsutsumi.io>
2018-04-27 22:30:27 -0700

    Addressing Code Review Feedback
    Refactoring to move the init_posix code to a central location,
    inside of sysconfigdata.
    
    Including an improved clause for detecting if gcc is installed on the machine,
    and using a correct build argument.

--- lib_pypy/_sysconfigdata.py
+++ lib_pypy/_sysconfigdata.py
@@ -1,5 +1,6 @@
 import _imp
 import os
+import sys
 from distutils.spawn import find_executable
 
 so_ext = _imp.extension_suffixes()[0]
@@ -8,13 +9,40 @@ build_time_vars = {
     "EXT_SUFFIX": so_ext,
     "SHLIB_SUFFIX": so_ext,
     "SOABI": '-'.join(so_ext.split('.')[1].split('-')[:2]),
-    "SO": so_ext  # deprecated in Python 3, for backward compatibility
+    "SO": so_ext,  # deprecated in Python 3, for backward compatibility
+    'CC': "cc -pthread",
+    'CXX': "c++ -pthread",
+    'OPT': "-DNDEBUG -O2",
+    'CFLAGS': "-DNDEBUG -O2",
+    'CCSHARED': "-fPIC",
+    'LDSHARED': "cc -pthread -shared",
+    'EXT_SUFFIX': so_ext,
+    'SHLIB_SUFFIX': ".so",
+    'AR': "ar",
+    'ARFLAGS': "rc",
+    'EXE': "",
+    'LIBDIR': os.path.join(sys.prefix, 'lib'),
+    'VERSION': sys.version[:3]
 }
 
-cc_compiler_path = os.path.realpath(find_executable("cc"))
-cc_compiler = os.path.basename(cc_compiler_path)
-build_time_vars["CC"] = cc_compiler
-if "gcc" in cc_compiler or "g++" in cc_compiler:
-    # If we used the gnu compiler, we can safely assume we are using the gnu
-    # linker
-    build_time_vars["GNULD"] = "yes"
+if sys.platform[:6] == "darwin":
+    import platform
+    if platform.machine() == 'i386':
+        if platform.architecture()[0] == '32bit':
+            arch = 'i386'
+        else:
+            arch = 'x86_64'
+    else:
+        # just a guess
+        arch = platform.machine()
+    build_time_vars['LDSHARED'] += ' -undefined dynamic_lookup'
+    build_time_vars['CC'] += ' -arch %s' % (arch,)
+
+if find_executable("gcc"):
+    build_time_vars.update({
+        "CC": "gcc -pthread",
+        "GNULD": "yes",
+        "LDSHARED": "gcc -pthread -shared",
+    })
+    if find_executable("g++"):
+        build_time_vars["CXX"] = "g++ -pthread"
