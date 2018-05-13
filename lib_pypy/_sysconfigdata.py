80768d8160d1272064146340a9ba72ab005b9372
Armin Rigo <arigo@tunes.org>
2018-05-13 10:48:19 +0200

    Remove these two entries because they appear also later. In the case of SHLIB_SUFFIX, it appears with a different value. Looking at the history I think we want to keep the second one.

--- lib_pypy/_sysconfigdata.py
+++ lib_pypy/_sysconfigdata.py
@@ -7,8 +7,6 @@ so_ext = _imp.extension_suffixes()[0]
 
 
 build_time_vars = {
-    "EXT_SUFFIX": so_ext,
-    "SHLIB_SUFFIX": so_ext,
     "SOABI": '-'.join(so_ext.split('.')[1].split('-')[:2]),
     "SO": so_ext,  # deprecated in Python 3, for backward compatibility
     'CC': "cc -pthread",
