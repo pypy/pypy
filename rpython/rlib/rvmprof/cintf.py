c278ef9383075158e5b0560bd18e72fcd7f345a1
Armin Rigo <arigo@tunes.org>
2018-07-23 17:33:02 +0200

    Issue #2853
    Patch by Babak F.

--- rpython/rlib/rvmprof/cintf.py
+++ rpython/rlib/rvmprof/cintf.py
@@ -17,7 +17,7 @@ class VMProfPlatformUnsupported(Exception):
 
 # vmprof works only on x86 for now
 IS_SUPPORTED = False
-if sys.platform in ('darwin', 'linux', 'linux2', 'freebsd10', 'freebsd11'):
+if sys.platform in ('darwin', 'linux', 'linux2') or sys.platform.startswith('freebsd'):
     try:
         IS_SUPPORTED = detect_cpu.autodetect().startswith('x86')
     except detect_cpu.ProcessorAutodetectError:
