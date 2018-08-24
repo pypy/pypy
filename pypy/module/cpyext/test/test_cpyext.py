86965b7108ac8eb19683e9c8079095a2e3896bf3
Matti Picus <matti.picus@gmail.com>
2018-08-24 14:09:15 +0300

    re-enable _cffi_backend, preload_builtins still disabled

--- pypy/module/cpyext/test/test_cpyext.py
+++ pypy/module/cpyext/test/test_cpyext.py
@@ -138,7 +138,7 @@ class LeakCheckingTest(object):
     """Base class for all cpyext tests."""
     spaceconfig = dict(usemodules=['cpyext', 'thread', 'struct', 'array',
                                    'itertools', 'time', 'binascii', 'mmap',
-                                   #'_cffi_backend',
+                                   '_cffi_backend',
                                    ])
 
     @classmethod
