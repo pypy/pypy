ff3c1a186eed04cc6a84d3d39e970648edc07fa6
Matti Picus <matti.picus@gmail.com>
2018-09-02 11:51:24 +0200

    revert 58568f219c61

--- rpython/rlib/rutf8.py
+++ rpython/rlib/rutf8.py
@@ -50,7 +50,6 @@ def unichr_as_utf8(code, allow_surrogates=False):
                 chr((0x80 | ((code >> 12) & 0x3f))) +
                 chr((0x80 | ((code >> 6) & 0x3f))) +
                 chr((0x80 | (code & 0x3f))))
-    return '?'
     raise ValueError
 
 @try_inline
