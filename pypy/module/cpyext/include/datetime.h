a309eda2e990489a5e003e1a35f593f0fa8ca485
ashwinahuja <unknown>
2018-09-13 17:14:59 +0100

    Making datetime objects more compatible with old C extensions written for CPython

--- pypy/module/cpyext/include/datetime.h
+++ pypy/module/cpyext/include/datetime.h
@@ -6,6 +6,18 @@ extern "C" {
 
 #include "cpyext_datetime.h"
 
+/* # of bytes for year, month, and day. */
+#define _PyDateTime_DATE_DATASIZE 4
+
+/* # of bytes for hour, minute, second, and usecond. */
+#define _PyDateTime_TIME_DATASIZE 6
+
+/* # of bytes for year, month, day, hour, minute, second, and usecond. */
+#define _PyDateTime_DATETIME_DATASIZE 10
+
+/* "magic" constant used to partially protect against developer mistakes. */
+#define DATETIME_API_MAGIC 0x414548d5
+
 PyAPI_DATA(PyDateTime_CAPI*) PyDateTimeAPI;
 
 #define PyDateTime_IMPORT (PyDateTimeAPI = _PyDateTime_Import())
@@ -18,13 +30,44 @@ PyAPI_DATA(PyDateTime_CAPI*) PyDateTimeAPI;
     PyDateTimeAPI->DateTime_FromDateAndTime(year, month, day, hour, \
         min, sec, usec, Py_None, PyDateTimeAPI->DateTimeType)
 
+#define PyDateTime_FromDateAndTimeAndFold(year, month, day, hour, min, sec, usec, fold) \
+    PyDateTimeAPI->DateTime_FromDateAndTimeAndFold(year, month, day, hour, \
+        min, sec, usec, Py_None, fold, PyDateTimeAPI->DateTimeType)
+
 #define PyTime_FromTime(hour, minute, second, usecond) \
     PyDateTimeAPI->Time_FromTime(hour, minute, second, usecond, \
         Py_None, PyDateTimeAPI->TimeType)
 
+#define PyTime_FromTimeAndFold(hour, minute, second, usecond, fold) \
+    PyDateTimeAPI->Time_FromTimeAndFold(hour, minute, second, usecond, \
+        Py_None, fold, PyDateTimeAPI->TimeType)
+
 #define PyDelta_FromDSU(days, seconds, useconds) \
     PyDateTimeAPI->Delta_FromDelta(days, seconds, useconds, 1, \
         PyDateTimeAPI->DeltaType)
+
+#ifdef Py_BUILD_CORE
+
+/* Macros for type checking when building the Python core. */
+#define PyDate_Check(op) PyObject_TypeCheck(op, &PyDateTime_DateType)
+#define PyDate_CheckExact(op) (Py_TYPE(op) == &PyDateTime_DateType)
+
+#define PyDateTime_Check(op) PyObject_TypeCheck(op, &PyDateTime_DateTimeType)
+#define PyDateTime_CheckExact(op) (Py_TYPE(op) == &PyDateTime_DateTimeType)
+
+#define PyTime_Check(op) PyObject_TypeCheck(op, &PyDateTime_TimeType)
+#define PyTime_CheckExact(op) (Py_TYPE(op) == &PyDateTime_TimeType)
+
+#define PyDelta_Check(op) PyObject_TypeCheck(op, &PyDateTime_DeltaType)
+#define PyDelta_CheckExact(op) (Py_TYPE(op) == &PyDateTime_DeltaType)
+
+#define PyTZInfo_Check(op) PyObject_TypeCheck(op, &PyDateTime_TZInfoType)
+#define PyTZInfo_CheckExact(op) (Py_TYPE(op) == &PyDateTime_TZInfoType)
+
+#else
+
+#endif  /* Py_BUILD_CORE */
+
 
 #ifdef __cplusplus
 }
--- pypy/module/cpyext/parse/cpyext_datetime.h
+++ pypy/module/cpyext/parse/cpyext_datetime.h
@@ -1,3 +1,15 @@
+/* # of bytes for year, month, and day. */
+#define _PyDateTime_DATE_DATASIZE 4
+
+/* # of bytes for hour, minute, second, and usecond. */
+#define _PyDateTime_TIME_DATASIZE 6
+
+/* # of bytes for year, month, day, hour, minute, second, and usecond. */
+#define _PyDateTime_DATETIME_DATASIZE 10
+
+/* "magic" constant used to partially protect against developer mistakes. */
+#define DATETIME_API_MAGIC 0x414548d5
+
 /* Define structure for C API. */
 typedef struct {
     /* type objects */
@@ -13,11 +25,20 @@ typedef struct {
         PyObject*, PyTypeObject*);
     PyObject *(*Time_FromTime)(int, int, int, int, PyObject*, PyTypeObject*);
     PyObject *(*Delta_FromDelta)(int, int, int, int, PyTypeObject*);
+    PyObject *(*TimeZone_FromTimeZone)(PyObject *offset, PyObject *name);
+
+    PyObject *(*DateTime_FromTimestamp)(PyObject*, PyObject*, PyObject*);
+    PyObject *(*Date_FromTimestamp)(PyObject*, PyObject*);
+
+        PyObject *(*DateTime_FromDateAndTimeAndFold)(int, int, int, int, int, int, int,
+        PyObject*, int, PyTypeObject*);
+    PyObject *(*Time_FromTimeAndFold)(int, int, int, int, PyObject*, int, PyTypeObject*);
 } PyDateTime_CAPI;
 
 typedef struct
 {
     PyObject_HEAD
+    long hashcode;
     int days;                   /* -MAX_DELTA_DAYS <= days <= MAX_DELTA_DAYS */
     int seconds;                /* 0 <= seconds < 24*3600 is invariant */
     int microseconds;           /* 0 <= microseconds < 1000000 is invariant */
@@ -31,22 +52,52 @@ typedef struct
     PyObject_HEAD
     unsigned char hastzinfo;
     PyObject *tzinfo;
-} PyDateTime_Time;
+} PyDateTime_Time; /* hastzinfo true */
+
+typedef struct 
+{
+    PyObject_HEAD
+    long hashcode;
+    char hastzinfo;
+    unsigned char data[_PyDateTime_TIME_DATASIZE];
+} PyDateTime_BaseTime; /* hastzinfo false */
+
+typedef struct 
+{
+    PyObject_HEAD
+    long hashcode;
+    char hastzinfo;
+    unsigned char data[_PyDateTime_DATETIME_DATASIZE];
+} PyDateTime_BaseDateTime; /* hastzinfo false */
 
 typedef struct
 {
     PyObject_HEAD
+    long hashcode;
     unsigned char hastzinfo;
+    unsigned char fold;
     PyObject *tzinfo;
-} PyDateTime_DateTime;
+    unsigned char data[_PyDateTime_DATETIME_DATASIZE];
+} PyDateTime_DateTime; /* hastzinfo true */
 
 
 typedef struct {
     PyObject_HEAD
+    long hashcode;
+    char hastzinfo;
+    unsigned char data[_PyDateTime_DATE_DATASIZE];
 } PyDateTime_Date;
 
 
 typedef struct {
     PyObject_HEAD
 } PyDateTime_TZInfo;
+
+typedef struct 
+{
+    PyObject_HEAD
+    long hashcode;
+    char hastzinfo;
+} _PyDateTime_BaseTZInfo;
+
 
