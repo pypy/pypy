9b45f5756478a4ac16a262a746590360badaf2bb
Armin Rigo <arigo@tunes.org>
2018-09-15 23:40:53 +0200

    Extra tests from cffi

--- pypy/module/_cffi_backend/test/_backend_test_c.py
+++ pypy/module/_cffi_backend/test/_backend_test_c.py
@@ -336,6 +336,16 @@ def test_newp_integer_types():
         assert newp(pp, max)[0] == max
         py.test.raises(OverflowError, newp, pp, min - 1)
         py.test.raises(OverflowError, newp, pp, max + 1)
+        py.test.raises(OverflowError, newp, pp, min - 1 - 2 ** 32)
+        py.test.raises(OverflowError, newp, pp, min - 1 - 2 ** 64)
+        py.test.raises(OverflowError, newp, pp, min - 1 + 2 ** 32)
+        py.test.raises(OverflowError, newp, pp, min - 1 + 2 ** 64)
+        py.test.raises(OverflowError, newp, pp, max + 1)
+        py.test.raises(OverflowError, newp, pp, max + 1 - 2 ** 32)
+        py.test.raises(OverflowError, newp, pp, max + 1 - 2 ** 64)
+        py.test.raises(OverflowError, newp, pp, max + 1 + 2 ** 32)
+        py.test.raises(OverflowError, newp, pp, max + 1 + 2 ** 64)
+        py.test.raises(TypeError, newp, pp, 1.0)
     for name in ['char', 'short', 'int', 'long', 'long long']:
         p = new_primitive_type('unsigned ' + name)
         pp = new_pointer_type(p)
