d59861ca300241942c49c5fc74bbd92bddd81b2a
Matti Picus <matti.picus@gmail.com>
2018-10-16 14:12:25 +0300

    use unicode length, not utf8 length

--- pypy/module/cpyext/unicodeobject.py
+++ pypy/module/cpyext/unicodeobject.py
@@ -72,7 +72,8 @@ def new_empty_unicode(space, length):
 def unicode_attach(space, py_obj, w_obj, w_userdata=None):
     "Fills a newly allocated PyUnicodeObject with a unicode string"
     value = space.utf8_w(w_obj)
-    set_wsize(py_obj, len(value))
+    length = space.len_w(w_obj)
+    set_wsize(py_obj, length)
     set_wbuffer(py_obj, lltype.nullptr(rffi.CWCHARP.TO))
     _readify(space, py_obj, value)
 
