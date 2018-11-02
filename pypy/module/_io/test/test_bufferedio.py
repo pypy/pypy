1bf801fdda42bbffe6e8a86e53d4098ba441787a
Matti Picus <matti.picus@gmail.com>
2018-11-02 20:09:43 +0200

    silence print statements in tests

--- pypy/module/_io/test/test_bufferedio.py
+++ pypy/module/_io/test/test_bufferedio.py
@@ -169,12 +169,12 @@ class AppTestBufferedReader:
             #
             exc = raises(TypeError, readinto, u"hello")
             msg = str(exc.value)
-            print(msg)
+            # print(msg)
             assert " read-write b" in msg and msg.endswith(", not str")
             #
             exc = raises(TypeError, readinto, memoryview(b"hello"))
             msg = str(exc.value)
-            print(msg)
+            # print(msg)
             assert " read-write b" in msg and msg.endswith(", not memoryview")
             #
             f.close()
