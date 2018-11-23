5a0f8984e412599f876f317f32e420ea4b1a0bf0
Matti Picus <matti.picus@gmail.com>
2018-11-23 15:47:04 -0600

    export function from dll

--- pypy/module/cpyext/include/pyerrors.h
+++ pypy/module/cpyext/include/pyerrors.h
@@ -17,6 +17,7 @@ extern "C" {
 PyAPI_FUNC(PyObject *) PyErr_NewException(const char *name, PyObject *base, PyObject *dict);
 PyAPI_FUNC(PyObject *) PyErr_NewExceptionWithDoc(const char *name, const char *doc, PyObject *base, PyObject *dict);
 PyAPI_FUNC(PyObject *) PyErr_Format(PyObject *exception, const char *format, ...);
+PyAPI_FUNC(PyObject *) _PyErr_FormatFromCause(PyObject *exception, const char *format, ...);
 
 /* These APIs aren't really part of the error implementation, but
    often needed to format error messages; the native C lib APIs are
