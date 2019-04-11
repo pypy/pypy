dbf0f03a2b9f951f921871e146edafb82868aba4
Matti Picus <matti.picus@gmail.com>
2019-04-11 14:38:05 +0300

    update version to 7.1.1

--- pypy/module/sys/version.py
+++ pypy/module/sys/version.py
@@ -13,7 +13,7 @@ CPYTHON_API_VERSION        = 1013   #XXX # sync with include/modsupport.h
 # make sure to keep PYPY_VERSION in sync with:
 #    module/cpyext/include/patchlevel.h
 #    doc/conf.py
-PYPY_VERSION               = (7, 1, 0, "beta", 0)
+PYPY_VERSION               = (7, 1, 1, "beta", 0)
 
 
 import pypy
