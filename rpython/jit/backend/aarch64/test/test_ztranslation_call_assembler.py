b3b8473119b491fd6343f1f3c0a818a01a169ffb
Maciej Fijalkowski <fijall@gmail.com>
2019-06-27 17:06:17 +0000

    fix tests

--- rpython/jit/backend/aarch64/test/test_ztranslation_call_assembler.py
+++ rpython/jit/backend/aarch64/test/test_ztranslation_call_assembler.py
@@ -1,7 +1,4 @@
 from rpython.jit.backend.llsupport.test.ztranslation_test import TranslationTestCallAssembler
-from rpython.translator.translator import TranslationContext
-from rpython.config.translationoption import DEFL_GC
-from rpython.jit.backend.x86.arch import WORD
 import sys
 
 
@@ -14,10 +11,5 @@ if sys.platform == 'win32':
     py.test.skip("crashes on Windows (Boehm issue?)")
 
 
-class TestTranslationCallAssemblerX86(TranslationTestCallAssembler):
-    def _check_cbuilder(self, cbuilder):
-        #We assume here that we have sse2.  If not, the CPUClass
-        # needs to be changed to CPU386_NO_SSE2, but well.
-        if WORD == 4 and sys.platform != 'win32':
-            assert '-msse2' in cbuilder.eci.compile_extra
-            assert '-mfpmath=sse' in cbuilder.eci.compile_extra
+class TestTranslationCallAssemblerAarch64(TranslationTestCallAssembler):
+    pass
