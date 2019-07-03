3d8988cb71b84ec80d655fa7da560ace880a5fcf
fijal <unknown>
2019-07-03 10:02:38 +0200

    remove a refcount test and add a release gil zrpy test

--- rpython/jit/backend/aarch64/test/test_ztranslation_external_exception.py
+++
@@ -1,9 +0,0 @@
-from rpython.jit.backend.llsupport.test.ztranslation_test import TranslationRemoveTypePtrTest
-from rpython.translator.translator import TranslationContext
-
-class TestTranslationRemoveTypePtrAarch64(TranslationRemoveTypePtrTest):
-    def _get_TranslationContext(self):
-        t = TranslationContext()
-        t.config.translation.list_comprehension_operations = True
-        t.config.translation.gcremovetypeptr = True
-        return t
