d8800d392b1ef41ccb811b816c00fdc1155a732a
Armin Rigo <arigo@tunes.org>
2019-07-16 16:34:57 +0200

    Remove tests for startswith/endswith from test_hypo_index_find: 7b256d2286ec should cover these cases and give the same result as CPython3.6

--- pypy/objspace/std/test/test_unicodeobject.py
+++ pypy/objspace/std/test/test_unicodeobject.py
@@ -95,38 +95,6 @@ class TestUnicodeObject:
                                         space.newint(start + len1))
             assert space.int_w(w_index) == rexpected
 
-            expected = u.startswith(v, start)
-            if expected and start > len(u):
-                expected = False # python2 vs. python3
-            w_res = space.call_method(w_u, 'startswith', w_v,
-                                      space.newint(start))
-            assert w_res is space.newbool(expected)
-
-            expected = u.startswith(v, start, start + len1)
-            if ((expected and start > len(u)) or 
-                    (start > 0 and start + len1 ==0)): 
-                expected = False # python2 vs. python3
-            w_res = space.call_method(w_u, 'startswith', w_v,
-                                      space.newint(start),
-                                      space.newint(start + len1))
-            assert w_res is space.newbool(expected)
-
-            expected = u.endswith(v, start)
-            if expected and start > len(u):
-                expected = False # python2 vs. python3
-            w_res = space.call_method(w_u, 'endswith', w_v,
-                                      space.newint(start))
-            assert w_res is space.newbool(expected)
-
-            expected = u.endswith(v, start, start + len1)
-            if ((expected and start > len(u)) or 
-                    (start > 0 and start + len1 ==0)): 
-                expected = False # python2 vs. python3
-            w_res = space.call_method(w_u, 'endswith', w_v,
-                                      space.newint(start),
-                                      space.newint(start + len1))
-            assert w_res is space.newbool(expected)
-
 
 class AppTestUnicodeStringStdOnly:
     def test_compares(self):
