a24683f910273f5ed1fc76c1b1e8782f189426f4
Armin Rigo <arigo@tunes.org>
2019-07-20 20:54:40 +0200

    Update docstring

--- lib_pypy/faulthandler.py
+++ lib_pypy/faulthandler.py
@@ -1,6 +1,5 @@
-# This is only imported for platforms where the built-in faulthandler module is not 
-# available.  It provides no function at all so far, but it is enough to start the
-# CPython test suite.
+# This is a dummy module imported for platforms where the built-in
+# faulthandler module is not available.
 
 def enable(*args, **kwargs):
     pass
