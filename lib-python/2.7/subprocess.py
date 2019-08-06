e97adc6678f070581ab62f6ef089a02cd6f308fc
Armin Rigo <arigo@tunes.org>
2019-08-06 14:17:32 +0200

    Issue #3050
    subprocess: accept arguments that are not directly subscriptable (like
    iterators)

--- lib-python/2.7/subprocess.py
+++ lib-python/2.7/subprocess.py
@@ -337,7 +337,7 @@ class Popen(object):
 
         # --- PyPy hack, see _pypy_install_libs_after_virtualenv() ---
         # match arguments passed by different versions of virtualenv
-        if args[1:] in (
+        if isinstance(args, (list, tuple)) and args[1:] in (
             ['-c', 'import sys; print(sys.prefix)'],        # 1.6 10ba3f3c
             ['-c', "\nimport sys\nprefix = sys.prefix\n"    # 1.7 0e9342ce
              "if sys.version_info[0] == 3:\n"
