010d72b65ea828f294abae176166545a7c122b34
Armin Rigo <arigo@tunes.org>
2019-08-24 15:26:26 +0200

    Remove these two modules after all

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -47,7 +47,7 @@ working_modules.update([
 # --sandbox
 sandbox_modules = default_modules.copy()
 sandbox_modules.update([
-    "array", "binascii", "_socket", "select",
+    "array", "binascii",
 ])
 
 import rpython.rlib.rvmprof.cintf
