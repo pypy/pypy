4575d8fb92d78fb4db2846f59893cccdb5651d1b
Ronan Lamy <ronan.lamy@gmail.com>
2019-08-29 17:52:27 +0100

    _thread is required by importlib now

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -17,6 +17,7 @@ all_modules = [p.basename for p in modulepath.listdir()
 essential_modules = set([
     "exceptions", "_io", "sys", "builtins", "posix", "_warnings",
     "itertools", "_frozen_importlib", "operator", "_locale", "struct",
+    "thread",
 ])
 if sys.platform == "win32":
     essential_modules.add("_winreg")
@@ -36,7 +37,7 @@ working_modules.update([
     "_socket", "unicodedata", "mmap", "fcntl", "pwd",
     "select", "zipimport", "_lsprof", "signal", "_rawffi", "termios",
     "zlib", "bz2", "_md5", "_minimal_curses",
-    "thread", "itertools", "pyexpat", "cpyext", "array",
+    "itertools", "pyexpat", "cpyext", "array",
     "binascii", "_multiprocessing", '_warnings', "_collections",
     "_multibytecodec", "_continuation", "_cffi_backend",
     "_csv", "_pypyjson", "_posixsubprocess", "_cppyy", # "micronumpy",
