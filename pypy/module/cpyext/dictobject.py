08d446695fb436c4ed0169ab6ba93fe78a4eb15d
Ronan Lamy <ronan.lamy@gmail.com>
2019-10-04 18:02:31 +0100

    Add _PyDict_GetItemWithError (part of the public API in py3) (grafted from d663ce56919c62a2bf5e242ee79b8bc3c640662a)

--- pypy/module/cpyext/dictobject.py
+++ pypy/module/cpyext/dictobject.py
@@ -80,6 +80,13 @@ def PyDict_GetItem(space, w_dict, w_key):
     # XXX this is wrong with IntMutableCell.  Hope it works...
     return w_dict.getitem(w_key)
 
+@cpython_api([PyObject, PyObject], PyObject, result_borrowed=True)
+def _PyDict_GetItemWithError(space, w_dict, w_key):
+    # Like PyDict_GetItem(), but doesn't swallow the error
+    if not isinstance(w_dict, W_DictMultiObject):
+        PyErr_BadInternalCall(space)
+    return w_dict.getitem(w_key)
+
 @cpython_api([PyObject, PyObject, PyObject], rffi.INT_real, error=-1)
 def PyDict_SetItem(space, w_dict, w_key, w_obj):
     if not isinstance(w_dict, W_DictMultiObject):
