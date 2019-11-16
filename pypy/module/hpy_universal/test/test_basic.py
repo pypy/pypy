93317e3e2d828824b0f90743116f54c4a8b4ac7f
Armin Rigo <arigo@tunes.org>
2019-11-16 19:12:56 +0100

    Put the next test to pass

--- pypy/module/hpy_universal/test/test_basic.py
+++ pypy/module/hpy_universal/test/test_basic.py
@@ -21,3 +21,21 @@ class AppTestBasic(HPyTest):
         assert mod.__loader__.name == 'mytest'
         assert mod.__spec__.loader is mod.__loader__
         assert mod.__file__
+
+    def test_different_name(self):
+        mod = self.make_module("""
+            @INIT
+        """, name="foo")
+        assert mod.__name__ == "foo"
+
+    def test_noop_function(self):
+        mod = self.make_module("""
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy args)
+            {
+                return HPyNone_Get(ctx);
+            }
+            @EXPORT f METH_NOARGS
+            @INIT
+        """)
+        assert mod.f() is None
