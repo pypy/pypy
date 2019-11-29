86ce1ccb7011d3f869bd23d3790a53e60cc6a5a1
Antonio Cuni <anto.cuni@gmail.com>
2019-11-29 18:07:03 +0100

    implement HPyDict_{New,SetItem}, test_hpydict passes now

---
+++ pypy/module/hpy_universal/interp_dict.py
@@ -0,0 +1,20 @@
+from rpython.rtyper.lltypesystem import rffi, lltype
+from pypy.interpreter.error import OperationError, oefmt
+from pypy.objspace.std.dictmultiobject import W_DictMultiObject
+from pypy.module.hpy_universal.apiset import API
+from pypy.module.hpy_universal import handles
+
+@API.func("HPy HPyDict_New(HPyContext ctx)")
+def HPyDict_New(space, ctx):
+    w_dict = space.newdict()
+    return handles.new(space, w_dict)
+
+@API.func("int HPyDict_SetItem(HPyContext ctx, HPy h_dict, HPy h_key, HPy h_val)")
+def HPyDict_SetItem(space, ctx, h_dict, h_key, h_val):
+    w_dict = handles.deref(space, h_dict)
+    # XXX the tests should check what happens in this case
+    assert isinstance(w_dict, W_DictMultiObject)
+    w_key = handles.deref(space, h_key)
+    w_val = handles.deref(space, h_val)
+    w_dict.setitem(w_key, w_val)
+    return rffi.cast(rffi.INT_real, 0)
--- pypy/module/hpy_universal/test/test_hpydict.py
+++ pypy/module/hpy_universal/test/test_hpydict.py
@@ -1,6 +1,6 @@
 import pytest
-from pypy.module.hpy_universal._vendored.test.test_dictobject import TestDictObject as _TestDictobject
+from pypy.module.hpy_universal._vendored.test.test_hpydict import TestDict as _Test
 from .support import HPyAppTest
 
-class AppTestDictObject(HPyAppTest, _TestDictObject):
+class AppTestDict(HPyAppTest, _Test):
     spaceconfig = {'usemodules': ['hpy_universal']}
