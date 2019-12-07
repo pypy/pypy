5cd559868301d7adcdd620f6b40c9aa37045f6aa
Matti Picus <matti.picus@gmail.com>
2019-12-07 19:35:45 +0200

    fix ncurses include/lib discovery: add from_config_tool("ncursesw6-config"), reverse order

--- pypy/module/_minimal_curses/fficurses.py
+++ pypy/module/_minimal_curses/fficurses.py
@@ -5,13 +5,6 @@ from rpython.rtyper.lltypesystem import rffi
 from rpython.rtyper.tool import rffi_platform
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
 
-# We cannot trust ncurses5-config, it's broken in various ways in
-# various versions.  For example it might not list -ltinfo even though
-# it's needed, or --cflags might be completely empty. Crap.
-
-except IOError:
-    pass
-
 def try_cflags():
     yield ExternalCompilationInfo(includes=['curses.h', 'term.h'])
     yield ExternalCompilationInfo(includes=['curses.h', 'term.h'],
@@ -33,7 +26,11 @@ def try_ldflags():
 
 def try_tools():
     try:
-        yield ExternalCompilationInfo.from_pkg_config("ncurses")
+        yield ExternalCompilationInfo.from_config_tool("ncursesw6-config")
+    except Exception:
+        pass
+    try:
+        yield ExternalCompilationInfo.from_config_tool("ncurses5-config")
     except Exception:
         pass
     try:
@@ -41,7 +38,7 @@ def try_tools():
     except Exception:
         pass
     try:
-        yield ExternalCompilationInfo.from_config_tool("ncurses5-config")
+        yield ExternalCompilationInfo.from_pkg_config("ncursesw")
     except Exception:
         pass
 
