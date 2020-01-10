33d84ca8067cd273dac3cc7e5d44f33e51b0d181
Antonio Cuni <anto.cuni@gmail.com>
2020-01-10 18:04:35 +0100

    implement HPy_AsPyObject

--- pypy/module/hpy_universal/interp_cpy_compat.py
+++ pypy/module/hpy_universal/interp_cpy_compat.py
@@ -2,14 +2,15 @@ from rpython.rtyper.lltypesystem import lltype, rffi
 from pypy.interpreter.error import OperationError, oefmt
 from pypy.module.hpy_universal.apiset import API
 from pypy.module.hpy_universal import handles
-
+from pypy.module.cpyext import pyobject
 
 @API.func("HPy HPy_FromPyObject(HPyContext ctx, void *obj)")
 def HPy_FromPyObject(space, ctx, obj):
-    from pypy.module.cpyext import pyobject
     w_obj = pyobject.from_ref(space, rffi.cast(pyobject.PyObject, obj))
     return handles.new(space, w_obj)
 
 @API.func("void *HPy_AsPyObject(HPyContext ctx, HPy h)")
 def HPy_AsPyObject(space, ctx, h):
-    import pdb;pdb.set_trace()
+    w_obj = handles.deref(space, h)
+    pyobj = pyobject.make_ref(space, w_obj)
+    return rffi.cast(rffi.VOIDP, pyobj)
