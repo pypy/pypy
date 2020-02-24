c65f637a39f5e1a20e980689f17e0bf95a61ed21
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2020-02-24 13:37:05 +0100

    ouch, it's *not* safe to promote list strategies, due to SizeListStrategy. use the type as the green key instead

--- pypy/objspace/std/listobject.py
+++ pypy/objspace/std/listobject.py
@@ -131,15 +131,15 @@ def get_strategy_from_list_objects(space, list_w, sizehint):
     return space.fromcache(ObjectListStrategy)
 
 
-def _get_printable_location(strategy, greenkey):
+def _get_printable_location(strategy_type, greenkey):
     return 'list__do_extend_from_iterable [%s, %s]' % (
-        strategy,
+        strategy_type,
         greenkey.iterator_greenkey_printable())
 
 
 _do_extend_jitdriver = jit.JitDriver(
     name='list__do_extend_from_iterable',
-    greens=['strategy', 'greenkey'],
+    greens=['strategy_type', 'greenkey'],
     reds='auto',
     get_printable_location=_get_printable_location)
 
@@ -150,7 +150,7 @@ def _do_extend_from_iterable(space, w_list, w_iterable):
     while True:
         _do_extend_jitdriver.jit_merge_point(
                 greenkey=greenkey,
-                strategy = w_list.strategy)
+                strategy_type=type(w_list.strategy))
         try:
             w_list.append(space.next(w_iterator))
         except OperationError as e:
@@ -749,9 +749,9 @@ class W_ListObject(W_Root):
         if mucked:
             raise oefmt(space.w_ValueError, "list modified during sort")
 
-def get_printable_location(strategy, tp):
-    return "list.find [%s, %s]" % (strategy, tp.getname(tp.space), )
-find_jmp = jit.JitDriver(greens = ['strategy', 'tp'], reds = 'auto', name = 'list.find', get_printable_location=get_printable_location)
+def get_printable_location(strategy_type, tp):
+    return "list.find [%s, %s]" % (strategy_type, tp.getname(tp.space), )
+find_jmp = jit.JitDriver(greens=['strategy_type', 'tp'], reds='auto', name='list.find', get_printable_location=get_printable_location)
 
 class ListStrategy(object):
 
@@ -779,7 +779,7 @@ class ListStrategy(object):
         # needs to be safe against eq_w mutating stuff
         tp = space.type(w_item)
         while i < stop and i < w_list.length():
-            find_jmp.jit_merge_point(tp=tp, strategy=self)
+            find_jmp.jit_merge_point(tp=tp, strategy_type=type(self))
             if space.eq_w(w_item, w_list.getitem(i)):
                 return i
             i += 1
