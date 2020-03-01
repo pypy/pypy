dd90228fd4ce1e25658adeb84d698b3171588fe1
quejebo <quejebo@gmail.com>
2020-03-01 14:21:33 -0800

    Convert apptest descroperation to new format.  Split test_binop_overriding such that all tests depending on external object are moved to extra_tests, and remaining applevel test is converted to new format.

--- extra_tests/test_binop_overriding.py
+++ extra_tests/test_binop_overriding.py
@@ -85,22 +85,6 @@ class AppTestBinopCombinations:
         cls.w_helpers = w_helpers
         cls.w_appdirect = cls.space.wrap(option.runappdirect)
 
-    def test_overriding_base_binop_explict(self):
-        class MulBase(object):
-            def __init__(self, value):
-                self.value = value
-            def __mul__(self, other):
-                return self.value * other.value
-            def __rmul__(self, other):
-                return other.value * self.value
-        class DoublerBase(MulBase):
-            def __mul__(self, other):
-                return 2 * (self.value * other.value)
-        class AnotherDoubler(DoublerBase):
-            pass
-        res = DoublerBase(2) * AnotherDoubler(3)
-        assert res == 12
-
     def test_binop_combinations_mul(self):
         if not self.appdirect:
             skip("slow test, should be run as appdirect test")
