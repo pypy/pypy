8af1aa179170c11d17be9ae0e090e3cbf3e7ee63
Antonio Cuni <anto.cuni@gmail.com>
2020-03-03 20:49:14 +0100

    (ab)use cpu._debug_errno_container to store also the thread_ident

--- rpython/jit/backend/llsupport/lltls.py
+++ rpython/jit/backend/llsupport/lltls.py
@@ -1,6 +1,9 @@
 from rpython.rlib import rthread
+from rpython.jit.backend.llsupport.symbolic import WORD
 
 
 def get_thread_ident_offset(cpu):
-    assert cpu.translate_support_code
-    return rthread.tlfield_thread_ident.getoffset()
+    if cpu.translate_support_code:
+        return rthread.tlfield_thread_ident.getoffset()
+    else:
+        return 1 * WORD
