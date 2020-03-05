3c9f7a3076781d9435dbb805a71b7cfc3d0d5cb6
Antonio Cuni <anto.cuni@gmail.com>
2020-03-05 09:14:33 +0100

    fix import

--- pypy/module/_hpy_universal/test/test_hpyunicode.py
+++ pypy/module/_hpy_universal/test/test_hpyunicode.py
@@ -1,5 +1,5 @@
 import pytest
-from pypy.module.hpy_universal._vendored.test.test_hpyunicode import TestUnicode as _Test
+from pypy.module._hpy_universal._vendored.test.test_hpyunicode import TestUnicode as _Test
 from .support import HPyAppTest
 
 class AppTestUnicode(HPyAppTest, _Test):
