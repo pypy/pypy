1770c4208b619c032d309d57fefcef3365864545
Manuel Jacob <me@manueljacob.de>
2020-03-07 18:51:19 +0100

    Use RawByteBuffer instead of ByteBuffer in buffered io classes when using split GC address space.

--- pypy/module/_io/interp_bufferedio.py
+++ pypy/module/_io/interp_bufferedio.py
@@ -9,7 +9,7 @@ from pypy.interpreter.typedef import (
 from pypy.interpreter.gateway import interp2app, unwrap_spec, WrappedDefault
 from pypy.interpreter.buffer import SimpleView
 
-from rpython.rlib.buffer import ByteBuffer, SubBuffer
+from rpython.rlib.buffer import ByteBuffer, RawByteBuffer, SubBuffer
 from rpython.rlib.rstring import StringBuilder
 from rpython.rlib.rarithmetic import r_longlong, intmask
 from rpython.rlib import rposix
@@ -152,7 +152,14 @@ class BufferedMixin:
             raise oefmt(space.w_ValueError,
                         "buffer size must be strictly positive")
 
-        self.buffer = ByteBuffer(self.buffer_size)
+        if space.config.translation.split_gc_address_space:
+            # When using split GC address space, it is not possible to get the
+            # raw address of a GC buffer. Therefore we use a buffer backed by
+            # raw memory.
+            self.buffer = RawByteBuffer(self.buffer_size)
+        else:
+            # TODO: test whether using the raw buffer is faster
+            self.buffer = ByteBuffer(self.buffer_size)
 
         self.lock = TryLock(space)
 
@@ -669,7 +676,7 @@ class BufferedMixin:
                 found = False
                 while pos < have:
                     # 'buffer.data[]' instead of 'buffer[]' because RPython...
-                    c = self.buffer.data[pos]
+                    c = self.buffer.getitem(pos)
                     pos += 1
                     if c == '\n':
                         self.pos = pos
