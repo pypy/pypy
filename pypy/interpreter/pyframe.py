b645dead15546d123b3adaf895c1b35008aaed4a
Ronan Lamy <ronan.lamy@gmail.com>
2020-03-20 17:22:29 +0000

    Reinstate workaround to ensure resetting sys.exc_info() on function exit (partially reverts 0057975cda68).

--- pypy/interpreter/pyframe.py
+++ pypy/interpreter/pyframe.py
@@ -256,7 +256,15 @@ class PyFrame(W_Root):
         if self._is_generator_or_coroutine():
             return self.initialize_as_generator(name, qualname)
         else:
-            return self.execute_frame()
+            # save and restore sys_exc_info, as an attempt to
+            # work around rare cases that can occur if RecursionError or
+            # MemoryError is raised at just the wrong place
+            executioncontext = self.space.getexecutioncontext()
+            exc_on_enter = executioncontext.sys_exc_info()
+            try:
+                return self.execute_frame()
+            finally:
+                executioncontext.set_sys_exc_info(exc_on_enter)
     run._always_inline_ = True
 
     def initialize_as_generator(self, name, qualname):
