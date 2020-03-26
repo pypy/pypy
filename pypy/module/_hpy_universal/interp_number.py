6cf0fb212f0d61a1e1d9a52704c77005c51db8f6
Antonio Cuni <anto.cuni@gmail.com>
2020-03-26 15:34:44 +0100

    update the context

--- pypy/module/_hpy_universal/interp_number.py
+++ pypy/module/_hpy_universal/interp_number.py
@@ -4,8 +4,8 @@ from pypy.module._hpy_universal.apiset import API
 from pypy.module._hpy_universal import handles
 
 
-@API.func("HPy HPyNumber_Add(HPyContext ctx, HPy x, HPy y)")
-def HPyNumber_Add(space, ctx, h1, h2):
+@API.func("HPy HPy_Add(HPyContext ctx, HPy x, HPy y)")
+def HPy_Add(space, ctx, h1, h2):
     w_obj1 = handles.deref(space, h1)
     w_obj2 = handles.deref(space, h2)
     w_result = space.add(w_obj1, w_obj2)
