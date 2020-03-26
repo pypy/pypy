ffbfed155e5bb6acb3d3b5b8456e0b6298bd0650
Antonio Cuni <anto.cuni@gmail.com>
2020-03-26 17:20:31 +0100

    implement all the functions which are needed to pass test_binary

--- pypy/module/_hpy_universal/interp_number.py
+++ pypy/module/_hpy_universal/interp_number.py
@@ -3,14 +3,6 @@ from pypy.interpreter.error import OperationError, oefmt
 from pypy.module._hpy_universal.apiset import API
 from pypy.module._hpy_universal import handles
 
-
-@API.func("HPy HPy_Add(HPyContext ctx, HPy x, HPy y)")
-def HPy_Add(space, ctx, h1, h2):
-    w_obj1 = handles.deref(space, h1)
-    w_obj2 = handles.deref(space, h2)
-    w_result = space.add(w_obj1, w_obj2)
-    return handles.new(space, w_result)
-
 def make_unary(name, spacemeth):
     assert spacemeth.startswith('space.')
     spacemeth = spacemeth[len('space.'):]
@@ -24,11 +16,40 @@ def make_unary(name, spacemeth):
     #
     globals()[name] = HPy_unary
 
+def make_binary(name, spacemeth):
+    assert spacemeth.startswith('space.')
+    spacemeth = spacemeth[len('space.'):]
+    #
+    @API.func("HPy HPy_binary(HPyContext ctx, HPy h1, HPy h2)", func_name=name)
+    def HPy_binary(space, ctx, h1, h2):
+        w_obj1 = handles.deref(space, h1)
+        w_obj2 = handles.deref(space, h2)
+        meth = getattr(space, spacemeth)
+        w_res = meth(w_obj1, w_obj2)
+        return handles.new(space, w_res)
+    #
+    globals()[name] = HPy_binary
+
+
 make_unary('HPy_Negative', 'space.neg')
 make_unary('HPy_Positive', 'space.pos')
 make_unary('HPy_Absolute', 'space.abs')
 make_unary('HPy_Invert', 'space.invert')
 make_unary('HPy_Index', 'space.index')
+
+make_binary('HPy_Add', 'space.add')
+make_binary('HPy_Subtract', 'space.sub')
+make_binary('HPy_Multiply', 'space.mul')
+make_binary('HPy_FloorDivide', 'space.floordiv')
+make_binary('HPy_TrueDivide', 'space.truediv')
+make_binary('HPy_Remainder', 'space.mod')
+make_binary('HPy_Divmod', 'space.divmod')
+make_binary('HPy_Lshift', 'space.lshift')
+make_binary('HPy_Rshift', 'space.rshift')
+make_binary('HPy_And', 'space.and_')
+make_binary('HPy_Xor', 'space.xor')
+make_binary('HPy_Or', 'space.or_')
+
 
 @API.func("HPy HPy_Long(HPyContext ctx, HPy h1)")
 def HPy_Long(space, ctx, h1):
@@ -44,17 +65,6 @@ def HPy_Float(space, ctx, h1):
     return handles.new(space, w_res)
 
 
-@API.func("HPy HPy_Subtract(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Subtract(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Multiply(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Multiply(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
 
 @API.func("HPy HPy_MatrixMultiply(HPyContext ctx, HPy h1, HPy h2)")
 def HPy_MatrixMultiply(space, ctx, h1, h2):
@@ -62,64 +72,9 @@ def HPy_MatrixMultiply(space, ctx, h1, h2):
     if NonConstant(False): return 0
     raise NotImplementedError
 
-@API.func("HPy HPy_FloorDivide(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_FloorDivide(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_TrueDivide(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_TrueDivide(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Remainder(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Remainder(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Divmod(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Divmod(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
 
 @API.func("HPy HPy_Power(HPyContext ctx, HPy h1, HPy h2, HPy h3)")
 def HPy_Power(space, ctx, h1, h2, h3):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-
-
-@API.func("HPy HPy_Lshift(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Lshift(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Rshift(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Rshift(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_And(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_And(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Xor(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Xor(space, ctx, h1, h2):
-    from rpython.rlib.nonconst import NonConstant # for the annotator
-    if NonConstant(False): return 0
-    raise NotImplementedError
-
-@API.func("HPy HPy_Or(HPyContext ctx, HPy h1, HPy h2)")
-def HPy_Or(space, ctx, h1, h2):
     from rpython.rlib.nonconst import NonConstant # for the annotator
     if NonConstant(False): return 0
     raise NotImplementedError
