b9d1490809a0a4b14b84432fc04dadf296c66570
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2020-04-13 15:02:24 +0200

    change benchmarking target

--- rpython/rlib/rsre/test/targetrsre.py
+++ rpython/rlib/rsre/test/targetrsre.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
+from rpython.rlib import jit
 from rpython.rlib.rarithmetic import intmask
-from rpython.rlib.rsre import rsre_core
-from rpython.rlib.rsre.rsre_char import MAXREPEAT
+from rpython.rlib.rsre import rsre_core, rsre_utf8, rpy
+from rpython.rlib.rsre.rsre_constants import MAXREPEAT
 import os, time
 
+r_code1 = rpy.get_code(ur"<item>\s*<title>(.*?)</title>")
+r_code2 = rpy.get_code(ur'[\w\.+-]+@[\w\.-]+\.[\w\.-]+')
+r_code3 = rpy.get_code(ur'[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?')
+r_code4 = rpy.get_code(ur'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])')
 
-# <item>\s*<title>(.*?)</title>
-r_code1 = [17, 18, 1, 21, 131091, 6, 6, 60, 105, 116, 101, 109, 62, 0,
-0, 0, 0, 0, 0, 19, 60, 19, 105, 19, 116, 19, 101, 19, 109, 19, 62, 29,
-9, 0, MAXREPEAT, 15, 4, 9, 2, 0, 1, 19, 60, 19, 116, 19, 105, 19, 116, 19,
-108, 19, 101, 19, 62, 21, 0, 31, 5, 0, MAXREPEAT, 2, 1, 21, 1, 19, 60, 19,
-47, 19, 116, 19, 105, 19, 116, 19, 108, 19, 101, 19, 62, 1]
-
+driver = jit.JitDriver(greens=["code"], reds="auto", is_recursive=True)
 
 def read(filename):
     fd = os.open(filename, os.O_RDONLY, 0666)
@@ -23,26 +22,33 @@ def read(filename):
     os.close(fd)
     return data
 
-def search_in_file(filename):
-    data = read(filename)
+def search_in_file(codenum, data):
+    code = [r_code1, r_code2, r_code3, r_code4][codenum - 1]
     p = 0
+    count = 0
     while True:
-        res = rsre_core.search(r_code1, data, p)
+        driver.jit_merge_point(code=code)
+        res = rsre_utf8.utf8search(code, data, p)
         if res is None:
             break
-        matchstart, matchstop = res.span(1)
+        matchstart, matchstop = res.span(0)
         assert 0 <= matchstart <= matchstop
-        print '%s: %s' % (filename, data[matchstart:matchstop])
         p = res.span(0)[1]
+        count += 1
+    print(count)
 
 # __________  Entry point  __________
 
 def entry_point(argv):
-    start = time.time()
-    for fn in argv[1:]:
-        search_in_file(fn)
-    stop = time.time()
-    print stop - start
+    if len(argv) != 2:
+        print "usage: %s <filename>" % (argv[0], )
+        return -1
+    data = read(argv[1]) # assumed valid utf-8
+    for codenum in [1, 2, 3, 4]:
+        start = time.time()
+        search_in_file(codenum, data)
+        stop = time.time()
+        print codenum, stop - start
     return 0
 
 # _____ Define and setup target ___
@@ -54,13 +60,24 @@ def target(*args):
 
 if __name__ == '__main__':
     import re, sys
-    r = re.compile(r"<item>\s*<title>(.*?)</title>")
-    start = time.time()
-    for fn in sys.argv[1:]:
-        f = open(fn, 'rb')
+    entry_point(sys.argv)
+    assert 0
+    codenum = int(sys.argv[1])
+    if codenum < 0:
+        sys.argv[1] = -codenum
+        entry_point(sys.argv)
+    else:
+        r = [ 
+            ur"<item>\s*<title>(.*?)</title>",
+            ur'[\w\.+-]+@[\w\.-]+\.[\w\.-]+',
+            ur'[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?',
+            ur'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])'][int(sys.argv[1]) - 1]
+        r = re.compile(r)
+        start = time.time()
+        f = open(sys.argv[2], 'rb')
         data = f.read()
         f.close()
         for title in r.findall(data):
-            print '%s: %s' % (fn, title)
-    stop = time.time()
-    print '%.4fs' % (stop - start,)
+            print '%s: %s' % (sys.argv[2], title)
+        stop = time.time()
+        print '%.4fs' % (stop - start,)
