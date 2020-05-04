35db2c33b4de9cbbb3686304f4d2be282643d9ec
Ronan Lamy <ronan.lamy@gmail.com>
2020-05-04 19:43:12 +0100

    fix translation

--- pypy/config/pypyoption.py
+++ pypy/config/pypyoption.py
@@ -17,7 +17,7 @@ all_modules = [p.basename for p in modulepath.listdir()
 essential_modules = set([
     "exceptions", "_io", "sys", "builtins", "posix", "_warnings",
     "itertools", "_frozen_importlib", "operator", "_locale", "struct",
-    "thread",
+    "thread", "__pypy__",
 ])
 if sys.platform == "win32":
     essential_modules.add("_winreg")
@@ -26,7 +26,7 @@ default_modules = essential_modules.copy()
 default_modules.update([
     "_codecs", "atexit", "gc", "_weakref", "marshal", "errno", "imp",
     "itertools", "math", "cmath", "_sre", "_pickle_support",
-    "parser", "symbol", "token", "_ast", "_random", "__pypy__",
+    "parser", "symbol", "token", "_ast", "_random",
     "_string", "_testing", "time", "_abc",
 ])
 
