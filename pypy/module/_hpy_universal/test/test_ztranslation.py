8a2ca9d26454d3a16c62dde3635161a052dda651
Ronan Lamy <ronan.lamy@gmail.com>
2020-07-13 18:55:59 +0100

    attach_dict_strategy() causes translation issues if the annotator doesn't see enough of the dict API. So use it only to check _hpy_universal.

--- pypy/module/_hpy_universal/test/test_ztranslation.py
+++ pypy/module/_hpy_universal/test/test_ztranslation.py
@@ -5,10 +5,21 @@ from pypy.objspace.fake.checkmodule import checkmodule
 from pypy.objspace.fake.objspace import FakeObjSpace
 from pypy.module._hpy_universal.state import State
 
+def attach_dict_strategy(space):
+    # this is needed for modules which do e.g. "isinstance(w_obj,
+    # W_DictMultiObject)", like _hpy_universal. Make sure that the
+    # annotator sees a concrete class, like W_DictObject, else lots of
+    # operations are blocked.
+    from pypy.objspace.std.dictmultiobject import W_DictObject, ObjectDictStrategy
+    strategy = ObjectDictStrategy(space)
+    storage = strategy.get_empty_storage()
+    w_obj = W_DictObject(space, strategy, storage)
+
 def test_checkmodule():
     def extra_func(space):
         state = space.fromcache(State)
         state.setup()
+        attach_dict_strategy(space)
 
     rpython_opts = {'translation.gc': 'boehm'}
     # it isn't possible to ztranslate cpyext easily, so we check _hpy_universal
