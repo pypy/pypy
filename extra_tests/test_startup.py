57f18f6c075abe34ab14f850c1b11529c09fff60
Matti Picus <matti.picus@gmail.com>
2020-07-16 15:01:32 +0300

    extend test

--- extra_tests/test_startup.py
+++ extra_tests/test_startup.py
@@ -6,3 +6,4 @@ def test_platform_not_imported():
          'import sys; print(list(sys.modules.keys()))'], universal_newlines=True)
     modules = [x.strip(' "\'') for x in out.strip().strip('[]').split(',')]
     assert 'platform' not in modules
+    assert 'threading' not in modules
