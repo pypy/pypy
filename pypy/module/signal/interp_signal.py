5df253f5db76b68fd545d0a8c2238e6d438e0ec1
Armin Rigo <arigo@tunes.org>
2020-07-21 17:19:08 +0200

    For now, don't do any checking in the app-level signal.set_wakeup_fd() on Windows

--- pypy/module/signal/interp_signal.py
+++ pypy/module/signal/interp_signal.py
@@ -256,13 +256,11 @@ def set_wakeup_fd(space, fd):
                     "__pypy__.thread.enable_signals()")
 
     if WIN32:
-        # fd can be a socket handle on win32. The code in CPython is very
-        # different and requires a refactor here and in
-        # rpython/translator/c/src/signals.c
-        raise oefmt(space.w_NotImplementedError, 
-                    "signal.set_wakeup_fd is not implemented on Windows")
-
-    if fd != -1:
+        # fd can be a socket handle on win32.  We assume on Windows
+        # that 'fd' is valid, either as a file or a socket descriptor,
+        # and don't bother doing checking here.  XXX fix me!
+        pass
+    elif fd != -1:
         try:
             os.fstat(fd)
             flags = rposix.get_status_flags(fd)
