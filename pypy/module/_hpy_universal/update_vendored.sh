e7982a1ead26ed7300a6f77b8193269c7da80083
Ronan Lamy <ronan.lamy@gmail.com>
2020-08-05 14:00:04 +0200

    Also update argparse.c

--- pypy/module/_hpy_universal/update_vendored.sh
+++ pypy/module/_hpy_universal/update_vendored.sh
@@ -18,6 +18,7 @@ git -C "$HPY" --no-pager diff --stat
 
 cp -R ${HPY}/hpy/devel/include/* ${DIR}/_vendored/hpy/devel/include
 cp -R ${HPY}/test/* ${DIR}/test/_vendored/
+cp ${HPY}/hpy/devel/src/runtime/argparse.c ${DIR}/_vendored/hpy/devel/src/runtime/argparse.c
 
 echo
 echo
