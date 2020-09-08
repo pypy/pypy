b3a32d2b333fa07789af0e606820956ae76bd0d2
Antonio Cuni <anto.cuni@gmail.com>
2020-09-08 15:43:16 +0200

    Improve the ./update_vendored.sh script in preparation of the merge of the hpy branch 'get-version':
      - use rsync instead of cp to sync the files: this way, it automatically
        deletes files which have been deleted in the src git repo
    
      - check that the versions reported by git describe and the one found in
        hpy/devel/version.py match: this way, we will be able to use
        _vendored/hpy/devel/version.py to implement _hpy_universal.get_version()

--- pypy/module/_hpy_universal/update_vendored.sh
+++ pypy/module/_hpy_universal/update_vendored.sh
@@ -8,18 +8,53 @@ if [ "$#" -ne 1 ]; then
 fi
 
 DIR=$(dirname $0)
-VENDORED=$DIR/_vendored
-VENDORED_TEST=$DIR/test/_vendored
 HPY=$1
+
+# ~~~ helper functions ~~~
+
+check_dirty() {
+    if [[ $(git -C "$HPY" diff --stat) != '' ]]; then
+        echo "WARNING! The source hpy repo is dirty"
+        echo
+    fi
+}
+
+check_version_status() {
+    # check that the version in hpy/devel/version.py corresponds to the one
+    # given by git
+    revgit=$(git -C "$HPY" describe --abbrev=7 --dirty --always --tags --long)
+
+    pushd "$HPY/hpy/devel" > /dev/null
+    revpy=$(python -c 'import version;print(version.__git_revision__)')
+    popd > /dev/null
+
+    if [ "$revgit" != "$revpy" ]
+    then
+        echo "ERROR: hpy/devel/version.py seems to be outdated"
+        echo "  revision reported by git describe: $revgit"
+        echo "  revision in hpy/devel/version.py:  $revpy"
+        echo
+        echo "Please run setup.py build in the hpy repo"
+        exit 1
+    fi
+}
+
+myrsync() {
+    rsync --exclude '*~' --exclude '*.pyc' --exclude __pycache__ "$@" 
+}
+
+# ~~~ main code ~~~
+
+check_dirty
+check_version_status
+
+myrsync -a --delete ${HPY}/hpy/devel/ ${DIR}/_vendored/hpy/devel/
+myrsync -a --delete ${HPY}/test/* ${DIR}/test/_vendored/
+
 
 echo "GIT status of $HPY"
 git -C "$HPY" --no-pager log --oneline -n 1
 git -C "$HPY" --no-pager diff --stat
-
-cp -R ${HPY}/hpy/devel/ ${DIR}/_vendored/hpy/
-cp -R ${HPY}/test/* ${DIR}/test/_vendored/
-
-echo
 echo
 echo "HG status of pypy"
 hg st $DIR
