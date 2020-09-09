39d1292e14742340abec311b55b0c61408cc4fac
Antonio Cuni <anto.cuni@gmail.com>
2020-09-09 17:32:18 +0200

    finally fix test_ztranslation.py :)

--- pypy/module/_hpy_universal/src/rffi_hacks.h
+++ pypy/module/_hpy_universal/src/rffi_hacks.h
@@ -6,3 +6,12 @@ typedef struct {
     HPySlot slot;
 } _pypy_HPyDef_as_slot;
 
+typedef struct {
+    HPyDef_Kind kind;
+    HPyMember member;
+} _pypy_HPyDef_as_member;
+
+typedef struct {
+    HPyDef_Kind kind;
+    HPyGetSet getset;
+} _pypy_HPyDef_as_getset;
