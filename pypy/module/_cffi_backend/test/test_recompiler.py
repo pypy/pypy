2976e8885511eed748e9644e71c983d3984256dc
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2020-09-23 21:28:14 +0200

    test_recompiler: xfail the long double test
    This fails on both Darwin/x86-64 and Linux/ARM64, and AFAICT the
    relevant 'shape', 'g', isn't defined anywhere.

--- pypy/module/_cffi_backend/test/test_recompiler.py
+++ pypy/module/_cffi_backend/test/test_recompiler.py
@@ -1624,6 +1624,7 @@ class AppTestRecompiler:
         res = lib.bok()
         assert [res.a, res.b, res.c] == [10, 20, 30]
 
+    @pytest.mark.xfail(reason="long double not implemented in ctypes")
     def test_extern_python_long_double(self):
         ffi, lib = self.prepare("""
             extern "Python" int bar(int, long double, int);
