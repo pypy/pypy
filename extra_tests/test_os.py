2ccc817a49632b5807720ea438d46799a3e2d16a
Matti Picus <matti.picus@gmail.com>
2020-10-06 08:48:26 +0300

    fix failing tests

--- extra_tests/test_os.py
+++ extra_tests/test_os.py
@@ -8,15 +8,17 @@ if hasattr(os, "execv"):
     def test_execv():
         if not hasattr(os, "fork"):
             skip("Need fork() to test execv()")
+        if not os.path.isdir('/tmp'):
+            skip("Need '/tmp' for test")
         pid = os.fork()
         if pid == 0:
             os.execv("/usr/bin/env", ["env", python, "-c",
-                     ("fid = open('onefile', 'w'); "
+                     ("fid = open('/tmp/onefile0', 'w'); "
                       "fid.write('1'); "
                       "fid.close()")])
         os.waitpid(pid, 0)
-        assert open("onefile").read() == "1"
-        os.unlink("onefile")
+        assert open("/tmp/onefile0").read() == "1"
+        os.unlink("/tmp/onefile0")
 
     def test_execv_raising():
         with raises(OSError):
@@ -37,6 +39,8 @@ if hasattr(os, "execv"):
     def test_execv_unicode():
         if not hasattr(os, "fork"):
             skip("Need fork() to test execv()")
+        if not os.path.isdir('/tmp'):
+            skip("Need '/tmp' for test")
         try:
             output = u"caf\xe9 \u1234\n".encode(sys.getfilesystemencoding())
         except UnicodeEncodeError:
@@ -44,11 +48,11 @@ if hasattr(os, "execv"):
         pid = os.fork()
         if pid == 0:
             os.execv(u"/bin/sh", ["sh", "-c",
-                                  u"echo caf\xe9 \u1234 > onefile"])
+                                  u"echo caf\xe9 \u1234 > /tmp/onefile1"])
         os.waitpid(pid, 0)
-        with open("onefile") as fid:
+        with open("/tmp/onefile1") as fid:
             assert fid.read() == output
-        os.unlink("onefile")
+        os.unlink("/tmp/onefile1")
 
     def test_execve():
         if not hasattr(os, "fork"):
