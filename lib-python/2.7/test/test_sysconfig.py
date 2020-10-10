1ae37ce13e89cd87169eb82de8948d4ff1afa592
Stefano Rivera <stefano@rivera.za.net>
2020-10-10 09:04:34 -0700

    We now have an nt_pypy scheme

--- lib-python/2.7/test/test_sysconfig.py
+++ lib-python/2.7/test/test_sysconfig.py
@@ -252,7 +252,7 @@ class TestSysConfig(unittest.TestCase):
                         not import_module('_testcapi'), config_h)
 
     def test_get_scheme_names(self):
-        wanted = ('nt', 'nt_user', 'os2', 'os2_home', 'osx_framework_user',
+        wanted = ('nt', 'nt_pypy', 'nt_user', 'os2', 'os2_home', 'osx_framework_user',
                   'posix_home', 'posix_prefix', 'posix_user', 'pypy')
         self.assertEqual(get_scheme_names(), wanted)
 
