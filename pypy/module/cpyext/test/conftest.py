d02e55c0196c4bbe9aa5039ea11b93487379ed3f
nulano <nulano@nulano.eu>
2020-10-10 22:36:21 +0100

    skip cpyext tests when calling a single file directly

--- pypy/module/cpyext/test/conftest.py
+++ pypy/module/cpyext/test/conftest.py
@@ -15,6 +15,12 @@ def pytest_ignore_collect(path, config):
         if os.path.commonprefix([path, THIS_DIR]) == THIS_DIR:  # workaround for bug in pytest<3.0.5
             return True
 
+def pytest_collect_file(path, parent):
+    if disabled:
+        # We end up here when calling py.test .../test_foo.py directly
+        # It's OK to kill the whole session with the following line
+        pytest.skip("cpyext not yet supported on windows 64 bit")
+
 def pytest_configure(config):
     if config.getoption('runappdirect') or config.getoption('direct_apptest'):
         import py
