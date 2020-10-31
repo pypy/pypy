cf09e4c6951cc4681f397aec8462a89e1d239a8d
Matti Picus <matti.picus@gmail.com>
2020-10-31 20:43:32 +0200

    fix version string

--- pypy/module/cpyext/include/patchlevel.h
+++ pypy/module/cpyext/include/patchlevel.h
@@ -26,7 +26,7 @@
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION		"3.7.3"
+#define PY_VERSION		"3.7.9"
 
 /* PyPy version as a string: make sure to keep this in sync with:
  *     module/sys/version.py
