f96e385f94d377c04309ff102a673d98fa367411
Matti Picus <matti.picus@gmail.com>
2020-12-27 10:36:30 +0200

    remove setrecursionlimit call from a535e6deaf20 in 2015
    It causes a crash when running tests with pypy2.7 7.3.3 on windows

--- pypy/conftest.py
+++ pypy/conftest.py
@@ -1,10 +1,6 @@
 import py, pytest, sys, os, textwrap
 from inspect import isclass
 
-if hasattr(sys, 'setrecursionlimit'):
-    # some tests fail otherwise
-    sys.setrecursionlimit(2000)
-
 LOOK_FOR_PYTHON3 = 'python3.7'
 PYTHON3 = os.getenv('PYTHON3') or py.path.local.sysfind(LOOK_FOR_PYTHON3)
 if PYTHON3 is not None:
