f5d2df318aed166efa25cc850b631560526bd82f
isidentical <batuhan@python.org>
2021-01-09 01:33:36 +0300

    Tests: Fix AST tests

--- pypy/interpreter/astcompiler/test/test_ast.py
+++ pypy/interpreter/astcompiler/test/test_ast.py
@@ -4,24 +4,24 @@ class TestAstToObject:
         assert space.issubtype_w(
                 ast.get(space).w_Module, ast.get(space).w_mod)
                                   
-    def test_num(self, space):
+    def test_constant_num(self, space):
         value = space.wrap(42)
-        node = ast.Num(value, lineno=1, col_offset=1)
+        node = ast.Constant(value, lineno=1, col_offset=1)
         w_node = node.to_object(space)
-        assert space.getattr(w_node, space.wrap("n")) is value
+        assert space.getattr(w_node, space.wrap("value")) is value
 
     def test_expr(self, space):
         value = space.wrap(42)
-        node = ast.Num(value, lineno=1, col_offset=1)
+        node = ast.Constant(value, lineno=1, col_offset=1)
         expr = ast.Expr(node, lineno=1, col_offset=1)
         w_node = expr.to_object(space)
         # node.value.n
         assert space.getattr(space.getattr(w_node, space.wrap("value")),
-                             space.wrap("n")) is value
+                             space.wrap("value")) is value
 
     def test_operation(self, space):
-        val1 = ast.Num(space.wrap(1), lineno=1, col_offset=1)
-        val2 = ast.Num(space.wrap(2), lineno=1, col_offset=1)
+        val1 = ast.Constant(space.wrap(1), lineno=1, col_offset=1)
+        val2 = ast.Constant(space.wrap(2), lineno=1, col_offset=1)
         node = ast.BinOp(left=val1, right=val2, op=ast.Add,
                          lineno=1, col_offset=1)
         w_node = node.to_object(space)
@@ -30,12 +30,12 @@ class TestAstToObject:
 
     def test_from_object(self, space):
         value = space.wrap(42)
-        w_node = space.call_function(ast.get(space).w_Num)
-        space.setattr(w_node, space.wrap('n'), value)
+        w_node = space.call_function(ast.get(space).w_Constant)
+        space.setattr(w_node, space.wrap('value'), value)
         space.setattr(w_node, space.wrap('lineno'), space.wrap(1))
         space.setattr(w_node, space.wrap('col_offset'), space.wrap(1))
-        node = ast.Num.from_object(space, w_node)
-        assert node.n is value
+        node = ast.Constant.from_object(space, w_node)
+        assert node.value is value
 
     def test_fields(self, space):
         w_fields = space.getattr(ast.get(space).w_FunctionDef,
