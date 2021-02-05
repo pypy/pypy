7e09ffe3a7c9b93970073ca35fa7aa3ef9df0a1c
Matti Picus <matti.picus@gmail.com>
2021-02-05 13:00:44 +0200

    backport changes from py3.7-big-sur-dyld-cache to default

--- lib-python/2.7/ctypes/macholib/dyld.py
+++ lib-python/2.7/ctypes/macholib/dyld.py
@@ -7,6 +7,12 @@ from framework import framework_info
 from dylib import dylib_info
 from itertools import *
 
+try:
+    from _ctypes import _dyld_shared_cache_contains_path
+except ImportError:
+    def _dyld_shared_cache_contains_path(*args):
+        raise NotImplementedError
+
 __all__ = [
     'dyld_find', 'framework_find',
     'framework_info', 'dylib_info',
@@ -130,8 +136,15 @@ def dyld_find(name, executable_path=None, env=None):
                 dyld_executable_path_search(name, executable_path),
                 dyld_default_search(name, env),
             ), env):
+
         if os.path.isfile(path):
             return path
+        try:
+            if _dyld_shared_cache_contains_path(path):
+                return path
+        except NotImplementedError:
+            pass
+
     raise ValueError("dylib %s could not be found" % (name,))
 
 def framework_find(fn, executable_path=None, env=None):
