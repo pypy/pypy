e8e4ce52b86f6b90f7c53f148ae6d7544d934376
Matti Picus <matti.picus@gmail.com>
2021-02-23 18:08:03 +0200

    fix use of `pip install --local`

--- lib-python/3/site.py
+++ lib-python/3/site.py
@@ -274,13 +274,15 @@ def _getuserbase():
 def _get_path(userbase):
     version = sys.version_info
 
+    implementation = _get_implementation()
+    implementation_lower = implementation.lower()
     if os.name == 'nt':
-        return f'{userbase}\\Python{version[0]}{version[1]}\\site-packages'
+        return f'{userbase}\\{implementation}{version[0]}{version[1]}\\site-packages'
 
     if sys.platform == 'darwin' and sys._framework:
-        return f'{userbase}/lib/python/site-packages'
+        return f'{userbase}/lib/{implementation_lower}/site-packages'
 
-    return f'{userbase}/lib/python{version[0]}.{version[1]}/site-packages'
+    return f'{userbase}/lib/{implementation_lower}{version[0]}.{version[1]}/site-packages'
 
 
 def getuserbase():
