d45d6dcc6826428b604177f323ff7425dcc02979
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2021-03-15 20:15:31 +0100

    remove test, it's now in test_tracelimit

--- rpython/jit/metainterp/test/test_recursive.py
+++ rpython/jit/metainterp/test/test_recursive.py
@@ -1334,49 +1334,6 @@ class RecursiveTests:
     def check_get_unique_id(self, lst):
         pass
 
-    def test_huge_trace_without_inlining(self):
-        def p(pc, code):
-            code = hlstr(code)
-            return "%s %d %s" % (code, pc, code[pc])
-        myjitdriver = JitDriver(greens=['pc', 'code'], reds=['n'],
-                                get_printable_location=p,
-                                is_recursive=True)
-
-        def f(code, n):
-            pc = 0
-            while pc < len(code):
-
-                myjitdriver.jit_merge_point(n=n, code=code, pc=pc)
-                op = code[pc]
-                if op == "-":
-                    n -= 1
-                elif op == "c":
-                    f('--------------------', n)
-                elif op == "l":
-                    if n > 0:
-                        myjitdriver.can_enter_jit(n=n, code=code, pc=0)
-                        pc = 0
-                        continue
-                else:
-                    assert 0
-                pc += 1
-            return n
-        def g(m):
-            set_param(None, 'inlining', True)
-            set_param(None, 'trace_limit', 40)
-            if m > 1000000:
-                f('', 0)
-            result = 0
-            s = '-' * 50 + '-c-l-'
-            for i in range(m):
-                result += f(s, i+100)
-        self.meta_interp(g, [10], backendopt=True, ProfilerClass=Profiler)
-        stats = get_stats()
-        assert stats.metainterp_sd.profiler.counters[
-            Counters.ABORT_SEGMENTED_TRACE] == 8
-        self.check_trace_count(10)
-        self.check_jitcell_token_count(2)
-
 
 class TestLLtype(RecursiveTests, LLJitMixin):
     pass
