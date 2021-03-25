0ca8d4f3da424f7f9287a469caa559e9d6d58ba9
Matti Picus <matti.picus@gmail.com>
2021-03-25 14:48:20 +0200

    typo (YannikJadoul from IRC)

--- pypy/tool/release/package.py
+++ pypy/tool/release/package.py
@@ -253,7 +253,7 @@ def create_package(basedir, options, _fake=False):
             os.symlink(POSIX_EXE, 'pypy{}'.format(python_ver))
             os.symlink(POSIX_EXE, 'python')
             os.symlink(POSIX_EXE, 'python{}'.format(python_ver))
-            os.symlink(POSIX_EXE, 'python{}'.format(python_ver[1]))
+            os.symlink(POSIX_EXE, 'python{}'.format(python_ver[0]))
         finally:
             os.chdir(old_dir)
     fix_permissions(pypydir)
