bad66bbddc5cd8f447bc00e343fde24221ce68ce
Matti Picus <matti.picus@gmail.com>
2021-06-20 00:21:16 +0300

    fix test: time is impported by pure-python zipimport

--- pypy/module/thread/test/test_import_lock.py
+++ pypy/module/thread/test/test_import_lock.py
@@ -101,9 +101,9 @@ class TestImportLock:
         importhook(space, 'sys')
         assert importlock.count == 0
         # A new module
-        importhook(space, 'time')
+        importhook(space, '_codecs')
         assert importlock.count >= 1
         # Import it again
         previous_count = importlock.count
-        importhook(space, "time")
+        importhook(space, "_codecs")
         assert importlock.count == previous_count
