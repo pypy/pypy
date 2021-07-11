06718f96275e21bf97596ba63c35da60ade87b84
Matti Picus <matti.picus@gmail.com>
2021-07-11 12:46:37 +0300

    backport libffi-win64

--- pypy/tool/release/package.py
+++ pypy/tool/release/package.py
@@ -151,7 +151,9 @@ def create_package(basedir, options, _fake=False):
             print("Picking %s" % str(pypyw))
         # Can't rename a DLL
         win_extras = [('lib' + POSIX_EXE + '-c.dll', None),
-                      ('sqlite3.dll', lib_pypy)]
+                      ('sqlite3.dll', lib_pypy),
+                      ('libffi-7.dll', None),
+                     ]
         if not options.no__tkinter:
             tkinter_dir = lib_pypy.join('_tkinter')
             win_extras += [('tcl86t.dll', tkinter_dir), ('tk86t.dll', tkinter_dir)]
