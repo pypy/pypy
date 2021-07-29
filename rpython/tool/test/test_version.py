f76887d9e7d30ee827a8f012abf54f7d60cead2b
Matti Picus <matti.picus@gmail.com>
2021-07-29 10:33:24 +0300

    support multiple tags in hg_archive, fixes platform._sys_version()

--- rpython/tool/test/test_version.py
+++ rpython/tool/test/test_version.py
@@ -2,18 +2,21 @@ import os, sys
 from rpython.tool.version import get_repo_version_info, _get_hg_archive_version
 
 def test_hg_archival_version(tmpdir):
-    def version_for(name, **kw):
+    def version_for(name, items):
         path = tmpdir.join(name)
-        path.write('\n'.join('%s: %s' % x for x in kw.items()))
+        path.write('\n'.join(('%s: %s' % (tag,value) for tag,value in items)))
         return _get_hg_archive_version(str(path))
 
     assert version_for('release',
-                       tag='release-123',
-                       node='000',
+                       (('tag', 'release-123'),
+                        ('tag', 'ignore-me'),
+                        ('node', '000'),
+                       ),
                       ) == ('release-123', '000')
     assert version_for('somebranch',
-                       node='000',
-                       branch='something',
+                       (('node', '000'),
+                        ('branch', 'something'),
+                       ),
                       ) == ('something', '000')
 
 
