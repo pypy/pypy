7fce1e526e750b4880c7fe61ce9362227ce60a70
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2021-09-04 22:30:32 +0200

    move this function out of the specialization, can be reused

--- rpython/rlib/listsort.py
+++ rpython/rlib/listsort.py
@@ -8,6 +8,23 @@ from rpython.rlib.objectmodel import specialize
 ## ------------------------------------------------------------------------
 ##         Adapted from CPython, original code and algorithms by Tim Peters
 
+
+def merge_compute_minrun(n):
+    # Compute a good value for the minimum run length; natural runs shorter
+    # than this are boosted artificially via binary insertion.
+    #
+    # If n < 64, return n (it's too small to bother with fancy stuff).
+    # Else if n is an exact power of 2, return 32.
+    # Else return an int k, 32 <= k <= 64, such that n/k is close to, but
+    # strictly less than, an exact power of 2.
+    #
+    # See listsort.txt for more info.
+    r = 0    # becomes 1 if any 1 bits are shifted off
+    while n >= 64:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
 def make_timsort_class(getitem=None, setitem=None, length=None,
                        getitem_slice=None, lt=None):
 
@@ -529,22 +546,7 @@ def make_timsort_class(getitem=None, setitem=None, length=None,
                 else:
                     self.merge_at(-2)
 
-        # Compute a good value for the minimum run length; natural runs shorter
-        # than this are boosted artificially via binary insertion.
-        #
-        # If n < 64, return n (it's too small to bother with fancy stuff).
-        # Else if n is an exact power of 2, return 32.
-        # Else return an int k, 32 <= k <= 64, such that n/k is close to, but
-        # strictly less than, an exact power of 2.
-        #
-        # See listsort.txt for more info.
-
-        def merge_compute_minrun(self, n):
-            r = 0    # becomes 1 if any 1 bits are shifted off
-            while n >= 64:
-                r |= n & 1
-                n >>= 1
-            return n + r
+        merge_compute_minrun = staticmethod(merge_compute_minrun)
 
         # ____________________________________________________________
         # Entry point.
