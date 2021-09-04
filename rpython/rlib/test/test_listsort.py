507eb4b0abca9037dbe8adcf6e42920736e068e9
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2021-09-04 22:24:33 +0200

    add a hypothesis test to the listsort testing

--- rpython/rlib/test/test_listsort.py
+++ rpython/rlib/test/test_listsort.py
@@ -2,15 +2,26 @@ import py
 from rpython.rlib.listsort import TimSort
 import random, os
 
+from hypothesis import given, strategies as st
+
 def makeset(lst):
     result = {}
     for a in lst:
         result.setdefault(id(a), []).append(True)
     return result
 
+class TestTimSort(TimSort):
+    def merge_compute_minrun(self, n):
+        return 1 # that means we use the "timsorty" bits of the algorithm more
+        # than just mostly binary insertion sort
+
 def sorttest(lst1):
+    _sorttest(TimSort, lst1)
+    _sorttest(TestTimSort, lst1)
+
+def _sorttest(cls, lst1):
     lst2 = lst1[:]
-    TimSort(lst2).sort()
+    cls(lst2).sort()
     assert len(lst1) == len(lst2)
     assert makeset(lst1) == makeset(lst2)
     position = {}
@@ -33,6 +44,10 @@ def test_v():
         up = 1 + int(v * random.random() * 2.7)
         lst1 = [C(random.randrange(0, up)) for i in range(v)]
         sorttest(lst1)
+
+@given(st.lists(st.integers(), min_size=2))
+def test_hypothesis(l):
+    sorttest(l)
 
 def test_file():
     for fn in py.path.local(__file__).dirpath().listdir():
