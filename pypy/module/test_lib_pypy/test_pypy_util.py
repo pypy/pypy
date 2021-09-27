3722bebf73313b9f9ec77073cdbda2854bb22003
fijal <unknown>
2021-09-27 12:53:46 +0200

    dumb test

--- pypy/module/test_lib_pypy/test_pypy_util.py
+++ pypy/module/test_lib_pypy/test_pypy_util.py
@@ -6,3 +6,7 @@ class AppTestStackNew:
         with StackNew("char[]", 1) as p:
             p[0] = b'\x13'
             assert p[0] == b'\x13'
+
+        # assert did not crash
+        with StackNew("char*") as p:
+            pass
