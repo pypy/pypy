07e06df07920329bea94d93903edbc47952362bf
Matti Picus <matti.picus@gmail.com>
2021-10-03 09:53:25 +0300

    remove pypy-specific copying of base/lib/*.so which is not needed for portable builds
    also rename lib/python3.8 to lib/pypy3.8

--- lib-python/3/venv/__init__.py
+++ lib-python/3/venv/__init__.py
@@ -118,7 +118,7 @@ class EnvBuilder:
             binname = 'bin'
             incpath = 'include'
             libpath = os.path.join(env_dir, 'lib',
-                                   'python%d.%d' % sys.version_info[:2],
+                                   'pypy%d.%d' % sys.version_info[:2],
                                    'site-packages')
         context.inc_path = path = os.path.join(env_dir, incpath)
         create_if_needed(path)
@@ -257,18 +257,6 @@ class EnvBuilder:
                     copier(src_library, dest_library)
                     if not os.path.islink(dest_library):
                         os.chmod(dest_library, 0o755)
-            libsrc = os.path.join(context.python_dir, '..', 'lib')
-            if os.path.exists(libsrc):
-                # PyPy: also copy lib/*.so* for portable builds
-                libdst = os.path.join(context.env_dir, 'lib')
-                if not os.path.exists(libdst):
-                    os.mkdir(libdst)
-                for f in os.listdir(libsrc):
-                    src = os.path.join(libsrc, f)
-                    if os.path.isdir(src):
-                        continue
-                    dst = os.path.join(libdst, f)
-                    copier(src, dst)
             #
         else:
             if self.symlinks:
