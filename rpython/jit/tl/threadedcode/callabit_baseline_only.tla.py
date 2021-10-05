5cb6cf1abb30ddfbc8e97991cbbae6ff6a059006
Yusuke Izawa <me@yizawa.com>
2021-10-06 03:55:54 +0900

    Add new examples using two JITs

---
+++ rpython/jit/tl/threadedcode/callabit_baseline_only.tla.py
@@ -0,0 +1,23 @@
+from rpython.jit.tl.threadedcode import tla
+
+code = [
+    tla.DUP,
+    tla.CALL, 16,
+    tla.POP,
+    tla.CONST_INT, 1,
+    tla.SUB,
+    tla.DUP,
+    tla.CONST_INT, 1,
+    tla.LT,
+    tla.JUMP_IF, 15,
+    tla.JUMP, 0,
+    tla.EXIT,
+    tla.CONST_INT, 1,
+    tla.SUB,
+    tla.DUP,
+    tla.CONST_INT, 1,
+    tla.LT,
+    tla.JUMP_IF, 27,
+    tla.JUMP, 16,
+    tla.RET, 1
+]
