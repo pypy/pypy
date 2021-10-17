0aaf6b0db54fb53dc7e12d27d037cf6f7c2ebd9d
Matti Picus <matti.picus@gmail.com>
2021-10-17 08:20:21 +0300

    revert 9dcb0815f970 - LDFLAGS should not define -shared (issue 3576)

--- lib_pypy/_sysconfigdata.py
+++ lib_pypy/_sysconfigdata.py
@@ -28,7 +28,6 @@ build_time_vars = {
     'OPT': "-DNDEBUG -O2",
     'CFLAGS': "-DNDEBUG -O2",
     'CCSHARED': "-fPIC",
-    'LDFLAGS': "-pthread -shared",
     'LDSHARED': "cc -pthread -shared",
     'EXT_SUFFIX': so_ext,
     'SHLIB_SUFFIX': ".so",
@@ -67,8 +66,7 @@ if sys.platform[:6] == "darwin":
     # Fix this if we ever get M1 support
     arch = 'x86_64'
     build_time_vars['CC'] += ' -arch %s' % (arch,)
-    build_time_vars['LDFLAGS'] = ' -shared -undefined dynamic_lookup'
-    build_time_vars['LDSHARED'] = build_time_vars['CC'] + build_time_vars['LDFLAGS']
+    build_time_vars['LDSHARED'] = build_time_vars['CC'] + ' -shared -undefined dynamic_lookup'
     build_time_vars['LDLIBRARY'] = 'libpypy3-c.dylib'
     if "CXX" in build_time_vars:
         build_time_vars['CXX'] += ' -arch %s' % (arch,)
