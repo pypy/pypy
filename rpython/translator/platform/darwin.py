4721213e82aa0537dc7dd0207bd33a49685fe22f
Matti Picus <matti.picus@gmail.com>
2021-11-22 16:42:33 +0200

    use 10.9 as a minimum macOS target

--- rpython/translator/platform/darwin.py
+++ rpython/translator/platform/darwin.py
@@ -8,8 +8,10 @@ import os
 # the @rpath handling used in Darwin._args_for_shared is only availabe
 # since 10.5, so we use that as minimum requirement. Bumped to 10.7
 # to allow the use of thread-local in __thread in C.
+# Bumped to 10.9 2021-11-22 to match CPython,
+# see https://github.com/python/cpython/blob/42205ee51
 #
-DARWIN_VERSION_MIN = '-mmacosx-version-min=10.7'
+DARWIN_VERSION_MIN = '-mmacosx-version-min=10.9'
 
 class Darwin(posix.BasePosix):
     name = "darwin"
