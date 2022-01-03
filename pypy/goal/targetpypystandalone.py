ade962952c66a7bab27989722e6d740efd1f4558
Matti Picus <matti.picus@gmail.com>
2022-01-03 23:01:59 +0200

    virtualenv-20.13.0 now supports the new pypy3.9 naming scheme

--- pypy/goal/targetpypystandalone.py
+++ pypy/goal/targetpypystandalone.py
@@ -269,10 +269,7 @@ class PyPyTarget(object):
         return pypy_optiondescription
 
     def target(self, driver, args):
-        if sys.platform == 'win32':
-            driver.exe_name = 'pypy3-%(backend)s'
-        else:
-            driver.exe_name = 'pypy%d.%d-%%(backend)s' % CPYTHON_VERSION[:2]
+        driver.exe_name = 'pypy%d.%d-%%(backend)s' % CPYTHON_VERSION[:2]
 
         config = driver.config
         parser = self.opt_parser(config)
