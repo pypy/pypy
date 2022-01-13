bcd7c3b1a297fac3e1601921a685da7106531aec
Matti Picus <matti.picus@gmail.com>
2022-01-13 16:54:44 +0200

    PyPy raises different exceptions

--- lib-python/3/test/test_generators.py
+++ lib-python/3/test/test_generators.py
@@ -130,8 +130,9 @@ class GeneratorTest(unittest.TestCase):
         # generator names must be a string and cannot be deleted
         self.assertRaises(TypeError, setattr, gen, '__name__', 123)
         self.assertRaises(TypeError, setattr, gen, '__qualname__', 123)
-        self.assertRaises(TypeError, delattr, gen, '__name__')
-        self.assertRaises(TypeError, delattr, gen, '__qualname__')
+        # PyPy: raises AttributeError
+        self.assertRaises((TypeError, AttributeError), delattr, gen, '__name__')
+        self.assertRaises((TypeError, AttributeError), delattr, gen, '__qualname__')
 
         # modify names of the function creating the generator
         func.__qualname__ = "func_qualname"
