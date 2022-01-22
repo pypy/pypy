a52d1c5415c9d587bbf0c5e29f7be0de8e425082
Matti Picus <matti.picus@gmail.com>
2022-01-22 19:48:45 +0200

    fix link, use OpenSSL 1.1.1m by default

--- lib_pypy/pypy_tools/build_cffi_imports.py
+++ lib_pypy/pypy_tools/build_cffi_imports.py
@@ -59,7 +59,7 @@ cffi_dependencies = {
               ['make', '-s', '-j', str(multiprocessing.cpu_count())],
               ['make', 'install', 'DESTDIR={}/'.format(deps_destdir)],
              ]),
-    '_ssl1': ('http://distfiles.macports.org/openssl/openssl-1.1.1m.tar.gz',
+    '_ssl1': ('http://artfiles.org/openssl.org/source/openssl-1.1.1m.tar.gz',
              'f89199be8b23ca45fc7cb9f1d8d3ee67312318286ad030f5316aca6462db6c96',
              [
               ['./config', '--prefix=/usr', 'no-shared'],
@@ -74,7 +74,7 @@ cffi_dependencies = {
                ['make', 'install', 'DESTDIR={}/'.format(deps_destdir)],
               ]),
 }
-cffi_dependencies['_ssl'] = cffi_dependencies['_ssl3']
+cffi_dependencies['_ssl'] = cffi_dependencies['_ssl1']
 
 if sys.platform == 'darwin':
     # this does not compile on the buildbot, linker is missing '_history_list'
