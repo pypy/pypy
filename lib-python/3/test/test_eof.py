e023319aa548ba2879fb82ab61bde025ebede750
Matti Picus <matti.picus@gmail.com>
2022-01-30 09:51:48 +0200

    allow PyPy's error messages in test

--- lib-python/3/test/test_eof.py
+++ lib-python/3/test/test_eof.py
@@ -28,7 +28,10 @@ class EOFTestCase(unittest.TestCase):
 
     def test_line_continuation_EOF(self):
         """A continuation at the end of input must be an error; bpo2180."""
-        expect = 'unexpected EOF while parsing (<string>, line 1)'
+        if sys.implementation.name == 'pypy':
+            expect = "end of file (EOF) in multi-line statement (<string>, line 2)"
+        else:
+            expect = "unexpected EOF while parsing (<string>, line 1)"
         with self.assertRaises(SyntaxError) as excinfo:
             exec('x = 5\\')
         self.assertEqual(str(excinfo.exception), expect)
@@ -39,14 +42,18 @@ class EOFTestCase(unittest.TestCase):
     @unittest.skipIf(not sys.executable, "sys.executable required")
     def test_line_continuation_EOF_from_file_bpo2180(self):
         """Ensure tok_nextc() does not add too many ending newlines."""
+        if sys.implementation.name == 'pypy':
+            msg = b"end of file (EOF) in multi-line statement"
+        else:
+            msg = b"unexpected EOF while parsing"
         with support.temp_dir() as temp_dir:
             file_name = script_helper.make_script(temp_dir, 'foo', '\\')
             rc, out, err = script_helper.assert_python_failure(file_name)
-            self.assertIn(b'unexpected EOF while parsing', err)
+            self.assertIn(msg, err)
 
             file_name = script_helper.make_script(temp_dir, 'foo', 'y = 6\\')
             rc, out, err = script_helper.assert_python_failure(file_name)
-            self.assertIn(b'unexpected EOF while parsing', err)
+            self.assertIn(msg, err)
 
 if __name__ == "__main__":
     unittest.main()
