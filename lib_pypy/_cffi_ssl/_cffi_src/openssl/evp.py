0c4d55b06a270401843e1ef4aab08a30be93deba
Matti Picus <matti.picus@gmail.com>
2022-05-15 23:44:46 +0300

    check results from EVP_DigestInit_ex and EVP_DigestUpdate, and fix some failing tests (issue 3741)

--- lib_pypy/_cffi_ssl/_cffi_src/openssl/evp.py
+++ lib_pypy/_cffi_ssl/_cffi_src/openssl/evp.py
@@ -60,6 +60,9 @@ int EVP_DigestFinal_ex(EVP_MD_CTX *, unsigned char *, unsigned int *);
 int EVP_DigestFinalXOF(EVP_MD_CTX *, unsigned char *, size_t);
 const EVP_MD *EVP_get_digestbyname(const char *);
 int EVP_MD_size(const EVP_MD *);
+void EVP_MD_do_all(void (*) (const EVP_MD *, const char *, const char *, void *), void *);
+int EVP_MD_nid(const EVP_MD *);
+
 
 EVP_PKEY *EVP_PKEY_new(void);
 void EVP_PKEY_free(EVP_PKEY *);
