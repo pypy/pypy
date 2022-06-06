f4006196ad73595744798c4f63f9618d6b3fa3ad
Matti Picus <matti.picus@gmail.com>
2022-06-06 08:07:07 +0300

    sys.platform is not rpython, redo differently

--- rpython/rlib/streamio.py
+++ rpython/rlib/streamio.py
@@ -110,15 +110,18 @@ def fdopen_as_stream(fd, mode, buffering=-1, signal_checker=None):
     return construct_stream_tower(stream, buffering, universal, reading,
                                   writing, binary)
 
+# XXX libffi on darwin does not support rposix.open untranslated since it uses
+# vararg, but windows passes in a FileEncoder/FileDecoder as path, which does
+# not translate
+if sys.platform == "win32":
+    helper_open = rposix.open
+else:
+    helper_open = os.open
+
 @specialize.argtype(0)
 def open_path_helper(path, os_flags, append, signal_checker=None):
     # XXX for now always return DiskFile
-    # XXX libffi on darwin does not support rposix.open, but windows passes
-    # in a FileEncoder/FileDecoder as path, which does not translate
-    if sys.platform == "win32":
-        fd = rposix.open(path, os_flags, 0666)
-    else:
-        fd = os.open(path, os_flags, 0666)
+    fd = helper_open(path, os_flags, 0666)
     if append:
         try:
             os.lseek(fd, 0, 2)
