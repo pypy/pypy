04fb950558e6776e2d8876eed17fcf17a2c4c6dc
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2022-06-11 18:05:54 +0200

    refactor to introduce a Cache class

--- rpython/translator/backendopt/cse.py
+++ rpython/translator/backendopt/cse.py
@@ -15,6 +15,67 @@ def has_side_effects(op):
         return True
 
 
+class Cache(object):
+    def __init__(self, heapcache=None):
+        #if purecache is None:
+        #    purecache = {}
+        if heapcache is None:
+            heapcache = {}
+        # (opname, concretetype of result, args) -> previous (life) result
+        #self.purecache = purecache
+        self.heapcache = heapcache
+
+    def translate_cache(self, link):
+        if link.target.operations == (): # exit or except block:
+            return None
+        block = link.target
+        local_versions = {var1: var2 for var1, var2 in zip(link.args, block.inputargs)}
+        def _translate_arg(arg):
+            if isinstance(arg, Variable):
+                res = local_versions.get(arg, None)
+                if res is None:
+                    res = Variable(arg)
+                    res.concretetype = arg.concretetype
+                    link.args.append(arg)
+                    block.inputargs.append(res)
+                    local_versions[arg] = res
+                return res
+            else:
+                return arg
+        new_cache = {}
+        for (var, field), res in self.heapcache.iteritems():
+            if var in local_versions or not isinstance(var, Variable):
+                new_cache[_translate_arg(var), field] = _translate_arg(res)
+        return Cache(new_cache)
+
+    def clear_for(self, concretetype, fieldname):
+        for k in self.heapcache.keys():
+            if k[0].concretetype == concretetype and k[1] == fieldname:
+                del self.heapcache[k]
+
+    def cse_block(self, block, inputlink):
+        added_some_same_as = False
+        for op in block.operations:
+            if op.opname == 'getfield':
+                tup = (op.args[0], op.args[1].value)
+                res = self.heapcache.get(tup, None)
+                if res is not None:
+                    op.opname = 'same_as'
+                    op.args = [res]
+                    added_some_same_as = True
+                else:
+                    self.heapcache[tup] = op.result
+            elif op.opname in ('setarrayitem', 'setinteriorfield', "malloc", "malloc_varsize"):
+                pass
+            elif op.opname == 'setfield':
+                target = op.args[0]
+                field = op.args[1].value
+                self.clear_for(target.concretetype, field)
+                self.heapcache[target, field] = op.args[2]
+            elif has_side_effects(op):
+                self.heapcache.clear()
+        return added_some_same_as
+
 def cse_graph(graph):
     """ remove superfluous getfields. use a super-local method: all non-join
     blocks inherit the heap information from their (single) predecessor
@@ -32,14 +93,14 @@ def cse_graph(graph):
         block, cache, inputlink = todo.pop()
         visited += 1
         if cache is None:
-            cache = {}
+            cache = Cache()
 
         if block.operations:
-            changed_block = _cse_block(block, cache, inputlink)
+            changed_block = cache.cse_block(block, inputlink)
             added_some_same_as = changed_block or added_some_same_as
         for link in block.exits:
             if len(entrymap[link.target]) == 1:
-                new_cache = _translate_cache(cache, link)
+                new_cache = cache.translate_cache(link)
                 todo.append((link.target, new_cache, link))
 
     assert visited == len(entrymap)
@@ -47,53 +108,3 @@ def cse_graph(graph):
         removenoops.remove_same_as(graph)
         simplify.transform_dead_op_vars(graph)
 
-def _translate_cache(cache, link):
-    if link.target.operations == (): # exit or except block:
-        return {}
-    block = link.target
-    local_versions = {var1: var2 for var1, var2 in zip(link.args, block.inputargs)}
-    def _translate_arg(arg):
-        if isinstance(arg, Variable):
-            res = local_versions.get(arg, None)
-            if res is None:
-                res = Variable(arg)
-                res.concretetype = arg.concretetype
-                link.args.append(arg)
-                block.inputargs.append(res)
-                local_versions[arg] = res
-            return res
-        else:
-            return arg
-    new_cache = {}
-    for (var, field), res in cache.iteritems():
-        if var in local_versions or not isinstance(var, Variable):
-            new_cache[_translate_arg(var), field] = _translate_arg(res)
-    return new_cache
-
-def _cse_block(block, cache, inputlink):
-    def clear_cache_for(cache, concretetype, fieldname):
-        for k in cache.keys():
-            if k[0].concretetype == concretetype and k[1] == fieldname:
-                del cache[k]
-
-    added_some_same_as = False
-    for op in block.operations:
-        if op.opname == 'getfield':
-            tup = (op.args[0], op.args[1].value)
-            res = cache.get(tup, None)
-            if res is not None:
-                op.opname = 'same_as'
-                op.args = [res]
-                added_some_same_as = True
-            else:
-                cache[tup] = op.result
-        elif op.opname in ('setarrayitem', 'setinteriorfield', "malloc", "malloc_varsize"):
-            pass
-        elif op.opname == 'setfield':
-            target = op.args[0]
-            field = op.args[1].value
-            clear_cache_for(cache, target.concretetype, field)
-            cache[target, field] = op.args[2]
-        elif has_side_effects(op):
-            cache.clear()
-    return added_some_same_as
