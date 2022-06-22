6c660f49b5788f7d42a2daf091922575cc5b616b
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2022-06-22 11:29:44 +0200

    add test for the existing optimization of int_?shift(x, 0) to x

--- rpython/jit/metainterp/optimizeopt/test/test_optimizebasic.py
+++ rpython/jit/metainterp/optimizeopt/test/test_optimizebasic.py
@@ -4126,11 +4126,13 @@ class TestOptimizeBasic(BaseTestBasic):
         [i0]
         i1 = int_lshift(0, i0)
         i2 = int_rshift(0, i0)
-        jump(i1, i2)
+        i3 = int_rshift(i0, 0)
+        i4 = int_rshift(i0, 0)
+        jump(i1, i2, i3, i4)
         """
         expected = """
         [i0]
-        jump(0, 0)
+        jump(0, 0, i0, i0)
         """
         self.optimize_loop(ops, expected)
 
