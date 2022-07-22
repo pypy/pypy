2fd6104e77db28b24d8e0fb0791dab0a078225e2
Matti Picus <matti.picus@gmail.com>
2022-07-22 12:17:49 +0300

    add HPyErr_SetFromErrnoWithFilename, HPyErr_SetFromErrnoWithFilenameObjects, HPyErr_ExceptionMatches

--- pypy/module/_hpy_universal/interp_err.py
+++ pypy/module/_hpy_universal/interp_err.py
@@ -1,7 +1,9 @@
 from rpython.rlib.nonconst import NonConstant
 from rpython.rtyper.lltypesystem import rffi, lltype, ll2ctypes
 from rpython.rlib.objectmodel import we_are_translated
-from pypy.interpreter.error import OperationError, oefmt, new_exception_class
+from rpython.rlib import rposix
+from pypy.interpreter.error import (
+    OperationError, oefmt, new_exception_class, strerror as _strerror)
 from pypy.module._hpy_universal.apiset import API
 #from pypy.module._hpy_universal.bridge import BRIDGE
 from pypy.module._hpy_universal import llapi
@@ -48,6 +50,55 @@ def HPyErr_SetObject(space, handles, ctx, h_exc_type, h_exc_value):
     w_exc_type = handles.deref(h_exc_type)
     w_obj = handles.deref(h_exc_value)
     raise OperationError(w_exc_type, w_obj)
+
+@API.func("void HPyErr_SetFromErrnoWithFilename(HPyContext *ctx, HPy type, const char *filename)")
+def HPyErr_SetFromErrnoWithFilename(space, handles, ctx, h_exc_type, utf8):
+    w_fname = _maybe_utf8_to_w(space, utf8)
+    w_exc_type = handles.deref(h_exc_type)
+    errno = rffi.cast(lltype.Signed, rposix._get_errno())
+    msg, lgt = _strerror(errno)
+    if w_fname:
+        w_error = space.call_function(w_exc_type,
+                                      space.newint(errno),
+                                      space.newtext(msg, lgt),
+                                      w_fname)
+    else:
+        w_error = space.call_function(w_exc_type,
+                                      space.newint(errno),
+                                      space.newtext(msg, lgt))
+    raise OperationError(w_exc_type, w_error)
+
+@API.func("void HPyErr_SetFromErrnoWithFilenameObjects(HPyContext *ctx, HPy type, HPy filename1, HPy filename2)")
+def HPyErr_SetFromErrnoWithFilenameObjects(space, handles, ctx, h_exc_type, h_fname1, h_fname2):
+    w_exc_type = handles.deref(h_exc_type)
+    if h_fname1:
+        w_fname1 = handles.deref(h_fname1)
+    else:
+        w_fname1 = None
+    if h_fname2:
+        w_fname2 = handles.deref(h_fname2)
+    else:
+        w_fname2 = None
+    errno = rffi.cast(lltype.Signed, rposix._get_errno())
+    msg, lgt = _strerror(errno)
+    if w_fname1:
+        if w_fname2:
+            w_error = space.call_function(w_exc_type,
+                                          space.newint(errno),
+                                          space.newtext(msg, lgt),
+                                          w_fname1, None, w_fname2)
+        else:
+            w_error = space.call_function(w_exc_type,
+                                          space.newint(errno),
+                                          space.newtext(msg, lgt),
+                                          w_fname1)
+    else:
+        w_error = space.call_function(w_exc_type,
+                                      space.newint(errno),
+                                      space.newtext(msg, lgt))
+    raise OperationError(w_exc_type, w_error)
+
+
 
 @API.func("int HPyErr_Occurred(HPyContext *ctx)", error_value=API.int(-1))
 def HPyErr_Occurred(space, handles, ctx):
@@ -110,3 +161,26 @@ def HPyErr_NewExceptionWithDoc(space, handles, ctx, c_name, c_doc, h_base, h_dic
         space.setitem_str(w_dict, "__doc__", space.newtext(doc))
 
     return handles.new(new_exception_class(space, name, w_base, w_dict))
+
+@API.func("HPy HPyErr_ExceptionMatches(HPyContext *ctx, HPy exc)")
+def HPyErr_ExceptionMatches(space, handles, ctx, h_exc):
+    w_exc = handles.deref(h_exc)
+    if not w_exc:
+        return 0
+    w_exc = handles.deref(h_exc)
+    # this is taken from PyErr_Ocurred
+    state = space.fromcache(State)
+    operror = state.get_exception()
+    if operror is None:
+        return 0
+    w_given = operror.w_type
+    # this is taken from PyErr_GivenExceptionMatches
+    if space.isinstance_w(w_given, space.w_BaseException):
+        w_given_type = space.type(w_given)
+    else:
+        w_given_type = w_given
+    try:
+        return space.exception_match(w_given_type, w_exc)
+    except:
+        return 0
+
