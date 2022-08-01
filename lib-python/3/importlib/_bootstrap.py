960f7c6f1abc3d001f159f4eb6ea140b24caca2e
Matti Picus <matti.picus@gmail.com>
2022-08-01 10:14:34 +0300

    cython 0.29.32 was released, revert the work-around

--- lib-python/3/importlib/_bootstrap.py
+++ lib-python/3/importlib/_bootstrap.py
@@ -1078,10 +1078,9 @@ def _calc___package__(globals):
     elif spec is not None:
         return spec.parent
     else:
-        # Emitted by c-extensions compiled with cython < 0.29.31
-        # _warnings.warn("can't resolve package from __spec__ or __package__, "
-        #                "falling back on __name__ and __path__",
-        #                ImportWarning, stacklevel=3)
+         _warnings.warn("can't resolve package from __spec__ or __package__, "
+                        "falling back on __name__ and __path__",
+                        ImportWarning, stacklevel=3)
         package = globals['__name__']
         if '__path__' not in globals:
             package = package.rpartition('.')[0]
