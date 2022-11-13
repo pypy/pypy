34f494aae5cb6ded7facf8bdfecca52f7bc80993
Matti Picus <matti.picus@gmail.com>
2022-11-13 07:54:50 +0200

    add a minimal embedding example

---
+++ lib-python/3/Programs/_test_minimumembed.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+// lib-python/3/Programs/_test_minimumembed.c
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+int
+main(int argc, char *argv[])
+{
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+    Py_SetProgramName(program);  /* optional but recommended */
+    Py_Initialize();
+    PyRun_SimpleString("from time import time,ctime\n"
+                       "print('Today is', ctime(time()))\n");
+    Py_Finalize();
+   
+    PyMem_RawFree(program);
+    return 0;
+}
+
