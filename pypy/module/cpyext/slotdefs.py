12256d0fa1481b037d5333e855f52d5b3d0b3d5a
Matti Picus <matti.picus@gmail.com>
2022-11-20 23:39:04 +0200

    decref even if an exception is raised in tp_init and tp_call

--- pypy/module/cpyext/slotdefs.py
+++ pypy/module/cpyext/slotdefs.py
@@ -62,8 +62,10 @@ class wrap_init(W_PyCWrapperObject):
         func_init = rffi.cast(initproc, func)
         py_args = tuple_from_args_w(space, __args__.arguments_w)
         w_kwargs = w_kwargs_from_args(space, __args__)
-        res = generic_cpy_call(space, func_init, w_self, py_args, w_kwargs)
-        decref(space, py_args)
+        try:
+            res = generic_cpy_call(space, func_init, w_self, py_args, w_kwargs)
+        finally:
+            decref(space, py_args)
         if rffi.cast(lltype.Signed, res) == -1:
             space.fromcache(State).check_and_raise_exception(always=True)
         return None
@@ -233,8 +235,10 @@ class wrap_call(W_PyCWrapperObject):
         func_target = rffi.cast(ternaryfunc, func)
         py_args = tuple_from_args_w(space, __args__.arguments_w)
         w_kwargs = w_kwargs_from_args(space, __args__)
-        ret = generic_cpy_call(space, func_target, w_self, py_args, w_kwargs)
-        decref(space, py_args)
+        try:
+            ret = generic_cpy_call(space, func_target, w_self, py_args, w_kwargs)
+        finally:
+            decref(space, py_args)
         return ret
 
 class wrap_ssizessizeobjargproc(W_PyCWrapperObject):
