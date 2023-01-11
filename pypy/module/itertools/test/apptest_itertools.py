3da63bd55cae2b01ffcb82cbf2c15682e2ebfea3
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-01-11 11:28:07 +0100

    itertools.count supports complex

--- pypy/module/itertools/test/apptest_itertools.py
+++ pypy/module/itertools/test/apptest_itertools.py
@@ -1,6 +1,6 @@
 import pytest
 
-from itertools import pairwise
+from itertools import pairwise, count, islice
 
 def test_pairwise():
     assert list(pairwise([])) == []
@@ -11,3 +11,6 @@ def test_pairwise():
 def test_posonly():
     with pytest.raises(TypeError):
         pairwise(iterable='abc')
+
+def test_count_complex():
+    assert list(islice(count(3.25-4j), 3)) == [3.25-4j, 4.25-4j, 5.25-4j]
