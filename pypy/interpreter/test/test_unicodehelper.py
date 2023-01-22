5773a3d677eb0434791d24fe11de98ca1e400906
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-01-22 17:34:25 +0100

    fix utf-16 errorhandler replacement logic if the replacement is bytes. those are just copied to the output

--- pypy/interpreter/test/test_unicodehelper.py
+++ pypy/interpreter/test/test_unicodehelper.py
@@ -10,7 +10,6 @@ from pypy.interpreter.unicodehelper import utf8_encode_ascii, str_decode_ascii
 from pypy.interpreter.unicodehelper import utf8_encode_latin_1, str_decode_unicode_escape
 from pypy.interpreter.unicodehelper import str_decode_raw_unicode_escape
 from pypy.interpreter.unicodehelper import utf8_encode_utf_16_le
-from pypy.interpreter.unicodehelper import utf8_encode_utf_32_le
 from pypy.interpreter import unicodehelper as uh
 from pypy.module._codecs.interp_codecs import CodecState
 
@@ -111,15 +110,6 @@ def test_raw_unicode_escape_backslash_without_escape():
     assert l == len(data)
     assert result == data
 
-def test_raw_unicode_escape_bug_escape_backslash():
-    data = b'\\\\'
-    res = str_decode_raw_unicode_escape(data, 'strict', True, None)
-    assert res[0] == '\\\\'
-
-    data = b'\\\xef'
-    res = str_decode_raw_unicode_escape(data, 'strict', True, None)
-    assert res[0].decode("utf-8") == u'\\\xef'
-
 def test_utf16_encode_bytes_replacement_is_simply_copied():
     def errorhandler(errors, encoding, msg, s, start, end):
         return 'abcd', end, 'b', s
@@ -131,15 +121,4 @@ def test_utf16_encode_bytes_replacement_is_simply_copied():
     )
     assert res == "[\x00abcd]\x00"
 
-
-def test_utf32_encode_bytes_replacement_is_simply_copied():
-    def errorhandler(errors, encoding, msg, s, start, end):
-        return 'abcd', end, 'b', s
-
-    res = utf8_encode_utf_32_le(
-        b'[\xed\xb2\x80]', 'strict',
-        errorhandler=errorhandler,
-        allow_surrogates=False
-    )
-    assert res == "[\x00\x00\x00abcd]\x00\x00\x00"
 
