c3345a28cfbc7c5dab92006f9f3a9c7012b87ccd
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-02-27 10:06:22 +0100

    fix tests for 46fe6bb9701c

--- extra_tests/test_traceback_suggestions.py
+++ extra_tests/test_traceback_suggestions.py
@@ -1,12 +1,19 @@
 import pytest
 from pathlib import Path
-from hypothesis import given, strategies as st
 
-with open(Path(__file__).parent.joinpath("..", "lib-python", "3", "traceback.py")) as f:
+try:
+    from hypothesis import given, strategies as st
+except ImportError:
+    given = st = None
+
+path = Path(__file__).parent.joinpath("..", "lib-python", "3", "traceback.py")
+
+with open(path) as f:
     content = f.read()
 
 d = {}
-exec(content, d, d)
+c = compile(content, path, 'exec')
+exec(c, d, d)
 _levenshtein_distance = d['_levenshtein_distance']
 _compute_suggestion_error = d['_compute_suggestion_error']
 TracebackException = d['TracebackException']
@@ -19,27 +26,28 @@ def test_levensthein():
     assert _levenshtein_distance("cat", "ca") == 1
     assert _levenshtein_distance("Kätzchen", "Satz") == 6
 
-@given(st.text())
-def test_x_x(s):
-    assert _levenshtein_distance(s, s) == 0
+if given is not None:
+    @given(st.text())
+    def test_x_x(s):
+        assert _levenshtein_distance(s, s) == 0
 
-@given(st.text())
-def test_x_empty(s):
-    assert _levenshtein_distance(s, '') == len(s)
+    @given(st.text())
+    def test_x_empty(s):
+        assert _levenshtein_distance(s, '') == len(s)
 
-@given(st.text(), st.text())
-def test_symmetric(a, b):
-    assert _levenshtein_distance(a, b) == _levenshtein_distance(b, a)
+    @given(st.text(), st.text())
+    def test_symmetric(a, b):
+        assert _levenshtein_distance(a, b) == _levenshtein_distance(b, a)
 
-@given(st.text(), st.text(), st.characters())
-def test_add_char(a, b, char):
-    d = _levenshtein_distance(a, b)
-    assert d == _levenshtein_distance(char + a, char + b)
-    assert d == _levenshtein_distance(a + char, b + char)
+    @given(st.text(), st.text(), st.characters())
+    def test_add_char(a, b, char):
+        d = _levenshtein_distance(a, b)
+        assert d == _levenshtein_distance(char + a, char + b)
+        assert d == _levenshtein_distance(a + char, b + char)
 
-@given(st.text(), st.text(), st.text())
-def test_triangle(a, b, c):
-    assert _levenshtein_distance(a, c) <= _levenshtein_distance(a, b) + _levenshtein_distance(b, c)
+    @given(st.text(), st.text(), st.text())
+    def test_triangle(a, b, c):
+        assert _levenshtein_distance(a, c) <= _levenshtein_distance(a, b) + _levenshtein_distance(b, c)
 
 
 # suggestion tests
@@ -66,7 +74,7 @@ def test_format_attribute_error():
     try:
         a.god
     except AttributeError as e:
-        assert fmt(e) == "AttributeError: 'A' object has no attribute 'god'. Did you mean: good?\n"
+        assert fmt(e) == "AttributeError: 'A' object has no attribute 'god'. Did you mean: 'good'?\n"
 
 def test_compute_suggestion_name_error():
     def f():
@@ -76,19 +84,19 @@ def test_compute_suggestion_name_error():
     try:
         f()
     except NameError as e:
-        assert fmt(e) == "NameError: name 'ab' is not defined. Did you mean: abc?\n"
+        assert fmt(e) == "NameError: name 'ab' is not defined. Did you mean: 'abc'?\n"
 
 def test_compute_suggestion_name_error_from_global():
     def f():
-        test_triang
+        test_compute_suggestion_name_error_from_globl
 
     try:
         f()
     except NameError as e:
-        assert fmt(e) == "NameError: name 'test_triang' is not defined. Did you mean: test_triangle?\n"
+        assert fmt(e) == "NameError: name 'test_compute_suggestion_name_error_from_globl' is not defined. Did you mean: 'test_compute_suggestion_name_error_from_global'?\n"
 
 def test_compute_suggestion_name_error_from_builtin():
     try:
         ab
     except NameError as e:
-        assert fmt(e) == "NameError: name 'ab' is not defined. Did you mean: abs?\n"
+        assert fmt(e) == "NameError: name 'ab' is not defined. Did you mean: 'abs'?\n"
