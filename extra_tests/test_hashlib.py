2ceeb487309a4993553c1e616ee33296710cf5d4
Matti Picus <matti.picus@gmail.com>
2023-07-11 09:10:19 +0300

    fix, test locking in HMAC update (issue 3962)

--- extra_tests/test_hashlib.py
+++ extra_tests/test_hashlib.py
@@ -7,3 +7,9 @@ def test_python_names():
     hashlib.new("md5_sha1")
     hashlib.new("sha512_224")
     hashlib.new("sha512_256")
+
+def test_large_hmac():
+    # issue 3962: problem with large msg code path
+    import hmac
+    m  = hmac.HMAC(b'', msg=b'0'*2049, digestmod='sha256')
+    assert len(m.digest()) == 32
