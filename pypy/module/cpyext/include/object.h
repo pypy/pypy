7a47073b63c5a2d817167f9a19b747f85c00e12d
mattip <matti.picus@gmail.com>
2024-01-09 21:15:12 +0200

    implement Py_Is in terms of space.is_w (issue #4044)

--- pypy/module/cpyext/include/object.h
+++ pypy/module/cpyext/include/object.h
@@ -608,9 +608,7 @@ Don't forget to apply Py_INCREF() when returning this value!!!
 PyAPI_DATA(PyObject) _Py_NoneStruct; /* Don't use this directly */
 #define Py_None (&_Py_NoneStruct)
 
-// Test if an object is the None singleton, the same as "x is None" in Python.
-PyAPI_FUNC(int) Py_IsNone(PyObject *x);
-#define Py_IsNone(x) Py_Is((x), Py_None)
+#define Py_IsNone(x) ((x) == Py_None)
 
 /* Macro for returning Py_None from a function */
 #define Py_RETURN_NONE return Py_NewRef(Py_None)
