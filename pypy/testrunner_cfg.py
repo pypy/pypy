1858b1fb93c9695286dd31fed0b25b06adec2109
Matti Picus <matti.picus@gmail.com>
2024-04-21 00:24:59 +1000

    try harder to disable host JIT for aarch64 backend tests on macos-arm64

--- pypy/testrunner_cfg.py
+++ pypy/testrunner_cfg.py
@@ -1,5 +1,12 @@
 # nightly test configuration for the paraller runner
 import os
+import platform
+import sys
+
+IS_ARM64 = platform.machine() == 'arm64'
+IS_MACOS = sys.platform == 'darwin'
+IS_PYPY = 'pypyjit' in sys.builtin_module_names
+IS_PYPY_MACOS_ARM64 = IS_ARM64 and IS_MACOS and IS_PYPY
 
 DIRS_SPLIT = [
     'translator/c', 'rlib',
@@ -10,6 +17,8 @@ DIRS_SPLIT = [
     'module/_hpy_universal/test/_vendored',
 ]
 
+pytestpath = os.path.abspath('pytest.py')
+
 def collect_one_testdir(testdirs, reldir, tests):
     for dir in DIRS_SPLIT:
         if reldir.startswith(dir):
@@ -18,6 +27,10 @@ def collect_one_testdir(testdirs, reldir, tests):
     else:
         testdirs.append(reldir)
 
+def get_test_driver(testdir):
+    if "jit/backend/aarch64" in testdir and IS_PYPY_MACOS_ARM64:
+        return ["--jit", "off",  pytestpath]
+    return [pytestpath]
 
 _cherrypick = os.getenv('PYPYCHERRYPICK', '')
 if _cherrypick:
