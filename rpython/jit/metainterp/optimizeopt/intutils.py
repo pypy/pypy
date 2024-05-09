pypy/pypy

commit f6635a7af296b6af972335a959d5ff923305cb8d

fix bug in leading_zeros_mask
leading_zeros_mask wrongly treated signed MAXINT special, but its
arguments and results are unsigned.

Co-authored-by: Max Bernstein <max@bernsteinbear.com>

rpython/jit/metainterp/optimizeopt/intutils.py
@@ -1406,12 +1406,7 @@ def leading_zeros_mask(n):
     of `n` are set (1).
     """
     assert isinstance(n, r_uint)
-    if n == MAXINT:
-        return r_uint(0)
-    elif n == 0:
-        return r_uint(-1)
-    else:
-        return ~next_pow2_m1(n)
+    return ~next_pow2_m1(n)
 
 def lowest_set_bit_only(val_uint):
     """

rpython/jit/metainterp/optimizeopt/test/test_intbound.py
@@ -1,6 +1,7 @@
 import pytest
 from rpython.jit.metainterp.optimizeopt.intutils import (IntBound,
-     next_pow2_m1, msbonly, MININT, MAXINT, lowest_set_bit_only)
+     next_pow2_m1, msbonly, MININT, MAXINT, lowest_set_bit_only,
+     leading_zeros_mask)
 from copy import copy
 import sys
 from rpython.rlib.rarithmetic import LONG_BIT, ovfcheck, r_uint, intmask
@@ -527,6 +528,22 @@ def test_next_pow2_m1():
     assert next_pow2_m1(r_uint(80)) == r_uint(127)
     assert next_pow2_m1(r_uint((1 << 32) - 5)) == r_uint((1 << 32) - 1)
     assert next_pow2_m1(r_uint((1 << 64) - 1)) == r_uint((1 << 64) - 1)
+
+def test_leading_zeros_mask():
+    assert leading_zeros_mask(r_uint(0)) == r_uint(-1)
+    assert leading_zeros_mask(r_uint(-1)) == r_uint(0)
+    assert leading_zeros_mask(r_uint(0b100)) == ~r_uint(0b111)
+    assert leading_zeros_mask(r_uint(MAXINT)) == ~r_uint(MAXINT)
+
+def test_shrink_bug():
+    lower = 1
+    upper = MAXINT - 1
+    assert r_uint(lower) ^ r_uint(upper) == r_uint(MAXINT)
+    b = IntBound(lower, upper, do_shrinking=False)
+    b._shrink_knownbits_by_bounds()
+    # the sign bit (highest bit) must be 0, because all values 1 <= x <= MAXINT - 1 are positive
+    assert b.tmask == r_uint(MAXINT) # 0?.....?
+    assert b.tvalue == r_uint(0)
 
 def test_invert_bound():
     for _, _, b1 in some_bounds():

commit f72b52d8e6f42546ff9c39b77e73fccc0a977567

fix tests that were broken by f6635a7af296b6af972335a959d5ff923305cb8d
(those tests could have shown us the bug earlier!)

rpython/jit/metainterp/optimizeopt/intutils.py
@@ -149,7 +149,7 @@ class IntBound(AbstractInfo):
     def __repr__(self):
         if self.lower == MININT and self.upper == MAXINT and self.tmask == TNUM_ONLY_MASK_UNKNOWN:
             return "IntBound.unbounded()"
-        if self.lower == 0 and self.upper == MAXINT and self.tmask == TNUM_ONLY_MASK_UNKNOWN:
+        if self.lower == 0 and self.upper == MAXINT and self._are_bounds_implied():
             return "IntBound.nonnegative()"
         if self.is_constant():
             return "IntBound.from_constant(%s)" % self._to_dec_or_hex_str_heuristics(self.get_constant_int())

rpython/jit/metainterp/optimizeopt/test/test_intbound.py
@@ -1170,7 +1170,7 @@ def test_intbound_str():
     b = IntBound()
     assert str(b) == '(?)'
     b = IntBound.nonnegative()
-    assert str(b) == '(0 <= ?)'
+    assert str(b) == '(0 <= 0b0?...?)'
     b = IntBound(lower=0, upper=100)
     assert str(b) == '(0 <= 0b0...0??????? <= 100)'
     b = IntBound().urshift_bound(IntBound.from_constant(10))