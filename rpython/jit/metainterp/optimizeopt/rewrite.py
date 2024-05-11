pypy/pypy

commit 800c101008745cb467845abac373a877eedae2d3

x // (1 << y) == x >> y

rpython/jit/metainterp/optimizeopt/rewrite.py
@@ -12,7 +12,7 @@ from rpython.jit.metainterp.optimizeopt.util import (
     _findall, make_dispatcher_method, have_dispatcher_method, get_box_replacement)
 from rpython.jit.metainterp.resoperation import (
     rop, ResOperation, opclasses, OpHelpers)
-from rpython.rlib.rarithmetic import highest_bit
+from rpython.rlib.rarithmetic import highest_bit, LONG_BIT
 import math
 
 
@@ -888,6 +888,13 @@ class OptRewrite(Optimization):
         self.optimizer.notice_guard_future_condition(op)
 
     def _optimize_CALL_INT_PY_DIV(self, op):
+        def _replace_with_shift(self, op, arg1, shiftvar):
+            from rpython.jit.metainterp.history import DONT_CHANGE
+            op = self.replace_op_with(op, rop.INT_RSHIFT,
+                        args=[arg1, shiftvar],
+                        descr=DONT_CHANGE)  # <- xxx rename? means "kill"
+            self.optimizer.send_extra_operation(op)
+            return True
         arg1 = op.getarg(1)
         b1 = self.getintbound(arg1)
         arg2 = op.getarg(2)
@@ -898,6 +905,16 @@ class OptRewrite(Optimization):
             self.last_emitted_operation = REMOVED
             return True
         if not b2.is_constant():
+            shiftop = self.optimizer.as_operation(get_box_replacement(arg2))
+            if (shiftop and shiftop.opnum == rop.INT_LSHIFT and
+                    shiftop.getarg(0).is_constant() and
+                    shiftop.getarg(0).getint() == 1):
+                # x // (1 << y) == 1 >> y
+                # but only if 0 <= y < LONG_BIT
+                shiftvar = get_box_replacement(shiftop.getarg(1))
+                shiftbound = self.getintbound(shiftvar)
+                if shiftbound.known_nonnegative() and shiftbound.known_lt_const(LONG_BIT):
+                    return _replace_with_shift(self, op, arg1, shiftvar)
             return False
         val = b2.get_constant_int()
         if val <= 0:
@@ -907,12 +924,7 @@ class OptRewrite(Optimization):
             self.last_emitted_operation = REMOVED
             return True
         elif val & (val - 1) == 0:   # val == 2**shift
-            from rpython.jit.metainterp.history import DONT_CHANGE
-            op = self.replace_op_with(op, rop.INT_RSHIFT,
-                        args=[arg1, ConstInt(highest_bit(val))],
-                        descr=DONT_CHANGE)  # <- xxx rename? means "kill"
-            self.optimizer.send_extra_operation(op)
-            return True
+            return _replace_with_shift(self, op, arg1, ConstInt(highest_bit(val)))
         else:
             from rpython.jit.metainterp.optimizeopt import intdiv
             known_nonneg = b1.known_nonnegative()

rpython/jit/metainterp/optimizeopt/test/test_optimizeintbound.py
@@ -2341,6 +2341,23 @@ class TestOptimizeIntBounds(BaseTestBasic):
         """
         self.optimize_loop(ops, expected)
 
+    def test_intdiv_pow2(self):
+        ops = """
+        [i0, i1]
+        i2 = int_and(i0, 31)
+        i3 = int_lshift(1, i2)
+        i4 = call_pure_i(321, i1, i3, descr=int_py_div_descr)
+        jump(i4)
+        """
+        expected = """
+        [i0, i1]
+        i2 = int_and(i0, 31)
+        i3 = int_lshift(1, i2)
+        i4 = int_rshift(i1, i2)
+        jump(i4)
+        """
+        self.optimize_loop(ops, expected)
+
 
 class TestComplexIntOpts(BaseTestBasic):
 

commit 45e496c72d2762c255be916b60b07412b2d64a47

x // (1 << y) == x >> y is not safe if y == 63
bug introduced in 800c101008745cb467845abac373a877eedae2d3, found by
enough fuzzing

rpython/jit/metainterp/optimizeopt/rewrite.py
@@ -925,10 +925,10 @@ class OptRewrite(Optimization):
                     shiftop.getarg(0).is_constant() and
                     shiftop.getarg(0).getint() == 1):
                 # x // (1 << y) == 1 >> y
-                # but only if 0 <= y < LONG_BIT
+                # but only if 0 <= y < LONG_BIT - 1
                 shiftvar = get_box_replacement(shiftop.getarg(1))
                 shiftbound = self.getintbound(shiftvar)
-                if shiftbound.known_nonnegative() and shiftbound.known_lt_const(LONG_BIT):
+                if shiftbound.known_nonnegative() and shiftbound.known_lt_const(LONG_BIT - 1):
                     return _replace_with_shift(self, op, arg1, shiftvar)
             return False
         val = b2.get_constant_int()

rpython/jit/metainterp/optimizeopt/test/test_optimizeintbound.py
@@ -2417,21 +2417,35 @@ class TestOptimizeIntBounds(BaseTestBasic):
         self.optimize_loop(ops, expected)
 
     def test_intdiv_pow2(self):
-        ops = """
+        ops1 = """
         [i0, i1]
-        i2 = int_and(i0, 31)
-        i3 = int_lshift(1, i2)
+        i6 = int_ge(i0, 0)
+        guard_true(i6) []
+        i7 = int_lt(i0, 63)
+        guard_true(i7) []
+        i3 = int_lshift(1, i0)
         i4 = call_pure_i(321, i1, i3, descr=int_py_div_descr)
         jump(i4)
         """
-        expected = """
+        expected1 = """
         [i0, i1]
-        i2 = int_and(i0, 31)
-        i3 = int_lshift(1, i2)
-        i4 = int_rshift(i1, i2)
+        i6 = int_ge(i0, 0)
+        guard_true(i6) []
+        i7 = int_lt(i0, 63)
+        guard_true(i7) []
+        i3 = int_lshift(1, i0)
+        i4 = int_rshift(i1, i0)
         jump(i4)
         """
-        self.optimize_loop(ops, expected)
+        ops2 = """
+        [i0, i1]
+        i2 = int_and(i0, 63)
+        i3 = int_lshift(1, i2)
+        i4 = call_i(321, i1, i3, descr=int_py_div_descr)
+        jump(i4)
+        """
+        self.optimize_loop(ops1, expected1)
+        self.optimize_loop(ops2, ops2)
 
     def test_knownbits_equality(self):
         ops = """
@@ -2817,6 +2831,8 @@ class TestOptimizeIntBounds(BaseTestBasic):
             """ % divisor
             expected = """
             [i1]
+            i3 = int_ge(i1, 0)
+            guard_true(i3) []
             i4 = uint_mul_high(i1, %d)
             i2 = uint_rshift(i4, %d)
             jump(i2)
@@ -2931,7 +2947,16 @@ class TestOptimizeIntBounds(BaseTestBasic):
         jump(i5)
         """
         expected = """
-        [i4, i5]
+        [i4]
+        i1 = int_ge(i4, -50)
+        guard_true(i1) []
+        i2 = int_le(i4, -40)
+        guard_true(i2) []
+        # here, -50 <= i4 <= -40
+        i3 = int_xor(i4, -1),
+        i5 = uint_mul_high(i3, -8608480567731124087),
+        i6 = uint_rshift(i5, 4),
+        i7 = int_xor(i6, -1),
         jump(-2)
         """
         self.optimize_loop(ops, expected)
@@ -3153,6 +3178,26 @@ class TestOptimizeIntBounds(BaseTestBasic):
         """
         self.optimize_loop(ops, expected)
 
+    def test_div_bug(self):
+        ops = """
+[i0, i1, i2, i3]
+i53 = int_and(i2, 63)
+i54 = int_lshift(1, i53)
+i83 = int_ne(i3, i1)
+i123 = int_add(42, i83)
+i130 = call_pure_i(12, -87905, i54, descr=int_py_div_descr) []
+i181 = int_sub(i123, i130)
+i316 = int_and(i181, -17)
+guard_value(i316, 42) []
+finish()
+        """
+        # run without timeout
+        oldtimeout = pytest.config.option.z3timeout
+        pytest.config.option.z3timeout = None
+        try:
+            self.optimize_loop(ops, ops.replace("call_pure_i", "call_i"))
+        finally:
+            pytest.config.option.z3timeout = oldtimeout
 
 class TestComplexIntOpts(BaseTestBasic):
 