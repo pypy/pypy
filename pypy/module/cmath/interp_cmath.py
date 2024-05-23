2a262ffb85c8a60cd94ccc29df9c11a4995ee8f7
devdanzin <74280297+devdanzin@users.noreply.github.com>
2024-05-23 18:03:22 -0300

    Add tau to cmath module.

--- pypy/module/cmath/interp_cmath.py
+++ pypy/module/cmath/interp_cmath.py
@@ -7,6 +7,7 @@ from pypy.module.cmath.moduledef import names_and_docstrings
 from rpython.rlib import rcomplex, rfloat
 
 pi   = math.pi
+tau  = math.tau
 e    = math.e
 inf  = float('inf')
 nan  = float('nan')
