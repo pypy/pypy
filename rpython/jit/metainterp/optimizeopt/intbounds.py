dbc36ad6fd85e1666aa47088577c2be55abd27a7
CF Bolz-Tereick <cfbolz@gmx.de>
2024-06-14 11:28:06 +0200

    normalize int_eq(x, 0) to int_is_zero and int_ne(x, 0) to int_is_true

--- rpython/jit/metainterp/optimizeopt/intbounds.py
+++ rpython/jit/metainterp/optimizeopt/intbounds.py
@@ -472,18 +472,38 @@ class OptIntBounds(Optimization):
             self.make_constant_int(op, 1)
         elif b1.is_constant() and b1.get_constant_int() == 1 and b0.is_bool():
             self.make_equal_to(op, op.getarg(0))
+        elif b0.is_constant() and b0.get_constant_int() == 0:
+            op = self.replace_op_with(op, rop.INT_IS_ZERO,
+                        args=[arg1])
+            self.optimizer.send_extra_operation(op)
+            return
+        elif b1.is_constant() and b1.get_constant_int() == 0:
+            op = self.replace_op_with(op, rop.INT_IS_ZERO,
+                        args=[arg0])
+            self.optimizer.send_extra_operation(op)
+            return
         else:
             return self.emit(op)
 
     def optimize_INT_NE(self, op):
         arg0 = get_box_replacement(op.getarg(0))
-        b1 = self.getintbound(arg0)
+        b0 = self.getintbound(arg0)
         arg1 = get_box_replacement(op.getarg(1))
-        b2 = self.getintbound(arg1)
-        if b1.known_ne(b2):
+        b1 = self.getintbound(arg1)
+        if b0.known_ne(b1):
             self.make_constant_int(op, 1)
         elif arg0 is arg1:
             self.make_constant_int(op, 0)
+        elif b0.is_constant() and b0.get_constant_int() == 0:
+            op = self.replace_op_with(op, rop.INT_IS_TRUE,
+                        args=[arg1])
+            self.optimizer.send_extra_operation(op)
+            return
+        elif b1.is_constant() and b1.get_constant_int() == 0:
+            op = self.replace_op_with(op, rop.INT_IS_TRUE,
+                        args=[arg0])
+            self.optimizer.send_extra_operation(op)
+            return
         else:
             return self.emit(op)
 
