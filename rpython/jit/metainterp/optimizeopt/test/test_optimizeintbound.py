304b0a898116eed1916c25cfe1c9012b910f051a
CF Bolz-Tereick <cfbolz@gmx.de>
2024-07-06 22:36:44 +0200

    another variant of commutativity

--- rpython/jit/metainterp/optimizeopt/test/test_optimizeintbound.py
+++ rpython/jit/metainterp/optimizeopt/test/test_optimizeintbound.py
@@ -851,8 +851,23 @@ class TestOptimizeIntBounds(BaseTestBasic):
         [i0, i1]
         i2 = int_add_ovf(i0, i1)
         guard_no_overflow() []
-        i3 = int_add(i1, i0)
+        i3 = int_add_ovf(i1, i0)
         guard_no_overflow() []
+        jump(i2, i3)
+        """
+        expected = """
+        [i0, i1]
+        i2 = int_add_ovf(i0, i1)
+        guard_no_overflow() []
+        jump(i2, i2)
+        """
+        self.optimize_loop(ops, expected)
+
+        ops = """
+        [i0, i1]
+        i2 = int_add_ovf(i0, i1)
+        guard_no_overflow() []
+        i3 = int_add(i1, i0)
         jump(i2, i3)
         """
         expected = """
