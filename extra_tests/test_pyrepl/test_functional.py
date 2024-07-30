09492445cb88b12575f95845b1d86157385dcbfe
CF Bolz-Tereick <cfbolz@gmx.de>
2024-07-30 12:48:19 +0200

    start some basic tests for pyrepl

--- extra_tests/test_pyrepl/test_functional.py
+++ extra_tests/test_pyrepl/test_functional.py
@@ -3,26 +3,45 @@
 # License: MIT
 # some functional tests, to see if this is really working
 
-import pytest
-import sys
+from contextlib import contextmanager
+import sys, os
 
 
-@pytest.fixture()
-def child():
+@contextmanager
+def start_repl():
     try:
         import pexpect
     except ImportError:
         pytest.skip("no pexpect module")
     except SyntaxError:
         pytest.skip('pexpect wont work on py3k')
-    child = pexpect.spawn(sys.executable, ['-S'], timeout=10)
+    child = pexpect.spawn(
+        sys.executable,
+        ['-S', '-c', "from pyrepl.main import interactive_console as __pyrepl_interactive_console; __pyrepl_interactive_console()"],
+        env=os.environ | {"PYTHON_COLORS": "0"},
+        timeout=10, encoding="utf-8")
     child.logfile = sys.stdout
-    child.sendline('from pyrepl.python_reader import main')
-    child.sendline('main()')
-    return child
+    yield child
+    child.close()
 
 
-def test_basic(child):
-    child.sendline('a = 3')
-    child.sendline('a')
-    child.expect('3')
+def test_basic():
+    with start_repl() as child:
+        child.sendline('a = 120202012012')
+        child.sendline('a')
+        child.expect('120202012012')
+
+def test_error():
+    with start_repl() as child:
+        child.sendline('1/0')
+        child.expect('Traceback.*File.*1/0.*ZeroDivisionError: division by zero')
+
+def test_ctrl_left_ctrl_right():
+    with start_repl() as child:
+        child.send('abc 123456789')
+        child.send('\033[1;5D') # ctrl-left
+        child.send('=')
+        child.send('\033[1;5C') # ctrl-right
+        child.sendline('88888')
+        child.sendline('abc')
+        child.expect('12345678988888')
