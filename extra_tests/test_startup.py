pypy/pypy

commit e4aa24d5f2bf05927599ac708ab619fdb241c943

implement https://bugs.python.org/issue1054041
an uncaught ctrl-C/KeyboardInterrupt turns into the right exit code

pypy/interpreter/app_main.py
@@ -134,6 +134,9 @@ def run_toplevel(f, *fargs, **fkwds):
             sys.setprofile(None)
     except SystemExit as e:
         handle_sys_exit(e)
+    except KeyboardInterrupt as e:
+        # handled a level up
+        raise
     except BaseException as e:
         display_exception(e)
         return False
@@ -869,6 +872,19 @@ def run_command_line(interactive,
         status = e.code
         if inspect_requested():
             display_exception(e)
+    except KeyboardInterrupt as e:
+        display_exception(e)
+        status = True
+        if not inspect_requested():
+            try:
+                import _signal
+                import os
+            except ImportError:
+                pass
+            else:
+                _signal.signal(_signal.SIGINT, _signal.SIG_DFL)
+                os.kill(os.getpid(), _signal.SIGINT);
+                assert 0, "should be unreachable"
     else:
         status = not success
 

pypy/interpreter/test/test_app_main.py
@@ -1067,6 +1067,15 @@ class TestNonInteractive:
         assert status == 1
         assert data.startswith("15\\u20ac ('strict', 'backslashreplace')")
 
+    def test_keyboardinterrupt_exit_signal(self):
+        import subprocess, signal, os
+        p= subprocess.Popen([get_python3(), app_main, "-c", "raise KeyboardInterrupt"],
+                            stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        assert p.returncode == -signal.SIGINT
+
 
 @py.test.mark.skipif('config.getoption("runappdirect")')
 class AppTestAppMain:

commit f3d1b3e55e4f6a5ed500bb2015fdfbb2e81ab386

fix gh-4995: make sure to call space.finish() after receiving sigint
this was broken by the code in e4aa24d5f2bf05927599ac708ab619fdb241c943

pypy/interpreter/app_main.py
@@ -92,6 +92,12 @@ except ImportError:
 import errno
 import sys
 
+try:
+    prepare_shutdown = sys._pypy_prepare_shutdown
+    del sys._pypy_prepare_shutdown
+except AttributeError:
+    prepare_shutdown = lambda: 0
+
 _MACOSX = sys.platform == 'darwin'
 
 DEBUG = False       # dump exceptions before calling the except hook
@@ -1046,14 +1052,9 @@ def run_command_line(interactive,
             except ImportError:
                 pass
             else:
-                try:
-                    sys.stdout.flush()
-                except Exception:
-                    pass
-                try:
-                    sys.stderr.flush()
-                except Exception:
-                    pass
+                prepare_shutdown()
+                # make sure we quit with the right process exit code on
+                # receiving a SIGINT
                 _signal.signal(_signal.SIGINT, _signal.SIG_DFL)
                 os.kill(os.getpid(), _signal.SIGINT);
                 assert 0, "should be unreachable"

pypy/interpreter/baseobjspace.py
@@ -482,7 +482,11 @@ class ObjSpace(object):
         w_atexit = self.getbuiltinmodule('atexit')
         try:
             self.call_method(w_atexit, '_run_exitfuncs')
-        except OperationError:
+        except OperationError as e:
+            try:
+                e.write_unraisable(self, '_run_exitfuncs')
+            except Exception:
+                pass
             # discard exceptions, see call_py_exitfuncs in pylifecycle.c in
             # CPython
             pass

pypy/module/sys/moduledef.py
@@ -112,6 +112,8 @@ class Module(MixedModule):
 
         'unraisablehook'        : 'vm.unraisablehook',
         '__unraisablehook__'    : 'vm.unraisablehook',
+
+        '_pypy_prepare_shutdown' : 'vm._pypy_prepare_shutdown',
         }
 
     if sys.platform == 'win32':

pypy/module/sys/vm.py
@@ -500,4 +500,11 @@ def unraisablehook(space, w_hookargs):
     OperationError.write_unraisable_default(space, w_type, w_value, w_tb, err_msg, w_object, extra_line)
 
 
+def _pypy_prepare_shutdown(space):
+    """ prepare for interpreter shutdown by calling atexit hooks and flushing
+    file descriptors. should only be called by pypy code. """
+    try:
+        return space.newint(space.finish())
+    except OperationError as e:
+        e.write_unraisable(space, 'interpreter shutdown')
 

extra_tests/test_startup.py
@@ -1,7 +1,11 @@
+import signal
+import time
+import os
+import subprocess
+import sys
 
 def test_platform_not_imported():
     import subprocess
-    import sys
     out = subprocess.check_output([sys.executable, '-c',
          'import sys; print(list(sys.modules.keys()))'], universal_newlines=True)
     modules = [x.strip(' "\'') for x in out.strip().strip('[]').split(',')]
@@ -12,7 +16,6 @@ def test_platform_not_imported():
 def test_executable_win32():
     # issue #4003
     import os
-    import subprocess
     import sys
 
     out = subprocess.check_output([r'*', '-c',
@@ -27,3 +30,18 @@ def test_executable_win32():
         assert out.strip() == repr(sys.executable)
     else:
         assert out.strip() == repr('')
+
+
+def test_ctrl_c_causes_atexit():
+    process = subprocess.Popen(
+            [sys.executable,
+             '-c',
+             "import atexit, time, sys; atexit.register(lambda : print('called atexit')); print('sleeping'); sys.stdout.flush(); time.sleep(100)"],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    line = process.stdout.readline()
+    assert line == b'sleeping\n'
+    os.kill(process.pid, signal.SIGINT)
+    process.wait()
+    assert process.returncode == -signal.SIGINT
+    line = process.stdout.read()
+    assert line == b'called atexit\n'