81eb5ae95bb072806850c0916f8e103a4a613aa4
mattip <matti.picus@gmail.com>
2024-08-17 20:25:42 +0300

    add a test for traceback.format(..., colorize) (issue 5004)

--- pypy/interpreter/test/apptest_traceback.py
+++ pypy/interpreter/test/apptest_traceback.py
@@ -161,7 +161,7 @@ def test_colors_in_traceback():
         original_exc_format = sys.excepthook(
             exc_info.type, exc_info.value, exc_info.value.__traceback__
         )
-        sys.stderr = original_exc_format
+        sys.stderr = original_std_err
         expected_exc_format = [
             f'    a      + {ANSIColors.BOLD_RED}b / 0{ANSIColors.RESET} # abc',
             f'             {ANSIColors.BOLD_RED}^^^^^{ANSIColors.RESET}',
@@ -174,3 +174,36 @@ def test_colors_in_traceback():
             del os.environ['FORCE_COLOR']
         else:
             os.environ['FORCE_COLOR'] = old_value
+
+def test_old_format_works():
+    """test that overriding traceback.TracebackException with a class
+    that doesn't know about colorize (like exceptiongroup._formatting) still works
+    issue 5004
+    """
+    import traceback
+    
+    def division_by_zero(a, b):
+        return (
+            a      + b / 0 # abc
+        )
+    
+    class MyTracebackException(traceback.TracebackException):
+
+        def format(self, chain=None):
+            return "done"
+
+    traceback_exception_original_format = traceback.TracebackException.format
+    traceback.TracebackException.format = MyTracebackException.format
+    try:
+        with raises(ZeroDivisionError) as exc_info:
+            division_by_zero(1, 2)
+
+        original_std_err = sys.stderr
+        sys.stderr = buffer = Buffer()
+        lines = traceback.format_exception(exc_info.type, exc_info.value, exc_info.value.__traceback__)
+        sys.stderr = original_std_err
+        expected_exc_format = ["done"]
+        assert buffer.get_lines()[-1:] == expected_exc_format
+    finally:
+        traceback.TracebackException.format = traceback_exception_original_format
+
