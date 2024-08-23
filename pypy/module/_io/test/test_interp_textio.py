d3397c189d3926913f72c796ccd61214af2c7435
CF Bolz-Tereick <cfbolz@gmx.de>
2024-08-23 11:29:11 +0200

    a hypothesis test for tell

--- pypy/module/_io/test/test_interp_textio.py
+++ pypy/module/_io/test/test_interp_textio.py
@@ -152,3 +152,29 @@ def test_next_char(text):
         pass
     assert buf.exhausted()
     assert u''.join(chars) == text
+
+
+@given(content=st_readline(), data=st.data())
+def test_tell(space, content, data):
+    txt, limits = content
+
+    restxt = translate_newlines(txt)
+    read_chars_before_seeking = data.draw(st.integers(min_value=0, max_value=len(restxt)))
+    w_stream = W_BytesIO(space)
+    w_stream.descr_init(space, space.newbytes(txt.encode('utf-8')))
+
+    w_textio = W_TextIOWrapper(space)
+    w_textio.descr_init(
+        space, w_stream,
+        encoding='utf-8'
+    )
+
+    w_res1 = w_textio.read_w(space, space.newint(read_chars_before_seeking))
+    assert space.len_w(w_res1) <= read_chars_before_seeking
+    w_tell = w_textio.tell_w(space)
+    w_textio.seek_w(space, space.newint(0)) # seek to beginning
+    w_textio.seek_w(space, w_tell) # then back to position
+    w_res2 = w_textio.read_w(space)
+    w_res = space.add(w_res1, w_res2)
+    res = space.text_w(w_res)
+    assert res == restxt
