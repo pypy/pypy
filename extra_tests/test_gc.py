494ff36035e99ffa72c7be8ee2a285474283a04e
CF Bolz-Tereick <cfbolz@gmx.de>
2024-08-29 18:18:42 +0200

    gh-5005: fix the gc.get_stats output
    before, the sums didn't work out correctly

--- extra_tests/test_gc.py
+++ extra_tests/test_gc.py
@@ -1,3 +1,4 @@
+import pytest
 import gc
 
 def test_memory_doesnt_jump_during_sweeping():
@@ -19,3 +20,23 @@ def test_memory_doesnt_jump_during_sweeping():
     assert minimum >= maximum // 2
 
     gc.hooks.on_gc_collect_step = lambda stats: None
+
+def test_gc_stats_sum_is_correct():
+    def extract_mb(line):
+        if '(' in line:
+            line = line[:line.index('(')].strip()
+        assert line.endswith("MB")
+        return float(line[:-2].rsplit(' ', 1)[-1])
+
+    l1 = [str(i) for i in range(100000)]
+    l2 = [str(i) for i in range(100000)]
+    l1[:] = []
+    gc.collect()
+    for with_memory_pressure in [0, 1]:
+        stats = str(gc.get_stats(with_memory_pressure))
+        first_half, second_half = stats.split("Total memory allocated")
+        for half in first_half, second_half:
+            lines = half.splitlines()
+            total = extract_mb(lines[1])
+            summed = extract_mb(lines[2]) + extract_mb(lines[3]) + extract_mb(lines[4])
+            assert 0.9 < summed / total < 1.1
