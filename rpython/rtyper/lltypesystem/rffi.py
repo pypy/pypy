4448bc2c6bb468e9e6d632d9c8ba11d0ec903fa4
mattip <matti.picus@gmail.com>
2024-09-04 11:35:57 +0300

    fix more tests, add rffi.CONST_VOIDP

--- rpython/rtyper/lltypesystem/rffi.py
+++ rpython/rtyper/lltypesystem/rffi.py
@@ -744,6 +744,7 @@ r_singlefloat = rarithmetic.r_singlefloat
 
 # void *   - for now, represented as char *
 VOIDP = lltype.Ptr(lltype.Array(lltype.Char, hints={'nolength': True, 'render_as_void': True}))
+CONST_VOIDP = lltype.Ptr(lltype.Array(lltype.Char, hints={'nolength': True, 'render_as_void': True, 'render_as_const': True}))
 NULL = None
 
 # void **
