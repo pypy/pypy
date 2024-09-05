0ad9b3eb826d9b0a8a8bb12278185630273b1fcf
mattip <matti.picus@gmail.com>
2024-09-05 15:25:45 +0300

    fix for cffi dev version

--- pypy/module/_cffi_backend/test/test_file.py
+++ pypy/module/_cffi_backend/test/test_file.py
@@ -12,7 +12,7 @@ def test_same_file():
     source = source[source.index('# _____________'):]
     dest = dest[dest.index('# _____________'):]
 
-    # source is for version 1.17.1dev0, dest is 1.17.0
+    # source is for upstream dev version, dest can be different
     source = source.replace("1.18.0.dev0", "1.17.0")
     # remove patch for issue 4937. Leave the starting #
     pstart = dest.index("# XXX patch start") + 1
