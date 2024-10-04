f90d23f4c1b551b94bfc2bc9e8bd63ddabbf1361
CF Bolz-Tereick <cfbolz@gmx.de>
2024-10-04 18:36:06 +0200

    start generating codes for the patterns

---
+++ rpython/jit/metainterp/ruleopt/codegen.py
@@ -0,0 +1,222 @@
+from contextlib import contextmanager
+from collections import defaultdict
+
+from rpython.jit.metainterp.ruleopt import parse
+
+commutative_ops = {"int_add", "int_mul"}
+
+
+def generate_commutative_patterns_args(args):
+    if not args:
+        yield []
+        return
+    arg0 = args[0]
+    args1 = args[1:]
+    for subarg0 in generate_commutative_patterns(arg0):
+        for subargs1 in generate_commutative_patterns_args(args1):
+            yield [subarg0] + subargs1
+
+
+def generate_commutative_patterns(pattern):
+    if not isinstance(pattern, parse.PatternOp):
+        yield pattern
+        return
+    for subargs in generate_commutative_patterns_args(pattern.args):
+        if pattern.opname not in commutative_ops:
+            yield pattern.newargs(subargs)
+        else:
+            yield pattern.newargs(subargs)
+            yield pattern.newargs(subargs[::-1])
+
+
+def generate_commutative_rules(rule):
+    for pattern in generate_commutative_patterns(rule.pattern):
+        yield parse.Rule(rule.name, pattern, rule.elements, rule.target)
+
+def sort_rules(rules):
+    return sorted(
+        rules, key=lambda rule: (rule.target.sort_key_result(), rule.pattern.sort_key())
+    )
+
+
+
+class Codegen(parse.Visitor):
+    def __init__(self):
+        self.code = []
+        self.level = 0
+
+    @contextmanager
+    def emit_indent(self, line=None):
+        if line is not None:
+            self.emit(line)
+        self.level += 1
+        yield
+        self.level -= 1
+
+    def emit_stacking_condition(self, cond):
+        self.emit("if %s:" % cond)
+        self.level += 1
+
+    def emit(self, line=""):
+        if self.level == 0 and line.startswith(("def ", "class ")):
+            self.code.append("")
+        if not line.strip():
+            self.code.append("")
+        else:
+            self.code.append("    " * self.level + line)
+
+    def visit_PatternVar(self, p, varname, intbound_name):
+        if p.name.startswith("C"):
+            cname = "C_" + varname
+            if p.name not in self.bindings:
+                self.emit_stacking_condition("%s.is_constant()" % intbound_name)
+                self.emit("%s = %s.get_constant_int()" % (cname, intbound_name))
+                self.bindings[p.name] = cname
+                return
+            elif cname == self.bindings[p.name]:
+                return
+            return self.emit_stacking_condition(
+                "%s.known_eq_const(%s)" % (intbound_name, self.bindings[p.name])
+            )
+        else:
+            if p.name not in self.bindings:
+                self.bindings[p.name] = varname
+                return
+            elif varname == self.bindings[p.name]:
+                return
+            return self.emit_stacking_condition(
+                "%s is %s" % (varname, self.bindings[p.name])
+            )
+    def visit_PatternConst(self, p, varname, intbound_name):
+        return self.emit_stacking_condition(
+            "%s.known_eq_const(%s)" % (intbound_name, p.const)
+        )
+
+    def visit_PatternOp(self, p, varname, intbound_name):
+        boxname = "%s_%s" % (varname, p.opname)
+        self.emit(
+            "%s = self.optimizer.as_operation(%s, rop.%s)"
+            % (boxname, varname, p.opname.upper())
+        )
+        self.emit_stacking_condition("%s is not None" % boxname)
+        boxnames, boundnames = self._emit_arg_reads(boxname, varname, len(p.args))
+        self._pattern_arg_check(boxnames, boundnames, p.args)
+        return
+
+    def generate_target(self, target):
+        if isinstance(target, parse.PatternVar):
+            self.emit("self.make_equal_to(op, %s)" % self.bindings[target.name])
+            return
+        if isinstance(target, parse.PatternConst):
+            self.emit("self.make_constant_int(op, %s)" % target.const)
+            return
+        if isinstance(target, parse.PatternOp):
+            args = []
+            for arg in target.args:
+                if isinstance(arg, parse.PatternVar):
+                    if arg.name.startswith('C'):
+                        args.append("ConstInt(%s)" % self.bindings[arg.name])
+                    else:
+                        args.append(self.bindings[arg.name])
+                elif isinstance(arg, PatternConst):
+                    args.append("ConstInt(%s)" % arg.const)
+                else:
+                    assert 0
+            self.emit(
+                "newop = self.replace_op_with(op, rop.%s, args=[%s])"
+                % (target.opname.upper(), ", ".join(args))
+            )
+            self.emit("self.optimizer.send_extra_operation(newop)")
+            return
+        assert 0
+
+    def _pattern_arg_check(self, boxnames, boundnames, args):
+        for i, p in enumerate(args):
+            self.visit(p, boxnames[i], boundnames[i])
+
+    def _emit_arg_reads(self, prefix, opname, numargs):
+        boxnames = []
+        boundnames = []
+        for i in range(numargs):
+            boxname = "%s_%s" % (prefix, i)
+            boundname = "b_" + boxname
+            boxnames.append(boxname)
+            boundnames.append(boundname)
+            self.emit("%s = get_box_replacement(%s.getarg(%s))" % (boxname, opname, i))
+            self.emit("%s = self.getintbound(%s)" % (boundname, boxname))
+        return boxnames, boundnames
+
+    def generate_method(self, opname, rules):
+        with self.emit_indent("def optimize_%s(self, op):" % opname.upper()):
+            numargs = len(rules[0].pattern.args)
+            boxnames, boundnames = self._emit_arg_reads("arg", "op", numargs)
+            all_rules = []
+            for rule in rules:
+                all_rules.extend(generate_commutative_rules(rule))
+            all_rules = sort_rules(all_rules)
+            for rule in all_rules:
+                self.bindings = {}
+                self.emit("# %s: %s => %s" % (rule.name, rule.pattern, rule.target))
+                currlevel = self.level
+                checks = []
+                self._pattern_arg_check(boxnames, boundnames, rule.pattern.args)
+                for el in rule.elements:
+                    self.visit(el)
+                self.generate_target(rule.target)
+                self.emit("return")
+                self.level = currlevel
+
+    def visit_Compute(self, el):
+        self.bindings[el.name] = el.name
+        res = self.visit(el.expr)
+        self.emit("%s = %s" % (el.name, res))
+
+    def visit_Check(self, el):
+        res = self.visit(el.expr)
+        self.emit_stacking_condition(res)
+
+    def visit_BinOp(self, expr, prec=0):
+        left_prec = expr.precedence
+        right_prec = expr.precedence + 1 # all left assoc for now
+        left = self.visit(expr.left, left_prec)
+        right = self.visit(expr.right, right_prec)
+        res = "%s %s %s" % (left, expr.pysymbol, right)
+        if prec > expr.precedence:
+            res = "(" + res + ")"
+        return res
+
+    def visit_Name(self, expr, prec=0):
+        return expr.name
+
+    def visit_Attribute(self, expr, prec=0):
+        return "%s.%s" % (expr.varname, expr.attrname)
+
+    def visit_Number(self, expr, prec=0):
+        return str(expr.value)
+
+    def visit_UnaryOp(self, expr, prec=0):
+        sub_prec = expr.precedence
+        sub = self.visit(expr.left, sub_prec + 1)
+        res = "%s%s" % (expr.pysymbol, sub)
+        if prec > expr.precedence:
+            res = "(" + res + ")"
+        return res
+
+    def visit_MethodCall(self, expr, prec=0):
+        sub_prec = expr.precedence
+        receiver = self.visit(expr.value)
+        args = [self.visit(arg) for arg in expr.args]
+        return "%s.%s(%s)" % (receiver, expr.methname, ", ".join(args))
+
+    def visit_FuncCall(self, expr, prec=0):
+        args = [self.visit(arg) for arg in expr.args]
+        return "%s(%s)" % (expr.funcname, ", ".join(args))
+
+    def generate_code(self, ast):
+        per_op = defaultdict(list)
+        for rule in ast.rules:
+            per_op[rule.pattern.opname].append(rule)
+        for opname, rules in per_op.items():
+            self.generate_method(opname, rules)
+        self.emit()
+        return "\n".join(self.code)
---
+++ rpython/jit/metainterp/ruleopt/test/test_codegen.py
@@ -0,0 +1,275 @@
+import pytest
+try:
+    import rply
+    import z3
+except ImportError:
+    pytest.skip('rply or z3 not installed')
+
+from rpython.rlib.rarithmetic import LONG_BIT, r_uint, intmask, ovfcheck, uint_mul_high
+
+from rpython.jit.metainterp.ruleopt.codegen import *
+from rpython.jit.metainterp.ruleopt.parse import *
+
+import os
+with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "all.rules")) as f:
+    ALLRULES = f.read()
+
+
+def test_generate_commutative_rules():
+    s = """\
+add_zero: int_add(x, 0)
+    => x
+"""
+    ast = parse(s)
+    patterns = list(generate_commutative_patterns(ast.rules[0].pattern))
+    assert patterns == [
+        PatternOp(
+            args=[PatternVar(name="x"), PatternConst(const="0")], opname="int_add"
+        ),
+        PatternOp(
+            args=[PatternConst(const="0"), PatternVar(name="x")], opname="int_add"
+        ),
+    ]
+    assert len(patterns) == 2
+
+    s = """\
+add_reassoc_consts: int_add(int_add(x, C1), C2)
+    C = C1 + C2
+    => int_add(x, C)
+"""
+    ast = parse(s)
+    patterns = list(generate_commutative_patterns(ast.rules[0].pattern))
+    assert patterns == [
+        PatternOp(
+            opname="int_add",
+            args=[
+                PatternOp(opname="int_add", args=[PatternVar("x"), PatternVar("C1")]),
+                PatternVar("C2"),
+            ],
+        ),
+        PatternOp(
+            opname="int_add",
+            args=[
+                PatternVar("C2"),
+                PatternOp(opname="int_add", args=[PatternVar("x"), PatternVar("C1")]),
+            ],
+        ),
+        PatternOp(
+            opname="int_add",
+            args=[
+                PatternOp(opname="int_add", args=[PatternVar("C1"), PatternVar("x")]),
+                PatternVar("C2"),
+            ],
+        ),
+        PatternOp(
+            opname="int_add",
+            args=[
+                PatternVar("C2"),
+                PatternOp(opname="int_add", args=[PatternVar("C1"), PatternVar("x")]),
+            ],
+        ),
+    ]
+
+def test_sort_patterns():
+    s = """\
+int_sub_zero: int_sub(x, 0)
+    => x
+int_sub_x_x: int_sub(x, x)
+    => 0
+int_sub_add: int_sub(int_add(x, y), y)
+    => x
+int_sub_zero_neg: int_sub(0, x)
+    => int_neg(x)
+    """
+    ast = parse(s)
+    rules = sort_rules(ast.rules)
+    assert rules == [
+        Rule(
+            name="int_sub_x_x",
+            pattern=PatternOp(
+                opname="int_sub", args=[PatternVar("x"), PatternVar("x")]
+            ),
+            elements=[],
+            target=PatternConst("0"),
+        ),
+        Rule(
+            name="int_sub_zero",
+            pattern=PatternOp(
+                opname="int_sub", args=[PatternVar("x"), PatternConst("0")]
+            ),
+            elements=[],
+            target=PatternVar("x"),
+        ),
+        Rule(
+            name="int_sub_add",
+            pattern=PatternOp(
+                opname="int_sub",
+                args=[
+                    PatternOp(
+                        opname="int_add", args=[PatternVar("x"), PatternVar("y")]
+                    ),
+                    PatternVar("y"),
+                ],
+            ),
+            elements=[],
+            target=PatternVar("x"),
+        ),
+        Rule(
+            name="int_sub_zero_neg",
+            pattern=PatternOp(
+                opname="int_sub", args=[PatternConst("0"), PatternVar("x")]
+            ),
+            elements=[],
+            target=PatternOp(opname="int_neg", args=[PatternVar("x")]),
+        ),
+    ]
+
+
+def test_generate_expr_precedence():
+    c = Codegen()
+    res = c.visit(Mul(Sub(Name('a'), Name('b')), Name('c')))
+    assert res == '(a - b) * c'
+    res = c.visit(Sub(Mul(Name('a'), Name('b')), Name('c')))
+    assert res == 'a * b - c'
+    res = c.visit(Sub(Sub(Name('a'), Name('b')), Name('c')))
+    assert res == 'a - b - c'
+    res = c.visit(Sub(Name('a'), Sub(Name('b'), Name('c'))))
+    assert res == 'a - (b - c)'
+
+def test_generate_code():
+    s = """\
+sub_zero: int_sub(x, 0)
+    => x
+sub_x_x: int_sub(x, x)
+    => 0
+sub_add: int_sub(int_add(x, y), y)
+    => x
+sub_zero_neg: int_sub(0, x)
+    => int_neg(x)
+sub_add_const: int_sub(int_add(x, C), C) # nonsense rule, but I want to see const matching working
+    => x
+sub_add_consts: int_sub(int_add(x, C1), C2)
+    C = C2 - C1
+    => int_sub(x, C)
+and_x_c_in_range: int_and(x, C)
+    check x.lower >= 0 and x.upper <= C & ~(C + 1)
+    => x
+    """
+    codegen = Codegen()
+    res = codegen.generate_code(parse(s))
+    print(res)
+    assert (
+        res
+        == """
+def optimize_INT_SUB(self, op):
+    arg_0 = get_box_replacement(op.getarg(0))
+    b_arg_0 = self.getintbound(arg_0)
+    arg_1 = get_box_replacement(op.getarg(1))
+    b_arg_1 = self.getintbound(arg_1)
+    # sub_x_x: int_sub(x, x) => 0
+    if arg_1 is arg_0:
+        self.make_constant_int(op, 0)
+        return
+    # sub_zero: int_sub(x, 0) => x
+    if b_arg_1.known_eq_const(0):
+        self.make_equal_to(op, arg_0)
+        return
+    # sub_add_const: int_sub(int_add(x, C), C) => x
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if b_arg_0_int_add_1.is_constant():
+            C_arg_0_int_add_1 = b_arg_0_int_add_1.get_constant_int()
+            if b_arg_1.known_eq_const(C_arg_0_int_add_1):
+                self.make_equal_to(op, arg_0_int_add_0)
+                return
+    # sub_add_const: int_sub(int_add(C, x), C) => x
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if b_arg_0_int_add_0.is_constant():
+            C_arg_0_int_add_0 = b_arg_0_int_add_0.get_constant_int()
+            if b_arg_1.known_eq_const(C_arg_0_int_add_0):
+                self.make_equal_to(op, arg_0_int_add_1)
+                return
+    # sub_add: int_sub(int_add(x, y), y) => x
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if arg_1 is arg_0_int_add_1:
+            self.make_equal_to(op, arg_0_int_add_0)
+            return
+    # sub_add: int_sub(int_add(y, x), y) => x
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if arg_1 is arg_0_int_add_0:
+            self.make_equal_to(op, arg_0_int_add_1)
+            return
+    # sub_zero_neg: int_sub(0, x) => int_neg(x)
+    if b_arg_0.known_eq_const(0):
+        newop = self.replace_op_with(op, rop.INT_NEG, args=[arg_1])
+        self.optimizer.send_extra_operation(newop)
+        return
+    # sub_add_consts: int_sub(int_add(x, C1), C2) => int_sub(x, C)
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if b_arg_0_int_add_1.is_constant():
+            C_arg_0_int_add_1 = b_arg_0_int_add_1.get_constant_int()
+            if b_arg_1.is_constant():
+                C_arg_1 = b_arg_1.get_constant_int()
+                C = C2 - C1
+                newop = self.replace_op_with(op, rop.INT_SUB, args=[arg_0_int_add_0, ConstInt(C)])
+                self.optimizer.send_extra_operation(newop)
+                return
+    # sub_add_consts: int_sub(int_add(C1, x), C2) => int_sub(x, C)
+    arg_0_int_add = self.optimizer.as_operation(arg_0, rop.INT_ADD)
+    if arg_0_int_add is not None:
+        arg_0_int_add_0 = get_box_replacement(arg_0.getarg(0))
+        b_arg_0_int_add_0 = self.getintbound(arg_0_int_add_0)
+        arg_0_int_add_1 = get_box_replacement(arg_0.getarg(1))
+        b_arg_0_int_add_1 = self.getintbound(arg_0_int_add_1)
+        if b_arg_0_int_add_0.is_constant():
+            C_arg_0_int_add_0 = b_arg_0_int_add_0.get_constant_int()
+            if b_arg_1.is_constant():
+                C_arg_1 = b_arg_1.get_constant_int()
+                C = C2 - C1
+                newop = self.replace_op_with(op, rop.INT_SUB, args=[arg_0_int_add_1, ConstInt(C)])
+                self.optimizer.send_extra_operation(newop)
+                return
+
+def optimize_INT_AND(self, op):
+    arg_0 = get_box_replacement(op.getarg(0))
+    b_arg_0 = self.getintbound(arg_0)
+    arg_1 = get_box_replacement(op.getarg(1))
+    b_arg_1 = self.getintbound(arg_1)
+    # and_x_c_in_range: int_and(x, C) => x
+    if b_arg_1.is_constant():
+        C_arg_1 = b_arg_1.get_constant_int()
+        if x.lower >= 0 and x.upper <= C & ~(C + 1):
+            self.make_equal_to(op, arg_0)
+            return
+"""
+    )
+
+def test_generate_code_many():
+    codegen = Codegen()
+    res = codegen.generate_code(parse(ALLRULES))
+    print(res)
+
