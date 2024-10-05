85ac4b697e286edd8c57c9a4dbd1541f2d71bb3a
CF Bolz-Tereick <cfbolz@gmx.de>
2024-10-05 14:20:30 +0200

    remaining two rshift rules ported

--- rpython/jit/metainterp/optimizeopt/rewrite.py
+++ rpython/jit/metainterp/optimizeopt/rewrite.py
@@ -160,17 +160,6 @@ class OptRewrite(Optimization):
             return True
         return False
 
-    def optimize_INT_RSHIFT(self, op):
-        b1 = self.getintbound(op.getarg(0))
-        b2 = self.getintbound(op.getarg(1))
-
-        if b2.known_eq_const(0):
-            self.make_equal_to(op, op.getarg(0))
-        elif b1.known_eq_const(0):
-            self.make_constant_int(op, 0)
-        else:
-            return self.emit(op)
-
     def postprocess_INT_XOR(self, op):
         arg0 = get_box_replacement(op.getarg(0))
         arg1 = get_box_replacement(op.getarg(1))
