3022549fad5064153fb0138d2495cca64758bafa
CF Bolz-Tereick <cfbolz@gmx.de>
2024-10-29 11:57:25 +0100

    add a test why c6470ae83dc6bd1fdff574821b2203574b60796b doesn't work

--- pypy/interpreter/test/apptest_function.py
+++ pypy/interpreter/test/apptest_function.py
@@ -913,3 +913,8 @@ def test_method_doc():
     doc2 = X.cm.__doc__
     assert doc1 == doc2 == 'A class method'
     assert isinstance(MethodType.__doc__, str)
+
+def test_method_getattr():
+    class A:
+        def f(self): pass
+    assert A().f.__code__ is A.f.__code__
