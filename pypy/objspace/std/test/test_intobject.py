9d69fa1e0baee7f334a188e763a466b27f6d91af
CF Bolz-Tereick <cfbolz@gmx.de>
2024-11-01 18:52:23 +0100

    optimize overflowing int*int multiplication that produces a long result
    instead of first converting one of the arguments to an rbigint, and then
    calling rbigint.int_mul, just construct the rbigint result directly.
    this is about 40% faster than before.

--- pypy/objspace/std/test/test_intobject.py
+++ pypy/objspace/std/test/test_intobject.py
@@ -129,6 +129,13 @@ class TestW_IntObject:
         v = f1.descr_mul(space, f2)
         assert space.isinstance_w(v, space.w_long)
         assert space.bigint_w(v).eq(rbigint.fromlong(x * y))
+        x = sys.maxint // 4
+        y = sys.maxint // 8
+        f1 = iobj.W_IntObject(x)
+        f2 = iobj.W_IntObject(y)
+        v = f1.descr_mul(space, f2)
+        assert space.isinstance_w(v, space.w_long)
+        assert space.bigint_w(v).eq(rbigint.fromlong(x * y))
 
     def test_div(self):
         space = self.space
