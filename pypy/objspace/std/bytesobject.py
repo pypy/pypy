003a94cac740c109a42a0a489468f9b1d18ba083
mattip <matti.picus@gmail.com>
2024-11-11 16:35:25 +0200

    remove multiple utf8_w definitions

--- pypy/objspace/std/bytesobject.py
+++ pypy/objspace/std/bytesobject.py
@@ -452,12 +452,6 @@ class W_BytesObject(W_AbstractBytesObject):
     def utf8_w(self, space):
         return self._value
 
-    def utf8_w(self, space):
-        return self._value
-
-    def utf8_w(self, space):
-        return self._value
-
     def buffer_w(self, space, flags):
         space.check_buf_flags(flags, True)
         return SimpleView(StringBuffer(self._value), w_obj=self)
