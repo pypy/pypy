68e1707038e5ad59570d4c5249f74349f5595c04
mattip <matti.picus@gmail.com>
2024-11-15 16:00:14 +0200

    pypy has an extra frame in async errors

--- lib-python/3/test/test_contextlib_async.py
+++ lib-python/3/test/test_contextlib_async.py
@@ -541,6 +541,8 @@ class TestAsyncExitStack(TestBaseExitStack, unittest.TestCase):
         ('__exit__', 'return self.run_coroutine(self.__aexit__(*exc_details))'),
         ('run_coroutine', 'raise exc'),
         ('run_coroutine', 'raise exc'),
+        # PyPy addition
+        ('__step', 'result = coro.send(None)'),
         ('__aexit__', 'raise exc_details[1]'),
         ('__aexit__', 'cb_suppress = cb(*exc_details)'),
     ]
