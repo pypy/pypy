870665aa13b4335ac1aa5eb5e2c49240c9e2e71b
Manuel Jacob <me@manueljacob.de>
2025-05-29 20:53:41 +0200

    Revert "port cpy starred expression error message improvements".
    This reverts commit bf3f9e5a65f80205d78d428a10511c69bf216f10.
    
    That commit broke translation. Excerpt from https://github.com/pypy/pypy/actions/runs/15325721219/job/43119624898#step:6:107 :
    
    [translation:info] Error:
       File "/home/runner/work/pypy/pypy/rpython/translator/goal/translate.py", line 320, in main
        drv.proceed(goals)
       File "/home/runner/work/pypy/pypy/rpython/translator/driver.py", line 568, in proceed
        result = self._execute(goals, task_skip = self._maybe_skip())
       File "/home/runner/work/pypy/pypy/rpython/translator/tool/taskengine.py", line 114, in _execute
        res = self._do(goal, taskcallable, *args, **kwds)
       File "/home/runner/work/pypy/pypy/rpython/translator/driver.py", line 278, in _do
        res = func()
       File "/home/runner/work/pypy/pypy/rpython/translator/driver.py", line 315, in task_annotate
        s = annotator.build_types(self.entry_point, self.inputtypes)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 92, in build_types
        return self.build_graph_types(flowgraph, inputs_s, complete_now=complete_now)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 140, in build_graph_types
        self.complete()
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 229, in complete
        self.complete_pending_blocks()
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 224, in complete_pending_blocks
        self.processblock(graph, block)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 398, in processblock
        self.flowin(graph, block)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 572, in flowin
        self.follow_link(graph, link, constraints)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 603, in follow_link
        self.addpendingblock(graph, link.target, inputs_s)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 189, in addpendingblock
        self.mergeinputargs(graph, block, cells)
       File "/home/runner/work/pypy/pypy/rpython/annotator/annrpython.py", line 435, in mergeinputargs
        unions = [annmodel.unionof(c1,c2) for c1, c2 in zip(oldcells,inputcells)]
       File "/home/runner/work/pypy/pypy/rpython/annotator/model.py", line 783, in unionof
        s1 = pair(s1, s2).union()
       File "/home/runner/work/pypy/pypy/rpython/annotator/binaryop.py", line 93, in union
        raise UnionError(obj1, obj2)
    [translation:ERROR] UnionError:
    
    Offending annotations:
      SomeList(listdef=<[SomeInstance(can_be_None=False, classdef=pypy.interpreter.astcompiler.ast.AST)]>)
      SomeInstance(can_be_None=False, classdef=pypy.interpreter.astcompiler.ast.Name)
    
    In <FunctionGraph of (pypy.interpreter.pyparser.rpypegparse:7743)PythonParser._tmp_145 at 0x781f2a30>:
    <return block>
    Processing block:
     block@37[mark_0...] is a <class 'rpython.flowspace.flowcontext.SpamBlock'>
     in (pypy.interpreter.pyparser.rpypegparse:7743)PythonParser._tmp_145
     containing the following operations:
           v991 = getattr(self_267, ('_tmp_234'))
           _tmp_234_0 = simple_call(v991)
           v992 = bool(_tmp_234_0)
     --end--

--- pypy/interpreter/pyparser/test/test_pyparse.py
+++ pypy/interpreter/pyparser/test/test_pyparse.py
@@ -436,11 +436,6 @@ if 1:
         assert info.value.offset == 8
         assert info.value.end_offset == 9
 
-    def test_invalid_star(self):
-        info = pytest.raises(SyntaxError, self.parse, "f(**kw, *)")
-        assert "Invalid star expression" in info.value.msg
-        assert info.value.offset == 10
-
 
 class TestPythonParserRevDB(TestPythonParser):
     spaceconfig = {"translation.reverse_debugger": True}
