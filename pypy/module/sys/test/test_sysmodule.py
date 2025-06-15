13bd270d9e9cb7585705c5144182afd6d333e802
mattip <matti.picus@gmail.com>
2025-06-15 14:41:14 +0300

    add debug for failing test

--- pypy/module/sys/test/test_sysmodule.py
+++ pypy/module/sys/test/test_sysmodule.py
@@ -858,6 +858,9 @@ class AppTestSysSettracePortedFromCpython(object):
 
         def compare_events(line_offset, events, expected_events):
             events = [(l - line_offset, e) for (l, e) in events]
+            print("len(events)", len(events))
+            print("events", [e for e in events])
+            print("len(expected_events)", len(expected_events))
             assert events == expected_events
 
         def run_test2(func):
