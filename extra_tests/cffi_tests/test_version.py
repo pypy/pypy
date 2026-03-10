1ad8828cf0427c3ce910bc5c095d1157a4948731
mattip <matti.picus@gmail.com>
2026-03-10 12:23:24 +0200

    skip pycparser version test, upstream HEAD is py3 only

--- extra_tests/cffi_tests/test_version.py
+++ extra_tests/cffi_tests/test_version.py
@@ -5,6 +5,7 @@ import urllib2
 
 import cffi
 import pypy
+import pytest
 
 metadata = py.path.local(pypy.__file__)/'../../lib_pypy/cffi.dist-info/METADATA'
 
@@ -12,11 +13,13 @@ def test_metadata():
     info = Parser().parsestr(metadata.read())
     assert info['version'] == cffi.__version__
 
+@pytest.skipif('sys.version_info[0] < 3', reason="upstream is python3-only")
 def test_pycparser_version():
     url = 'https://raw.githubusercontent.com/eliben/pycparser/master/pycparser/__init__.py'
     source = urllib2.urlopen(url).read()
     dest = py.path.local(__file__).join('..', '..', '..', 'lib_pypy', 'cffi',
                                         '_pycparser', '__init__.py').read()
     # if this fails, the vendored pycparser is not the latest version
+    # lib_pypy/cffi/_pycparser/README
     assert source.strip() == dest.strip()
     
