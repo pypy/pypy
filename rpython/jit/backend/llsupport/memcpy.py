78769883fc60755a0a1513c8e998e508781ec67f
Matti Picus <matti.picus@gmail.com>
2026-05-27 22:17:52 +0300

    fix another signature of memcpy

--- rpython/jit/backend/llsupport/memcpy.py
+++ rpython/jit/backend/llsupport/memcpy.py
@@ -1,6 +1,6 @@
 from rpython.rtyper.lltypesystem import lltype, rffi, llmemory
 
-memcpy_fn = rffi.llexternal('memcpy', [llmemory.Address, llmemory.Address,
+memcpy_fn = rffi.llexternal('memcpy', [llmemory.Address, rffi.CONST_VOIDP,
                                        rffi.SIZE_T], rffi.VOIDP,
                             sandboxsafe=True, _nowrapper=True)
 memset_fn = rffi.llexternal('memset', [llmemory.Address, rffi.INT,
