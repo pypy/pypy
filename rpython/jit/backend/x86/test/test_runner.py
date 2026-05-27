59df9425652d0bc150fe1c92edd5db1cd92c240d
Matti Picus <matti.picus@gmail.com>
2026-05-27 21:59:29 +0300

    cmp -> cmpq in newer assembler, accept both

--- rpython/jit/backend/x86/test/test_runner.py
+++ rpython/jit/backend/x86/test/test_runner.py
@@ -39,7 +39,7 @@ class TestX86(LLtypeBackendTest):
                                  'nopl?; '    # for the label
                                  'add; test; je; jmp;')   # plus some padding
         bridge_loop_instructions = (
-            'cmp; jl; mov(abs)?; jmp;')
+            'cmpq?; jl; mov(abs)?; jmp;')
 
     def get_cpu(self):
         cpu = CPU(rtyper=None, stats=FakeStats())
