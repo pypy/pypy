2dc6c89ec74cf876f9d6f711c67eb9002c7e99fc
mattip <matti.picus@gmail.com>
2026-07-22 17:01:54 +0300

    try harder to release ll2ctypes in test teardown

--- pypy/module/bz2/test/support.py
+++ pypy/module/bz2/test/support.py
@@ -3,12 +3,15 @@ class CheckAllocation:
         from rpython.rtyper.lltypesystem import ll2ctypes
         import gc
         tries = 20
-        # remove the GC strings from ll2ctypes
-        for key, value in ll2ctypes.ALLOCATED.items():
-            if value._TYPE._gckind == 'gc':
-                del ll2ctypes.ALLOCATED[key]
-        #
-        while tries and ll2ctypes.ALLOCATED:
+        while tries:
+            # gc-kind entries (rpy_string keepalives, some created while
+            # running finalizers below) are not real leaks -- drop them so
+            # only raw buffer allocations remain to be checked.
+            for key, value in list(ll2ctypes.ALLOCATED.items()):
+                if value._TYPE._gckind == 'gc':
+                    del ll2ctypes.ALLOCATED[key]
+            if not ll2ctypes.ALLOCATED:
+                break
             gc.collect() # to make sure we disallocate buffers
             self.space.getexecutioncontext()._run_finalizers_now()
             tries -= 1
