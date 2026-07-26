d1c63027c948513b39c8674aa708b972abf760c4
Matti Picus <matti.picus@gmail.com>
2026-07-26 08:02:02 +0300

    redo d95aca26de to not raise in ll function

--- rpython/rlib/rmmap.py
+++ rpython/rlib/rmmap.py
@@ -796,11 +796,7 @@ if _POSIX:
         if we_are_translated():
             flags = NonConstant(flags)
             prot = NonConstant(prot)
-        res = c_mmap_safe(hintp, map_size, prot, flags, -1, 0)
-        if res == rffi.cast(PTR, -1):
-            errno = rposix.get_saved_errno()
-            raise OSError(errno, os.strerror(errno))
-        return res
+        return c_mmap_safe(hintp, map_size, prot, flags, -1, 0)
 
     def clear_large_memory_chunk_aligned(addr, map_size):
         addr = rffi.cast(PTR, addr)
