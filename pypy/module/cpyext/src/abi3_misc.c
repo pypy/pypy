bdbe236b9ea36497431f2f45a932382cec830ee6
mattip <matti.picus@gmail.com>
2026-09-06 20:17:06 +0300

    fix merge

--- pypy/module/cpyext/src/abi3_misc.c
+++ pypy/module/cpyext/src/abi3_misc.c
@@ -53,18 +53,6 @@ PyAPI_FUNC(int) PyDict_MergeFromSeq2(PyObject *d, PyObject *seq2, int override)
     return -1;
 }
 
-PyAPI_FUNC(int) PyCFunction_GetFlags(PyObject * _a0)
-{
-    PyErr_SetString(PyExc_NotImplementedError, "PyCFunction_GetFlags() is not implemented in PyPy");
-    return -1;
-}
-
-PyAPI_FUNC(PyObject *) PyCFunction_GetSelf(PyObject * _a0)
-{
-    PyErr_SetString(PyExc_NotImplementedError, "PyCFunction_GetSelf() is not implemented in PyPy");
-    return NULL;
-}
-
 PyAPI_FUNC(PyCodeObject *) PyFrame_GetCode(PyFrameObject *frame)
 {
     PyErr_SetString(PyExc_NotImplementedError, "PyFrame_GetCode() is not implemented in PyPy");
