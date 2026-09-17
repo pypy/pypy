faede4826e098ba0abefd3302fba99a348ddf325
mattip <matti.picus@gmail.com>
2026-09-17 17:57:47 +0300

    fix test

--- pypy/module/sys/test/test_sysmodule.py
+++ pypy/module/sys/test/test_sysmodule.py
@@ -1001,6 +1001,7 @@ class AppTestCurrentFramesWithThread:
         assert other_frame.f_code.co_name in ('other_thread', '?')
 
     def test_current_exceptions(self):
+        import sys
         if sys.platform in ('darwin', 'win32'):
             skip('test can hang on macos and windows')
         import sys
